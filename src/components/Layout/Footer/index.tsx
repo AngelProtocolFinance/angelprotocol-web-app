a1e19f5d657c1f0b8c916f3be4e652b603de71f8
green dev <greencodedev912@gmail.com>
2021-08-04 01:40:57 +0800

    complete footer bar

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -3,19 +3,30 @@ const Footer = () => {
 
   return (
     <footer>
-      <nav className="container mx-auto flex justify-between items-center h-16 px-4">
-        <a href="/" className="font-regular text-xs">
-          &copy; AngelProtocol 2021 — {currentDate.getFullYear()}
+      <nav className="container mx-auto flex justify-between items-center h-16 px-4 bg-footer">
+        <a href="/" className="font-sans text-xs uppercase text-white">
+          Copyright 2021 angelprotocol. All rights reserved.
         </a>
-        <ul className="flex font-regular text-base">
+        <ul className="flex font-montserrat text-base">
           <li className="mr-4">
-            <a href="#">Terms &amp; Privacy</a>
+            <a href="#" className="uppercase text-white">
+              About Us
+            </a>
           </li>
           <li className="mr-4">
-            <a href="#">About Us</a>
+            <a href="#" className="uppercase text-white">
+              Donate Now
+            </a>
+          </li>
+          <li className="mr-4">
+            <a href="#" className="uppercase text-white">
+              For Charities
+            </a>
           </li>
           <li>
-            <a href="#">About UNSDGs</a>
+            <a href="#" className="uppercase text-white">
+              About UNSDGs
+            </a>
           </li>
         </ul>
       </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'montserrat': ['Montserrat', 'serif']
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'footer': '#262729',
+     })
   },
   variants: {
     extend: {},
