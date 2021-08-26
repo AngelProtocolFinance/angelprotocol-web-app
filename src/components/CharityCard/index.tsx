3ba08e5817481f45db3a434e38b507e492ece73f
jpmariano <juanpaolo.mariano28@gmail.com>
2021-08-26 13:38:57 +0800

    Used an anchor tag in the Charity Card component

--- src/components/CharityCard/index.tsx
+++ src/components/CharityCard/index.tsx
@@ -11,15 +11,17 @@ const CharityCard = ({
 }: CharityCardProps) => {
   return (
     <article className="w-64 h-48 ml-4 flex-none ">
-      <img
-        className="rounded-lg img-no-drag"
-        src={backgroundImageUrl}
-        alt="charity banner"
-      />
-      <h1 className="font-bold text-base uppercase text-grey-light font-bold mt-1">
-        {title}
-      </h1>
-      <p className="text-xs text-grey-light">{description}</p>
+      <a href={`/donate/${title}`}>
+        <img
+          className="rounded-lg img-no-drag"
+          src={backgroundImageUrl}
+          alt="charity banner"
+        />
+        <h1 className="font-bold text-base uppercase text-grey-light font-bold mt-1">
+          {title}
+        </h1>
+        <p className="text-xs text-grey-light">{description}</p>
+      </a>
     </article>
   );
 };
