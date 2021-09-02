4dc89272cf2970fc26af802c0ce5e6b56190a800
ap-justin <justin@angelprotocol.io>
2021-09-02 14:51:07 +0800

    done with supporters section

--- src/pages/Home/Sponsors.tsx
+++ src/pages/Home/Sponsors.tsx
@@ -1,3 +1,57 @@
+//supporters icons
+import alice from "assets/icons/sponsors/alice.png";
+import apollo from "assets/icons/sponsors/apollo.png";
+import astral from "assets/icons/sponsors/astral.png";
+import betterra from "assets/icons/sponsors/betterra.png";
+import fantasyinvestar from "assets/icons/sponsors/fantasyinvestar.png";
+import harpoon from "assets/icons/sponsors/harpoon.png";
+import kado from "assets/icons/sponsors/kado.png";
+import kash from "assets/icons/sponsors/kash.png";
+import loop from "assets/icons/sponsors/loop.png";
+import loterra from "assets/icons/sponsors/loterra.png";
+import mavolo from "assets/icons/sponsors/mavolo.png";
+import orion from "assets/icons/sponsors/orion.png";
+import plutospot from "assets/icons/sponsors/plutospot.png";
+import pylon from "assets/icons/sponsors/pylon.png";
+import smartstake from "assets/icons/sponsors/smartstake.png";
+import spaar from "assets/icons/sponsors/spaar.png";
+import spar from "assets/icons/sponsors/spar.png";
+import starterra from "assets/icons/sponsors/starterra.png";
+import talis from "assets/icons/sponsors/talis.png";
+
+const supporterIcons = [
+  alice,
+  apollo,
+  astral,
+  betterra,
+  fantasyinvestar,
+  harpoon,
+  kado,
+  kash,
+  loop,
+  loterra,
+  mavolo,
+  orion,
+  plutospot,
+  pylon,
+  smartstake,
+  spaar,
+  spar,
+  starterra,
+  talis,
+];
+
 export default function Sponsors() {
-  return <section className="h-96 bg-blue-400">sponsors</section>;
+  return (
+    <section className="grid content-start justify-items-center h-96 bg-light-grey">
+      <h3 className="text-2xl text-angel-grey font-bold my-10">SUPPORTED BY</h3>
+      <ul className="flex justify-center flex-wrap gap-3 max-w-3xl">
+        {supporterIcons.map((icon) => (
+          <li>
+            <img src={icon} className="w-16" />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
