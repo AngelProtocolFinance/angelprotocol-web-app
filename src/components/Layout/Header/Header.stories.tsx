4cd4642751b60960750e87f9a3b4ef7a6594c51d
ap-justin <justin@angelprotocol.io>
2021-09-09 09:44:56 +0800

    merge and resolved conflicts with main

--- src/components/Layout/Header/Header.stories.tsx
+++ src/components/Layout/Header/Header.stories.tsx
@@ -1,5 +1,19 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import {
+  ConnectType,
+  NetworkInfo,
+  StaticWalletProvider,
+  WalletStatus,
+} from "@terra-money/wallet-provider";
+
+import { MemoryRouter } from "react-router-dom";
 import Header from ".";
+
+const testnet: NetworkInfo = {
+  name: "testnet",
+  chainID: "tequila-0004",
+  lcd: "https://tequila-lcd.terra.dev",
+};
 
 export default {
   title: "Layout/Header",
@@ -7,7 +21,13 @@ export default {
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = () => {
-  return <Header />;
+  return (
+    <MemoryRouter>
+      <StaticWalletProvider defaultNetwork={testnet}>
+        <Header />;
+      </StaticWalletProvider>
+    </MemoryRouter>
+  );
 };
 
 export const Default = Template.bind({});
