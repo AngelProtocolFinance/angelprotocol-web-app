076cc79ea1d71f06df4c542cd0280c34eaa2c2ce
jpmariano <juanpaolo.mariano28@gmail.com>
2021-09-09 10:59:22 +0800

    Undefined route handling and redirect them to Donate page

--- src/constants/urls.tsx
+++ src/constants/urls.tsx
@@ -1,4 +1,3 @@
 export const API_URLS: any = {
-  TCA_login:
-    "https://mu2d2e0oj0.execute-api.us-east-1.amazonaws.com/tca-portal-login",
+  TCA_login: "https://mu2d2e0oj0.execute-api.us-east-1.amazonaws.com/tca-login",
 };
