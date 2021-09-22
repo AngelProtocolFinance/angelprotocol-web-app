ddf79b4ffa7933dc6fd4c3ac4a313aa78a6488f3
ap-justin <justin@angelprotocol.io>
2021-09-22 15:17:04 +0800

    added transitions to for-charities access-section

--- src/pages/Charities/Access.tsx
+++ src/pages/Charities/Access.tsx
@@ -1,27 +1,47 @@
 import vaultIcon from "assets/icons/36_network_secure_vault.svg";
 import liquidIcon from "assets/icons/08_money_flow.svg";
+import useObserve from "hooks/useObserver";
+import transitionIn, { Direction } from "helpers/transitionIn";
 
 export default function Access() {
+  const { isVisible, ref } = useObserve({ threshold: 0.5 });
   return (
-    <div className="py-24 px-10">
-      <h3 className="uppercase mb-4 text-2xl lg:text-3xl font-bold text-center text-blue-accent">
+    <div className="py-24 px-10" ref={ref}>
+      <h3
+        className={`${transitionIn(
+          isVisible,
+          Direction.fromLeft
+        )} uppercase mb-4 text-2xl lg:text-3xl font-bold text-center text-blue-accent`}
+      >
         Easy access to donated funds
       </h3>
-      <p className="text-center text-angel-grey text-lg lg:xl-lg mb-6">
+      <p
+        className={`${transitionIn(
+          isVisible,
+          Direction.fromRight
+        )} text-center text-angel-grey text-lg lg:xl-lg mb-6`}
+      >
         Donors choose what percent of their donation goes to your Endowment
         Account versus Liquid Account
       </p>
-      <div className="flex  flex-col items-center md:flex-row md:justify-center md:items-stretch gap-12">
+      <div
+        className={`${transitionIn(
+          isVisible,
+          Direction.fromBottom
+        )} flex  flex-col items-center md:flex-row md:justify-center md:items-stretch gap-12`}
+      >
         {articles.map((article) => (
           <article
             key={article.id}
             className="w-full max-w-lg shadow-md flex flex-col items-center p-6 border-4 border-blue-accent"
           >
             <img className="w-32 h-32" src={article.icon} />
-            <h4 className="uppercase text-lg lg:text-xl text-center font-bold text-angel-grey my-4">
+            <h4
+              className={`uppercase text-lg lg:text-xl text-center font-bold text-angel-grey my-4`}
+            >
               {article.title}
             </h4>
-            <p className="text-angel-grey text-center">{article.desc}</p>
+            <p className={`text-angel-grey text-center`}>{article.desc}</p>
           </article>
         ))}
       </div>
