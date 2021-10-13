a242a9251a9f0a2641b6b11721b9a3f0c2bdd9cf
ap-justin <justin@angelprotocol.io>
2021-10-13 21:00:16 +0800

    finished nested board layout

---
+++ src/pages/Leaderboard/BoardCharity.tsx
@@ -0,0 +1,7 @@
+export default function BoardCharity() {
+  return (
+    <div className="p-6 my-5 grid grid-cols-a1 place-items-center overflow-hidden bg-white-grey rounded-xl shadow-lg padded-container">
+      Board Charity
+    </div>
+  );
+}
--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,43 +1,25 @@
 import AppHead from "components/Headers/AppHead";
-import useLeaderboard from "./useLeaderboard";
-import tcaLogo from "assets/icons/tcalogo.png";
-import Entry from "./Entry";
-import { Names } from "./names";
-import Heading from "./Heading";
-import Loader from "components/Loader/Loader";
+import Nav from "./Nav";
+
+import { Route, Switch, useRouteMatch } from "react-router";
+import { app } from "types/routes";
+import BoardTCA from "./BoardTCA";
+import BoardCharity from "./BoardCharity";
 
 export default function Leaderboard() {
-  const { sums, error, isReady, isLoading } = useLeaderboard();
+  const { path } = useRouteMatch();
   //cast names to desired type
   return (
-    <section className="pb-16 grid h-screen grid-rows-a1">
+    <section className="pb-16 grid content-start h-screen">
       <AppHead />
-      <div className="p-6 my-10 grid grid-cols-a1 place-items-center overflow-hidden container mx-auto bg-white-grey  rounded-xl shadow-lg">
-        <img className="m-20 w-60 h-60" src={tcaLogo} />
-        {error && <p className="uppercase text-angel-grey">{error}</p>}
-        {isLoading && (
-          <Loader
-            gapClass="gap-4"
-            widthClass="w-4"
-            bgColorClass="bg-angel-grey"
-          />
-        )}
-        {isReady && (
-          <table className="border-collapse self-start w-full">
-            <thead className="">
-              <tr>
-                <Heading text="TCA Member" />
-                <Heading text="Total Donation" />
-              </tr>
-            </thead>
-            <tbody>
-              {sums.map(([name, amount], idx) => (
-                <Entry key={idx} name={name} amount={amount} />
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
+      <h3 className="pt-2 padded-container uppercase text-white-grey text-3xl font-bold">
+        Leaderboards
+      </h3>
+      <Nav />
+      <Switch>
+        <Route exact path={`${path}`} component={BoardCharity} />
+        <Route path={`${path}${app.board_tca}`} component={BoardTCA} />
+      </Switch>
     </section>
   );
 }
