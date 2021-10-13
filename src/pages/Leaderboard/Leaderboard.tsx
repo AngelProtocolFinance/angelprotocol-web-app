61d79688482d58df11da896181b6769cdea1f20a
ap-justin <justin@angelprotocol.io>
2021-10-13 21:28:17 +0800

    adjusted header to title space

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -12,7 +12,7 @@ export default function Leaderboard() {
   return (
     <section className="pb-16 grid content-start h-screen">
       <AppHead />
-      <h3 className="pt-2 padded-container uppercase text-white-grey text-3xl font-bold">
+      <h3 className="pt-8 padded-container uppercase text-white-grey text-3xl font-bold">
         Leaderboards
       </h3>
       <Nav />
