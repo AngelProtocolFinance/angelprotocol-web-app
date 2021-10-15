acdb1fd6ba0425cdffab61de47322ad03ca266b7
ap-justin <justin@angelprotocol.io>
2021-10-15 15:05:43 +0800

    refactor boards ui

--- src/pages/Leaderboard/Charity/Board.tsx
+++ src/pages/Leaderboard/Charity/Board.tsx
@@ -1,5 +1,7 @@
 import Loader from "components/Loader/Loader";
 import Heading from "../Heading";
+import Updator from "../Updator";
+import View from "../View";
 import Entry from "./Entry";
 // import { Addresses } from "./types";
 import useBoard from "./useBoard";
@@ -9,19 +11,12 @@ export default function BoardCharity() {
     useBoard();
 
   return (
-    <div className="relative min-h-leader-table p-6 pt-10 my-5 mt-2 grid place-items-center overflow-hidden bg-white rounded-xl shadow-lg padded-container">
-      <div className="flex absolute top-3 right-6 gap-2 text-sm font-body">
-        <p className="text-angel-grey">
-          last update: {new Date(lastUpdate).toLocaleTimeString()}
-        </p>
-        <button
-          className="uppercase text-xs bg-angel-blue disabled:bg-thin-grey px-2 text-white rounded-sm font-semibold"
-          disabled={isLoading}
-          onClick={refresh}
-        >
-          update
-        </button>
-      </div>
+    <View>
+      <Updator
+        isLoading={isLoading}
+        refresh={refresh}
+        lastUpdate={lastUpdate}
+      />
       {error && (
         <p className="grid items-center uppercase text-angel-grey">{error}</p>
       )}
@@ -47,45 +42,10 @@ export default function BoardCharity() {
               {charities.map(([address, balance], idx) => (
                 <Entry key={idx} balance={balance} address={address} />
               ))}
-              {/* <Entry
-                key={1}
-                balance={{ total_locked: 1000, total_liq: 1000, overall: 1000 }}
-                address={Addresses.terra12crxq8nxml96e9h38fe67c4p76pc24l54zjzzh}
-              />
-              <Entry
-                key={1}
-                balance={{ total_locked: 1000, total_liq: 1000, overall: 1000 }}
-                address={Addresses.terra12crxq8nxml96e9h38fe67c4p76pc24l54zjzzh}
-              />
-              <Entry
-                key={1}
-                balance={{ total_locked: 1000, total_liq: 1000, overall: 1000 }}
-                address={Addresses.terra12crxq8nxml96e9h38fe67c4p76pc24l54zjzzh}
-              />
-              <Entry
-                key={1}
-                balance={{ total_locked: 1000, total_liq: 1000, overall: 1000 }}
-                address={Addresses.terra12crxq8nxml96e9h38fe67c4p76pc24l54zjzzh}
-              />
-              <Entry
-                key={1}
-                balance={{ total_locked: 1000, total_liq: 1000, overall: 1000 }}
-                address={Addresses.terra12crxq8nxml96e9h38fe67c4p76pc24l54zjzzh}
-              />
-              <Entry
-                key={1}
-                balance={{ total_locked: 1000, total_liq: 1000, overall: 1000 }}
-                address={Addresses.terra12crxq8nxml96e9h38fe67c4p76pc24l54zjzzh}
-              />
-              <Entry
-                key={1}
-                balance={{ total_locked: 1000, total_liq: 1000, overall: 1000 }}
-                address={Addresses.terra12crxq8nxml96e9h38fe67c4p76pc24l54zjzzh}
-              /> */}
             </tbody>
           </table>
         </div>
       )}
-    </div>
+    </View>
   );
 }
