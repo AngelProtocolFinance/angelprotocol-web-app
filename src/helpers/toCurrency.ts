6d9f355ddd3ac4104a6775567c4046a3721114ab
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-10-21 19:06:36 +0800

    currency formatting display 2 decimals

--- src/helpers/toCurrency.ts
+++ src/helpers/toCurrency.ts
@@ -2,7 +2,7 @@ export default function toCurrency(num = 0) {
   //set local to undefined to use user's default format
 
   return num.toLocaleString(undefined, {
-    minimumFractionDigits: 3,
-    maximumFractionDigits: 3,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   });
 }
