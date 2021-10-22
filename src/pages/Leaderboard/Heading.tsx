e402039a79fb6c5a6c987e83af11fadf248cb978
ap-justin <justin@angelprotocol.io>
2021-10-22 15:51:59 +0800

    added tooltip to 10Y projection

--- src/pages/Leaderboard/Heading.tsx
+++ src/pages/Leaderboard/Heading.tsx
@@ -1,7 +1,37 @@
-export default function Heading({ text }: { text: string }) {
+import { ReactNode, useState } from "react";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+
+type Props =
+  | { text: string; children?: never }
+  | { text: string; children: ReactNode };
+export default function Heading(props: Props) {
+  const [hovered, setHovered] = useState(false);
+  const toolkit = !!props.children;
+
+  const enter = () => {
+    toolkit && setHovered(true);
+  };
+  const exit = () => {
+    toolkit && setHovered(false);
+  };
+
+  //when hover is not available
+  const handleClick = () => {
+    toolkit && setHovered((prev) => !prev);
+  };
+
   return (
-    <th className="text-left uppercase font-heading text-md p-2 pl-0 text-angel-grey">
-      {text}
+    <th
+      onClick={handleClick}
+      onMouseEnter={enter}
+      onMouseLeave={exit}
+      className={`${
+        toolkit ? "cursor-help relative" : "cursor-text"
+      } text-left uppercase font-heading text-md p-2 pl-0 text-angel-grey`}
+    >
+      {props.text}{" "}
+      {toolkit && <AiOutlineQuestionCircle className="inline text-lg mb-1" />}
+      {hovered && props.children}
     </th>
   );
 }
