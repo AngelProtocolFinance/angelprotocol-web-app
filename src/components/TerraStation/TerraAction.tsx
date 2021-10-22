97e5958f425f1fd222f8b19401b4cb3469c7829d
ap-justin <justin@angelprotocol.io>
2021-10-22 14:34:42 +0800

    changed action button hover color

--- src/components/TerraStation/TerraAction.tsx
+++ src/components/TerraStation/TerraAction.tsx
@@ -17,11 +17,11 @@ export default function TerraAction(props: Props) {
   return (
     <button
       onClick={handleClick}
-      className={`transform active:translate-x-1 hover:bg-white flex items-center gap-2 rounded-full pitems-center bg-thin-grey p-1 pr-4 shadow-md`}
+      className={`transform active:translate-x-1  bg-white hover:bg-blue-100 flex items-center gap-2 rounded-full items-center p-1 pr-4 shadow-md`}
     >
       <img
         src={props.icon}
-        className="w-8 h-8 p-2 bg-white rounded-full shadow-md"
+        className="w-8 h-8 p-2 bg-white-grey rounded-full shadow-md"
         alt=""
       />
       <p className="uppercase text-angel-grey text-sm ">{props.label}</p>
