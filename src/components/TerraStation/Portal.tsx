093e59efe11e94d0acb27ce2c150a10a03bb2fc6
ap-justin <justin@angelprotocol.io>
2021-10-22 19:28:03 +0800

    connect my-endowment button to withdraw page

--- src/components/TerraStation/Portal.tsx
+++ src/components/TerraStation/Portal.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useLookupQuery } from "api/endowmentsAPI/endowmentAPI";
 import { useConnectedWallet } from "@terra-dev/use-wallet";
 import { chains } from "contracts/types";
+import { app, site } from "types/routes";
 
 export default function Portal() {
   const wallet = useConnectedWallet();
@@ -14,10 +15,7 @@ export default function Portal() {
   } else {
     return (
       <Link
-        onClick={() => {
-          alert(`go to charity address | ${endowmentAddr} |`);
-        }}
-        to="#"
+        to={`${site.app}/${app.withdraw}/${endowmentAddr}`}
         className="ml-4 mr-auto bg-blue-accent hover:bg-angel-blue active:bg-angel-blue text-sm text-white-grey rounded-sm py-1 px-2 mt-2"
       >
         MY ENDOWMENT
