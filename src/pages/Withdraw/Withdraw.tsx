5bc5de5092ce6b7ecf0c352b73f2ccffa7926beb
Paolo Mariano <paolo@pop-os.localdomain>
2021-10-22 13:56:39 +0800

    Added WithdrawForm.tsx

--- src/pages/Withdraw/Withdraw.tsx
+++ src/pages/Withdraw/Withdraw.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { IoCloseOutline } from "react-icons/io5";
 import AppHead from "components/Headers/AppHead";
 import Loader from "components/Loader/Loader";
-import Modal from "components/Modal/Modal";
 import toCurrency from "helpers/toCurrency";
 import Liquid from "./Liquid";
 import Locked from "./Locked";
+import WithdrawForm from "./WithdrawForm";
 import useWithdraw from "./useWithdraw";
 
 export default function Withdraw() {
@@ -38,10 +38,10 @@ export default function Withdraw() {
             <Liquid liquidBalance={liquid} />
             <Locked lockedBalance={locked} />
           </div>
-          <div className="flex justify-center mt-4">
+          <div className="flex justify-start mt-4 pl-6">
             {/*//TODO: should disable/hide when curr wallet_addr is not endowment owner */}
             <button
-              className="uppercase hover:bg-blue-accent bg-angel-blue rounded-lg pr-4 pl-4 h-12 text-sm font-bold"
+              className="uppercase hover:bg-blue-accent bg-angel-blue rounded-lg w-56 h-12 text-sm font-bold"
               onClick={() => setShowModal(true)}
             >
               Withdraw from Accounts
@@ -57,14 +57,14 @@ export default function Withdraw() {
                   >
                     <IoCloseOutline className="text-angel-grey" />
                   </button>
-                  <h3 className="mb-2 text-angel-grey text-center font-semibold font-heading">
+                  <h3 className="mb-1 text-lg text-angel-grey text-center font-semibold font-heading">
                     Withdraw from Accounts
                   </h3>
                   <p className="mb-2 text-angel-grey text-center text-xs">
                     Enter the quantity of tokens to withdraw from each of the
                     active Liquid Account's current strategies.
                   </p>
-                  {/* Add form here */}
+                  <WithdrawForm />
                 </div>
               </div>
             ) : null}
