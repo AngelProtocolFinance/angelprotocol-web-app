7f9e38ab55f612eff1904ef4a3f040e69845763f
Paolo Mariano <paolo@pop-os.localdomain>
2021-10-27 13:53:14 +0800

    withdraw pg

--- src/pages/Withdraw/useWithdraw.ts
+++ src/pages/Withdraw/useWithdraw.ts
@@ -1,26 +1,52 @@
 import { useEffect, useState } from "react";
 import { useConnectedWallet } from "@terra-money/wallet-provider";
+import { useLookupQuery } from "api/endowmentsAPI/endowmentAPI";
 import Account from "contracts/Account";
+import { chains } from "contracts/types";
 
 export default function useWithdraw(address: string) {
+  const [redirect, setRedirect] = useState(false);
   const [isLoading, setLoading] = useState(false);
+  const [isEndowmentOwner, setEndowmentOwner] = useState(false);
   const [error, setError] = useState("");
   const [locked, setLocked] = useState<number>();
   const [liquid, setLiquid] = useState<number>();
   const [overall, setOverall] = useState<number>();
   const wallet = useConnectedWallet();
+  const isTestNet = wallet?.network.chainID === chains.testnet;
+  //on testnet --> url resolves to endpoint/endowments/testnet
+  const { data } = useLookupQuery(isTestNet);
+
+  const getEndowmentBalance = async () => {
+    const account = new Account(address, wallet);
+    const result = await account.getBalance();
+    setLocked(result.total_locked);
+    setLiquid(result.total_liq);
+    setOverall(result.overall);
+    setLoading(false);
+  };
 
   useEffect(() => {
     (async () => {
       try {
         setError("");
         setLoading(true);
-        const account = new Account(address, wallet);
-        const result = await account.getBalance();
-        setLocked(result.total_locked);
-        setLiquid(result.total_liq);
-        setOverall(result.overall);
-        setLoading(false);
+        if (!wallet) {
+          // If no wallet is connected
+          setError("Please connect a wallet to view this page.");
+          setLoading(false);
+        } else if (!data?.[wallet?.walletAddress]) {
+          // If wallet connected is not the endowment owner's wallet address
+          setEndowmentOwner(false);
+          getEndowmentBalance();
+        } else if (data?.[wallet?.walletAddress] !== address) {
+          // If endowment address is invalid
+          setRedirect(true);
+          console.log("HUY!");
+        } else {
+          setEndowmentOwner(true);
+          getEndowmentBalance();
+        }
       } catch (err) {
         console.error(err);
         setError("Failed to get balance.");
@@ -31,8 +57,10 @@ export default function useWithdraw(address: string) {
   }, [wallet]);
 
   return {
+    redirect,
     isReady: !isLoading && !error,
     isLoading,
+    isEndowmentOwner,
     error: !isLoading && error,
     locked,
     liquid,
