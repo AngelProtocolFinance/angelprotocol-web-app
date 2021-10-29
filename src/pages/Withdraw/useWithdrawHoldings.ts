af2b33f910c9419565e3145961f4cc3e98df63a1
Paolo Mariano <paolo@pop-os.localdomain>
2021-10-29 12:33:08 +0800

    Merge latest with main and refactor onSubmit logic of Withdraw form

--- src/pages/Withdraw/useWithdrawHoldings.ts
+++ src/pages/Withdraw/useWithdrawHoldings.ts
@@ -1,19 +1,43 @@
 import { useEffect, useState } from "react";
 import { useConnectedWallet } from "@terra-money/wallet-provider";
+import { FormikHelpers } from "formik";
 import Account from "contracts/Account";
 
-export default function useWithdrawHoldings(
+interface Values {
+  withdraw: string;
+}
+
+function useWithdrawHoldings(
   address: string,
-  anchorVault: string
+  anchorVault: string,
+  withdrawTokenQty: string
 ) {
   const wallet = useConnectedWallet();
 
-  useEffect(() => {
-    (async () => {
-      const account = new Account(address, wallet);
-      // const result = await account.createWithdrawTx(anchorVault);
-      console.log(account);
-      console.log(anchorVault);
-    })();
-  }, [wallet]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const account = new Account(address, wallet);
+  //     // const result = await account.createWithdrawTx(anchorVault);
+  //     console.log(account);
+  //     console.log(anchorVault);
+  //   })();
+  // }, [wallet]);
+
+  async function withdrawHoldings(
+    values: Values,
+    actions: FormikHelpers<Values>
+  ) {
+    values.withdraw = withdrawTokenQty;
+    const tokenQty = values.withdraw;
+    console.log("Vault:", anchorVault);
+    console.log("Token QTY:", tokenQty);
+    try {
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return withdrawHoldings;
 }
+
+export default useWithdrawHoldings;
