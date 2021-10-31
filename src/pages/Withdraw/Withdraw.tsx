292a7beda96caebb81bb616d3426941edc02406d
Paolo Mariano <paolo@pop-os.localdomain>
2021-10-31 12:42:40 +0800

    withdraw pg

--- src/pages/Withdraw/Withdraw.tsx
+++ src/pages/Withdraw/Withdraw.tsx
@@ -1,22 +1,14 @@
 import { useState } from "react";
-import { Formik, Form } from "formik";
-import Slider from "rc-slider";
 import AppHead from "components/Headers/AppHead";
 import Loader from "components/Loader/Loader";
 import toCurrency from "helpers/toCurrency";
 import Liquid from "./Liquid";
 import Locked from "./Locked";
-// import WithdrawForm from "./WithdrawForm";
-import useHoldings from "./useHoldings";
+import WithdrawForm from "./WithdrawForm";
 import useWithdraw from "./useWithdraw";
-import useWithdrawHoldings from "./useWithdrawHoldings";
 import { RouteComponentProps } from "react-router";
 import { Redirect } from "react-router-dom";
 import { site } from "types/routes";
-
-interface Values {
-  withdraw: string;
-}
 
 type Param = { address: string };
 
@@ -25,10 +17,8 @@ export default function Withdraw(props: RouteComponentProps<Param>) {
   //can also pass address as state but no guarantee that user will go to this page using
   //only the in-app link provided
   const address = props.match.params.address;
+  const [withdrawFormIsOpen, setWithdrawForm] = useState(false);
 
-  const [showModal, setShowModal] = useState(false);
-  const [withdrawAmount, setWithdrawAmount] = useState<number>(0);
-  const [withdrawTokenQty, setWithdrawTokenQty] = useState("");
   const {
     redirect,
     isReady,
@@ -39,19 +29,14 @@ export default function Withdraw(props: RouteComponentProps<Param>) {
     liquid,
     overall,
   } = useWithdraw(address);
-  const { liquidCW20Tokens, anchorVault } = useHoldings(address);
 
-  const computeWithdrawAmount = (value: number) => {
-    // value is the percentage based on the slider
-    setWithdrawAmount((liquid! * value) / 100);
-    setWithdrawTokenQty(((liquidCW20Tokens! * value) / 100).toString());
-  };
+  function openWithdrawForm() {
+    setWithdrawForm(true);
+  }
 
-  const withdrawHoldings = useWithdrawHoldings(
-    address,
-    anchorVault,
-    withdrawTokenQty
-  );
+  function closeWithdrawForm() {
+    setWithdrawForm(false);
+  }
 
   return (
     <div className="pb-16 grid content-start min-h-screen">
@@ -86,84 +71,19 @@ export default function Withdraw(props: RouteComponentProps<Param>) {
             {isEndowmentOwner ? (
               <button
                 className="uppercase hover:bg-blue-accent bg-angel-blue rounded-lg w-56 h-12 text-sm font-bold"
-                onClick={() => setShowModal(true)}
+                onClick={openWithdrawForm}
               >
                 Withdraw from Accounts
               </button>
             ) : null}
           </div>
           <div>
-            {showModal ? (
-              <div className="absolute bg-gray-800 bg-opacity-80 w-full h-full top-0 left-0 right-0 bottom-0 z-50 grid place-items-center">
-                <div className="p-3 md:p-6 bg-white-grey w-full max-w-xs min-h-r15 rounded-xl shadow-lg overflow-hidden relative">
-                  <h3 className="mb-1 text-lg text-angel-grey text-center font-semibold font-heading">
-                    Withdraw from Accounts
-                  </h3>
-                  <p className="mb-3 md:mb-6 text-angel-grey text-center text-xs">
-                    Enter the quantity of tokens to withdraw from each of the
-                    active Liquid Account's current strategies.
-                  </p>
-                  <div className="text-angel-grey">
-                    <Formik<Values>
-                      initialValues={{ withdraw: "0" }}
-                      onSubmit={withdrawHoldings}
-                    >
-                      {/*TODO:// separate this form in separate component*/}
-                      <Form>
-                        <div className="flex flex-row justify-around mb-3">
-                          <div>
-                            <div className="my-1.5">
-                              <label htmlFor="withdraw">Anchor Protocol</label>
-                            </div>
-                            <div className="align-bottom">
-                              <p className="text-xs italic">
-                                Available: {toCurrency(lockedCW20Tokens! / 1e6)}
-                                {} tokens
-                              </p>
-                              <p className="text-xs italic">
-                                (~$ {toCurrency(locked)})
-                              </p>
-                            </div>
-                          </div>
-                          <div>
-                            <div className="my-3">
-                              <Slider
-                                min={0}
-                                max={100}
-                                defaultValue={0}
-                                onChange={computeWithdrawAmount}
-                              />
-                            </div>
-                            <div className="align-bottom">
-                              <p className="text-xs italic py-px">
-                                Withdraw Amt: ~$ {withdrawAmount}
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-row mt-6">
-                          <button
-                            type="submit"
-                            className="m-auto uppercase hover:bg-blue-accent bg-angel-blue rounded-lg w-28 h-8 text-white-grey text-sm font-bold"
-                          >
-                            Withdraw
-                          </button>
-                          <button
-                            onClick={() => {
-                              setWithdrawAmount(0);
-                              setShowModal(false);
-                            }}
-                            className="m-auto uppercase hover:bg-angel-orange hover:text-white-grey hover:border-opacity-0 rounded-lg w-28 h-8 text-angel-orange border-2 border-angel-orange text-sm font-bold"
-                          >
-                            Cancel
-                          </button>
-                        </div>
-                      </Form>
-                    </Formik>
-                  </div>
-                </div>
-              </div>
-            ) : null}
+            <WithdrawForm
+              address={address}
+              liquid={liquid!}
+              isModalOpened={withdrawFormIsOpen}
+              onCloseModal={closeWithdrawForm}
+            />
           </div>
         </div>
       )}
--- src/pages/Withdraw/WithdrawForm.tsx
+++ src/pages/Withdraw/WithdrawForm.tsx
@@ -1,46 +1,115 @@
-import { Formik, Field, Form, FormikHelpers } from "formik";
+import { useState } from "react";
+import Modal from "react-modal";
+import { Formik, Form } from "formik";
+import Slider from "rc-slider";
+import toCurrency from "helpers/toCurrency";
+import useHoldings from "./useHoldings";
+import useWithdrawHoldings from "./useWithdrawHoldings";
 
 interface Values {
-  strategy: string;
+  withdraw: string;
 }
 
-export default function WithdrawForm() {
+interface Props {
+  address: string;
+  liquid: number | undefined;
+  isModalOpened: boolean;
+  onCloseModal: () => void;
+}
+
+export default function WithdrawForm(props: Props) {
+  const [withdrawAmount, setWithdrawAmount] = useState<number>(0);
+  const [withdrawTokenQty, setWithdrawTokenQty] = useState("");
+
+  const { liquidCW20Tokens, anchorVault } = useHoldings(props.address);
+
+  const computeWithdrawAmount = (value: number) => {
+    // value is the percentage based on the slider
+    setWithdrawAmount((props.liquid! * value) / 100);
+    setWithdrawTokenQty(((liquidCW20Tokens! * value) / 100).toString());
+  };
+
+  const withdrawHoldings = useWithdrawHoldings(
+    props.address,
+    anchorVault,
+    withdrawAmount,
+    withdrawTokenQty
+  );
+
+  function onModalClose() {
+    setWithdrawAmount(0);
+    props.onCloseModal();
+  }
+
   return (
-    <div className="text-angel-grey">
-      <Formik
-        initialValues={{ strategy: "" }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>
-        ) => {
-          setSubmitting(false);
-        }}
-      >
-        <Form>
-          <label htmlFor="strategy">Anchor Protocol</label>
-          <Field
-            className="bg-gray-200"
-            id="strategy"
-            name="strategy"
-            autoComplete="off"
-            type="text"
-          />
-          <div className="flex justify-around mt-6">
-            <button
-              type="submit"
-              className="uppercase hover:bg-blue-accent bg-angel-blue rounded-lg w-28 h-8 text-white-grey text-sm font-bold"
-            >
-              Withdraw
-            </button>
-            <button
-              type="button"
-              className="uppercase hover:bg-angel-orange bg-orange rounded-lg w-28 h-8 text-white-grey text-sm font-bold"
-            >
-              Cancel
-            </button>
-          </div>
-        </Form>
-      </Formik>
-    </div>
+    <Modal
+      isOpen={props.isModalOpened}
+      ariaHideApp={false}
+      className="absolute bg-gray-400 bg-opacity-50 w-full h-full top-0 left-0 right-0 bottom-0 z-50 grid place-items-center"
+    >
+      <div className="p-3 md:p-6 bg-white-grey w-full max-w-xs min-h-r15 rounded-xl shadow-lg overflow-hidden relative">
+        <h3 className="mb-1 text-lg text-angel-grey text-center font-semibold font-heading">
+          Withdraw from Accounts
+        </h3>
+        <p className="mb-3 md:mb-6 text-angel-grey text-center text-xs">
+          Enter the quantity of tokens to withdraw from each of the active
+          Liquid Account's current strategies.
+        </p>
+        <div className="text-angel-grey">
+          <Formik<Values>
+            initialValues={{ withdraw: "0" }}
+            onSubmit={withdrawHoldings}
+          >
+            <Form>
+              <div className="flex flex-row justify-around mb-3">
+                <div>
+                  <div className="my-1.5">
+                    <label htmlFor="withdraw">Anchor Protocol</label>
+                  </div>
+                  <div className="align-bottom">
+                    <p className="text-xs italic">
+                      Available: {toCurrency(liquidCW20Tokens! / 1e6)}
+                      {} tokens
+                    </p>
+                    <p className="text-xs italic">
+                      (~$ {toCurrency(props.liquid)})
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="my-3">
+                    <Slider
+                      min={0}
+                      max={100}
+                      defaultValue={0}
+                      onChange={computeWithdrawAmount}
+                    />
+                  </div>
+                  <div className="align-bottom">
+                    <p className="text-xs italic py-px">
+                      Withdraw Amt: ~$ {toCurrency(withdrawAmount)}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row mt-6">
+                <button
+                  type="submit"
+                  className="m-auto uppercase hover:bg-blue-accent bg-angel-blue rounded-lg w-28 h-8 text-white-grey text-sm font-bold"
+                >
+                  Withdraw
+                </button>
+                <button
+                  onClick={onModalClose}
+                  className="m-auto uppercase hover:bg-angel-orange hover:text-white-grey hover:border-opacity-0 rounded-lg w-28 h-8 text-angel-orange border-2 border-angel-orange text-sm font-bold"
+                >
+                  Cancel
+                </button>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    </Modal>
   );
 }
