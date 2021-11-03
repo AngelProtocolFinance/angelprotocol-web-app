ecd246cc2c60596df4115b6333d12caf4bdbe42d
Paolo Mariano <paolo@pop-os.localdomain>
2021-11-03 18:49:59 +0800

    Refactored Confirm TX and Success modals

--- src/pages/Withdraw/Results.tsx
+++ src/pages/Withdraw/Results.tsx
@@ -11,14 +11,14 @@ export default function Results(props: ResultProps) {
       <h3 className="mb-3 text-lg text-angel-grey text-center font-semibold font-heading">
         {message}
       </h3>
-      <div className="p-2 font-heading text-angel-blue font-semibold">
-        <p className="w-full mb-5 uppercase grid items-center">
+      <div className="p-2 font-heading text-angel-grey">
+        <p className="w-full mb-5 grid uppercase font-semibold text-center">
           <span className="text-xs">Withdrawn Amount :</span>
-          <span className="font-semibold">~$ {withdrawn}</span>
+          <span className="">~{withdrawn} UST</span>
         </p>
-        <p className="text-xs">
+        <p className="text-xs text-center">
           <a
-            className="hover:text-blue-accent"
+            className="hover:text-blue-accent text-angel-blue"
             href={result?.url}
             target="_blank"
             rel="noreferrer"
@@ -27,7 +27,7 @@ export default function Results(props: ResultProps) {
           </a>
         </p>
       </div>
-      <div className="flex flex-row mt-2">
+      <div className="flex flex-row mt-3">
         <button
           onClick={props.onCloseModal}
           className="m-auto uppercase hover:bg-angel-orange hover:text-white-grey hover:border-opacity-0 rounded-lg w-28 h-8 text-angel-orange border-2 border-angel-orange text-sm font-bold"
