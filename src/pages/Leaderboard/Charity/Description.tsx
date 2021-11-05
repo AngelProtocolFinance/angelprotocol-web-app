ec78ad820aff9338fa1727e4b9d37aefa1cb16c7
ap-justin <justin@angelprotocol.io>
2021-11-05 12:45:13 +0800

    remove subtle box shadow

--- src/pages/Leaderboard/Charity/Description.tsx
+++ src/pages/Leaderboard/Charity/Description.tsx
@@ -18,7 +18,7 @@ export default function Description(props: Props) {
           alt=""
           className={`bg-white ${
             iconLight ? "bg-angel-blue" : ""
-          } p-3 rounded-sm shadow-sm m-1 object-contain mr-4`}
+          } p-3 rounded-sm m-1 object-contain mr-4`}
         />
       </div>
       <div>
