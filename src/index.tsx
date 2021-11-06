b747d34ea7dbceb736d3653aae0b966f804b9184
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-11-06 12:50:13 +0800

    sentry integration added

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,17 @@ import { Provider } from "react-redux";
 import AuthProvider from "contexts/AuthProvider";
 import { store } from "store/store";
 import LoadPage from "pages/Loading/Loading";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://4da0ddd9b67e4801af699469048265f8@o1061537.ingest.sentry.io/6051923",
+  integrations: [new Integrations.BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const LazyApp = React.lazy(() => import("./App/App"));
 
