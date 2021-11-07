2014f509d07039f8d5ad9a1ba8867e6cadad6ea9
Paolo Mariano <paolo@pop-os.localdomain>
2021-11-07 12:17:46 +0800

    Added form validation and ErrorMessage component to the KYC form

--- src/components/Donator/KYCForm.tsx
+++ src/components/Donator/KYCForm.tsx
@@ -1,4 +1,4 @@
-import { Field } from "formik";
+import { ErrorMessage, Field } from "formik";
 import { useRef } from "react";
 
 export default function KYCForm() {
@@ -9,68 +9,123 @@ export default function KYCForm() {
       <label className="col-span-5 lg:col-span-1 flex justify-left items-center lg:ml-3">
         Full Name
       </label>
-      <Field
-        innerRef={inputRef}
-        className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2 col-span-5 lg:col-span-4"
-        type="text"
-        name="fullName"
-        placeholder="Full name"
-      />
+      <div className="col-span-5 lg:col-span-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="text"
+          name="fullName"
+          placeholder="Full name"
+        />
+        <ErrorMessage
+          name="fullName"
+          component="div"
+          className="lg:col-start-2 mb-2 text-sm text-white"
+        />
+      </div>
+
       <label className="col-span-5 lg:col-span-1 flex justify-left items-center lg:ml-3">
         Email
       </label>
-      <Field
-        innerRef={inputRef}
-        className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2 col-span-5 lg:col-span-4"
-        type="email"
-        name="email"
-        placeholder="Email address"
-      />
+      <div className="col-span-5 lg:col-span-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="email"
+          name="email"
+          placeholder="Email address"
+        />
+        <ErrorMessage
+          name="email"
+          component="div"
+          className="lg:col-start-2 mb-2 text-sm text-white"
+        />
+      </div>
+
       <label className="col-span-5 lg:col-span-1 flex justify-left items-center lg:ml-3">
         Address
       </label>
-      <Field
-        innerRef={inputRef}
-        className="lg:mr-4 my-2 border-transparent outline-none text-black w-auto rounded-md px-2 py-2 col-span-5 lg:col-span-2"
-        type="text"
-        name="streetAddress"
-        placeholder="Street & number"
-      />
-      <Field
-        innerRef={inputRef}
-        className="lg:ml-4 my-2 border-transparent outline-none text-black w-auto rounded-md px-2 py-2 col-span-5 lg:col-span-2"
-        type="text"
-        name="addressComplement"
-        placeholder="Apt., suite, etc. (optional)"
-      />
-      <Field
-        innerRef={inputRef}
-        className="lg:col-start-2 lg:mr-4 my-2 border-transparent outline-none text-black w-auto rounded-md px-2 py-2 col-span-5 lg:col-span-2"
-        type="text"
-        name="zipCode"
-        placeholder="Zip code"
-      />
-      <Field
-        innerRef={inputRef}
-        className="lg:col-start-4 lg:ml-4 my-2 border-transparent outline-none text-black w-auto rounded-md px-2 py-2 col-span-5 lg:col-span-2"
-        type="text"
-        name="city"
-        placeholder="City"
-      />
-      <Field
-        innerRef={inputRef}
-        className="lg:col-start-2 lg:mr-4 my-2 border-transparent outline-none text-black w-auto rounded-md px-2 py-2 col-span-5 lg:col-span-2"
-        type="text"
-        name="stateAddress"
-        placeholder="State"
-      />
-      <Field
-        innerRef={inputRef}
-        className="lg:col-start-4 lg:ml-4 my-2 border-transparent outline-none text-black w-auto rounded-md px-2 py-2 col-span-5 lg:col-span-2"
-        type="text"
-        name="country"
-        placeholder="Country"
-      />
+      <div className="col-span-5 lg:col-span-2 lg:mr-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="text"
+          name="streetAddress"
+          placeholder="Street & number"
+        />
+        <ErrorMessage
+          name="streetAddress"
+          component="div"
+          className="lg:col-start-2 mb-2 text-sm text-white"
+        />
+      </div>
+      <div className="col-span-5 lg:col-start-4 lg:col-span-2 lg:ml-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="text"
+          name="addressComplement"
+          placeholder="Apt., suite, etc. (optional)"
+        />
+      </div>
+
+      <div className="col-span-5 lg:col-start-2 lg:col-span-2 lg:mr-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="text"
+          name="city"
+          placeholder="City or municipality"
+        />
+        <ErrorMessage
+          name="city"
+          component="div"
+          className="lg:col-start-2 mb-2 text-sm text-white"
+        />
+      </div>
+      <div className="col-span-5 lg:col-start-4 lg:col-span-2 lg:ml-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="text"
+          name="zipCode"
+          placeholder="ZIP code"
+        />
+        <ErrorMessage
+          name="zipCode"
+          component="div"
+          className="lg:col-start-4 mb-2 text-sm text-white"
+        />
+      </div>
+
+      <div className="col-span-5 lg:col-start-2 lg:col-span-2 lg:mr-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="text"
+          name="stateAddress"
+          placeholder="State, region or province"
+        />
+        <ErrorMessage
+          name="stateAddress"
+          component="div"
+          className="lg:col-start-2 mb-2 text-sm text-white"
+        />
+      </div>
+      <div className="col-span-5 lg:col-start-4 lg:col-span-2 lg:ml-4">
+        <Field
+          innerRef={inputRef}
+          className="my-2 border-transparent outline-none text-black w-full rounded-md px-2 py-2"
+          type="text"
+          name="country"
+          placeholder="Country"
+        />
+        <ErrorMessage
+          name="country"
+          component="div"
+          className="lg:col-start-4 mb-2 text-sm text-white"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Donator/donatorSchema.ts
+++ src/components/Donator/donatorSchema.ts
@@ -5,6 +5,37 @@ export const donatorSchema = Yup.object().shape({
     .typeError("Amount must be a number")
     .required("Kindly specify amount")
     .positive("Amount must be greater than zero "),
+  receiptRequested: Yup.boolean(),
+  fullName: Yup.string().when("receiptRequested", {
+    is: true,
+    then: Yup.string().required("Please enter your full name"),
+  }),
+  email: Yup.string().when("receiptRequested", {
+    is: true,
+    then: Yup.string()
+      .email("Must be a valid email address")
+      .required("Please enter your email address"),
+  }),
+  streetAddress: Yup.string().when("receiptRequested", {
+    is: true,
+    then: Yup.string().required("Street address is required"),
+  }),
+  city: Yup.string().when("receiptRequested", {
+    is: true,
+    then: Yup.string().required("City or municipality is required"),
+  }),
+  zipCode: Yup.string().when("receiptRequested", {
+    is: true,
+    then: Yup.string().required("ZIP or Postal code is required"),
+  }),
+  stateAddress: Yup.string().when("receiptRequested", {
+    is: true,
+    then: Yup.string().required("State, region or province is required"),
+  }),
+  country: Yup.string().when("receiptRequested", {
+    is: true,
+    then: Yup.string().required("Country is required"),
+  }),
 
   //taken care of by <Slider/> restrictions
   // split: Yup.number().min(50).max(100),
