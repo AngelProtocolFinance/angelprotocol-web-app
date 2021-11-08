50fd37bbc943e09e933a1705231009ac3988f822
ap-justin <justin@angelprotocol.io>
2021-11-08 19:08:09 +0800

    wallet suite redux refactor and eth tx loader refactor

--- src/components/Donater/useEthSender.ts
+++ src/components/Donater/useEthSender.ts
@@ -1,6 +1,4 @@
 import ErrPop, { Props as ErrProps } from "./ErrPop";
-// import Waiter, { Props as WaitProps } from "./Waiter";
-// import Result, { Props as ResProps } from "./Result";
 import { useSetModal } from "components/Nodal/Nodal";
 import { chains } from "contracts/types";
 import { useWallet } from "use-wallet";
@@ -58,19 +56,6 @@ export default function useEthSender() {
       });
 
       dispatch(setPending({ amount: +data.amount, hash: response.hash }));
-
-      // showModal<WaitProps>(Waiter, {});
-
-      // const receipt = await response.wait();
-
-      // showModal<ResProps>(Result, {
-      //   sent: +data.amount,
-      //   received: +data.amount,
-      //   // url: `https://kovan.etherscan.io/tx/${receipt.transactionHash}`,
-      //   url: `https://ropsten.etherscan.io/tx/${receipt.transactionHash}`,
-      //   denom: denoms.ether,
-      //   precision: 7,
-      // });
     } catch (error) {
       displayEthError(error, showModal);
     } finally {
