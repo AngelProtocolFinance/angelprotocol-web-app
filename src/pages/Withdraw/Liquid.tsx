5539dc93955fa5a2e656c01170f11b64c2393d83
Paolo Mariano <paolo@pop-os.localdomain>
2021-11-17 14:31:09 +0800

    Initial commit

--- src/pages/Withdraw/Liquid.tsx
+++ src/pages/Withdraw/Liquid.tsx
@@ -10,7 +10,7 @@ export default function Liquid(props: LiquidBalance) {
     <div className="flex-none pl-2 pr-2 mb-3 md:mb-0">
       <div className="p-4 md:p-5 block bg-grey-accent overflow-hidden h-full rounded-lg">
         <h3 className="mb-2 text-lg font-bold uppercase">
-          Liquid Account
+          Current Account
           <div className="float-right">
             <span className="inline-block">
               <FaCog size={16} title="Coming Soon!" />
