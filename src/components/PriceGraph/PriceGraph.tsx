2e61497e656d8dbb85f63d3c3a5f4089a7972a86
Nenad Misic <n.misic@outlook.com>
2021-12-01 18:49:38 +0100

    Add hook for getting price data

--- src/components/PriceGraph/PriceGraph.tsx
+++ src/components/PriceGraph/PriceGraph.tsx
@@ -8,11 +8,12 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import useGetHistoricPrices, {
+  PriceData,
+  toUNIXTime,
+} from "./useGetHistoricPrices";
 
 // const APIURL = "https://api.coingecko.com/api/v3/coins/terra-luna/market_chart";
-
-const toUNIXTime = (stringTime: string) =>
-  new Date(stringTime).getTime() / 1000;
 
 const tickDateFormatter = (dateUNIX: number) =>
   new Date(dateUNIX * 1000).toLocaleDateString("en-us", {
@@ -30,68 +31,33 @@ const dates = [
   "2021-12-02 11:00",
 ];
 
-const tempPredictionData = [
-  {
-    name: "Page A",
-    pv: 2400,
-    date: toUNIXTime("2021-11-29 01:00"),
-  },
-  {
-    name: "Page B",
-    pv: 1398,
-    date: toUNIXTime("2021-11-30 01:00"),
-  },
-  {
-    name: "Page C",
-    pv: 9800,
-    date: toUNIXTime("2021-11-30 11:00"),
-  },
-  {
-    name: "Page D",
-    pv: 3908,
-    date: toUNIXTime("2021-12-01 01:00"),
-  },
-  {
-    name: "Page E",
-    pv: 4800,
-    date: toUNIXTime("2021-12-01 11:00"),
-  },
-  {
-    name: "Page F",
-    pv: 3800,
-    date: toUNIXTime("2021-12-02 01:00"),
-  },
-  {
-    name: "Page G",
-    pv: 4300,
-    date: toUNIXTime("2021-12-02 11:00"),
-  },
-];
-
-let currentDate = 0;
-
-interface CurrentData {
-  uv: number;
-  date: number;
-}
-
 export default function PriceGraph() {
-  const [predictionData, _] = useState(tempPredictionData);
-  const [currentData, setCurrentData] = useState(new Array<CurrentData>());
+  const [predictedPriceData, setPredictedPriceData] = useState(
+    new Array<PriceData>()
+  );
+  const [currentData, setCurrentData] = useState(new Array<PriceData>());
+  const [currentDateIndex, setCurrentDateIndex] = useState(0);
+
+  const { currentPriceData } = useGetHistoricPrices();
+
+  useEffect(() => {
+    setCurrentData(currentPriceData);
+  }, [currentPriceData]);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      if (currentDate === dates.length) {
+      if (currentDateIndex === dates.length) {
         clearTimeout(timer);
         return;
       }
 
       const newDataPoint = {
-        uv: Math.floor(Math.random() * 10000),
-        date: toUNIXTime(dates[currentDate++]),
+        price: Math.floor(Math.random() * 10000),
+        date: toUNIXTime(dates[currentDateIndex]),
       };
-      setCurrentData((prevData) => [...prevData, newDataPoint]);
-    }, 3000);
+      setPredictedPriceData((prevData) => [...prevData, newDataPoint]);
+      setCurrentDateIndex((prevDateIndex) => prevDateIndex + 1);
+    }, 1000);
     return () => {
       if (timer) {
         clearTimeout(timer);
@@ -122,15 +88,15 @@ export default function PriceGraph() {
         <Legend />
         <Line
           type="monotone"
-          dataKey="pv"
+          dataKey="price"
           stroke="#8884d8"
           dot={false}
-          data={predictionData}
+          data={predictedPriceData}
         />
         <Line
           dot={false}
           type="monotone"
-          dataKey="uv"
+          dataKey="price"
           stroke="#82ca9d"
           data={currentData}
           strokeWidth={2}
---
+++ src/components/PriceGraph/useGetHistoricPrices.ts
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+
+export interface PriceData {
+  price: number;
+  date: number;
+}
+
+export const toUNIXTime = (stringTime: string) =>
+  new Date(stringTime).getTime() / 1000;
+
+const tempPriceData: PriceData[] = [
+  {
+    price: 2400,
+    date: toUNIXTime("2021-11-29 01:00"),
+  },
+  {
+    price: 1398,
+    date: toUNIXTime("2021-11-30 01:00"),
+  },
+  {
+    price: 9800,
+    date: toUNIXTime("2021-11-30 11:00"),
+  },
+  {
+    price: 3908,
+    date: toUNIXTime("2021-12-01 01:00"),
+  },
+  {
+    price: 4800,
+    date: toUNIXTime("2021-12-01 11:00"),
+  },
+  {
+    price: 3800,
+    date: toUNIXTime("2021-12-02 01:00"),
+  },
+  {
+    price: 4300,
+    date: toUNIXTime("2021-12-02 11:00"),
+  },
+];
+
+export default function useGetHistoricPrices() {
+  const [index, setIndex] = useState(0);
+  const [currentPriceData, setCurrentPriceData] = useState(
+    new Array<PriceData>()
+  );
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (index === tempPriceData.length) {
+        clearTimeout(timer);
+        return;
+      }
+
+      setCurrentPriceData([...currentPriceData, tempPriceData[index]]);
+      setIndex((prevIndex) => prevIndex + 1);
+    }, 1000);
+
+    return () => {
+      if (timer) clearTimeout(timer);
+    };
+  });
+
+  return { currentPriceData };
+}
