fae68905eecfd6b923146e39153a9bc771d4cfde
Nenad Misic <n.misic@outlook.com>
2021-12-07 15:07:02 +0100

    Add LBP contract class

---
+++ src/contracts/LBP.ts
@@ -0,0 +1,29 @@
+import { ConnectedWallet } from "@terra-money/wallet-provider";
+import { contracts } from "constants/contracts";
+import Contract from "./Contract";
+import { sc } from "./types";
+
+interface LBPPair {
+  start_time: number;
+  end_time: number;
+  contract_addr: string;
+  asset_infos: any[];
+}
+
+export default class LBP extends Contract {
+  address: string;
+  //contract address
+
+  constructor(wallet?: ConnectedWallet) {
+    super(wallet);
+    this.address = contracts[this.chainID][sc.index_fund];
+  }
+
+  async getLBPs() {
+    const pairs = await this.query<LBPPair>(this.address, {
+      pairs: {},
+    });
+
+    return pairs;
+  }
+}
