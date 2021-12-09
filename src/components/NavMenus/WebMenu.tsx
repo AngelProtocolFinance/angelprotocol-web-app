e04c10c2da3bf960b376f1c2aa2ea4b35997e3b6
shadrach <shadrachtemitayo@gmail.com>
2021-12-09 16:41:42 +0100

    remove dropdown on webmenu

--- src/components/NavMenus/WebMenu.tsx
+++ src/components/NavMenus/WebMenu.tsx
@@ -1,8 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { app, site, web } from "types/routes";
 import { useRouteMatch } from "react-router-dom";
-import { createRef, useState } from "react";
-import { createPopper } from "@popperjs/core/lib/createPopper";
 
 export default function WebMenu() {
   const { url } = useRouteMatch();
@@ -10,19 +8,6 @@ export default function WebMenu() {
     className: `text-angel-blue hover:text-opacity-75 uppercase inline-flex items-center`,
     activeClassName: "font-bold",
   };
-  const govButtonRef = createRef<any>();
-  const govDropdownRef = createRef<any>();
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const openDropdwonPopover = () => {
-    console.log("open ", showDropdown);
-    createPopper(govButtonRef.current, govDropdownRef.current, {
-      placement: "bottom-start",
-    });
-    setShowDropdown(true);
-  };
-
-  const closeDropdown = () => setShowDropdown(false);
 
   return (
     <ul
@@ -34,60 +19,9 @@ export default function WebMenu() {
         </NavLink>
       </li>
       <li className="mr-4 relative">
-        <button
-          {...linkStyles}
-          type="button"
-          id="governanceDropdown"
-          data-dropdown-toggle="gDropdown"
-          ref={govButtonRef}
-          onClick={() =>
-            showDropdown ? closeDropdown() : openDropdwonPopover()
-          }
-        >
-          Governance
-          <svg
-            className="w-4 h-4 ml-2"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </button>
-        <div
-          id="gDropdown"
-          ref={govDropdownRef}
-          className={`${
-            showDropdown ? "block" : "hidden"
-          } absolute top-10 left-0 bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700`}
-        >
-          <ul className="py-1" aria-labelledby="governanceDropdown">
-            <li>
-              <NavLink
-                to={`${url}${app.auction}`}
-                href="##"
-                className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >
-                Staking (Gov page)
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={`${url}${app.auction}`}
-                href="##"
-                className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >
-                HALO Swap (AMM page)
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        <NavLink to={`${url}${web.donors}`} {...linkStyles}>
+          For Donors
+        </NavLink>
       </li>
       <li className="mr-4">
         <NavLink
