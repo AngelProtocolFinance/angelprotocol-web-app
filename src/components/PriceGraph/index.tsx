4e0144e61cf9e93d8545cba010a706d73df9ea19
Nenad Misic <n.misic@outlook.com>
2021-12-14 11:52:33 +0100

    Modify graph container

--- src/components/PriceGraph/index.tsx
+++ src/components/PriceGraph/index.tsx
@@ -21,84 +21,6 @@ type Props = {
   lbpPairData: LBPPairData;
 };
 
-export default function PriceGraph({ isLoading, lbpPairData }: Props) {
-  const graphData = getGraphData(lbpPairData);
-
-  return (
-    <>
-      {isLoading && (
-        <Loader gapClass="gap-4" widthClass="w-4" bgColorClass="bg-white" />
-      )}
-      {!isLoading && (
-        <ResponsiveContainer
-          width="90%"
-          aspect={2}
-          className="flex justify-center content-center m-5"
-        >
-          <LineChart
-            width={500}
-            height={400}
-            margin={{ top: 50, left: 70 }}
-            data={graphData.priceData}
-            className="bg-white rounded-md pb-2"
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              tickLine={false}
-              axisLine={false}
-              tickFormatter={tickDateFormatter}
-              dataKey="timestamp"
-              allowDuplicatedCategory={false}
-              type="number"
-              ticks={graphData.dateAxisData.ticks}
-              domain={graphData.dateAxisData.axisDomain}
-              dy={10}
-              height={80}
-              interval={0}
-            />
-            <YAxis
-              tickLine={false}
-              axisLine={false}
-              type="number"
-              ticks={graphData.priceAxisData.ticks}
-              domain={graphData.priceAxisData.axisDomain}
-              dx={-15}
-              tickFormatter={tickPriceFormatter}
-            />
-            <Line
-              type="monotone"
-              strokeWidth={3}
-              dataKey="historicPrice"
-              stroke="#901ef2"
-              name={`${graphData.tokenName} price`}
-              dot={false}
-              isAnimationActive={false}
-            />
-            {lbpPairData.predictedPriceData?.length && (
-              <Line
-                type="monotone"
-                dataKey="predictedPrice"
-                stroke="#ffa6f7"
-                dot={false}
-                name={`${graphData.tokenName} predicted price`}
-                isAnimationActive={false}
-              />
-            )}
-            {graphData.referenceDotCoordinates && (
-              <ReferenceDot
-                r={4}
-                {...graphData.referenceDotCoordinates}
-                stroke="#901ef2"
-              />
-            )}
-            <Legend iconType="circle" formatter={legendFormatter} />
-          </LineChart>
-        </ResponsiveContainer>
-      )}
-    </>
-  );
-}
-
 const tickDateFormatter = (dateInMiliseconds: number) =>
   new Date(dateInMiliseconds).toLocaleString("en-us", {
     hour: "numeric",
@@ -113,13 +35,57 @@ const tickPriceFormatter = (value: number) =>
     maximumFractionDigits: 2,
   }).format(value);
 
-const legendFormatter = (value: string, _: any, index: number) => {
+export default function PriceGraph({ isLoading, lbpPairData }: Props) {
+  const graphData = getGraphData(lbpPairData);
+
   return (
-    <LegendLabel>
-      {value}
-      {!!index && (
-        <span className="text-gray-500 text-sm ml-1">(without new buyers)</span>
+    <>
+      {isLoading && (
+        <Loader gapClass="gap-4" widthClass="w-4" bgColorClass="bg-white" />
       )}
-    </LegendLabel>
+      {!isLoading && (
+        <div className="graph-container">
+          <ResponsiveContainer>
+            <LineChart
+              data={graphData.priceData}
+              className="bg-white rounded-md pb-2"
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                tickLine={false}
+                axisLine={false}
+                tickFormatter={tickDateFormatter}
+                dataKey="timestamp"
+                allowDuplicatedCategory={false}
+                type="number"
+                ticks={graphData.dateAxisData.ticks}
+                domain={graphData.dateAxisData.axisDomain}
+                dy={10}
+                height={80}
+                interval={0}
+              />
+              <YAxis
+                tickLine={false}
+                axisLine={false}
+                type="number"
+                ticks={graphData.priceAxisData.ticks}
+                domain={graphData.priceAxisData.axisDomain}
+                dx={-15}
+                tickFormatter={tickPriceFormatter}
+              />
+              <Line
+                type="monotone"
+                strokeWidth={3}
+                dataKey="historicPrice"
+                stroke="#901ef2"
+                name={`${graphData.tokenName} price`}
+                dot={false}
+                isAnimationActive={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      )}
+    </>
   );
-};
+}
