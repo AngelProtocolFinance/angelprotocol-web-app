c56898031fd78cb075a4c4c2f7f31281d1f02cb1
Justin Rafael Salas <justin@angelprotocol.io>
2021-12-16 12:53:45 +0800

    simplifications to slide in ListView

--- src/pages/Leaderboard/Heading.tsx
+++ src/pages/Leaderboard/Heading.tsx
@@ -1,16 +1,8 @@
-import useTooltip from "hooks/useTooltip";
-import _Tooltip from "./Charity/Tooltip";
-
 type Props = { text: string };
 
 export default function Heading(props: Props) {
-  const { enter, exit, handleClick } = useTooltip(_Tooltip);
-
   return (
     <th
-      onClick={handleClick}
-      onMouseEnter={enter}
-      onMouseLeave={exit}
       className={`text-left uppercase font-heading text-md p-2 pl-0 text-angel-grey`}
     >
       {props.text}
