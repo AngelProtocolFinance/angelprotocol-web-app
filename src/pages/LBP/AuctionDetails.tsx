155ee19d39f1a59b05aa9f2a6abeac888beb1b4d
Andrey <andrey@angelprotocol.io>
2021-12-20 09:12:24 +0800

    add auction details

--- src/pages/LBP/AuctionDetails.tsx
+++ src/pages/LBP/AuctionDetails.tsx
@@ -29,7 +29,7 @@ function AuctionLink({ PreIcon, content, url }: AuctionLinkProps) {
         alt="link icon"
         src={PreIcon}
       />
-      <span className="text-sm text-angel-grey font-light">{content}</span>
+      <span className="text-md text-angel-grey font-light">{content}</span>
     </a>
   );
 }
@@ -45,80 +45,113 @@ export default function AuctionDetails() {
   const remaining_halo = new Dec(pool.token).div(1e6).toNumber();
 
   return (
-    <div className="flex flex-wrap justify-between items-start font-heading">
-      <div className="auction-details flex-grow">
-        <div className="launch-description">
-          <h1 className="text-md font-semibold text-white-grey mb-5">
-            Launch Description
-          </h1>
-          <p className="text-md font-light text-white-grey mb-4">
-            HALO is the governance token of Angel Protocol
-          </p>
-        </div>
-        <h1 className="text-md font-semibold text-white-grey mb-3 mt-5">
-          Launch Details
+    <div className="auction-details font-heading">
+      <div className="launch-description mb-10">
+        <h1 className="text-md font-semibold text-white-grey mb-5">
+          Launch Description
         </h1>
-        <div className="w-full flex flex-wrap gap-5 mt-3">
-          <Details title="status" value={is_live ? "Active" : "Inactive"} />
-          <Details
-            title="start date"
-            value={new Date(start * 1000).toLocaleString()}
-          />
-          <Details
-            title="end date"
-            value={new Date(end * 1000).toLocaleString()}
-          />
-        </div>
-        <h1 className="text-md font-semibold text-white-grey mb-3 mt-5">
-          HaloSwap Statistics
-        </h1>
-        <div className="w-full flex flex-wrap gap-5 mt-3">
-          {/* <Details title="Starting HALO" value="80000000.00000" /> */}
-          <Details
-            title="Remaining HALO"
-            value={toCurrency(remaining_halo, 2)}
-          />
-        </div>
-      </div>
-      <div className="contract-details w-128 p-5">
-        <p className="font-semibold text-md mb-4">
-          HALO Token Contract Address
+        <p className="text-md font-light text-white-grey mb-4">
+          HALO is the governance token of Angel Protocol, launched following a
+          number of months work together with Delphi Labs. Details of how the
+          token works can be found{" "}
+          <a
+            className="font-light text-angel-blue"
+            href="https://members.delphidigital.io/reports/angel-protocol-token-design/"
+          >
+            here
+          </a>
+          .
         </p>
-        <div className="icon-link py-1 pb-1.5 px-3 bg-gray-100 inline-block shadow-md rounded-full mb-4">
-          <span className="text-xs text-angel-grey font-light pr-1">
-            {"terra1w8kvd6cqpsthupsk4l0clwnmek4l3zr7c84kwq"}
-          </span>
-          <Copier
-            text={"terra1w8kvd6cqpsthupsk4l0clwnmek4l3zr7c84kwq" as Addr}
-            colorClass="text-angel-blue"
+        <p className="text-md font-light text-white-grey mb-4">
+          Expanding on these tokenomics, do take a look at the recent four-part
+          series starting with{" "}
+          <a
+            className="font-light text-angel-blue"
+            href="https://angelprotocol.medium.com/halo-the-charitable-token-part-1-dc9e667b7dea?source=friends_link&sk=a62acb7590231090bb3da0febc7808cd"
+          >
+            Article One
+          </a>{" "}
+          from Angel Protocol.
+        </p>
+        <p className="text-md font-light text-white-grey mb-4">
+          Finally, for a quick video introduction to the $HALO token and what it
+          enables. Do watch and share{" "}
+          <a
+            className="font-light text-angel-blue"
+            href="https://youtu.be/sDAVEc2UYd4"
+          >
+            this short video
+          </a>
+          .
+        </p>
+      </div>
+      <div className="launch-details flex flex-row">
+        <div className="lg:w-2/3 md:w-full">
+          <h1 className="text-md font-semibold text-white-grey mb-3 mt-5">
+            Launch Details
+          </h1>
+          <div className="w-full flex flex-wrap gap-5 mt-3">
+            <Details title="status" value={is_live ? "Active" : "Inactive"} />
+            <Details
+              title="start date"
+              value={new Date(start * 1000).toLocaleString()}
+            />
+            <Details
+              title="end date"
+              value={new Date(end * 1000).toLocaleString()}
+            />
+          </div>
+          <h1 className="text-md font-semibold text-white-grey mb-3 mt-5">
+            HaloSwap Statistics
+          </h1>
+          <div className="w-full flex flex-wrap gap-5 mt-3">
+            {/* <Details title="Starting HALO" value="80000000.00000" /> */}
+            <Details
+              title="Remaining HALO"
+              value={toCurrency(remaining_halo, 2)}
+            />
+          </div>
+        </div>
+        <div className="contract-details lg:w-1/3 md:w-full flex flex-col">
+          <p className="font-semibold text-md mb-4">
+            HALO Token Contract Address
+          </p>
+          <div className="icon-link py-1 pb-1.5 px-3 bg-gray-100 inline-block shadow-md rounded-full mb-4">
+            <span className="text-xs text-angel-grey font-light pr-1">
+              {"terra1w8kvd6cqpsthupsk4l0clwnmek4l3zr7c84kwq"}
+            </span>
+            <Copier
+              text={"terra1w8kvd6cqpsthupsk4l0clwnmek4l3zr7c84kwq" as Addr}
+              colorClass="text-angel-blue"
+            />
+          </div>
+          <p className="font-semibold text-md my-4">Links </p>
+          <AuctionLink
+            content="HALO Token on ET Finder"
+            PreIcon={"/favicon.png"}
+            url="https://finder.extraterrestrial.money/mainnet/address/terra1w8kvd6cqpsthupsk4l0clwnmek4l3zr7c84kwq"
+          />
+          <AuctionLink
+            content="HaloSwap Owner on ET Finder"
+            PreIcon={"/favicon.png"}
+            url="https://finder.extraterrestrial.money/mainnet/address/terra1numzqm5mgr56ftd4y8mfen7705nfs4vpz5jf0s"
+          />
+          <AuctionLink
+            content="HaloSwap Pair  on ET Finder"
+            PreIcon={"/favicon.png"}
+            url="https://finder.extraterrestrial.money/mainnet/address/terra1hhpgcp2stvzx952zfxtxg4dhgf60yfzchesj3e"
+          />
+          <AuctionLink
+            content="HaloSwap Documentation"
+            PreIcon={"/favicon.png"}
+            url="https://bit.ly/HaloSwap"
+          />
+          <AuctionLink
+            content="HaloSwap Terms & Conditions"
+            PreIcon={"/favicon.png"}
+            url="https://drive.google.com/file/d/19R5CiaZny7UYAIKNlRc4ssvqJDhETzKk/view?usp=sharing"
           />
         </div>
-        <p className="font-semibold text-md my-4">Links </p>
-        <AuctionLink
-          content="HALO Token on ET Finder"
-          PreIcon={"/favicon.png"}
-          url="https://finder.extraterrestrial.money/mainnet/address/terra1w8kvd6cqpsthupsk4l0clwnmek4l3zr7c84kwq"
-        />
-        <AuctionLink
-          content="HaloSwap Owner on ET Finder"
-          PreIcon={"/favicon.png"}
-          url="https://finder.extraterrestrial.money/mainnet/address/terra1numzqm5mgr56ftd4y8mfen7705nfs4vpz5jf0s"
-        />
-        <AuctionLink
-          content="HaloSwap Pair  on ET Finder"
-          PreIcon={"/favicon.png"}
-          url="https://finder.extraterrestrial.money/mainnet/address/terra1hhpgcp2stvzx952zfxtxg4dhgf60yfzchesj3e"
-        />
-        <AuctionLink
-          content="HaloSwap Documentation"
-          PreIcon={"/favicon.png"}
-          url="https://bit.ly/HaloSwap"
-        />
-        <AuctionLink
-          content="HaloSwap Terms & Conditions"
-          PreIcon={"/favicon.png"}
-          url="https://drive.google.com/file/d/19R5CiaZny7UYAIKNlRc4ssvqJDhETzKk/view?usp=sharing"
-        />
       </div>
     </div>
   );
