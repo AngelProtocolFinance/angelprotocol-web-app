af6f0b597ec76fd25d123e8149adbdae211adf52
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2021-12-21 17:44:17 +0800

    Church Portal (#401)
    * basic charity endowment donation portal
    
    * split is 100% locked always
    
    * Revert "split is 100% locked always"
    
    This reverts commit c6c37163b2aae9b0045b8b74f9e5c35816b4a7a2.
    
    * 100% to locked 4 realz this time
    
    * donation request instructions

--- src/pages/ChurchPortal/ChurchPortal.tsx
+++ src/pages/ChurchPortal/ChurchPortal.tsx
@@ -1,23 +1,42 @@
-import { useGetToken } from "contexts/AuthProvider";
-import { Redirect } from "react-router-dom";
-import { app, site } from "types/routes";
+import { RouteComponentProps } from "react-router-dom";
+import DappHead from "components/Headers/DappHead";
 import Donater from "components/Donater/Donater";
 import DonateSuite from "components/TransactionSuite/DonateSuite";
-import DappHead from "components/Headers/DappHead";
 
-export default function ChurchPortal() {
-  const decodedToken = useGetToken();
-  //user can't access ChurchPortal page when not logged in or his prev token expired
-  if (!decodedToken?.token) {
-    return <Redirect to={`${site.app}/${app.login}`} />;
-  }
+export type ChurchPortalParam = { address: string };
 
+const ChurchPortal = (props: RouteComponentProps<ChurchPortalParam>) => {
+  const endowment_addr = props.match.params.address;
   return (
     <div className="grid grid-rows-a1 place-items-center pt-2">
       <DappHead />
-      <Donater to="tca">
+      <h2 className="text-2xl font-bold text-white mt-20 m-5">
+        Church Endowment Donation Portal
+      </h2>
+      <p className="text-white pb-10">
+        <span className="font-semibold">Donation Address: </span>
+        {endowment_addr || "N/A"}
+      </p>
+      <Donater to="charity" receiver={endowment_addr} min_liq={0} max_liq={0}>
         <DonateSuite />
       </Donater>
+      <p className="p-5 p-10 text-white font-light text-md">
+        If you would like a tax receipt issued please email{" "}
+        <a className="text-angel-blue" href="mailto:support@angelprotocol.io">
+          support@angelprotocol.io
+        </a>{" "}
+        with the following information:
+        <ul>
+          <li>Your name</li>
+          <li>Your address</li>
+          <li>
+            The link to your donation transaction (found in the final window
+            after donating)
+          </li>
+        </ul>
+      </p>
     </div>
   );
-}
+};
+
+export default ChurchPortal;
