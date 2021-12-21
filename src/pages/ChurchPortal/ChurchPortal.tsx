ca2fd05d1471143dbe7a4cfd751aa75b0b594ba0
Andrey <andrey@angelprotocol.io>
2021-12-21 16:15:12 +0800

    100% to locked 4 realz this time

--- src/pages/ChurchPortal/ChurchPortal.tsx
+++ src/pages/ChurchPortal/ChurchPortal.tsx
@@ -17,7 +17,7 @@ const ChurchPortal = (props: RouteComponentProps<ChurchPortalParam>) => {
         <span className="font-semibold">Donation Address: </span>
         {endowment_addr || "N/A"}
       </p>
-      <Donater to="charity" receiver={endowment_addr}>
+      <Donater to="charity" receiver={endowment_addr} min_liq={0} max_liq={0}>
         <DonateSuite />
       </Donater>
     </div>
