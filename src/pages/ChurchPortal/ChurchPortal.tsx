c8bb7c368434fe16e3bf2fb185971494f2d59637
Andrey <andrey@angelprotocol.io>
2021-12-21 16:55:06 +0800

    donation request instructions

--- src/pages/ChurchPortal/ChurchPortal.tsx
+++ src/pages/ChurchPortal/ChurchPortal.tsx
@@ -20,6 +20,21 @@ const ChurchPortal = (props: RouteComponentProps<ChurchPortalParam>) => {
       <Donater to="charity" receiver={endowment_addr} min_liq={0} max_liq={0}>
         <DonateSuite />
       </Donater>
+      <p className="p-5 p-10 text-white font-light text-md">
+        If you would like a tax receipt issued please email{" "}
+        <a className="text-angel-blue" href="mailto:support@angelprotocol.io">
+          support@angelprotocol.io
+        </a>{" "}
+        with the following information:
+        <ul>
+          <li>Your name</li>
+          <li>Your address</li>
+          <li>
+            The link to your donation transaction (found in the final window
+            after donating)
+          </li>
+        </ul>
+      </p>
     </div>
   );
 };
