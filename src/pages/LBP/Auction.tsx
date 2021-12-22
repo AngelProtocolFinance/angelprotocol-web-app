440a9082bb61cda5fce9b7a5dde965a91d740c6d
Nenad Misic <n.misic@outlook.com>
2021-12-22 18:29:18 +0100

    add text-center to haloswap ended message

--- src/pages/LBP/Auction.tsx
+++ src/pages/LBP/Auction.tsx
@@ -43,10 +43,10 @@ export default function Auction() {
             />
           </div>
           <div className="hidden xl:w-2/5 xl:flex flex-col rounded items-center justify-center p-10">
-            <p className="uppercase font-heading font-bold text-lg self-left mb-2">
+            <p className="uppercase font-heading font-bold text-lg mb-2 text-center">
               HaloSwap LBP has ended!
             </p>
-            <p className="uppercase font-heading font-bold text-lg self-left mb-2">
+            <p className="uppercase font-heading font-bold text-lg mb-2 text-center">
               Thank you to everyone who participated!
             </p>
           </div>
