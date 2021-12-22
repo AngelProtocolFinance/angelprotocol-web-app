4cbb5ccaaaffa8e4710d63a89c2c1bbc2f8ace16
Nenad Misic <n.misic@outlook.com>
2021-12-22 09:35:49 +0100

    Update responsivness of slider

--- src/components/Donator/SliderComponent.tsx
+++ src/components/Donator/SliderComponent.tsx
@@ -5,8 +5,8 @@ export default function SliderComponent(props: SliderProps) {
   const { min, max, value, onChange, onAfterChange } = props;
 
   return (
-    <div className="w-3/4 flex flex-col gap-3">
-      <span className="text-dark-grey text-xs 2xl:text-base font-semibold">
+    <div className="w-3/4 flex flex-col gap-2">
+      <span className="text-dark-grey text-xs 3xl:text-sm font-semibold">
         Percentage<sup className="text-red-500">*</sup>
       </span>
       <Slider
@@ -17,20 +17,20 @@ export default function SliderComponent(props: SliderProps) {
         onAfterChange={onAfterChange}
         className="w-full"
         railStyle={{
-          height: 10,
+          height: 6,
           backgroundColor: "lightgrey",
         }}
         // bg color is the same as angel blue, but since rc-slider doesn't support tailwind, we needed to hardcode the color
-        trackStyle={{ height: 10, backgroundColor: "#3FA9F5" }}
+        trackStyle={{ height: 6, backgroundColor: "#3FA9F5" }}
         handleStyle={{
           borderColor: "white",
-          height: 28,
-          width: 28,
+          height: 24,
+          width: 24,
           marginTop: -9,
           backgroundColor: "white",
         }}
       />
-      <p className="flex justify-between text-base">
+      <p className="flex justify-between ">
         <span>{min}%</span>
         <span>{max}%</span>
       </p>
