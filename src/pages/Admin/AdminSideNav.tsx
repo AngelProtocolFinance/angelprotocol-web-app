4b233de382e2c2df92402d317d8ec0672b8df4f0
shadrach-tayo <shadrachtemitayo@gmail.com>
2021-12-22 15:29:00 +0100

    feat: Admin UI rework and Admin section refactor

--- src/pages/Admin/AdminSideNav.tsx
+++ src/pages/Admin/AdminSideNav.tsx
@@ -1,3 +1,4 @@
+import Action from "components/ActionButton/Action";
 import { useSetToken } from "contexts/AuthProvider";
 import { NavLink, useHistory } from "react-router-dom";
 import { admin } from "types/routes";
@@ -12,13 +13,13 @@ export default function AdminSideNav() {
   };
 
   const linkStyles =
-    "block uppercase text-sm md:text-lg text-gray-400 hover:text-gray-700 py-1 p-5 text-center rounded-sm border-t border-b border-opacity-20 font-semibold";
-  const activeStyles = "bg-white bg-opacity-40 text-angel-blue";
+    "block uppercase text-sm md:text-lg hover:text-white py-1 px-5 text-center rounded-sm border-0 border-b border-opacity-20 font-semibold";
+  const activeStyles = "text-white";
 
   return (
-    <div className="flex flex-col w-128 min-h-3/4 hidden md:block bg-gray-200 py-10 rounded-l-xl font-heading">
+    <div className="flex flex-col w-128 min-h-3/4 hidden md:block bg-white bg-opacity-10 py-10 rounded-l-xl font-heading">
       <div className="flex-none">
-        <h2 className="flex-none text-2xl font-semibold capitalize text-center">
+        <h2 className="flex-none text-2xl text-white font-semibold capitalize text-center">
           <span className="inline-block">Angel Protocol:</span>
           <span className="inline-block">Admin Portal</span>
         </h2>
@@ -33,15 +34,15 @@ export default function AdminSideNav() {
           Charity Applications
         </NavLink>
         <NavLink
-          activeClassName={activeStyles}
           className={linkStyles}
+          activeClassName={activeStyles}
           to={`${admin.endowments}`}
         >
           Endowments
         </NavLink>
         <NavLink
-          activeClassName={activeStyles}
           className={linkStyles}
+          activeClassName={activeStyles}
           to={`${admin.index_fund_management}`}
         >
           Index Funds
@@ -55,13 +56,18 @@ export default function AdminSideNav() {
         </NavLink>
       </div>
       {/* side nav footer  */}
-      <div className="flex justify-center h-20 relative justify-end">
-        <button
+      <div className="flex items-end justify-center h-20 relative">
+        {/* <button
           className="mt-8 cols-start-1 col-span-2 capitalize hover:text-gray-500 text-white bg-orange disabled:bg-thin-grey shadow-md rounded-md w-48 py-2 font-bold justify-self-center absolute bottom-0"
           onClick={logout}
         >
           Logout
-        </button>
+        </button> */}
+        <Action
+          title="Logout"
+          classes="action-button font-light"
+          onClick={logout}
+        />
       </div>
     </div>
   );
