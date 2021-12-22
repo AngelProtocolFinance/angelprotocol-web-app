b400ee6093a137d4dc3ebaefdbfb9d878233ec72
Andrey <andrey@angelprotocol.io>
2021-12-22 16:28:56 +0800

    add swap to Gov page & setup for Loop LPs

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -24,6 +24,12 @@ export const contracts: Contracts = {
     [sc.lbp_pair]: "terra1hhpgcp2stvzx952zfxtxg4dhgf60yfzchesj3e",
     [sc.lbp_router]: "terra1l32eafhapmn9c8m7epyraxa2yty4xngamvewfs",
     [sc.lbp_lp]: "terra1kt26adtzwu4yefw37snr73n393vsu8w0hmazxc",
+
+    //lbp
+    [sc.loop_factory]: "terra10dl5u40lj50scntv4qmwykfw2zulf77zyv34u0",
+    [sc.loop_router]: "terra1l32eafhapmn9c8m7epyraxa2yty4xngamvewfs",
+    [sc.loop_haloust_pair]: "terra1hhpgcp2stvzx952zfxtxg4dhgf60yfzchesj3e",
+    [sc.loop_haloust_lp]: "terra1kt26adtzwu4yefw37snr73n393vsu8w0hmazxc",
   },
   [chainIDs.testnet]: {
     //core
@@ -42,6 +48,12 @@ export const contracts: Contracts = {
     [sc.lbp_pair]: "terra17al3hudq2vcxtyvw9008edjhyqaw74mayva2d8",
     [sc.lbp_router]: "terra19dpanzuhtmdsw8ds5zschrh4mnxcejc0ut6dnk",
     [sc.lbp_lp]: "terra19zgdunfrx79nqvznqmx4satj5kxndvmrsx502m",
+
+    //lbp
+    [sc.loop_factory]: "terra167m64seqj7cucxm5wep3hyu4suqw4sl5s8uzjz",
+    [sc.loop_router]: "terra19dpanzuhtmdsw8ds5zschrh4mnxcejc0ut6dnk",
+    [sc.loop_haloust_pair]: "terra19dpanzuhtmdsw8ds5zschrh4mnxcejc0ut6dnk",
+    [sc.loop_haloust_lp]: "terra19zgdunfrx79nqvznqmx4satj5kxndvmrsx502m",
   },
   [chainIDs.localterra]: {
     //core
@@ -60,5 +72,11 @@ export const contracts: Contracts = {
     [sc.lbp_pair]: "",
     [sc.lbp_router]: "",
     [sc.lbp_lp]: "",
+
+    //lbp
+    [sc.loop_factory]: "",
+    [sc.loop_router]: "",
+    [sc.loop_haloust_pair]: "",
+    [sc.loop_haloust_lp]: "",
   },
 };
