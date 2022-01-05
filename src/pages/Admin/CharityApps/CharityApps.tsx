3dff6520114233e9012b6c112997c5e528784a91
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-01-05 14:48:39 +0100

    pr fix

--- src/pages/Admin/CharityApps/CharityApps.tsx
+++ src/pages/Admin/CharityApps/CharityApps.tsx
@@ -1,11 +1,9 @@
 import { useGetAuthorized } from "contexts/AuthProvider";
 import { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
-import { useGetToken } from "contexts/AuthProvider";
 import { Redirect } from "react-router-dom";
 import { useGetCharityListEndowmentQuery } from "services/aws/charity";
 import { admin, site } from "types/routes";
-import AdminSideNav from "../AdminSideNav";
 import CharityTable from "./CharityTable";
 import Loader from "components/Loader/Loader";
 import withSideNav from "Admin/withSideNav";
