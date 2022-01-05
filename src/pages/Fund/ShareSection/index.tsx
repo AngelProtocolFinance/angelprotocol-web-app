a0cd6d3f23fa0970f29b96227fcefb76ae08d0e7
Nenad Misic <n.misic@outlook.com>
2022-01-05 12:52:01 +0100

    Add share urls for social media

--- src/pages/Fund/ShareSection/index.tsx
+++ src/pages/Fund/ShareSection/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { FaFacebookSquare, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { CSSTransition } from "react-transition-group";
+import {
+  FACEBOOK_SHARE_URL,
+  LINKEDIN_SHARE_URL,
+  TWITTER_SHARE_URL,
+} from "./shareUrls";
 import "./styles.css";
 
 type IconProps = {
@@ -24,12 +29,9 @@ function ShareSection({ isOpen }: any) {
   return (
     <CSSTransition in={isOpen} timeout={300} classNames="share" unmountOnExit>
       <div className="flex gap-2 text-angel-blue h-full">
-        <IconLink link="https://twitter.com/angelprotocol" icon={FaTwitter} />
-        <IconLink
-          link="https://www.linkedin.com/company/angel-protocol"
-          icon={FaLinkedinIn}
-        />
-        <IconLink link="" icon={FaFacebookSquare} />
+        <IconLink link={TWITTER_SHARE_URL} icon={FaTwitter} />
+        <IconLink link={LINKEDIN_SHARE_URL} icon={FaLinkedinIn} />
+        <IconLink link={FACEBOOK_SHARE_URL} icon={FaFacebookSquare} />
       </div>
     </CSSTransition>
   );
