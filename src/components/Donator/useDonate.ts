64dd42e7aeb15d2f34f5c588b4b8be5790c99b51
Nenad Misic <n.misic@outlook.com>
2022-01-10 16:23:38 +0100

    Allow prettier to format useDonate (and format it)

--- src/components/Donator/useDonate.ts
+++ src/components/Donator/useDonate.ts
@@ -1,17 +1,21 @@
-import { useConnectedWallet } from "@terra-money/wallet-provider";
-import { Values, Steps, SetStatus, Status } from "./types";
-import createStatusFromError from "./createStatusFromError";
-import Indexfund from "contracts/IndexFund";
-import useUSTBalance from "hooks/useUSTBalance";
-import { FormikHelpers } from "formik";
 import { AccAddress } from "@terra-money/terra.js";
-import getDepositAmount from "./getDepositAmount";
-import Account from "contracts/Account";
+import { useConnectedWallet } from "@terra-money/wallet-provider";
 import { denoms } from "constants/currency";
-import { useLogDonationTransactionMutation } from "services/apes/donations";
+import Account from "contracts/Account";
+import Indexfund from "contracts/IndexFund";
+import { FormikHelpers } from "formik";
 import createAuthToken from "helpers/createAuthToken";
-//prettier-ignore
-function useDonate(status: Status, setStatus: SetStatus, receiver?: AccAddress | number ) {
+import useUSTBalance from "hooks/useUSTBalance";
+import { useLogDonationTransactionMutation } from "services/apes/donations";
+import createStatusFromError from "./createStatusFromError";
+import getDepositAmount from "./getDepositAmount";
+import { SetStatus, Status, Steps, Values } from "./types";
+
+function useDonate(
+  status: Status,
+  setStatus: SetStatus,
+  receiver?: AccAddress | number
+) {
   const wallet = useConnectedWallet();
   const UST_balance = useUSTBalance();
   const [logDonationTransaction] = useLogDonationTransactionMutation();
@@ -22,7 +26,7 @@ function useDonate(status: Status, setStatus: SetStatus, receiver?: AccAddress |
     const UST_Amount = values.amount;
 
     //values.split = split to locked acc
-    const splitToLiquid = 100 - values.split
+    const splitToLiquid = 100 - values.split;
 
     actions.setSubmitting(true);
     if (!wallet) {
@@ -45,10 +49,10 @@ function useDonate(status: Status, setStatus: SetStatus, receiver?: AccAddress |
     try {
       //typeof receiver for IndexFund is number | undefined as enforced by <Donator/> Props
       const contract =
-        typeof receiver === 'number' || typeof receiver === 'undefined' 
-          ? new Indexfund(wallet, receiver) 
-          : new Account(receiver, wallet); 
-      
+        typeof receiver === "number" || typeof receiver === "undefined"
+          ? new Indexfund(wallet, receiver)
+          : new Account(receiver, wallet);
+
       //createTx errors will be on catch block
       const transaction = await contract.createDepositTx(
         UST_Amount,
@@ -83,11 +87,14 @@ function useDonate(status: Status, setStatus: SetStatus, receiver?: AccAddress |
           message: "Waiting for transaction result",
         });
 
-        const getTxInfo = contract.pollTxInfo(response.result.txhash, 7, 1000)
+        const getTxInfo = contract.pollTxInfo(response.result.txhash, 7, 1000);
         const txInfo = await getTxInfo;
 
         if (!txInfo.code) {
-          const depositAmount = getDepositAmount(txInfo.logs!, wallet.network.chainID);
+          const depositAmount = getDepositAmount(
+            txInfo.logs!,
+            wallet.network.chainID
+          );
           // Every transaction is recorded in our APES AWS DynamoDB donations table
           // When a Tax Receipt is requested, an email will be sent to them
           let valuesToBeSubmitted: any = values;
@@ -96,7 +103,10 @@ function useDonate(status: Status, setStatus: SetStatus, receiver?: AccAddress |
           valuesToBeSubmitted["fundId"] = fundId;
           valuesToBeSubmitted["transactionId"] = txInfo.txhash;
           valuesToBeSubmitted["transactionDate"] = new Date().toISOString();
-          Object.keys(valuesToBeSubmitted).forEach(key => valuesToBeSubmitted[key] === "" && delete valuesToBeSubmitted[key]); // Removes blank strings ("")
+          Object.keys(valuesToBeSubmitted).forEach(
+            (key) =>
+              valuesToBeSubmitted[key] === "" && delete valuesToBeSubmitted[key]
+          );
           // Auth token to be passed as part of the header of the request
           const authToken = createAuthToken("angelprotocol-web-app");
           // Call APES endpoint
@@ -108,7 +118,9 @@ function useDonate(status: Status, setStatus: SetStatus, receiver?: AccAddress |
           };
 
           const response: any = await logDonationTransaction(postData); // Logs all donation transactions in APES' donations DynamoDB table
-          const result = response.error ? response.error.data.message : response.data.message; // Contains the success messages or some instructions if an error occured in APES AWS
+          const result = response.error
+            ? response.error.data.message
+            : response.data.message; // Contains the success messages or some instructions if an error occured in APES AWS
           setStatus({
             step: Steps.success,
             message: result,
@@ -124,7 +136,6 @@ function useDonate(status: Status, setStatus: SetStatus, receiver?: AccAddress |
             message: `The transaction ran but failed`,
           });
         }
-  
       }
     } catch (error) {
       console.error(error);
