ef3dbdcff6236c9ccb77b2a822ca91a06d9eed09
Nenad Misic <n.misic@outlook.com>
2022-01-11 14:43:15 +0100

    Fix ButtonSection responsivness on smaller screens

--- src/pages/Fund/ButtonSection.tsx
+++ src/pages/Fund/ButtonSection.tsx
@@ -13,13 +13,13 @@ export default function ButtonSection({ showShare, onDonate, onShare }: Props) {
     <div className="flex justify-center lg:justify-start gap-4 lg:gap-0 lg:flex-col order-3 lg:order-4">
       <Action
         title="Donate"
-        classes="bg-orange w-2/5 lg:w-1/2 h-12"
+        classes="bg-orange w-2/5 sm:w-52 h-12"
         onClick={onDonate}
       />
       <div className="flex gap-5">
         <Action
           title="Share"
-          classes="bg-angel-blue w-2/5 lg:w-1/2 h-12"
+          classes="bg-angel-blue w-2/5 sm:w-52 h-12"
           onClick={onShare}
         />
         <ShareSection isOpen={showShare} />
