95405e3e0a8eab8e4cf86dc08eb905993b51ff03
Nenad Misic <n.misic@outlook.com>
2022-01-11 16:40:09 +0100

    Add overflow to fund description

--- src/pages/Fund/Overview.tsx
+++ src/pages/Fund/Overview.tsx
@@ -6,7 +6,7 @@ export default function Overview(props: { fund_id: number }) {
   return (
     <article className="col-start-1 col-span-1 row-start-2 row-span-1">
       <h3 className="text-2xl text-white mb-4 font-semibold">{sdg.desc}</h3>
-      <p className="text-white-grey pr-4">
+      <p className="text-white-grey pr-4 max-h-96 overflow-y-auto">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam vero
         similique sapiente voluptate repellat dolorum doloremque perferendis
         iusto? Amet ea, est repellendus ut quisquam maxime nam voluptatum et
