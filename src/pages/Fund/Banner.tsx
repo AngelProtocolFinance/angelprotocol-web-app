37367d1f7a5b411549554e94037623f2321711d3
Nenad Misic <n.misic@outlook.com>
2022-01-11 14:41:46 +0100

    Change Banner layout on sm screen

--- src/pages/Fund/Banner.tsx
+++ src/pages/Fund/Banner.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function Banner({ bg, icon, title, fund_id }: Props) {
   return (
     <div
-      className={`${bg} flex-1 order-1 flex flex-col md:flex-row items-center rounded-xl shadow-md`}
+      className={`${bg} flex-1 order-1 flex flex-col sm:flex-row items-center rounded-xl shadow-md`}
     >
       <img src={icon} alt="" className="h-24 md:h-44 m-4 md:m-9" />
       <h1 className="text-3xl md:text-5xl text-white uppercase font-bold ">
