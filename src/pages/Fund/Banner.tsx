884a50486a76a2e4cc95f101cd794e0c3e128f05
Nenad Misic <n.misic@outlook.com>
2022-01-11 14:40:09 +0100

    Make Banner more mobile friendly

--- src/pages/Fund/Banner.tsx
+++ src/pages/Fund/Banner.tsx
@@ -10,10 +10,10 @@ type Props = {
 export default function Banner({ bg, icon, title, fund_id }: Props) {
   return (
     <div
-      className={`${bg} flex-1 order-1 col-start-1 col-span-1 self-stretch grid grid-cols-a1 items-center rounded-xl shadow-md`}
+      className={`${bg} flex-1 order-1 flex flex-col md:flex-row items-center rounded-xl shadow-md`}
     >
-      <img src={icon} alt="" className="h-44 m-9" />
-      <h1 className="text-5xl text-white uppercase font-bold ">
+      <img src={icon} alt="" className="h-24 md:h-44 m-4 md:m-9" />
+      <h1 className="text-3xl md:text-5xl text-white uppercase font-bold ">
         <div className="mb-2">{`UNSDG#${fund_id}`}</div>
         <div>{title}</div>
       </h1>
