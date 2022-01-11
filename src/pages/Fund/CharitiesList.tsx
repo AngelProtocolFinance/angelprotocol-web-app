9b24fee55ae63e9990b733ea76357181975f8062
Nenad Misic <n.misic@outlook.com>
2022-01-11 14:29:38 +0100

    Make CharitiesList more mobile-friendly

--- src/pages/Fund/CharitiesList.tsx
+++ src/pages/Fund/CharitiesList.tsx
@@ -8,11 +8,11 @@ type Props = {
 
 export default function CharitiesList({ profiles }: Props) {
   return (
-    <div className="mt-8 text-white-grey">
+    <div className="mt-8 text-white-grey flex flex-col items-center lg:items-start">
       <p className="uppercase text-2xl font-heading font-semibold mb-4">
         Charities in this index
       </p>
-      <ul className="flex flex-wrap gap-4">
+      <ul className="flex flex-wrap gap-4 justify-center">
         {profiles.map((profile) => (
           <div className="max-h-116 overflow-hidden">
             <CharityCard address={profile.endowment_address} />
