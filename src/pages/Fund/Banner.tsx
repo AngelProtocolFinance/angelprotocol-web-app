42f6c3e3aaf23477ec7f225f2456d38ba34bcee4
Nenad Misic <n.misic@outlook.com>
2022-01-11 16:57:39 +0100

    Make Banner icon and text sizes more responsive

--- src/pages/Fund/Banner.tsx
+++ src/pages/Fund/Banner.tsx
@@ -12,8 +12,12 @@ export default function Banner({ bg, icon, title, fund_id }: Props) {
     <div
       className={`${bg} flex-1 order-1 flex flex-col sm:flex-row items-center rounded-xl shadow-md`}
     >
-      <img src={icon} alt="" className="h-24 md:h-44 m-4 md:m-9" />
-      <h1 className="text-3xl md:text-5xl text-white uppercase font-bold ">
+      <img
+        src={icon}
+        alt=""
+        className="h-24 md:h-44 lg:h-24 xl:h-44 m-4 md:m-9"
+      />
+      <h1 className="text-3xl md:text-5xl lg:text-3xl xl:text-5xl text-white uppercase font-bold ">
         <div className="mb-2">{`UNSDG#${fund_id}`}</div>
         <div>{title}</div>
       </h1>
