388d93507dbb446178be5592399be194a67aba2f
Justin Rafael Salas <justin@angelprotocol.io>
2022-01-12 19:05:06 +0800

    massive dependency trimming

--- src/components/DonateForm/useDonateForm.ts
+++ src/components/DonateForm/useDonateForm.ts
@@ -1,21 +1,21 @@
 import { Values } from "components/Donater/types";
 import { denoms } from "constants/currency";
 import { useFormContext } from "react-hook-form";
-import useEthSender from "../Donater/useEthSender";
+// import useEthSender from "../Donater/useEthSender";
 import useUSTSender from "../Donater/useUSTSender";
-import useBTCSender from "../Donater/useBTCSender";
-import useSolSender from "components/Donater/useSolSender";
-import useAtomSender from "components/Donater/useAtomSender";
+// import useBTCSender from "../Donater/useBTCSender";
+// import useSolSender from "components/Donater/useSolSender";
+// import useAtomSender from "components/Donater/useAtomSender";
 import { useEffect, useRef } from "react";
 
 type Senders = { [index: string]: (data: Values) => Promise<void> };
 export default function useSubmit() {
   const { watch, handleSubmit, formState, setValue } = useFormContext<Values>();
   const ustSender = useUSTSender();
-  const ethSender = useEthSender();
-  const btcSender = useBTCSender();
-  const solSender = useSolSender();
-  const atomSender = useAtomSender();
+  // const ethSender = useEthSender();
+  // const btcSender = useBTCSender();
+  // const solSender = useSolSender();
+  // const atomSender = useAtomSender();
   const denomRef = useRef<denoms>(denoms.uusd);
   const currency = watch("currency");
 
@@ -30,10 +30,10 @@ export default function useSubmit() {
 
   const senders: Senders = {
     [denoms.uusd]: ustSender,
-    [denoms.ether]: ethSender,
-    [denoms.btc]: btcSender,
-    [denoms.sol]: solSender,
-    [denoms.uatom]: atomSender,
+    // [denoms.ether]: ethSender,
+    // [denoms.btc]: btcSender,
+    // [denoms.sol]: solSender,
+    // [denoms.uatom]: atomSender,
   };
 
   return {
