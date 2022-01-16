40be1ac0f9a93c98210bf503992883f513e81c34
ap-justin <justin@angelprotocol.io>
2022-01-16 14:56:12 +0000

    change default offer amount

--- src/components/Swapper/getSpotPrice.ts
+++ src/components/Swapper/getSpotPrice.ts
@@ -1,11 +1,11 @@
 import { Dec } from "@terra-money/terra.js";
 import { Simulation } from "services/terra/types";
-export function getSpotPrice(simul: Simulation, offer = 1_000_000) {
+export function getSpotPrice(simul: Simulation, offer = 1) {
   if (simul.is_placeholder) {
     return new Dec(0);
   }
   const return_amount = new Dec(simul.return_amount);
-  const offer_amount = new Dec(offer);
+  const offer_amount = new Dec(offer).mul(1e6);
 
   return offer_amount.div(return_amount);
 }
