2254a9b686c2375e9b8bdac116d2dc41a967e9fd
Nenad Misic <n.misic@outlook.com>
2022-01-17 12:23:11 +0100

    Lazy load pages on ./src/Website/Website.tsx

--- src/Website/Website.tsx
+++ src/Website/Website.tsx
@@ -6,13 +6,16 @@ import {
   useLocation,
 } from "react-router-dom";
 import Home from "pages/Home/Home";
-import Donors from "pages/Donors/Donors";
-import PrivacyPolicy from "pages/PrivacyPolicy";
-import Charities from "pages/Charities/Charities";
-import Contact from "pages/Contact/Contact";
 import WebHead from "components/Headers/WebHead";
 import WebFoot from "components/Footers/WebFoot";
 import { site, web } from "types/routes";
+import { lazy, Suspense } from "react";
+import Loader from "components/Loader/Loader";
+
+const Donors = lazy(() => import("pages/Donors/Donors"));
+const PrivacyPolicy = lazy(() => import("pages/PrivacyPolicy"));
+const Charities = lazy(() => import("pages/Charities/Charities"));
+const Contact = lazy(() => import("pages/Contact/Contact"));
 
 const Website = () => {
   const { path } = useRouteMatch();
@@ -20,15 +23,25 @@ const Website = () => {
   return (
     <div className={`grid grid-rows-1a bg-white`}>
       <WebHead />
-      <Switch>
-        <Redirect from="/:url*(/+)" to={location.pathname.slice(0, -1)} />
-        <Route path={`${path}${web.contact}`} component={Contact} />
-        <Route path={`${path}${web.privacy}`} component={PrivacyPolicy} />
-        <Route path={`${path}${web.donors}`} component={Donors} />
-        <Route path={`${path}${web.charities}`} component={Charities} />
-        <Route path={`${path}${web.index}`} component={Home} />
-        <Redirect from="*" to={site.home} />
-      </Switch>
+      <Suspense
+        fallback={
+          <Loader
+            bgColorClass="bg-angel-blue"
+            gapClass="gap-2"
+            widthClass="w-4"
+          />
+        }
+      >
+        <Switch>
+          <Redirect from="/:url*(/+)" to={location.pathname.slice(0, -1)} />
+          <Route path={`${path}${web.contact}`} component={Contact} />
+          <Route path={`${path}${web.privacy}`} component={PrivacyPolicy} />
+          <Route path={`${path}${web.donors}`} component={Donors} />
+          <Route path={`${path}${web.charities}`} component={Charities} />
+          <Route path={`${path}${web.index}`} component={Home} />
+          <Redirect from="*" to={site.home} />
+        </Switch>
+      </Suspense>
       <WebFoot />
     </div>
   );
