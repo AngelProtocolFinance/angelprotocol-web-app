4600efe89dadc2200695951aac0935f03f28efa6
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-01-19 19:12:20 +0100

    refactor: clean up

--- src/pages/Market/CharityCard.tsx
+++ src/pages/Market/CharityCard.tsx
@@ -1,35 +1,28 @@
-import { Link, useHistory, useRouteMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { app, site } from "types/routes";
 import useCharityCard from "./useCharityCard";
 
 export default function CharityCard(props: { address: string }) {
   const profile = useCharityCard(props.address);
-  const history = useHistory();
-  const { path } = useRouteMatch();
-
-  const open = () => {
-    history.push(`${path}${app.charity}/${props.address}`);
-  };
 
   return (
-    <div
+    <Link
+      to={`${site.app}/${app.charity}/${props.address}`}
       className="relative w-72 flex-none break-words rounded-2xl hover:shadow-3xl cursor-pointer mb-4 mx-2 p-2"
-      onClick={open}
     >
       <img
         className="bg-white rounded-lg img-no-drag w-full h-32 object-cover"
         src={profile.charity_image}
         alt="charity banner"
       />
-      <Link
-        to={`${site.app}/${app.charity}/${props.address}`}
+      <p
         className={`block cursor-pointer font-heading text-white-grey hover:text-angel-orange font-bold text-sm uppercase mt-1.5`}
       >
         {profile.charity_name}
-      </Link>
+      </p>
       <span className="text-opacity-80 line-clamp-2 text-sm text-white-grey">
         {profile.charity_overview}
       </span>
-    </div>
+    </Link>
   );
 }
