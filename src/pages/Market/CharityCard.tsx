bff8e4ef70ad235b628fbd06a68d8eb67c58a12c
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2022-01-20 11:44:49 +0100

    fix: make charity chard clickable (#521)
    * fix: make charity chard clickable
    
    * fix: padding on title and texts
    
    * remove undefined row
    
    * refactor: clean up
    
    * Change templ. literal to reg. string
    
    Co-authored-by: Nenad Misic <n.misic@outlook.com>

--- src/pages/Market/CharityCard.tsx
+++ src/pages/Market/CharityCard.tsx
@@ -4,22 +4,23 @@ import useCharityCard from "./useCharityCard";
 
 export default function CharityCard(props: { address: string }) {
   const profile = useCharityCard(props.address);
+
   return (
-    <div className="relative w-72 flex-none break-words">
+    <Link
+      to={`${site.app}/${app.charity}/${props.address}`}
+      className="relative w-72 flex-none break-words rounded-2xl hover:shadow-3xl cursor-pointer mb-4 mx-2 p-2"
+    >
       <img
         className="bg-white rounded-lg img-no-drag w-full h-32 object-cover"
         src={profile.charity_image}
         alt="charity banner"
       />
-      <Link
-        to={`${site.app}/${app.charity}/${props.address}`}
-        className="block cursor-pointer font-heading text-white-grey hover:text-angel-orange font-bold text-sm uppercase mt-1.5"
-      >
+      <p className="block cursor-pointer font-heading text-white-grey hover:text-angel-orange font-bold text-sm uppercase mt-1.5">
         {profile.charity_name}
-      </Link>
+      </p>
       <span className="text-opacity-80 line-clamp-2 text-sm text-white-grey">
         {profile.charity_overview}
       </span>
-    </div>
+    </Link>
   );
 }
