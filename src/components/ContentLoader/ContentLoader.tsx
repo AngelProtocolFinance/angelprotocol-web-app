c9073166e75b3b2ec7b7265111dd00ec13b1d613
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-01-21 20:40:06 +0100

    refactor: content loader colors

--- src/components/ContentLoader/ContentLoader.tsx
+++ src/components/ContentLoader/ContentLoader.tsx
@@ -10,20 +10,20 @@ export default function ContentLoader(props: LoaderProps) {
   return (
     <svg width={props.width || "100%"} height={props.height || "100%"}>
       <defs>
-        <linearGradient id="myGradient" gradientTransform="rotate(20)">
+        <linearGradient id="myGradient" gradientTransform="rotate(35)">
           <stop offset="5%" stopColor="#eee">
             <animate
               attributeName="stop-color"
-              values="#EEEEEE; #CCCCCC; #EEEEEE"
-              dur="2s"
+              values="#EEEEEE; #DDDDDD; #EEEEEE"
+              dur="3s"
               repeatCount="indefinite"
             />
           </stop>
           <stop offset="95%" stopColor="#aaa">
             <animate
               attributeName="stop-color"
-              values="#DDDDDD; #AAAAAA; #EEEEEE"
-              dur="3s"
+              values="#EEEEEE; #AAAAAA; #EEEEEE"
+              dur="1s"
               repeatCount="indefinite"
             />
           </stop>
