4fd033f5085c2ea0017ce20e088fec4b7b67889e
GreenCode Dev <52554129+greencodedev@users.noreply.github.com>
2022-01-24 20:53:54 +0800

    complete refactoring withdraw form (#530)
    * complete refactoring withdraw form
    
    * remove unused file
    
    * merged codes
    
    * fix feedback
    
    * fix debouncer feedback
    
    * Withdraw refactor updates (#543)
    
    * included multiple vaults
    
    * estimate tx on different withdraw sources
    
    * change form mode
    
    * finalize tx and add meta
    
    * remove lint errors
    
    * disable builtin form validation
    
    Co-authored-by: Andrey <andrey@angelprotocol.io>
    Co-authored-by: ap-justin <89639563+ap-justin@users.noreply.github.com>

--- src/pages/Endowment_Admin/Endowment_Admin.tsx
+++ src/pages/Endowment_Admin/Endowment_Admin.tsx
@@ -1,113 +1,55 @@
-import { createContext, useContext, useState } from "react";
-import Loader from "components/Loader/Loader";
-import toCurrency from "helpers/toCurrency";
-import Liquid from "./Liquid";
-import Locked from "./Locked";
-import WithdrawForm from "./WithdrawForm";
-import useWithdraw from "./useWithdraw";
+import Liquid from "./Summary";
 import { RouteComponentProps } from "react-router-dom";
-import { Redirect } from "react-router-dom";
-import { site } from "types/routes";
-import { RouteParam, Steps, Status, SetStatus } from "./types";
+import { RouteParam } from "./types";
 import TransactionList from "./TransactionList";
-
-const initialStatus = {
-  step: Steps.initial,
-};
-
-const getContext = createContext<Status>(initialStatus);
-const setContext = createContext<SetStatus>(() => initialStatus);
-//use these hooks only in components inside Withdraw.tsx
-export const useGetStatus = () => useContext(getContext);
-export const useSetStatus = () => useContext(setContext);
+import { useSetModal } from "components/Nodal/Nodal";
+import Withdrawer from "components/Withdraw/Withdrawer";
+import WithdrawSuite from "components/TransactionSuite/WithdrawSuite";
+import { useExchangeRate } from "services/terra/vaults/queriers";
+import { useEndowmentHoldings } from "services/terra/queriers";
+import useProfile from "pages/Market/useProfile";
+import { useConnectedWallet } from "@terra-dev/use-wallet";
+import PageMeta from "./PageMeta";
 
 export default function Withdraw(props: RouteComponentProps<RouteParam>) {
-  //use can malinger this address url param
-  //can also pass address as state but no guarantee that user will go to this page using
-  //only the in-app link provided
+  const wallet = useConnectedWallet();
+  //fetch exchange rate here
+  useExchangeRate();
   const address = props.match.params.address;
-  const [status, setStatus] = useState<Status>(initialStatus);
+  const profile = useProfile(address);
+  const { holdings } = useEndowmentHoldings(address, profile.is_placeholder);
 
-  const [withdrawFormIsOpen, setWithdrawForm] = useState(false);
-  const {
-    redirect,
-    isReady,
-    isLoading,
-    isEndowmentOwner,
-    error,
-    locked,
-    liquid,
-    overall,
-  } = useWithdraw(address);
+  const is_owner = profile.charity_owner === wallet?.walletAddress;
 
+  const { showModal } = useSetModal();
   function openWithdrawForm() {
-    setWithdrawForm(true);
-  }
-
-  function closeWithdrawForm() {
-    setWithdrawForm(false);
+    showModal(WithdrawModal, {
+      address: address,
+    });
   }
 
   return (
-    <div className="grid content-start">
-      {redirect ? <Redirect to={site.app} /> : null}
-      {error && (
-        <div className="min-h-withdraw-table grid place-items-center">
-          <p className="uppercase text-white-grey">{error}</p>
-        </div>
-      )}
-      {isLoading && (
-        <div className="min-h-withdraw-table grid place-items-center">
-          <Loader
-            gapClass="gap-4"
-            widthClass="w-4"
-            bgColorClass="bg-white-grey"
-          />
-        </div>
-      )}
-      {isReady && (
-        <div className="padded-container justify-center">
-          <div className="mt-0 md:mt-8 mx-auto w-auto text-white-grey">
-            <h2 className="uppercase mb-2">
-              <span className="uppercase text-xs mr-0.5">Total Balance </span>
-              <span className="text-lg font-semibold">
-                ${toCurrency(overall)}
-              </span>
-            </h2>
-            <div className="grid gap-4 sm:grid-cols-2">
-              <Liquid
-                liquidBalance={liquid}
-                opener={openWithdrawForm}
-                isOwner={isEndowmentOwner}
-              />
-              <Locked lockedBalance={locked} />
-            </div>
-            <div>
-              <getContext.Provider value={status}>
-                <setContext.Provider value={setStatus}>
-                  <WithdrawForm
-                    address={address}
-                    liquid={liquid!}
-                    isModalOpened={withdrawFormIsOpen}
-                    onCloseModal={closeWithdrawForm}
-                  />
-                </setContext.Provider>
-              </getContext.Provider>
-            </div>
-          </div>
-          <div className="self-start mb-6 mt-4 w-full font-heading">
-            <TransactionList address={address} />
-          </div>
-        </div>
-        // <div className="flex justify-end padded-container mb-6 md:px-10 xl:px-6 px-6">
-        //   <button
-        //     className="uppercase hover:bg-blue-accent bg-angel-blue rounded-lg w-56 h-12 text-sm font-bold text-white"
-        //     // onClick={openWithdrawForm}
-        //   >
-        //     Manage Admins
-        //   </button>
-        // </div>
-      )}
+    <div className="grid grid-cols-2 gap-4 content-start padded-container justify-center">
+      <PageMeta address={address} />
+      <Liquid
+        type="liquid"
+        holdings={holdings.liquid_cw20}
+        opener={openWithdrawForm}
+        isOwner={is_owner}
+      />
+      <Liquid type="locked" holdings={holdings.locked_cw20} />
+
+      <TransactionList address={address} />
     </div>
+  );
+}
+
+type WithdrawProps = { address: string };
+
+function WithdrawModal(props: WithdrawProps) {
+  return (
+    <Withdrawer account_addr={props.address}>
+      <WithdrawSuite inModal />
+    </Withdrawer>
   );
 }
