46ebd67307e7596d3a9e139f92d8c815e2d8e365
Nenad Misic <n.misic@outlook.com>
2022-01-27 06:27:17 +0100

    App Footer: Make "Register" Link Navigate to Registration Page (#565)
    * Make 'Register' Link Navigate to Registration Page
    
    * Move link to separate component

--- src/components/NavMenus/AppMenu.tsx
+++ src/components/NavMenus/AppMenu.tsx
@@ -1,31 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, LinkProps, useRouteMatch } from "react-router-dom";
 import { app } from "types/routes";
-import { useRouteMatch } from "react-router-dom";
 
 export default function AppMenu() {
   const { url } = useRouteMatch();
-  const linkStyles = {
-    className:
-      "text-white-grey text-sm hover:text-opacity-75 px-1 lg:text-base font-heading uppercase font-semibold lg:px-2",
-  };
 
   return (
     <ul className="flex lg:items-center">
-      <li>
-        <Link to={`${url}/${app.leaderboard}`} {...linkStyles}>
-          LEADERBOARD
-        </Link>
-      </li>
-      <li>
-        <a
-          rel="noreferrer"
-          target="_blank"
-          href="https://www.angelprotocol.io/contact"
-          {...linkStyles}
-        >
-          REGISTER
-        </a>
-      </li>
+      <ListLinkItem to={`${url}/${app.leaderboard}`}>LEADERBOARD</ListLinkItem>
+      <ListLinkItem
+        rel="noreferrer"
+        target="_blank"
+        to="https://www.angelprotocol.io/contact"
+      >
+        REGISTER
+      </ListLinkItem>
     </ul>
+  );
+}
+
+function ListLinkItem({ className, ...rest }: LinkProps) {
+  return (
+    <li>
+      <Link
+        className={`text-white-grey text-sm hover:text-opacity-75 px-1 lg:text-base font-heading uppercase font-semibold lg:px-2 ${className}`}
+        {...rest}
+      />
+    </li>
   );
 }
