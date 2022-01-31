eeb1cebd8572014817229b52627d78da942c2dfb
Nenad Misic <n.misic@outlook.com>
2022-01-31 02:36:19 +0100

    Charity Registration: Send an Email to AP Team when User Clicks on "Please Help Me" Button (#598)
    * Implement sendEmailNoticeToAPTeam
    
    * Wrap functions with useCallback
    
    * Move updating userData into useEffect
    
    * Move common send-email logic to a separate useCallback
    
    * Decrease button width
    
    * Refactor ConfirmEmail

--- src/pages/registration/ConfirmEmail.tsx
+++ src/pages/registration/ConfirmEmail.tsx
@@ -1,10 +1,11 @@
-import { useHistory, useLocation } from "react-router-dom";
 import banner2 from "assets/images/banner-register-2.jpg";
+import { useCallback, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import Action from "../../components/ActionButton/Action";
-import { useGetter, useSetter } from "store/accessors";
 import { useRequestEmailMutation } from "services/aws/registration";
 import { removeUserData, updateUserData } from "services/user/userSlice";
+import { useGetter, useSetter } from "store/accessors";
+import Action from "../../components/ActionButton/Action";
 
 const ConfirmEmail = () => {
   const history = useHistory();
@@ -12,78 +13,88 @@ const ConfirmEmail = () => {
   let user = useGetter((state) => state.user);
   const location: any = useLocation();
   const is_sent = location.state?.is_sent;
-  //eslint-disable-next-line
   const [resendEmail, { isLoading }] = useRequestEmailMutation();
-  const resendVerificationEmail = async () => {
-    if (user.PK) {
+
+  const sendEmail = useCallback(
+    async (emailType) => {
+      if (!user.PK) {
+        toast.error("Invalid Data. Please ask the administrator about that.");
+        return;
+      }
+
       const response: any = await resendEmail({
         uuid: user.PK,
-        type: "verify-email",
+        type: emailType,
         body: user,
       });
       response.data
         ? toast.info(response.data?.message)
         : toast.error(response.error?.data.message);
-    } else {
-      toast.error("Invalid Data. Please ask the administrator about that.");
-    }
-  };
-  const returnMain = () => {
+    },
+    [user, resendEmail]
+  );
+
+  const resendVerificationEmail = useCallback(
+    () => sendEmail("verify-email"),
+    [sendEmail]
+  );
+
+  const sendEmailNoticeToAPTeam = useCallback(
+    () => sendEmail("help-verify-email"),
+    [sendEmail]
+  );
+
+  const returnMain = useCallback(() => {
     dispatch(removeUserData());
     history.push("/");
-  };
+  }, [dispatch, history]);
 
-  if (!user.PK) {
-    user = JSON.parse(localStorage.getItem("userData") || "{}");
-    dispatch(updateUserData(user));
-  }
+  useEffect(() => {
+    if (user.PK) {
+      return;
+    }
+    const newUserData = JSON.parse(localStorage.getItem("userData") || "{}");
+    dispatch(updateUserData(newUserData));
+  }, [user, dispatch]);
 
   return (
-    <div>
+    <div className="flex flex-col gap-4 font-bold">
       {is_sent && (
-        <div className="rounded-xl mb-5">
-          <img src={banner2} width="100%" className="rounded-xl" alt="" />
-        </div>
+        <img src={banner2} width="100%" className="rounded-xl" alt="" />
       )}
       {is_sent ? (
-        <div>
-          <p className="text-4xl font-bold">Hi {user.FirstName}!</p>
-          <span className="text-4xl font-bold">
+        <div className="text-4xl">
+          <p>Hi {user.FirstName}!</p>
+          <span>
             We're still waiting for you to confirm your email address.
           </span>
         </div>
       ) : (
-        <div>
-          <p className="text-2xl font-bold">Thank you for registering</p>
-          <p className="text-2xl font-bold mb-10">
-            {user.FirstName}, {user.CharityName}!{" "}
+        <div className="text-2xl">
+          <p>Thank you for registering</p>
+          <p className="mb-10">
+            {user.FirstName}, {user.CharityName}!
           </p>
-          <p className="text-2xl font-bold">Your registration reference is</p>
-          <p className="text-orange text-2xl font-bold">{user.PK || ""}</p>
+          <p>Your registration reference is</p>
+          <p className="text-orange">{user.PK}</p>
         </div>
       )}
-      <div className="mt-3 mb-10">
-        <span className="text-base">
-          Please click on the link in the email and you'll be able to continue
-          with the registration of {user.CharityName} on Angel.
-        </span>
-      </div>
-      <div className="mb-2">
+      <span className="font-normal">
+        Please click on the link in the email and you'll be able to continue
+        with the registration of {user.CharityName} on Angel.
+      </span>
+      <div className="flex flex-col gap-1 items-center mt-3">
         <Action
           onClick={resendVerificationEmail}
           classes="bg-orange w-64 h-12 text-sm"
           title="Resend verification email"
           isLoading={isLoading}
         />
-      </div>
-      <div className="mb-2">
         <Action
-          onClick={resendVerificationEmail}
+          onClick={sendEmailNoticeToAPTeam}
           title="I'm having trouble with my email"
-          classes="bg-yellow-blue w-96 h-12 text-sm"
+          classes="bg-yellow-blue w-80 h-12 text-sm"
         />
-      </div>
-      <div className="mb-2">
         <Action
           onClick={returnMain}
           title="close"
