0ff89f323e256b346b6bc9791e06f5d9fd84c802
Nenad Misic <n.misic@outlook.com>
2022-01-31 10:54:15 +0100

    Verified email refactor (#602)
    * Implement useMemo for calculated values
    
    * Fix capitalization
    
    * Move updating user data to useEffect
    
    * Move resendVerificationEmail to useCallback
    
    * Extract conditional page contents to separate components
    
    * Move VerifiedEmail & subcomponents to separate folder
    
    * Remove redundant selector styles
    
    * Use 'styles' prop to style react-select on disabled
    
    * Refactor VerifiedEmail
    
    * Refactor LinkExpired
    
    * Move ToastContainer to VerifiedEmail
    
    * Refactor Action
    
    * Add type to responseData var & rename to userData
    
    * Refactor VerificationSuccessful
    
    * Turn span into p in LinkExpired

---
+++ src/pages/registration/VerifiedEmail/VerificationSuccessful.tsx
@@ -0,0 +1,38 @@
+import Action from "components/ActionButton/Action";
+import { MouseEventHandler } from "react";
+import { FaCheck } from "react-icons/fa";
+import { User } from "services/user/types";
+
+type Props = {
+  userData: User;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  isLoading: boolean;
+};
+
+export default function VerificationSuccessful(props: Props) {
+  const { userData, onClick, isLoading } = props;
+
+  return (
+    <div className="flex flex-col gap-10 items-center">
+      <FaCheck className="text-4xl text-yellow-blue" />
+      <div className="text-2xl font-bold">
+        <p>Thank you for registering.</p>
+        <p>
+          {userData.CharityName}, {userData.FirstName}!
+        </p>
+      </div>
+      <div className="text-2xl font-bold">
+        <p>Your registration reference is</p>
+        <p className="text-yellow-600">{userData.PK}</p>
+      </div>
+      <p>We have sent it to your email address for your future reference.</p>
+      <Action
+        classes="bg-thin-blue w-48 h-12"
+        onClick={onClick}
+        title="Continue"
+        disabled={isLoading}
+        isLoading={isLoading}
+      />
+    </div>
+  );
+}
---
+++ src/pages/registration/VerifiedEmail/VerifiedEmail.tsx
@@ -0,0 +1,91 @@
+import jwtDecode from "jwt-decode";
+import { useCallback, useEffect, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useRequestEmailMutation } from "services/aws/registration";
+import { User } from "services/user/types";
+import { updateUserData } from "services/user/userSlice";
+import { useSetter } from "store/accessors";
+import { app, registration, site } from "types/routes";
+import LinkExpired from "./LinkExpired";
+import VerificationSuccessful from "./VerificationSuccessful";
+
+export default function VerifiedEmail() {
+  const history = useHistory();
+  const dispatch = useSetter();
+  const [resendEmail, { isLoading }] = useRequestEmailMutation();
+  const pathNames = history.location.pathname.split("/");
+  const jwtData: any = useMemo(
+    () => jwtDecode(pathNames[pathNames.length - 1]),
+    [pathNames]
+  );
+  const is_expired = useMemo(
+    () => Math.floor(Date.now() / 1000) >= jwtData.exp,
+    [jwtData]
+  );
+  const userData = useMemo(
+    () => createUserData(jwtData, pathNames),
+    [jwtData, pathNames]
+  );
+
+  useEffect(() => {
+    if (!is_expired) {
+      dispatch(updateUserData(userData));
+      localStorage.setItem("userData", JSON.stringify(userData));
+    }
+  }, [is_expired, dispatch, userData]);
+
+  const resendVerificationEmail = useCallback(async () => {
+    if (!userData.PK) {
+      toast.error("Invalid Data. Please ask the administrator about that.");
+      return;
+    }
+
+    const response: any = await resendEmail({
+      uuid: userData.PK,
+      type: "verify-email",
+      body: userData,
+    });
+    response.data
+      ? toast.info(response.data?.message)
+      : toast.error(response.error?.data.message);
+  }, [userData, resendEmail]);
+
+  const navigateToRegistrationStatusPage = useCallback(
+    () => history.push(`${site.app}/${app.register}/${registration.status}`),
+    [history]
+  );
+
+  if (is_expired) {
+    return (
+      <LinkExpired onClick={resendVerificationEmail} isLoading={isLoading} />
+    );
+  }
+
+  return (
+    <VerificationSuccessful
+      userData={userData}
+      onClick={navigateToRegistrationStatusPage}
+      isLoading={isLoading}
+    />
+  );
+}
+
+function createUserData(jwtData: any, pathNames: string[]): User {
+  return {
+    ...jwtData.ContactPerson,
+    CharityName: jwtData.Registration.CharityName,
+    CharityName_ContactEmail: jwtData.Registration.CharityName_ContactEmail,
+    RegistrationDate: jwtData.Registration.RegistrationDate,
+    RegistrationStatus: jwtData.Registration.RegistrationStatus,
+    userType: jwtData.user,
+    authorization: jwtData.authorization,
+    token: pathNames[pathNames.length - 1],
+    ProofOfIdentity: jwtData.Registration.ProofOfIdentity,
+    ProofOfEmployment: jwtData.Registration.ProofOfEmployment,
+    EndowmentAgreement: jwtData.Registration.EndowmentAgreement,
+    ProofOfIdentityVerified: jwtData.Registration.ProofOfIdentityVerified,
+    ProofOfEmploymentVerified: jwtData.Registration.ProofOfEmploymentVerified,
+    EndowmentAgreementVerified: jwtData.Registration.EndowmentAgreementVerified,
+  };
+}
