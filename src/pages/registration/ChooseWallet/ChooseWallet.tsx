b64ed913832cdaa08ec402711a8ce4a170a87714
Nenad Misic <n.misic@outlook.com>
2022-02-02 13:55:52 +0100

    Add underline on hover to connect a terra station wallet

--- src/pages/registration/ChooseWallet/ChooseWallet.tsx
+++ src/pages/registration/ChooseWallet/ChooseWallet.tsx
@@ -6,7 +6,10 @@ export default function ChooseWallet() {
     <div className="flex flex-col gap-5 items-center">
       <h2 className="text-3xl font-semibold">Register your wallet</h2>
       <Torus />
-      <Link to="" className="uppercase text-bright-blue text-sm">
+      <Link
+        to=""
+        className="uppercase text-bright-blue text-sm hover:underline"
+      >
         Connect a terra station wallet
       </Link>
     </div>
