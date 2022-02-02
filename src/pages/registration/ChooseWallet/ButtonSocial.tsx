c5a22c773f66ca583a9a37bd99d2fcf9cf843033
Nenad Misic <n.misic@outlook.com>
2022-02-02 09:58:14 +0100

    Add Facebook, Twitch, Discord

--- src/pages/registration/ChooseWallet/ButtonSocial.tsx
+++ src/pages/registration/ChooseWallet/ButtonSocial.tsx
@@ -1,20 +1,23 @@
 import { ButtonHTMLAttributes } from "react";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-  icon?: string;
+  icon?: string | JSX.Element;
   text?: string;
   alt?: string;
 };
 
 export default function ButtonSocial(props: Props) {
   const { icon, text, alt = "", className, ...buttonProps } = props;
-
   return (
     <button
-      className={`flex gap-3 h-12 justify-center items-center bg-white text-dark-grey rounded-sm ${className}`}
+      className={`flex gap-3 h-12 w-full justify-center items-center bg-white text-dark-grey rounded-sm ${className}`}
       {...buttonProps}
     >
-      <img src={icon} alt={alt} height={30} width={30} />
+      {typeof icon === "string" ? (
+        <img src={icon} alt={alt} height={30} width={30} />
+      ) : (
+        icon
+      )}
       {text}
     </button>
   );
--- src/pages/registration/ChooseWallet/Torus.tsx
+++ src/pages/registration/ChooseWallet/Torus.tsx
@@ -1,15 +1,34 @@
 import gmailIcon from "assets/images/gmail.png";
+import { BsDiscord } from "react-icons/bs";
+import { FaFacebook, FaTwitch } from "react-icons/fa";
 import ButtonSocial from "./ButtonSocial";
 
 export default function Torus() {
   return (
-    <div className="flex flex-col justify-between bg-green-500 h-96 w-96 p-3">
+    <div className="flex flex-col justify-between bg-green-500 h-96 w-96 p-4">
       <ButtonSocial
         icon={gmailIcon}
         text="Continue with Google"
         alt="Google"
         onClick={() => console.log("connect with Google")}
       />
+      <div className="flex justify-between gap-4">
+        <ButtonSocial
+          icon={<FaFacebook className="text-angel-blue" size={30} />}
+          alt="Facebook"
+          onClick={() => console.log("connect with Facebook")}
+        />
+        <ButtonSocial
+          icon={<FaTwitch className="text-purple-500" size={30} />}
+          alt="Twitch"
+          onClick={() => console.log("connect with Twitch")}
+        />
+        <ButtonSocial
+          icon={<BsDiscord className="text-indigo-400" size={30} />}
+          alt="Discord"
+          onClick={() => console.log("connect with Discord")}
+        />
+      </div>
     </div>
   );
 }
