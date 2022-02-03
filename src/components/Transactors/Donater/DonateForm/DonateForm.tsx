8e84ba7e80e8f0d92ed5467d7e563fcdef645aae
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2022-02-03 14:59:55 +0100

    feat: toggle split component (#629)
    * feat: toggle split component
    
    * hide option button for tca
    
    * add notes and change options button content with state changes
    
    * minor UI changes

--- src/components/Transactors/Donater/DonateForm/DonateForm.tsx
+++ src/components/Transactors/Donater/DonateForm/DonateForm.tsx
@@ -8,16 +8,23 @@ import Amount from "./Amount";
 import useDonateForm from "./useDonateForm";
 import Breakdown from "./Breakdown";
 import Split from "./Split";
+import { useState } from "react";
 
 export default function DonateForm() {
   const { form_loading, form_error } = useGetter((state) => state.transaction);
   const { watch } = useFormContext<Values>();
   const { submitHandler, isSubmitting } = useDonateForm();
+  const [showSplit, setShowSplit] = useState(false);
   const to = watch("to");
+
+  const toggleAdvancedOptions = () => setShowSplit(!showSplit);
 
   return (
     <form
-      onSubmit={submitHandler}
+      onSubmit={() => {
+        console.log("submit");
+        submitHandler();
+      }}
       className="bg-white grid p-4 rounded-md w-full max-w-lg"
       autoComplete="off"
     >
@@ -31,14 +38,32 @@ export default function DonateForm() {
         <Currency currency={denoms.uatom} withTooltip />*/}
       </div>
       <Breakdown />
-      {to !== "tca" && <Split />}
-      <button
-        disabled={isSubmitting || form_loading || !!form_error}
-        className="bg-angel-orange disabled:bg-grey-accent p-1 rounded-md mt-2 uppercase text-md text-white font-bold"
-        type="submit"
-      >
-        {form_loading ? "estimating fee.." : "proceed"}
-      </button>
+      {to !== "tca" && showSplit && (
+        <>
+          <Split />
+          <span className="inline-block text-grey-accent font-normal text-sm mt-1 mb-2 pl-2">
+            Note: Donations into the endowment provide sustainable financial
+            runaway and allow your gift to give forever
+          </span>
+        </>
+      )}
+      <div className="flex flex-row gap-2">
+        {to !== "tca" && (
+          <span
+            onClick={toggleAdvancedOptions}
+            className="w-full bg-transparent p-1 rounded-md mt-2 capitalize text-md text-grey-accent font-semibold hover:text-angel-grey cursor-pointer"
+          >
+            {showSplit ? "Hide options" : "Advanced Options"}
+          </span>
+        )}
+        <button
+          disabled={isSubmitting || form_loading || !!form_error}
+          className="w-full bg-angel-orange disabled:bg-grey-accent p-1 rounded-md mt-2 uppercase text-md text-white font-bold"
+          type="submit"
+        >
+          {form_loading ? "estimating fee.." : "proceed"}
+        </button>
+      </div>
     </form>
   );
 }
