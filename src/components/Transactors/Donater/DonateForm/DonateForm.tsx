967dc3358fbf481e28e64bd5fc7babdcaaefe506
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-02-03 10:11:09 +0100

    feat: toggle split component

--- src/components/Transactors/Donater/DonateForm/DonateForm.tsx
+++ src/components/Transactors/Donater/DonateForm/DonateForm.tsx
@@ -8,16 +8,23 @@ import Amount from "./Amount";
 import useDonateForm from "./useDonateForm";
 import Breakdown from "./Breakdown";
 import Split from "./Split";
+import { useState } from "react";
 
 export default function DonateForm() {
   const { form_loading, form_error } = useGetter((state) => state.transaction);
   const { watch } = useFormContext<Values>();
   const { submitHandler, isSubmitting } = useDonateForm();
   const to = watch("to");
+  const [showSplit, setShowSplit] = useState(false);
+
+  const toggleAdvancedOptions = () => setShowSplit(!showSplit);
 
   return (
     <form
-      onSubmit={submitHandler}
+      onSubmit={() => {
+        console.log("submit");
+        submitHandler();
+      }}
       className="bg-white grid p-4 rounded-md w-full max-w-lg"
       autoComplete="off"
     >
@@ -31,14 +38,22 @@ export default function DonateForm() {
         <Currency currency={denoms.uatom} withTooltip />*/}
       </div>
       <Breakdown />
-      {to !== "tca" && <Split />}
-      <button
-        disabled={isSubmitting || form_loading || !!form_error}
-        className="bg-angel-orange disabled:bg-grey-accent p-1 rounded-md mt-2 uppercase text-md text-white font-bold"
-        type="submit"
-      >
-        {form_loading ? "estimating fee.." : "proceed"}
-      </button>
+      {to !== "tca" && showSplit && <Split />}
+      <div className="flex flex-row gap-2">
+        <span
+          onClick={toggleAdvancedOptions}
+          className="w-full bg-transparent p-1 rounded-md mt-2 capitalize text-md text-grey-accent font-semibold hover:text-angel-grey cursor-pointer"
+        >
+          Advanced Options
+        </span>
+        <button
+          disabled={isSubmitting || form_loading || !!form_error}
+          className="w-full bg-angel-orange disabled:bg-grey-accent p-1 rounded-md mt-2 uppercase text-md text-white font-bold"
+          type="submit"
+        >
+          {form_loading ? "estimating fee.." : "proceed"}
+        </button>
+      </div>
     </form>
   );
 }
