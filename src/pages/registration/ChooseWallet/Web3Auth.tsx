32f364072e5136bf39177988491eba2f0eff169a
Nenad Misic <n.misic@outlook.com>
2022-02-04 09:28:36 +0100

    Increase Web3Auth roundness

--- src/pages/registration/ChooseWallet/Web3Auth.tsx
+++ src/pages/registration/ChooseWallet/Web3Auth.tsx
@@ -13,7 +13,7 @@ export default function Web3Auth() {
   );
 
   return (
-    <div className="flex flex-col justify-between bg-angel-blue h-96 w-96 p-4 rounded-xs">
+    <div className="flex flex-col justify-between bg-angel-blue h-96 w-96 p-4 rounded-sm">
       <ButtonSocial onClick={() => console.log("connect with Google")}>
         <img src={gmailIcon} alt="Google" height={30} width={30} />
         <span className="text-gray-500">Continue with Google</span>
