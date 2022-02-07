6570aac14a0286808884d6452d5c3cd759b46690
Nenad Misic <n.misic@outlook.com>
2022-02-07 14:08:47 +0100

    Refactor Register and remove automatic redirect to Confirm

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,22 +1,18 @@
-import { useHistory, useRouteMatch } from "react-router-dom";
-import { registration } from "types/routes";
-import banner1 from "assets/images/banner-register-1.jpg";
-import { ToastContainer } from "react-toastify";
-import Action from "../../components/ActionButton/Action";
-import { useSetter } from "store/accessors";
-import { removeUserData } from "services/user/userSlice";
 import { yupResolver } from "@hookform/resolvers/yup";
+import banner1 from "assets/images/banner-register-1.jpg";
+import { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
-import { ReferInfo, FormInfoSchema, useRegistration } from "./useRegistration";
-import { useState } from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { registration } from "types/routes";
+import Action from "../../components/ActionButton/Action";
+import { FormInfoSchema, ReferInfo, useRegistration } from "./useRegistration";
 
 const Registration = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const dispatch = useSetter();
   const { onResume } = useRegistration();
   const { url } = useRouteMatch();
   const history = useHistory();
-  const userData: any = JSON.parse(localStorage.getItem("userData") || "{}");
   const {
     register,
     handleSubmit,
@@ -25,72 +21,56 @@ const Registration = () => {
     resolver: yupResolver(FormInfoSchema),
   });
 
-  const onResumeRefer = async (values: ReferInfo) => {
-    setIsLoading(true);
-    await onResume(values);
-    setIsLoading(false);
-  };
+  const onResumeRefer = useCallback(
+    async (values: ReferInfo) => {
+      setIsLoading(true);
+      await onResume(values);
+      setIsLoading(false);
+    },
+    [onResume]
+  );
 
-  if (userData && userData.EmailVerified === false) {
-    history.push({
-      pathname: `${url}/${registration.confirm}`,
-      state: { is_sent: true },
-    });
-  } else {
-    localStorage.setItem("userData", JSON.stringify({}));
-    dispatch(removeUserData());
-  }
+  const handleStart = useCallback(
+    () => history.push(`${url}/${registration.detail}`),
+    [history, url]
+  );
 
   return (
-    <div>
-      <div className="rounded-xl mb-5">
-        <img src={banner1} width="100%" className="rounded-xl" alt="banner" />
-      </div>
-      <div>
-        <span className="text-4xl font-bold">
-          Thank you for registering, we'd love to have you on board!
-        </span>
-      </div>
-      <div className="mb-10 mt-2">
-        <span className="text-xl">
-          You're just steps away from bringing all the benefits of endowments to
-          your organization.
-        </span>
-      </div>
-      <div className="mb-2">
-        <Action
-          onClick={() => history.push(`${url}/${registration.detail}`)}
-          title="Start"
-          classes="bg-orange w-48 h-12"
+    <div className="flex flex-col gap-3 items-center">
+      <img src={banner1} width="100%" className="rounded-xl" alt="banner" />
+      <span className="text-4xl font-bold">
+        Thank you for registering, we'd love to have you on board!
+      </span>
+      <span className="text-xl mb-5">
+        You're just steps away from bringing all the benefits of endowments to
+        your organization.
+      </span>
+      <Action
+        onClick={handleStart}
+        title="Start"
+        classes="bg-orange w-48 h-12"
+      />
+      <p className="text-xl font-bold text-thin-blue">OR</p>
+      <form
+        onSubmit={handleSubmit(onResumeRefer)}
+        className="flex flex-col items-center gap-2 w-full"
+      >
+        <input
+          {...register("refer")}
+          className="rounded-md outline-none border-none w-3/5 md:w-2/5 px-3 py-2 text-black"
+          placeholder="Enter your registration reference"
+          type="text"
         />
-        <div className="cursor-pointer mb-3">
-          <p className="text-xl font-bold text-thin-blue">OR</p>
-        </div>
-      </div>
-      <div className="">
-        <form onSubmit={handleSubmit(onResumeRefer)}>
-          <div className="flex items-center justify-center mb-2">
-            <div className="rounded-md bg-white flex items-center w-3/5 md:w-2/5 text-black py-2">
-              <input
-                {...register("refer")}
-                className="outline-none border-none w-full px-3"
-                placeholder="Enter your registration reference"
-                type="text"
-                required
-              />
-            </div>
-          </div>
-          <p className="text-base text-failed-red">{errors.refer?.message}</p>
-          <Action
-            submit
-            title="Resume"
-            classes="bg-thin-blue w-48 h-12 mt-3"
-            disabled={isLoading}
-            isLoading={isLoading}
-          />
-        </form>
-      </div>
-      <p className="text-base mt-6">
+        <p className="text-failed-red">{errors.refer?.message}</p>
+        <Action
+          submit
+          title="Resume"
+          classes="bg-thin-blue w-48 h-12"
+          disabled={isLoading}
+          isLoading={isLoading}
+        />
+      </form>
+      <p className="mt-5">
         Can't find a registration file with this reference?
       </p>
       <ToastContainer />
