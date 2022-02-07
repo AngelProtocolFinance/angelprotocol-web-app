28a41b8452997a7166c16ab0da20d82c721ee140
ap-justin <justin@angelprotocol.io>
2022-02-07 17:09:17 +0800

    re-disabled fund and test page

--- src/App/Views.tsx
+++ src/App/Views.tsx
@@ -9,7 +9,7 @@ import { app, site } from "../types/routes";
 import { lazy, Suspense } from "react";
 import Loader from "components/Loader/Loader";
 import Market from "pages/Market/Market";
-import Test from "pages/Test";
+// import Test from "pages/Test";
 
 const Login = lazy(() => import("pages/Login/Login"));
 const TCA = lazy(() => import("pages/TCA/TCA"));
@@ -21,7 +21,7 @@ const Endowment_Admin = lazy(
   () => import("pages/Endowment_Admin/Endowment_Admin")
 );
 const Charity = lazy(() => import("pages/Charity/Charity"));
-const Fund = lazy(() => import("pages/Fund/Fund"));
+// const Fund = lazy(() => import("pages/Fund/Fund"));
 // const Test = lazy(() => import("pages/Test"));
 // const Register = lazy(() => import("pages/registration/index"));
 
@@ -47,13 +47,13 @@ export default function Views() {
           component={ChurchPortal}
         />
         <Route path={`${path}/${app.govern}`} component={Governance} />
-        <Route path={`${path}/${app.fund}/:id`} component={Fund} />
+        {/* <Route path={`${path}/${app.fund}/:id`} component={Fund} /> */}
         <Route path={`${path}/${app.auction}`} component={Auction} />
         <Route
           path={`${path}/${app.endowment_admin}/:address`}
           component={Endowment_Admin}
         />
-        <Route path={`${path}/${app.test}`} component={Test} />
+        {/* <Route path={`${path}/${app.test}`} component={Test} /> */}
         <Route path={`${path}${app.index}`} component={Market} />
         <Redirect from="*" to={site.home} />
       </Switch>
