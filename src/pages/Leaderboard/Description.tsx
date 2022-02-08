b8fb6bcecf9d4ebece4f78d6296a2ddd99965388
ap-justin <justin@angelprotocol.io>
2022-02-08 18:17:34 +0800

    convert <a/> to Link

--- src/pages/Leaderboard/Description.tsx
+++ src/pages/Leaderboard/Description.tsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 import { useConnectedWallet } from "@terra-money/wallet-provider";
 import { chainIDs } from "constants/chainIDs";
 import { useAccountsQuery } from "services/aws/endowments/endowments";
 import { charity_details } from "services/aws/endowments/placeholders";
-import { app, site } from "types/routes";
+import { app } from "types/routes";
 
 type Props = { address: string };
 
@@ -22,12 +23,12 @@ export default function Description(props: Props) {
         } p-3 rounded-sm object-contain mr-4 m-1`}
       />
 
-      <a
-        href={`${site.app}/${app.charity}/${props.address}`}
+      <Link
+        to={`${app.charity}/${props.address}`}
         className="col-start-2 text-lg text-angel-grey hover:text-angel-blgue active:text-angel-blue font-bold pt-2 mb-1"
       >
         {details.name}
-      </a>
+      </Link>
       <p className="relative pr-16 text-sm text-angel-grey  leading-normal mb-2 line-clamp-3">
         {details.description}
       </p>
