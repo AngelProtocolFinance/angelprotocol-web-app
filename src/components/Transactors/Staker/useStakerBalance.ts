9c4ed7feefe6fb85be2e461d30c2357b3e241587
ap-justin <justin@angelprotocol.io>
2022-02-08 11:41:12 +0800

    fix zero balance

--- src/components/Transactors/Staker/useStakerBalance.ts
+++ src/components/Transactors/Staker/useStakerBalance.ts
@@ -4,12 +4,12 @@ import { useGovStaker, useHaloBalance } from "services/terra/queriers";
 
 export default function useStakerBalance(is_stake: boolean) {
   const gov_staker = useGovStaker();
-  const halo_balance = useHaloBalance();
+  const { haloBalance } = useHaloBalance();
 
   const [balance, locked] = useMemo((): [Dec, Dec] => {
     const staked = new Dec(gov_staker.balance);
     if (is_stake) {
-      return [new Dec(halo_balance).mul(1e6), new Dec(0)];
+      return [new Dec(haloBalance).mul(1e6), new Dec(0)];
     } else {
       //used in voting to a poll
       const vote_locked = gov_staker.locked_balance.reduce(
@@ -18,8 +18,8 @@ export default function useStakerBalance(is_stake: boolean) {
       );
       return [staked, vote_locked];
     }
-    // is_stake ? halo_balance : new Dec(gov_staker.balance).div(1e6).toNumber();
-  }, [gov_staker, halo_balance, is_stake]);
+    // is_stake ? haloBalance : new Dec(gov_staker.balance).div(1e6).toNumber();
+  }, [gov_staker, haloBalance, is_stake]);
 
   return { balance, locked };
 }
