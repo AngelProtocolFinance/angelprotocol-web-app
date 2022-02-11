1727bd6f05bc5878278f8b388883cdba41026918
Nenad Misic <n.misic@outlook.com>
2022-02-11 22:56:27 +0100

    Refactor NavigationToDashboard

--- src/pages/registration/WalletRegistration/RegisterWallet/NavigationToDashboard.tsx
+++ src/pages/registration/WalletRegistration/RegisterWallet/NavigationToDashboard.tsx
@@ -2,35 +2,36 @@ import LinkButton from "components/LinkButton";
 import { Link } from "react-router-dom";
 import { app, registration, site } from "types/routes";
 
-type Props = {
-  isSuccess: boolean;
-  walletAddress: string;
-};
+type Props = { isSuccess: boolean; walletAddress: string };
 
 export default function NavigationToDashboard(props: Props) {
   const { isSuccess, walletAddress } = props;
 
-  return !isSuccess ? (
+  if (isSuccess) {
+    return (
+      <>
+        <div>
+          <p>Thanks for registering your wallet:</p>
+          <p>your address is</p>
+          <p className="font-bold">{walletAddress}</p>
+        </div>
+        <LinkButton
+          to={`${site.app}/${app.register}/${registration.status}`}
+          className="w-60 h-10 mt-8"
+          bgColorClass="bg-angel-blue"
+        >
+          Back to dashboard
+        </LinkButton>
+      </>
+    );
+  }
+
+  return (
     <Link
       to={`${site.app}/${app.register}/${registration.status}`}
       className="uppercase text-bright-blue text-sm hover:underline"
     >
       Click here to go back to the registration dashboard
     </Link>
-  ) : (
-    <>
-      <div>
-        <p>Thanks for registering your wallet:</p>
-        <p>your address is</p>
-        <p className="font-bold">{walletAddress}</p>
-      </div>
-      <LinkButton
-        to={`${site.app}/${app.register}/${registration.status}`}
-        className="w-60 h-10 mt-8"
-        bgColorClass="bg-angel-blue"
-      >
-        Back to dashboard
-      </LinkButton>
-    </>
   );
 }
