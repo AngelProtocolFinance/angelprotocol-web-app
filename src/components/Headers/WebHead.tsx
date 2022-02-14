c5a84d4922e4d7c72320d17eaf4d893be1d7e347
Andrey <andrey@angelprotocol.io>
2022-02-14 10:26:12 +0800

    mobile nav: extra padding removed & hide longer mini-banner text

--- src/components/Headers/WebHead.tsx
+++ src/components/Headers/WebHead.tsx
@@ -20,7 +20,7 @@ export default function WebHead() {
       ref={shadowRef}
       className="grid fixed bg-white w-full h-24 z-10 transition-shadow "
     >
-      <nav className="w-full grid grid-cols-a1a items-center justify-items-end md:justify-items-center padded-container">
+      <nav className="w-full grid grid-cols-a1a items-center justify-items-end md:justify-items-center md:padded-container">
         <Logo />
         <WebMenu />
         <button
@@ -36,9 +36,9 @@ export default function WebHead() {
           rel="noopener noreferrer"
           className="absolute -bottom-6 font-heading cursor-pointer hover:bg-black bg-angel-grey w-full p-2 flex items-center justify-center text-white text-xs"
         >
-          <div className="flex items-center mr-2 md:uppercase">
+          <span className="hidden md:block flex w-fit items-center mr-2 md:uppercase">
             New Charity Marketplace is now live!
-          </div>
+          </span>
           <Word icon={heart_icon} title="Connect" />
           <Word icon={star_icon} title="Select" />
           <Word icon={earth_icon} title="Donate" />
