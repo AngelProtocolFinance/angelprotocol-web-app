fbfd0e019b34dbc001f62af6ed33397dbea9305f
Nenad Misic <n.misic@outlook.com>
2022-02-15 09:36:14 +0100

    Update useTypeWriter var names and add explanatory comments

--- src/hooks/useTypeWriter.ts
+++ src/hooks/useTypeWriter.ts
@@ -10,46 +10,50 @@ type TypeWriterResult = {
  *
  * @param strings Strings to be returned with a typewriter effect in order in which they were passed in
  * @param durationBetweenTypes How fast the typewriter types
- * @param showTime Idle time to show typed out string
+ * @param idleTime Idle time to show typed out string
+ *
  * @returns Result containing currently typed out text with a flag indicating whether Cursor should be shown
  */
 export default function useTypeWriter(
   strings: string[],
   durationBetweenTypes: number,
-  showTime: number
+  idleTime: number
 ): TypeWriterResult {
   const [index, setIndex] = useState(0);
-  const [subIndex, setSubIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
   const [isCursorShown, setCursorShown] = useState(true);
-  const [reverse, setReverse] = useState(false);
+  const [inReverse, setReverse] = useState(false);
 
-  // typeWriter
   useEffect(() => {
-    const shouldReverse = subIndex === strings[index].length && !reverse;
+    const shouldReverse = charIndex === strings[index].length && !inReverse;
+    const delay = shouldReverse ? idleTime : durationBetweenTypes;
 
-    const waitTime = shouldReverse ? showTime : durationBetweenTypes;
-
-    if (subIndex === 0 && reverse) {
+    // if we were going in reverse and are at the beginning of the string,
+    // start printing the next string (or first one if we finished printing the last string)
+    // and print it in the non-reverse direction
+    if (charIndex === 0 && inReverse) {
       setReverse(false);
       setIndex((prev) => (prev + 1) % strings.length);
       return;
     }
 
+    // after the set delay, reverse direction if necessary,
+    // otherwise move the char index to the next char and toggle cursor visibility
     const timeout = setTimeout(() => {
       if (shouldReverse) {
         setReverse(true);
       } else {
-        setSubIndex((prev) => prev + (reverse ? -1 : 1));
+        setCharIndex((prev) => prev + (inReverse ? -1 : 1));
         setCursorShown((prev) => !prev);
       }
-    }, waitTime);
+    }, delay);
 
     return () => clearTimeout(timeout);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [subIndex, index, reverse]);
+  }, [charIndex, index, inReverse]);
 
   return {
     isCursorShown,
-    typedText: strings[index].substring(0, subIndex),
+    typedText: strings[index].substring(0, charIndex),
   };
 }
