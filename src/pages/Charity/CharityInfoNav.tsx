538d653e8a862110be6e4bb2a542d5cadd4144f7
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-02-16 20:35:47 +0100

    refactor: charityInfoNav page, make nav responsive and scrollable

--- src/pages/Charity/CharityInfoNav.tsx
+++ src/pages/Charity/CharityInfoNav.tsx
@@ -1,5 +1,5 @@
 import { NavLink, useHistory, useParams } from "react-router-dom";
-import { app, site } from "types/routes";
+import { charityNav } from "./constants";
 
 type CharityParams = {
   address: string;
@@ -12,21 +12,21 @@ export default function CharityInfoNav() {
   } = useHistory();
 
   const getClassNames = (isActive: boolean, isDisabled: boolean = false) => {
-    const classes = `block w-full ${
+    const classes = `flex justify-center w-full ${
       isDisabled
         ? "text-white-grey bg-grey-accent"
         : isActive
         ? "bg-angel-blue text-white"
         : "text-dark-grey text-white text-opacity-80 hover:bg-angel-blue hover:text-white"
-    }  font-semibold bg-white uppercase border-0 py-3 px-4`;
+    }  font-semibold bg-white uppercase border-0 py-3 px-6`;
     return classes;
   };
 
   return (
-    <nav className="overflow-y-hidden overflow-x-scroll scroll-hidden grid items-start justify-stretch lg:padded-container my-5 lg:mb-0 md:pl-0">
-      <ul className="flex font-body text-sm lg:text-base ml-1 block w-full">
+    <nav className="max-w-full overflow-hidden scroll-hidden grid items-start justify-stretch lg:padded-container my-5 lg:mb-0 md:pl-0">
+      <ul className="flex font-body text-sm lg:text-base ml-1 block w-full overflow-hidden overflow-x-auto scroll-hidden">
         {charityNav.map((navItem, i) => (
-          <li className="mr-1 flex block w-full" key={i}>
+          <li className="flex block min-w-200" key={i}>
             {/**just use buttons since page switching is programmatic and no involved page semantics*/}
             {navItem.disabled ? (
               <button className={getClassNames(false, true)}>
@@ -51,49 +51,3 @@ export default function CharityInfoNav() {
     </nav>
   );
 }
-
-type CharityNavProps = {
-  title: string;
-  disabled: boolean;
-  getLink: (address: string) => string;
-  isDefault?: boolean;
-  defaultPath: (address: string) => string;
-};
-
-const charityNav: CharityNavProps[] = [
-  {
-    title: "overview",
-    disabled: false,
-    getLink: (address: string) =>
-      `${site.app}/${app.charity}/${address}/overview`,
-    isDefault: true,
-    defaultPath: (address: string) => `${site.app}/${app.charity}/${address}`,
-  },
-  {
-    title: "endowment",
-    disabled: false,
-    getLink: (address: string) =>
-      `${site.app}/${app.charity}/${address}/endowment`,
-    defaultPath: (address: string) => `${site.app}/${app.charity}/${address}`,
-  },
-  {
-    title: "programs",
-    disabled: true,
-    getLink: (address: string) =>
-      `${site.app}/${app.charity}/${address}/programs`,
-    defaultPath: (address: string) => `${site.app}/${app.charity}/${address}`,
-  },
-  {
-    title: "media",
-    disabled: true,
-    getLink: (address: string) => `${site.app}/${app.charity}/${address}/media`,
-    defaultPath: (address: string) => `${site.app}/${app.charity}/${address}`,
-  },
-  {
-    title: "governance",
-    disabled: true,
-    getLink: (address: string) =>
-      `${site.app}/${app.charity}/${address}/governance`,
-    defaultPath: (address: string) => `${site.app}/${app.charity}/${address}`,
-  },
-];
