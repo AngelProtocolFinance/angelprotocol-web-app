2e34668e25f0de521dce63d8bc0813e4b231d972
Nenad Misic <n.misic@outlook.com>
2022-02-16 18:55:38 +0100

    Update registration/index.ts

--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -7,42 +7,56 @@ const ContactDetails = lazy(() => import("./ContactDetails"));
 const ConfirmEmail = lazy(() => import("./ConfirmEmail"));
 const VerifiedEmail = lazy(() => import("./VerifiedEmail"));
 const RegistrationStatus = lazy(() => import("./RegistrationStatus"));
+const ChooseWallet = lazy(() => import("./WalletRegistration/ChooseWallet"));
+const RedirectAuth = lazy(() => import("./WalletRegistration/RedirectAuth"));
+const RegisterWallet = lazy(
+  () => import("./WalletRegistration/RegisterWallet")
+);
 
 export default function Register() {
   //this component will only render under '/app/register/'
   const { path } = useRouteMatch();
 
   return (
-    <section>
-      <div className="relative sm:w-4/5 max-w-6xl p-10 mt-5 text-center text-white mx-auto">
-        <Switch>
-          <Route
-            exact
-            path={`${path}/${routes.detail}`}
-            component={ContactDetails}
-          />
-          <Route
-            exact
-            path={`${path}/${routes.confirm}`}
-            component={ConfirmEmail}
-          />
-          <Route
-            exact
-            path={`${path}/${routes.verify}`}
-            component={VerifiedEmail}
-          />
-          <Route
-            exact
-            path={`${path}/${routes.status}`}
-            component={RegistrationStatus}
-          />
-          <Route
-            exact
-            path={`${path}${routes.index}`}
-            component={Registration}
-          />
-        </Switch>
-      </div>
+    <section className="flex items-center justify-center relative sm:w-4/5 max-w-6xl text-center text-white mx-auto h-full">
+      <Switch>
+        <Route
+          exact
+          path={`${path}/${routes.detail}`}
+          component={ContactDetails}
+        />
+        <Route
+          exact
+          path={`${path}/${routes.confirm}`}
+          component={ConfirmEmail}
+        />
+        <Route
+          exact
+          path={`${path}/${routes.verify}`}
+          component={VerifiedEmail}
+        />
+        <Route
+          exact
+          path={`${path}/${routes.status}`}
+          component={RegistrationStatus}
+        />
+        <Route
+          exact
+          path={`${path}/${routes.wallet_check}`}
+          component={ChooseWallet}
+        />
+        <Route
+          exact
+          path={`${path}/${routes.wallet_check}/${routes.auth}`}
+          component={RedirectAuth}
+        />
+        <Route
+          exact
+          path={`${path}/${routes.register_wallet}`}
+          component={RegisterWallet}
+        />
+        <Route exact path={`${path}${routes.index}`} component={Registration} />
+      </Switch>
     </section>
   );
 }
--- src/pages/registration/routes.ts
+++ src/pages/registration/routes.ts
@@ -1,18 +1,14 @@
 enum routes {
   index = "/",
   detail = "detail",
+  status = "status",
   confirm = "confirm",
   verify = "verify/:token",
-  status = "status",
   wallet_check = "wallet-check",
-  connect_wallet = "connect-wallet",
   register_wallet = "register-wallet",
-  select_wallet = "select-wallet",
-  upload_docs = "upload-docs",
   charity_profile = "charity-profile",
-  others = "others",
-  self_custody = "self-custody",
-  key_person = "key-person",
+  auth = "auth",
+  upload_docs = "upload-docs",
 }
 
 export default routes;
