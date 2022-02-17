75fa4b5200d70fd4597a8427931d708bb535f39e
Nenad Misic <n.misic@outlook.com>
2022-02-17 10:18:57 +0100

    Modify title and link text on ChooseWallet

--- src/pages/registration/WalletRegistration/ChooseWallet/ChooseWallet.tsx
+++ src/pages/registration/WalletRegistration/ChooseWallet/ChooseWallet.tsx
@@ -12,13 +12,16 @@ export default function ChooseWallet() {
 
   return (
     <div className="flex flex-col gap-5 items-center">
-      <h2 className="text-3xl font-semibold">Register your wallet</h2>
+      <h2 className="lg:text-2xl font-semibold">
+        Please choose one of the options below to register your wallet
+      </h2>
       <Web3Auth onLogin={login} />
       <Link
         to=""
-        className="uppercase text-bright-blue text-sm hover:underline"
+        className="uppercase text-bright-blue text-sm hover:underline mb-5 lg:mb-0"
       >
-        Connect a terra station wallet
+        Or click here if you already have a Terra wallet that you would like to
+        use
       </Link>
     </div>
   );
