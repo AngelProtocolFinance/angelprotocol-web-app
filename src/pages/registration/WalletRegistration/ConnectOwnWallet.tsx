bc7531f4e89a2ba9e186276bc58af6f8afd81a81
Nenad Misic <n.misic@outlook.com>
2022-02-17 13:01:05 +0100

    Open WalletSuite on loading ConnectOwnWallet

--- src/pages/registration/WalletRegistration/ConnectOwnWallet.tsx
+++ src/pages/registration/WalletRegistration/ConnectOwnWallet.tsx
@@ -1,4 +1,11 @@
+import { WalletSuiteContext } from "providers/WalletSuiteProvider";
+import { useContext, useEffect } from "react";
+
 export default function ConnectOwnWallet() {
+  const { connectorsShown, setShowConnectors } = useContext(WalletSuiteContext);
+
+  useEffect(() => setShowConnectors(true), []);
+
   return (
     <h2 className="md:text-xl font-bold">Please connect your own wallet</h2>
   );
