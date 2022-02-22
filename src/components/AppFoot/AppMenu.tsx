28a84b0379b07006b68ea0cb298b28784eaa2de1
Nenad Misic <n.misic@outlook.com>
2022-02-22 13:21:31 +0100

    Refactor AppMenu and fix REGISTER link

--- src/components/AppFoot/AppMenu.tsx
+++ src/components/AppFoot/AppMenu.tsx
@@ -1,34 +1,55 @@
-import { PropsWithChildren } from "react";
+import { app, site } from "constants/routes";
+import { Link } from "react-router-dom";
 
 export default function AppMenu() {
   return (
     <ul className="flex lg:items-center">
-      <AppMenuItem href="https://share-eu1.hsforms.com/14aljI0OEQje2DDmJiZoLFgetp37">
-        SUPPORT
-      </AppMenuItem>
-      <AppMenuItem href="https://share-eu1.hsforms.com/10igJPVeBQMemEpTmUsxSVwetp37">
-        FEEDBACK
-      </AppMenuItem>
-      <AppMenuItem href="https://www.angelprotocol.io/contact">
-        REGISTER
-      </AppMenuItem>
+      <SupportLinkItem />
+      <FeedbackLinkItem />
+      <RegisterLinkItem />
     </ul>
   );
 }
 
-type Props = PropsWithChildren<{ href: string }>;
+const LINK_STYLE =
+  "text-white-grey text-sm hover:text-opacity-75 px-1 lg:text-base font-heading uppercase font-semibold lg:px-2";
 
-function AppMenuItem({ href, children }: Props) {
+function SupportLinkItem() {
   return (
     <li>
       <a
         rel="noreferrer noopener"
         target="_blank"
-        href={href}
-        className="text-white-grey text-sm hover:text-opacity-75 px-1 lg:text-base font-heading uppercase font-semibold lg:px-2"
+        href="https://share-eu1.hsforms.com/14aljI0OEQje2DDmJiZoLFgetp37"
+        className={LINK_STYLE}
       >
-        {children}
+        SUPPORT
       </a>
+    </li>
+  );
+}
+
+function FeedbackLinkItem() {
+  return (
+    <li>
+      <a
+        rel="noreferrer noopener"
+        target="_blank"
+        href="https://share-eu1.hsforms.com/10igJPVeBQMemEpTmUsxSVwetp37"
+        className={LINK_STYLE}
+      >
+        FEEDBACK
+      </a>
+    </li>
+  );
+}
+
+function RegisterLinkItem() {
+  return (
+    <li>
+      <Link to={`${site.app}/${app.register}`} className={LINK_STYLE}>
+        REGISTER
+      </Link>
     </li>
   );
 }
