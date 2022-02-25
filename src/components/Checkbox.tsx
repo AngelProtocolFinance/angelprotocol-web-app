b8aa1113d264b9dc2b6bc3ca3b3103616f4fba26
Nenad Misic <n.misic@outlook.com>
2022-02-25 12:53:51 +0100

    Refactor Checkbox error

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,4 +1,9 @@
-import { forwardRef, InputHTMLAttributes, PropsWithChildren } from "react";
+import {
+  ForwardedRef,
+  forwardRef,
+  InputHTMLAttributes,
+  PropsWithChildren,
+} from "react";
 
 export type CheckboxProps = PropsWithChildren<
   InputHTMLAttributes<HTMLInputElement> & {
@@ -7,33 +12,39 @@ export type CheckboxProps = PropsWithChildren<
   }
 >;
 
-const Checkbox = forwardRef((props: CheckboxProps, ref: any) => {
-  const { error, centerError, children, ...rest } = props;
-  const id = rest.id || rest.name;
+const Checkbox = forwardRef(
+  (props: CheckboxProps, ref: ForwardedRef<HTMLInputElement>) => {
+    const { error, centerError, children, ...rest } = props;
+    const id = rest.id || rest.name;
 
-  return (
-    <div className="flex gap-2 items-center relative">
-      <input
-        id={id}
-        ref={ref}
-        type="checkbox"
-        className="cursor-pointer"
-        {...rest}
-      />
-      <label htmlFor={id} className="cursor-pointer">
-        {children}
-      </label>
-      {error && (
-        <p
-          className={`text-sm text-failed-red absolute -bottom-4 ${
-            centerError ? "left-1/2 transform -translate-x-1/2" : "left-5"
-          }`}
-        >
-          {error}
-        </p>
-      )}
-    </div>
-  );
-});
+    const errorPositionClasses = centerError
+      ? "left-1/2 transform -translate-x-1/2"
+      : "left-5";
+
+    return (
+      <div className="flex gap-2 items-center relative">
+        <input
+          id={id}
+          ref={ref}
+          type="checkbox"
+          className="cursor-pointer"
+          {...rest}
+        />
+        <label htmlFor={id} className="cursor-pointer">
+          {children}
+        </label>
+        <Error message={error} positionClasses={errorPositionClasses} />
+      </div>
+    );
+  }
+);
+
+type ErrorProps = { message?: string; positionClasses: string };
+
+function Error({ message, positionClasses }: ErrorProps) {
+  if (!message) return null;
+  const className = `text-sm text-failed-red absolute -bottom-4 ${positionClasses}`;
+  return <p className={className}>{message}</p>;
+}
 
 export default Checkbox;
