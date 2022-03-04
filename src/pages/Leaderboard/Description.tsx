ff6d1f589a1db1c0565bd4bb24c285a64306be27
Andrey <andrey@angelprotocol.io>
2022-03-04 15:39:59 +0800

    remove description richtext field from leaderboard

--- src/pages/Leaderboard/Description.tsx
+++ src/pages/Leaderboard/Description.tsx
@@ -1,9 +1,6 @@
-import { Link } from "react-router-dom";
 import { Endowment } from "services/aws/leaderboard/types";
 import { charity_details } from "services/aws/endowments/placeholders";
-import { app } from "constants/routes";
 import LazyImage from "components/LazyImage/LazyImage";
-import RichTextRenderer from "components/RichTextRenderer/RichTextRenderer";
 
 export default function Description(props: { endowments: Endowment }) {
   const details = props.endowments || charity_details;
@@ -20,15 +17,6 @@ export default function Description(props: { endowments: Endowment }) {
         height="96"
         rounded
       />
-      <Link
-        to={`${app.charity}/${details.endowment_address}`}
-        className="col-start-2 text-lg text-angel-grey hover:text-angel-blue active:text-angel-blue font-bold pt-2 mb-1"
-      >
-        {details.charity_name}
-      </Link>
-      <div className="relative pr-16 text-sm text-angel-grey  leading-normal mb-2 line-clamp-3">
-        <RichTextRenderer text={details.charity_overview} />
-      </div>
     </div>
   );
 }
--- src/pages/Leaderboard/TableEntry.tsx
+++ src/pages/Leaderboard/TableEntry.tsx
@@ -2,6 +2,8 @@ import { Endowment } from "services/aws/leaderboard/types";
 import Amount from "./Amount";
 import Description from "./Description";
 import projectFunds from "./projectFunds";
+import { Link } from "react-router-dom";
+import { app } from "constants/routes";
 
 export default function TableEntry(props: Endowment) {
   const { locked, liquid } = projectFunds(
@@ -15,6 +17,14 @@ export default function TableEntry(props: Endowment) {
     <tr className="border-b">
       <td>
         <Description endowments={props} />
+      </td>
+      <td>
+        <Link
+          to={`${app.charity}/${props.endowment_address}`}
+          className="col-start-2 text-lg text-angel-grey hover:text-angel-blue active:text-angel-blue font-bold pt-2 mb-1"
+        >
+          {props.charity_name}
+        </Link>
       </td>
       <td>
         <div className="flex flex-col w-40">
