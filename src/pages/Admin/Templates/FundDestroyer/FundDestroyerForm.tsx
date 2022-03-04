94dd257f28c890c26fe393ea19c54dc6e4c0e11d
ap-justin <justin@angelprotocol.io>
2022-03-04 21:15:33 +0800

    fund selection table

--- src/pages/Admin/Templates/FundDestroyer/FundDestroyerForm.tsx
+++ src/pages/Admin/Templates/FundDestroyer/FundDestroyerForm.tsx
@@ -1,10 +1,12 @@
+import Label from "pages/Admin/Label";
 import TextInput from "../../TextInput";
+import FundSelection from "./FundSelection";
 import useCreateFund from "./useDestroyFund";
 
 export default function FundDestroyerForm() {
   const { createFund } = useCreateFund();
   return (
-    <div className="w-full p-6 rounded-md grid content-start rounded-md bg-white-grey">
+    <form className="w-full p-6 rounded-md grid content-start rounded-md bg-white-grey">
       <TextInput title="Proposal Title" name="title" required />
       <TextInput
         title="proposal description"
@@ -12,14 +14,15 @@ export default function FundDestroyerForm() {
         wide
         required
       />
+      <Label text="Fund to remove" required />
+      <FundSelection />
 
       <button
         type="button"
-        onClick={createFund}
         className="justify-self-center text-blue-accent hover:text-angel-blue uppercase text-white font-extrabold mt-4"
       >
         Propose changes
       </button>
-    </div>
+    </form>
   );
 }
--- src/pages/Admin/Templates/FundDestroyer/FundSelection.tsx
+++ src/pages/Admin/Templates/FundDestroyer/FundSelection.tsx
@@ -1,3 +1,58 @@
+import React, { ReactChildren, ReactChild, ReactNode } from "react";
+import { FundDetails } from "contracts/types";
+import { useFundList } from "services/terra/indexFund/queriers";
+
 export default function FundSelection() {
-  return <div>fund selection</div>;
+  const { fundList, isFundListLoading } = useFundList();
+  console.log(fundList);
+  return (
+    <table
+      className="table-auto bg-light-grey shadow-inner-white-grey 
+    rounded-md text-angel-grey text-left"
+    >
+      <TableSection<"heading"> rowClass="font-heading uppercase text-sm ">
+        <>
+          <th className="px-4 pt-2">fund id</th>
+          <th className="px-4 pt-2">fund name</th>
+        </>
+      </TableSection>
+      <TableSection<"body"> rowClass="border-b hover:bg-angel-blue hover:bg-opacity-10 cursor-pointer">
+        {fundList.map((fund) => (
+          <Cells
+            key={fund.id}
+            attributes={fund}
+            toInclude={["id", "name"]}
+            cellClass="font-mono px-4 py-2"
+          />
+        ))}
+      </TableSection>
+    </table>
+  );
+}
+
+function TableSection<T extends "body" | "heading">(props: {
+  children: T extends "body" ? JSX.Element[] : JSX.Element;
+  rowClass: string;
+}) {
+  return (
+    <tbody>
+      {React.Children.map(props.children, (child) => (
+        <tr className={props.rowClass}>{child}</tr>
+      ))}
+    </tbody>
+  );
+}
+
+function Cells<T extends object>(props: {
+  cellClass: string;
+  attributes: T;
+  toInclude: (keyof T)[];
+}) {
+  let cells: JSX.Element[] = [];
+  for (const attr in props.attributes) {
+    if (props.toInclude.includes(attr)) {
+      cells.push(<td className={props.cellClass}>{props.attributes[attr]}</td>);
+    }
+  }
+  return <>{cells}</>;
 }
