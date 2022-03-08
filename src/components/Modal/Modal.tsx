1cbc25a6bf4915199db8f6db55223bb75494e9b8
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-03-08 14:13:53 +0100

    feat: Implement callback hook to trap focus on modal when opened

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -10,20 +10,65 @@ import {
 } from "react";
 import { Handlers, Opener, Props } from "./types";
 
+const TAB_KEY = "Tab";
+const focusableElementsString =
+  'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+
 export default function Modal(props: Props) {
   const [Content, setContent] = useState<ReactNode>();
   const ref = useRef<HTMLDivElement>();
   const [backdropDismiss, setBackdropDismiss] = useState(true);
   const escKeyPressed = useKeyPress("Escape");
 
+  // hook to trap focus within modal
+  const focusHandler = useCallback(
+    (evt?: KeyboardEvent) => {
+      const focusableElements = Array.from(
+        ref?.current?.querySelectorAll(focusableElementsString) || []
+      );
+
+      const firstTabStop = focusableElements[0] as HTMLElement;
+      const lastTabStop = focusableElements[
+        focusableElements.length - 1
+      ] as HTMLElement;
+
+      if (focusableElements.length === 0) return;
+
+      if (!evt) {
+        firstTabStop.focus();
+        return;
+      }
+
+      // TAB
+      if (evt.key === TAB_KEY) {
+        // SHIFT + TAB
+        if (evt.shiftKey) {
+          if (document.activeElement === firstTabStop) {
+            evt.preventDefault();
+            lastTabStop.focus();
+          }
+        } else {
+          if (document.activeElement === lastTabStop) {
+            evt.preventDefault();
+            firstTabStop.focus();
+          }
+        }
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [ref?.current]
+  );
+
   const showModal: Opener = (Content, props) => {
     setBackdropDismiss(props.isDismissDisabled ?? true);
     setContent(<Content {...props} />);
+    focusHandler();
   };
 
   function closeModal() {
     setContent(undefined);
     setBackdropDismiss(true);
+    ref.current = undefined;
   }
 
   const dismissModal = (event: any) => {
@@ -50,11 +95,13 @@ export default function Modal(props: Props) {
       if (node !== null && backdropDismiss) {
         ref.current = node;
         ref.current?.addEventListener("click", dismissModal);
+        window.addEventListener("keydown", focusHandler);
+        focusHandler();
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [backdropDismiss]
+    [backdropDismiss, Content]
   );
 
   return (
