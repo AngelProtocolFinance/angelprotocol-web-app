16ef9dc57277ffcad86d8327089a0c79e440bcff
jp-mariano <65009749+jp-mariano@users.noreply.github.com>
2022-03-08 10:43:39 +0800

    Display High Level Donations Metrics in the Leaderboard Page

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,8 +1,10 @@
 import CharityLeaderboard from "./Board";
+import DonationMetrics from "./DonationMetrics";
 
 export default function Leaderboard() {
   return (
     <section className="grid content-start padded-container">
+      <DonationMetrics />
       <h3 className="mt-6 uppercase text-white-grey text-3xl font-bold">
         Leaderboard
       </h3>
