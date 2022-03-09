82f418656f56a1a0d9dba9c086e1ddab62533336
Nenad Misic <n.misic@outlook.com>
2022-03-09 18:13:11 +0100

    Add button section

--- src/pages/Registration/AdditionalInformation/AdditionalInformation.tsx
+++ src/pages/Registration/AdditionalInformation/AdditionalInformation.tsx
@@ -1,3 +1,39 @@
+import { FormProvider, useForm } from "react-hook-form";
+import ButtonSection from "./ButtonSection";
+
 export default function AdditionalInformation() {
-  return <div>AdditionalInformation</div>;
+  const methods = useForm({
+    // resolver: yupResolver(ContactInfoSchema),
+    // defaultValues: {
+    //   charityName: props.contactData?.CharityName || "",
+    //   firstName: props.contactData?.FirstName || "",
+    //   lastName: props.contactData?.LastName || "",
+    //   email: props.contactData?.Email || "",
+    //   phone: props.contactData?.PhoneNumber || "",
+    //   orgRole: props.contactData?.Role || UserRoles.ceo,
+    //   otherRole: props.contactData?.otherRole || "",
+    //   checkedPolicy: false,
+    //   uniqueID: props.contactData?.PK || "",
+    // },
+  });
+
+  return (
+    <div className="flex flex-col gap-5 items-center">
+      <Title />
+
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(() => console.log("Upload"))}>
+          <ButtonSection />
+        </form>
+      </FormProvider>
+    </div>
+  );
+}
+
+function Title() {
+  return (
+    <h2 className="flex text-lg xl:text-xl font-semibold gap-2 items-center">
+      Your logo and description will be used to populate your public profile
+    </h2>
+  );
 }
---
+++ src/pages/Registration/AdditionalInformation/ButtonSection.tsx
@@ -0,0 +1,30 @@
+import Loader from "components/Loader/Loader";
+import { useFormContext } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import Button from "../Button";
+import routes from "../routes";
+
+export default function ButtonSection() {
+  const history = useHistory();
+  const {
+    formState: { isSubmitting },
+  } = useFormContext();
+
+  if (isSubmitting) {
+    return <Loader bgColorClass="bg-white" widthClass="w-3" gapClass="gap-1" />;
+  }
+
+  return (
+    <div className="flex justify-center">
+      <Button
+        className="bg-green-400 w-48 h-12 mr-2"
+        onClick={() => history.push(routes.dashboard)}
+      >
+        Back
+      </Button>
+      <Button submit className="bg-thin-blue w-48 h-12">
+        Continue
+      </Button>
+    </div>
+  );
+}
