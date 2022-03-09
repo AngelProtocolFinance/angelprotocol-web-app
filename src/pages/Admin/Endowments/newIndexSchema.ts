79ac9b49a172493181571e880d04dbf860e9bbd3
ap-justin <justin@angelprotocol.io>
2022-03-09 15:23:06 +0800

    removed unused components

--- src/pages/Admin/Endowments/newIndexSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as Yup from "yup";
-export const newIndexFundSchema = Yup.object().shape({
-  id: Yup.string().required("ID is required"),
-  name: Yup.string().required("Name is required"),
-  description: Yup.string().required("Description is required"),
-});
