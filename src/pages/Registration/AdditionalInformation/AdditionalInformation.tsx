81d315b52849207e429fa78481044959412cbf64
Nenad Misic <n.misic@outlook.com>
2022-03-11 10:27:45 +0100

    Add org. name field

--- src/pages/Registration/AdditionalInformation/AdditionalInformation.tsx
+++ src/pages/Registration/AdditionalInformation/AdditionalInformation.tsx
@@ -3,6 +3,8 @@ import { FormProvider, useForm } from "react-hook-form";
 import ButtonSection from "./ButtonSection";
 import { FormValues, SCHEMA } from "./types";
 import DescriptionInput from "./DescriptionInput";
+import { useGetter } from "store/accessors";
+import InputRow from "./InputRow";
 
 export default function AdditionalInformation() {
   const methods = useForm<FormValues>({
@@ -19,9 +21,10 @@ export default function AdditionalInformation() {
 
       <FormProvider {...methods}>
         <form
-          className="flex flex-col w-full h-full gap-4 items-center"
+          className="flex flex-col w-5/6 h-full gap-4"
           onSubmit={methods.handleSubmit(() => console.log("Upload"))}
         >
+          <OrganizationName />
           <DescriptionInput />
 
           <ButtonSection />
@@ -36,5 +39,15 @@ function Title() {
     <h2 className="flex text-lg xl:text-xl font-semibold gap-2 items-center">
       Your logo and description will be used to populate your public profile
     </h2>
+  );
+}
+
+function OrganizationName() {
+  const user = useGetter((state) => state.user);
+
+  return (
+    <InputRow id="charityName" label="Name of your organization">
+      <span className="flex w-full font-bold">{user.CharityName}</span>
+    </InputRow>
   );
 }
