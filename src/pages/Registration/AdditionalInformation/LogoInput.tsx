716841f0164a1391e114024617a8b7a4d60a0e00
Nenad Misic <n.misic@outlook.com>
2022-03-11 11:19:15 +0100

    Add LogoInput

---
+++ src/pages/Registration/AdditionalInformation/LogoInput.tsx
@@ -0,0 +1,27 @@
+import FileDropzone from "components/FileDropzone";
+import { useFormContext } from "react-hook-form";
+import { InputRow } from "../common";
+import { FormValues } from "./types";
+
+export default function LogoInput() {
+  const {
+    formState: { errors, isSubmitting },
+  } = useFormContext<FormValues>();
+
+  // For some reason Yup doesn't set any error fields related to the array itself (judged by the type assumed
+  // to be 'FieldError[] | undefined'), but only sets the fields of its items, so we have to convert it to 'any'
+  const errorMessage = !!errors?.logo?.length
+    ? errors.logo[0].message
+    : (errors?.logo as any)?.message;
+
+  return (
+    <InputRow id="logo" label="Logo of your organization" required>
+      <FileDropzone name="logo" className="h-8" disabled={isSubmitting} />
+      {errorMessage && (
+        <p className="w-full text-xs text-failed-red text-center">
+          {errorMessage}
+        </p>
+      )}
+    </InputRow>
+  );
+}
