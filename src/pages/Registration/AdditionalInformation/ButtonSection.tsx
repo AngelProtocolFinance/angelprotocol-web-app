74eaa8ef79806716796369377b3fa6fea5d94ebb
Nenad Misic <n.misic@outlook.com>
2022-03-11 11:26:54 +0100

    Refactor ButtonSections

--- src/pages/Registration/AdditionalInformation/ButtonSection.tsx
+++ src/pages/Registration/AdditionalInformation/ButtonSection.tsx
@@ -3,29 +3,30 @@ import { useFormContext } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import { Button } from "../common";
 import routes from "../routes";
-import { FormValues } from "./types";
 
 export default function ButtonSection() {
   const history = useHistory();
   const {
     formState: { isSubmitting },
-  } = useFormContext<FormValues>();
-
-  if (isSubmitting) {
-    return <Loader bgColorClass="bg-white" widthClass="w-3" gapClass="gap-1" />;
-  }
+  } = useFormContext();
 
   return (
     <div className="flex justify-center">
-      <Button
-        className="bg-green-400 w-48 h-12 mr-2"
-        onClick={() => history.push(routes.dashboard)}
-      >
-        Back
-      </Button>
-      <Button submit className="bg-thin-blue w-48 h-12">
-        Continue
-      </Button>
+      {isSubmitting ? (
+        <Loader bgColorClass="bg-white" widthClass="w-3" gapClass="gap-1" />
+      ) : (
+        <>
+          <Button
+            className="bg-green-400 w-48 h-12 mr-2"
+            onClick={() => history.push(routes.dashboard)}
+          >
+            Back
+          </Button>
+          <Button submit className="bg-thin-blue w-48 h-12">
+            Submit
+          </Button>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/Registration/Documentation/ButtonSection.tsx
+++ src/pages/Registration/Documentation/ButtonSection.tsx
@@ -3,29 +3,30 @@ import { useFormContext } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import { Button } from "../common";
 import routes from "../routes";
-import { FormValues } from "./types";
 
 export default function ButtonSection() {
   const history = useHistory();
   const {
     formState: { isSubmitting },
-  } = useFormContext<FormValues>();
-
-  if (isSubmitting) {
-    return <Loader bgColorClass="bg-white" widthClass="w-3" gapClass="gap-1" />;
-  }
+  } = useFormContext();
 
   return (
     <div className="flex justify-center">
-      <Button
-        className="w-48 h-12 bg-green-400 mr-2"
-        onClick={() => history.push(routes.dashboard)}
-      >
-        Back
-      </Button>
-      <Button submit className="w-48 h-12 bg-thin-blue">
-        Upload
-      </Button>
+      {isSubmitting ? (
+        <Loader bgColorClass="bg-white" widthClass="w-3" gapClass="gap-1" />
+      ) : (
+        <>
+          <Button
+            className="bg-green-400 w-48 h-12 mr-2"
+            onClick={() => history.push(routes.dashboard)}
+          >
+            Back
+          </Button>
+          <Button submit className="bg-thin-blue w-48 h-12">
+            Upload
+          </Button>
+        </>
+      )}
     </div>
   );
 }
