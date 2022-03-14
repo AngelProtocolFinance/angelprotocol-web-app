ae938c2aea339fee7a0352f95329ce452e91794b
Andrey <andrey@angelprotocol.io>
2022-03-14 14:22:43 +0800

    revert breaking hero commits

--- src/Website/Header/WebHead.tsx
+++ src/Website/Header/WebHead.tsx
@@ -5,9 +5,10 @@ import { IoClose } from "react-icons/io5";
 import { GoLinkExternal } from "react-icons/go";
 import betaBlueLogo from "assets/images/angelprotocol-beta-horiz-blu.png";
 import useScrollShadow from "hooks/useScrollShadow";
-import heart_icon from "assets/icons/broken_heart.svg";
-import ua_icon from "assets/icons/ukraine.svg";
-import { site, app } from "types/routes";
+import earth_icon from "assets/icons/earth.svg";
+import heart_icon from "assets/icons/heart.svg";
+import star_icon from "assets/icons/star.svg";
+import { site, app } from "constants/routes";
 import MobileNav from "./MobileNav";
 import WebMenu from "./WebMenu";
 
@@ -47,16 +48,17 @@ export default function WebHead() {
 
       <a
         ref={shadowRef}
-        href={`https://ukraine.angelprotocol.io/`}
+        href={`${site.app}/markeplace`}
         target="_blank"
         rel="noopener noreferrer"
         className="transition-shadow absolute bottom-0 transform translate-y-full font-heading cursor-pointer hover:bg-black bg-angel-grey w-full p-2 flex items-center justify-center text-white text-xs"
       >
-        <Word icon={heart_icon} title="" />
-        <span className="flex w-fit items-center mr-2 md:uppercase">
-          Donate now to humanitarian relief in Ukraine
+        <span className="hidden md:block flex w-fit items-center mr-2 md:uppercase">
+          Charity Marketplace is now live!
         </span>
-        <Word icon={ua_icon} title="" />
+        <Word icon={star_icon} title="Select" />
+        <Word icon={heart_icon} title="Connect" />
+        <Word icon={earth_icon} title="Donate" />
         <GoLinkExternal className="text-lg" />
       </a>
     </header>
