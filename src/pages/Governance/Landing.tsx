63fe44ee342cfe9b67ed13f44cab0468189cf99a
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-03-14 15:01:49 +0100

    ui enchancement

--- src/pages/Governance/Landing.tsx
+++ src/pages/Governance/Landing.tsx
@@ -1,11 +1,33 @@
 import { currency_text, denoms } from "constants/currency";
+import Action from "./Action";
 import Figure from "./Figure";
 import Polls from "./Polls";
 import Portal from "./Portal";
 import useGov from "./useGov";
+import { useHistory } from "react-router-dom";
+import { app, site } from "constants/routes";
+
+function DonationAdvert() {
+  const history = useHistory();
+  return (
+    <div className="mt-5 w-full col-span-2 border border-opacity-10 bg-white bg-opacity-10 rounded-md p-5 text-white-grey shadow-xl cursor-pointer">
+      <h4 className="text-white font-normal text-lg mt-1 mb-10 overflow-hidden">
+        The charity Marketplace is now open! Give directly to the endowment of
+        your choice and get 10% back in HALO airdrops.
+      </h4>
+      <div className="w-full flex justify-end">
+        <Action
+          title="Donate now"
+          action={() => history.push(`${site.app}/${app.marketplace}`)}
+        />
+      </div>
+    </div>
+  );
+}
 
 export default function Landing() {
   const { staked, percentStaked, spot_price } = useGov();
+
   return (
     <div className="padded-container grid grid-rows-aa1 gap-4 pb-4 min-h-screen">
       <h2 className="font-heading uppercase font-bold text-4xl mt-4 text-white-grey">
@@ -26,6 +48,7 @@ export default function Landing() {
           percent={percentStaked}
         />
         <Portal />
+        <DonationAdvert />
       </div>
       <Polls />
     </div>
--- src/pages/Governance/Polls.tsx
+++ src/pages/Governance/Polls.tsx
@@ -5,14 +5,11 @@ import { SiHiveBlockchain } from "react-icons/si";
 import toCurrency from "helpers/toCurrency";
 import usePoller from "components/Transactors/Poller/usePoller";
 import Action from "./Action";
-import { useHistory } from "react-router-dom";
-import { app, site } from "constants/routes";
 
 export default function Polls() {
   const block_height = useLatestBlock();
   const gov_polls = useGovPolls();
   const showPoller = usePoller();
-  const history = useHistory();
 
   return (
     <div className="mt-4">
@@ -45,19 +42,6 @@ export default function Polls() {
         {gov_polls.map((poll) => (
           <Poll key={poll.id} poll_id={poll.id} />
         ))}
-      </div>
-      <hr className="mt-5" />
-      <div className="mt-5 w-130 cursor-pointer border border-opacity-10 bg-blue-accent bg-opacity-50 hover:bg-opacity-70 rounded-md p-4 text-white-grey text-opacity-80 shadow-lg">
-        <h4 className="text-white font-normal text-lg mt-1 mb-5 pb-1 overflow-hidden">
-          The charity Marketplace is now open! Give directly to the endowment of
-          your choice and get 10% back in HALO airdrops.
-        </h4>
-        <div className="w-full flex justify-end">
-          <Action
-            title="Visit Marketplace"
-            action={() => history.push(`${site.app}/${app.marketplace}`)}
-          />
-        </div>
       </div>
     </div>
   );
