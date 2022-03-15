9e4aac046ec77b99fa6159a112db3c4ac449f0e3
Brian Lee <brianlee1597@gmail.com>
2022-03-15 21:52:04 +0900

    auto terra connection

--- src/components/WalletSuite/Connectors/TerraConnector.tsx
+++ src/components/WalletSuite/Connectors/TerraConnector.tsx
@@ -1,16 +1,31 @@
-import { Connection, useWallet } from "@terra-money/wallet-provider";
+import {
+  Connection,
+  ConnectType,
+  useWallet,
+} from "@terra-money/wallet-provider";
+import { DeviceType, deviceType } from "helpers/deviceType";
+import { useEffect } from "react";
 import { useGetter } from "store/accessors";
 import { setIcon } from "../manageIcon";
 import ConnectButton from "./ConnectButton";
 
 export default function TerraConnector(props: Connection) {
   const { isUpdating } = useGetter((state) => state.wallet);
-  const { connect } = useWallet();
+  const { availableConnectTypes, connect } = useWallet();
 
   function handleClick() {
     connect(props.type, props.identifier);
     setIcon(props.icon);
   }
+
+  useEffect(() => {
+    const isSafePal =
+      deviceType() === DeviceType.MOBILE &&
+      availableConnectTypes.includes(ConnectType.EXTENSION);
+
+    isSafePal && connect(ConnectType.EXTENSION);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [availableConnectTypes]);
 
   return (
     <ConnectButton
