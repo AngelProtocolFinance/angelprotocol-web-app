0ef96492569cc259d89ed5effc63b17ca43f0155
Brian Lee <brianlee1597@gmail.com>
2022-03-15 17:51:31 +0900

    removed isXdefi redundant function

--- src/components/WalletSuite/Connectors/isXdefiPrioritized.ts
+++
@@ -1,6 +0,0 @@
-import { Dwindow } from "services/provider/types";
-
-const dwindow = window as Dwindow;
-export default function isXdefiPrioritized() {
-  return dwindow.xfi?.ethereum?.isMetaMask;
-}
