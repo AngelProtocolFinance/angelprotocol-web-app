222c298174a1b0abc58beab545edded8e3b036ce
ap-justin <justin@angelprotocol.io>
2022-03-18 10:36:11 +0800

    update navbar style

--- src/pages/Admin/AdminNav.tsx
+++ src/pages/Admin/AdminNav.tsx
@@ -4,17 +4,14 @@ import { NavLink } from "react-router-dom";
 
 export default function AdminNav() {
   return (
-    <div className="bg-white bg-opacity-10 shadow-inner flex divide-x">
+    <div className="flex justify-end divide-x border-opacity-80">
       <NavLink end to={admin.index} className={styler}>
         proposals
       </NavLink>
       <NavLink to={admin.alliance} className={styler}>
         alliance members
       </NavLink>
-      <NavLink
-        to={admin.charity_applications}
-        className="py-1 uppercase text-sm text-center px-2 font-semibold font-heading text-white text-opacity-80 pointer-events-none cursor-none bg-grey-accent  bg-opacity-60 text-grey-accent"
-      >
+      <NavLink to={admin.charity_applications} className={disabledStyle}>
         charity applications
       </NavLink>
     </div>
@@ -22,6 +19,9 @@ export default function AdminNav() {
 }
 
 const styler = createNavLinkStyler(
-  "py-1 uppercase text-sm text-center px-2 font-semibold font-heading text-white text-opacity-80",
-  "bg-white text-angel-grey"
+  "px-2 uppercase text-sm text-center font-semibold font-heading text-white",
+  "text-angel-orange"
 );
+
+const disabledStyle =
+  "uppercase text-sm text-center px-2 font-semibold font-heading pointer-events-none select-none text-grey-accent";
