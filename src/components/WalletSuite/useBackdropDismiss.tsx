47fa9278dc6184698ea48164f3b07a0657dc70ba
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-03-18 20:39:46 +0100

    feat: Dismiss wallet popup by clicking outside it or pressing the escape key as a shortcut

---
+++ src/components/WalletSuite/useBackdropDismiss.tsx
@@ -0,0 +1,26 @@
+import { MutableRefObject, useCallback, useEffect, useRef } from "react";
+
+export default function useBackdropDismiss(callback: () => void) {
+  const ref = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    document.addEventListener("click", handler);
+    return () => document.removeEventListener("click", handler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const dismissHandler = (
+    currentRef?: MutableRefObject<HTMLDivElement | undefined>
+  ) => {
+    if (currentRef) ref.current = currentRef?.current;
+  };
+
+  const handler = useCallback((event: any) => {
+    const path = (event.path ||
+      (event.composedPath && event.composedPath())) as any[];
+    if (path.includes(ref.current)) return;
+    callback();
+  }, []);
+
+  return dismissHandler;
+}
