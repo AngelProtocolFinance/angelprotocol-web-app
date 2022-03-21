9378d220c7df9b13ea9bbbe208d91a8c9592a64a
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2022-03-21 10:27:33 +0100

    feat: Dismiss wallet popup by clicking outside it or pressing the escape key as a shortcut (#849)
    * feat: Dismiss wallet popup by clicking outside it or pressing the escape key as a shortcut
    
    * clean up

--- src/components/WalletSuite/WalletSuite.tsx
+++ src/components/WalletSuite/WalletSuite.tsx
@@ -1,18 +1,24 @@
 import { IoWalletSharp } from "react-icons/io5";
 import Display from "./Display";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import ConnectOptions from "./ConnectOptions";
 import { useGetter } from "store/accessors";
 import { Providers } from "services/wallet/types";
 import useWalletUpdator from "./useWalletUpdator";
+import useKeyPress from "hooks/useKeyPress";
+import useBackdropDismiss from "./useBackdropDismiss";
 
 export default function WalletSuite() {
   const provider = useGetter((state) => state.provider);
+  const escKeyPressed = useKeyPress("Escape");
+  const ref = useRef<HTMLDivElement>();
+
   useWalletUpdator(provider.active);
 
   const [connectOptionsShown, showConnectOptions] = useState(false);
   const toggleConnectOptions = () => showConnectOptions((p) => !p);
   const hideConnectOptions = () => showConnectOptions(false);
+  const dismissHandler = useBackdropDismiss(hideConnectOptions);
 
   const isProviderActive = provider.active !== Providers.none;
   //close modal after connecting
@@ -21,8 +27,28 @@ export default function WalletSuite() {
     //eslint-disable-next-line
   }, [isProviderActive]);
 
+  useEffect(() => {
+    if (escKeyPressed && connectOptionsShown) {
+      hideConnectOptions();
+    }
+  }, [escKeyPressed, connectOptionsShown]);
+
+  const handleRef = useCallback(
+    (node) => {
+      if (node !== null) {
+        ref.current = node;
+        dismissHandler(ref);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
   return (
-    <div className="relative border border-opacity-40 hover:bg-white hover:bg-opacity-10 rounded-md">
+    <div
+      ref={handleRef}
+      className="relative border border-opacity-40 hover:bg-white hover:bg-opacity-10 rounded-md"
+    >
       {!isProviderActive && (
         <button
           className="flex py-2 px-3 items-center text-white  "
