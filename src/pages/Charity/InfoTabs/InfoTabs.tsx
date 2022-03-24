d7fc0bfb7b5f4f1a31ce50189625025914da02f1
Andrey <andrey@angelprotocol.io>
2022-03-24 23:25:23 +0800

    remove unused import

--- src/pages/Charity/InfoTabs/InfoTabs.tsx
+++ src/pages/Charity/InfoTabs/InfoTabs.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { charity } from "constants/routes";
 import { EndowmentInfo } from "./EndowmentInfo";
 import Overview from "./Overview";
