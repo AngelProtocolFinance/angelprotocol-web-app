566817a754f8325be7ee05d144e1ffc5348808d4
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-03-25 19:45:05 +0100

    increase size options

--- src/pages/CharityEdit/useImageCompressor.tsx
+++ src/pages/CharityEdit/useImageCompressor.tsx
@@ -14,8 +14,8 @@ export default function useImageCompressor() {
     const resizedImage = await ref.current?.compress([file], {
       size: 2, // the max size in MB, defaults to 2MB
       quality: 1, // the quality of the image, max is 1,
-      maxWidth: 300, // the max width of the output image, defaults to 1920px
-      maxHeight: 300, // the max height of the output image, defaults to 1920px
+      maxWidth: 500, // the max width of the output image, defaults to 1920px
+      maxHeight: 500, // the max height of the output image, defaults to 1920px
       resize: true, // defaults to true, set false if you do not want to resize the image width and height
     });
     const img = resizedImage[0];
