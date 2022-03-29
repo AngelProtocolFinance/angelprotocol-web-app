d83fa75b60aa024f1e7058ac8778c3005f26de90
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-03-29 12:24:09 +0100

    disable image resize to prevent lossy compression

--- src/pages/CharityEdit/useImageCompressor.tsx
+++ src/pages/CharityEdit/useImageCompressor.tsx
@@ -11,18 +11,16 @@ export default function useImageCompressor() {
 
   async function resize(file: File) {
     if (!ref.current) return;
+    console.log("before: ", file);
     const resizedImage = await ref.current?.compress([file], {
       size: 2, // the max size in MB, defaults to 2MB
       quality: 1, // the quality of the image, max is 1,
-      maxWidth: 500, // the max width of the output image, defaults to 1920px
-      maxHeight: 500, // the max height of the output image, defaults to 1920px
-      resize: true, // defaults to true, set false if you do not want to resize the image width and height
     });
     const img = resizedImage[0];
     const base64str = img.data;
     const imgExt = img.ext;
     const resizedFiile = Compress.convertBase64ToFile(base64str, imgExt);
-
+    console.log("after: ", resizedFiile);
     return resizedFiile;
   }
 
