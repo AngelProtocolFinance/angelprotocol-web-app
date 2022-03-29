80c25053f90f23d1dab7934acc00156e34a050ab
Nenad Misic <n.misic@outlook.com>
2022-03-29 10:19:17 +0200

    Make /overview the default route for charity cards

--- src/pages/Market/CharityCard.tsx
+++ src/pages/Market/CharityCard.tsx
@@ -1,13 +1,13 @@
 import LazyImage from "components/LazyImage/LazyImage";
 import { Link } from "react-router-dom";
-import { app, site } from "constants/routes";
+import { app, charity, site } from "constants/routes";
 import RichTextRenderer from "components/RichTextRenderer/RichTextRenderer";
 import { Profile } from "services/aws/endowments/types";
 
 export default function CharityCard(props: Profile) {
   return (
     <Link
-      to={`${site.app}/${app.charity}/${props.endowment_address}`}
+      to={`${site.app}/${app.charity}/${props.endowment_address}${charity.overview}`}
       className="relative w-72 flex-none break-words rounded-2xl hover:shadow-3xl cursor-pointer mb-4 mx-2 p-2"
     >
       <LazyImage
