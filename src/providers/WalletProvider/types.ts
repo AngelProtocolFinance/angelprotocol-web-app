84224c7400d50b9087f0fdfc60129bd98cbdc9b6
Nenad Misic <n.misic@outlook.com>
2022-03-30 12:05:30 +0200

    Add local Connection and ConnectType

--- src/providers/WalletProvider/types.ts
+++ src/providers/WalletProvider/types.ts
@@ -1,12 +1,31 @@
 import {
   ConnectedWallet,
-  Connection,
   Installation,
   NetworkInfo,
   WalletStatus,
 } from "@terra-money/wallet-provider";
 import { chainIDs } from "constants/chainIDs";
 import { terra_lcds } from "constants/urls";
+
+// Enum extending @terra-money/wallet-types/types > ConnectType with Torus type
+export enum ConnectType {
+  /** Terra Station Extension or compatible browser extensions */
+  EXTENSION = "EXTENSION",
+  /** Terra Station Mobile or compatible mobile wallets */
+  WALLETCONNECT = "WALLETCONNECT",
+  /** Read only mode - View an address */
+  READONLY = "READONLY",
+  /** Torus wallet */
+  TORUS = "TORUS",
+}
+
+// Local type version of @terra-money/wallet-types/types > Connection
+export type Connection = {
+  type: ConnectType;
+  identifier?: string;
+  name: string;
+  icon: string;
+};
 
 export type IWalletContext = {
   wallet?: WalletProxy;
