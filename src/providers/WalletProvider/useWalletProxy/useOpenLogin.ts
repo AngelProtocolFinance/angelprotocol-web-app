c00943940582d4676eaa619b6a4610987a277013
Nenad Misic <n.misic@outlook.com>
2022-03-30 11:42:46 +0200

    Add wallet creation logic to useOpenLogin

--- src/providers/WalletProvider/useWalletProxy/useOpenLogin.ts
+++ src/providers/WalletProvider/useWalletProxy/useOpenLogin.ts
@@ -1,4 +1,7 @@
+import { MnemonicKey, Wallet } from "@terra-money/terra.js";
+import { useLCDClient } from "@terra-money/wallet-provider";
 import OpenLogin from "@toruslabs/openlogin";
+import { entropyToMnemonic } from "bip39";
 import { useCallback, useEffect, useState } from "react";
 import { useGetter } from "store/accessors";
 
@@ -10,7 +13,19 @@ const openLogin = new OpenLogin({
 export default function useOpenLogin() {
   const user = useGetter((state) => state.user);
   const [isLoading, setLoading] = useState(true);
-  const [privateKey, setPrivateKey] = useState("");
+  const [wallet, setWallet] = useState<Wallet>();
+  const lcd = useLCDClient();
+
+  const handleCreateWallet = useCallback(
+    (entropy: string) => {
+      const mnemonic = entropyToMnemonic(entropy);
+      const mnemonicKey = new MnemonicKey({ mnemonic });
+      const newWallet = lcd.wallet(mnemonicKey);
+
+      setWallet(newWallet);
+    },
+    [lcd]
+  );
 
   useEffect(() => {
     async function initializeOpenlogin() {
@@ -20,12 +35,13 @@ export default function useOpenLogin() {
       // NOTE: to successfully read this value, it is necessary to call this hook in the component
       // that is Torus is set to redirect to, otherwise this value would be empty
       if (openLogin.privKey) {
-        setPrivateKey(openLogin.privKey);
+        handleCreateWallet(openLogin.privKey);
       }
       setLoading(false);
     }
 
     initializeOpenlogin();
+    // eslint-disable-next-line
   }, []);
 
   const connect = useCallback(async () => {
@@ -37,19 +53,23 @@ export default function useOpenLogin() {
     };
 
     setLoading(true);
-    await openLogin.login(loginParams);
+    const loginResult = await openLogin.login(loginParams);
+    if (loginResult?.privKey) {
+      handleCreateWallet(loginResult.privKey);
+    }
     setLoading(false);
-  }, [user.Email]);
+  }, [user.Email, handleCreateWallet]);
 
   const disconnect = useCallback(async () => {
     setLoading(true);
     await openLogin.logout();
+    setWallet(undefined);
     setLoading(false);
   }, []);
 
   return {
+    wallet,
     isLoading,
-    privateKey,
     connect,
     disconnect,
   };
