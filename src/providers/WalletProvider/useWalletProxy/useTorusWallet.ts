0ab066b9646d49ce3630bab69b35bfceaff8ae0f
Nenad Misic <n.misic@outlook.com>
2022-03-30 17:16:41 +0200

    Refactor useTorusWallet

--- src/providers/WalletProvider/useWalletProxy/useTorusWallet.ts
+++ src/providers/WalletProvider/useWalletProxy/useTorusWallet.ts
@@ -1,13 +1,24 @@
-import { MnemonicKey, Wallet } from "@terra-money/terra.js";
-import { useLCDClient, WalletStatus } from "@terra-money/wallet-provider";
+import { LCDClient, MnemonicKey, Wallet } from "@terra-money/terra.js";
+import { WalletStatus } from "@terra-money/wallet-provider";
 import OpenLogin from "@toruslabs/openlogin";
 import { entropyToMnemonic } from "bip39";
+import { chainIDs } from "constants/chainIDs";
+import { terra_lcds } from "constants/urls";
 import { useCallback, useEffect, useState } from "react";
 import { useGetter } from "store/accessors";
 
+// TODO: would be good to set this value using the environment variables
+const NETWORK = "testnet";
+
 const openLogin = new OpenLogin({
   clientId: process.env.REACT_APP_WEB_3_AUTH_CLIENT_ID || "",
-  network: "testnet",
+  network: NETWORK,
+  uxMode: "popup",
+});
+
+const lcdClient = new LCDClient({
+  URL: terra_lcds[chainIDs[NETWORK]],
+  chainID: chainIDs[NETWORK],
 });
 
 export default function useTorusWallet() {
@@ -16,18 +27,13 @@ export default function useTorusWallet() {
     WalletStatus.WALLET_NOT_CONNECTED
   );
   const [wallet, setWallet] = useState<Wallet>();
-  const lcd = useLCDClient();
 
-  const handleCreateWallet = useCallback(
-    (entropy: string) => {
-      const mnemonic = entropyToMnemonic(entropy);
-      const mnemonicKey = new MnemonicKey({ mnemonic });
-      const newWallet = lcd.wallet(mnemonicKey);
-
-      setWallet(newWallet);
-    },
-    [lcd]
-  );
+  const handleCreateWallet = useCallback((entropy: string) => {
+    const mnemonic = entropyToMnemonic(entropy);
+    const mnemonicKey = new MnemonicKey({ mnemonic });
+    const newWallet = lcdClient.wallet(mnemonicKey);
+    setWallet(newWallet);
+  }, []);
 
   useEffect(() => {
     async function initializeOpenlogin() {
@@ -53,14 +59,11 @@ export default function useTorusWallet() {
   const connect = useCallback(async () => {
     setStatus(WalletStatus.INITIALIZING);
 
-    const loginParams = {
-      relogin: true,
+    const loginResult = await openLogin.login({
       extraLoginOptions: {
         login_hint: user.Email,
       },
-    };
-
-    const loginResult = await openLogin.login(loginParams);
+    });
     if (loginResult?.privKey) {
       handleCreateWallet(loginResult.privKey);
       setStatus(WalletStatus.WALLET_CONNECTED);
