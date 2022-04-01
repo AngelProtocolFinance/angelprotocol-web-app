4e433bba86875028eff99711010007fdb1792d62
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-04-01 11:56:40 +0100

    throw errors and adjust image max size and quality

--- src/pages/CharityEdit/optimizeImage.tsx
+++ src/pages/CharityEdit/optimizeImage.tsx
@@ -2,10 +2,10 @@ import Compress from "compress.js";
 
 const compress = new Compress();
 export default async function optimizeImage(file: File) {
-  if (!compress) return;
-  const resizedImage = await compress?.compress([file], {
-    size: 1, // the max size in MB, defaults to 2MB
-    quality: 1, // the quality of the image, max is 1,
+  if (!compress) throw new Error("Image compressor is not initialized");
+  const resizedImage = await compress.compress([file], {
+    size: 0.4, // the max size in MB, defaults to 2MB
+    quality: 0.8, // the quality of the image, max is 1,
   });
   const img = resizedImage[0];
   const base64str = img.data;
