4eafc6887afda164fb68fa753155d606df169436
Andrey <andrey@angelprotocol.io>
2022-04-04 10:25:09 +0800

    try/catch for compress && fix var spelling typo

--- src/pages/CharityEdit/optimizeImage.tsx
+++ src/pages/CharityEdit/optimizeImage.tsx
@@ -1,15 +1,21 @@
 import Compress from "compress.js";
 
 const compress = new Compress();
+
 export default async function optimizeImage(file: File) {
-  if (!compress) throw new Error("Image compressor is not initialized");
-  const resizedImage = await compress.compress([file], {
-    size: 0.4, // the max size in MB, defaults to 2MB
-    quality: 0.8, // the quality of the image, max is 1,
-  });
-  const img = resizedImage[0];
-  const base64str = img.data;
-  const imgExt = img.ext;
-  const resizedFiile = Compress.convertBase64ToFile(base64str, imgExt);
-  return resizedFiile;
+  try {
+    const resizedImage = await compress.compress([file], {
+      size: 0.4, // the max size in MB, defaults to 2MB
+      quality: 0.8, // the quality of the image, max is 1,
+    });
+    const img = resizedImage[0];
+    const base64str = img.data;
+    const imgExt = img.ext;
+    const resizedFile = Compress.convertBase64ToFile(base64str, imgExt);
+    return resizedFile;
+  } catch (err) {
+    throw new Error(
+      `An error occured during the image compression process: ${err}`
+    );
+  }
 }
