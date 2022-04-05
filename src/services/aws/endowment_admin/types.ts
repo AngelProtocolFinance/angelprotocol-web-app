0024330946a9b0a92f96f562a25484c80ae78bd7
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-04-05 13:46:44 +0100

    add transaction hash link to donations table

--- src/services/aws/endowment_admin/types.ts
+++ src/services/aws/endowment_admin/types.ts
@@ -1,3 +1,5 @@
+import { chainIDs } from "constants/chainIDs";
+
 export interface DepositTransactions {
   endowment_address: string; // Charity's endowment address
   wallet_address: string; // Owner's wallet address
@@ -6,6 +8,7 @@ export interface DepositTransactions {
   amount: number; // 14.251521
   app_used: string; // "restore-earth"
   transaction_type: string; // "deposit"
+  chain_id?: chainIDs;
 }
 
 export interface DonationTransactions extends DepositTransactions {}
