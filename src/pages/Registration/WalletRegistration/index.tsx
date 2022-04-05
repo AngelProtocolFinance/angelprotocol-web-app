9f336ffb5801bd5888a9ab9299bebed8322d2d6d
Nenad Misic <n.misic@outlook.com>
2022-04-05 10:33:03 +0200

    Create ConnectWallet page scaffold

--- src/pages/Registration/WalletRegistration/index.tsx
+++ src/pages/Registration/WalletRegistration/index.tsx
@@ -4,11 +4,13 @@ import routes from "./routes";
 
 const ChooseWallet = lazy(() => import("./ChooseWallet"));
 const RegisterWallet = lazy(() => import("./RegisterWallet"));
+const ConnectWallet = lazy(() => import("./ConnectWallet"));
 
 export default function WalletRegistration() {
   return (
     <Routes>
       <Route path={routes.index} element={<ChooseWallet />} />
+      <Route path={routes.connect} element={<ConnectWallet />} />
       <Route path={routes.submit} element={<RegisterWallet />} />
     </Routes>
   );
