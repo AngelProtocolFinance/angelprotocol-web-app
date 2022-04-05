c9b29bf2fe3c1c809083b85ec2ee01aa6a2a1111
Nenad Misic <n.misic@outlook.com>
2022-04-05 10:42:53 +0200

    Auto-redirect user once wallet is connected

--- src/pages/Registration/WalletRegistration/ConnectWallet.tsx
+++ src/pages/Registration/WalletRegistration/ConnectWallet.tsx
@@ -1,4 +1,20 @@
+import { WalletStatus } from "@terra-money/wallet-provider";
+import useWalletContext from "hooks/useWalletContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import routes from "./routes";
+import registerRoutes, { registerRootPath } from "../routes";
+
 export default function ConnectWallet() {
+  const navigate = useNavigate();
+  const { status } = useWalletContext();
+
+  useEffect(() => {
+    if (status === WalletStatus.WALLET_CONNECTED) {
+      navigate(`${registerRootPath}/${registerRoutes.wallet}/${routes.submit}`);
+    }
+  }, [status, navigate]);
+
   return (
     <div className="xl:w-1/2 font-semibold text-sm md:text-lg">
       Please connect your wallet using the "Connect" button in the top-right of
