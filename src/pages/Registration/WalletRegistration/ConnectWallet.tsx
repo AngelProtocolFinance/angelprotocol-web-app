b8f859d9489ba910f1ecc4d93853f58afb51f317
Nenad Misic <n.misic@outlook.com>
2022-04-05 10:36:47 +0200

    Add text to the ConnectWallet page

--- src/pages/Registration/WalletRegistration/ConnectWallet.tsx
+++ src/pages/Registration/WalletRegistration/ConnectWallet.tsx
@@ -1,3 +1,8 @@
 export default function ConnectWallet() {
-  return <div>ConnectWallet</div>;
+  return (
+    <div className="xl:w-1/2 font-semibold text-sm md:text-lg">
+      Please connect your wallet using the "Connect" button in the top-right of
+      the page
+    </div>
+  );
 }
