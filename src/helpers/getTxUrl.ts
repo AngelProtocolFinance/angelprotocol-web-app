47b2727742f04d9dd4d04731ca351571075f6e69
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-04-05 12:51:50 +0100

    update terra finder links and  update helper function to create transaction url to use "testnet" and "mainnet" with new finder url

--- src/helpers/getTxUrl.ts
+++ src/helpers/getTxUrl.ts
@@ -4,8 +4,9 @@ import { TERRA_FINDER } from "constants/urls";
 export default function getTxUrl(chainID: chainIDs, txhash: string) {
   switch (chainID) {
     case chainIDs.mainnet:
+      return `${TERRA_FINDER}mainnet/tx/${txhash}`;
     case chainIDs.testnet:
-      return `${TERRA_FINDER}${chainID}/tx/${txhash}`;
+      return `${TERRA_FINDER}testnet/tx/${txhash}`;
     case chainIDs.eth_ropsten:
       return `https://ropsten.etherscan.io/tx/${txhash}`;
     case chainIDs.eth_main:
