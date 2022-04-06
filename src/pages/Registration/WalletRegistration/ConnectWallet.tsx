88f13eec52949abc2d2f5f364d31fd7ee8284e5a
Nenad Misic <n.misic@outlook.com>
2022-04-06 13:09:40 +0200

    Add 'back to reg. dash' button

--- src/pages/Registration/WalletRegistration/ConnectWallet.tsx
+++ src/pages/Registration/WalletRegistration/ConnectWallet.tsx
@@ -5,10 +5,9 @@ import { useGetBinance } from "providers/BinanceWallet/BinanceWallet";
 import { useGetMetamask } from "providers/Metamask/Metamask";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "../common";
 import registerRoutes from "../routes";
 import routes from "./routes";
-
-const TEXT_CLASS_NAMES = "xl:w-1/2 font-semibold text-sm md:text-lg";
 
 export default function ConnectWallet() {
   const navigate = useNavigate();
@@ -24,15 +23,27 @@ export default function ConnectWallet() {
     }
   }, [status, navigate]);
 
-  return isMetamaskConnected || isBinanceConnected ? (
-    <div className={`flex flex-col gap-5 ${TEXT_CLASS_NAMES}`}>
-      <p>Only Terra compatible wallets are allowed!</p>
-      <p>Please connect your Terra wallet</p>
+  return (
+    <div className="flex flex-col gap-10 items-center xl:w-1/2 font-semibold text-sm md:text-lg">
+      {isMetamaskConnected || isBinanceConnected ? (
+        <div className="flex flex-col gap-3">
+          <p>Only Terra compatible wallets are allowed!</p>
+          <p>Please connect your Terra wallet</p>
+        </div>
+      ) : (
+        <p>
+          Please connect your Terra wallet using the "Connect" button in the
+          top-right of the page
+        </p>
+      )}
+      <Button
+        className="bg-green-400 w-72 md:w-80 h-10 text-sm md:text-base"
+        onClick={() =>
+          navigate(`${site.app}/${app.register}/${registerRoutes.dashboard}`)
+        }
+      >
+        Back to registration dashboard
+      </Button>
     </div>
-  ) : (
-    <p className={TEXT_CLASS_NAMES}>
-      Please connect your Terra wallet using the "Connect" button in the
-      top-right of the page
-    </p>
   );
 }
