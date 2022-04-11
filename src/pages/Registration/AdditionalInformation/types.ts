2f1c76f3f29cc7d6c028dc9051312d4ae4e798b5
Nenad Misic <n.misic@outlook.com>
2022-04-11 09:12:02 +0200

    Update FormValues schema file schemas

--- src/pages/Registration/AdditionalInformation/types.ts
+++ src/pages/Registration/AdditionalInformation/types.ts
@@ -1,11 +1,12 @@
+import { FileWrapper } from "components/FileDropzone/types";
 import * as Yup from "yup";
 
 export type FormValues = {
   charityOverview: string;
   // Expects an array because FileDropzone component always returns an array of Files,
   // so this way it's easier to handle (Yup validation ensures single file uploaded)
-  charityLogo: File[];
-  charityBanner: File[];
+  charityLogo: FileWrapper[];
+  charityBanner: FileWrapper[];
 };
 
 const VALID_MIME_TYPES = [
@@ -16,40 +17,49 @@ const VALID_MIME_TYPES = [
   "image/webp",
 ];
 
+const FILE_SCHEMA = Yup.mixed<FileWrapper>()
+  .test({
+    name: "fileType",
+    message: "Valid file types are PDF, JPG and PNG",
+    test: (fileWrapper) =>
+      fileWrapper?.file
+        ? VALID_MIME_TYPES.includes(fileWrapper.file.type)
+        : true,
+  })
+  .test({
+    name: "invalidState",
+    message: "Invalid internal state",
+    test: (fileWrapper) =>
+      // fileWrapper must be instantiated
+      !!fileWrapper &&
+      // file name must be set
+      !!fileWrapper.name &&
+      // either new file is uploaded or source URL to file is set
+      (!!fileWrapper.file || !!fileWrapper.sourceUrl),
+  });
+
 export const SCHEMA = Yup.object().shape({
   charityOverview: Yup.string().required("Organization description required"),
-  charityLogo: Yup.array<File>()
+  charityLogo: Yup.array<FileWrapper>()
     .of(
-      Yup.mixed<File>()
-        .test({
-          name: "fileSize",
-          message: "Image size must be smaller than 300KB",
-          test: (file) => (file?.size || 0) <= 3e5,
-        })
-        .test({
-          name: "fileType",
-          message: "Valid file types are JPG, PNG and other image types",
-          test: (file) => VALID_MIME_TYPES.includes(file?.type || "none"),
-        })
+      FILE_SCHEMA.test({
+        name: "fileSize",
+        message: "Image size must be smaller than 300KB",
+        test: (fileWrapper) => (fileWrapper?.file?.size || 0) <= 3e5,
+      })
     )
     .test({
       name: "exactlyOne",
       message: "Charity logo required",
       test: (arr) => arr?.length === 1,
     }),
-  charityBanner: Yup.array<File>()
+  charityBanner: Yup.array<FileWrapper>()
     .of(
-      Yup.mixed<File>()
-        .test({
-          name: "fileSize",
-          message: "Image size must be smaller than 1MB",
-          test: (file) => (file?.size || 0) <= 1e6,
-        })
-        .test({
-          name: "fileType",
-          message: "Valid file types are JPG, PNG and other image types",
-          test: (file) => VALID_MIME_TYPES.includes(file?.type || "none"),
-        })
+      FILE_SCHEMA.test({
+        name: "fileSize",
+        message: "Image size must be smaller than 1MB",
+        test: (fileWrapper) => (fileWrapper?.file?.size || 0) <= 1e6,
+      })
     )
     .test({
       name: "exactlyOne",
