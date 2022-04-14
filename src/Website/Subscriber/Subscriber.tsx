104dee94b489a58d2257daaa45197df966c2ec73
Nenad Misic <n.misic@outlook.com>
2022-04-14 13:38:46 +0200

    Align Subscriber file and function name and fix ring opacity (#929)

--- src/Website/Subscriber/Subscriber.tsx
+++ src/Website/Subscriber/Subscriber.tsx
@@ -2,7 +2,7 @@ import { ErrorMessage } from "@hookform/error-message";
 import Loader from "components/Loader/Loader";
 import useSubscribe from "./useSubscribe";
 
-export default function SubscriberForm() {
+export default function Subscriber() {
   const { register, isSubmitting, subscribe, errors } = useSubscribe();
   return (
     <form
@@ -16,7 +16,7 @@ export default function SubscriberForm() {
         disabled={isSubmitting}
         autoComplete="off"
         type="text"
-        className="block p-2 rounded-md  disabled:bg-thin-grey text-blue-accent font-semibold focus:outline-none focus:ring-2 focus:ring-white-grey focus:ring-opacity-50"
+        className="block p-2 rounded-md  disabled:bg-thin-grey text-blue-accent font-semibold focus:outline-none focus:ring-2 focus:ring-white-grey/50"
       />
       <ErrorMessage
         errors={errors}
