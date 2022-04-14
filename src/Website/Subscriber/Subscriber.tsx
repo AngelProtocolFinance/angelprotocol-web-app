bf051ae883f6ca04a36130a9b5716491ca89b50e
Nenad Misic <n.misic@outlook.com>
2022-04-14 09:34:40 +0200

    Align changes with RC-v1.6

--- src/Website/Subscriber/Subscriber.tsx
+++ src/Website/Subscriber/Subscriber.tsx
@@ -2,7 +2,7 @@ import { ErrorMessage } from "@hookform/error-message";
 import Loader from "components/Loader/Loader";
 import useSubscribe from "./useSubscribe";
 
-export default function SubscribeForm() {
+export default function SubscriberForm() {
   const { register, isSubmitting, subscribe, errors } = useSubscribe();
   return (
     <form
