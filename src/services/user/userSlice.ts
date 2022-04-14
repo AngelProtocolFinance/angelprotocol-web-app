87de5a7f8ecdf387729ee33b5b8fd7753d33a992
Nenad Misic <n.misic@outlook.com>
2022-04-14 12:04:36 +0200

    Update loadLocalStorageUser to get default user data if no localStorage version is available

--- src/services/user/userSlice.ts
+++ src/services/user/userSlice.ts
@@ -1,52 +1,17 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import getDefaultUser from "./getDefaultUser";
 import { User } from "./types";
-
-const initialState: User = {
-  ContactPerson: {
-    Email: "",
-    EmailVerified: false,
-    FirstName: "",
-    LastName: "",
-    PhoneNumber: "",
-    Role: "",
-    PK: "",
-  },
-  Registration: {
-    CharityName: "",
-    CharityName_ContactEmail: "",
-    RegistrationDate: "",
-    RegistrationStatus: "Not Complete",
-    Website: "",
-    UN_SDG: -1,
-    ProofOfIdentity: { name: "" },
-    ProofOfRegistration: { name: "" },
-    FinancialStatements: [],
-    AuditedFinancialReports: [],
-    ProofOfIdentityVerified: false,
-    ProofOfRegistrationVerified: false,
-    FinancialStatementsVerified: false,
-    AuditedFinancialReportsVerified: false,
-  },
-  Metadata: {
-    Banner: { name: "" },
-    CharityLogo: { name: "" },
-    CharityOverview: "",
-    TerraWallet: "",
-  },
-  token: "",
-};
 
 const userSlice = createSlice({
   name: "user",
-  initialState,
+  initialState: getDefaultUser(),
   reducers: {
     loadLocalStorageUser: (state) => {
-      const localUserData: User = JSON.parse(
-        localStorage.getItem("userData") || "{}"
-      );
-      return localUserData;
+      const localUser = localStorage.getItem("userData");
+      const user: User = !!localUser ? JSON.parse(localUser) : getDefaultUser();
+      return user;
     },
-    removeUserData: (state) => initialState,
+    removeUserData: (state) => getDefaultUser(),
     updateUserData: (state, { payload }: PayloadAction<User>) => payload,
   },
 });
