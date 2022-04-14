b4595dc8a563b9949e748e6afb670f02772ade16
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-04-14 17:31:46 +0100

    refactor files

--- src/pages/Admin/Proposals/Proposal.tsx
+++ src/pages/Admin/Proposals/Proposal.tsx
@@ -1,9 +1,5 @@
 import { useParams } from "react-router-dom";
-import {
-  useProposal,
-  useVoteList,
-  VOTES_PER_PAGE,
-} from "services/terra/admin/queriers";
+import { useProposal } from "services/terra/admin/queriers";
 import useProposalDetails, { ProposalDetails } from "./useProposalDetails";
 import PollAction from "./PollAction";
 import Status from "./Status";
@@ -11,8 +7,7 @@ import VoteStat from "./VoteStat";
 import Icon from "components/Icons/Icons";
 import DetailLabel from "./DetailLabel";
 import ProposalContent from "./ProposalContent/ProposalContent";
-import TableSection, { Cells } from "components/TableSection/TableSection";
-import { useState } from "react";
+import VotesTable from "./Votes";
 
 export type ProposalIdParam = { id: string };
 export default function Proposal() {
@@ -50,7 +45,7 @@ export default function Proposal() {
           </span>
         </h4>
         <Votes {...proposalDetails} />
-        {proposalDetails.votes?.length > 0 && (
+        {proposalDetails.votes.length > 0 && (
           <VotesTable proposalId={proposal.id} />
         )}
       </div>
@@ -79,66 +74,6 @@ function Votes(props: ProposalDetails) {
         pct={props.pctNotYet}
         textColor="text-white/80"
       />
-    </div>
-  );
-}
-
-function VotesTable(props: { proposalId: number | number }) {
-  const [pageNum, setPageNum] = useState(0);
-  const { votes, isVoteListLoading } = useVoteList(props.proposalId, pageNum);
-
-  const loadMoreVotes = () => setPageNum(pageNum + 1);
-  const isLoadMoreShown = votes.length >= VOTES_PER_PAGE;
-  return (
-    <div>
-      <table className="mt-4 w-full text-white/80 mt-4 overflow-hidden">
-        <TableSection
-          type="thead"
-          rowClass="sm:visible invisible  sm:flex sm:inline-block mb-2"
-        >
-          <Cells
-            type="th"
-            cellClass="px-2 first:pl-0 last:pr-0 text-left flex-1"
-          >
-            <>Addresses</>
-            <>Vote</>
-          </Cells>
-        </TableSection>
-        <TableSection
-          type="tbody"
-          rowClass="border-white/10 hover:bg-angel-blue hover:bg-angel-blue/10 mb-6 sm:mb-0 flex flex-row flex-wrap sm:flex-no-wrap"
-        >
-          {votes.map((vote, i) => (
-            <Cells
-              type="td"
-              cellClass="p-2 first:pl-0 last:pr-0 group pl-4 pt-8 sm:pt-2 pb-2 text-left relative w-full border-t border-l border-b border-r sm:flex-1"
-              key={i}
-            >
-              <p className="pl-2">{vote.voter}</p>
-              <p
-                className={`pl-2 ${
-                  vote.vote === "yes" ? "text-bright-green" : "text-failed-red"
-                }`}
-              >
-                {vote.vote}
-              </p>
-            </Cells>
-          ))}
-        </TableSection>
-      </table>
-      {isLoadMoreShown && (
-        <button
-          disabled={isVoteListLoading}
-          className="mt-3 px-3 py-1 justify-self-center text-white/80 text-xs bg-angel-blue/80 disabled:bg-grey-accent uppecase font-heading uppercase rounded-sm"
-          onClick={loadMoreVotes}
-        >
-          {isVoteListLoading ? (
-            <Icon type="Loading" className="animate-spin" size={18} />
-          ) : (
-            "more"
-          )}
-        </button>
-      )}
     </div>
   );
 }
