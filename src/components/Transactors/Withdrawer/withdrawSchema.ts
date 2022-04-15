5b6358f7e7335c9ddc8668def8b0decb9cfd181f
ap-justin <justin@angelprotocol.io>
2022-04-15 16:09:03 +0800

    used common schema

--- src/components/Transactors/Withdrawer/withdrawSchema.ts
+++ src/components/Transactors/Withdrawer/withdrawSchema.ts
@@ -1,38 +1,15 @@
 import * as Yup from "yup";
 import { VaultFieldIds } from "services/terra/multicall/types";
+import { tokenAmount } from "schemas/number";
+import { requiredAddress } from "schemas/string";
 import { SchemaShape } from "types/schema";
 import { WithdrawValues } from "./types";
-
-export const tokenAmount = Yup.lazy((value) =>
-  value === ""
-    ? Yup.string()
-    : Yup.number()
-        .positive("invalid: must be greater than zero ")
-        .typeError("invalid: must be a number")
-        .test(
-          "max precision",
-          "must not be greater than 6 digits",
-          testTokenDigits
-        )
-);
-
-const addressSchema = Yup.string()
-  .required("address is required")
-  .test("is valid", "address format is invalid", testAddress);
 
 const withdrawShape: SchemaShape<WithdrawValues> = {
   [VaultFieldIds.anchor1_amount]: tokenAmount,
   [VaultFieldIds.anchor2_amount]: tokenAmount,
-  beneficiary: addressSchema,
+  beneficiary: requiredAddress("beneficiary"),
   //add other vault fields here
 };
 
 export const withdrawSchema = Yup.object(withdrawShape);
-
-function testAddress(address?: string): boolean {
-  return !address || /^terra[a-z0-9]{39}$/i.test(address);
-}
-
-function testTokenDigits(tokenAmount?: number): boolean {
-  return !tokenAmount || /^\d+(\.\d{1,6})?$/.test(`${tokenAmount}`);
-}
