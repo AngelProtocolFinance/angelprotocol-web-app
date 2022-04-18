56a737dfc710f490e73f3728ca7489b80b159b37
ap-justin <justin@angelprotocol.io>
2022-04-18 21:41:53 +0800

    selection modal

--- src/hooks/useTransak.ts
+++ src/hooks/useTransak.ts
@@ -8,32 +8,23 @@ const apiKey = isDevelopment
   : process.env.REACT_APP_TRANSAK_API_KEY_PRODUCTION;
 
 const env = isDevelopment ? "STAGING" : "PRODUCTION";
+let transak = new transakSDK({
+  apiKey: apiKey, // Your API Key (Required)
+  environment: env, // STAGING/PRODUCTION (Required)
+  cryptoCurrencyCode: currency_text[denoms.uusd],
+  network: "terra",
+  walletAddress: ap_wallets[denoms.uusd],
+  hostURL: window.location.origin,
+  widgetHeight: "550px",
+  widgetWidth: "450px",
+  isFeeCalculationHidden: true,
+});
 
 export default function useTransak() {
   function initTransak() {
-    let transak = new transakSDK({
-      apiKey: apiKey, // Your API Key (Required)
-      environment: env, // STAGING/PRODUCTION (Required)
-      cryptoCurrencyCode: currency_text[denoms.uusd],
-      network: "terra",
-      walletAddress: ap_wallets[denoms.uusd],
-      hostURL: window.location.origin,
-      widgetHeight: "550px",
-      widgetWidth: "450px",
-    });
-
-    console.log(transak);
     transak.init();
-    // To get all the events
-    transak.on(transak.ALL_EVENTS, () => {});
 
-    // This will trigger when the user closed the widget
     transak.on(transak.EVENTS.TRANSAK_WIDGET_CLOSE, () => {
-      transak.close();
-    });
-
-    // This will trigger when the user marks payment is made.
-    transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, () => {
       transak.close();
     });
   }
--- src/pages/Charity/CharityHeader/CharityHeader.tsx
+++ src/pages/Charity/CharityHeader/CharityHeader.tsx
@@ -3,14 +3,15 @@ import { unsdgs } from "constants/unsdgs";
 import { Profile } from "services/aws/endowments/types";
 import CharityLinks from "./CharityLinks";
 import useTransak from "hooks/useTransak";
+import { useSetModal } from "components/Modal/Modal";
+import React from "react";
 
 export default function CharityHeader(props: Profile) {
-  const showDonater = useDonater({
-    to: "charity",
-    receiver: props.endowment_address!,
-  });
+  const { showModal } = useSetModal();
+  function showDonateSelection() {
+    showModal(DonateSelection, { endowmentAddr: props.endowment_address! });
+  }
   const sdg = unsdgs[+props.un_sdg];
-  const { initTransak } = useTransak();
 
   return (
     <div className="flex flex-col items-start gap-2">
@@ -27,15 +28,44 @@ export default function CharityHeader(props: Profile) {
       </h3>
 
       <div className="flex items-center gap-2 flex-wrap">
-        <button
-          disabled={props.is_placeholder}
-          onClick={initTransak}
-          className="disabled:bg-grey-accent uppercase bg-orange hover:bg-angel-orange font-heading text-white font-semibold rounded-xl px-6 py-3"
-        >
+        <Button disabled={props.is_placeholder} onClick={showDonateSelection}>
           DONATE NOW
-        </button>
+        </Button>
         <CharityLinks />
       </div>
     </div>
+  );
+}
+
+function DonateSelection(props: { endowmentAddr: string }) {
+  const { hideModal } = useSetModal();
+  const { initTransak } = useTransak();
+  const showDonater = useDonater({
+    to: "charity",
+    receiver: props.endowmentAddr,
+  });
+
+  function showTransak() {
+    hideModal();
+    initTransak();
+  }
+
+  return (
+    <div className="grid justify-items-center gap-4 w-full max-w-md min-h-[10rem] bg-white rounded-md p-4">
+      <h2 className="text-xl font-heading font-semibold">
+        How do you want to donate?
+      </h2>
+      <Button onClick={showDonater}>Cryptocurrency</Button>
+      <Button onClick={showTransak}>Fiat (USD, EUR, etc)</Button>
+    </div>
+  );
+}
+
+function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className="disabled:bg-grey-accent uppercase bg-orange hover:bg-angel-orange font-heading text-white font-semibold rounded-xl px-6 py-3"
+    />
   );
 }
