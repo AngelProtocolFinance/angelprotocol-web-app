96773f4ab86373be4d76d0466d7415e7e452f3a8
ap-justin <justin@angelprotocol.io>
2022-04-18 19:39:54 +0800

    test lint fixes

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,19 +1,22 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { act } from "react-dom/test-utils";
 import { MdOutlineClose } from "react-icons/md";
 import Modal, { useSetModal } from "./Modal";
 
 const modalCssClass =
   "ap-modal bg-black/50 fixed top-0 right-0 bottom-0 left-0 z-50 grid place-items-center";
 
+function MockModal() {
+  return (
+    <Modal classes={modalCssClass}>
+      <TriggerModal />
+    </Modal>
+  );
+}
+
 describe("<Modal/> renders correctly", () => {
   test("<Modal /> is triggered and opened", async () => {
-    render(
-      <Modal classes={modalCssClass}>
-        <TriggerModal />
-      </Modal>
-    );
+    render(<MockModal />);
 
     // find tigger modal button
     const button = await screen.findByText(/show modal/i);
@@ -55,11 +58,7 @@ describe("<Modal/> is Dismissed", () => {
   });
 
   test("<Modal /> is closed on backdrop dismiss click", async () => {
-    render(
-      <Modal classes={modalCssClass}>
-        <TriggerModal />
-      </Modal>
-    );
+    render(<MockModal />);
 
     // find tigger modal button
     const button = await screen.findByText(/show modal/i);
@@ -72,9 +71,7 @@ describe("<Modal/> is Dismissed", () => {
 
     // click on the modal wrapper to close the modal
     const wrapper = await screen.findByRole("alertdialog");
-    act(() => {
-      userEvent.click(wrapper);
-    });
+    userEvent.click(wrapper);
     expect(modalContent).not.toBeInTheDocument();
   });
 });
