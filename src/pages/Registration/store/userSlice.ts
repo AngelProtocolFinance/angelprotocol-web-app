0b8f7c4f4e9c65a5394b62ddfd64401ebc478481
Nenad Misic <n.misic@outlook.com>
2022-04-18 11:11:29 +0200

    Move getDefaultUser method to top of file

--- src/pages/Registration/store/userSlice.ts
+++ src/pages/Registration/store/userSlice.ts
@@ -3,31 +3,6 @@ import { User } from "./types";
 
 const STORAGE_KEY = "user";
 
-const loadUser = (): User => {
-  const localUser = localStorage.getItem(STORAGE_KEY);
-  return !!localUser ? (JSON.parse(localUser) as User) : getDefaultUser();
-};
-
-const userSlice = createSlice({
-  name: "user",
-  initialState: loadUser(),
-  reducers: {
-    removeUser: (state) => {
-      localStorage.removeItem(STORAGE_KEY);
-      return getDefaultUser();
-    },
-    updateUser: (state, { payload }: PayloadAction<User>) => {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(payload));
-      return payload;
-    },
-  },
-});
-
-const userReducer = userSlice.reducer;
-
-export { userReducer };
-export const { removeUser, updateUser } = userSlice.actions;
-
 const getDefaultUser = (): User => ({
   ContactPerson: {
     Email: "",
@@ -62,3 +37,28 @@ const getDefaultUser = (): User => ({
   },
   token: "",
 });
+
+const loadUser = (): User => {
+  const localUser = localStorage.getItem(STORAGE_KEY);
+  return !!localUser ? (JSON.parse(localUser) as User) : getDefaultUser();
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: loadUser(),
+  reducers: {
+    removeUser: (state) => {
+      localStorage.removeItem(STORAGE_KEY);
+      return getDefaultUser();
+    },
+    updateUser: (state, { payload }: PayloadAction<User>) => {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(payload));
+      return payload;
+    },
+  },
+});
+
+const userReducer = userSlice.reducer;
+
+export { userReducer };
+export const { removeUser, updateUser } = userSlice.actions;
