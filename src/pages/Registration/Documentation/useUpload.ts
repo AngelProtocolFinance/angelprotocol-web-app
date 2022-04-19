ae0450edbbc6e06523945e802858c0d4e4e89fa4
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-04-20 00:44:26 +0100

    clean up

--- src/pages/Registration/Documentation/useUpload.ts
+++ src/pages/Registration/Documentation/useUpload.ts
@@ -2,8 +2,7 @@ import { SerializedError } from "@reduxjs/toolkit";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
 import { useCallback } from "react";
 import { useUpdateDocumentationMutation } from "services/aws/registration";
-import { FileObject, UpdateDocumentationResult } from "services/aws/types";
-import { FileWrapper } from "components/FileDropzone/types";
+import { UpdateDocumentationResult } from "services/aws/types";
 import { useGetter, useSetter } from "store/accessors";
 import uploadToIpfs from "helpers/uploadToIpfs";
 import { updateCharity } from "../store";
@@ -79,49 +78,3 @@ async function getUploadUrls(values: FormValues) {
     AuditedFinancialReports,
   };
 }
-
-async function getUploadBody(values: FormValues) {
-  const poiPromise = readFileToDataUrl(values.proofOfIdentity);
-  const porPromise = readFileToDataUrl(values.proofOfRegistration);
-  const fsPromise = Promise.all(
-    values.financialStatements.map((x) => readFileToDataUrl(x))
-  );
-  const afrPromise = Promise.all(
-    values.auditedFinancialReports.map((x) => readFileToDataUrl(x))
-  );
-
-  const [
-    ProofOfIdentity,
-    ProofOfRegistration,
-    FinancialStatements,
-    AuditedFinancialReports,
-  ] = await Promise.all([poiPromise, porPromise, fsPromise, afrPromise]);
-
-  return {
-    Website: values.website,
-    UN_SDG: values.un_sdg,
-    ProofOfIdentity,
-    ProofOfRegistration,
-    FinancialStatements,
-    AuditedFinancialReports,
-  };
-}
-
-const readFileToDataUrl = (fileWrapper: FileWrapper) =>
-  new Promise<FileObject>((resolve, reject) => {
-    if (!fileWrapper.file) {
-      return resolve({
-        name: fileWrapper.name,
-        sourceUrl: fileWrapper.sourceUrl,
-      });
-    }
-
-    const reader = new FileReader();
-    reader.onerror = (error) => reject(error);
-    reader.onload = (e: ProgressEvent<FileReader>) =>
-      resolve({
-        name: fileWrapper.name,
-        dataUrl: e.target!.result as string,
-      });
-    reader.readAsDataURL(fileWrapper.file as Blob);
-  });
