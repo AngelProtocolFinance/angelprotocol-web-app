768ff56ae3d9906f1a09662afecd8d601e5c51c1
ap-justin <89639563+ap-justin@users.noreply.github.com>
2022-04-25 11:35:33 +0800

    FIX: marketplace cards not showing (#952)
    * remove skip and minor nits
    
    * script name

--- src/services/terra/registrar/registrar.ts
+++ src/services/terra/registrar/registrar.ts
@@ -30,21 +30,17 @@ export const registrar_api = terra.injectEndpoints({
       providesTags: [{ type: tags.endowment }],
       query: contract_querier,
       transformResponse: (res: QueryRes<EndowmentListRes>) => {
-        let result = res.query_result.endowments.reduce(
-          (result: CategorizedEndowments, profile: EndowmentEntry) => {
-            if (!profile.un_sdg || profile.tier === "Level1") {
-              return result;
-            } else {
-              if (!result[profile.un_sdg]) {
-                result[profile.un_sdg] = [];
-              }
-              result[+profile.un_sdg!].push(profile);
-              return result;
+        return res.query_result.endowments.reduce((result, profile) => {
+          if (!profile.un_sdg || profile.tier === "Level1") {
+            return result;
+          } else {
+            if (!result[profile.un_sdg]) {
+              result[profile.un_sdg] = [];
             }
-          },
-          {} as CategorizedEndowments
-        );
-        return result;
+            result[+profile.un_sdg!].push(profile);
+            return result;
+          }
+        }, {} as CategorizedEndowments);
       },
     }),
   }),
