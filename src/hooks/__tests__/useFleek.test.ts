1b4597aed36c7501e678b712737ae7802fad6aa4
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-04-25 04:28:38 +0100

    refactor: remove asyn test

--- src/hooks/__tests__/useFleek.test.ts
+++ src/hooks/__tests__/useFleek.test.ts
@@ -1,30 +1,12 @@
 import { renderHook } from "@testing-library/react-hooks";
-import { act } from "react-dom/test-utils";
 import useFleek from "hooks/useFleek";
 
 jest.useFakeTimers("legacy");
 describe("useFleek Test", () => {
-  beforeEach((): void => {
-    jest.setTimeout(30000);
-  });
-
-  it("should render correctly", async () => {
+  it("should initialize correctly", async () => {
     const { result } = renderHook(() => useFleek());
     expect(result.current.isUploading).toBe(false);
     expect(result.current.upload).toBeDefined();
     expect(typeof result.current.upload).toBe("function");
-  });
-
-  it("uploads a file and returns ipfs url", async () => {
-    const { result } = renderHook(() => useFleek());
-    let uploaded: string | undefined;
-
-    await act(async () => {
-      uploaded = await result.current.upload("file", "string data");
-    });
-
-    expect(typeof uploaded).toBe("string");
-    const isFleekUrl = uploaded?.startsWith("https://storageapi.fleek.co");
-    expect(isFleekUrl).toBe(true);
   });
 });
