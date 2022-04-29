b7748d9028cc5b4734b6f1365549b5ed00b49b98
Nenad Misic <n.misic@outlook.com>
2022-04-29 18:42:22 +0200

    Move useSubmit to separate folder

--- src/pages/Registration/Dashboard/useSubmit/useSubmit.ts
+++ src/pages/Registration/Dashboard/useSubmit/useSubmit.ts
@@ -10,16 +10,14 @@ import {
   setStage,
 } from "services/transaction/transactionSlice";
 import { Stage, Step } from "services/transaction/types";
-import { WalletProxy } from "providers/WalletProvider";
 import { useModalContext } from "components/ModalContext/ModalContext";
 import Popup from "components/Popup/Popup";
 import TransactionPrompt from "components/TransactionStatus/TransactionPrompt";
 import { useGetter, useSetter } from "store/accessors";
-import Registrar from "contracts/Registrar";
-import { RegistrarCreateEndowmentPayload as RegistrarEndowmentCreationPayload } from "contracts/types";
 import useWalletContext from "hooks/useWalletContext";
 import processEstimateError from "helpers/processEstimateError";
-import { updateCharity } from "../store";
+import { updateCharity } from "../../store";
+import createEndowmentCreationMsg from "./createEndowmentCreationMsg";
 
 type Result =
   | { data: SubmitResult }
@@ -131,49 +129,6 @@ export default function useSubmit() {
   );
 
   return { submit, isSubmitting: form_loading };
-}
-
-function createEndowmentCreationMsg(charity: Charity, wallet?: WalletProxy) {
-  const payload = createMessagePayload(charity);
-  const contract = new Registrar(wallet);
-  return contract.createEndowmentCreationMsg(payload);
-}
-
-function createMessagePayload(
-  charity: Charity
-): RegistrarEndowmentCreationPayload {
-  return {
-    beneficiary: charity.Metadata.TerraWallet,
-    cw4_members: [],
-    guardians_multisig_addr: undefined,
-    maturity_height: undefined,
-    maturity_time: undefined,
-    owner: charity.Metadata.TerraWallet,
-    profile: {
-      annual_revenue: undefined,
-      average_annual_budget: undefined,
-      charity_navigator_rating: undefined,
-      contact_email: charity.ContactPerson.Email,
-      country_of_origin: undefined,
-      endow_type: "Charity",
-      image: charity.Metadata.Banner.publicUrl!,
-      logo: charity.Metadata.CharityLogo.publicUrl!,
-      name: charity.Registration.CharityName,
-      number_of_employees: undefined,
-      overview: charity.Metadata.CharityOverview,
-      registration_number: undefined,
-      social_media_urls: {
-        facebook: undefined,
-        linkedin: undefined,
-        twitter: undefined,
-      },
-      street_address: undefined,
-      tier: charity.Registration.Tier!,
-      un_sdg: charity.Registration.UN_SDG,
-      url: charity.Registration.Website,
-    },
-    withdraw_before_maturity: false,
-  };
 }
 
 function getEndowmentContract(stage: Stage) {
