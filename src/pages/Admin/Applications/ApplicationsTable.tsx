1c12361b908804b5890d1684d09bc9d843c22937
shadrach-tayo <shadrachtemitayo@gmail.com>
2022-05-02 11:44:19 +0100

    Bring in-line with rest of admin area styling

--- src/pages/Admin/Applications/ApplicationsTable.tsx
+++ src/pages/Admin/Applications/ApplicationsTable.tsx
@@ -30,35 +30,35 @@ export default function ApplicationsTable(props: {
       </TableSection>
       <TableSection
         type="tbody"
-        rowClass="border-white hover:bg-angel-blue hover:bg-angel-blue/10 mb-6 sm:mb-0 flex flex-row flex-wrap sm:flex-no-wrap"
+        rowClass="hover:bg-angel-blue hover:bg-angel-blue/10 mb-6 sm:mb-0 flex flex-row flex-wrap sm:flex-no-wrap"
       >
         {props.applications.map((ap) => (
           <Cells
             type="td"
-            cellClass="p-2 first:pl-0 last:pr-0 group pl-4 pt-8 sm:pt-2 pb-2 text-left relative w-full border-t border-l border-b border-r sm:flex-1"
+            cellClass="p-2 first:pl-0 last:pr-0 group pl-4 pt-8 sm:pt-2 pb-2 text-left relative w-full border-grey-accent/30 border-t border-l border-b border-r sm:flex-1"
             key={ap.PK}
           >
             <p className="pl-1 capitalize w-42 sm:w-32">
               {" "}
-              <span className="font-bold font-thin text-xs text-gray-700 uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200 pl-2">
+              <span className="font-bold font-thin text-xs text-white uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200/20 pl-2">
                 Name
               </span>
               {ap.CharityName}
             </p>
             <p className="text-base font-bold w-42 sm:w-32 truncate">
-              <span className="font-bold font-thin text-xs text-gray-700 uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200 pl-2">
+              <span className="font-bold font-thin text-xs text-white uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200/20 pl-2">
                 Contact Email
               </span>
               {ap.CharityName_ContactEmail}
             </p>
             <p className="text-base w-42 sm:w-32">
-              <span className="font-bold font-thin text-xs text-gray-700 uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200 pl-2">
+              <span className="font-bold font-thin text-xs text-white uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200/20 pl-2">
                 Registration Date
               </span>
               {new Date(ap.RegistrationDate).toDateString()}
             </p>
             <p className="font-mono flex justify-between items-center w-42 sm:w-32">
-              <span className="font-bold font-thin text-xs text-gray-700 uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200 pl-2">
+              <span className="font-bold font-thin text-xs text-white uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200/20 pl-2">
                 Wallet Address
               </span>
               <span className="text-base truncate w-22">{ap.TerraWallet}</span>
@@ -66,7 +66,7 @@ export default function ApplicationsTable(props: {
             </p>
             {/* <Status label={ap.RegistrationStatus} /> */}
             <div className="w-42 sm:w-32">
-              <span className="font-bold font-thin text-xs text-gray-700 uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200 pl-2">
+              <span className="font-bold font-thin text-xs text-white uppercase sm:hidden absolute top-0 inset-x-0 p-1 bg-gray-200/20 pl-2">
                 Click to review
               </span>
               <button
