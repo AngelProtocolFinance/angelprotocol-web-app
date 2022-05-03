8b7f1591f70ce745a4247da8e0ef98c3b31080a2
Nenad Misic <n.misic@outlook.com>
2022-05-03 09:45:18 +0200

    Refactore useRegistration to useResume

---
+++ src/pages/Registration/useResume.ts
@@ -0,0 +1,45 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCheckPreviousRegistrationMutation } from "services/aws/registration";
+import { useModalContext } from "components/ModalContext/ModalContext";
+import Popup, { PopupProps } from "components/Popup/Popup";
+import { useSetter } from "store/accessors";
+import { app, site } from "constants/routes";
+import { createInitializedCharity } from "./helpers";
+import routes from "./routes";
+import { updateCharity } from "./store";
+
+export default function useResume() {
+  const [checkData] = useCheckPreviousRegistrationMutation();
+  const dispatch = useSetter();
+  const navigate = useNavigate();
+  const { showModal } = useModalContext();
+
+  const resume = useCallback(
+    async (values: { refer: string }) => {
+      const result = await checkData(values.refer);
+
+      if ("error" in result) {
+        console.log(result.error);
+        return showModal<PopupProps>(Popup, {
+          message:
+            "No active charity application found with this registration reference",
+        });
+      }
+
+      const charity = createInitializedCharity(result.data);
+      dispatch(updateCharity(charity));
+
+      if (charity.ContactPerson.EmailVerified) {
+        navigate(`${site.app}/${app.register}/${routes.dashboard}`);
+      } else {
+        navigate(`${site.app}/${app.register}/${routes.confirm}`, {
+          state: { is_sent: true },
+        });
+      }
+    },
+    [checkData, showModal, dispatch, navigate]
+  );
+
+  return resume;
+}
