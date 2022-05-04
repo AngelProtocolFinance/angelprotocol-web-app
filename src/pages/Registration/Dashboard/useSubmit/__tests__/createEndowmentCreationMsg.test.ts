c02a209a33e5ef4cf91e6e7999edae2478e9933d
Nenad Misic <n.misic@outlook.com>
2022-05-04 11:33:52 +0200

    Fix createEndowmentCreationMsg test

--- src/pages/Registration/Dashboard/useSubmit/__tests__/createEndowmentCreationMsg.test.ts
+++ src/pages/Registration/Dashboard/useSubmit/__tests__/createEndowmentCreationMsg.test.ts
@@ -3,42 +3,49 @@ import { Charity } from "services/aws/types";
 import { WalletProxy } from "providers/WalletProvider";
 import { chainOptions } from "providers/WalletProvider/chainOptions";
 import { TORUS_CONNECTION } from "providers/WalletProvider/useWalletContext/types";
+import { chainIDs } from "constants/chainIDs";
+import { contracts } from "constants/contracts";
+import { sc } from "constants/sc";
 import createEndowmentCreationMsg from "../createEndowmentCreationMsg";
 
 it("should return payload", () => {
   const payload = createEndowmentCreationMsg(charity, walletProxy);
 
-  expect(payload).toBe({
-    beneficiary: "terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
-    cw4_members: [],
-    guardians_multisig_addr: undefined,
-    maturity_height: undefined,
-    maturity_time: undefined,
-    owner: "terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
-    profile: {
-      annual_revenue: undefined,
-      average_annual_budget: undefined,
-      charity_navigator_rating: undefined,
-      contact_email: "test@test.com",
-      country_of_origin: undefined,
-      endow_type: "Charity",
-      image: "https://www.storage.path/banner",
-      logo: "https://www.storage.path/logo",
-      name: "charity",
-      number_of_employees: undefined,
-      overview: "some overview",
-      registration_number: undefined,
-      social_media_urls: {
-        facebook: undefined,
-        linkedin: undefined,
-        twitter: undefined,
+  expect(payload.contract).toBe(contracts[chainIDs.testnet][sc.registrar]);
+  expect(payload.sender).toBe("terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek");
+  expect(payload.execute_msg).toStrictEqual({
+    create_endowment: {
+      beneficiary: "terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
+      cw4_members: [],
+      guardians_multisig_addr: undefined,
+      maturity_height: undefined,
+      maturity_time: undefined,
+      owner: "terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
+      profile: {
+        annual_revenue: undefined,
+        average_annual_budget: undefined,
+        charity_navigator_rating: undefined,
+        contact_email: "test@test.com",
+        country_of_origin: undefined,
+        endow_type: "Charity",
+        image: "https://www.storage.path/banner",
+        logo: "https://www.storage.path/logo",
+        name: "charity",
+        number_of_employees: undefined,
+        overview: "some overview",
+        registration_number: undefined,
+        social_media_urls: {
+          facebook: undefined,
+          linkedin: undefined,
+          twitter: undefined,
+        },
+        street_address: undefined,
+        tier: 1,
+        un_sdg: 0,
+        url: "www.test.com",
       },
-      street_address: undefined,
-      tier: 1,
-      un_sdg: 0,
-      url: "www.test.com",
+      withdraw_before_maturity: false,
     },
-    withdraw_before_maturity: false,
   });
 });
 
@@ -83,7 +90,7 @@ const charity: Charity = {
 
 const walletProxy: WalletProxy = {
   connection: TORUS_CONNECTION,
-  address: "terra1ke4aktw6zvz2jxsyqx55ejsj7rmxdl9p5xywus",
+  address: "terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
   network: chainOptions.walletConnectChainIds[0], // testnet
   post: async (_: CreateTxOptions) => ({
     result: {
