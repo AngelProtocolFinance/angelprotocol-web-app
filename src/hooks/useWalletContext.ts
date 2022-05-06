cef62b861560c093774ecd4874a776d0010c6c42
Nenad Misic <nenad@angelprotocol.io>
2022-05-06 09:59:46 +0200

    Minor fixes (#977)
    * Refactor importOrder in .prettierrc.json
    
    * Refactor App.test to use WalletProvider
    
    * Refactor WalletSuite.hideConnectOptions to check if connected first

--- src/hooks/useWalletContext.ts
+++ src/hooks/useWalletContext.ts
@@ -1,5 +1,5 @@
-import { WalletContext } from "providers";
 import { useContext } from "react";
+import { WalletContext } from "providers";
 
 export default function useWalletContext() {
   return useContext(WalletContext);
