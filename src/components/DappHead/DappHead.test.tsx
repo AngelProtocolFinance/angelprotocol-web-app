87b6b1f3a570562095926e5ee3aaf64914cfb387
ap-justin <justin@angelprotocol.io>
2022-05-06 20:47:02 +0800

    include lazy loading

--- src/components/DappHead/DappHead.test.tsx
+++ src/components/DappHead/DappHead.test.tsx
@@ -31,34 +31,70 @@ describe("DappHead test", () => {
   test("dapphead routing", async () => {
     render(<TestApp />);
 
-    //get links
-    const leaderboardLink = screen.getByText(/leaderboard/i);
-    const marketPlaceLink = screen.getByText(/marketplace/i);
-    const govLink = screen.getByText(/governance/i);
+    const govText1 = /total staked/i;
+    const govText2 = /halo price/i;
 
-    //marketplace --> governance
+    const marketText1 = /angel protocol supports/i;
+    const marketText2 = /displaced ukrainians/i;
+
+    const leaderboardText1 = /total donations/i;
+    const leaderboardText2 = /daily donations total/i;
+
+    /** user starts at marketplace */
+    const loader = screen.getByTestId("loader");
+    expect(loader).toBeInTheDocument();
+
+    //view is not yet rendered and being lazy loaded
+    expect(screen.queryByText(marketText1)).toBeNull();
+    expect(screen.queryByText(marketText2)).toBeNull();
+
+    //view is finally loaded,
+    expect(await screen.findByText(marketText1)).toBeInTheDocument();
+    expect(await screen.findByText(marketText2)).toBeInTheDocument();
+    expect(loader).not.toBeInTheDocument();
+
+    //user goes to governance
+    const govLink = screen.getByText(/governance/i);
     userEvent.click(govLink);
 
-    //page is lazy loaded, test details in `pages/Governance.test.tsx`
-    expect(await screen.findByText(/total staked/i)).toBeInTheDocument();
-    expect(await screen.findByText(/halo price/i)).toBeInTheDocument();
+    /** user is in governance */
+    const loader2 = screen.getByTestId("loader");
+    expect(loader2).toBeInTheDocument();
 
-    //governance --> leaderboard
+    //view is not yet rendered and being lazy loaded
+    expect(screen.queryByText(govText1)).toBeNull();
+    expect(screen.queryByText(govText2)).toBeNull();
+
+    //view is finally loaded,
+    expect(await screen.findByText(govText1)).toBeInTheDocument();
+    expect(await screen.findByText(govText2)).toBeInTheDocument();
+    expect(loader2).not.toBeInTheDocument();
+
+    //user goes to leaderboard
+    const leaderboardLink = screen.getByText(/leaderboard/i);
     userEvent.click(leaderboardLink);
-    //page is lazy loaded, test details in `pages/Leaderboard.test.tsx`
-    expect(await screen.findByText(/total donations/i)).toBeInTheDocument();
-    expect(
-      await screen.findByText(/daily donations total/i)
-    ).toBeInTheDocument();
 
-    //leaderboard --> marketplace
+    /** user is in leaderboard */
+    const loader3 = screen.getByTestId("loader");
+    expect(loader3).toBeInTheDocument();
+
+    //view is not yet rendered and being lazy loaded
+    expect(screen.queryByText(leaderboardText1)).toBeNull();
+    expect(screen.queryByText(leaderboardText2)).toBeNull();
+
+    //view is finally loaded,
+    expect(await screen.findByText(leaderboardText1)).toBeInTheDocument();
+    expect(await screen.findByText(leaderboardText2)).toBeInTheDocument();
+    expect(loader3).not.toBeInTheDocument();
+
+    //user goes back to marketplace
+    const marketPlaceLink = screen.getByText(/marketplace/i);
     userEvent.click(marketPlaceLink);
-    //page is lazy loaded, test details in `pages/Market.test.tsx`
-    expect(
-      await screen.findByText(/angel protocol supports/i)
-    ).toBeInTheDocument();
-    expect(
-      await screen.findByText(/displaced ukrainians/i)
-    ).toBeInTheDocument();
+
+    /**user is back to marketplace */
+
+    //view is loaded immediately since it's been loaded before
+    expect(screen.getByText(marketText1)).toBeInTheDocument();
+    expect(screen.getByText(marketText2)).toBeInTheDocument();
   });
 });
