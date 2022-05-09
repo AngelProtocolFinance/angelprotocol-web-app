8c1980c2e0a7d8bb4394c4f766373c0244279ba3
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2022-05-09 02:04:34 +0100

    Bring in-line with rest of admin area styling (#967)
    * Bring in-line with rest of admin area styling
    
    * refactor table component and create a separate listview for smaller screens
    
    * UI enhancement

--- src/pages/Admin/Applications/Applications.tsx
+++ src/pages/Admin/Applications/Applications.tsx
@@ -10,15 +10,13 @@ export default function Applications() {
   } = useGetCharityApplicationsQuery(RegistrationStatus.InReview);
 
   return (
-    <div className="p-3 grid grid-rows-a1">
+    <div className="px-3 pb-3 grid grid-rows-a1 bg-white/10 shadow-inner rounded-md">
       {(data.length > 0 && (
-        <div className="scroll-hidden p-3 overflow-auto">
-          <h1 className="text-2xl text-white font-semibold">
+        <div className="scroll-hidden p-3 overflow-auto ">
+          <h1 className="text-2xl text-white font-semibold mb-10">
             Charity Applications
           </h1>
-          <div className="bg-white/10 shadow-inner rounded-md p-4 mt-4">
-            <ApplicationsTable applications={data} isError={isError} />
-          </div>
+          <ApplicationsTable applications={data} isError={isError} />
         </div>
       )) || (
         <p className="font-mono text-white place-self-center mt-20">
