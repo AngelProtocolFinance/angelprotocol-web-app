a3a72f63147768c86976a0254c3e058769bd1d13
Nenad Misic <n.misic@outlook.com>
2022-05-10 10:02:47 +0200

    Add getRegistrationState tests

---
+++ src/pages/Registration/Dashboard/__tests__/getRegistrationState.test.ts
@@ -0,0 +1,274 @@
+import { Charity, RegistrationStatus } from "services/aws/types";
+import getRegistrationState from "../getRegistrationState";
+
+describe("getRegistrationState tests", () => {
+  it("sets only stepOne to complete", () => {
+    const charity: Charity = {
+      ContactPerson: {
+        Email: "test@test.com",
+        EmailVerified: true,
+        FirstName: "first",
+        LastName: "last",
+        PhoneNumber: "+114323888",
+        Role: "ceo",
+        PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
+      },
+      Registration: {
+        CharityName: "charity",
+        CharityName_ContactEmail: "CHARITY_test@test.com",
+        RegistrationDate: "2022-05-04T10:10:10Z",
+        RegistrationStatus: "Inactive",
+        Website: "",
+        UN_SDG: 0,
+        ProofOfIdentity: { name: "" },
+        ProofOfRegistration: { name: "" },
+        Tier: undefined,
+        FinancialStatements: [],
+        AuditedFinancialReports: [],
+        ProofOfIdentityVerified: false,
+        ProofOfRegistrationVerified: false,
+        FinancialStatementsVerified: false,
+        AuditedFinancialReportsVerified: false,
+      },
+      Metadata: {
+        Banner: { name: "" },
+        CharityLogo: { name: "" },
+        CharityOverview: "",
+        EndowmentContract: "",
+        TerraWallet: "",
+      },
+    };
+
+    const state = getRegistrationState(charity);
+
+    expect(state.stepOne.completed).toBe(true);
+    expect(state.stepTwo.completed).toBe(false);
+    expect(state.stepThree.completed).toBe(false);
+    expect(state.stepFour.completed).toBe(false);
+    expect(state.getIsReadyForSubmit()).toBe(false);
+  });
+
+  it("sets only steps One and Two to complete", () => {
+    const charity: Charity = {
+      ContactPerson: {
+        Email: "test@test.com",
+        EmailVerified: true,
+        FirstName: "first",
+        LastName: "last",
+        PhoneNumber: "+114323888",
+        Role: "ceo",
+        PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
+      },
+      Registration: {
+        CharityName: "charity",
+        CharityName_ContactEmail: "CHARITY_test@test.com",
+        RegistrationDate: "2022-05-04T10:10:10Z",
+        RegistrationStatus: "Inactive",
+        Website: "",
+        UN_SDG: 0,
+        ProofOfIdentity: { name: "" },
+        ProofOfRegistration: { name: "" },
+        Tier: undefined,
+        FinancialStatements: [],
+        AuditedFinancialReports: [],
+        ProofOfIdentityVerified: false,
+        ProofOfRegistrationVerified: false,
+        FinancialStatementsVerified: false,
+        AuditedFinancialReportsVerified: false,
+      },
+      Metadata: {
+        Banner: { name: "" },
+        CharityLogo: { name: "" },
+        CharityOverview: "",
+        EndowmentContract: "",
+        TerraWallet: "terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
+      },
+    };
+
+    const state = getRegistrationState(charity);
+
+    expect(state.stepOne.completed).toBe(true);
+    expect(state.stepTwo.completed).toBe(true);
+    expect(state.stepThree.completed).toBe(false);
+    expect(state.stepFour.completed).toBe(false);
+    expect(state.getIsReadyForSubmit()).toBe(false);
+  });
+
+  it("sets only steps One and Three to complete", () => {
+    const charity: Charity = {
+      ContactPerson: {
+        Email: "test@test.com",
+        EmailVerified: true,
+        FirstName: "first",
+        LastName: "last",
+        PhoneNumber: "+114323888",
+        Role: "ceo",
+        PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
+      },
+      Registration: {
+        CharityName: "charity",
+        CharityName_ContactEmail: "CHARITY_test@test.com",
+        RegistrationDate: "2022-05-04T10:10:10Z",
+        RegistrationStatus: "Inactive",
+        Website: "www.test.com",
+        UN_SDG: 0,
+        ProofOfIdentity: {
+          name: "poi",
+          publicUrl: "https://www.storage.path/poi",
+        },
+        ProofOfRegistration: {
+          name: "por",
+          publicUrl: "https://www.storage.path/por",
+        },
+        Tier: 1,
+        FinancialStatements: [],
+        AuditedFinancialReports: [],
+        ProofOfIdentityVerified: false,
+        ProofOfRegistrationVerified: false,
+        FinancialStatementsVerified: false,
+        AuditedFinancialReportsVerified: false,
+      },
+      Metadata: {
+        Banner: { name: "" },
+        CharityLogo: { name: "" },
+        CharityOverview: "",
+        EndowmentContract: "",
+        TerraWallet: "",
+      },
+    };
+
+    const state = getRegistrationState(charity);
+
+    expect(state.stepOne.completed).toBe(true);
+    expect(state.stepTwo.completed).toBe(false);
+    expect(state.stepThree.completed).toBe(true);
+    expect(state.stepFour.completed).toBe(false);
+    expect(state.getIsReadyForSubmit()).toBe(false);
+  });
+
+  it("sets only steps One and Four to complete", () => {
+    const charity: Charity = {
+      ContactPerson: {
+        Email: "test@test.com",
+        EmailVerified: true,
+        FirstName: "first",
+        LastName: "last",
+        PhoneNumber: "+114323888",
+        Role: "ceo",
+        PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
+      },
+      Registration: {
+        CharityName: "charity",
+        CharityName_ContactEmail: "CHARITY_test@test.com",
+        RegistrationDate: "2022-05-04T10:10:10Z",
+        RegistrationStatus: "Inactive",
+        Website: "",
+        UN_SDG: 0,
+        ProofOfIdentity: { name: "" },
+        ProofOfRegistration: { name: "" },
+        Tier: undefined,
+        FinancialStatements: [],
+        AuditedFinancialReports: [],
+        ProofOfIdentityVerified: false,
+        ProofOfRegistrationVerified: false,
+        FinancialStatementsVerified: false,
+        AuditedFinancialReportsVerified: false,
+      },
+      Metadata: {
+        Banner: {
+          name: "banner",
+          publicUrl: "https://www.storage.path/banner",
+        },
+        CharityLogo: {
+          name: "logo",
+          publicUrl: "https://www.storage.path/logo",
+        },
+        CharityOverview: "some overview",
+        EndowmentContract: "",
+        TerraWallet: "",
+      },
+    };
+
+    const state = getRegistrationState(charity);
+
+    expect(state.stepOne.completed).toBe(true);
+    expect(state.stepTwo.completed).toBe(false);
+    expect(state.stepThree.completed).toBe(false);
+    expect(state.stepFour.completed).toBe(true);
+    expect(state.getIsReadyForSubmit()).toBe(false);
+  });
+
+  it("sets all steps to complete even when Inactive", () => {
+    runSetsAllStepsToCompleteEvenWhen("Inactive");
+  });
+
+  it("sets all steps to complete even when Under Review", () => {
+    runSetsAllStepsToCompleteEvenWhen("Under Review");
+  });
+
+  it("sets all steps to complete even when Approved", () => {
+    runSetsAllStepsToCompleteEvenWhen("Approved");
+  });
+
+  it("sets all steps to complete even when Active", () => {
+    runSetsAllStepsToCompleteEvenWhen("Active");
+  });
+
+  function runSetsAllStepsToCompleteEvenWhen(status: RegistrationStatus) {
+    const charity: Charity = {
+      ContactPerson: {
+        Email: "test@test.com",
+        EmailVerified: true,
+        FirstName: "first",
+        LastName: "last",
+        PhoneNumber: "+114323888",
+        Role: "ceo",
+        PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
+      },
+      Registration: {
+        CharityName: "charity",
+        CharityName_ContactEmail: "CHARITY_test@test.com",
+        RegistrationDate: "2022-05-04T10:10:10Z",
+        RegistrationStatus: status,
+        Website: "www.test.com",
+        UN_SDG: 0,
+        ProofOfIdentity: {
+          name: "poi",
+          publicUrl: "https://www.storage.path/poi",
+        },
+        ProofOfRegistration: {
+          name: "por",
+          publicUrl: "https://www.storage.path/por",
+        },
+        Tier: 1,
+        FinancialStatements: [],
+        AuditedFinancialReports: [],
+        ProofOfIdentityVerified: true,
+        ProofOfRegistrationVerified: true,
+        FinancialStatementsVerified: false,
+        AuditedFinancialReportsVerified: false,
+      },
+      Metadata: {
+        Banner: {
+          name: "banner",
+          publicUrl: "https://www.storage.path/banner",
+        },
+        CharityLogo: {
+          name: "logo",
+          publicUrl: "https://www.storage.path/logo",
+        },
+        CharityOverview: "some overview",
+        EndowmentContract: "terra1ke4aktw6zvz2jxsyqx55ejsj7rmxdl9p5xywus",
+        TerraWallet: "terra1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
+      },
+    };
+
+    const state = getRegistrationState(charity);
+
+    expect(state.stepOne.completed).toBe(true);
+    expect(state.stepTwo.completed).toBe(true);
+    expect(state.stepThree.completed).toBe(true);
+    expect(state.stepFour.completed).toBe(true);
+    expect(state.getIsReadyForSubmit()).toBe(true);
+  }
+});
