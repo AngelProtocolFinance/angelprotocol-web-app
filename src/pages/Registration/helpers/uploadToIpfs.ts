d258d5b131b8bac9c141d065e6efa20162defadf
Nenad Misic <n.misic@outlook.com>
2022-05-10 15:31:50 +0200

    Update uploadToIpfs to encode file names before upload

--- src/pages/Registration/helpers/uploadToIpfs.ts
+++ src/pages/Registration/helpers/uploadToIpfs.ts
@@ -1,27 +1,29 @@
 import fleekStorage from "@fleekhq/fleek-storage-js";
 import { FileObject } from "services/aws/types";
 
-const API_KEY = process.env.REACT_APP_FLEEK_API_KEY;
-const API_SECRET = process.env.REACT_APP_FLEEK_API_SECRET;
+const API_KEY = process.env.REACT_APP_FLEEK_API_KEY!;
+const API_SECRET = process.env.REACT_APP_FLEEK_API_SECRET!;
 
-export default async function uploadToIpfs(
-  file: File,
-  folder?: string
-): Promise<FileObject> {
+async function uploadToIpfs(file: File, folder: string): Promise<FileObject> {
   try {
-    const uploadedFile = await fleekStorage.upload({
-      key: folder ? [folder, file.name].join("") : file.name,
-      apiKey: API_KEY!,
-      apiSecret: API_SECRET!,
+    const relativePath = [folder, encodeURIComponent(file.name)].join("");
+
+    const result = await fleekStorage.upload({
+      key: relativePath,
+      apiKey: API_KEY,
+      apiSecret: API_SECRET,
       data: file,
     });
+
     return {
       name: file.name,
-      publicUrl: uploadedFile.publicUrl,
+      publicUrl: result.publicUrl,
     };
   } catch (e) {
-    // console.log("Error uploading file: ", file.name);
+    // console.log("Error uploading file: ", filename);
     // throw new Error("Error uploading file to ipfs");
     return { name: "", publicUrl: "" };
   }
 }
+
+export default uploadToIpfs;
