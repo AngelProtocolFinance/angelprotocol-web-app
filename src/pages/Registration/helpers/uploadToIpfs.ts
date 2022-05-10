b983ec276c6e30c6408a282503e1effef006dc0d
Nenad Misic <n.misic@outlook.com>
2022-05-10 15:35:23 +0200

    Inline api key/secret reading

--- src/pages/Registration/helpers/uploadToIpfs.ts
+++ src/pages/Registration/helpers/uploadToIpfs.ts
@@ -1,17 +1,14 @@
 import fleekStorage from "@fleekhq/fleek-storage-js";
 import { FileObject } from "services/aws/types";
 
-const API_KEY = process.env.REACT_APP_FLEEK_API_KEY!;
-const API_SECRET = process.env.REACT_APP_FLEEK_API_SECRET!;
-
 async function uploadToIpfs(file: File, folder: string): Promise<FileObject> {
   try {
     const relativePath = [folder, encodeURIComponent(file.name)].join("");
 
     const result = await fleekStorage.upload({
       key: relativePath,
-      apiKey: API_KEY,
-      apiSecret: API_SECRET,
+      apiKey: process.env.REACT_APP_FLEEK_API_KEY!,
+      apiSecret: process.env.REACT_APP_FLEEK_API_SECRET!,
       data: file,
     });
 
