cf714e9e7f36afaed1774df46ab5f69ee616cf6a
Nenad Misic <n.misic@outlook.com>
2022-05-16 13:08:52 +0200

    Update register wallet title

--- src/pages/Registration/WalletRegistration/ChooseWallet/Title.tsx
+++ src/pages/Registration/WalletRegistration/ChooseWallet/Title.tsx
@@ -7,32 +7,11 @@ import TerraInfoModal from "./TerraInfoModal";
 // React renders it all the way to the right, making it appear unrelated to the title, reducing UX)
 export default function Title() {
   return (
-    <>
-      {/* Appear inline on large screens */}
-      <h2 className="hidden lg:flex text-lg xl:text-xl font-semibold gap-2 items-center">
-        Please choose one of the options below to register your Terra wallet
-        <InfoIcon />
-      </h2>
-
-      {/* Appear in two rows on small to large screens */}
-      <h2 className="hidden sm:flex text-lg flex-col lg:hidden font-semibold items-center">
-        Please choose one of the options
-        <div className="flex gap-1">
-          below to register your Terra wallet
-          <InfoIcon />
-        </div>
-      </h2>
-
-      {/* Appear in three rows up to small screens */}
-      <h2 className="sm:hidden flex-col font-semibold">
-        <div>Please choose one of</div>
-        <div>the options below to</div>
-        <div className="flex gap-1 justify-center">
-          register your Terra wallet
-          <InfoIcon />
-        </div>
-      </h2>
-    </>
+    <h2 className="flex text-lg xl:text-xl font-semibold gap-2 items-center">
+      Please choose one of the options below to register a new Terra wallet for
+      your charity
+      <InfoIcon />
+    </h2>
   );
 }
 
