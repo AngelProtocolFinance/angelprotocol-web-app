9c09590f234897a9f1cb179fb70cb148549a9236
ap-justin <justin@angelprotocol.io>
2022-05-18 12:28:16 +0800

    change words

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -9,7 +9,8 @@ export default function App() {
   return (
     <div className="grid grid-rows-a1a bg-gradient-to-b from-blue-accent to-black-blue relative pt-6">
       <p className="transition-shadow fixed z-20 top-0 inset-x-0 font-heading font-bold bg-angel-orange w-full p-2 text-center text-angel-grey text-xs">
-        Terra donations are disabled, app accepts BNB & ETH only
+        Terra donations are disabled until Terra V2 launch, currently accepting
+        BNB & ETH.
       </p>
       <WalletProvider>
         <Modal classes="bg-black/50 fixed top-0 right-0 bottom-0 left-0 z-50 grid place-items-center">
