7abb5d38d0d8a2fca93066c949717e637270add8
Nenad Misic <n.misic@outlook.com>
2022-05-19 09:57:58 +0200

    Fix Icon import in TableView

--- src/pages/Admin/Applications/TableView.tsx
+++ src/pages/Admin/Applications/TableView.tsx
@@ -1,4 +1,4 @@
-import Icon from "components/Icons/Icons";
+import Icon from "components/Icon";
 import { useModalContext } from "components/ModalContext/ModalContext";
 import TableSection, { Cells } from "components/TableSection/TableSection";
 import { statusColorClasses } from "./Applications";
