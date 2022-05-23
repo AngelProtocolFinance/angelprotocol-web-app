e9991865f6c9f3d3118460191556605ed16f4974
ap-justin <justin@angelprotocol.io>
2022-05-23 19:05:00 +0800

    remove unused helpers

--- src/helpers/idParamToNum.ts
+++
@@ -1,7 +0,0 @@
-export default function idParamToNumber(id?: string | number) {
-  if (id === undefined) {
-    return 0;
-  } else {
-    return isNaN(id as unknown as number) ? 0 : Math.floor(+id);
-  }
-}
