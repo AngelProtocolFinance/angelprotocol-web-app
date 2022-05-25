f12e49642882445c6d5d56d9593003861670625d
Nenad Misic <n.misic@outlook.com>
2022-05-25 09:50:34 +0200

    Fix tests

--- src/pages/Registration/Registration/__tests__/ButtonMailTo.test.tsx
+++ src/pages/Registration/Registration/__tests__/ButtonMailTo.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import renderer from "react-test-renderer";
 import ButtonMailTo from "../ButtonMailTo";
@@ -25,13 +25,11 @@ describe("ButtonMailTo tests", () => {
   it("opens email with correct recepient and subject", () => {
     const expectedLabel = "Label";
     const email = "test@email.com";
-    const { container } = render(
-      <MockComponent label={expectedLabel} mailTo={email} />
-    );
+    render(<MockComponent label={expectedLabel} mailTo={email} />);
 
-    const link = container.querySelector("a");
-    expect(link).toBeInTheDocument();
-    expect(link?.href).toBe(
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute(
+      "href",
       `mailto:${email}?subject=Charity%20Registration%3A%20Trouble%20with%20confirmation%20email`
     );
   });
