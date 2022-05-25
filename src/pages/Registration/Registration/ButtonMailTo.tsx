07d45d0a6a542a2ba322ef2701a84a2e84d477eb
Nenad Misic <n.misic@outlook.com>
2022-05-25 09:32:15 +0200

    Add ButtomMailTo tests

--- src/pages/Registration/Registration/ButtonMailTo.tsx
+++ src/pages/Registration/Registration/ButtonMailTo.tsx
@@ -4,17 +4,13 @@ type Props = { mailTo: string; label: string };
 
 export default function ButtonMailTo({ mailTo, label }: Props) {
   return (
-    <Link
-      to="#"
+    <a
+      href={`mailto:${mailTo}?subject=${encodeURIComponent(
+        "Charity Registration: Trouble with confirmation email"
+      )}`}
       className="italic underline"
-      onClick={(e) => {
-        window.location.href = `mailto:${mailTo}?subject=${encodeURIComponent(
-          "Charity Registration: Trouble with confirmation email"
-        )}`;
-        e.preventDefault();
-      }}
     >
       {label}
-    </Link>
+    </a>
   );
 }
---
+++ src/pages/Registration/Registration/__tests__/ButtonMailTo.test.tsx
@@ -0,0 +1,38 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
+import ButtonMailTo from "../ButtonMailTo";
+
+type Props = { label: string; mailTo: string };
+
+const MockComponent = ({ label, mailTo }: Props) => {
+  return (
+    <MemoryRouter>
+      <ButtonMailTo label={label} mailTo={mailTo} />
+    </MemoryRouter>
+  );
+};
+
+describe("ButtonMailTo tests", () => {
+  it("matches snapshot", () => {
+    const component = renderer.create(
+      <MockComponent label="Label" mailTo="some@email.com" />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("opens email with correct recepient and subject", () => {
+    const expectedLabel = "Label";
+    const email = "test@email.com";
+    const { container } = render(
+      <MockComponent label={expectedLabel} mailTo={email} />
+    );
+
+    const link = container.querySelector("a");
+    expect(link).toBeInTheDocument();
+    expect(link?.href).toBe(
+      `mailto:${email}?subject=Charity%20Registration%3A%20Trouble%20with%20confirmation%20email`
+    );
+  });
+});
