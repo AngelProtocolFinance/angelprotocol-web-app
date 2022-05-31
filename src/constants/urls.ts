69418bb091bdb840ed51ba421962356a5f924d99
ap-justin <justin@angelprotocol.io>
2022-05-31 13:30:50 +0800

    remove unused urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -14,43 +14,9 @@ export const flipside_endpoint = "https://flipside.leslug.com/angel";
 
 //terra urls
 export const terra_lcds: URL_GROUP = {
-  [chainIDs.terra_test]:
-    "https://59vigz9r91.execute-api.us-east-1.amazonaws.com/terra/lcd/test",
-  [chainIDs.terra_main]:
-    "https://59vigz9r91.execute-api.us-east-1.amazonaws.com/terra/lcd/main",
+  [chainIDs.terra_test]: "https://pisco-lcd.terra.dev/ ",
+  [chainIDs.terra_main]: "https://lcd.terra.dev/",
   [chainIDs.terra_local]: "http://localhost:3060",
-};
-
-export const terra_rpcs: URL_GROUP = {
-  [chainIDs.terra_test]:
-    "https://bombay-12--rcp--full.datahub.figment.io/apikey/1ddd68fdcf80475836f8f815095d34c6",
-  [chainIDs.terra_main]:
-    "https://columbus-5--rpc--full.datahub.figment.io/apikey/1ddd68fdcf80475836f8f815095d34c6",
-  [chainIDs.terra_local]: "",
-};
-
-//cosmos urls
-export const cosmos4_lcds: URL_GROUP = {
-  [chainIDs.cosmos_4]:
-    "https://cosmoshub-4--lcd--full.datahub.figment.io/apikey/afe639fa07917ec085c0ce737ab39e67",
-  [chainIDs.cosmos_test]: "https://api.testnet.cosmos.network:443",
-};
-
-export const cosmos4_rpcs: URL_GROUP = {
-  [chainIDs.cosmos_4]:
-    "https://cosmoshub-4--rpc--full.datahub.figment.io/apikey/afe639fa07917ec085c0ce737ab39e67",
-  [chainIDs.cosmos_test]:
-    "https://cosmoshub-4--lcd--full.datahub.figment.io/apikey/afe639fa07917ec085c0ce737ab39e67",
-};
-
-//ethereum
-export const eth_rpcs: URL_GROUP = {
-  [chainIDs.eth_main]:
-    "https://mainnet.infura.io/v3/f7ca16d6c4704dee939ca7557896cf07",
-  [chainIDs.eth_ropsten]:
-    "https://ropsten.infura.io/v3/f7ca16d6c4704dee939ca7557896cf07",
-  [chainIDs.eth_kovan]:
-    "https://kovan.infura.io/v3/f7ca16d6c4704dee939ca7557896cf07",
 };
 
 export const TERRA_FINDER = "https://terrascope.info/";
