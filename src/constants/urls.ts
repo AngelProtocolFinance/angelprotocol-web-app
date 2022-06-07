c38b3793c7a9ad2b89c539a399b7786dd575bedb
ap-justin <justin@angelprotocol.io>
2022-06-07 16:54:26 +0800

    unused chainId

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,9 +1,4 @@
 import { IconTypes } from "components/Icon";
-import { chainIDs } from "./chainIDs";
-
-type URL_GROUP = {
-  [index: string]: string;
-};
 
 export const aws_endpoint =
   "https://mu2d2e0oj0.execute-api.us-east-1.amazonaws.com";
