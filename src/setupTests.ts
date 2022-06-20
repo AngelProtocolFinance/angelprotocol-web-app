6774118cd772e64f5ce90475866d6f57d1644ee2
Nenad Misic <n.misic@outlook.com>
2022-06-20 09:00:32 +0200

    Remove @peculiar/webcrypto and setupTests terra-related config

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,9 +2,4 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-// NOTE(davidlumley): Override default Crypto for terra.js
-//                    see: https://github.com/terra-money/terra.js/issues/100
-import { Crypto } from "@peculiar/webcrypto";
 import "@testing-library/jest-dom";
-
-global.crypto = new Crypto();
