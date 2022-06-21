6fbde94c750e5541fc41b0e6ade05625431d6661
Nenad Misic <n.misic@outlook.com>
2022-06-21 10:35:26 +0200

    Fix Decimal operations missing fractional digis

--- src/helpers/Decimal.ts
+++ src/helpers/Decimal.ts
@@ -18,17 +18,17 @@ export default class Decimal {
 
   public plus(b: Decimal): Decimal {
     const result = this.value.plus(b.value);
-    return new Decimal(result.atomics);
+    return new Decimal(result.atomics, this.value.fractionalDigits);
   }
 
   public minus(b: Decimal): Decimal {
     const result = this.value.minus(b.value);
-    return new Decimal(result.atomics);
+    return new Decimal(result.atomics, this.value.fractionalDigits);
   }
 
   public multiply(b: number | string): Decimal {
     const result = this.value.multiply(Uint64.fromString(b.toString()));
-    return new Decimal(result.atomics);
+    return new Decimal(result.atomics, this.value.fractionalDigits);
   }
 
   /**
