eed4cda240256fb0d8b9ee9aa5b4ac4fb5ddc50e
Nenad Misic <n.misic@outlook.com>
2022-06-22 08:55:35 +0200

    Update useStakerBalance to Decimal

--- src/components/Transactors/Staker/useStakerBalance.ts
+++ src/components/Transactors/Staker/useStakerBalance.ts
@@ -1,7 +1,7 @@
-import { Dec } from "@terra-money/terra.js";
 import { useMemo } from "react";
 import { useGovStaker } from "services/terra/gov/queriers";
 import { useGetWallet } from "contexts/WalletContext/WalletContext";
+import Decimal from "helpers/Decimal";
 import getTokenBalance from "helpers/getTokenBalance";
 import { denoms } from "constants/currency";
 
@@ -9,22 +9,23 @@ export default function useStakerBalance(is_stake: boolean) {
   const gov_staker = useGovStaker();
   const { wallet } = useGetWallet();
 
-  const [balance, locked] = useMemo((): [Dec, Dec] => {
+  const [balance, locked] = useMemo((): [Decimal, Decimal] => {
     const haloBalance = getTokenBalance(wallet?.coins || [], denoms.halo);
-    const staked = new Dec(gov_staker.balance);
     if (is_stake) {
-      return [new Dec(haloBalance).mul(1e6), new Dec(0)];
+      return [new Decimal(haloBalance).multiply(1e6), new Decimal(0)];
     } else {
       //used in voting to a poll
       const vote_locked = gov_staker.locked_balance.reduce(
         (res, [, vote]) =>
           //get max vote locked
-          new Dec(vote.balance).gt(res) ? new Dec(vote.balance) : res,
-        new Dec(0)
+          new Decimal(vote.balance).isGreaterThan(res)
+            ? new Decimal(vote.balance)
+            : res,
+        new Decimal(0)
       );
-      return [staked, vote_locked];
+      return [new Decimal(gov_staker.balance), vote_locked];
     }
-    // is_stake ? haloBalance : new Dec(gov_staker.balance).div(1e6).toNumber();
+    // is_stake ? haloBalance : new Decimal(gov_staker.balance).div(1e6).toNumber();
   }, [gov_staker, wallet, is_stake]);
 
   return { balance, locked };
