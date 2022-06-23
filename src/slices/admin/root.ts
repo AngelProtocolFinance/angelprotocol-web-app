93a8f4b7005dd54d406990761038447d8a6615bc
ap-justin <justin@angelprotocol.io>
2022-06-23 13:34:17 +0800

    move state to slice

--- src/slices/admin/root.ts
+++ src/slices/admin/root.ts
@@ -4,6 +4,7 @@ import apCW4MembersReducer from "./apCW4Members";
 import cwContractsReducer from "./cwContracts";
 import fundMembersReducer from "./fundMembers";
 import newFundMembersReducer from "./newFundMembers";
+import proposalsReducer from "./proposals";
 
 export const adminReducer = combineReducers({
   apCW4Members: apCW4MembersReducer,
@@ -11,4 +12,5 @@ export const adminReducer = combineReducers({
   fundMembers: fundMembersReducer,
   cwContracts: cwContractsReducer,
   allianceMembers: allianceMembersReducer,
+  proposals: proposalsReducer,
 });
