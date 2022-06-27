517f4239aa67b74adaa2601b1c58bc223d4b09e4
Nenad Misic <n.misic@outlook.com>
2022-06-27 10:54:27 +0200

    Update createContract

--- src/contracts/createContract.ts
+++ src/contracts/createContract.ts
@@ -1,55 +1,39 @@
 import { EncodeObject } from "@cosmjs/proto-signing";
 import { Coin } from "@cosmjs/stargate";
 import { Keplr } from "@keplr-wallet/types";
-import { EmbeddedBankMsg, EmbeddedWasmMsg } from "types/server/contracts";
+import { EmbeddedWasmMsg } from "types/server/contracts";
 import { WalletState } from "contexts/WalletContext/WalletContext";
 import configureCosmosClient from "helpers/configureCosmosClient";
+import toBase64 from "helpers/toBase64";
 import { WalletDisconnectError } from "errors/errors";
 
 export default async function createContract(wallet: WalletState) {
   const signer = (wallet.provider as Keplr).getOfflineSigner(wallet.chainId);
   const { client, address } = await configureCosmosClient(signer);
 
-  function checkWallet() {
-    if (!address) {
-      throw new WalletDisconnectError();
-    }
-  }
-
-  function createEmbeddedBankMsg(funds: Coin[], to: string): EmbeddedBankMsg {
-    return {
-      bank: {
-        send: {
-          to_address: to,
-          amount: funds,
-        },
-      },
-    };
-  }
-
   function createEmbeddedWasmMsg(
     funds: Coin[],
     to: string,
     msg: object
   ): EmbeddedWasmMsg {
-    const encodedMsg = btoa(JSON.stringify(msg));
     return {
       wasm: {
         execute: {
           contract_addr: to,
           funds,
-          msg: encodedMsg,
+          msg: toBase64(msg),
         },
       },
     };
   }
 
   async function estimateFee(msgs: readonly EncodeObject[]): Promise<number> {
-    checkWallet();
-    return await client.simulate(address!, msgs, undefined);
+    if (!address) {
+      throw new WalletDisconnectError();
+    }
+    return await client.simulate(address, msgs, undefined);
   }
 
-  //for on-demand query, use RTK where possible
   async function query<T>(
     source: string,
     message: Record<string, unknown>
@@ -61,8 +45,6 @@ export default async function createContract(wallet: WalletState) {
   return {
     client,
     walletAddress: address,
-    checkWallet,
-    createEmbeddedBankMsg,
     createEmbeddedWasmMsg,
     estimateFee,
     query,
