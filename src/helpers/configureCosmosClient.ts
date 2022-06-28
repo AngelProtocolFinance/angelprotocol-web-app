f255bc6e3568c8abac8a5cb5d0c48f90ce519bba
Nenad Misic <n.misic@outlook.com>
2022-06-28 09:33:47 +0200

    Return only client from configureCosmosClient

--- src/helpers/configureCosmosClient.ts
+++ src/helpers/configureCosmosClient.ts
@@ -5,20 +5,13 @@ import { junoRpcUrl } from "constants/urls";
 
 const GAS_PRICE = GasPrice.fromString("0.025ujunox");
 
-type ReturnType = {
-  client: SigningCosmWasmClient;
-  address: string;
-  gasPrice: GasPrice;
-};
-
 export default async function configureCosmosClient(
   signer: OfflineSigner
-): Promise<ReturnType> {
+): Promise<SigningCosmWasmClient> {
   const client = await SigningCosmWasmClient.connectWithSigner(
     junoRpcUrl,
     signer,
     { gasPrice: GAS_PRICE }
   );
-  const [{ address }] = await signer.getAccounts();
-  return { client, address, gasPrice: GAS_PRICE };
+  return client;
 }
