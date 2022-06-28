fd6a8e5e5cdcd426a820e888ca73c2296f71f582
Nenad Misic <n.misic@outlook.com>
2022-06-28 12:15:44 +0200

    Refactor Contract

--- src/contracts/Contract.ts
+++ src/contracts/Contract.ts
@@ -1,5 +1,7 @@
-import { Coin, Fee, LCDClient, Msg } from "@terra-money/terra.js";
+import { Coin } from "@cosmjs/stargate";
 import { EmbeddedWasmMsg } from "types/server/contracts";
+import { WalletState } from "contexts/WalletContext/WalletContext";
+import getCosmosClient from "helpers/getCosmosClient";
 import toBase64 from "helpers/toBase64";
 import { WalletDisconnectError } from "errors/errors";
 import { denoms } from "constants/currency";
@@ -7,31 +9,25 @@ import { terraChainId } from "constants/env";
 import { terraLcdUrl } from "constants/urls";
 
 export default class Contract {
-  client: LCDClient;
-  walletAddr: string;
+  wallet?: WalletState;
 
-  constructor(walletAddr?: string) {
-    this.walletAddr = walletAddr || "";
-    this.client = new LCDClient({
-      chainID: terraChainId,
-      URL: terraLcdUrl,
-      gasAdjustment: Contract.gasAdjustment, //use gas units 20% greater than estimate
-      gasPrices: Contract.gasPrices,
-    });
+  constructor(wallet?: WalletState) {
+    this.wallet = wallet;
   }
 
-  static gasAdjustment = 1.6; //use gas units 60% greater than estimate
+  // static gasAdjustment = 1.6; //use gas units 60% greater than estimate
 
-  // https://fcd.terra.dev/v1/txs/gas_prices - doesn't change too often
-  static gasPrices = [
-    new Coin(denoms.uusd, 0.15),
-    //for classic, pisco is 0.15
-    new Coin(denoms.uluna, 5.665),
-  ];
+  // // https://fcd.terra.dev/v1/txs/gas_prices - doesn't change too often
+  // static gasPrices = [
+  //   new Coin(denoms.uusd, 0.15),
+  //   //for classic, pisco is 0.15
+  //   new Coin(denoms.uluna, 5.665),
+  // ];
 
   //for on-demand query, use RTK where possible
   async query<T>(source: string, message: object) {
-    return this.client.wasm.contractQuery<T>(source, message);
+    const client = await getCosmosClient(this.wallet);
+    return await client.queryContractSmart(source, message);
   }
 
   async estimateFee(msgs: Msg[]): Promise<Fee> {
@@ -48,7 +44,7 @@ export default class Contract {
   }
 
   createEmbeddedWasmMsg(
-    funds: Coin.Data[],
+    funds: Coin[],
     to: string,
     msg: object
   ): EmbeddedWasmMsg {
