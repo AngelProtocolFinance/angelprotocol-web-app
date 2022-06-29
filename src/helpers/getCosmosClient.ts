927ffd589ea9ddf2dd2637f78964a80e942e7ff9
Nenad Misic <n.misic@outlook.com>
2022-06-29 14:16:26 +0200

    Remove default gas price from getCosmosClient

--- src/helpers/getCosmosClient.ts
+++ src/helpers/getCosmosClient.ts
@@ -1,12 +1,9 @@
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import { GasPrice } from "@cosmjs/stargate";
 import { Keplr } from "@keplr-wallet/types";
 import { WalletState } from "contexts/WalletContext/WalletContext";
 import { WalletDisconnectError, WrongNetworkError } from "errors/errors";
 import { chainIDs } from "constants/chainIDs";
 import { junoRpcUrl } from "constants/urls";
-
-const GAS_PRICE = GasPrice.fromString("0.025ujunox");
 
 export default async function getCosmosClient(
   wallet?: WalletState
@@ -23,8 +20,7 @@ export default async function getCosmosClient(
   const signer = (wallet.provider as Keplr).getOfflineSigner(wallet.chainId);
   const client = await SigningCosmWasmClient.connectWithSigner(
     junoRpcUrl,
-    signer,
-    { gasPrice: GAS_PRICE }
+    signer
   );
   return client;
 }
