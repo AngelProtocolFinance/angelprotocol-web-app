fb833bc67c59c03427de3f36b1d86d2a74df44b9
Andrey <andrey@angelprotocol.io>
2022-06-29 16:04:36 +0800

    new litepaper url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -30,7 +30,7 @@ export const PRIVACY_POLICY =
   "https://storageapi2.fleek.co/57b943eb-ed70-478a-8899-c7859400f77b-bucket/documents/Website and WebApp Privacy Policy (v.110121).docx";
 
 export const LITEPAPER =
-  "https://storageapi2.fleek.co/57b943eb-ed70-478a-8899-c7859400f77b-bucket/documents/ap-litepaper.pdf";
+  "https://storageapi.fleek.co/57b943eb-ed70-478a-8899-c7859400f77b-bucket/documents/ap-litepaper-v3.pdf";
 
 type SocialMediaLink = {
   id: number;
