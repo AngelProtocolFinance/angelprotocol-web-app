9c74333d3df412fdce2b07bbc72e8aae47a845a4
ap-justin <justin@angelprotocol.io>
2022-06-30 18:27:15 +0800

    make use of ErrorMessage component in form components that use useFormContext

--- src/pages/Registration/Documentation/Fields/FileDropzones/FinancialStatements.tsx
+++ src/pages/Registration/Documentation/Fields/FileDropzones/FinancialStatements.tsx
@@ -1,4 +1,5 @@
-import { FieldError, useFormContext } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { useFormContext } from "react-hook-form";
 import { DocumentationValues } from "pages/Registration/types";
 import FileDropzone from "components/FileDropzone";
 import { InputRow } from "../../../common";
@@ -19,29 +20,12 @@ export default function FinancialStatements() {
         multiple
         disabled={isSubmitting}
       />
-      {!!errors.financialStatements?.length &&
-        errors.financialStatements
-          // Yup assumes wrong field names for errors, so we need to cast them first as "any",
-          // otherwise it would issue a warning
-          .map((fieldError: any) => (fieldError as FieldError).message)
-          .filter(checkUnique)
-          .map((message) => (
-            <p
-              key={message}
-              className="w-full text-xs text-failed-red text-center"
-            >
-              {message}
-            </p>
-          ))}
+      <ErrorMessage
+        className="w-full text-xs text-failed-red text-center"
+        errors={errors}
+        as="p"
+        name="financialStatements"
+      />
     </InputRow>
   );
-}
-
-// Checks, if the given value is the first occurring. If not, it must be a duplicate.
-function checkUnique(
-  value: string | undefined,
-  index: number,
-  self: (string | undefined)[]
-) {
-  return self.indexOf(value) === index;
 }
