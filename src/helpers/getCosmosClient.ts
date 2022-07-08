a90ba36e696a03add16589f0fbf8192a213da08d
Nenad Misic <n.misic@outlook.com>
2022-07-08 08:46:19 +0200

    Refactor wallet.chainId check in getCosmosClient

--- src/helpers/getCosmosClient.ts
+++ src/helpers/getCosmosClient.ts
@@ -2,19 +2,16 @@ import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { Dwindow } from "types/ethereum";
 import { WalletState } from "contexts/WalletContext/WalletContext";
 import { WalletDisconnectError, WrongNetworkError } from "errors/errors";
-import { chainIDs } from "constants/chainIDs";
+import { junoChainId } from "constants/chainIDs";
 import { junoRpcUrl } from "constants/urls";
 
 export default async function getCosmosClient(
-  wallet?: WalletState
+  wallet: WalletState | undefined
 ): Promise<SigningCosmWasmClient> {
   if (!wallet) {
     throw new WalletDisconnectError();
   }
-  if (
-    wallet.chainId !== chainIDs.juno_main &&
-    wallet.chainId !== chainIDs.juno_test
-  ) {
+  if (wallet.chainId !== junoChainId) {
     throw new WrongNetworkError();
   }
   const signer = (window as Dwindow).keplr!.getOfflineSigner(wallet.chainId);
