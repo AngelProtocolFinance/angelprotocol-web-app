c2341982e9752bdd8b069ab11cd16cb6a43de716
Nenad Misic <n.misic@outlook.com>
2022-07-12 09:50:09 +0200

    Skip Admin queries when not connected to Juno network

--- src/services/juno/admin/useAdminContract.ts
+++ src/services/juno/admin/useAdminContract.ts
@@ -3,6 +3,7 @@ import { CWContracts } from "types/server/contracts";
 import { useGetWallet } from "contexts/WalletContext/WalletContext";
 import { useGetter } from "store/accessors";
 import Admin from "contracts/Admin";
+import { junoChainId } from "constants/chainIDs";
 
 export default function useAdminContract(customCWs?: CWContracts) {
   const { cwContracts } = useGetter((state) => state.admin.cwContracts);
@@ -12,9 +13,11 @@ export default function useAdminContract(customCWs?: CWContracts) {
   const contract = useMemo(() => new Admin(wallet, cws), [wallet, cws]);
 
   const isAdminSkip =
-    cws !== "apTeam" &&
-    //skip query if user didn't provide any address
-    (cws.cw3 === undefined || cws.cw4 === undefined);
+    // skip if wallet not connected to Juno blockchain
+    wallet?.chainId !== junoChainId ||
+    (cws !== "apTeam" &&
+      //skip query if user didn't provide any address
+      (cws.cw3 === undefined || cws.cw4 === undefined));
 
   return { contract, walletAddr: wallet?.address, isAdminSkip };
 }
