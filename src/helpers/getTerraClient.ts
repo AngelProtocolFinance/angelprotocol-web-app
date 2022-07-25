8f23f7f06fa55fed235eded3261f63ba31e54d81
Nenad Misic <n.misic@outlook.com>
2022-07-25 15:07:22 +0200

    Refactor getTerraClient

--- src/helpers/getTerraClient.ts
+++ src/helpers/getTerraClient.ts
@@ -1,7 +1,4 @@
 import { Coin, LCDClient } from "@terra-money/terra.js";
-import { WalletState } from "contexts/WalletContext/WalletContext";
-import { WalletDisconnectError, WrongNetworkError } from "errors/errors";
-import { terraChainId } from "constants/chainIDs";
 import { denoms } from "constants/currency";
 
 const GAS_ADJUSTMENT = 1.6; //use gas units 60% greater than estimate
@@ -13,17 +10,10 @@ const GAS_PRICES = [
   new Coin(denoms.uluna, 5.665),
 ];
 
-export default function getTerraClient(wallet: WalletState | undefined) {
-  if (!wallet) {
-    throw new WalletDisconnectError();
-  }
-  if (wallet.chain.chain_id !== terraChainId) {
-    throw new WrongNetworkError("Terra", terraChainId);
-  }
-
+export default function getTerraClient(chain_id: string, lcd_url: string) {
   return new LCDClient({
-    chainID: wallet.chain.chain_id,
-    URL: wallet.chain.lcd_url,
+    chainID: chain_id,
+    URL: lcd_url,
     gasAdjustment: GAS_ADJUSTMENT, //use gas units 20% greater than estimate
     gasPrices: GAS_PRICES,
   });
