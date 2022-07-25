676bd4abd88ad14871dbf509ff7a8d662354829f
Nenad Misic <n.misic@outlook.com>
2022-07-25 17:28:21 +0200

    Move cosmos balance fetching back to tokens and fix calculation

--- src/services/apes/tokens/tokens.ts
+++ src/services/apes/tokens/tokens.ts
@@ -1,13 +1,15 @@
+import { Coin } from "@cosmjs/proto-signing";
 import { ethers, utils } from "ethers";
 import { ProviderInfo } from "contexts/WalletContext/types";
 import { Chain, Token } from "types/server/aws";
 import { isJunoChain, isTerraChain } from "helpers/checkChain";
 import createAuthToken from "helpers/createAuthToken";
+import getCosmosClient from "helpers/getCosmosClient";
+import getTerraClient from "helpers/getTerraClient";
 import { UnsupportedNetworkError } from "errors/errors";
 import { apes_endpoint } from "constants/urls";
 import { apes } from "../apes";
 import { getERC20Holdings } from "../helpers/getERC20Holdings";
-import getCosmosBalances from "./getCosmosBalances";
 
 const tokens_api = apes.injectEndpoints({
   endpoints: (builder) => ({
@@ -36,7 +38,32 @@ const tokens_api = apes.injectEndpoints({
 
           // fetch balances for juno or terra
           if (isJunoChain(chainId) || isTerraChain(chainId)) {
-            return await getCosmosBalances(chain, address);
+            const balancesRes = await fetch(
+              chain.lcd_url + `/cosmos/bank/v1beta1/balances/${address}`
+            );
+
+            // returns only positive balances
+            const { balances: nativeBalances }: { balances: Coin[] } =
+              await balancesRes.json();
+
+            const cw20Balances = isJunoChain(chain.chain_id)
+              ? await getJunoCW20Balances(chain, address)
+              : await getTerraCW20Balances(chain, address);
+
+            const allBalances = nativeBalances.concat(cw20Balances);
+
+            [chain.native_currency, ...chain.tokens].forEach((token) => {
+              const balance = allBalances.find(
+                (x) => x.denom === token.token_id
+              );
+              if (balance?.amount)
+                token.balance = +utils.formatUnits(
+                  balance.amount,
+                  token.decimals
+                );
+            });
+
+            return { data: chain };
           }
 
           /**fetch balances for ethereum */
@@ -82,3 +109,40 @@ const tokens_api = apes.injectEndpoints({
 });
 
 export const { useTokensQuery, useChainQuery } = tokens_api;
+
+async function getJunoCW20Balances(
+  chain: Chain,
+  walletAddress: string
+): Promise<Coin[]> {
+  const cosmosClient = await getCosmosClient(chain.chain_id, chain.rpc_url);
+
+  const cw20BalancePromises = chain.tokens
+    .filter((x) => x.type === "cw20")
+    .map((x) => cosmosClient.getBalance(walletAddress, x.token_id));
+
+  const cw20Balances = await Promise.all(cw20BalancePromises);
+  return cw20Balances;
+}
+
+async function getTerraCW20Balances(
+  chain: Chain,
+  walletAddress: string
+): Promise<Coin[]> {
+  const terraClient = getTerraClient(chain.chain_id, chain.rpc_url);
+
+  const cw20BalancePromises = chain.tokens
+    .filter((x) => x.type === "cw20")
+    .map((x) =>
+      terraClient.wasm
+        .contractQuery<{ balance: string }>(x.token_id, {
+          balance: { address: walletAddress },
+        })
+        .then((data) => ({
+          denom: x.token_id,
+          amount: data.balance,
+        }))
+    );
+
+  const cw20Balances = await Promise.all(cw20BalancePromises);
+  return cw20Balances;
+}
