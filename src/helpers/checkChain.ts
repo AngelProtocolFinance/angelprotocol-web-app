f0d4939ef911455fce9de855732b89bcef75f498
Nenad Misic <n.misic@outlook.com>
2022-07-27 07:31:05 +0200

    Check wallet.chain.type instead of chainId for correct blockchain where possible

--- src/helpers/checkChain.ts
+++
@@ -1,7 +0,0 @@
-import { junoChainId, terraChainId } from "constants/chainIDs";
-
-export const isJunoChain = (chainId: string | undefined) =>
-  chainId === junoChainId;
-
-export const isTerraChain = (chainId: string | undefined) =>
-  chainId === terraChainId;
