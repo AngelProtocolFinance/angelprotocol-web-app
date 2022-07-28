9379f2a6a5350cff44f9bc5843dc3864db535ff7
Nenad Misic <n.misic@outlook.com>
2022-07-28 08:09:08 +0200

    Log actual chain_id instead of just 'testnet' or 'mainnet'

--- src/pages/Admin/Applications/logApplicationReview.ts
+++ src/pages/Admin/Applications/logApplicationReview.ts
@@ -1,25 +1,22 @@
 import createAuthToken from "helpers/createAuthToken";
 import { LogApplicationUpdateError } from "errors/errors";
-import { chainIDs } from "constants/chainIDs";
 import { aws_endpoint } from "constants/urls";
 
-interface UpdateApplicationPayload {
+interface Payload {
   PK: string;
   poll_id: string;
   chain_id: string;
 }
-type ReviewLogger = (payload: UpdateApplicationPayload) => Promise<void>;
-const logApplicationReview: ReviewLogger = async (payload) => {
+
+const logApplicationReview = async (payload: Payload): Promise<void> => {
   const generatedToken = createAuthToken("charity-owner");
-  const is_test = payload.chain_id === chainIDs.terra_test;
-  const chain_id = is_test ? "testnet" : "mainnet";
   const response = await fetch(
     `${aws_endpoint}/registration?uuid=${payload.PK}`,
     {
       method: "PUT",
       headers: { authorization: generatedToken },
       body: JSON.stringify({
-        chain_id,
+        chain_id: payload.chain_id,
         poll_id: +payload.poll_id,
       }),
     }
@@ -27,7 +24,7 @@ const logApplicationReview: ReviewLogger = async (payload) => {
 
   //success = 2xx
   if (response.status < 200 || response.status > 299) {
-    throw new LogApplicationUpdateError(chain_id, payload.poll_id);
+    throw new LogApplicationUpdateError(payload.chain_id, payload.poll_id);
   }
 };
 
