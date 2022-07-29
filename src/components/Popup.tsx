66531a6ee0abb1362c8eaa8eb2e5fc724ea7cd26
Nenad Misic <n.misic@outlook.com>
2022-07-29 12:31:47 +0200

    Revert changes to Popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -2,21 +2,16 @@ import { PropsWithChildren } from "react";
 import { useModalContext } from "contexts/ModalContext";
 import Icon from "components/Icon";
 
-type Props = PropsWithChildren<{ message: string; canBeClosed?: boolean }>;
-
-export default function Popup(props: Props) {
+export default function Popup(props: PropsWithChildren<{ message: string }>) {
   const { closeModal } = useModalContext();
-  const { children, message, canBeClosed = true } = props;
 
   return (
     <div className="fixed-center z-10 p-4 grid grid-rows-1a place-items-center  bg-white-grey w-full max-w-xs min-h-115  rounded-xl shadow-lg overflow-hidden">
-      {canBeClosed && (
-        <button className="absolute top-3 right-3" onClick={closeModal}>
-          <Icon type="Close" className="text-angel-grey" />
-        </button>
-      )}
-      <p className="text-angel-grey text-center my-18">{message}</p>
-      {children}
+      <button className="absolute top-3 right-3" onClick={closeModal}>
+        <Icon type="Close" className="text-angel-grey" />
+      </button>
+      <p className="text-angel-grey text-center my-18">{props.message}</p>
+      {props.children}
     </div>
   );
 }
