c9df2880dfab85fbb149b4b281218fd3a616c66d
Nenad Misic <n.misic@outlook.com>
2022-07-29 08:43:16 +0200

    Move WalletContext to App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 import ModalContext from "contexts/ModalContext";
 import WalletContext from "contexts/WalletContext/WalletContext";
 import Loader from "components/Loader";
-import ErrorBoundary from "errors/ErrorBoundary";
 import AppFoot from "./AppFoot";
 import DappHead from "./DappHead";
 import Views from "./Views";
@@ -38,16 +37,14 @@ export default function App() {
         </div>
       ) : (
         <div className="grid grid-rows-a1">
-          <ErrorBoundary>
-            <WalletProvider {...chainOptions}>
-              <WalletContext>
-                <ModalContext backdropClasses="z-10 fixed inset-0 bg-black/50">
-                  <DappHead />
-                  <Views />
-                </ModalContext>
-              </WalletContext>
-            </WalletProvider>
-          </ErrorBoundary>
+          <WalletProvider {...chainOptions}>
+            <WalletContext>
+              <ModalContext backdropClasses="z-10 fixed inset-0 bg-black/50">
+                <DappHead />
+                <Views />
+              </ModalContext>
+            </WalletContext>
+          </WalletProvider>
         </div>
       )}
       <AppFoot />
