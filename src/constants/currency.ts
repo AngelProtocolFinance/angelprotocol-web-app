c5c1d7fc471507765dcaf7630350d06d9e6ead37
Nenad Misic <n.misic@outlook.com>
2022-08-01 11:37:00 +0200

    Make denoms into enum

--- src/constants/currency.ts
+++ src/constants/currency.ts
@@ -1,10 +1,10 @@
 import { GasPrice } from "@cosmjs/stargate";
 import { IS_TEST } from "./env";
 
-export const denoms = {
-  uusd: "uusd",
-  halo: "halo",
-};
+export enum denoms {
+  uusd = "uusd",
+  halo = "halo",
+}
 
 // TODO: uni-3 and juno-1 have diff gas prices for fee display only,
 // actual rate during submission is set by wallet - can be overridden with custom but keplr is buggy when customizing
