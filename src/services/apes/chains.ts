3bd9942b0afac04f3a3aa1fcea66724c2c70027d
Nenad Misic <n.misic@outlook.com>
2022-08-02 08:59:54 +0200

    Remove try/catch from chains.ts

--- src/services/apes/chains.ts
+++ src/services/apes/chains.ts
@@ -25,82 +25,69 @@ const chains_api = apes.injectEndpoints({
     chain: builder.query<Chain, { providerInfo: ProviderInfo }>({
       providesTags: [],
       async queryFn(args, queryApi, extraOptions, baseQuery) {
-        try {
-          const { address, chainId } = args.providerInfo;
-          const chainRes = await fetch(`${apes_endpoint}/chain/${chainId}`);
+        const { address, chainId } = args.providerInfo;
+        const chainRes = await fetch(`${apes_endpoint}/chain/${chainId}`);
 
-          const chain: Chain | { message: string } = await chainRes.json();
+        const chain: Chain | { message: string } = await chainRes.json();
 
-          if (!chain || "message" in chain) {
-            throw new UnsupportedNetworkError(chainId);
-          }
+        if (!chain || "message" in chain) {
+          throw new UnsupportedNetworkError(chainId);
+        }
 
-          // fetch balances for juno or terra
-          if (chain.type === "juno-native" || chain.type === "terra-native") {
-            const balancesRes = await fetch(
-              chain.lcd_url + `/cosmos/bank/v1beta1/balances/${address}`
+        // fetch balances for juno or terra
+        if (chain.type === "juno-native" || chain.type === "terra-native") {
+          const balancesRes = await fetch(
+            chain.lcd_url + `/cosmos/bank/v1beta1/balances/${address}`
+          );
+
+          // returns only positive balances
+          const { balances: nativeBalances }: { balances: Coin[] } =
+            await balancesRes.json();
+
+          // checking providerId to know which specific wallet is connected
+          // this way once Terra v2 is enabled on Keplr again, the users will be able to
+          // fetch their balances even when using Keplr
+          const cw20Balances = await getCW20Balance(chain, address);
+
+          const allBalances = nativeBalances.concat(cw20Balances);
+
+          [chain.native_currency, ...chain.tokens].forEach((token) => {
+            const balance = allBalances.find((x) => x.denom === token.token_id);
+            token.balance = +utils.formatUnits(
+              balance?.amount ?? 0,
+              token.decimals
             );
-
-            // returns only positive balances
-            const { balances: nativeBalances }: { balances: Coin[] } =
-              await balancesRes.json();
-
-            // checking providerId to know which specific wallet is connected
-            // this way once Terra v2 is enabled on Keplr again, the users will be able to
-            // fetch their balances even when using Keplr
-            const cw20Balances = await getCW20Balance(chain, address);
-
-            const allBalances = nativeBalances.concat(cw20Balances);
-
-            [chain.native_currency, ...chain.tokens].forEach((token) => {
-              const balance = allBalances.find(
-                (x) => x.denom === token.token_id
-              );
-              token.balance = +utils.formatUnits(
-                balance?.amount ?? 0,
-                token.decimals
-              );
-            });
-
-            return { data: chain };
-          }
-
-          /**fetch balances for ethereum */
-          const jsonProvider = new ethers.providers.JsonRpcProvider(
-            chain.rpc_url,
-            { chainId: +chainId, name: chain.chain_name }
-          );
-          const queryResults = await jsonProvider.getBalance(address);
-
-          chain.native_currency.balance = +utils.formatUnits(
-            queryResults,
-            chain.native_currency.decimals
-          );
-
-          const erc20Holdings = await getERC20Holdings(
-            chain.rpc_url,
-            address,
-            chain.tokens.map((token) => token.token_id)
-          );
-
-          chain.tokens.forEach((token) => {
-            const erc20 = erc20Holdings.find(
-              (x) => x.contractAddress === token.token_id
-            );
-            token.balance = +(erc20?.balance ?? 0); // erc20 balance is already in decimal format
           });
 
           return { data: chain };
-        } catch (err) {
-          console.log("Failed to get balances", err);
-          return {
-            error: {
-              status: 500,
-              statusText: "Query error",
-              data: "Failed to get balances",
-            },
-          };
         }
+
+        /**fetch balances for ethereum */
+        const jsonProvider = new ethers.providers.JsonRpcProvider(
+          chain.rpc_url,
+          { chainId: +chainId, name: chain.chain_name }
+        );
+        const queryResults = await jsonProvider.getBalance(address);
+
+        chain.native_currency.balance = +utils.formatUnits(
+          queryResults,
+          chain.native_currency.decimals
+        );
+
+        const erc20Holdings = await getERC20Holdings(
+          chain.rpc_url,
+          address,
+          chain.tokens.map((token) => token.token_id)
+        );
+
+        chain.tokens.forEach((token) => {
+          const erc20 = erc20Holdings.find(
+            (x) => x.contractAddress === token.token_id
+          );
+          token.balance = +(erc20?.balance ?? 0); // erc20 balance is already in decimal format
+        });
+
+        return { data: chain };
       },
     }),
   }),
