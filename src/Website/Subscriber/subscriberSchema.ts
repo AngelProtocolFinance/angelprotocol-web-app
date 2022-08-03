c66ec6db34ec9a5f2bab731cfb9f64f271985aee
Nenad Misic <n.misic@outlook.com>
2022-08-03 14:10:50 +0200

    Remove Website

--- src/Website/Subscriber/subscriberSchema.ts
+++
@@ -1,5 +0,0 @@
-import * as Yup from "yup";
-
-export const subscriberSchema = Yup.object().shape({
-  email: Yup.string().required("Email is required").email("Email is invalid"),
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,17 @@
-import { StrictMode, Suspense, lazy } from "react";
+import App from "App/App";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Loader from "components/Loader";
 import { store } from "store/store";
-import { siteRoutes } from "constants/routes";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
-const App = lazy(() => import("./App/App"));
-const Website = lazy(() => import("./Website/Website"));
-
-const LoaderComponent = () => (
-  <Loader bgColorClass="bg-angel-blue" gapClass="gap-2" widthClass="w-4" />
-);
 
 // Readin chain options as per the official docs:
 // https://docs.terra.money/docs/develop/wallet-provider/wallet-provider-tutorial.html#wrap-your-app-in-walletprovider
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Suspense fallback={<LoaderComponent />}>
-          <Routes>
-            <Route path={`${siteRoutes.app}/*`} element={<App />} />
-            <Route path={`${siteRoutes.home}*`} element={<Website />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+      <App />
     </Provider>
   </StrictMode>,
   document.getElementById("root")
