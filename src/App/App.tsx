1f90025fe1a91b969c8ac761ef4f7a403f6a8b40
Nenad Misic <n.misic@outlook.com>
2022-08-03 11:27:01 +0200

    Display note and footer on loading

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -11,8 +11,6 @@ import AppFoot from "./AppFoot";
 import DappHead from "./DappHead";
 import Views from "./Views";
 
-const backgroundColor = "bg-gradient-to-b from-blue-accent to-black-blue";
-
 export default function App() {
   const [chainOptions, setChainOptions] =
     useState<WalletControllerChainOptions>();
@@ -24,33 +22,31 @@ export default function App() {
     })();
   }, []);
 
-  if (!chainOptions) {
-    return (
-      <div
-        className={`flex h-screen items-center justify-center ${backgroundColor}`}
-      >
-        <Loader
-          bgColorClass="bg-white-grey"
-          gapClass="gap-2"
-          widthClass="w-4"
-        />
-      </div>
-    );
-  }
-
   return (
-    <div className={`grid grid-rows-aa1a ${backgroundColor}`}>
+    <div className="grid grid-rows-a1a bg-gradient-to-b from-blue-accent to-black-blue">
       <p className="transition-shadow flex z-20 items-center justify-center font-heading font-bold bg-angel-orange w-full p-2 text-center text-angel-grey text-xs">
         Please note: Donations are currently disabled. V2 coming soon!
       </p>
-      <WalletProvider {...chainOptions}>
-        <WalletContext>
-          <ModalContext backdropClasses="z-10 fixed inset-0 bg-black/50">
-            <DappHead />
-            <Views />
-          </ModalContext>
-        </WalletContext>
-      </WalletProvider>
+      {!chainOptions ? (
+        <div className="flex items-center justify-center">
+          <Loader
+            bgColorClass="bg-white-grey"
+            gapClass="gap-2"
+            widthClass="w-4"
+          />
+        </div>
+      ) : (
+        <div className="grid grid-rows-a1">
+          <WalletProvider {...chainOptions}>
+            <WalletContext>
+              <ModalContext backdropClasses="z-10 fixed inset-0 bg-black/50">
+                <DappHead />
+                <Views />
+              </ModalContext>
+            </WalletContext>
+          </WalletProvider>
+        </div>
+      )}
       <AppFoot />
     </div>
   );
