f7ec7324428823016000982b4ff988f86a4e7084
ap-justin <justin@angelprotocol.io>
2022-08-05 18:12:12 +0800

    fix admin layout

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,7 +4,7 @@ import Views from "./Views";
 
 export default function App() {
   return (
-    <div className="grid grid-rows-a1a bg-gradient-to-b from-blue-accent to-black-blue bg-fixed">
+    <div className="grid grid-rows-aa1a bg-gradient-to-b from-blue-accent to-black-blue bg-fixed">
       <p className="transition-shadow flex z-20 items-center justify-center font-heading font-bold bg-angel-orange w-full p-2 text-center text-angel-grey text-xs">
         Please note: Donations are currently disabled. V2 coming soon!
       </p>
