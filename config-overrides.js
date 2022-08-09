75bcf50d161717de5e31c756bc0a97108e65ac14
Nenad Misic <n.misic@outlook.com>
2022-08-09 12:16:47 +0200

    Add webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
 const { ProvidePlugin } = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = function override(config, env) {
   config.resolve.fallback = {
@@ -12,7 +14,8 @@ module.exports = function override(config, env) {
     new ProvidePlugin({
       Buffer: ["buffer", "Buffer"],
       process: "process/browser",
-    })
+    }),
+    new BundleAnalyzerPlugin()
   );
 
   return config;
