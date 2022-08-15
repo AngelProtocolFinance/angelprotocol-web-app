d601afd3abf99ce605f0b5d64137076b14d7c023
ap-justin <justin@angelprotocol.io>
2022-08-15 14:49:26 +0800

    done cw4

--- src/contracts/CW4.ts
+++ src/contracts/CW4.ts
@@ -1,26 +1,7 @@
-import { ContractQueryArgs } from "services/types";
 import { Member } from "types/server/contracts";
-import { WalletState } from "contexts/WalletContext/WalletContext";
 import Contract from "./Contract";
 
 export default class CW4 extends Contract {
-  members: ContractQueryArgs;
-  member: ContractQueryArgs;
-
-  constructor(wallet: WalletState | undefined, address: string) {
-    super(wallet, address);
-
-    this.members = {
-      address: this.contractAddress,
-      msg: { list_members: {} },
-    };
-
-    this.member = {
-      address: this.contractAddress,
-      msg: { member: { addr: this.walletAddress } },
-    };
-  }
-
   createEmbeddedUpdateMembersMsg(to_add: Member[], to_remove: string[]) {
     return this.createEmbeddedWasmMsg([], {
       update_members: {
--- src/pages/Admin/Templates/cw4/MemberUpdator/MemberUpdator.tsx
+++ src/pages/Admin/Templates/cw4/MemberUpdator/MemberUpdator.tsx
@@ -6,21 +6,20 @@ import { useAdminResources } from "pages/Admin/Guard";
 import FormError from "pages/Admin/common/FormError";
 import FormSkeleton from "pages/Admin/common/FormSkeleton";
 import { useMembersQuery } from "services/juno/cw4";
+import { queryObject } from "services/juno/queryContract/queryObjects";
 import { useSetter } from "store/accessors";
 import { setMembers } from "slices/admin/apCW4Members";
-import CW4 from "contracts/CW4";
 import MemberUpdateForm from "./MemberUpdaterForm";
 import { memberUpdatorSchema } from "./memberUpdatorSchema";
 
 export default function MemberUpdator() {
   const { cw4 } = useAdminResources();
   const dispatch = useSetter();
-  const contract = new CW4(undefined, cw4);
   const {
     data: members = [],
     isLoading,
     isError,
-  } = useMembersQuery(contract.members);
+  } = useMembersQuery({ address: cw4, msg: queryObject.cw4Members });
 
   useEffect(() => {
     if (members.length > 0) {
