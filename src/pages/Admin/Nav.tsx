86ee84636e93d9f0719c5a674eed6448b9eab464
ap-justin <89639563+ap-justin@users.noreply.github.com>
2022-08-17 07:31:23 +0800

    bug fixes (#1154)

--- src/pages/Admin/Nav.tsx
+++ src/pages/Admin/Nav.tsx
@@ -40,6 +40,6 @@ export default function Nav() {
 }
 
 const styler = createNavLinkStyler(
-  "px-2 uppercase text-sm text-center font-semibold font-heading text-white",
+  "px-2 uppercase text-sm text-center font-semibold font-heading text-white-grey",
   "text-angel-orange"
 );
