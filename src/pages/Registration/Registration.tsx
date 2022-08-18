ba726e818fd439929ef6fc28c4e3fc4bbbd4e013
Nenad Misic <n.misic@outlook.com>
2022-08-18 15:02:17 +0200

    Rename /Registration/Registration/Registration.tsx -> /Registration/WelcomeScreen.tsx

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -8,7 +8,7 @@ const AdditionalInformation = lazy(() => import("./AdditionalInformation"));
 const ConfirmEmail = lazy(() => import("./ConfirmEmail"));
 const ContactDetails = lazy(() => import("./ContactDetails"));
 const Dashboard = lazy(() => import("./Dashboard"));
-const Registration = lazy(() => import("./Registration"));
+const WelcomeScreen = lazy(() => import("./WelcomeScreen"));
 const VerifiedEmail = lazy(() => import("./VerifiedEmail"));
 const WalletRegistration = lazy(() => import("./WalletRegistration"));
 const Documentation = lazy(() => import("./Documentation"));
@@ -60,7 +60,7 @@ export default function Register() {
             </StepOneCompleteGuard>
           }
         />
-        <Route index element={<Registration />} />
+        <Route index element={<WelcomeScreen />} />
         <Route
           path="*"
           element={
