8222881ef6db89da788273498a68cfafcf10254a
Nenad Misic <n.misic@outlook.com>
2022-08-19 13:01:25 +0200

    Revert Transactor/* changes

--- src/components/Transactor/index.tsx
+++ src/components/Transactor/index.tsx
@@ -1,1 +1,20 @@
-export { default } from "./Transactor";
+import { FC } from "react";
+import TransactionPrompt from "./TransactionPrompt";
+
+export type TxProps<C> = {
+  Content: FC<C>;
+  contentProps: C;
+  inModal?: false;
+};
+
+export default function Transactor<C>({
+  Content,
+  contentProps,
+  inModal,
+}: TxProps<C>) {
+  return (
+    <TransactionPrompt inModal={inModal}>
+      <Content {...contentProps} />
+    </TransactionPrompt>
+  );
+}
