cfa8595250ef02ae42a20af7e42a7a67197ba893
ap-justin <justin@angelprotocol.io>
2022-08-22 16:05:10 +0800

    add meta to refetch endow list

--- src/pages/Admin/reviewer/Applications/Table/Application/useProposeStatusChange.ts
+++ src/pages/Admin/reviewer/Applications/Table/Application/useProposeStatusChange.ts
@@ -1,3 +1,4 @@
+import { EndowmentStatusMeta } from "pages/Admin/types";
 import { CharityApplication } from "types/server/aws";
 import {
   EndowmentStatusNum,
@@ -14,6 +15,7 @@ import Registrar from "contracts/Registrar";
 import { cleanObject } from "helpers/admin/cleanObject";
 import { logStatusUpdateProposal } from "./logStatusUpdateProposal";
 
+const NUM_ID = 15; //TODO: get this from ap (not yet present)
 export default function useProposeStatusChange(app: CharityApplication) {
   const dispatch = useSetter();
   const { wallet } = useGetWallet();
@@ -23,7 +25,7 @@ export default function useProposeStatusChange(app: CharityApplication) {
   function updateStatus(status: Extract<EndowmentStatusNum, 1 | 3>) {
     const statusChangePayload: StatusChangePayload = {
       status,
-      endowment_id: 16, //TODO: get this from ap (not yet present)
+      endowment_id: NUM_ID,
     };
     const statusWord = status === 1 ? "Approve" : "Reject";
 
@@ -33,11 +35,21 @@ export default function useProposeStatusChange(app: CharityApplication) {
         cleanObject(statusChangePayload)
       );
 
+    const statusUpdateMeta: EndowmentStatusMeta = {
+      type: "reg_endow_status",
+      data: {
+        id: NUM_ID,
+        fromStatus: "Inactive",
+        toStatus: `${status}`,
+      },
+    };
+
     const contract = new CW3(wallet, cw3 /** cw3Reviewer */);
     const proposalMsg = contract.createProposalMsg(
       `${statusWord} ${app.CharityName}`,
-      "",
-      [embeddedMsg]
+      `registration id: ${app.PK}`,
+      [embeddedMsg],
+      JSON.stringify(statusUpdateMeta)
     );
 
     dispatch(
