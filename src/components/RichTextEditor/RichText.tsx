050899501262e768d54baa6ec16037f2ef97f7d2
ap-justin <justin@angelprotocol.io>
2022-08-23 15:10:50 +0800

    remove listener cleanup

--- src/components/RichTextEditor/RichText.tsx
+++ src/components/RichTextEditor/RichText.tsx
@@ -1,6 +1,6 @@
 import Quill from "quill";
 import "quill/dist/quill.bubble.css";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useState } from "react";
 import "./richtext.css";
 
 type ReadOnly = {
@@ -21,24 +21,12 @@ type Props = (ReadOnly | Editable) & {
 };
 
 export default function RichText(props: Props) {
-  const quillRef = useRef<Quill>();
-  const containerRef = useRef<HTMLDivElement>(null);
-
   const [numChars, setNumChars] = useState(0);
 
-  function handleEditorChange() {
-    if (!quillRef.current || !props.onChange) return;
-    const length = quillRef.current.getLength();
-    setNumChars(length - 1); //quill content min length is 1
-    props.onChange(
-      //quill clean state has residual `\n`
-      length <= 1 ? "" : JSON.stringify(quillRef.current.getContents())
-    );
-  }
+  const containerRef = useCallback((container: HTMLDivElement | null) => {
+    if (!container) return;
 
-  useEffect(() => {
-    if (quillRef.current) return;
-    quillRef.current = new Quill(containerRef.current as Element, {
+    const quill = new Quill(container, {
       placeholder: props.placeHolder,
       readOnly: props.readOnly,
       theme: "bubble",
@@ -52,11 +40,11 @@ export default function RichText(props: Props) {
     });
 
     try {
-      quillRef.current.setContents(JSON.parse(props.content));
+      quill.setContents(JSON.parse(props.content));
     } catch (err) {
       //previous rich text format based on draft-js will throw parse error
       //in this case just set it to blank
-      quillRef.current.setContents(quillRef.current.getContents());
+      quill.setContents(quill.getContents());
     }
 
     if (!props.readOnly) {
@@ -64,18 +52,23 @@ export default function RichText(props: Props) {
       after mounting, set form value to "", so that form state with initially empty quill value
       will be blocked by validation on re-submit
       */
-      if (quillRef.current.getLength() <= 1) {
+      if (quill.getLength() <= 1) {
         props.onChange("");
       }
 
-      quillRef.current.on("editor-change", handleEditorChange);
+      quill.on("editor-change", function handleChange() {
+        if (!props.onChange) return;
+        const length = quill.getLength();
+        setNumChars(length - 1); //quill content min length is 1
+        props.onChange(
+          //quill clean state has residual `\n`
+          length <= 1 ? "" : JSON.stringify(quill.getContents())
+        );
+      });
+      /**NOTE: `const quill` will be garbage collected when RichText unmounts,
+       * no need to add quill.off cleanup
+       */
     }
-
-    return () => {
-      if (!props.readOnly) {
-        quillRef.current?.off("editor-change", handleEditorChange);
-      }
-    };
     //eslint-disable-next-line
   }, []);
 
