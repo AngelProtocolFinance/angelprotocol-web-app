28d7ced94e1f4e9d8f8c011239341d397f652fcc
Nenad Misic <n.misic@outlook.com>
2022-08-25 12:05:17 +0200

    Move emailVerified check to ContactDetailsCompleteGuard

--- src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
+++ src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useRegistrationQuery } from "services/aws/registration";
 import { appRoutes } from "constants/routes";
 import RegLoader from "../common/RegLoader";
+import routes from "../routes";
 
 export function ContactDetailsCompleteGuard(props: any) {
   const { data: charity, isLoading } = useRegistrationQuery("");
@@ -12,6 +13,12 @@ export function ContactDetailsCompleteGuard(props: any) {
 
   if (!charity || !charity.ContactPerson.Email) {
     return <Navigate to={appRoutes.register} />;
+  }
+
+  // if EmailVerified === true this means the charity has finished contact details step but hasn't initiated an update of contact details
+  // we therefore navigate to dashboard and let its guard decide whether they should be allowed in
+  if (charity.ContactPerson.EmailVerified) {
+    return <Navigate to={`${appRoutes.register}/${routes.dashboard}`} />;
   }
 
   return <>{props.children}</>;
