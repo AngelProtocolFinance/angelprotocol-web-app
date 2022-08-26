0a7a1b2fc1bdb07a33a75ff277a85808c8b6a137
Nenad Misic <n.misic@outlook.com>
2022-08-26 11:41:41 +0200

    Remove EmailVerified check from ContactDetailsCompleteGuard

--- src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
+++ src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 import { useRegistrationQuery } from "services/aws/registration";
 import { appRoutes } from "constants/routes";
 import RegLoader from "../common/RegLoader";
-import routes from "../routes";
 
 export function ContactDetailsCompleteGuard(props: PropsWithChildren<{}>) {
   const { data: charity, isLoading } = useRegistrationQuery("");
@@ -14,12 +13,6 @@ export function ContactDetailsCompleteGuard(props: PropsWithChildren<{}>) {
 
   if (!charity || !charity.ContactPerson.Email) {
     return <Navigate to={appRoutes.register} />;
-  }
-
-  // if EmailVerified === true this means the charity has finished contact details step but hasn't initiated an update of contact details
-  // we therefore navigate to dashboard and let its guard decide whether they should be allowed in
-  if (charity.ContactPerson.EmailVerified) {
-    return <Navigate to={`${appRoutes.register}/${routes.dashboard}`} />;
   }
 
   return <>{props.children}</>;
