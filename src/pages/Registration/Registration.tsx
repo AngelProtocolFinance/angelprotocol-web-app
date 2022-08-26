3ecf6e2c17294c3ba184b85a4abdf2b3cbc2e3d9
Nenad Misic <n.misic@outlook.com>
2022-08-26 11:17:26 +0200

    Read registration data from aws in Registration

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,6 +1,8 @@
 import { lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useRegistrationQuery } from "services/aws/registration";
 import { appRoutes } from "constants/routes";
+import RegLoader from "./common/RegLoader";
 import {
   AdditionalInformationCompleteGuard,
   ContactDetailsCompleteGuard,
@@ -19,6 +21,12 @@ const VerifiedEmail = lazy(() => import("./VerifiedEmail"));
 const WalletRegistration = lazy(() => import("./WalletRegistration"));
 
 export default function Registration() {
+  const { isLoading } = useRegistrationQuery("");
+
+  if (isLoading) {
+    return <RegLoader />;
+  }
+
   return (
     <Container>
       <Routes>
--- src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
+++ src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
@@ -2,15 +2,10 @@ import { PropsWithChildren } from "react";
 import { Navigate } from "react-router-dom";
 import { useRegistrationState } from "services/aws/registration";
 import { appRoutes } from "constants/routes";
-import RegLoader from "../common/RegLoader";
 import routes from "../routes";
 
 export function ContactDetailsCompleteGuard(props: PropsWithChildren<{}>) {
-  const { charity, isLoading } = useRegistrationState();
-
-  if (isLoading) {
-    return <RegLoader />;
-  }
+  const { charity } = useRegistrationState();
 
   if (!charity.ContactPerson.Email) {
     return <Navigate to={appRoutes.register} />;
