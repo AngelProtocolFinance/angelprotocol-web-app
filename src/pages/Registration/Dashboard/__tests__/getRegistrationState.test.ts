0d7a092b6f10608efc523976b462a64d63c1d4b0
Nenad Misic <n.misic@outlook.com>
2022-08-26 08:58:07 +0200

    Refactor getRegistrationState.test 'sets all steps to complete...' tests

--- src/pages/Registration/Dashboard/__tests__/getRegistrationState.test.ts
+++ src/pages/Registration/Dashboard/__tests__/getRegistrationState.test.ts
@@ -222,83 +222,76 @@ describe("getRegistrationState tests", () => {
     expect(state.getIsReadyForSubmit()).toBe(false);
   });
 
-  it("sets all steps to complete even when Inactive", () => {
-    runSetsAllStepsToCompleteEvenWhen("Inactive");
-  });
-
-  it("sets all steps to complete even when Under Review", () => {
-    runSetsAllStepsToCompleteEvenWhen("Under Review");
-  });
-
-  it("sets all steps to complete even when Approved", () => {
-    runSetsAllStepsToCompleteEvenWhen("Approved");
-  });
-
-  it("sets all steps to complete even when Active", () => {
-    runSetsAllStepsToCompleteEvenWhen("Active");
-  });
-
-  function runSetsAllStepsToCompleteEvenWhen(status: RegistrationStatus) {
-    const charity: Charity = {
-      ContactPerson: {
-        Email: "test@test.com",
-        EmailVerified: true,
-        FirstName: "first",
-        LastName: "last",
-        Goals: "hello world",
-        PhoneNumber: "+114323888",
-        ReferralMethod: "angel-alliance",
-        PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
-        Role: "ceo",
-        SK: "ContactPerson",
-      },
-      Registration: {
-        AuditedFinancialReports: [],
-        AuditedFinancialReportsVerified: false,
-        CharityName: "charity",
-        CharityName_ContactEmail: "CHARITY_test@test.com",
-        FinancialStatements: [],
-        FinancialStatementsVerified: false,
-        ProofOfIdentity: {
-          name: "poi",
-          publicUrl: "https://www.storage.path/poi",
+  const statuses: RegistrationStatus[] = [
+    "Inactive",
+    "Under Review",
+    "Approved",
+    "Active",
+  ];
+  test.each(statuses)(
+    "sets all steps to complete even when registration status is %j",
+    (status) => {
+      const charity: Charity = {
+        ContactPerson: {
+          Email: "test@test.com",
+          EmailVerified: true,
+          FirstName: "first",
+          LastName: "last",
+          Goals: "hello world",
+          PhoneNumber: "+114323888",
+          ReferralMethod: "angel-alliance",
+          PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
+          Role: "ceo",
+          SK: "ContactPerson",
         },
-        ProofOfIdentityVerified: true,
-        ProofOfRegistration: {
-          name: "por",
-          publicUrl: "https://www.storage.path/por",
+        Registration: {
+          AuditedFinancialReports: [],
+          AuditedFinancialReportsVerified: false,
+          CharityName: "charity",
+          CharityName_ContactEmail: "CHARITY_test@test.com",
+          FinancialStatements: [],
+          FinancialStatementsVerified: false,
+          ProofOfIdentity: {
+            name: "poi",
+            publicUrl: "https://www.storage.path/poi",
+          },
+          ProofOfIdentityVerified: true,
+          ProofOfRegistration: {
+            name: "por",
+            publicUrl: "https://www.storage.path/por",
+          },
+          ProofOfRegistrationVerified: true,
+          RegistrationDate: "2022-05-04T10:10:10Z",
+          RegistrationStatus: status,
+          SK: "Registration",
+          Tier: 1,
+          UN_SDG: 0,
+          Website: "www.test.com",
         },
-        ProofOfRegistrationVerified: true,
-        RegistrationDate: "2022-05-04T10:10:10Z",
-        RegistrationStatus: status,
-        SK: "Registration",
-        Tier: 1,
-        UN_SDG: 0,
-        Website: "www.test.com",
-      },
-      Metadata: {
-        Banner: {
-          name: "banner",
-          publicUrl: "https://www.storage.path/banner",
+        Metadata: {
+          Banner: {
+            name: "banner",
+            publicUrl: "https://www.storage.path/banner",
+          },
+          CharityLogo: {
+            name: "logo",
+            publicUrl: "https://www.storage.path/logo",
+          },
+          CharityOverview: "some overview",
+          EndowmentContract: "juno1ke4aktw6zvz2jxsyqx55ejsj7rmxdl9p5xywus",
+          SK: "Metadata",
+          JunoWallet: "juno1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
+          KycDonorsOnly: false,
         },
-        CharityLogo: {
-          name: "logo",
-          publicUrl: "https://www.storage.path/logo",
-        },
-        CharityOverview: "some overview",
-        EndowmentContract: "juno1ke4aktw6zvz2jxsyqx55ejsj7rmxdl9p5xywus",
-        SK: "Metadata",
-        JunoWallet: "juno1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
-        KycDonorsOnly: false,
-      },
-    };
+      };
 
-    const state = getRegistrationState(charity);
+      const state = getRegistrationState(charity);
 
-    expect(state.stepOne.completed).toBe(true);
-    expect(state.stepTwo.completed).toBe(true);
-    expect(state.stepThree.completed).toBe(true);
-    expect(state.stepFour.completed).toBe(true);
-    expect(state.getIsReadyForSubmit()).toBe(true);
-  }
+      expect(state.stepOne.completed).toBe(true);
+      expect(state.stepTwo.completed).toBe(true);
+      expect(state.stepThree.completed).toBe(true);
+      expect(state.stepFour.completed).toBe(true);
+      expect(state.getIsReadyForSubmit()).toBe(true);
+    }
+  );
 });
