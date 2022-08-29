7a20e244ab72a759e523956a16db8f4883b8afac
Nenad Misic <n.misic@outlook.com>
2022-08-29 11:34:43 +0200

    Update 'Back' button logic on WalletRegistration

--- src/pages/Registration/WalletRegistration/ChooseWallet/index.tsx
+++ src/pages/Registration/WalletRegistration/ChooseWallet/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "pages/Registration/common";
+import { useRegistrationState } from "services/aws/registration";
 import { appRoutes } from "constants/routes";
 import { default as registerRoutes } from "../../routes";
 import KeplrConnector from "./KeplrConnector";
@@ -7,17 +8,21 @@ import Title from "./Title";
 
 export default function ChooseWallet() {
   const navigate = useNavigate();
+  const { charity } = useRegistrationState();
+
+  const onClick = () => {
+    const url = charity.Metadata.JunoWallet
+      ? `${appRoutes.register}/${registerRoutes.dashboard}`
+      : `${appRoutes.register}/${registerRoutes.additionalInformation}`;
+    navigate(url);
+  };
+
   return (
     <div className="flex flex-col gap-5 items-center">
       <Title />
       <KeplrConnector />
-      <Button
-        className="bg-green-400 w-80 h-10"
-        onClick={() =>
-          navigate(`${appRoutes.register}/${registerRoutes.dashboard}`)
-        }
-      >
-        Back to registration dashboard
+      <Button className="bg-green-400 w-80 h-10" onClick={onClick}>
+        Back
       </Button>
     </div>
   );
