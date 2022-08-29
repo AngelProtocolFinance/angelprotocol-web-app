dd4250d4ce5e2ce54dd66808666344eed547856f
Nenad Misic <n.misic@outlook.com>
2022-08-29 12:32:42 +0200

    Add auto-navigation on useRegisterWallet

--- src/pages/Registration/WalletRegistration/useRegisterWallet.ts
+++ src/pages/Registration/WalletRegistration/useRegisterWallet.ts
@@ -1,17 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import {
   useRegistrationState,
   useUpdateCharityMetadataMutation,
 } from "services/aws/registration";
 import { useErrorContext } from "contexts/ErrorContext";
 import { UnexpectedStateError } from "errors/errors";
+import { appRoutes } from "constants/routes";
 import { FORM_ERROR } from "../constants";
+import routes from "../routes";
 import { Wallet } from "./WalletSubmission";
 
 export default function useRegisterWallet() {
   const { charity } = useRegistrationState();
   const [updateMetadata, { isSuccess, isLoading }] =
     useUpdateCharityMetadataMutation();
-
+  const navigate = useNavigate();
   const { handleError } = useErrorContext();
 
   const registerWallet = async (data: Wallet) => {
@@ -27,6 +30,12 @@ export default function useRegisterWallet() {
 
       if ("error" in result) {
         handleError(result.error, "Error updating wallet ❌");
+      }
+
+      if (charity.ContactPerson.EmailVerified) {
+        navigate(`${appRoutes.register}/${routes.dashboard}`);
+      } else {
+        navigate(`${appRoutes.register}/${routes.confirmEmail}`);
       }
     } catch (err) {
       handleError(err, FORM_ERROR);
