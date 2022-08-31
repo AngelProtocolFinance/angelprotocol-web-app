90026956c150162ed56f218a4de492e9eed42165
ap-justin <justin@angelprotocol.io>
2022-08-31 22:29:13 +0800

    strats

--- src/pages/Admin/Charity/Strategies/index.tsx
+++ src/pages/Admin/Charity/Strategies/index.tsx
@@ -3,12 +3,33 @@ import { useAdminResources } from "pages/Admin/Guard";
 export default function Strategies() {
   const { endowment } = useAdminResources();
 
-  if (
-    endowment.strategies.liquid.length <= 0 &&
-    endowment.strategies.locked.length <= 0
-  ) {
-    return <div>add a strategy</div>;
-  }
+  // if (
+  //   endowment.strategies.liquid.length <= 0 &&
+  //   endowment.strategies.locked.length <= 0
+  // ) {
+  //   return <div>add a strategy</div>;
+  // }
 
-  return <div></div>;
+  return <div>strategies</div>;
 }
+
+interface StrategyComponent {
+  vault: string; // Vault SC Address
+  percentage: string; // percentage of funds to invest
+}
+
+interface AccountStrategies {
+  locked: StrategyComponent[];
+  liquid: StrategyComponent[];
+}
+
+const strategies: AccountStrategies = {
+  locked: [
+    { vault: "a", percentage: "0.5" },
+    { vault: "b", percentage: "0.5" },
+  ],
+  liquid: [
+    { vault: "a", percentage: "0.5" },
+    { vault: "b", percentage: "0.5" },
+  ],
+};
