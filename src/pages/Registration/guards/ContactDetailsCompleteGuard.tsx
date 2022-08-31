1e69c10f88579d3abccd7ccb6b53e79e6afefe07
Nenad Misic <n.misic@outlook.com>
2022-08-31 12:00:10 +0200

    Add isError check to ContactDetailsCompleteGuard

--- src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
+++ src/pages/Registration/guards/ContactDetailsCompleteGuard.tsx
@@ -1,12 +1,26 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useRegistrationQuery } from "services/aws/registration";
+import { useErrorContext } from "contexts/ErrorContext";
 import { appRoutes } from "constants/routes";
 import RegLoader from "../common/RegLoader";
 import routes from "../routes";
 
 export function ContactDetailsCompleteGuard(props: PropsWithChildren<{}>) {
-  const { data: charity, isLoading, isFetching } = useRegistrationQuery("");
+  const {
+    data: charity,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useRegistrationQuery("");
+  const { handleError } = useErrorContext();
+
+  useEffect(() => {
+    if (!isLoading && !isFetching && isError) {
+      handleError(error);
+    }
+  }, [isLoading, isFetching, isError, error, handleError]);
 
   if (isLoading || isFetching) {
     return <RegLoader />;
