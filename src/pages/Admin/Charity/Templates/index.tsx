22d938a52fd8868c6042300932199294061cd2bf
ap-justin <justin@angelprotocol.io>
2022-08-31 10:42:46 +0800

    wrap routes

--- src/pages/Admin/Charity/Templates/index.tsx
+++ src/pages/Admin/Charity/Templates/index.tsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { templateRoutes as routes } from "../../constants";
 import Config from "../../templates/cw3/Config";
 import FundSender from "../../templates/cw3/FundSender";
@@ -10,15 +10,17 @@ export default function Templates() {
   return (
     <div className="grid gap-2 grid-cols-[auto_1fr]">
       <Nav />
-      {/** _endowment */}
-      <Route path={routes.acc_profile} element={<ProfileEditor />} />
+      <Routes>
+        {/** _endowment */}
+        <Route path={routes.acc_profile} element={<ProfileEditor />} />
 
-      {/**_cw3 */}
-      <Route path={routes.cw3_config} element={<Config />} />
-      <Route path={routes.cw3_transfer} element={<FundSender />} />
+        {/**_cw3 */}
+        <Route path={routes.cw3_config} element={<Config />} />
+        <Route path={routes.cw3_transfer} element={<FundSender />} />
 
-      {/**_cw4 */}
-      <Route path={routes.cw4_members} element={<Members />} />
+        {/**_cw4 */}
+        <Route path={routes.cw4_members} element={<Members />} />
+      </Routes>
     </div>
   );
 }
