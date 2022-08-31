40c57652f2496cb73b6f06aca50246a56088ed90
Nenad Misic <n.misic@outlook.com>
2022-08-31 09:18:10 +0200

    Move WalletRegistration back to index.tsx

--- src/pages/Registration/WalletRegistration/index.tsx
+++ src/pages/Registration/WalletRegistration/index.tsx
@@ -1,1 +1,25 @@
-export { default } from "./WalletRegistration";
+import { useRegistrationState } from "services/aws/registration";
+import { useGetWallet } from "contexts/WalletContext/WalletContext";
+import RegLoader from "../common/RegLoader";
+import ChooseWallet from "./ChooseWallet";
+import RegisteredWallet from "./RegisteredWallet";
+import WalletSubmission from "./WalletSubmission";
+
+export default function WalletRegistration() {
+  const { charity } = useRegistrationState();
+  const { wallet, isLoading } = useGetWallet();
+
+  if (charity.Metadata.JunoWallet) {
+    return <RegisteredWallet />;
+  }
+
+  if (isLoading) {
+    return <RegLoader message="Wallet is loading" />;
+  }
+
+  if (!wallet) {
+    return <ChooseWallet />;
+  }
+
+  return <WalletSubmission />;
+}
