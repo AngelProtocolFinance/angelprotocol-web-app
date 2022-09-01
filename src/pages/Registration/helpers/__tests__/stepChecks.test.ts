71bbf034c75c33e55e30ccf6be7eb01bbbcf1644
Nenad Misic <n.misic@outlook.com>
2022-09-01 18:35:05 +0200

    Move charity mocks to stepChecks.test

--- src/pages/Registration/helpers/__tests__/stepChecks.test.ts
+++ src/pages/Registration/helpers/__tests__/stepChecks.test.ts
@@ -8,12 +8,6 @@ import {
   getDocumentationStepData,
   getWalletRegistrationStepData,
 } from "../stepChecks";
-import {
-  ADDITIONAL_INFO_STEP_COMPLETE_CHARITY,
-  CONTACT_DETAILS_STEP_COMPLETE_CHARITY,
-  DOCUMENTATION_STEP_COMPLETE_CHARITY,
-  VALIDLY_FILLED_CHARITY,
-} from "./data";
 
 describe("stepChecks tests", () => {
   describe("getContactDetailsStepData tests", () => {
@@ -157,3 +151,76 @@ describe("stepChecks tests", () => {
     });
   });
 });
+
+const VALIDLY_FILLED_CHARITY: Charity = {
+  ContactPerson: {
+    Email: "test@test.com",
+    EmailVerified: true,
+    Goals: "i have a goal",
+    FirstName: "first",
+    LastName: "last",
+    PhoneNumber: "+114323888",
+    PK: "7fe792be-5132-4f2b-b37c-4bcd9445b773",
+    ReferralMethod: "angel-alliance",
+    Role: "ceo",
+    SK: "ContactPerson",
+  },
+  Registration: {
+    CharityName: "charity",
+    CharityName_ContactEmail: "CHARITY_test@test.com",
+    RegistrationDate: "2022-05-04T10:10:10Z",
+    RegistrationStatus: "Inactive",
+    SK: "Registration",
+    UN_SDG: 1,
+    Tier: 1,
+    Website: "https://www.example.com",
+    ProofOfIdentity: { name: "poi.pdf", publicUrl: "https://path.to.poi" },
+    ProofOfIdentityVerified: false,
+    ProofOfRegistration: { name: "por.pdf", publicUrl: "https://path.to.por" },
+    ProofOfRegistrationVerified: false,
+    AuditedFinancialReports: [
+      { name: "afr.pdf", publicUrl: "https://path.to.afr" },
+    ],
+    AuditedFinancialReportsVerified: false,
+    FinancialStatements: [{ name: "fs.pdf", publicUrl: "https://path.to.fs" }],
+    FinancialStatementsVerified: false,
+  },
+  Metadata: {
+    SK: "Metadata",
+    Banner: { name: "banner.png", publicUrl: "https://path.to.banner" },
+    CharityLogo: { name: "logo.png", publicUrl: "https://path.to.logo" },
+    CharityOverview: "text",
+    EndowmentContract: "juno1ke4aktw6zvz2jxsyqx55ejsj7rmxdl9p5xywus",
+    JunoWallet: "juno1wf89rf7xeuuk5td9gg2vd2uzytrqyw49l24rek",
+    KycDonorsOnly: false,
+  },
+};
+
+const CONTACT_DETAILS_STEP_COMPLETE_CHARITY: Charity = {
+  ...placeholderCharity,
+  ContactPerson: { ...VALIDLY_FILLED_CHARITY.ContactPerson },
+  Registration: {
+    ...placeholderCharity.Registration,
+    CharityName: VALIDLY_FILLED_CHARITY.Registration.CharityName,
+    CharityName_ContactEmail:
+      VALIDLY_FILLED_CHARITY.Registration.CharityName_ContactEmail,
+    RegistrationDate: VALIDLY_FILLED_CHARITY.Registration.RegistrationDate,
+    RegistrationStatus: VALIDLY_FILLED_CHARITY.Registration.RegistrationStatus,
+  },
+};
+
+const DOCUMENTATION_STEP_COMPLETE_CHARITY: Charity = {
+  ...VALIDLY_FILLED_CHARITY,
+  Metadata: { ...placeholderCharity.Metadata },
+};
+
+const ADDITIONAL_INFO_STEP_COMPLETE_CHARITY: Charity = {
+  ContactPerson: { ...VALIDLY_FILLED_CHARITY.ContactPerson },
+  Registration: { ...VALIDLY_FILLED_CHARITY.Registration },
+  Metadata: {
+    ...placeholderCharity.Metadata,
+    Banner: { ...VALIDLY_FILLED_CHARITY.Metadata.Banner },
+    CharityLogo: { ...VALIDLY_FILLED_CHARITY.Metadata.CharityLogo },
+    CharityOverview: VALIDLY_FILLED_CHARITY.Metadata.CharityOverview,
+  },
+};
