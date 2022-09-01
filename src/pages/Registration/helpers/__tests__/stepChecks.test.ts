7ac89fe8c184c59f8acadcc7d91dd0822d95fafe
Nenad Misic <n.misic@outlook.com>
2022-09-01 17:53:28 +0200

    Add getAdditionalInformationStepData tests

--- src/pages/Registration/helpers/__tests__/stepChecks.test.ts
+++ src/pages/Registration/helpers/__tests__/stepChecks.test.ts
@@ -3,6 +3,7 @@ import routes from "pages/Registration/routes";
 import { placeholderCharity } from "services/aws/registration";
 import { appRoutes } from "constants/routes";
 import {
+  getAdditionalInformationStepData,
   getContactDetailsStepData,
   getDocumentationStepData,
 } from "../stepChecks";
@@ -86,6 +87,77 @@ describe("stepChecks tests", () => {
       expect(stepData.urlToPreviousStep).toBe(
         `${appRoutes.register}/${routes.documentation}`
       );
+    });
+
+    describe("getAdditionalInformationStepData tests", () => {
+      test("returns 'completed' result when Additional Information and previous steps' data are validly set", () => {
+        const charity: Charity = {
+          ContactPerson: { ...VALIDLY_FILLED_CHARITY.ContactPerson },
+          Registration: { ...VALIDLY_FILLED_CHARITY.Registration },
+          Metadata: {
+            ...placeholderCharity.Metadata,
+            Banner: { ...VALIDLY_FILLED_CHARITY.Metadata.Banner },
+            CharityLogo: { ...VALIDLY_FILLED_CHARITY.Metadata.CharityLogo },
+            CharityOverview: VALIDLY_FILLED_CHARITY.Metadata.CharityOverview,
+          },
+        };
+
+        const stepData = getAdditionalInformationStepData(charity);
+
+        expect(stepData.isComplete).toBeTruthy();
+        expect(stepData.urlToPreviousStep).toBeUndefined();
+      });
+
+      test("returns 'incomplete' result when previous steps are incomplete with the previous step's URL to Documentation step", () => {
+        const charity: Charity = {
+          ...placeholderCharity,
+        };
+
+        const stepData = getAdditionalInformationStepData(charity);
+
+        expect(stepData.isComplete).toBeFalsy();
+        expect(stepData.urlToPreviousStep).toBe(appRoutes.register);
+      });
+
+      test("returns 'incomplete' result when previous steps are incomplete with the previous step's URL to Documentation step", () => {
+        const charity: Charity = {
+          ...placeholderCharity,
+          ContactPerson: { ...VALIDLY_FILLED_CHARITY.ContactPerson },
+          Registration: {
+            ...placeholderCharity.Registration,
+            CharityName: VALIDLY_FILLED_CHARITY.Registration.CharityName,
+            CharityName_ContactEmail:
+              VALIDLY_FILLED_CHARITY.Registration.CharityName_ContactEmail,
+            RegistrationDate:
+              VALIDLY_FILLED_CHARITY.Registration.RegistrationDate,
+            RegistrationStatus:
+              VALIDLY_FILLED_CHARITY.Registration.RegistrationStatus,
+          },
+        };
+
+        const stepData = getAdditionalInformationStepData(charity);
+
+        expect(stepData.isComplete).toBeFalsy();
+        expect(stepData.urlToPreviousStep).toBe(
+          `${appRoutes.register}/${routes.documentation}`
+        );
+      });
+
+      test("returns 'incomplete' result when Additional Information data is incomplete", () => {
+        const charity: Charity = {
+          ...VALIDLY_FILLED_CHARITY,
+          Metadata: {
+            ...placeholderCharity.Metadata,
+          },
+        };
+
+        const stepData = getAdditionalInformationStepData(charity);
+
+        expect(stepData.isComplete).toBeFalsy();
+        expect(stepData.urlToPreviousStep).toBe(
+          `${appRoutes.register}/${routes.additionalInformation}`
+        );
+      });
     });
   });
 });
