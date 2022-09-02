3517c588ce508f3bc6cdd2863e3e4812b2871be4
Nenad Misic <n.misic@outlook.com>
2022-09-02 09:35:17 +0200

    Add try/catch in useContactDetails

--- src/pages/Registration/ContactDetails/ContactDetailsForm/useContactDetails.ts
+++ src/pages/Registration/ContactDetails/ContactDetailsForm/useContactDetails.ts
@@ -27,78 +27,82 @@ export default function useSaveContactDetails() {
   const saveContactDetails = useCallback(
     async (contactData: ContactDetails) => {
       // call API to add or update contact details information(contactData)
-      setError(false);
-      const is_create = !contactData?.uniqueID;
-      // the submitted email is considered 'verified' only if it:
-      // 1. was already verified
-      // 2. is the same as the the pre-contact-details-update email
-      const isEmailVerified =
-        !!originalCharityData.ContactPerson.EmailVerified &&
-        originalCharityData.ContactPerson.Email === contactData.email;
+      try {
+        setError(false);
+        const is_create = !contactData?.uniqueID;
+        // the submitted email is considered 'verified' only if it:
+        // 1. was already verified
+        // 2. is the same as the the pre-contact-details-update email
+        const isEmailVerified =
+          !!originalCharityData.ContactPerson.EmailVerified &&
+          originalCharityData.ContactPerson.Email === contactData.email;
 
-      const postData: ContactDetailsRequest = {
-        PK: contactData.uniqueID,
-        body: {
-          Registration: {
-            CharityName: contactData.charityName,
+        const postData: ContactDetailsRequest = {
+          PK: contactData.uniqueID,
+          body: {
+            Registration: {
+              CharityName: contactData.charityName,
+            },
+            ContactPerson: {
+              FirstName: contactData.firstName,
+              LastName: contactData.lastName,
+              Email: contactData.email,
+              Goals: contactData.goals,
+              OtherReferralMethod: contactData.otherReferralMethod,
+              OtherRole: contactData.otherRole,
+              PhoneNumber: contactData.phone,
+              ReferralMethod: contactData.referralMethod,
+              Role: contactData.role,
+              EmailVerified: is_create ? undefined : isEmailVerified,
+            },
           },
-          ContactPerson: {
-            FirstName: contactData.firstName,
-            LastName: contactData.lastName,
-            Email: contactData.email,
-            Goals: contactData.goals,
-            OtherReferralMethod: contactData.otherReferralMethod,
-            OtherRole: contactData.otherRole,
-            PhoneNumber: contactData.phone,
-            ReferralMethod: contactData.referralMethod,
-            Role: contactData.role,
-            EmailVerified: is_create ? undefined : isEmailVerified,
-          },
-        },
-      };
+        };
 
-      const result = is_create
-        ? await registerCharity(postData)
-        : await updateContactPerson(postData);
+        const result = is_create
+          ? await registerCharity(postData)
+          : await updateContactPerson(postData);
 
-      if ("error" in result) {
-        setError(true);
+        if ("error" in result) {
+          setError(true);
 
-        const fetchError = result.error as FetchBaseQueryError;
-        if (fetchError) {
-          if (fetchError.status === 409) {
-            handleError(
-              fetchError,
-              `${fetchError.data} Please check your email for the registration reference.`
-            );
+          const fetchError = result.error as FetchBaseQueryError;
+          if (fetchError) {
+            if (fetchError.status === 409) {
+              handleError(
+                fetchError,
+                `${fetchError.data} Please check your email for the registration reference.`
+              );
+            } else {
+              handleError(fetchError, `${fetchError.data}`);
+            }
           } else {
-            handleError(fetchError, `${fetchError.data}`);
+            handleError(result.error, GENERIC_ERROR_MESSAGE);
           }
-        } else {
-          handleError(result.error, GENERIC_ERROR_MESSAGE);
+
+          return;
         }
 
-        return;
+        // if the user is updating contact details, that means they navigated to step 1 from the dashboard
+        // return them back there
+        if (!is_create && isEmailVerified) {
+          return navigate(`${appRoutes.register}/${routes.dashboard}`);
+        }
+
+        const { data } = result;
+        // Extracting SK, EmailVerified so that 'contactPerson' does not include them
+        const { PK, SK, EmailVerified, ...contactPerson } = data.ContactPerson;
+        //save ref before invalidating empty cache to retrigger fetch
+        storeRegistrationReference(PK || "");
+        //sending this email invalidated registration query cache
+        await sendVerificationEmail(PK!, {
+          ...contactPerson,
+          CharityName: data.Registration.CharityName,
+        });
+
+        navigate(`${appRoutes.register}/${routes.confirmEmail}`);
+      } catch (error) {
+        handleError(error, GENERIC_ERROR_MESSAGE);
       }
-
-      // if the user is updating contact details, that means they navigated to step 1 from the dashboard
-      // return them back there
-      if (!is_create && isEmailVerified) {
-        return navigate(`${appRoutes.register}/${routes.dashboard}`);
-      }
-
-      const { data } = result;
-      // Extracting SK, EmailVerified so that 'contactPerson' does not include them
-      const { PK, SK, EmailVerified, ...contactPerson } = data.ContactPerson;
-      //save ref before invalidating empty cache to retrigger fetch
-      storeRegistrationReference(PK || "");
-      //sending this email invalidated registration query cache
-      await sendVerificationEmail(PK!, {
-        ...contactPerson,
-        CharityName: data.Registration.CharityName,
-      });
-
-      navigate(`${appRoutes.register}/${routes.confirmEmail}`);
     },
     [
       originalCharityData,
