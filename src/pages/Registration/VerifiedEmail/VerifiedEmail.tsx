ba5ae2c9d1bd9863a66abcbf306367144c7907d0
Nenad Misic <n.misic@outlook.com>
2022-09-02 09:25:31 +0200

    Improve error handling in VerifiedEmail

--- src/pages/Registration/VerifiedEmail/VerifiedEmail.tsx
+++ src/pages/Registration/VerifiedEmail/VerifiedEmail.tsx
@@ -2,8 +2,9 @@ import jwtDecode from "jwt-decode";
 import { useCallback } from "react";
 import { useLocation } from "react-router-dom";
 import { UnprocessedCharity } from "types/aws";
-import { logger } from "helpers";
+import { useErrorContext } from "contexts/ErrorContext";
 import useSendVerificationEmail from "../common/useSendVerificationEmail";
+import { GENERIC_ERROR_MESSAGE } from "../constants";
 import LinkExpired from "./LinkExpired";
 import VerificationSuccessful from "./VerificationSuccessful";
 
@@ -17,20 +18,20 @@ type JwtData = UnprocessedCharity & {
 export default function VerifiedEmail() {
   const location = useLocation();
   const { sendVerificationEmail, isLoading } = useSendVerificationEmail();
+  const { handleError } = useErrorContext();
+
   const pathNames = location.pathname.split("/");
   const jwtToken = pathNames[pathNames.length - 1];
   const jwtData = jwtDecode<JwtData>(jwtToken);
   const { authorization, exp, iat, user, ...newCharity } = jwtData;
-
   const is_expired = Math.floor(Date.now() / 1000) >= exp;
 
   const resendVerificationEmail = useCallback(async () => {
-    if (!newCharity.ContactPerson.PK) {
-      logger.error("Invalid Data. Please ask the administrator about that.");
-      return;
+    try {
+      await sendVerificationEmail(newCharity.ContactPerson.PK, newCharity);
+    } catch (error) {
+      handleError(error, GENERIC_ERROR_MESSAGE);
     }
-
-    await sendVerificationEmail(newCharity.ContactPerson.PK, newCharity);
   }, [newCharity, sendVerificationEmail]);
 
   if (is_expired) {
