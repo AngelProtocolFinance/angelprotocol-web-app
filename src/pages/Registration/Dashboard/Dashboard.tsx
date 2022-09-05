590c1f4e536eb9e21a7d40d2afe2a27cf71d877e
Nenad Misic <n.misic@outlook.com>
2022-09-05 13:27:33 +0200

    Add ProgressIndicator back to Dashboard

--- src/pages/Registration/Dashboard/Dashboard.tsx
+++ src/pages/Registration/Dashboard/Dashboard.tsx
@@ -8,8 +8,8 @@ import useSendVerificationEmail from "../common/useSendVerificationEmail";
 import { isRegistrationEditable } from "../helpers";
 import routes from "../routes";
 import EndowmentStatus from "./EndowmentStatus";
+import ProgressIndicator from "./ProgressIndicator";
 import Step from "./Step";
-// import ProgressIndicator from "./ProgressIndicator";
 import getRegistrationState from "./getRegistrationState";
 import useActivate from "./useActivate";
 import useSubmit from "./useSubmit";
@@ -48,6 +48,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex flex-col gap-4 items-center w-full">
+      <ProgressIndicator />
       <h3 className="text-3xl font-bold">Necessary Information</h3>
       <span>
         Please complete all the following steps to be able to create your
