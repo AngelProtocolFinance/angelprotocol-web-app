9049b2adc56edef0677804fd7e53f7245b167e70
Nenad Misic <n.misic@outlook.com>
2022-09-09 13:48:33 +0200

    EndowmentStatus refactor

--- src/pages/Registration/Dashboard/EndowmentStatus/EndowmentStatus.tsx
+++ src/pages/Registration/Dashboard/EndowmentStatus/EndowmentStatus.tsx
@@ -11,12 +11,11 @@ type Props = {
   onSubmit: MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function EndowmentStatus(props: Props) {
+export default function EndowmentStatus({ isLoading, onSubmit }: Props) {
   const { charity } = useRegistrationQuery();
-  const { RegistrationStatus, CharityName } = charity.Registration;
-  const { JunoWallet } = charity.Metadata;
+  const { Registration } = charity;
 
-  if (props.isLoading) {
+  if (isLoading) {
     return <Loader bgColorClass="bg-white" widthClass="w-4" gapClass="gap-2" />;
   }
 
@@ -26,35 +25,33 @@ export default function EndowmentStatus(props: Props) {
     <div className="flex flex-col w-full gap-4 items-center">
       <div className="flex w-9/12 items-center justify-end rounded-md border-2 border-white border-solid p-2 px-9 font-bold">
         <p className="ml-3 mr-auto">Status of Your Endowment</p>
-        {RegistrationStatus === "Under Review" && (
+        {Registration.RegistrationStatus === "Under Review" && (
           <p className="flex items-center justify-center w-40 h-10 mr-40 uppercase text-yellow-500">
             Under Review
           </p>
         )}
-        {RegistrationStatus === "Inactive" && (
+        {Registration.RegistrationStatus === "Inactive" && (
           <>
             <p className="uppercase text-green-500 w-40">
-              {RegistrationStatus}
+              {Registration.RegistrationStatus}
             </p>
             <Button
-              className="w-full md:w-2/3 h-10 mt-5 btn-primary"
-              onClick={props.onSubmit}
-              disabled={
-                !registrationState.getIsReadyForSubmit() || props.isLoading
-              }
+              className="w-full md:w-2/3 h-10 mt-5 bg-yellow-blue"
+              onClick={onSubmit}
+              disabled={!registrationState.getIsReadyForSubmit() || isLoading}
             >
               Submit for review
             </Button>
           </>
         )}
-        {RegistrationStatus === "Active" && (
+        {Registration.RegistrationStatus === "Active" && (
           <p className="flex items-center h-10 ml-14 mr-auto text-green-500 uppercase">
-            Created: {maskAddress(JunoWallet)}
+            Created: {maskAddress(charity.Metadata.JunoWallet)}
           </p>
         )}
       </div>
-      {RegistrationStatus === "Active" && (
-        <EndowmentCreated charityName={CharityName} />
+      {Registration.RegistrationStatus === "Active" && (
+        <EndowmentCreated charityName={Registration.CharityName} />
       )}
     </div>
   );
