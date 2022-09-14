389cc3e72235e53c6e431f067080ae3c3f42f88a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2022-09-14 17:00:20 +0800

    update contracts (#1233)

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -22,22 +22,22 @@ export const contracts: Contracts = IS_TEST
       //TESTNET CONTRACTS
       //core
       index_fund:
-        "juno1s3vz9nu049es0ev89zwcd7amv0an4cwd3t2krvy93qvdp7sqyu9qsn9ykl",
+        "juno1mvr3c4ezjkfmu8j3eka4rxh92zcc6j7ln8jlapt8yg3fv6xhwmjq4m9tfk",
       registrar:
-        "juno1vatmkkf406ylknexkdrzfqtjdmeye2pzgzr5j6d6fgymd09yc3kqld5pp6",
+        "juno1p06jg204wnv262xywsagww49t0gvunqk4vzjcdvhn55sqvprls5sgrw8jd",
       accounts:
-        "juno1t09v4r2h2y8tdwhz3v8rcl22cwxjjl9arjce2dmp8mgs6snqd49sxpenxr",
+        "juno1y0wuuhyhrl7e5auu6pmgydhry20cu2pecte9ekeye4u3ddlvmwmqk6ve2a",
 
       // Admin
       cw3ApTeam:
-        "juno1xe3xg8auwx6krqz88depem703lekl7ce2jmnyrfs7s6ze9pdp36smcth68",
+        "juno1u9atwm9ut0krjvez87akc8u873r4hqhg05vscwztzufaujfz8etqh20g8h",
       cw4GrpApTeam:
-        "juno1jjjax430dzrgqdapsgp306dp5tanc7zq4uvrx7vmf6mgg00tkw8su6p6te",
+        "juno1nrp3q7rnfc8qq8a3rchg689rl5qks68h9gqy9kfcs0z8wqql0vhsx3rv8s",
 
       cw3ReviewTeam:
-        "juno1mu2erhc5k6ucvpdzq0kum3dcwln9la4mpdfgm5psehl98pw67zcs7qysy6",
+        "juno1dsluvths8jrdajvz89ge7epnwrdmrcgg4ua02j2kkw8m2fvp3shqsr5glp",
       cw4GrpReviewTeam:
-        "juno17pjg8zpkddd6hcsyqpu3u6vk4rry3uumf0zz05saw7d5jjavqavqu5e6my",
+        "juno18tlf8peuc3xm0ua897ucsampmavv5m897pr596tawc4tu3le8usqs33tsx",
 
       //terraswap
       halo_token: "",
