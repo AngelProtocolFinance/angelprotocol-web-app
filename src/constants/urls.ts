7adb05319f47ed368dbc445519b8c2d041b66415
Nenad Misic <n.misic@outlook.com>
2022-09-14 18:32:07 +0200

    Update keplr install link

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -7,5 +7,4 @@ export const APIs = {
 export const PRIVACY_POLICY =
   "https://storageapi2.fleek.co/57b943eb-ed70-478a-8899-c7859400f77b-bucket/documents/Website and WebApp Privacy Policy (v.110121).pdf";
 
-export const KEPLR_INSTALL_LINK =
-  "https://chrome.google.com/webstore/detail/keplr/dmkamcknogkgcdfhhbddcghachkejeap";
+export const KEPLR_INSTALL_LINK = "https://www.keplr.app/";
