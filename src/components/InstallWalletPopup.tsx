809ec970438dbd8540208d447abc0ac77492db8d
Nenad Misic <n.misic@outlook.com>
2022-09-15 18:19:38 +0200

    Make InstallWalletPopup mobile-friendly

--- src/components/InstallWalletPopup.tsx
+++ src/components/InstallWalletPopup.tsx
@@ -7,28 +7,36 @@ export default function InstallWalletPopup(props: { providerId: ProviderId }) {
   const { installUrl, logo, name } = WALLET_METADATA[props.providerId];
 
   return (
-    <Dialog.Panel className="fixed-center z-20 max-w-md outline-none md:w-[90%] w-full md:p-5 p-8 bg-blue-accent rounded-2xl z-50 flex flex-col gap-5">
-      <h2 className="text-xl text-white-grey font-bold font-heading">
+    <Dialog.Panel className="fixed-center z-20 max-w-md outline-none w-[90%] p-5 bg-blue-accent rounded-2xl z-50 flex flex-col gap-5">
+      <h2 className="text-lg md:text-xl text-white-grey font-bold font-heading">
         Install Wallet
       </h2>
       <a
-        className="bg-blue-dark hover:bg-angel-blue rounded-2xl p-5 flex items-center"
+        className="bg-blue-dark hover:bg-angel-blue rounded-2xl p-3 md:p-5 flex items-center"
         rel="noreferrer"
         target="_blank"
         href={installUrl}
       >
-        <img src={logo} alt="keplr logo" className="w-12 h-12 object-contain" />
+        <img
+          src={logo}
+          alt="wallet logo"
+          className="w-8 h-8 md:w-12 md:h-12 object-contain"
+        />
 
-        <div className="flex flex-col text-left ml-5">
+        <div className="flex flex-col text-left ml-3 md:ml-5">
           <div className="flex items-center gap-2 text-white-grey font-bold">
-            <h6 className="text-xl font-heading">Install {name}</h6>
-            <Icon type="ExternalLink" size={24} />
+            <h6 className="text-base md:text-lg font-heading">
+              Install {name}
+            </h6>
+            <Icon type="ExternalLink" size={20} />
           </div>
-          <p className="text-grey-accent mt-1 truncate w-72">{installUrl}</p>
+          <p className="text-sm md:text-base text-grey-accent mt-1 truncate w-40 md:w-72">
+            {installUrl}
+          </p>
         </div>
       </a>
-      <div className="p-5 rounded-2xl bg-thin-blue">
-        <p className="text-white/70 text-sm">
+      <div className="p-3 md:p-5 rounded-2xl bg-thin-blue">
+        <p className="text-white/70 text-xs md:text-sm">
           You may need to refresh the page once you have downloaded the wallet.
         </p>
       </div>
