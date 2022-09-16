1d7bd2e5017917bbf2e128a2fbcfe70a5afe3564
Nenad Misic <n.misic@outlook.com>
2022-09-16 12:18:14 +0200

    Make un_sdg form field required and increase min value to 1

--- src/pages/Registration/Documentation/documentationSchema.ts
+++ src/pages/Registration/Documentation/documentationSchema.ts
@@ -44,7 +44,7 @@ const documentationShape: SchemaShape<DocumentationValues> = {
   website: Yup.string()
     .required("Organization website required")
     .url("Must be a valid URL"),
-  un_sdg: Yup.number().min(0).max(17),
+  un_sdg: Yup.number().required("UNSDG must be selected").min(1).max(17),
   checkedAuthority: Yup.bool().isTrue("Authority checkbox must be checked"),
   checkedPolicy: Yup.bool().isTrue("Policy checkbox must be checked"),
 };
