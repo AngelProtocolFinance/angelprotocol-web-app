bc2e3b39c56b0bca62344f3abc55c4b9b26a7b58
ap-justin <89639563+ap-justin@users.noreply.github.com>
2022-09-19 09:27:01 +0800

    replace hardcoded endowmentId (#1244)

--- src/pages/Admin/Charity/index.tsx
+++ src/pages/Admin/Charity/index.tsx
@@ -1,13 +1,15 @@
 import { AdminMobileNavPortal } from "App/Header/MobileNav";
+import { useAdminResources } from "../Guard";
 import Nav from "./Nav";
 import Views from "./Views";
 
 export default function Charity() {
+  const { endowmentId } = useAdminResources();
   return (
     <div className="padded-container grid grid-rows-[auto_1fr] pb-4 gap-2">
       <Nav />
       <div className="block md:hidden">
-        <AdminMobileNavPortal id={1} />
+        <AdminMobileNavPortal id={endowmentId} />
       </div>
       <Views />
     </div>
