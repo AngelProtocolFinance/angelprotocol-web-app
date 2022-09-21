3c80cc7277a15e7e02b817ad8eb14176668068df
Nenad Misic <n.misic@outlook.com>
2022-09-21 18:34:50 +0200

    Update EndowmentStatus

--- src/pages/Registration/Dashboard/EndowmentStatus.tsx
+++ src/pages/Registration/Dashboard/EndowmentStatus.tsx
@@ -26,27 +26,35 @@ export default function EndowmentStatus({ isLoading, onSubmit }: Props) {
     <div className="flex flex-col w-full gap-4 items-center">
       <div className="flex w-9/12 items-center justify-end rounded-md border-2 border-white border-solid p-2 px-9 font-bold">
         <p className="ml-3 mr-auto">Status of Your Endowment</p>
-        {(status === "Inactive" || status === "Rejected") && (
+        {status === "Inactive" && (
+          <Button
+            className="w-40 h-10 btn-primary"
+            onClick={onSubmit}
+            disabled={!registrationState.getIsReadyForSubmit() || isLoading}
+          >
+            Submit
+          </Button>
+        )}
+        {status === "Rejected" && (
           <>
-            <p
-              className={`uppercase w-40 mr-2 ${
-                status === "Inactive" ? "text-green-500" : "text-red-500"
-              }`}
-            >
-              {status}
-            </p>
+            <p className="uppercase w-40 mr-2 text-red-500">{status}</p>
             <Button
               className="w-40 h-10 btn-primary"
               onClick={onSubmit}
               disabled={!registrationState.getIsReadyForSubmit() || isLoading}
             >
-              Submit
+              Resubmit
             </Button>
           </>
         )}
         {status === "Under Review" && (
           <p className="flex items-center justify-center w-40 h-10 mr-40 uppercase text-yellow-500">
             Under Review
+          </p>
+        )}
+        {status === "Active" && (
+          <p className="flex items-center justify-center w-40 h-10 mr-40 uppercase text-green-500">
+            Active
           </p>
         )}
       </div>
