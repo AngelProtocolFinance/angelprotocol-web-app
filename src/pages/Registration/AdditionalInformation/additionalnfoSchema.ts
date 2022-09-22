64a0581085924911b8b0bdd106075d542a1ddff7
Nenad Misic <n.misic@outlook.com>
2022-09-22 19:45:56 +0200

    Add requirement for logo and banner

--- src/pages/Registration/AdditionalInformation/additionalnfoSchema.ts
+++ src/pages/Registration/AdditionalInformation/additionalnfoSchema.ts
@@ -14,33 +14,24 @@ export const VALID_MIME_TYPES = [
 const FILE_SCHEMA = Yup.mixed<FileWrapper>()
   .test({
     name: "fileType",
-    message: "Valid file types are JPG, PNG and WEBP",
-    test: (fileWrapper) =>
-      fileWrapper?.file
+    message: "Valid file types are JPG, PNG, WEBP and SVG",
+    test: (fileWrapper) => {
+      console.log(fileWrapper);
+      return fileWrapper?.file
         ? VALID_MIME_TYPES.includes(fileWrapper.file.type)
-        : true,
+        : true;
+    },
   })
   .test({
     name: "fileSize",
     message: "Image size must be smaller than 1MB",
     test: (fileWrapper) => (fileWrapper?.file?.size || 0) <= 1e6,
-  })
-  .test({
-    name: "invalidState",
-    message: "Invalid internal state",
-    test: (fileWrapper) =>
-      // fileWrapper must be instantiated
-      !!fileWrapper &&
-      // file name must be set
-      !!fileWrapper.name &&
-      // either new file is uploaded or source URL to file is set
-      (!!fileWrapper.file || !!fileWrapper.publicUrl),
   });
 
 const additionalnfoShape: SchemaShape<AdditionalInfoValues> = {
   charityOverview: stringByteSchema("overview", 4, 1024),
-  charityLogo: FILE_SCHEMA,
-  banner: FILE_SCHEMA,
+  charityLogo: FILE_SCHEMA.required("Please upload the Charity Logo"),
+  banner: FILE_SCHEMA.required("Please upload the Charity Banner"),
 };
 
 export const additionalInfoSchema = Yup.object().shape(additionalnfoShape);
