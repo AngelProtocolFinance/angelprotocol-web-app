7e6cfab0551f4fdc884c13981b7b3c614a3554c3
Nenad Misic <n.misic@outlook.com>
2022-09-22 18:56:46 +0200

    Use reusable ImgEditor in Admin/Charity/EditProfile

--- src/pages/Admin/Charity/EditProfile/schema.ts
+++ src/pages/Admin/Charity/EditProfile/schema.ts
@@ -1,9 +1,43 @@
 import * as Yup from "yup";
 import { UpdateProfileValues } from "pages/Admin/types";
 import { SchemaShape } from "schemas/types";
+import { FileWrapper } from "components/FileDropzone";
 import { positiveNumber } from "schemas/number";
 import { stringByteSchema, url } from "schemas/string";
 import { proposalShape } from "../../constants";
+
+export const VALID_MIME_TYPES = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "image/svg",
+];
+
+const FILE_SCHEMA = Yup.mixed<FileWrapper>()
+  .test({
+    name: "fileType",
+    message: "Valid file types are JPG, PNG and WEBP",
+    test: (fileWrapper) =>
+      fileWrapper?.file
+        ? VALID_MIME_TYPES.includes(fileWrapper.file.type)
+        : true,
+  })
+  .test({
+    name: "fileSize",
+    message: "Image size must be smaller than 1MB",
+    test: (fileWrapper) => (fileWrapper?.file?.size || 0) <= 1e6,
+  })
+  .test({
+    name: "invalidState",
+    message: "Invalid internal state",
+    test: (fileWrapper) =>
+      // fileWrapper must be instantiated
+      !!fileWrapper &&
+      // file name must be set
+      !!fileWrapper.name &&
+      // either new file is uploaded or source URL to file is set
+      (!!fileWrapper.file || !!fileWrapper.publicUrl),
+  });
 
 //construct strict shape to avoid hardcoding shape keys
 const shape: SchemaShape<UpdateProfileValues> = {
@@ -13,7 +47,7 @@ const shape: SchemaShape<UpdateProfileValues> = {
   //sdgNum: no need to validate, selected from dropdown with default value
   //tier: TODO: this field is not touched here for endowment owner, will be added on distinction of config owner
   //logo: no need to validate, url is auto generated
-  //image: no need to validate, url is auto generated
+  image: FILE_SCHEMA,
   url: url,
   // registration_number: no need to validate
   // country_city_origin: no need to validate
