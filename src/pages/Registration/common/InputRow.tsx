593a160ff09a73f0ecf5b186afb2482736a4bddf
Nenad Misic <n.misic@outlook.com>
2022-09-23 11:15:05 +0200

    Move InfoIcon to separate file

--- src/pages/Registration/common/InputRow.tsx
+++ src/pages/Registration/common/InputRow.tsx
@@ -1,32 +1,19 @@
-import { BsQuestionCircle } from "react-icons/bs";
 import { InputProps } from "./types";
-import { useModalContext } from "contexts/ModalContext";
+import InfoIcon from "./InfoIcon";
 
 export function InputRow(props: InputProps) {
-  const { htmlFor: id, label, required, infoModal, children } = props;
-
   return (
     <div className="grid grid-cols-2 gap-2">
       <div className="flex items-center gap-2 h-8">
-        <label htmlFor={id} className="cursor-pointer">
-          {label}
-          {required && <span className="text-failed-red ml-0.5">*</span>}
+        <label htmlFor={props.htmlFor} className="cursor-pointer">
+          {props.label}
+          {props.required && <span className="text-failed-red ml-0.5">*</span>}
         </label>
-        {!!infoModal && <InfoIcon modal={infoModal} />}
+        {!!props.infoModal && <InfoIcon modal={props.infoModal} />}
       </div>
       <div className="flex flex-col justify-center gap-1 w-full">
-        {children}
+        {props.children}
       </div>
     </div>
-  );
-}
-
-function InfoIcon({ modal }: { modal: React.FC<{}> }) {
-  const { showModal } = useModalContext();
-  return (
-    <BsQuestionCircle
-      className="text-thin-blue cursor-pointer"
-      onClick={() => showModal(modal, {})}
-    />
   );
 }
