4296367f753e71baf43b0e3a6e1cd80de137d2e0
Nenad Misic <nenad@angelprotocol.io>
2022-09-26 09:20:00 +0200

    Remove 'donations disabled..' banner (#1269)

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -23,10 +23,7 @@ export default function App() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] bg-gradient-to-b from-blue-accent to-black-blue bg-fixed">
-      <p className="transition-shadow flex z-20 items-center justify-center font-heading font-bold bg-angel-orange w-full p-2 text-center text-angel-grey text-xs">
-        Please note: Donations are currently disabled. V2 coming soon!
-      </p>
+    <div className="grid grid-rows-[1fr_auto] bg-gradient-to-b from-blue-accent to-black-blue bg-fixed">
       {!chainOptions ? (
         <div className="flex justify-center items-center w-full h-full">
           <Loader
