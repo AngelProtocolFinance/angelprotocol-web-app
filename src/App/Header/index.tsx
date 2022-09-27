547b614e18a8ffbaa3a5b0c29acc73af34c0f1e4
Andrey <andrey@angelprotocol.io>
2022-09-27 08:55:54 +0800

    private lcd node! Thank you WildSage; Fix main top logo linking;

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -1,18 +1,21 @@
-import { Link } from "react-router-dom";
 import betaWhiteLogo from "assets/images/angelprotocol-beta-horiz-wht.png";
 import TransactionHint from "components/Transactor/TransactionHint";
 import Airdrop from "components/Transactors/Airdrop/Airdrop";
 import WalletSuite from "components/WalletSuite";
-import { appRoutes } from "constants/routes";
 import MobileNav from "./MobileNav";
 import DappMenu from "./Nav";
 
 export default function Header() {
   return (
     <header className="mb-4 grid grid-cols-[auto_1fr_auto] items-center w-full padded-container pt-3">
-      <Link to={appRoutes.index} title="to home">
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href="https://angelprotocol.io/"
+        title="to home"
+      >
         <img src={betaWhiteLogo} alt="" className="w-32 sm:w-36" />
-      </Link>
+      </a>
       <DappMenu />
       <div className="ml-auto grid grid-cols-[auto_1fr_auto]">
         <TransactionHint />
