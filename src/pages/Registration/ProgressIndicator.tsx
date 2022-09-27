866a8721935b05199e6ad57fd9a99c4a821e723d
ap-justin <justin@angelprotocol.io>
2022-09-28 00:13:08 +0800

    interpolated tailwind class

--- src/pages/Registration/ProgressIndicator.tsx
+++ src/pages/Registration/ProgressIndicator.tsx
@@ -27,6 +27,7 @@ export default function ProgressIndicator() {
       </div>
       <div className="h-4 w-full bg-white rounded-3xl">
         <div
+          style={{ width: `${(completedCount / progress.length) * 100}%` }}
           className={`h-full w-${completedCount}/${progress.length} bg-blue rounded-3xl`}
         />
       </div>
