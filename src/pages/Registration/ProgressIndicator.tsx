074300dd1d4555981593e19a4dbaba3d2559be26
ap-justin <justin@angelprotocol.io>
2022-09-28 11:13:09 +0800

    reuse prev percent

--- src/pages/Registration/ProgressIndicator.tsx
+++ src/pages/Registration/ProgressIndicator.tsx
@@ -27,7 +27,7 @@ export default function ProgressIndicator() {
       </div>
       <div className="h-4 w-full bg-white-grey rounded-3xl">
         <div
-          style={{ width: `${(completedCount / progress.length) * 100}%` }}
+          style={{ width: `${percent}%` }}
           className={`h-full bg-angel-blue rounded-3xl`}
         />
       </div>
