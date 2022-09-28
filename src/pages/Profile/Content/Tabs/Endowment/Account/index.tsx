58b99fed726bc7071d58864343655f1edadd6577
Andrey <andrey@angelprotocol.io>
2022-09-29 07:47:06 +0800

    account type labels

--- src/pages/Profile/Content/Tabs/Endowment/Account/index.tsx
+++ src/pages/Profile/Content/Tabs/Endowment/Account/index.tsx
@@ -18,7 +18,7 @@ export default function Account({ type, balance }: TAcount) {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] rounded-md text-white/80 p-4 border border-zinc-50/20">
       <h3 className="mb-2 text-lg w-full font-bold uppercase flex items-center justify-self-start">
-        <span>{type}</span>
+        <span>{type === "locked" ? "Endowment" : "Current"}</span>
       </h3>
       {((cw20.length > 0 || native.length > 0) && (
         <Holdings balance={balance} />
