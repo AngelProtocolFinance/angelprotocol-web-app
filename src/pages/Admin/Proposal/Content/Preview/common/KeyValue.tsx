64fe25a8fb803fb8f49df0c3beccac301cde8dcf
Nenad Misic <nenad@angelprotocol.io>
2022-10-03 10:38:31 +0200

    Fix 'gadiv-2' to 'gap-2' (#1299)

--- src/pages/Admin/Proposal/Content/Preview/common/KeyValue.tsx
+++ src/pages/Admin/Proposal/Content/Preview/common/KeyValue.tsx
@@ -6,7 +6,7 @@ export default function KeyValue({
   children,
 }: PropsWithChildren<{}> & { _key: string; _classes?: string }) {
   return (
-    <div className={`flex items-baseline gadiv-2 ${_classes} p-0.5`}>
+    <div className={`flex items-baseline gap-2 ${_classes} p-0.5`}>
       <span className="text-xs font-heading uppercase w-48">{_key}</span>
       {children}
     </div>
