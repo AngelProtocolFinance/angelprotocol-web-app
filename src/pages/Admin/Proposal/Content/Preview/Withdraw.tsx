7cf1559d6e2c0e7ccffb04ddccd9ec87b272489e
ap-justin <justin@angelprotocol.io>
2022-10-03 15:00:17 +0800

    style preview

--- src/pages/Admin/Proposal/Content/Preview/Withdraw.tsx
+++ src/pages/Admin/Proposal/Content/Preview/Withdraw.tsx
@@ -1,7 +1,7 @@
 import { WithdrawMeta } from "pages/Admin/types";
 import TableSection, { Cells } from "components/TableSection";
 import { condense, humanize } from "helpers";
-import { symbols } from "constants/tokens";
+import { symbols, tokens } from "constants/tokens";
 import KeyValue from "./common/KeyValue";
 import PreviewContainer from "./common/PreviewContainer";
 
@@ -15,19 +15,27 @@ export default function Withdraw({
         <span className="font-mono text-sm">{beneficiary}</span>
       </KeyValue>
 
-      <TableSection type="tbody" rowClass="">
-        {assets.map((asset) => {
-          const denom =
-            "native" in asset.info ? asset.info.native : asset.info.cw20;
+      <KeyValue _key="assets" _classes="flex-col mt-4 gap-2">
+        <TableSection type="tbody" rowClass="">
+          {assets.map((asset) => {
+            const denom =
+              "native" in asset.info ? asset.info.native : asset.info.cw20;
 
-          return (
-            <Cells key={denom} cellClass="" type="td">
-              <>{humanize(condense(asset.amount), 4)}</>
-              <>{symbols[denom]}</>
-            </Cells>
-          );
-        })}
-      </TableSection>
+            return (
+              <Cells key={denom} cellClass="" type="td">
+                <img
+                  src={tokens[denom].icon}
+                  className="w-5 h-5 rounded-full mr-2"
+                />
+                <>{humanize(condense(asset.amount), 4)}</>
+                <span className="text-xs font-mono uppercase pl-2">
+                  {tokens[denom].symbol}
+                </span>
+              </Cells>
+            );
+          })}
+        </TableSection>
+      </KeyValue>
     </PreviewContainer>
   );
 }
