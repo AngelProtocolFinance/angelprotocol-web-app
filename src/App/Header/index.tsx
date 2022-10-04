d42b068fe95e9eda9da2ddc129c924e4f726c3ab
Nenad Misic <n.misic@outlook.com>
2022-10-04 08:52:14 +0200

    Make header logo navigate to root URL

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import betaWhiteLogo from "assets/images/angelprotocol-beta-horiz-wht.png";
 import TransactionHint from "components/Transactor/TransactionHint";
 import Airdrop from "components/Transactors/Airdrop/Airdrop";
@@ -8,13 +9,9 @@ import DappMenu from "./Nav";
 export default function Header() {
   return (
     <header className="mb-4 grid grid-cols-[auto_1fr_auto] items-center w-full padded-container pt-3">
-      <a
-        rel="noreferrer"
-        href="https://angelprotocol.io/"
-        title="Go to Marketing page"
-      >
-        <img src={betaWhiteLogo} alt="" className="w-32 sm:w-36" />
-      </a>
+      <Link rel="noreferrer" to="/" title="Go to Marketing page">
+        <img src={betaWhiteLogo} alt="logo" className="w-32 sm:w-36" />
+      </Link>
       <DappMenu />
       <div className="ml-auto grid grid-cols-[auto_1fr_auto]">
         <TransactionHint />
