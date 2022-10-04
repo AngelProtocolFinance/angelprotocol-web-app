361bd9a656eb587b5da8cb66e1bf8e2bfe4fc073
Nenad Misic <n.misic@outlook.com>
2022-10-04 08:40:38 +0200

    Update uploadToIpfs to include file name

--- src/helpers/uploadToIpfs.test.ts
+++ src/helpers/uploadToIpfs.test.ts
@@ -3,12 +3,11 @@ import { IPFS_GATEWAY, uploadToIpfs } from "./uploadToIpfs";
 
 describe("uploadToIpfs tests", () => {
   it("returns correctly extracted data after successful upload", async () => {
-    const file = new File([], "test");
+    const fileName = "test";
+    const file = new File([], fileName);
 
     const result = await uploadToIpfs(file);
-    expect(mockedUpload).toHaveBeenCalledWith([file], {
-      wrapWithDirectory: false,
-    });
-    expect(result).toEqual(`${IPFS_GATEWAY}/${cid}`);
+    expect(mockedUpload).toHaveBeenCalledWith([file], { name: fileName });
+    expect(result).toEqual(`${IPFS_GATEWAY}/${cid}/${fileName}`);
   });
 });
--- src/helpers/uploadToIpfs.ts
+++ src/helpers/uploadToIpfs.ts
@@ -1,16 +1,22 @@
 import { Web3Storage } from "web3.storage";
 
 //https://docs.ipfs.tech/concepts/ipfs-gateway/#gateway-providers
-export const IPFS_GATEWAY = "https://cloudflare-ipfs.com/ipfs"; //public
+export const IPFS_GATEWAY = "https://ipfs.io/ipfs"; //public
 
 export async function uploadToIpfs(file: File): Promise<string> {
   const client = new Web3Storage({
     token: process.env.REACT_APP_WEB3_STORAGE_API_KEY!,
     endpoint: new URL("https://api.web3.storage"),
   });
-  const cid = await client.put([file], { wrapWithDirectory: false });
+  const fileName = file.name.replace(/\s/g, "");
+  const cid = await client.put([file], { name: fileName });
   //remove all whitespace from file name
   //NOTE: AWS doesn't accept URLs with space e.g google.com/hello to the world.jpg
-  return `${IPFS_GATEWAY}/${cid}`;
+  return genPublicUrl(cid, fileName);
   //let caller handle errors
+}
+
+//https://docs.ipfs.tech/concepts/ipfs-gateway/#gateway-providers
+function genPublicUrl(cid: string, fileName: string) {
+  return `${IPFS_GATEWAY}/${cid}/${fileName}`;
 }
