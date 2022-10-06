77a43af8499ba31f08c17d76639b865a742b5642
Nenad Misic <n.misic@outlook.com>
2022-10-06 10:23:55 +0200

    Add ThemeToggle component

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -4,6 +4,7 @@ import Airdrop from "components/Transactors/Airdrop/Airdrop";
 import WalletSuite from "components/WalletSuite";
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Header() {
   return (
@@ -16,7 +17,8 @@ export default function Header() {
         <img src={betaWhiteLogo} alt="" className="w-32 sm:w-36" />
       </a>
       <DesktopNav />
-      <div className="ml-auto grid grid-cols-[auto_1fr_auto]">
+      <div className="ml-auto grid grid-cols-[auto_auto_1fr_auto]">
+        <ThemeToggle />
         <TransactionHint />
         <WalletSuite />
         <Airdrop />
