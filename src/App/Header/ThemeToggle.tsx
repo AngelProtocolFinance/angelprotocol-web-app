e8996a3ef4b29944b6baea7f4723b62453733a0c
Nenad Misic <n.misic@outlook.com>
2022-10-06 11:31:41 +0200

    Extract toggle options into separate component

--- src/App/Header/ThemeToggle.tsx
+++ src/App/Header/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 import { Switch } from "@headlessui/react";
 import { useState } from "react";
-import Icon from "components/Icon";
+import Icon, { IconTypes } from "components/Icon";
 
 export default function ThemeToggle() {
   const [isDarkMode, setDarkMode] = useState(false);
@@ -9,29 +9,24 @@ export default function ThemeToggle() {
     <Switch
       checked={isDarkMode}
       onChange={setDarkMode}
-      className="relative flex items-center justify-center gap-1 h-10 w-[92px] shrink-0 cursor-pointer rounded-full shadow-[inset_-.5px_.5px_2px] shadow-white-grey/60 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 bg-transparent"
+      className="flex items-center justify-center gap-1 h-10 w-[92px] shrink-0 cursor-pointer rounded-full shadow-[inset_-.5px_.5px_2px] shadow-white-grey/60 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 bg-transparent"
     >
       <span className="sr-only">Enable dark mode</span>
-      <span
-        className={`flex items-center justify-center h-7 w-7 rounded-full pointer-events-none ${
-          isDarkMode ? "bg-transparent" : "bg-white"
-        }`}
-      >
-        <Icon
-          type="Sun"
-          className={`text-lg ${isDarkMode ? "text-white" : "text-black"}`}
-        />
-      </span>
-      <span
-        className={`flex items-center justify-center h-7 w-7 rounded-full pointer-events-none ${
-          isDarkMode ? "bg-white" : "bg-transparent"
-        }`}
-      >
-        <Icon
-          type="Moon"
-          className={`text-lg ${isDarkMode ? "text-black" : "text-white"}`}
-        />
-      </span>
+      <ToggleOption checked={!isDarkMode} icon="Sun" />
+      <ToggleOption checked={isDarkMode} icon="Moon" />
     </Switch>
   );
 }
+
+const ToggleOption = (props: { checked: boolean; icon: IconTypes }) => (
+  <span
+    className={`flex items-center justify-center h-7 w-7 rounded-full pointer-events-none ${
+      props.checked ? "bg-white" : "bg-transparent"
+    }`}
+  >
+    <Icon
+      type={props.icon}
+      className={`text-lg ${props.checked ? "text-black" : "text-white"}`}
+    />
+  </span>
+);
