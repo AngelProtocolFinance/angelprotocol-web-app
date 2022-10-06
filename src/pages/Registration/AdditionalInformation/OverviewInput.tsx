eae1eacb7366e2b2b45feea4f4618f931450480d
ap-justin <justin@angelprotocol.io>
2022-10-06 20:34:40 +0800

    fix residual error

--- src/pages/Registration/AdditionalInformation/OverviewInput.tsx
+++ src/pages/Registration/AdditionalInformation/OverviewInput.tsx
@@ -4,12 +4,12 @@ import RichTextEditor from "components/RichTextEditor";
 export default function OverviewInput() {
   return (
     <div className="flex flex-col w-full text-left">
-      <label htmlFor="charityOverview" className="cursor-pointer">
+      <label htmlFor="overview" className="cursor-pointer">
         Description of your organization
         <span className="ml-0.5 text-failed-red">*</span>
       </label>
       <RichTextEditor<AdditionalInfoValues>
-        fieldName="charityOverview"
+        fieldName="overview"
         classes={{
           container: "text-white/80 p-3 rounded-md bg-white/10 shadow-inner",
           error: "text-sm text-failed-red ml-1",
