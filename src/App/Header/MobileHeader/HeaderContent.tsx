45b01828266fa8a68a67162800455ba7d4a378e2
Nenad Misic <n.misic@outlook.com>
2022-10-06 21:56:32 +0200

    Extract logo into separate component

--- src/App/Header/MobileHeader/HeaderContent.tsx
+++ src/App/Header/MobileHeader/HeaderContent.tsx
@@ -1,8 +1,8 @@
-import betaWhiteLogo from "assets/images/angelprotocol-beta-horiz-wht.png";
 import Icon, { IconTypes } from "components/Icon";
 import TransactionHint from "components/Transactor/TransactionHint";
 import Airdrop from "components/Transactors/Airdrop/Airdrop";
 import WalletSuite from "components/WalletSuite";
+import Logo from "../Logo";
 
 type Props = {
   onMenuClick: () => void;
@@ -12,13 +12,7 @@ type Props = {
 export default function HeaderContent({ onMenuClick, menuIconType }: Props) {
   return (
     <div className="grid grid-cols-[auto_1fr_auto]  mb-4 items-center w-full padded-container pt-3">
-      <a
-        rel="noreferrer"
-        href="https://angelprotocol.io/"
-        title="Go to Marketing page"
-      >
-        <img src={betaWhiteLogo} alt="" className="w-24 sm:w-36" />
-      </a>
+      <Logo />
       <div className="ml-5 grid grid-cols-[auto_1fr_auto]">
         <TransactionHint />
         <WalletSuite />
--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -1,21 +1,15 @@
-import betaWhiteLogo from "assets/images/angelprotocol-beta-horiz-wht.png";
 import TransactionHint from "components/Transactor/TransactionHint";
 import Airdrop from "components/Transactors/Airdrop/Airdrop";
 import WalletSuite from "components/WalletSuite";
 import DesktopNav from "./DesktopNav";
+import Logo from "./Logo";
 import MobileHeader from "./MobileHeader";
 
 export default function Header() {
   return (
     <>
       <header className="hidden lg:grid grid-cols-[auto_1fr_auto] mb-4 items-center w-full padded-container pt-3">
-        <a
-          rel="noreferrer"
-          href="https://angelprotocol.io/"
-          title="Go to Marketing page"
-        >
-          <img src={betaWhiteLogo} alt="" className="w-32 sm:w-36" />
-        </a>
+        <Logo />
         <DesktopNav />
         <div className="ml-5 grid grid-cols-[auto_1fr_auto]">
           <TransactionHint />
