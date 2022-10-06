e77c24979e2e540aad51718ca8aaa430cc7312ea
Nenad Misic <n.misic@outlook.com>
2022-10-06 22:00:15 +0200

    Rename MobileHeader/HeaderContent to Content

--- src/App/Header/MobileHeader/MobileHeader.tsx
+++ src/App/Header/MobileHeader/MobileHeader.tsx
@@ -1,14 +1,14 @@
 import { Dialog } from "@headlessui/react";
 import { useModalContext } from "contexts/ModalContext";
 import AppLinks from "./AppLinks";
-import HeaderContent from "./HeaderContent";
+import Content from "./Content";
 
 export default function MobileHeader() {
   const { showModal, closeModal } = useModalContext();
 
   return (
     <header className="lg:hidden">
-      <HeaderContent
+      <Content
         onMenuClick={() => showModal(Modal, { closeModal })}
         menuIconType="Menu"
       />
@@ -22,7 +22,7 @@ function Modal({ closeModal }: { closeModal: () => void }) {
       as="header"
       className="fixed top-0 left-0 right-0 w-full z-10 bg-blue-accent flex flex-col"
     >
-      <HeaderContent onMenuClick={closeModal} menuIconType="Close" />
+      <Content onMenuClick={closeModal} menuIconType="Close" />
       <div className="h-px bg-white-grey w-full col-span-3 mb-5" />
       <AppLinks />
     </Dialog.Panel>
