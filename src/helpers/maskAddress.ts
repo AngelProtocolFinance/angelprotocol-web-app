ee2594c7da6404f66ee3b3eb52e837287dceae90
Nenad Misic <n.misic@outlook.com>
2022-10-07 15:06:31 +0200

    Reduce masked address lenght

--- src/helpers/maskAddress.ts
+++ src/helpers/maskAddress.ts
@@ -1,10 +1,8 @@
 export function maskAddress(addr?: string) {
-  const nChars = 6;
   if (!addr) {
     return "";
   } else {
-    const len = addr.length;
-    const middle = addr.substring(nChars, len - nChars);
+    const middle = addr.substring(3, addr.length - 4);
     return addr.replace(middle, "...");
   }
 }
