b2298626a0db72bc9a101a0579fca14dad04c958
Nenad Misic <n.misic@outlook.com>
2022-10-07 11:43:55 +0200

    Handle closing MobileHeader on large screens

--- src/App/Header/MobileHeader/MobileHeader.tsx
+++ src/App/Header/MobileHeader/MobileHeader.tsx
@@ -1,4 +1,5 @@
 import { Dialog } from "@headlessui/react";
+import { useEffect, useState } from "react";
 import { useModalContext } from "contexts/ModalContext";
 import AppLinks from "./AppLinks";
 import Content from "./Content";
@@ -6,24 +7,47 @@ import { adminMobileNavId } from "./constants";
 
 export default function MobileHeader() {
   const { showModal, closeModal } = useModalContext();
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleCloseModal = () => {
+    closeModal();
+    setModalOpen(false);
+  };
+
+  const handleOpenModal = () => {
+    showModal(Modal, { onClose: handleCloseModal });
+    setModalOpen(true);
+  };
+
+  useEffect(() => {
+    if (!isModalOpen) {
+      return;
+    }
+
+    function closeModal() {
+      if (window.innerWidth >= 1024) {
+        closeModal();
+      }
+    }
+    window.addEventListener("resize", closeModal);
+
+    return () => window.removeEventListener("resize", closeModal);
+  }, [isModalOpen, closeModal]);
 
   return (
     <header className="lg:hidden">
-      <Content
-        onMenuClick={() => showModal(Modal, { closeModal })}
-        menuIconType="Menu"
-      />
+      <Content onMenuClick={handleOpenModal} menuIconType="Menu" />
     </header>
   );
 }
 
-function Modal({ closeModal }: { closeModal: () => void }) {
+function Modal({ onClose }: { onClose: () => void }) {
   return (
     <Dialog.Panel
       as="header"
       className="fixed top-0 left-0 right-0 w-full z-10 bg-blue-accent flex flex-col"
     >
-      <Content onMenuClick={closeModal} menuIconType="Close" />
+      <Content onMenuClick={onClose} menuIconType="Close" />
       <div className="h-px bg-white-grey w-full col-span-3 mb-5" />
       <div className="grid grid-cols-[1fr_1fr]">
         <AppLinks />
