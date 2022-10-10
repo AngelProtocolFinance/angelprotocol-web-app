3b6f7ff1fd7cda43c536f7967ad38b4fab4fca3f
Nenad Misic <n.misic@outlook.com>
2022-10-10 11:54:29 +0200

    Update Footer style

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import Socials from "./Socials";
 
 export default function Footer() {
   return (
-    <footer className="padded-container flex flex-col items-center justify-self-stretch">
+    <footer className="padded-container flex flex-col items-center justify-self-stretch text-white">
       <Navigation />
       <div className="flex flex-col items-center gap-2 justify-self-stretch">
         <Socials />
-        <p className="font-body font-semibold text-white text-center text-2xs uppercase">
+        <p className="font-body font-normal text-white text-center text-2xs uppercase">
           Copyright {new Date().getFullYear()} Angel Protocol. All rights
           reserved
         </p>
