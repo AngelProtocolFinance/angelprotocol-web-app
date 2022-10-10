35ee7642f7b5b294717b626b3e6b6da713975bb2
Nenad Misic <n.misic@outlook.com>
2022-10-10 10:37:09 +0200

    Add new Footer scaffold

---
+++ src/App/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import Socials from "./Socials";
+
+export default function Footer() {
+  return (
+    <footer className="padded-container flex flex-col items-center justify-self-stretch bg-thin-blue">
+      <div>
+        <Socials />
+        <p className="text-white-grey/70 text-center text-xs uppercase ">
+          Copyright {new Date().getFullYear()} Angel Protocol. All rights
+          reserved
+        </p>
+      </div>
+    </footer>
+  );
+}
