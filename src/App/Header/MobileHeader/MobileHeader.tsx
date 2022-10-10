d25b1f0cad408b736e16686d170b4125c8329ebf
Nenad Misic <n.misic@outlook.com>
2022-10-10 10:08:16 +0200

    Create MenuButton component in MobileHeader

--- src/App/Header/MobileHeader/MobileHeader.tsx
+++ src/App/Header/MobileHeader/MobileHeader.tsx
@@ -1,22 +1,14 @@
-import Icon from "components/Icon";
 import WalletSuite from "components/WalletSuite";
 import Logo from "../Logo";
-import useMenuModal from "./useMenuModal";
+import MenuButton from "./MenuButton";
 
 export default function MobileHeader() {
-  const openMenu = useMenuModal();
-
   return (
     <header className="lg:hidden flex justify-between items-center w-full padded-container py-3">
       <Logo />
       <div className="flex justify-end gap-3">
         <WalletSuite />
-        <button
-          onClick={openMenu}
-          className="flex items-center text-white-grey justify-center w-10 h-10"
-        >
-          <Icon type="Menu" className="text-2xl" />
-        </button>
+        <MenuButton />
       </div>
     </header>
   );
