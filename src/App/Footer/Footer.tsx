37c86070f92688cbab123415dc7a954b176fa626
Nenad Misic <n.misic@outlook.com>
2022-10-10 10:51:26 +0200

    Create socials and copyright container styles

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="padded-container flex flex-col items-center justify-self-stretch bg-thin-blue">
       <Navigation />
-      <div>
+      <div className="flex flex-col items-center gap-8 justify-self-stretch">
         <Socials />
         <p className="text-white-grey/70 text-center text-xs uppercase ">
           Copyright {new Date().getFullYear()} Angel Protocol. All rights
