e2ace2be2b576d6be8c722f1aaa3cc248e628f4a
Nenad Misic <n.misic@outlook.com>
2022-10-10 10:49:43 +0200

    Add Navigation scaffold

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -1,8 +1,10 @@
+import Navigation from "./Navigation";
 import Socials from "./Socials";
 
 export default function Footer() {
   return (
     <footer className="padded-container flex flex-col items-center justify-self-stretch bg-thin-blue">
+      <Navigation />
       <div>
         <Socials />
         <p className="text-white-grey/70 text-center text-xs uppercase ">
