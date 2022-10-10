97e6598fd1dac2acf118a5c9befa27d0d613cd67
Nenad Misic <n.misic@outlook.com>
2022-10-10 10:04:40 +0200

    Remove redundant div in MobileHeader

--- src/App/Header/MobileHeader/MobileHeader.tsx
+++ src/App/Header/MobileHeader/MobileHeader.tsx
@@ -7,18 +7,16 @@ export default function MobileHeader() {
   const openMenu = useMenuModal();
 
   return (
-    <header className="lg:hidden">
-      <div className="flex justify-between items-center w-full padded-container py-3">
-        <Logo />
-        <div className="flex justify-end gap-3">
-          <WalletSuite />
-          <button
-            onClick={openMenu}
-            className="flex items-center text-white-grey justify-center w-10 h-10"
-          >
-            <Icon type="Menu" className="text-2xl" />
-          </button>
-        </div>
+    <header className="lg:hidden flex justify-between items-center w-full padded-container py-3">
+      <Logo />
+      <div className="flex justify-end gap-3">
+        <WalletSuite />
+        <button
+          onClick={openMenu}
+          className="flex items-center text-white-grey justify-center w-10 h-10"
+        >
+          <Icon type="Menu" className="text-2xl" />
+        </button>
       </div>
     </header>
   );
