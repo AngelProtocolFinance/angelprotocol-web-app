67d02dd57f47627a52474cbff7d0e0fa8536748f
Nenad Misic <n.misic@outlook.com>
2022-10-11 09:39:03 +0200

    Move sections back into Links

--- src/App/Footer/Navigation/Links/Links.tsx
+++ src/App/Footer/Navigation/Links/Links.tsx
@@ -1,10 +1,6 @@
-import { sections } from "./constants";
-
 export default function Links() {
   return (
-    <div
-      className={`grid grid-cols-${sections.length} gap-3 xl:gap-6 max-w-2xl`}
-    >
+    <div className="grid grid-cols-4 gap-3 xl:gap-6 max-w-2xl">
       {sections.map((section) => (
         <div
           key={section.title}
@@ -15,7 +11,7 @@ export default function Links() {
           </h6>
           <div className="flex flex-col items-start h-full">
             {section.links.map((link) => {
-              const commonStyle = "font-sans text-xs xl:text-sm";
+              const commonStyle = "font-body text-xs xl:text-sm";
               return link.href ? (
                 <a
                   key={link.text}
@@ -36,3 +32,66 @@ export default function Links() {
     </div>
   );
 }
+
+type Section = {
+  title: string;
+  links: {
+    text: string;
+    href?: string;
+  }[];
+};
+
+const sections: Section[] = [
+  {
+    title: "Products",
+    links: [
+      {
+        text: "For Non-profit orgs.",
+        href: "https://angelprotocol.io/non-profit-organizations/",
+      },
+      { text: "For Donors", href: "https://angelprotocol.io/donors/" },
+      {
+        text: "For Changemakers",
+        href: "https://angelprotocol.io/changemakers/",
+      },
+      { text: "For Investors", href: "https://angelprotocol.io/investors/" },
+      {
+        text: "For CSR Partners",
+        href: "https://angelprotocol.io/csr-partners/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    links: [
+      { text: "About us", href: "https://angelprotocol.io/about/" },
+      {
+        text: "Meet the team",
+        href: "https://angelprotocol.io/about/#:~:text=MEET%20THE%20TEAM-,Operations,-DUFFY%20CASEY",
+      },
+      { text: "News", href: "https://angelprotocol.io/news/" },
+      { text: "Stories", href: "https://angelprotocol.io/stories/" },
+      { text: "Careers", href: "https://angelprotocol.io/careers/" },
+    ],
+  },
+  {
+    title: "Docs",
+    links: [
+      {
+        text: "Litepaper",
+        href: "https://angelprotocol.io/docs/litepaper-introduction/",
+      },
+      { text: "FAQs (coming soon)" },
+      { text: "Technical doc (coming soon)" },
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      {
+        text: "Privacy policy",
+        href: "https://angelprotocol.io/privacy-policy/",
+      },
+    ],
+  },
+];
