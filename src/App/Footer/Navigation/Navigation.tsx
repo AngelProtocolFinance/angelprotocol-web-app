8043f0feb133e16bcd3609a2a490e2359df55d66
Nenad Misic <n.misic@outlook.com>
2022-10-11 16:38:51 +0200

    Fix Navigation responsiveness

--- src/App/Footer/Navigation/Navigation.tsx
+++ src/App/Footer/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import Newsletter from "./Newsletter";
 
 export default function Navigation() {
   return (
-    <section className="flex items-center justify-center w-full px-40 pt-8 pb-16 border-b border-gray-l2">
+    <section className="flex items-center justify-center w-full pt-8 pb-16 border-b border-gray-l2">
       <div className="padded-container flex justify-between items-start w-full">
         <Links />
         <Newsletter />
