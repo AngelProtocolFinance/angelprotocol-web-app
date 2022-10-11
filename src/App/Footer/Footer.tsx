93fececa21eac3098b9afbfc4c408d93d59e8444
Nenad Misic <n.misic@outlook.com>
2022-10-11 09:48:13 +0200

    Fix overall styling/paddings

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import Socials from "./Socials";
 
 export default function Footer() {
   return (
-    <footer className="grid grid-rows-2 items-center px-2.5 text-white">
+    <footer className="flex flex-col items-center px-2.5 text-white">
       <Navigation />
-      <section className="flex flex-col items-center gap-2 justify-self-stretch">
+      <section className="flex flex-col items-center gap-2 w-full py-4 px-40">
         <Socials />
         <p className="font-body font-normal text-white text-center text-2xs uppercase">
           Copyright {new Date().getFullYear()} Angel Protocol. All rights
--- src/App/Footer/Navigation/Navigation.tsx
+++ src/App/Footer/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import Newsletter from "./Newsletter";
 
 export default function Navigation() {
   return (
-    <section className="flex justify-between justify-self-stretch items-start px-40 pt-8 pb-16 border-b border-gray-l2">
+    <section className="flex justify-between items-start w-full px-40 pt-8 pb-16 border-b border-gray-l2">
       <Links />
       <Newsletter />
     </section>
