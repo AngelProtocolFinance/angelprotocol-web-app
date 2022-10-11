f37ed1ea9e5c0d4c3305861a8c5c466718f5445e
Nenad Misic <n.misic@outlook.com>
2022-10-11 08:15:30 +0200

    Add links and newsletter scaffolds

--- src/App/Footer/Navigation/Navigation.tsx
+++ src/App/Footer/Navigation/Navigation.tsx
@@ -1,7 +1,11 @@
+import Links from "./Links";
+import Newsletter from "./Newsletter";
+
 export default function Navigation() {
   return (
     <section className="flex justify-between justify-self-stretch items-start pt-8 pb-16 border-b border-gray-l2">
-      Navigation
+      <Links />
+      <Newsletter />
     </section>
   );
 }
