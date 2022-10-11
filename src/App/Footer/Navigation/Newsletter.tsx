fd54b96464a388dc62bb45e8b1e3b94903960ae4
Nenad Misic <n.misic@outlook.com>
2022-10-11 10:34:20 +0200

    Add text section to newsletter

--- src/App/Footer/Navigation/Newsletter.tsx
+++ src/App/Footer/Navigation/Newsletter.tsx
@@ -1,3 +1,24 @@
 export default function Newsletter() {
-  return <div>Newsletter</div>;
+  return (
+    <div className="flex flex-col items-start gap-2.5 ">
+      <div className="flex flex-col items-start">
+        <h6 className="font-header font-bold text-sm xl:text-base uppercase">
+          Subscribe to our newsletter
+        </h6>
+        <p className="font-body font-normal text-xs xl:text-sm">
+          By subscribing to this newsletter you confirm that you have read and
+          agree with our{" "}
+          <a
+            href="https://angelprotocol.io/privacy-policy/"
+            rel="noreferrer"
+            target="_blank"
+            className="cursor-pointer underline"
+          >
+            Privacy Policy
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
