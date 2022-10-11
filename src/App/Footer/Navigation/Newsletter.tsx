57f3b49252ad827505c9e015a8d5a3340410ba2d
Nenad Misic <n.misic@outlook.com>
2022-10-11 10:52:18 +0200

    Add email form

--- src/App/Footer/Navigation/Newsletter.tsx
+++ src/App/Footer/Navigation/Newsletter.tsx
@@ -18,6 +18,18 @@ export default function Newsletter() {
           </a>
           .
         </p>
+        <form
+          className="flex items-start gap-3 xl:gap-6"
+          onSubmit={(e) => console.log("submitted", e)}
+        >
+          <input
+            className="flex items-center py-2 px-3 border border-gray-l2 rounded-lg"
+            placeholder="Enter your email address here"
+          />
+          <button type="submit" className="btn-primary uppercase">
+            Subscribe
+          </button>
+        </form>
       </div>
     </div>
   );
