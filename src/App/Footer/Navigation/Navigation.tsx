b7bfe47271936de8f125c884c92471de53271982
Nenad Misic <n.misic@outlook.com>
2022-10-13 11:32:13 +0200

    Add back dark mode border

--- src/App/Footer/Navigation/Navigation.tsx
+++ src/App/Footer/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import Newsletter from "./Newsletter";
 
 export default function Navigation() {
   return (
-    <section className="flex items-center justify-center w-full pt-8 pb-16 border-b border-gray-l2">
+    <section className="flex items-center justify-center w-full pt-8 pb-16 border-b border-gray-l2 dark:border-gray-d3">
       <div className="padded-container flex justify-between items-start w-full">
         <Links />
         <Newsletter />
