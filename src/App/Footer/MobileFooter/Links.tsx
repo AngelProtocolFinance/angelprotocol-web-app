0da9e7244f907b9704818c5621b41997f9ef71e0
Nenad Misic <n.misic@outlook.com>
2022-10-14 10:21:54 +0200

    Add missing rel and target to litepaper <a> tag

--- src/App/Footer/MobileFooter/Links.tsx
+++ src/App/Footer/MobileFooter/Links.tsx
@@ -3,7 +3,13 @@ import { LITEPAPER } from "constants/urls";
 export default function Links() {
   return (
     <div className="flex flex-col items-center py-6 gap-3 w-full">
-      <a href={LITEPAPER} className="font-normal text-sm" title="Litepaper">
+      <a
+        href={LITEPAPER}
+        className="font-normal text-sm"
+        title="Litepaper"
+        rel="noreferrer"
+        target="_blank"
+      >
         Download Litepaper
       </a>
     </div>
