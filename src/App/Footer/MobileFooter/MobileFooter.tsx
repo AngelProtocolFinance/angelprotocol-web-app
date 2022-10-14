d8a01ad7052a6f7b3081325a3edff1f478ac3b86
Nenad Misic <n.misic@outlook.com>
2022-10-14 11:16:51 +0200

    Move Links content to MobileFooter

--- src/App/Footer/MobileFooter/MobileFooter.tsx
+++ src/App/Footer/MobileFooter/MobileFooter.tsx
@@ -1,7 +1,11 @@
 import WalletSuite from "App/Header/WalletSuite";
-import Links from "./Links";
+import { PropsWithChildren } from "react";
+import { FEEDBACK, LITEPAPER, PRIVACY_POLICY, SUPPORT } from "constants/urls";
 import Logo from "./Logo";
 import Socials from "./Socials";
+
+const LINK_STYLE =
+  "font-['Work_Sans'] text-sm hover:text-gray-l4 active:gray-l3";
 
 export default function MobileFooter() {
   return (
@@ -14,7 +18,38 @@ export default function MobileFooter() {
         </div>
       </section>
 
-      <Links />
+      <section className="flex flex-col items-center py-6 gap-3 w-full">
+        <a
+          className={`font-normal ${LINK_STYLE}`}
+          href={LITEPAPER}
+          rel="noreferrer"
+          target="_blank"
+          title="Litepaper"
+        >
+          Download Litepaper
+        </a>
+        <span className="flex justify-around w-full">
+          <BoldLink href={SUPPORT}>Support</BoldLink>
+          <BoldLink href={FEEDBACK}>Feedback</BoldLink>
+          <BoldLink href={PRIVACY_POLICY}>Privacy Policy</BoldLink>
+        </span>
+        <span className="font-['work_sans'] font-normal text-sm">
+          &#169; {new Date().getFullYear()} Angel Protocol
+        </span>
+      </section>
     </div>
+  );
+}
+
+function BoldLink({ children, href }: PropsWithChildren<{ href: string }>) {
+  return (
+    <a
+      className={`font-bold ${LINK_STYLE}`}
+      href={href}
+      rel="noreferrer"
+      target="_blank"
+    >
+      {children}
+    </a>
   );
 }
