80e8abd07b738485b6d63036449badd561f08707
Nenad Misic <n.misic@outlook.com>
2022-10-14 10:30:34 +0200

    Add bold links

--- src/App/Footer/MobileFooter/Links.tsx
+++ src/App/Footer/MobileFooter/Links.tsx
@@ -1,17 +1,38 @@
-import { LITEPAPER } from "constants/urls";
+import { PropsWithChildren } from "react";
+import { FEEDBACK, LITEPAPER, PRIVACY_POLICY, SUPPORT } from "constants/urls";
+
+const LINK_STYLE = "text-sm hover:text-gray-l4 active:gray-l3";
 
 export default function Links() {
   return (
     <div className="flex flex-col items-center py-6 gap-3 w-full">
       <a
+        className={`font-normal ${LINK_STYLE}`}
         href={LITEPAPER}
-        className="font-normal text-sm"
-        title="Litepaper"
         rel="noreferrer"
         target="_blank"
+        title="Litepaper"
       >
         Download Litepaper
       </a>
+      <span className="flex justify-between w-full">
+        <BoldLink href={SUPPORT}>Support</BoldLink>
+        <BoldLink href={FEEDBACK}>Feedback</BoldLink>
+        <BoldLink href={PRIVACY_POLICY}>Privacy Policy</BoldLink>
+      </span>
     </div>
+  );
+}
+
+function BoldLink({ children, href }: PropsWithChildren<{ href: string }>) {
+  return (
+    <a
+      className={`font-bold ${LINK_STYLE}`}
+      href={href}
+      rel="noreferrer"
+      target="_blank"
+    >
+      {children}
+    </a>
   );
 }
