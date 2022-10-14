935e676628babead6abd9c8d089a1d40f0dfb31a
Nenad Misic <n.misic@outlook.com>
2022-10-14 10:05:33 +0200

    Refactor mobile footer to widen the bottom border to full

--- src/App/Footer/MobileFooter/MobileFooter.tsx
+++ src/App/Footer/MobileFooter/MobileFooter.tsx
@@ -5,11 +5,13 @@ import Socials from "./Socials";
 export default function MobileFooter() {
   return (
     <div className="flex sm:hidden flex-col items-center px-6 bg-blue dark:bg-blue-d4 text-white">
-      <div className="flex flex-col items-center pt-6 pb-10 gap-6 border-b border-gray-l2 dark:border-gray-l3">
-        <Logo />
-        <Socials />
-        <WalletSuite menuPlacement="top" />
-      </div>
+      <section className="border-b border-gray-l2 dark:border-gray-l3 w-full">
+        <div className="flex flex-col items-center gap-6 pt-6 pb-10">
+          <Logo />
+          <Socials />
+          <WalletSuite menuPlacement="top" />
+        </div>
+      </section>
     </div>
   );
 }
