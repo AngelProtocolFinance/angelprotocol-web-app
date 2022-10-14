3a0c8f624493a3de2665743d88cfa12d4feaaa20
Nenad Misic <n.misic@outlook.com>
2022-10-14 10:19:09 +0200

    Add text to litepaper link

--- src/App/Footer/MobileFooter/Links.tsx
+++ src/App/Footer/MobileFooter/Links.tsx
@@ -3,7 +3,9 @@ import { LITEPAPER } from "constants/urls";
 export default function Links() {
   return (
     <div className="flex flex-col items-center py-6 gap-3 w-full">
-      <a href={LITEPAPER} className="font-normal text-sm"></a>
+      <a href={LITEPAPER} className="font-normal text-sm" title="Litepaper">
+        Download Litepaper
+      </a>
     </div>
   );
 }
