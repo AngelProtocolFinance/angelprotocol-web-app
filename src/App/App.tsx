5e6f799cbbc20cecd6debddfcf3b464bcf0dc589
ap-justin <justin@angelprotocol.io>
2022-10-18 11:32:48 +0800

    revert prev app bg

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,7 +8,7 @@ import { chainOptions } from "./chainOptions";
 
 export default function App() {
   return (
-    <div className="grid grid-rows-[1fr_auto] bg-orange-l6 dark:bg-blue-d4">
+    <div className="grid grid-rows-[1fr_auto] bg-gradient-to-b from-blue to-black bg-fixed dark:from-blue-d3 dark:to-blue-d3">
       <WalletProvider {...chainOptions}>
         <WalletContext>
           <ModalContext backdropClasses="z-10 fixed inset-0 bg-black/50">
