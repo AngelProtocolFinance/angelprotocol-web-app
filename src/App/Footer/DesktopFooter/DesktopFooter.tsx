28e11a8d64bb14b8e3693e70335989345cb99ec7
Nenad Misic <n.misic@outlook.com>
2022-10-18 10:16:02 +0200

    Remove horiz. padding on Socials section

--- src/App/Footer/DesktopFooter/DesktopFooter.tsx
+++ src/App/Footer/DesktopFooter/DesktopFooter.tsx
@@ -5,7 +5,7 @@ export default function DesktopFooter() {
   return (
     <footer className="hidden sm:flex flex-col items-center px-2.5 text-white bg-blue dark:bg-blue-d4">
       <Navigation />
-      <section className="flex flex-col items-center gap-2 w-full py-4 px-40">
+      <section className="flex flex-col items-center gap-2 w-full py-4">
         <Socials />
         <p className="font-body font-normal text-white text-center text-2xs uppercase">
           Copyright {new Date().getFullYear()} Angel Protocol. All rights
