53c3afe03923a51335d723652c06d89f985bb4f6
Nenad Misic <n.misic@outlook.com>
2022-10-18 09:48:39 +0200

    Align footer in column on smaller screens

--- src/App/Footer/DesktopFooter/Navigation/Navigation.tsx
+++ src/App/Footer/DesktopFooter/Navigation/Navigation.tsx
@@ -4,7 +4,7 @@ import Newsletter from "./Newsletter";
 export default function Navigation() {
   return (
     <section className="flex items-center justify-center w-full pt-8 pb-16 border-b border-gray-l2 dark:border-gray-l3">
-      <div className="padded-container flex justify-between items-start w-full">
+      <div className="padded-container flex flex-col items-center justify-between w-full lg:flex-row lg:items-start">
         <Links />
         <Newsletter />
       </div>
