cf563d5c638c4f82f7fa5a9c69c54adc60e97239
Nenad Misic <n.misic@outlook.com>
2022-10-19 17:17:20 +0200

    Refactor DesktopProfile (remove Banner.tsx)

--- src/pages/Profile/DesktopProfile/DesktopProfile.tsx
+++ src/pages/Profile/DesktopProfile/DesktopProfile.tsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import PageError from "../PageError";
 import { ProfileContext } from "../ProfileProvider";
-import Banner from "./Banner";
 import Body from "./Body";
+import { useLocalContext } from "./LocalContext";
 import LocalContext from "./LocalContext";
 import Logo from "./Logo";
 import Skeleton from "./Skeleton";
@@ -20,14 +20,30 @@ export default function DesktopProfile() {
 
   return (
     <LocalContext.Provider value={{ profile }}>
-      <section className="hidden sm:flex flex-col items-center isolate relative w-full h-full">
-        <Banner />
-        <Body />
-
+      <section className="hidden sm:block isolate relative w-full h-full">
         <div className="absolute left-20 top-52">
           <Logo />
         </div>
+
+        <div className="flex flex-col items-center w-full h-full">
+          <Banner />
+          <Body />
+        </div>
       </section>
     </LocalContext.Provider>
+  );
+}
+
+function Banner() {
+  const { profile } = useLocalContext();
+  return (
+    <div className="relative w-full h-72">
+      <img
+        src={profile.image}
+        alt=""
+        className="absolute h-full w-full object-cover object-right opacity-10"
+      />
+      <div className="absolute -z-10 bg-blue dark:bg-blue-d4 h-full w-full" />
+    </div>
   );
 }
