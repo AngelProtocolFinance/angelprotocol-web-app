65d156fbd0efdfc4219cd204de5b59381972683f
Nenad Misic <n.misic@outlook.com>
2022-10-19 14:48:45 +0200

    Fix: Display logo if existing

--- src/pages/Profile/DesktopProfile/Logo.tsx
+++ src/pages/Profile/DesktopProfile/Logo.tsx
@@ -7,7 +7,7 @@ const logoStyle =
 export default function Logo() {
   const { profile } = useLocalContext();
 
-  if (profile.logo) {
+  if (!profile.logo) {
     return (
       <div className={`${logoStyle} flex items-center justify-center`}>
         <Icon type="Picture" className="w-9 h-9 text-white dark:text-blue-l3" />
