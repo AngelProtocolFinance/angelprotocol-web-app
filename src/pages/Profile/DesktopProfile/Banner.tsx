db4076d4bd3ec815b7ee0318f7eb0c7b7c3d7650
Nenad Misic <n.misic@outlook.com>
2022-10-19 13:36:51 +0200

    Add Banner

--- src/pages/Profile/DesktopProfile/Banner.tsx
+++ src/pages/Profile/DesktopProfile/Banner.tsx
@@ -1,3 +1,17 @@
-export default function Banner() {
-  return <div>Banner</div>;
+import { useLocalContext } from "./LocalContext";
+
+type Props = { className?: string };
+
+export default function Banner({ className = "" }: Props) {
+  const { profile } = useLocalContext();
+  return (
+    <div className={`relative w-full ${className}`}>
+      <img
+        src={profile.image}
+        alt=""
+        className="absolute h-full w-full object-cover object-right opacity-10"
+      />
+      <div className="absolute -z-10 bg-blue dark:bg-blue-d4 h-full w-full" />
+    </div>
+  );
 }
