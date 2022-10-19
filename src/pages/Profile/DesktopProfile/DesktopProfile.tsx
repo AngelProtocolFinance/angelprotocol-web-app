0b17069a9f97ffe673acb1459d3b79f451f8136a
Nenad Misic <n.misic@outlook.com>
2022-10-19 16:54:36 +0200

    Update Skeleton

--- src/pages/Profile/DesktopProfile/DesktopProfile.tsx
+++ src/pages/Profile/DesktopProfile/DesktopProfile.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import ContentLoader from "components/ContentLoader";
 import Icon from "components/Icon";
+import Loader from "components/Loader";
 import { appRoutes } from "constants/routes";
 import { ProfileContext } from "../ProfileProvider";
 import Banner from "./Banner";
@@ -36,15 +37,32 @@ export default function DesktopProfile() {
 
 function Skeleton() {
   return (
-    <section className="padded-container grid grid-cols-1 lg:grid-cols-[2fr_5fr] grid-rows-[auto_auto_1fr] gap-4 pb-16 pt-28 content-start opacity-20">
-      <ContentLoader className="w-48 h-10 lg:col-span-2" />
-      <ContentLoader className="w-full rounded-md" />
-      <div className="w-full row-span-2 grid grid grid-rows-[auto_auto_1fr]">
-        <ContentLoader className="w-full h-[300px] rounded-md" />
-        <ContentLoader className="w-full h-10 mt-2 rounded-md" />
-        <ContentLoader className="w-full h-full mt-2 rounded-md" />
+    <section className="hidden sm:flex flex-col items-center isolate relative w-full h-full">
+      <div className="flex items-center justify-center h-72 w-full">
+        <Loader bgColorClass="bg-white/80" gapClass="gap-2" widthClass="w-4" />
       </div>
-      <ContentLoader className="hidden lg:block mt-2 h-full w-full rounded-md" />
+      <div className="w-full h-full bg-white dark:bg-blue-d5 flex pt-6 pb-20 px-20">
+        <div className="flex flex-col gap-10 w-full h-full">
+          <div className="w-full h-52 grid grid-rows-2 gap-4">
+            <ContentLoader className="w-2/5 h-full justify-self-end" />
+            <ContentLoader className="w-full h-full" />
+          </div>
+          <div className="flex gap-8 w-full h-full">
+            <ContentLoader className="w-full h-[905px]" />
+            <ContentLoader className="w-[405px] h-[905px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute left-auto lg:left-20 top-36">
+        <div className="box-border h-44 w-44 rounded-full bg-blue-l3 object-contain dark:bg-blue">
+          <Loader
+            bgColorClass="bg-white/80"
+            gapClass="gap-2"
+            widthClass="w-4"
+          />
+        </div>
+      </div>
     </section>
   );
 }
