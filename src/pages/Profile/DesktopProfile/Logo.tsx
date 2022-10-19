090191b06913c2eb32a0ee17540c3335e18264c7
Nenad Misic <n.misic@outlook.com>
2022-10-19 14:43:55 +0200

    Refactor Logo

--- src/pages/Profile/DesktopProfile/Logo.tsx
+++ src/pages/Profile/DesktopProfile/Logo.tsx
@@ -1,20 +1,29 @@
 import Icon from "components/Icon";
 import { useLocalContext } from "./LocalContext";
 
+const logoStyle =
+  "box-border h-44 w-44 border border-gray-l2 rounded-full bg-blue-l3 object-contain dark:bg-blue dark:border-blue-capital";
+
 type Props = { className?: string };
 
 export default function Logo({ className = "" }: Props) {
   const { profile } = useLocalContext();
 
-  const logoStyle = `box-border h-44 w-44 border border-gray-l2 rounded-full bg-blue-l3 object-contain dark:bg-blue dark:border-blue-capital ${className}`;
-
   if (profile.logo) {
     return (
-      <div className={`${logoStyle} flex items-center justify-center`}>
+      <div
+        className={`${logoStyle} ${className} flex items-center justify-center`}
+      >
         <Icon type="Picture" className="w-9 h-9 text-white dark:text-blue-l3" />
       </div>
     );
   }
 
-  return <img className={logoStyle} alt="logo" src={profile.logo} />;
+  return (
+    <img
+      className={`${logoStyle} ${className}`}
+      alt="logo"
+      src={profile.logo}
+    />
+  );
 }
