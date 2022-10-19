e915d2be61d45efdb9f5d8448a9a6824788dac00
Nenad Misic <n.misic@outlook.com>
2022-10-19 16:56:14 +0200

    Extract Skeleton and PageError

---
+++ src/pages/Profile/DesktopProfile/Skeleton.tsx
@@ -0,0 +1,34 @@
+import ContentLoader from "components/ContentLoader";
+import Loader from "components/Loader";
+
+export default function Skeleton() {
+  return (
+    <section className="hidden sm:flex flex-col items-center isolate relative w-full h-full">
+      <div className="flex items-center justify-center h-72 w-full">
+        <Loader bgColorClass="bg-white/80" gapClass="gap-2" widthClass="w-4" />
+      </div>
+      <div className="w-full h-full bg-white dark:bg-blue-d5 flex pt-6 pb-20 px-20">
+        <div className="flex flex-col gap-10 w-full h-full">
+          <div className="w-full h-52 grid grid-rows-2 gap-4">
+            <ContentLoader className="w-2/5 h-full justify-self-end" />
+            <ContentLoader className="w-full h-full" />
+          </div>
+          <div className="flex gap-8 w-full h-full">
+            <ContentLoader className="w-full h-[905px]" />
+            <ContentLoader className="w-[405px] h-[905px]" />
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute left-auto lg:left-20 top-36">
+        <div className="box-border h-44 w-44 rounded-full bg-blue-l3 object-contain dark:bg-blue">
+          <Loader
+            bgColorClass="bg-white/80"
+            gapClass="gap-2"
+            widthClass="w-4"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
