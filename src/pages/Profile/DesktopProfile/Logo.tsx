036f10f10bb7c264f647b13ba7c9985b2324d666
Nenad Misic <n.misic@outlook.com>
2022-10-19 14:21:46 +0200

    Add dark mode styles

--- src/pages/Profile/DesktopProfile/Logo.tsx
+++ src/pages/Profile/DesktopProfile/Logo.tsx
@@ -6,12 +6,12 @@ type Props = { className?: string };
 export default function Logo({ className = "" }: Props) {
   const { profile } = useLocalContext();
 
-  const logoStyle = `box-border h-44 w-44 border border-gray-l2 rounded-full bg-blue-l3 object-contain ${className}`;
+  const logoStyle = `box-border h-44 w-44 border border-gray-l2 rounded-full bg-blue-l3 object-contain dark:bg-blue dark:border-blue-capital ${className}`;
 
-  if (!profile.logo) {
+  if (profile.logo) {
     return (
       <div className={`${logoStyle} flex items-center justify-center`}>
-        <Icon type="Picture" className="w-9 h-9 text-white" />
+        <Icon type="Picture" className="w-9 h-9 text-white dark:text-blue-l3" />
       </div>
     );
   }
