5ee6997b222776cf25601225b7fc4e5104b11d93
Nenad Misic <n.misic@outlook.com>
2022-10-19 17:35:21 +0200

    Make default logo bg white

--- src/pages/Profile/DesktopProfile/Logo.tsx
+++ src/pages/Profile/DesktopProfile/Logo.tsx
@@ -2,7 +2,7 @@ import Icon from "components/Icon";
 import { useLocalContext } from "./LocalContext";
 
 const logoStyle =
-  "box-border h-44 w-44 border border-gray-l2 rounded-full bg-blue-l3 object-contain dark:bg-blue dark:border-blue-capital";
+  "box-border h-44 w-44 border border-gray-l2 rounded-full dark:border-blue-capital";
 
 export default function Logo() {
   const { profile } = useLocalContext();
@@ -10,10 +10,19 @@ export default function Logo() {
   if (!profile.logo) {
     return (
       <div className={`${logoStyle} flex items-center justify-center`}>
-        <Icon type="Picture" className="w-9 h-9 text-white dark:text-blue-l3" />
+        <Icon
+          type="Picture"
+          className="w-9 h-9 bg-blue-l3 text-white dark:bg-blue dark:text-blue-l3"
+        />
       </div>
     );
   }
 
-  return <img className={logoStyle} alt="logo" src={profile.logo} />;
+  return (
+    <img
+      className={`${logoStyle} bg-white object-contain`}
+      alt="logo"
+      src={profile.logo}
+    />
+  );
 }
