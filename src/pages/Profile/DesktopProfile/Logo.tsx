05f83770685533365d5588847dbf3ba148b8a957
Nenad Misic <n.misic@outlook.com>
2022-10-19 13:51:42 +0200

    Create Logo scaffold

--- src/pages/Profile/DesktopProfile/Logo.tsx
+++ src/pages/Profile/DesktopProfile/Logo.tsx
@@ -1,3 +1,15 @@
-export default function Logo() {
-  return <div>Logo</div>;
+import { useLocalContext } from "./LocalContext";
+
+type Props = { className?: string };
+
+export default function Logo({ className = "" }: Props) {
+  const { profile } = useLocalContext();
+
+  return (
+    <div
+      className={`box-border border border-gray-l2 h-44 w-44 rounded-full ${className}`}
+    >
+      Logo
+    </div>
+  );
 }
