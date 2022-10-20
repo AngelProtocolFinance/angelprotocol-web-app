dd9a2e451be9feb305908d15e494de00f8f228bd
Nenad Misic <n.misic@outlook.com>
2022-10-20 13:08:32 +0200

    Remove MobileProfile

--- src/pages/Profile/MobileProfile/Body/Body.tsx
+++
@@ -1,3 +0,0 @@
-export default function Body() {
-  return <div className="w-full h-80 bg-white dark:bg-blue-d4">Body</div>;
-}
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,66 @@
-import DesktopProfile from "./DesktopProfile";
-import ProfileProvider from "./ProfileProvider";
+import { useParams } from "react-router-dom";
+import {
+  useEndowmentDetailsQuery,
+  useEndowmentProfileQuery,
+} from "services/juno/account";
+import { idParamToNum } from "helpers";
+import Body from "./Body";
+import LocalContext, { useLocalContext } from "./LocalContext";
+import Logo from "./Logo";
+import PageError from "./PageError";
+import Skeleton from "./Skeleton";
 
 export default function Profile() {
+  const { id } = useParams<{ id: string }>();
+  const numId = idParamToNum(id);
+  const {
+    data: profile,
+    isLoading: isProfileLoading,
+    isError: isProfileError,
+  } = useEndowmentProfileQuery({ id: numId }, { skip: numId === 0 });
+
+  const {
+    data: endowment,
+    isLoading: isEndowLoading,
+    isError: isEndowError,
+  } = useEndowmentDetailsQuery({ id: numId }, { skip: numId === 0 });
+
+  if (isEndowLoading || isProfileLoading) {
+    return <Skeleton />;
+  }
+
+  if (isEndowError || isProfileError || !profile || !endowment) {
+    return <PageError />;
+  }
+
   return (
-    <ProfileProvider>
-      <DesktopProfile />
-    </ProfileProvider>
+    <LocalContext.Provider
+      value={{
+        ...profile,
+        kyc_donors_only: endowment.kyc_donors_only,
+        id: numId,
+      }}
+    >
+      <section className="flex flex-col items-center isolate relative w-full h-full">
+        <Banner />
+        <Body />
+
+        <Logo className="absolute left-auto top-32 z-10 sm:left-20 sm:top-48" />
+      </section>
+    </LocalContext.Provider>
+  );
+}
+
+function Banner() {
+  const { image } = useLocalContext();
+  return (
+    <div className="relative w-full h-52 sm:h-72">
+      <img
+        src={image}
+        alt=""
+        className="absolute h-full w-full object-cover object-right opacity-10"
+      />
+      <div className="absolute -z-10 bg-blue dark:bg-blue-d3 h-full w-full" />
+    </div>
   );
 }
