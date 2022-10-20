b9d394f8866885a067d12f0af9cb6951c50ceeff
Nenad Misic <n.misic@outlook.com>
2022-10-20 12:56:00 +0200

    Remove padded-container from PageError

--- src/pages/Profile/PageError.tsx
+++ src/pages/Profile/PageError.tsx
@@ -4,7 +4,7 @@ import { appRoutes } from "constants/routes";
 
 export default function PageError() {
   return (
-    <section className="padded-container flex flex-col items-center justify-center w-full h-screen gap-2 bg-blue dark:bg-blue-d4 text-red-l4 dark:text-red-l2">
+    <section className="flex flex-col items-center justify-center w-full h-screen gap-2 bg-blue dark:bg-blue-d4 text-red-l4 dark:text-red-l2">
       <Icon type="Warning" size={30} />
       <p className="text-lg text-center">Failed to load endowment profile</p>
       <Link
