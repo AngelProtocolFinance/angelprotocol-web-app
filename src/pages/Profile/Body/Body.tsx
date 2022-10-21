0bf14541b1c66f0aea85621021811a563e6dab11
Nenad Misic <n.misic@outlook.com>
2022-10-21 16:13:33 +0200

    Make name address h-full only on lg

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -32,7 +32,7 @@ export default function Body() {
         </button>
       </div>
 
-      <div className="order-1 flex flex-col gap-8 items-center w-full max-w-sm h-full text-black dark:text-white lg:order-2 lg:gap-6 lg:items-start">
+      <div className="order-1 flex flex-col gap-8 items-center w-full max-w-sm text-black dark:text-white lg:order-2 lg:gap-6 lg:items-start lg:h-full">
         <h3 className="font-header font-bold text-3xl">{profile.name}</h3>
         <span className="flex items-center justify-center gap-2 text-base font-work uppercase">
           <Icon type="MapPin" className="h-6 w-6" />
