74e743d9dfda4b7114d519efd8c4c6c581890a9b
Nenad Misic <n.misic@outlook.com>
2022-10-21 10:43:48 +0200

    Add balance scaffold

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -1,8 +1,11 @@
+import { useBalanceQuery } from "services/juno/account";
 import Icon from "components/Icon";
+import { QueryLoader } from "components/admin";
 import { useProfileContext } from "../ProfileContext";
 
 export default function Body() {
   const profile = useProfileContext();
+  const queryState = useBalanceQuery({ id: profile.id });
 
   return (
     <div className="grid grid-rows-[auto_auto_1fr] grid-cols-[auto_auto] gap-10 items-end w-full h-full pt-6 px-20 pb-20 bg-white dark:bg-blue-d4">
@@ -34,10 +37,35 @@ export default function Body() {
           {profile.street_address}
         </span>
       </div>
-      <div className="flex items-center justify-end gap-6 w-[810px]">
-        balances
+      <div className="flex items-center justify-cener gap-4 w-full">
+        <QueryLoader
+          queryState={queryState}
+          messages={{
+            loading: "Fetching endowment balances",
+            error: "Failed to get endowment balances ",
+          }}
+        >
+          {({ tokens_on_hand: { locked, liquid } }) => (
+            <>
+              <Balance title="Total Value" amount={293.281} />
+              <Balance title="Total Locked Account" amount={293.281} />
+              <Balance title="Total Liquid Account" amount={293.281} />
+            </>
+          )}
+        </QueryLoader>
       </div>
       <div className="col-span-2 w-full h-40">Overview</div>
+    </div>
+  );
+}
+
+function Balance({ title, amount }: { title: string; amount: number }) {
+  return (
+    <div className="flex flex-col justify-center items-start px-6 gap-2 border border-gray-l2 rounded">
+      <h6 className="font-heading font-bold text-xs tracking-wider uppercase">
+        {title}
+      </h6>
+      <p className="font-work font-normal text-lg text-gray-d1">${amount}</p>
     </div>
   );
 }
