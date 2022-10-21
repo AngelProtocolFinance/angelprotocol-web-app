29cbb6aaebb80d0e5d36b1f31bde5329f781e164
Nenad Misic <n.misic@outlook.com>
2022-10-21 15:52:06 +0200

    Make Logo position part of regular layout (non-absolute)

--- src/pages/Profile/Logo.tsx
+++ src/pages/Profile/Logo.tsx
@@ -1,32 +1,33 @@
 import Icon from "components/Icon";
 import { useProfileContext } from "./ProfileContext";
 
+const anchorStyle =
+  "flex items-center justify-center w-full h-0 overflow-visible isolate lg:justify-start lg:pl-20";
+
 const logoStyle =
-  "box-border h-40 w-40 sm:h-44 sm:w-44 border border-gray-l2 rounded-full dark:border-bluegray-d1";
+  "box-border h-40 w-40 sm:h-44 sm:w-44 border border-gray-l2 rounded-full object-contain dark:border-bluegray-d1";
 
-type Props = { className?: string };
-
-export default function Logo({ className = "" }: Props) {
+export default function Logo() {
   const { logo } = useProfileContext();
 
   if (!logo) {
     return (
-      <div
-        className={`${logoStyle} ${className} flex items-center justify-center`}
-      >
-        <Icon
-          type="Picture"
-          className="w-9 h-9 bg-blue-l3 text-white dark:bg-blue dark:text-blue-l3"
-        />
+      <div className={anchorStyle}>
+        <div
+          className={`${logoStyle} flex items-center justify-center bg-blue-l3 dark:bg-blue`}
+        >
+          <Icon
+            type="Picture"
+            className="w-9 h-9 bg-blue-l3 text-white dark:bg-blue dark:text-blue-l3"
+          />
+        </div>
       </div>
     );
   }
 
   return (
-    <img
-      className={`${logoStyle} ${className} bg-white object-contain`}
-      alt="logo"
-      src={logo}
-    />
+    <div className={anchorStyle}>
+      <img className={`${logoStyle} bg-white`} alt="logo" src={logo} />
+    </div>
   );
 }
