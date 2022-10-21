6a26c98be96cbb9f28525ad79ec4a5622e406e2b
Nenad Misic <n.misic@outlook.com>
2022-10-21 13:31:59 +0200

    Add token balances component

---
+++ src/pages/Profile/Body/Balances.tsx
@@ -0,0 +1,36 @@
+import { memo } from "react";
+import { BalanceInfo } from "types/contracts";
+import { axlUSDCDenom } from "constants/tokens";
+
+function Balances({ liquid, locked }: BalanceInfo) {
+  const lockedAmount =
+    locked.native.find((bal) => bal.denom === axlUSDCDenom)?.amount ?? 0;
+  const liquidAmount =
+    liquid.native.find((bal) => bal.denom === axlUSDCDenom)?.amount ?? 0;
+
+  return (
+    <div className="flex items-center justify-cener gap-4 w-full">
+      <Balance
+        title="Total Value"
+        amount={Number(lockedAmount) + Number(liquidAmount)}
+      />
+      <Balance title="Total Endowment Account" amount={lockedAmount} />
+      <Balance title="Total Liquid Account" amount={liquidAmount} />
+    </div>
+  );
+}
+
+function Balance(props: { title: string; amount: number | string }) {
+  return (
+    <div className="flex flex-col justify-center items-start px-6 gap-2 border border-gray-l2 rounded">
+      <h6 className="font-heading font-bold text-xs tracking-wider uppercase">
+        {props.title}
+      </h6>
+      <p className="font-work font-normal text-lg text-gray-d1">
+        ${props.amount}
+      </p>
+    </div>
+  );
+}
+
+export default memo(Balances);
