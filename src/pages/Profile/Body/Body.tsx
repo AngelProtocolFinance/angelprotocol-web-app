fec5ed70a6d29e223112f3870a9cda358f778800
Nenad Misic <n.misic@outlook.com>
2022-10-21 09:08:18 +0200

    Add Body scaffold

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -1,7 +1,18 @@
+import { useProfileContext } from "../ProfileContext";
+
 export default function Body() {
+  const profile = useProfileContext();
+
   return (
-    <div className="flex flex-col gap-10 items-end w-full h-full pt-6 px-20 pb-20 bg-white dark:bg-blue-d4">
-      Body
+    <div className="grid grid-rows-[auto_auto_1fr] grid-cols-[auto_auto] gap-10 items-end w-full h-full pt-6 px-20 pb-20 bg-white dark:bg-blue-d4">
+      <div className="col-span-2 flex items-center justify-end gap-6">
+        website + button
+      </div>
+      <div className="flex items-center justify-end gap-6">name + address</div>
+      <div className="flex items-center justify-end gap-6 w-[810px]">
+        balances
+      </div>
+      <div className="col-span-2 w-full h-40">Overview</div>
     </div>
   );
 }
