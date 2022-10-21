546e83354fbfa080c42f82905b44f608d4ad381b
Nenad Misic <n.misic@outlook.com>
2022-10-21 14:36:21 +0200

    Add general mobile layout to Body

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -9,8 +9,8 @@ export default function Body() {
   const queryState = useBalanceQuery({ id: profile.id });
 
   return (
-    <div className="grid grid-rows-[auto_auto_1fr] grid-cols-[auto_auto] gap-10 items-end w-full h-full pt-6 px-20 pb-20 bg-white dark:bg-blue-d4">
-      <div className="col-span-2 flex items-center justify-end gap-6">
+    <div className="flex flex-col gap-8 items-end w-full h-full pt-32 px-6 pb-8 bg-white dark:bg-blue-d4 lg:grid lg:grid-rows-[auto_auto_1fr] lg:grid-cols-[auto_auto] lg:gap-10 lg:pt-6 lg:px-20 lg:pb-20">
+      <div className="order-2 flex items-center justify-end gap-6 lg:order-1 lg:col-span-2">
         {/* {profile.url && ( */}
         <span className="flex items-center gap-2 text-black font-sans font-medium text-base dark:text-white">
           <Icon type="Globe" className="h-6 w-6" />
@@ -32,7 +32,7 @@ export default function Body() {
         </button>
       </div>
 
-      <div className="flex flex-col gap-6 items-start max-w-sm h-full text-black dark:text-white">
+      <div className="order-1 flex flex-col gap-6 items-start max-w-sm h-full text-black dark:text-white lg:order-2">
         <h3 className="font-header font-bold text-3xl">{profile.name}</h3>
         <span className="flex items-center justify-center gap-2 text-base font-work uppercase">
           <Icon type="MapPin" className="h-6 w-6" />
