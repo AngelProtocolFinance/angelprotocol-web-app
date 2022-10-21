348dfffec029ef2df8d1d8fc99f04578e83bb4c0
Nenad Misic <n.misic@outlook.com>
2022-10-21 08:52:56 +0200

    Add missing Body container styles

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -1,3 +1,7 @@
 export default function Body() {
-  return <div className="w-full h-80 bg-white dark:bg-blue-d4">Body</div>;
+  return (
+    <div className="flex flex-col gap-10 items-end w-full h-full pt-6 px-20 pb-20 bg-white dark:bg-blue-d4">
+      Body
+    </div>
+  );
 }
