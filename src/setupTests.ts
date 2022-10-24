7b5a46e1197da69089aae4b676e06063a469b392
ap-justin <justin@angelprotocol.io>
2022-10-24 13:15:30 +0800

    fix test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -31,16 +31,15 @@ Object.defineProperty(window, "IntersectionObserver", {
   value: IntersectionObserver,
 });
 
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(), // Deprecated
+  removeListener: jest.fn(), // Deprecated
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
 });
 
 // required after adding @cosmjs/cosmwasm-stargate
