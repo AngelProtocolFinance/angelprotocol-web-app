0e79d7c17c929651c5f2316a7a8085e1cca70456
Nenad Misic <n.misic@outlook.com>
2022-10-24 18:13:16 +0200

    Remove placeholder URL

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -11,19 +11,18 @@ export default function Body() {
   return (
     <div className="flex flex-col gap-8 items-center w-full h-full pt-32 px-6 pb-8 bg-gray-l5 dark:bg-blue-d4 lg:grid lg:grid-rows-[auto_auto_1fr] lg:grid-cols-[auto_auto] lg:items-end lg:gap-16 xl:pt-6 xl:px-20 xl:pb-20">
       <div className="order-2 flex flex-col items-center justify-end gap-8 w-full xl:order-1 xl:col-span-2 xl:flex-row xl:gap-6">
-        {/* {profile.url && ( */}
-        <span className="flex items-center justify-center gap-2 w-full text-black font-sans font-medium text-sm dark:text-white sm:w-auto sm:text-base">
-          <Icon type="Globe" className="h-5 w-5 sm:h-6 sm:w-6" />
-          <a
-            href={profile.url}
-            title="organization url"
-            className="cursor-pointer hover:underline"
-          >
-            {profile.url} https://www.example.com
-            {/* --> DELETE PRIOR TO MERGING AND UNCOMMENT THE `profile.url` CHECK */}
-          </a>
-        </span>
-        {/* )} */}
+        {profile.url && (
+          <span className="flex items-center justify-center gap-2 w-full text-black font-sans font-medium text-sm dark:text-white sm:w-auto sm:text-base">
+            <Icon type="Globe" className="h-5 w-5 sm:h-6 sm:w-6" />
+            <a
+              href={profile.url}
+              title="organization url"
+              className="cursor-pointer hover:underline"
+            >
+              {profile.url}
+            </a>
+          </span>
+        )}
         <button
           onClick={() => console.log("donate")}
           className="btn btn-orange w-full h-12 max-w-xs py-2 px-6 rounded text-sm normal-case"
