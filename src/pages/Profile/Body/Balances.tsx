3a1d4c117602e1a4735991cc3198b7a1bb60e5cb
Nenad Misic <n.misic@outlook.com>
2022-10-24 17:14:08 +0200

    Update Balances alignment on xs < X < lg screens

--- src/pages/Profile/Body/Balances.tsx
+++ src/pages/Profile/Body/Balances.tsx
@@ -12,7 +12,7 @@ function Balances({ liquid, locked, className = "" }: Props) {
 
   return (
     <div
-      className={`grid grid-rows-3 items-center justify-stretch gap-4 w-full xl:grid-rows-1 xl:grid-cols-3 xl:justify-center ${className}`}
+      className={`grid grid-rows-3 items-center justify-stretch gap-4 w-full max-w-xs md:max-w-none md:grid-rows-1 md:grid-cols-3 md:justify-center ${className}`}
     >
       <Balance
         title="Total Value"
@@ -26,7 +26,7 @@ function Balances({ liquid, locked, className = "" }: Props) {
 
 function Balance(props: { title: string; amount: number | string }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 h-20 py-4 rounded border border-gray-l2 dark:bg-blue-d6 dark:border-bluegray-d1 xl:items-start xl:h-28 xl:px-6 xl:py-0">
+    <div className="flex flex-col justify-center items-center gap-2 h-20 py-4 rounded border border-gray-l2 dark:bg-blue-d6 dark:border-bluegray-d1 md:items-start md:h-28 md:px-6 md:py-0">
       <h6 className="font-heading font-bold text-black text-xs tracking-wider uppercase dark:text-white">
         {props.title}
       </h6>
