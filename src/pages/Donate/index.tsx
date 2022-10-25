9d06b660b06a5f8e19671ff0ec40f66d6a4483c2
ap-justin <justin@angelprotocol.io>
2022-10-25 13:43:01 +0800

    banner

--- src/pages/Donate/index.tsx
+++ src/pages/Donate/index.tsx
@@ -1,3 +1,8 @@
 export default function Donate() {
-  return <></>;
+  return (
+    <section className="min-h-screen w-full bg-orange-l6 dark:bg-blue-d3">
+      <div className="w-full bg-market-banner-light dark:bg-market-banner-dark h-72 bg-center bg-cover pt-24" />
+      donate page
+    </section>
+  );
 }
