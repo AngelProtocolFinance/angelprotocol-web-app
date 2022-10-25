dbcbf5ba261b86ea1c7e8ad9881ec74f7271c10a
ap-justin <justin@angelprotocol.io>
2022-10-25 14:12:04 +0800

    steps folder

--- src/pages/Donate/index.tsx
+++ src/pages/Donate/index.tsx
@@ -2,7 +2,9 @@ export default function Donate() {
   return (
     <section className="min-h-screen w-full bg-orange-l6 dark:bg-blue-d3">
       <div className="w-full bg-market-banner-light dark:bg-market-banner-dark h-72 bg-center bg-cover pt-24" />
-      donate page
+      <div className="padded-container grid justify-items-center">
+        donate page
+      </div>
     </section>
   );
 }
