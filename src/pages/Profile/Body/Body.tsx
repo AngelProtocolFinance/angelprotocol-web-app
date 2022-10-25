6934a3ee8158e27cb6d97ad30a794451be80e4a4
Nenad Misic <n.misic@outlook.com>
2022-10-25 10:14:14 +0200

    Set default light text color to  text-gray-d2

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -4,7 +4,7 @@ import UrlDonateSection from "./UrlDonateSection";
 
 export default function Body() {
   return (
-    <div className="flex flex-col gap-8 items-center w-full h-full pt-32 px-6 pb-8 bg-gray-l5 text-black dark:bg-blue-d4 dark:text-white lg:grid lg:grid-rows-[auto_auto_1fr] lg:grid-cols-[auto_auto] lg:items-end lg:gap-16 xl:pt-6 xl:px-20 xl:pb-20">
+    <div className="flex flex-col gap-8 items-center w-full h-full pt-32 px-6 pb-8 bg-gray-l5 text-gray-d2 dark:bg-blue-d4 dark:text-white lg:grid lg:grid-rows-[auto_auto_1fr] lg:grid-cols-[auto_auto] lg:items-end lg:gap-16 xl:pt-6 xl:px-20 xl:pb-20">
       <UrlDonateSection className="order-2 xl:order-1 xl:col-span-2" />
 
       <NameAddressSection className="order-1 xl:order-2" />
