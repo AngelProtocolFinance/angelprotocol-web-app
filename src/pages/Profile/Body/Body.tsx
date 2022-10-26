d7458b9dcd1a3a2b7e2c241db2d5006c2cecb825
Nenad Misic <n.misic@outlook.com>
2022-10-26 09:27:57 +0200

    Remove lg < X xl view

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -4,12 +4,12 @@ import UrlDonateSection from "./UrlDonateSection";
 
 export default function Body() {
   return (
-    <div className="grid gap-8 justify-items-center w-full h-full pt-32 px-6 pb-8 bg-gray-l5 text-gray-d2 dark:bg-blue-d4 dark:text-white lg:grid-rows-[auto_auto_1fr] lg:grid-cols-[auto_auto] lg:items-end lg:gap-16 xl:pt-6 xl:px-20 xl:pb-20">
+    <div className="grid gap-8 justify-items-center w-full h-full pt-32 px-6 pb-8 bg-gray-l5 text-gray-d2 dark:bg-blue-d4 dark:text-white xl:grid-rows-[auto_auto_1fr] xl:grid-cols-[auto_auto] xl:items-end xl:gap-16 xl:pt-6 xl:px-20 xl:pb-20">
       <UrlDonateSection className="order-2 xl:order-1 xl:col-span-2" />
 
       <NameAddressSection className="order-1 xl:order-2" />
 
-      <Balances className="order-3 lg:col-span-2 xl:col-span-1 xl:justify-self-end" />
+      <Balances className="order-3 xl:col-span-2 xl:col-span-1 xl:justify-self-end" />
 
       <div className="order-4 xl:col-span-2 w-full h-40">Overview</div>
     </div>
--- src/pages/Profile/Body/UrlDonateSection.tsx
+++ src/pages/Profile/Body/UrlDonateSection.tsx
@@ -10,18 +10,18 @@ export default function UrlDonateSection({ className }: { className: string }) {
     <div
       className={`flex flex-col items-center justify-end gap-8 w-full xl:flex-row xl:gap-6 ${className}`}
     >
-      {profile.url && (
-        <span className="flex items-center justify-center gap-2 w-full font-sans font-medium text-sm sm:w-auto sm:text-base">
-          <Icon type="Globe" className="h-5 w-5 sm:h-6 sm:w-6" />
-          <a
-            href={profile.url}
-            title="organization url"
-            className="cursor-pointer hover:underline"
-          >
-            {profile.url.replace(/^https?:\/\//i, "")}
-          </a>
-        </span>
-      )}
+      {/* {profile.url && ( */}
+      <span className="flex items-center justify-center gap-2 w-full font-sans font-medium text-sm sm:w-auto sm:text-base">
+        <Icon type="Globe" className="h-5 w-5 sm:h-6 sm:w-6" />
+        <a
+          href={profile.url}
+          title="organization url"
+          className="cursor-pointer hover:underline"
+        >
+          {/* {profile.url.replace(/^https?:\/\//i, "")} */} www.example.com
+        </a>
+      </span>
+      {/* )} */}
       <button
         onClick={donate}
         className="btn btn-orange w-full h-12 max-w-xs py-2 px-6 rounded text-sm normal-case"
