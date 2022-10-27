536dcdd542b799388359cc6ac11f5a384dc75d59
Nenad Misic <n.misic@outlook.com>
2022-10-27 11:40:34 +0200

    Contain Body of the Profile page

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -4,14 +4,16 @@ import UrlDonateSection from "./UrlDonateSection";
 
 export default function Body() {
   return (
-    <div className="grid gap-8 justify-items-center w-full h-full pt-32 px-6 pb-8 bg-gray-l5 text-gray-d2 dark:bg-blue-d4 dark:text-white xl:grid-rows-[auto_auto_1fr] xl:grid-cols-[auto_auto] xl:items-end xl:gap-16 xl:pt-6 xl:px-20 xl:pb-20">
-      <UrlDonateSection className="order-2 xl:order-1 xl:col-span-2" />
+    <div className="flex justify-center items-center w-full h-full bg-gray-l5 text-gray-d2 dark:bg-blue-d4 dark:text-white">
+      <div className="container grid gap-8 justify-items-center w-full h-full pt-32 px-6 pb-8 xl:grid-rows-[auto_auto_1fr] xl:grid-cols-[auto_auto] xl:items-end xl:gap-16 xl:pt-6 xl:px-20 xl:pb-20">
+        <UrlDonateSection className="order-2 xl:order-1 xl:col-span-2" />
 
-      <NameAddressSection className="order-1 xl:order-2" />
+        <NameAddressSection className="order-1 xl:order-2 xl:pl-2" />
 
-      <Balances className="order-3 xl:col-span-2 xl:col-span-1 xl:justify-self-end" />
+        <Balances className="order-3 xl:col-span-2 xl:col-span-1 xl:justify-self-end" />
 
-      <div className="order-4 xl:col-span-2 w-full h-40">Overview</div>
+        <div className="order-4 xl:col-span-2 w-full h-40">Overview</div>
+      </div>
     </div>
   );
 }
