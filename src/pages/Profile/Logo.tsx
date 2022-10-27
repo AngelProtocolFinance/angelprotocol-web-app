4675603a99cf687311621c6be2655051e28a687e
Nenad Misic <n.misic@outlook.com>
2022-10-27 11:39:07 +0200

    Update Logo to contain the width ratio

--- src/pages/Profile/Logo.tsx
+++ src/pages/Profile/Logo.tsx
@@ -1,33 +1,34 @@
 import Icon from "components/Icon";
 import { useProfileContext } from "./ProfileContext";
 
-const anchorStyle =
-  "flex items-center justify-center w-full h-0 overflow-visible isolate xl:justify-start xl:pl-20";
-
 const logoStyle =
   "box-border h-44 w-44 border border-gray-l2 rounded-full object-contain dark:border-bluegray-d1";
 
 export default function Logo() {
+  return (
+    <div className="flex items-center justify-center w-full h-0 isolate">
+      <div className="container w-full overflow-visible xl:justify-start xl:pl-20">
+        <Image />
+      </div>
+    </div>
+  );
+}
+
+function Image() {
   const { logo } = useProfileContext();
 
   if (!logo) {
     return (
-      <div className={anchorStyle}>
-        <div
-          className={`${logoStyle} flex items-center justify-center bg-blue-l3 dark:bg-blue`}
-        >
-          <Icon
-            type="Picture"
-            className="w-9 h-9 bg-blue-l3 text-white dark:bg-blue dark:text-blue-l3"
-          />
-        </div>
+      <div
+        className={`${logoStyle} flex items-center justify-center bg-blue-l3 dark:bg-blue`}
+      >
+        <Icon
+          type="Picture"
+          className="w-9 h-9 bg-blue-l3 text-white dark:bg-blue dark:text-blue-l3"
+        />
       </div>
     );
   }
 
-  return (
-    <div className={anchorStyle}>
-      <img className={`${logoStyle} bg-white`} alt="logo" src={logo} />
-    </div>
-  );
+  return <img className={`${logoStyle} bg-white`} alt="logo" src={logo} />;
 }
