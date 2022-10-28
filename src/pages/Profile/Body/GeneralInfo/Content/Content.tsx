63b5d2c20c97b480427ed604af37e49834eed0f4
Nenad Misic <n.misic@outlook.com>
2022-10-28 17:57:46 +0200

    Remove max width limit from Content

--- src/pages/Profile/Body/GeneralInfo/Content/Content.tsx
+++ src/pages/Profile/Body/GeneralInfo/Content/Content.tsx
@@ -2,7 +2,7 @@ import Container from "./Container";
 
 export default function Content() {
   return (
-    <div className="flex flex-col gap-8 w-full h-full max-w-4xl">
+    <div className="flex flex-col gap-8 w-full h-full">
       <Container title="Overview">Overview</Container>
     </div>
   );
