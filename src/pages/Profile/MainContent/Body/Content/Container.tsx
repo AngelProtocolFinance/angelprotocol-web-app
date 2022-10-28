1538d422602272ba832a4c8b271a534f6125cad6
Nenad Misic <n.misic@outlook.com>
2022-10-28 09:03:03 +0200

    Add dark theme support

--- src/pages/Profile/MainContent/Body/Content/Container.tsx
+++ src/pages/Profile/MainContent/Body/Content/Container.tsx
@@ -9,7 +9,7 @@ export default function Container(props: Props) {
   const [isOpen, setOpen] = useState(true);
 
   return (
-    <div className="flex flex-col gap-px w-full border border-gray-l2 rounded">
+    <div className="flex flex-col gap-px w-full border border-gray-l2 rounded dark:bg-blue-d6 dark:border-bluegray">
       <Header
         isOpen={isOpen}
         title={props.title}
@@ -27,11 +27,11 @@ function Header(props: {
   onClick: () => void;
 }) {
   return (
-    <div className="flex items-center justify-between px-8 py-5 w-full bg-orange-l5 border-b border-gray-l2 rounded">
+    <div className="flex items-center justify-between px-8 py-5 w-full bg-orange-l5 border-b border-gray-l2 rounded dark:bg-blue-d7 dark:border-bluegray">
       <span className="font-heading font-bold text-xl">{props.title}</span>
       <button
         onClick={props.onClick}
-        className="flex items-center justify-center p-px w-10 h-10 border border-gray-l2 rounded"
+        className="flex items-center justify-center p-px w-10 h-10 border border-gray-l2 rounded dark:border-bluegray"
       >
         <Icon type={props.isOpen ? "Dash" : "Plus"} />
       </button>
