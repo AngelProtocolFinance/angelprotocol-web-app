b20bdebdf82ca0fb195a3c95cf497987ad705f6b
Nenad Misic <n.misic@outlook.com>
2022-10-28 10:24:09 +0200

    Add RichTextView to Overview of Profile Content

--- src/pages/Profile/Body/GeneralInfo/Content/Content.tsx
+++ src/pages/Profile/Body/GeneralInfo/Content/Content.tsx
@@ -1,9 +1,18 @@
+import { useProfileContext } from "pages/Profile/ProfileContext";
+import { RichTextView } from "components/RichText";
 import Container from "./Container";
 
 export default function Content() {
+  const profile = useProfileContext();
+
   return (
     <div className="flex flex-col gap-8 w-full h-full">
-      <Container title="Overview">Overview</Container>
+      <Container title="Overview">
+        <RichTextView
+          value={profile.overview}
+          classes={{ container: "w-full h-full px-8 py-10" }}
+        />
+      </Container>
     </div>
   );
 }
