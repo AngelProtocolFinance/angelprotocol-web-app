12d1924f9691cabc21fd7757f92639cc3beb70b4
Nenad Misic <n.misic@outlook.com>
2022-10-28 09:17:30 +0200

    Rename MainContent to Body

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -3,7 +3,7 @@ import GeneralInfo from "./GeneralInfo";
 import NameAddressSection from "./NameAddressSection";
 import UrlDonateSection from "./UrlDonateSection";
 
-export default function MainContent() {
+export default function Body() {
   return (
     <div className="flex justify-center items-center w-full h-full bg-gray-l5 text-gray-d2 dark:bg-blue-d4 dark:text-white">
       <div className="padded-container grid gap-8 justify-items-center w-full h-full pt-32 pb-8 lg:grid-rows-[auto_auto_1fr] lg:grid-cols-[auto_auto] lg:justify-items-start lg:items-end lg:gap-16 lg:pt-6 lg:pb-20">
