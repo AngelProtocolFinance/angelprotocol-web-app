b57be0d66012eb4ae22bca5e67bb47a67177184b
Nenad Misic <nenad@angelprotocol.io>
2022-11-02 08:11:51 +0100

    Fix useSubmit.test.ts  tests (#1408)
    Thanks Nenad! 💯

--- src/pages/Registration/Dashboard/useSubmit/__tests__/useSubmit.test.ts
+++ src/pages/Registration/Dashboard/useSubmit/__tests__/useSubmit.test.ts
@@ -8,6 +8,12 @@ jest.mock("contexts/ModalContext", () => ({
   useModalContext: () => ({ showModal: mockShowModal }),
 }));
 
+const mockSubmitMutation = jest.fn();
+jest.mock("services/aws/registration", () => ({
+  __esModule: true,
+  useSubmitMutation: () => mockSubmitMutation(),
+}));
+
 const mockUseGetWallet = jest.fn();
 jest.mock("contexts/WalletContext/WalletContext", () => ({
   __esModule: true,
@@ -16,7 +22,6 @@ jest.mock("contexts/WalletContext/WalletContext", () => ({
 
 const mockDispatch = jest.fn();
 const mockUseGetter = jest.fn();
-
 jest.mock("store/accessors", () => ({
   __esModule: true,
   useGetter: (..._: any[]) => mockUseGetter(),
@@ -25,6 +30,7 @@ jest.mock("store/accessors", () => ({
 
 describe("useSubmit tests", () => {
   it("initializes correctly", () => {
+    mockSubmitMutation.mockReturnValue([(_: any) => ({})]);
     mockUseGetter.mockReturnValue({ form_loading: false });
     mockUseGetWallet.mockReturnValue({ wallet: PLACEHOLDER_WALLET });
     const { result } = renderHook(() => useSubmit());
@@ -33,6 +39,7 @@ describe("useSubmit tests", () => {
   });
 
   it("assigns 'isSubmitting' value correctly", () => {
+    mockSubmitMutation.mockReturnValue([(_: any) => ({})]);
     mockUseGetter.mockReturnValue({ form_loading: true });
     mockUseGetWallet.mockReturnValue({ wallet: PLACEHOLDER_WALLET });
     const { result } = renderHook(() => useSubmit());
