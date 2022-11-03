499ad723b1de0f09caed9ac0d623a75a0b9b0c5a
ap-justin <justin@angelprotocol.io>
2022-11-03 21:49:42 +0800

    contact details ype

--- src/services/aws/registration/types.ts
+++ src/services/aws/registration/types.ts
@@ -1,1 +1,30 @@
 export type RegistrationState = {};
+
+//REF_ID is global to registration
+type InitialStep = {
+  email: string;
+};
+
+//STEP 1
+type ContactDetails = {
+  name: {
+    first: string;
+    last: string;
+  };
+  //https://www.npmjs.com/package/react-phone-input-2
+  phone: {
+    format: string;
+    value: string;
+  };
+  // disabled, can't be changed once confirmed
+  email: string;
+
+  organization: {
+    name: string;
+    role: string;
+    otherRole: string;
+    goal: string;
+  };
+
+  referrer: string;
+};
