6b227f804db74b36574f15f1753537ba041ba849
Nenad Misic <nenad@angelprotocol.io>
2022-11-07 12:30:24 +0100

    Show img value in edit profile proposal preview (#1418)
    * Set max width for the DiffTable
    
    * Add loading indicator to ImageWrapper
    
    * Add ability to show images in DiffTable
    
    * Add min height to DiffTable image column on sm
    
    * Update image size from sm to lg
    
    * Handle no value first in createColumn
    
    * Add explanatory comment to value IPFS gateway check

--- src/pages/Admin/Proposal/Content/Preview/DiffTable.tsx
+++ src/pages/Admin/Proposal/Content/Preview/DiffTable.tsx
@@ -1,5 +1,7 @@
 import { DiffSet } from "types/utils";
+import ImageWrapper from "components/ImageWrapper";
 import TableSection, { Cells } from "components/TableSection";
+import { IPFS_GATEWAY } from "helpers";
 import PreviewContainer from "./common/PreviewContainer";
 
 export default function DiffTable<T extends object>(props: {
@@ -23,18 +25,38 @@ export default function DiffTable<T extends object>(props: {
           {props.diffSet.map(([key, prev, next]) => (
             <Cells
               type="td"
-              cellClass="text-right p-2 border-r border-white/20"
+              cellClass="text-right p-2 border-r border-white/20 truncate max-w-2xl"
               dual
               key={key as string} //T is a normal object with string keys
               verticalHeaderClass="uppercase text-xs text-left p-2 pl-0 font-heading border-r border-white/20"
             >
               <>{(key as string).replace(/_/g, " ")}</>
-              <>{prev || "not set"}</>
-              <>{next || "not set"}</>
+              {createColumn(prev)}
+              {createColumn(next)}
             </Cells>
           ))}
         </TableSection>
       </table>
     </PreviewContainer>
   );
+}
+
+function createColumn<T extends object>(value: T[keyof T]): JSX.Element {
+  if (!value) {
+    return <>not set</>;
+  }
+
+  // if the string value starts with the IPFS gateway URL value, this is surely a file
+  // the user has uploaded and a preview should be displayed
+  if (typeof value === "string" && value.startsWith(IPFS_GATEWAY)) {
+    return (
+      <ImageWrapper
+        src={value}
+        alt=""
+        className="w-40 lg:w-[40rem] lg:min-h-[5rem] max-w-2xl object-contain"
+      />
+    );
+  }
+
+  return <>{value}</>;
 }
