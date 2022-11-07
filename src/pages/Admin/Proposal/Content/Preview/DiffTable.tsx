737e712d1791f5d6aacca73cc6cc583239c91e94
Nenad Misic <n.misic@outlook.com>
2022-11-07 10:56:21 +0100

    Set max width for the DiffTable

--- src/pages/Admin/Proposal/Content/Preview/DiffTable.tsx
+++ src/pages/Admin/Proposal/Content/Preview/DiffTable.tsx
@@ -7,7 +7,7 @@ export default function DiffTable<T extends object>(props: {
 }) {
   return (
     <PreviewContainer>
-      <table>
+      <table className="w-full">
         <TableSection type="thead" rowClass="">
           <Cells
             type="th"
@@ -23,7 +23,7 @@ export default function DiffTable<T extends object>(props: {
           {props.diffSet.map(([key, prev, next]) => (
             <Cells
               type="td"
-              cellClass="text-right p-2 border-r border-white/20"
+              cellClass="text-right p-2 border-r border-white/20 truncate max-w-2xl"
               dual
               key={key as string} //T is a normal object with string keys
               verticalHeaderClass="uppercase text-xs text-left p-2 pl-0 font-heading border-r border-white/20"
