c7a26f5d209200a4217e7bc23bdaeecc16c84172
Nenad Misic <n.misic@outlook.com>
2022-11-10 09:32:56 +0100

    Update GeneralInfo.tsx > Body class name to GeneralInfo

--- src/pages/Profile/Body/GeneralInfo/GeneralInfo.tsx
+++ src/pages/Profile/Body/GeneralInfo/GeneralInfo.tsx
@@ -3,7 +3,7 @@ import InfoColumn from "./InfoColumn";
 
 type Props = { className: string };
 
-export default function Body({ className }: Props) {
+export default function GeneralInfo({ className }: Props) {
   return (
     <div
       className={`${className} grid grid-rows-[auto_auto] gap-8 w-full h-full xl:grid-rows-1 xl:grid-cols-[1fr_auto]`}
