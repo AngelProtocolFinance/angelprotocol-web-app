654f26585f554c7ebedf3d9c15e44684c0b3dd7d
Nenad Misic <n.misic@outlook.com>
2022-11-10 18:24:05 +0100

    Use deep-equal pkg to check equality in getPayloadDiff

--- src/helpers/admin/getPayloadDiff.ts
+++ src/helpers/admin/getPayloadDiff.ts
@@ -1,3 +1,5 @@
+import deepEqual from "deep-equal";
+
 //NOTE: intended for shallow form objects only atm
 export function getPayloadDiff<T extends object>(prev: T, next: T): Partial<T> {
   const diff: any = {};
@@ -5,7 +7,7 @@ export function getPayloadDiff<T extends object>(prev: T, next: T): Partial<T> {
   for (const key in prev) {
     const n = next[key];
     const p = prev[key];
-    if (p !== n) {
+    if (!deepEqual(p, n, { strict: true })) {
       diff[key] = n;
     }
   }
