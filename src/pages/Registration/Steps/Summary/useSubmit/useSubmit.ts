b27cfdc811997fae4bec2172a9089dddeed2bedf
ap-justin <justin@angelprotocol.io>
2022-11-10 21:58:30 +0800

    remove tx elements in aws reg submit

--- src/pages/Registration/Steps/Summary/useSubmit/useSubmit.ts
+++ src/pages/Registration/Steps/Summary/useSubmit/useSubmit.ts
@@ -1,27 +1,19 @@
 import { CompleteRegistration } from "services/aws/registration/types";
 import { SubmitData } from "types/aws";
-import { invalidateAwsTags } from "services/aws/aws";
 import { useSubmitMutation } from "services/aws/registration";
-import { adminTags, awsTags } from "services/aws/tags";
 import { useModalContext } from "contexts/ModalContext";
-import TransactionPrompt from "components/Transactor/TransactionPrompt";
-import { useGetter, useSetter } from "store/accessors";
-import { setStage } from "slices/transaction/transactionSlice";
+import Popup from "components/Popup";
+import { useGetter } from "store/accessors";
 import { logger } from "helpers";
 import { chainIds } from "constants/chainIds";
 
 export default function useSubmit() {
   const [submitApplication] = useSubmitMutation();
   const { form_loading } = useGetter((state) => state.transaction);
-  const dispatch = useSetter();
   const { showModal } = useModalContext();
 
   const submit = async ({ init }: CompleteRegistration) => {
     try {
-      dispatch(
-        setStage({ step: "submit", message: "Saving endowment proposal" })
-      );
-
       const postData: SubmitData = {
         PK: init.reference,
         chain_id: chainIds.juno, //applications are processed on Juno network
@@ -30,32 +22,17 @@ export default function useSubmit() {
       const response = await submitApplication(postData);
 
       if ("error" in response) {
-        throw new Error("Request failed");
+        showModal(Popup, {
+          message:
+            "Registration submission failed. Contact support@angelprotocol.io",
+        });
       }
-
-      dispatch(
-        setStage({
-          step: "success",
-          message: "Endowment submitted for review",
-          txHash: "",
-          chainId: "",
-        })
-      );
-
-      dispatch(
-        invalidateAwsTags([{ type: awsTags.admin, id: adminTags.registration }])
-      );
     } catch (err) {
       logger.error(err);
-      dispatch(
-        setStage({
-          step: "error",
-          message:
-            "Failed to create endowment. Contact support@angelprotocol.io",
-        })
-      );
-    } finally {
-      showModal(TransactionPrompt, {});
+      showModal(Popup, {
+        message:
+          "Registration submission failed. Contact support@angelprotocol.io",
+      });
     }
   };
 
