0a984bfbd7dd43bc260f5d390bd10318db85892b
Nenad Misic <n.misic@outlook.com>
2022-11-10 21:01:27 +0100

    Add container styles

--- src/pages/Profile/Body/GeneralInfo/InfoColumn.tsx
+++ src/pages/Profile/Body/GeneralInfo/InfoColumn.tsx
@@ -1,6 +1,6 @@
 export default function InfoColumn() {
   return (
-    <div className="w-full lg:w-96 h-[928px] box-border border border-gray-l2 rounded">
+    <div className="flex flex-col gap-10 w-full lg:w-96 h-[928px] p-8 box-border border border-gray-l2 rounded">
       InfoColumn
     </div>
   );
