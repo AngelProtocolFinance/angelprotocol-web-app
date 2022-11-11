843f9888c0f75f374944b3b3cbc69a39a33b6c7d
Nenad Misic <n.misic@outlook.com>
2022-11-11 16:53:37 +0100

    Increase sticky column top to 28

--- src/pages/Profile/Body/GeneralInfo/GeneralInfo.tsx
+++ src/pages/Profile/Body/GeneralInfo/GeneralInfo.tsx
@@ -9,7 +9,7 @@ export default function GeneralInfo({ className }: Props) {
       className={`${className} grid grid-rows-[auto_auto] gap-8 w-full h-full lg:grid-rows-1 lg:grid-cols-[1fr_auto]`}
     >
       <Content />
-      <DetailsColumn className="self-start lg:sticky lg:top-24" />
+      <DetailsColumn className="self-start lg:sticky lg:top-28" />
     </div>
   );
 }
