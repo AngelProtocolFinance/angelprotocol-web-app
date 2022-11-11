161d01551d7b4e3b973f722bf8bebf15f41f1a37
ap-justin <justin@angelprotocol.io>
2022-11-11 10:48:21 +0800

    clearer testnames

--- src/helpers/admin/getPayloadDiff.test.ts
+++ src/helpers/admin/getPayloadDiff.test.ts
@@ -3,38 +3,43 @@ import { getPayloadDiff } from "./getPayloadDiff";
 describe("getPayloadDiff", () => {
   test("include changes from prev to next", () => {
     expect(
-      getPayloadDiff({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3 })
-    ).toMatchObject({});
+      getPayloadDiff(
+        //prettier-ignore
+        { a: 1, b: 2, c: 3 },
+        { a: 1, b: 2, c: 3 }
+      )
+    ).toStrictEqual({});
 
-    expect(
-      getPayloadDiff({ a: 1, b: 2, c: 3 }, { a: 4, b: 5, c: 6 })
-    ).toMatchObject({ a: 4, b: 5, c: 6 });
-  });
-
-  test("prevs (excluding 0) are falsies, next (including 0) are truthies", () => {
-    expect(
-      getPayloadDiff({ a: undefined, b: null, c: "" }, { a: 0, b: "b", c: 3 })
-    ).toMatchObject({ a: 0, b: "b", c: 3 });
-  });
-  test("prevs (excluding 0) are falsies, next (excluding 0) are falsies", () => {
     expect(
       getPayloadDiff(
-        { a: undefined, b: null, c: "", d: 0 },
-        { a: null, b: "", c: 0, d: undefined }
+        //prettier-ignore
+        { a: 1, b: 2, c: 3 },
+        { a: 4, b: 5, c: 6 }
       )
-    ).toMatchObject({ c: 0 });
+    ).toStrictEqual({ a: 4, b: 5, c: 6 });
   });
 
-  test("include attributes not in prev but in next (truthies including 0)", () => {
+  test("if prev !== next, include next if it's truthy (including 0)", () => {
     expect(
       getPayloadDiff(
-        { a: "a", b: "b", c: "c" },
-        { a: null, b: undefined, c: "", d: 0, e: undefined, f: 0 }
+        //prettier-ignore
+        { a: 1, b: "a", c: 3, d:undefined, e:"" },
+        { a: 0, b: "b", c: "", d: null, e: undefined }
       )
-    ).toMatchObject({ d: 0, f: 0 });
+    ).toStrictEqual({ a: 0, b: "b" });
+  });
+
+  test("include attributes not in prev but in next, given that next is truthy (including 0)", () => {
+    expect(
+      getPayloadDiff(
+        //prettier-ignore
+        {},
+        { a: null, b: "b", c: "", d: 0, e: undefined, f: 0 }
+      )
+    ).toStrictEqual({ b: "b", d: 0, f: 0 });
   });
 
   test("both zero in prev and next", () => {
-    expect(getPayloadDiff({ a: 0 }, { a: 0 })).toMatchObject({});
+    expect(getPayloadDiff({ a: 0 }, { a: 0 })).toStrictEqual({});
   });
 });
--- src/helpers/admin/getPayloadDiff.ts
+++ src/helpers/admin/getPayloadDiff.ts
@@ -1,16 +1,20 @@
 //NOTE: intended for shallow form objects only atm
 export function getPayloadDiff<T extends object>(prev: T, next: T): Partial<T> {
   const diff: any = {};
-  // include attr in next different from prev
+  /** include attr in next different from prev,
+   *  given that next is truthy (including 0) */
   for (const key in prev) {
     const n = next[key];
     const p = prev[key];
-    if (p !== n) {
+    if (p !== n && (n || isZero(n))) {
       diff[key] = n;
     }
   }
 
-  // include attr not in prev but in next
+  /**
+   * if prev is falsy (excluding 0),
+   * include next value if it's truthy (including )
+   */
   for (const key in next) {
     const n = next[key];
     const p = prev[key];
