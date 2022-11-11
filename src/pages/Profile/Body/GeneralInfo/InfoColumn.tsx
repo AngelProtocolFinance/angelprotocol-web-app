d0e7fa8119f7fee853e94c35726d664e9344bf6e
Nenad Misic <n.misic@outlook.com>
2022-11-11 13:48:36 +0100

    Add subcontainer comps to InfoColumn

--- src/pages/Profile/Body/GeneralInfo/InfoColumn.tsx
+++ src/pages/Profile/Body/GeneralInfo/InfoColumn.tsx
@@ -1,7 +1,8 @@
 export default function InfoColumn() {
   return (
     <div className="flex flex-col gap-10 w-full lg:w-96 h-[928px] p-8 box-border border border-gray-l2 rounded">
-      InfoColumn
+      <div className="flex flex-col justify-center items-start gap-4 w-full"></div>
+      <div className="flex flex-col items-start gap-6 w-full"></div>
     </div>
   );
 }
