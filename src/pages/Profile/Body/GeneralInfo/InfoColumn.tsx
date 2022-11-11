ab838da7e9817b6c87494b0464ed0beb0844b299
Nenad Misic <n.misic@outlook.com>
2022-11-11 15:11:33 +0100

    Add Socials

--- src/pages/Profile/Body/GeneralInfo/InfoColumn.tsx
+++ src/pages/Profile/Body/GeneralInfo/InfoColumn.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 import { useProfileContext } from "pages/Profile/ProfileContext";
-import Icon from "components/Icon";
+import Icon, { IconTypes } from "components/Icon";
 import { unsdgs } from "constants/unsdgs";
 import DonateButton from "../DonateButton";
 
@@ -30,6 +30,8 @@ export default function InfoColumn() {
         tortor vitae, ornare ac, ultricies lacus. In sed arcu enim eu. Risus nam
         egestas sit id eget.
       </p>
+
+      <Socials />
     </div>
   );
 }
@@ -38,4 +40,41 @@ const Tag = (props: PropsWithChildren<{}>) => (
   <div className="flex items-center gap-2 px-4 py-2 bg-blue-l4 rounded-[20px] font-body font-semibold text-sm">
     {props.children}
   </div>
+);
+
+function Socials() {
+  const profile = useProfileContext();
+
+  return (
+    <div className="flex items-center gap-3">
+      {profile.social_media_urls.facebook && (
+        <SocialsIcon
+          href={profile.social_media_urls.facebook}
+          iconType="Facebook"
+        />
+      )}
+      {profile.social_media_urls.linkedin && (
+        <SocialsIcon
+          href={profile.social_media_urls.linkedin}
+          iconType="LinkedinIn"
+        />
+      )}
+      {profile.social_media_urls.twitter && (
+        <SocialsIcon
+          href={profile.social_media_urls.twitter}
+          iconType="Telegram"
+        />
+      )}
+    </div>
+  );
+}
+
+const SocialsIcon = (props: { href: string; iconType: IconTypes }) => (
+  <a
+    href={props.href}
+    rel="noopener noreferrer"
+    className="flex items-center justify-center p-1 w-10 h-10 border border-gray-d2 rounded"
+  >
+    <Icon type={props.iconType} />
+  </a>
 );
