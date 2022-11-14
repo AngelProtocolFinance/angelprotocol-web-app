38003bcec54b1b2ecec7ade66deac144b66bdfe1
Nenad Misic <n.misic@outlook.com>
2022-11-14 14:16:00 +0100

    Make active link darker orange

--- src/App/Header/MobileNav/constants.ts
+++ src/App/Header/MobileNav/constants.ts
@@ -3,9 +3,9 @@ import { createNavLinkStyler } from "helpers";
 export const adminMobileNavId = "admin_mobile_nav";
 
 export const commonNavItemStyle =
-  "text-white font-heading font-semibold w-full text-2xl";
+  "text-white font-heading font-bold w-full text-2xl";
 
 export const navLinkStyle = createNavLinkStyler(
   `${commonNavItemStyle} hover:text-orange-l6`,
-  "text-orange-l2 pointer-events-none"
+  "text-orange pointer-events-none"
 );
