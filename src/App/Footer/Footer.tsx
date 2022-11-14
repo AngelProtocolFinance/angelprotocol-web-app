e2fe25b12a89d18381a2b556c765f2f8b5133adc
Nenad Misic <n.misic@outlook.com>
2022-11-14 17:49:07 +0100

    Remove redundant div from Footer

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -5,15 +5,15 @@ import Socials from "./Socials";
 
 function Footer() {
   return (
-    <footer className="padded-container flex flex-col items-center px-2.5 text-white bg-blue dark:bg-blue-d3">
-      <section className="flex flex-col items-center justify-center w-full pt-8 pb-16 border-b border-gray-l2 dark:border-gray-l3 md:flex-row">
-        <div className="flex flex-col gap-10 items-center justify-between w-full lg:flex-row lg:items-start">
-          <Links />
-          <Newsletter />
-        </div>
+    <footer className="flex flex-col items-center text-white bg-blue dark:bg-blue-d3">
+      <section className="padded-container flex flex-col gap-8 items-center justify-center w-full pt-8 pb-10 lg:flex-row lg:items-start lg:justify-between lg:gap-0 lg:pb-16">
+        <Links />
+        <Newsletter />
       </section>
 
-      <section className="flex flex-col items-center gap-2 w-full py-4">
+      <div className="w-full h-px bg-gray-l2 dark:bg-gray-l3" />
+
+      <section className="padded-container flex flex-col items-center gap-2 w-full py-4">
         <Socials />
         <p className="font-body font-normal text-white text-center text-2xs uppercase">
           Copyright {new Date().getFullYear()} Angel Protocol. All rights
