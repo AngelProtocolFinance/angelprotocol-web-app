5e51609ebb5e7300584d1e9db58c5502e172f5ab
Nenad Misic <n.misic@outlook.com>
2022-11-14 14:16:51 +0100

    Increase link text size

--- src/App/Header/MobileNav/constants.ts
+++ src/App/Header/MobileNav/constants.ts
@@ -3,9 +3,9 @@ import { createNavLinkStyler } from "helpers";
 export const adminMobileNavId = "admin_mobile_nav";
 
 export const commonNavItemStyle =
-  "text-white font-heading font-bold w-full text-2xl";
+  "text-white font-heading font-bold w-full text-3xl";
 
 export const navLinkStyle = createNavLinkStyler(
-  `${commonNavItemStyle} hover:text-orange-l6`,
+  `${commonNavItemStyle} hover:text-orange-l5`,
   "text-orange pointer-events-none"
 );
