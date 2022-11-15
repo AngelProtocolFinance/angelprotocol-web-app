576d19d4e568335e21054b45189cd09587222dc0
Nenad Misic <nenad@angelprotocol.io>
2022-11-15 12:24:21 +0100

    Remove placeholder text from DetailsColumn (#1452)
    * Remove placeholder text from DetailsColumn
    
    * Remove -mt-2 from Socials

--- src/pages/Profile/Body/GeneralInfo/DetailsColumn/DetailsColumn.tsx
+++ src/pages/Profile/Body/GeneralInfo/DetailsColumn/DetailsColumn.tsx
@@ -12,13 +12,7 @@ export default function DetailsColumn({ className }: { className: string }) {
 
       <Tags />
 
-      <p className="font-work font-normal text-base">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra
-        tortor vitae, ornare ac, ultricies lacus. In sed arcu enim eu. Risus nam
-        egestas sit id eget.
-      </p>
-
-      <Socials className="-mt-2 mb-4" />
+      <Socials className="mb-4" />
 
       <Details />
     </div>
