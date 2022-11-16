206ba11d5ec88ddd5dac70c12cbb80b8ecb669c9
Nenad Misic <n.misic@outlook.com>
2022-11-16 11:38:04 +0100

    Set text-gray-d2 for all WalletModal comps

--- src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
+++ src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
@@ -6,10 +6,10 @@ export default function WalletModal() {
   const { closeModal } = useModalContext();
 
   return (
-    <Dialog.Panel className="fixed-center z-20 flex flex-col items-center bg-gray-l5 border border-gray-l2 rounded">
+    <Dialog.Panel className="fixed-center z-20 flex flex-col items-center bg-gray-l5 border border-gray-l2 rounded text-gray-d2">
       <Dialog.Title
         as="h3"
-        className="grid grid-rows-1 grid-cols-3 items-center justify-items-end w-full px-4 py-6 bg-orange-l6 border-b border-gray-l2 text-gray-d2"
+        className="grid grid-rows-1 grid-cols-3 items-center justify-items-end w-full px-4 py-6 bg-orange-l6 border-b border-gray-l2"
       >
         <span className="col-start-2 w-full font-heading font-bold text-xl justify-self-center">
           Connect Wallet
