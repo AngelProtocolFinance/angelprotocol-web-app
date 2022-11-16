44118d68a47702812b18fa2967efd2626f3aba83
Nenad Misic <n.misic@outlook.com>
2022-11-16 10:35:13 +0100

    Add modal container styles

--- src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
+++ src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
@@ -1,3 +1,7 @@
 export default function WalletModal() {
-  return <div>WalletModal</div>;
+  return (
+    <div className="flex flex-col items-center bg-gray-l5 border border-gray-l2 rounded shadow-[0_0_60px_rgba(0,0,0,0.3)]">
+      WalletModal
+    </div>
+  );
 }
