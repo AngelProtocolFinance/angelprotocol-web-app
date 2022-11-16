fcea9e6edde5ac8bf49b382840cc83321c3a9b39
Nenad Misic <n.misic@outlook.com>
2022-11-16 11:44:52 +0100

    Set max size

--- src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
+++ src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
@@ -1,12 +1,14 @@
 import { Dialog } from "@headlessui/react";
 import { useModalContext } from "contexts/ModalContext";
+import { useSetWallet } from "contexts/WalletContext";
 import Icon from "components/Icon";
 
 export default function WalletModal() {
   const { closeModal } = useModalContext();
+  const { connections } = useSetWallet();
 
   return (
-    <Dialog.Panel className="fixed-center z-20 flex flex-col items-center bg-gray-l5 border border-gray-l2 rounded text-gray-d2">
+    <Dialog.Panel className="fixed-center z-20 flex flex-col items-center max-w-lg max-h-[572px] border border-gray-l2 rounded bg-gray-l5 text-gray-d2">
       <Dialog.Title
         as="h3"
         className="grid grid-rows-1 grid-cols-3 items-center justify-items-end w-full px-4 py-6 bg-orange-l6 border-b border-gray-l2"
@@ -21,7 +23,7 @@ export default function WalletModal() {
           <Icon type="Close" size={24} />
         </button>
       </Dialog.Title>
-      <div className="flex flex-col justify-center items-center p-8 gap-6">
+      <div className="flex flex-col justify-center items-center gap-6 p-8 w-full">
         <h4 className="font-heading font-bold font-lg">
           Select one of the available wallets to continue
         </h4>
