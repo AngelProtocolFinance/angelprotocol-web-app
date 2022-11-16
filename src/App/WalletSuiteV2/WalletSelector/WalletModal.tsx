594658e55bef9243a73348847beae394433b2f43
Nenad Misic <n.misic@outlook.com>
2022-11-16 11:24:18 +0100

    Add modal header

--- src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
+++ src/App/WalletSuiteV2/WalletSelector/WalletModal.tsx
@@ -1,7 +1,31 @@
+import { Dialog } from "@headlessui/react";
+import { useModalContext } from "contexts/ModalContext";
+import Icon from "components/Icon";
+
 export default function WalletModal() {
+  const { closeModal } = useModalContext();
+
   return (
-    <div className="flex flex-col items-center bg-gray-l5 border border-gray-l2 rounded shadow-[0_0_60px_rgba(0,0,0,0.3)]">
-      WalletModal
-    </div>
+    <Dialog.Panel className="fixed-center z-20 flex flex-col items-center bg-gray-l5 border border-gray-l2 rounded">
+      <Dialog.Title
+        as="h3"
+        className="grid grid-rows-1 grid-cols-3 items-center justify-items-end w-full px-4 py-6 bg-orange-l6 border-b border-gray-l2 text-gray-d2"
+      >
+        <span className="col-start-2 w-full font-heading font-bold text-xl justify-self-center">
+          Connect Wallet
+        </span>
+        <button
+          className="flex items-center justify-center w-10 h-10 mr-4 border border-gray-l2 rounded"
+          onClick={closeModal}
+        >
+          <Icon type="Close" size={24} />
+        </button>
+      </Dialog.Title>
+      <div className="flex flex-col justify-center items-center p-8 gap-6">
+        <h4 className="font-heading font-bold font-lg">
+          Select one of the available wallets to continue
+        </h4>
+      </div>
+    </Dialog.Panel>
   );
 }
