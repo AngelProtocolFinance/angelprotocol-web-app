c53daa0c68d7c3029dfcdeceb0c28bdc97b515cc
Nenad Misic <n.misic@outlook.com>
2022-11-17 14:09:38 +0100

    Add Details container

--- src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
+++ src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
@@ -1,3 +1,8 @@
-export default function Details() {
-  return <div>Details</div>;
+import { Popover } from "@headlessui/react";
+import { WalletState } from "contexts/WalletContext";
+
+export default function Details(props: WalletState) {
+  return (
+    <Popover.Panel className="absolute top-2 right-0 grid bg-white rounded-lg border border-gray-l2 shadow-[0_0_16px_rgba(15,46,67,0.25)]"></Popover.Panel>
+  );
 }
