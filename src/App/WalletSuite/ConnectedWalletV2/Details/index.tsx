1a2fb1b8863e6f00c5f82a0635e2c510ded4d763
Nenad Misic <n.misic@outlook.com>
2022-11-17 14:39:47 +0100

    Add width

--- src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
+++ src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
@@ -8,7 +8,7 @@ import { AdminLink } from "components/admin";
 
 export default function Details(props: WalletState) {
   return (
-    <Popover.Panel className="absolute top-2 right-0 grid bg-white rounded-lg border border-gray-l2 shadow-[0_0_16px_rgba(15,46,67,0.25)] text-gray-d2">
+    <Popover.Panel className="absolute top-2 right-0 grid w-80 bg-white rounded-lg border border-gray-l2 shadow-[0_0_16px_rgba(15,46,67,0.25)] text-gray-d2">
       <div className="grid p-4 gap-3 border-b border-gray-l2">
         <h3 className="font-heading font-bold text-sm text-gray-d1">
           My Endowment
