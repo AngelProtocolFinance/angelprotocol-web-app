795bfe34bc79fff63896a1988efa7955d6fa5166
Nenad Misic <n.misic@outlook.com>
2022-11-17 09:24:50 +0100

    Add missing box shadow

--- src/App/WalletSuite/WalletSelector/WalletModal.tsx
+++ src/App/WalletSuite/WalletSelector/WalletModal.tsx
@@ -9,7 +9,7 @@ export default function WalletModal() {
   const { connections } = useSetWallet();
 
   return (
-    <Dialog.Panel className="fixed inset-0 sm:fixed-center z-20 flex flex-col sm:items-center w-full sm:max-w-lg h-full sm:max-h-[572px] sm:border border-gray-l2 sm:rounded bg-gray-l5 text-gray-d2 dark:bg-blue-d6 dark:border-bluegray dark:text-white">
+    <Dialog.Panel className="fixed inset-0 sm:fixed-center z-20 flex flex-col sm:items-center w-full sm:max-w-lg h-full sm:max-h-[572px] sm:border border-gray-l2 sm:rounded bg-gray-l5 text-gray-d2 dark:bg-blue-d6 dark:border-bluegray dark:text-white shadow-[0_0_60px_rgba(0,0,0,0.3)]">
       <Dialog.Title
         as="h3"
         className="relative w-full pl-4 px-4 sm:px-0 py-4 sm:py-6 bg-orange-l6 border-b border-gray-l2 font-heading font-black sm:font-bold sm:text-center text-xl text-orange sm:text-inherit dark:bg-blue-d7 dark:border-bluegray"
