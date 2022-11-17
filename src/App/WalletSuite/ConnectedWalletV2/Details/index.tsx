0b6038e4441156fe9e1733130d7a3ffee465ce29
Nenad Misic <n.misic@outlook.com>
2022-11-17 15:15:05 +0100

    Add wallet coins

--- src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
+++ src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
@@ -1,11 +1,13 @@
 import { Popover } from "@headlessui/react";
 import { WalletState } from "contexts/WalletContext";
 import MyEndowment from "./MyEndowment";
+import WalletDetails from "./WalletDetails";
 
 export default function Details(props: WalletState) {
   return (
     <Popover.Panel className="absolute top-2 right-0 grid w-80 bg-white rounded-lg border border-gray-l2 shadow-[0_0_16px_rgba(15,46,67,0.25)] text-gray-d2">
       <MyEndowment />
+      <WalletDetails {...props} />
     </Popover.Panel>
   );
 }
