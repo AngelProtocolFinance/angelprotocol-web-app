0b4d0f5fca27c2d0625590e170756d6a86e8d2c2
Nenad Misic <n.misic@outlook.com>
2022-11-17 13:46:42 +0100

    Add ConnectedWalletV2 folder

---
+++ src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
@@ -0,0 +1,3 @@
+export default function Details() {
+  return <div>Details</div>;
+}
---
+++ src/App/WalletSuite/ConnectedWalletV2/index.tsx
@@ -0,0 +1,29 @@
+import { Popover } from "@headlessui/react";
+import { WalletState, useGetWallet } from "contexts/WalletContext";
+import { DrawerIcon } from "components/Icon";
+import { maskAddress } from "helpers";
+import { COMMON_BUTTON_STYLE } from "../constants";
+import Details from "./Details";
+
+//this component won't be rendered if wallet is not connected
+export default function ConnectedWallet(props: WalletState) {
+  const { isLoading } = useGetWallet();
+  const maskedAddr = maskAddress(props.address);
+
+  return (
+    <Popover as="div" className="relative">
+      <Popover.Button
+        disabled={isLoading}
+        className={`${COMMON_BUTTON_STYLE} text-xs normal-case`}
+      >
+        {({ open }) => (
+          <>
+            <span>{maskedAddr}</span>
+            <DrawerIcon isOpen={open} className="text-2xl opacity-50" />
+          </>
+        )}
+      </Popover.Button>
+      <Details {...props} />
+    </Popover>
+  );
+}
