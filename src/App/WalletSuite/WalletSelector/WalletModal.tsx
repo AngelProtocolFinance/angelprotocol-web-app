adfecc6d7faf5893eea623d33c83e41ba0bbbf9b
Nenad Misic <n.misic@outlook.com>
2022-11-17 11:03:17 +0100

    Fix overflow cutting the subheader

--- src/App/WalletSuite/WalletSelector/WalletModal.tsx
+++ src/App/WalletSuite/WalletSelector/WalletModal.tsx
@@ -9,7 +9,7 @@ export default function WalletModal() {
   const { connections } = useSetWallet();
 
   return (
-    <Dialog.Panel className="fixed inset-0 sm:fixed-center z-20 flex flex-col sm:items-center w-full sm:max-w-lg h-full sm:max-h-[572px] sm:border border-gray-l2 sm:rounded bg-gray-l5 text-gray-d2 dark:bg-blue-d6 dark:border-bluegray dark:text-white shadow-[0_0_60px_rgba(0,0,0,0.3)]">
+    <Dialog.Panel className="fixed inset-0 sm:fixed-center z-20 grid sm:items-center w-full sm:max-w-lg h-full sm:h-fit sm:border border-gray-l2 sm:rounded bg-gray-l5 text-gray-d2 dark:bg-blue-d6 dark:border-bluegray dark:text-white shadow-[0_0_60px_rgba(0,0,0,0.3)]">
       <Dialog.Title
         as="h3"
         className="relative w-full pl-4 px-4 sm:px-0 py-4 sm:py-6 bg-orange-l6 border-b border-gray-l2 font-heading font-black sm:font-bold sm:text-center text-xl text-orange sm:text-inherit uppercase sm:capitalize dark:bg-blue-d7 dark:border-bluegray"
@@ -22,7 +22,7 @@ export default function WalletModal() {
           <Icon type="Close" className="w-8 sm:w-7 h-8 sm:h-7" />
         </button>
       </Dialog.Title>
-      <div className="flex flex-col justify-center items-center gap-4 sm:gap-6 p-6 sm:p-8 dark:p-8 w-full overflow-y-auto">
+      <div className="flex flex-col items-center gap-4 sm:gap-6 p-6 sm:p-8 dark:p-8 w-full sm:max-h-[492px] overflow-y-auto">
         <h4 className="font-work sm:font-heading font-semibold sm:font-bold text-center text-base sm:text-lg">
           Select one of the available wallets to continue
         </h4>
