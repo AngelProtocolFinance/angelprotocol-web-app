b947351b23a2180c0c9aed4667471441de6b5210
Nenad Misic <n.misic@outlook.com>
2022-11-18 11:52:55 +0100

    Improve address width

--- src/App/WalletSuite/ConnectedWalletV2/Details/WalletDetails/WalletDetails.tsx
+++ src/App/WalletSuite/ConnectedWalletV2/Details/WalletDetails/WalletDetails.tsx
@@ -6,8 +6,8 @@ export default function WalletDetails(props: WalletState) {
   return (
     <div className="grid gap-3 p-4 border-b border-gray-l2">
       <Balances coins={props.coins} />
-      <div className="flex items-center gap-2 p-4 pl-3 bg-gray-l4 border border-gray-l2 rounded">
-        <span className="font-body font-normal text-sm truncate">
+      <div className="flex items-center w-full gap-2 p-4 pl-3 bg-gray-l4 border border-gray-l2 rounded">
+        <span className="w-56 font-body font-normal text-sm truncate">
           {props.address}
         </span>
         <Copier text={props.address} className="w-6 h-6" />
