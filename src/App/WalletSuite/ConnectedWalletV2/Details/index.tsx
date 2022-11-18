a04e505af5a3ac999808d2559d9c10c45d60eea6
Nenad Misic <n.misic@outlook.com>
2022-11-18 09:07:58 +0100

    Add disconnect button

--- src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
+++ src/App/WalletSuite/ConnectedWalletV2/Details/index.tsx
@@ -1,13 +1,21 @@
 import { Popover } from "@headlessui/react";
-import { WalletState } from "contexts/WalletContext";
+import { WalletState, useSetWallet } from "contexts/WalletContext";
 import MyEndowment from "./MyEndowment";
 import WalletDetails from "./WalletDetails";
 
 export default function Details(props: WalletState) {
+  const { disconnect } = useSetWallet();
+
   return (
     <Popover.Panel className="absolute top-2 right-0 grid w-80 bg-white rounded-lg border border-gray-l2 shadow-[0_0_16px_rgba(15,46,67,0.25)] text-gray-d2">
       <MyEndowment />
       <WalletDetails {...props} />
+      <button
+        onClick={disconnect}
+        className="btn bg-orange-l5 uppercase text-sm hover:bg-orange-l3 px-8 py-2 font-body font-bold text-base rounded-b"
+      >
+        disconnect
+      </button>
     </Popover.Panel>
   );
 }
