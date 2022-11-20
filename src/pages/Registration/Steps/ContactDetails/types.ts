8767a8f8084e51603faf5dcfdb1a1924a0143779
ap-justin <justin@angelprotocol.io>
2022-11-20 18:46:41 +0800

    merge type

--- src/pages/Registration/Steps/ContactDetails/types.ts
+++ src/pages/Registration/Steps/ContactDetails/types.ts
@@ -5,6 +5,5 @@ import { OptionType } from "components/registration";
 export type FormValues = Omit<ContactPerson, "role" | "referralMethod"> & {
   role: OptionType<ContactRoles>;
   referralMethod: OptionType<ReferralMethods>;
-} & {
   ref: string;
 };
