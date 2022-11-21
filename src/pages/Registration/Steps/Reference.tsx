30346d087195071cb05860226bb35dace785d82e
ap-justin <justin@angelprotocol.io>
2022-11-21 15:32:25 +0800

    reg right margin

--- src/pages/Registration/Steps/Reference.tsx
+++ src/pages/Registration/Steps/Reference.tsx
@@ -14,7 +14,7 @@ export default function Reference({ id, classes = "" }: Props) {
       className={`${classes} w-full py-4 px-6 text-sm text-left md:text-center bg-gray-l5 dark:bg-blue-d4 text-gray-d2 dark:text-white md:text-gray-d1 md:dark:text-gray md:border-t border-gray-l2 dark:border-bluegray rounded-b-lg`}
     >
       <div className="relative">
-        <span className="font-semibold">Your registration number:</span>
+        <span className="font-semibold mr-2">Your registration number:</span>
         <span className="block mt-1 md:inline md:mt-0">{id}</span>
 
         <WithTooltip text={tooltip} classes="before:left-0 before:top-0">
