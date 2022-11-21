6b3c918aa3870df20fc8f3d4df88b4c6717e3125
Nenad Misic <n.misic@outlook.com>
2022-11-21 12:09:50 +0100

    Rename UnsupportedNetworkError to UnsupportedChainError

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -86,10 +86,10 @@ export class WrongNetworkError extends APError {
   }
 }
 
-export class UnsupportedNetworkError extends APError {
+export class UnsupportedChainError extends APError {
   constructor(unsupportedChainId: string) {
     super(
-      "UnsupportedNetworkError",
+      "UnsupportedChainError",
       `Chain ID ${unsupportedChainId} not supported. The only supported networks are on: Juno, Terra, Binance and Ethereum. Please switch to one of those and reload the page`
     );
   }
