198a9d9dd5ff83b30fcc3374d9b497be16c214b1
Nenad Misic <n.misic@outlook.com>
2022-11-21 16:06:12 +0100

    Extract fetching chain data from populating the token balances

--- src/contexts/WalletContext/WalletContext.tsx
+++ src/contexts/WalletContext/WalletContext.tsx
@@ -1,3 +1,5 @@
+import { Coin } from "@cosmjs/proto-signing";
+import { ethers, utils } from "ethers";
 import {
   PropsWithChildren,
   createContext,
@@ -14,10 +16,12 @@ import {
 } from "./types";
 import { Chain, Token } from "types/aws";
 import { useChainQuery } from "services/apes";
+import { queryContract } from "services/juno/queryContract";
 import { WalletDisconnectedError, WrongNetworkError } from "errors/errors";
 import { EXPECTED_NETWORK_TYPE } from "constants/env";
 import { useErrorContext } from "../ErrorContext";
 import { placeholderChain } from "./constants";
+import { getERC20Holdings } from "./helpers/getERC20Holdings";
 import useInjectedProvider from "./useInjectedProvider";
 import useKeplr from "./useKeplr";
 import useTerra from "./useTerra";
@@ -145,11 +149,76 @@ export default function WalletContext(props: PropsWithChildren<{}>) {
     isFetching: isChainFetching,
     error,
   } = useChainQuery(
-    { providerInfo: activeProviderInfo! },
+    { chainId: activeProviderInfo?.chainId ?? "" },
     { skip: !activeProviderInfo }
   );
 
   useVerifyChain(activeProviderInfo, chain, error, disconnect);
+
+  useEffect(() => {
+    if (!activeProviderInfo) {
+      return;
+    }
+
+    (async function () {
+      const { address, chainId } = activeProviderInfo;
+
+      // fetch balances for juno or terra
+      if (chain.type === "juno-native" || chain.type === "terra-native") {
+        const balancesRes = await fetch(
+          chain.lcd_url + `/cosmos/bank/v1beta1/balances/${address}`
+        );
+
+        // returns only positive balances
+        const { balances: nativeBalances }: { balances: Coin[] } =
+          await balancesRes.json();
+
+        // checking providerId to know which specific wallet is connected
+        // this way once Terra v2 is enabled on Keplr again, the users will be able to
+        // fetch their balances even when using Keplr
+        const cw20Balances = await getCW20Balance(chain, address);
+
+        const allBalances = nativeBalances.concat(cw20Balances);
+
+        [chain.native_currency, ...chain.tokens].forEach((token) => {
+          const balance = allBalances.find((x) => x.denom === token.token_id);
+          token.balance = +utils.formatUnits(
+            balance?.amount ?? 0,
+            token.decimals
+          );
+        });
+
+        return { data: chain };
+      }
+
+      /**fetch balances for ethereum */
+      const jsonProvider = new ethers.providers.JsonRpcProvider(chain.rpc_url, {
+        chainId: +chainId,
+        name: chain.chain_name,
+      });
+      const queryResults = await jsonProvider.getBalance(address);
+
+      chain.native_currency.balance = +utils.formatUnits(
+        queryResults,
+        chain.native_currency.decimals
+      );
+
+      const erc20Holdings = await getERC20Holdings(
+        chain.rpc_url,
+        address,
+        chain.tokens.map((token) => token.token_id)
+      );
+
+      chain.tokens.forEach((token) => {
+        const erc20 = erc20Holdings.find(
+          (x) => x.contractAddress === token.token_id
+        );
+        token.balance = +(erc20?.balance ?? 0); // erc20 balance is already in decimal format
+      });
+
+      return { data: chain };
+    });
+  }, [activeProviderInfo, chain]);
 
   const walletState: WalletState | undefined = useMemo(() => {
     if (activeProviderInfo) {
@@ -233,6 +302,25 @@ function useVerifyChain(
       handle(new WrongNetworkError());
     }
   }, [activeProviderInfo, chain, chainError, handle]);
+}
+
+async function getCW20Balance(chain: Chain, walletAddress: string) {
+  const cw20BalancePromises = chain.tokens
+    .filter((x) => x.type === "cw20")
+    .map((x) =>
+      queryContract(
+        "cw20Balance",
+        x.token_id,
+        { addr: walletAddress },
+        chain.lcd_url
+      ).then((data) => ({
+        denom: x.token_id,
+        amount: data.balance,
+      }))
+    );
+
+  const cw20Balances = await Promise.all(cw20BalancePromises);
+  return cw20Balances;
 }
 
 const getContext = createContext<State>(initialState);
--- src/services/apes/apes.ts
+++ src/services/apes/apes.ts
@@ -1,12 +1,7 @@
-import { Coin } from "@cosmjs/proto-signing";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { ethers, utils } from "ethers";
-import { ProviderInfo } from "contexts/WalletContext/types";
 import { BaseChain, Chain, WithdrawLog } from "types/aws";
-import { queryContract } from "services/juno/queryContract";
 import { UnsupportedNetworkError } from "errors/errors";
 import { APIs } from "constants/urls";
-import { getERC20Holdings } from "./helpers/getERC20Holdings";
 import { apesTags } from "./tags";
 
 export const apes = createApi({
@@ -24,11 +19,10 @@ export const apes = createApi({
       providesTags: [{ type: apesTags.withdraw_logs }],
       query: (cw3) => `v1/withdraw/${cw3}`,
     }),
-    chain: builder.query<Chain, { providerInfo: ProviderInfo }>({
+    chain: builder.query<Chain, { chainId: string }>({
       providesTags: [{ type: apesTags.chain }],
-      async queryFn(args) {
+      async queryFn({ chainId }) {
         try {
-          const { address, chainId } = args.providerInfo;
           const chainRes = await fetch(`${APIs.apes}/v1/chain/${chainId}`);
 
           const chain: Chain | { message: string } = await chainRes.json();
@@ -37,67 +31,12 @@ export const apes = createApi({
             throw new UnsupportedNetworkError(chainId);
           }
 
-          // fetch balances for juno or terra
-          if (chain.type === "juno-native" || chain.type === "terra-native") {
-            const balancesRes = await fetch(
-              chain.lcd_url + `/cosmos/bank/v1beta1/balances/${address}`
-            );
-
-            // returns only positive balances
-            const { balances: nativeBalances }: { balances: Coin[] } =
-              await balancesRes.json();
-
-            // checking providerId to know which specific wallet is connected
-            // this way once Terra v2 is enabled on Keplr again, the users will be able to
-            // fetch their balances even when using Keplr
-            const cw20Balances = await getCW20Balance(chain, address);
-
-            const allBalances = nativeBalances.concat(cw20Balances);
-
-            [chain.native_currency, ...chain.tokens].forEach((token) => {
-              const balance = allBalances.find(
-                (x) => x.denom === token.token_id
-              );
-              token.balance = +utils.formatUnits(
-                balance?.amount ?? 0,
-                token.decimals
-              );
-            });
-
-            return { data: chain };
-          }
-
-          /**fetch balances for ethereum */
-          const jsonProvider = new ethers.providers.JsonRpcProvider(
-            chain.rpc_url,
-            { chainId: +chainId, name: chain.chain_name }
-          );
-          const queryResults = await jsonProvider.getBalance(address);
-
-          chain.native_currency.balance = +utils.formatUnits(
-            queryResults,
-            chain.native_currency.decimals
-          );
-
-          const erc20Holdings = await getERC20Holdings(
-            chain.rpc_url,
-            address,
-            chain.tokens.map((token) => token.token_id)
-          );
-
-          chain.tokens.forEach((token) => {
-            const erc20 = erc20Holdings.find(
-              (x) => x.contractAddress === token.token_id
-            );
-            token.balance = +(erc20?.balance ?? 0); // erc20 balance is already in decimal format
-          });
-
           return { data: chain };
         } catch (error) {
           return {
             error: {
               status: "CUSTOM_ERROR",
-              error: "Error querying balances",
+              error: "Error fetching chain data",
               data:
                 error instanceof UnsupportedNetworkError
                   ? error.toSerializable()
@@ -116,22 +55,3 @@ export const {
   useWithdrawLogsQuery,
   util: { invalidateTags: invalidateApesTags },
 } = apes;
-
-async function getCW20Balance(chain: Chain, walletAddress: string) {
-  const cw20BalancePromises = chain.tokens
-    .filter((x) => x.type === "cw20")
-    .map((x) =>
-      queryContract(
-        "cw20Balance",
-        x.token_id,
-        { addr: walletAddress },
-        chain.lcd_url
-      ).then((data) => ({
-        denom: x.token_id,
-        amount: data.balance,
-      }))
-    );
-
-  const cw20Balances = await Promise.all(cw20BalancePromises);
-  return cw20Balances;
-}
