db8383f58d4ae0bc22201fe4afb0e46196984756
personal <alihayder19@gmail.com>
2022-11-22 19:51:02 +0600

    Add Search Filter

---
+++ src/pages/Donations/SearchFilter.tsx
@@ -0,0 +1,150 @@
+import { Menu, Popover } from "@headlessui/react";
+import type { FC } from "react";
+import Icon from "components/Icon";
+
+interface SearchFilterProps {}
+
+const SearchFilter: FC<SearchFilterProps> = ({}) => {
+  return (
+    <Popover className="relative py-3 px-4 border border-gray-l2 rounded-sm">
+      <Popover.Button
+        className={
+          "w-full flex justify-between items-center outline-0 text-gray-d2"
+        }
+      >
+        <div className="uppercase font-semibold">Filter</div>
+        <Icon type="ArrowDown" size={20}></Icon>
+      </Popover.Button>
+
+      <Popover.Panel className="absolute w-full right-[.05rem] z-10 border border-gray-l2 rounded-sm mt-4">
+        <div className="flex flex-col">
+          <div>
+            <div className="flex flex-col w-full p-6 gap-6 bg-white">
+              <div className="flex flex-col text-gray-d2 gap-2">
+                <label>Date</label>
+                <div className="flex gap-4">
+                  <input
+                    type="date"
+                    className="w-full py-3 pl-3 border border-gray-l2 rounded-sm"
+                    placeholder="From"
+                  />
+                  <input
+                    type="date"
+                    className="w-full py-3 pl-3 border border-gray-l2 rounded-sm"
+                    placeholder="To"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col text-gray-d2 gap-2">
+                <label>Recipient</label>
+                <Menu>
+                  <Menu.Button
+                    className={
+                      "inline-flex w-full justify-between items-center border border-gray-l2 rounded-sm p-3"
+                    }
+                  >
+                    <div className="text-gray-l2">Select recipient...</div>
+                    <Icon type="ArrowDown" size={30}></Icon>
+                  </Menu.Button>
+                  <Menu.Items>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${active && "bg-blue-500"}`}
+                          href="/account-settings"
+                        >
+                          Account settings
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              </div>
+              <div className="flex flex-col text-gray-d2 gap-2">
+                <label>Network</label>
+                <Menu>
+                  <Menu.Button
+                    className={
+                      "inline-flex w-full justify-between items-center border border-gray-l2 rounded-sm p-3"
+                    }
+                  >
+                    <div className="text-gray-l2">Select network...</div>
+                    <Icon type="ArrowDown" size={30}></Icon>
+                  </Menu.Button>
+                  <Menu.Items>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${active && "bg-blue-500"}`}
+                          href="/account-settings"
+                        >
+                          Account settings
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              </div>
+              <div className="flex flex-col text-gray-d2 gap-2">
+                <label>Currency</label>
+                <Menu>
+                  <Menu.Button
+                    className={
+                      "inline-flex w-full justify-between items-center border border-gray-l2 rounded-sm p-3"
+                    }
+                  >
+                    <div className="text-gray-l2">Select currency...</div>
+                    <Icon type="ArrowDown" size={30}></Icon>
+                  </Menu.Button>
+                  <Menu.Items>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${active && "bg-blue-500"}`}
+                          href="/account-settings"
+                        >
+                          Account settings
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              </div>
+              <div className="flex flex-col text-gray-d2 gap-2">
+                <label>Status</label>
+                <Menu>
+                  <Menu.Button
+                    className={
+                      "inline-flex w-full justify-between items-center border border-gray-l2 rounded-sm p-3"
+                    }
+                  >
+                    <div className="text-gray-l2">Select status...</div>
+                    <Icon type="ArrowDown" size={30}></Icon>
+                  </Menu.Button>
+                  <Menu.Items>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <a
+                          className={`${active && "bg-blue-500"}`}
+                          href="/account-settings"
+                        >
+                          Account settings
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              </div>
+            </div>
+            <div className="py-6 pl-6 bg-orange-l6">
+              <a href="#" className="text-orange underline">
+                Reset filters
+              </a>
+            </div>
+          </div>
+        </div>
+      </Popover.Panel>
+    </Popover>
+  );
+};
+export default SearchFilter;
--- src/pages/Donations/index.tsx
+++ src/pages/Donations/index.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useDonationsQuery } from "services/apes";
 import { QueryLoader } from "components/admin";
+import SearchFilter from "./SearchFilter";
 import Table from "./Table";
 
 // import DonationsTable from "./DonationsTable";
@@ -15,10 +16,23 @@ export default function Donations() {
   );
 
   return (
-    <div className="grid grid-rows-[auto_1fr] padded-container pb-8 pt-28 text-white/80">
-      <h1 className="text-2xl font-extrabold uppercase text-white mb-4">
-        My Donations
-      </h1>
+    <div className="grid grid-rows-[auto_1fr] padded-container pb-8 pt-28 text-white/80 bg-white">
+      <div className="flex justify-between mt-10 mb-6">
+        <h1 className="text-2xl font-bold mb-4 text-gray-d2">My Donations</h1>
+        <button className="p-3 px-8 font-semibold bg-orange uppercase rounded-sm">
+          Export to CSV
+        </button>
+      </div>
+      <div className="grid grid-cols-3 my-8">
+        <div className="col-span-2 mr-4">
+          <input
+            className="py-3 px-4 border border-gray-l2 w-full"
+            type="text"
+            placeholder="Search donations..."
+          />
+        </div>
+        <SearchFilter />
+      </div>
       <QueryLoader
         queryState={queryState}
         messages={{
