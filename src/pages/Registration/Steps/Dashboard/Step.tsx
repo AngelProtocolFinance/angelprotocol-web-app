fe6e7986d3dac0d6822d0a8ec00a7314d64401e1
ap-justin <justin@angelprotocol.io>
2022-11-22 21:13:50 +0800

    endowment status

--- src/pages/Registration/Steps/Dashboard/Step.tsx
+++ src/pages/Registration/Steps/Dashboard/Step.tsx
@@ -1,32 +1,48 @@
+import { RegStep } from "pages/Registration/types";
 import { BtnSec } from "components/registration";
+import { useRegState } from "../StepGuard";
 
+type TStep = Exclude<RegStep, 4>;
 type Props = {
-  buttonLabel?: string;
-  customStatus?: string;
   disabled: boolean;
-  title: string;
-  isIncomplete?: boolean;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  num: TStep;
+  status?: string;
 };
 
-export default function Step(props: Props) {
+export default function Step({
+  num,
+  status = "Completed" /** not possible to visit this page without completing steps */,
+  disabled,
+}: Props) {
+  const {
+    data: { init },
+  } = useRegState<4>();
+
   return (
-    <div className="grid grid-cols-[2fr_1fr_auto] items-center w-full font-bold">
-      <p className="mr-auto text-left">{props.title}</p>
-      {props.isIncomplete ? (
-        <p className="uppercase text-orange-l1 w-40">Incomplete</p>
-      ) : (
-        <p className="uppercase text-green w-40">
-          {props.customStatus || "Completed"}
-        </p>
-      )}
+    <div
+      className={`py-6 pl-2 pr-4 grid grid-cols-[1fr_auto_auto] items-center border-b ${
+        num === 1 ? "border-t" : ""
+      } border-gray-l2 dark:border-bluegray `}
+    >
+      <p className="mr-auto text-left">{title[num]}</p>
+
+      <p className="text-green font-semibold max-sm:row-start-2">{status}</p>
+
       <BtnSec
-        className="min-w-[8rem]"
-        onClick={props.onClick}
-        disabled={props.disabled}
+        as="link"
+        to={`../${num}`}
+        className="min-w-[8rem] ml-6 max-sm:row-span-2"
+        aria-disabled={disabled}
+        state={init}
       >
-        {props.buttonLabel || "Update"}
+        Update
       </BtnSec>
     </div>
   );
 }
+
+const title: { [key in TStep]: string } = {
+  1: "Contact Details",
+  2: "Documentation",
+  3: "Wallet address",
+};
