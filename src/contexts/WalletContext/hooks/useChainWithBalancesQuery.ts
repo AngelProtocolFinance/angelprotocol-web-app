f2b49d9a303868fc46e81a0ae3e26b1e37bfc4e9
shonmisic <n.misic@outlook.com>
2022-11-23 13:13:45 +0100

    Update loading logic in useChainWithBalancesQuery

--- src/contexts/WalletContext/hooks/useChainWithBalancesQuery.ts
+++ src/contexts/WalletContext/hooks/useChainWithBalancesQuery.ts
@@ -18,99 +18,109 @@ export function useChainWithBalancesQuery(
   disconnect: () => void
 ): Result {
   const [chainWithBalance, setChainWithBalance] = useState(placeholderChain);
+  const [isLoading, setLoading] = useState(true);
+
+  const { handleError } = useErrorContext();
 
   const {
     data: chain,
     isLoading: isChainLoading,
-    isFetching: isChainFetching,
+    isFetching,
     error,
   } = useChainQuery(activeProviderInfo?.chainId ?? "", {
     skip: !activeProviderInfo,
   });
 
-  const isLoading = isChainLoading || isChainFetching;
+  const isChainFetching = isChainLoading || isFetching;
 
-  useVerifyChain(activeProviderInfo, chain, isLoading, error, disconnect);
+  useVerifyChain(activeProviderInfo, chain, isChainFetching, error, disconnect);
 
   useEffect(() => {
-    if (isLoading || !activeProviderInfo || !chain) {
+    if (isChainFetching || !activeProviderInfo || !chain) {
       return;
     }
 
     (async function () {
       // tokens & native_currency need to be copied separately, otherwise
       // the balance will not be settable (due to the returned `chain` not being modifiable)
-      const result: Chain = {
-        ...chain,
-        native_currency: {
-          ...chain.native_currency,
-        },
-        tokens: chain.tokens.map((t) => ({ ...t })),
-      };
+      setLoading(true);
+      try {
+        const result: Chain = {
+          ...chain,
+          native_currency: {
+            ...chain.native_currency,
+          },
+          tokens: chain.tokens.map((t) => ({ ...t })),
+        };
 
-      const { address, chainId } = activeProviderInfo;
+        const { address, chainId } = activeProviderInfo;
 
-      // fetch balances for juno or terra
-      if (chain.type === "juno-native" || chain.type === "terra-native") {
-        const balancesRes = await fetch(
-          chain.lcd_url + `/cosmos/bank/v1beta1/balances/${address}`
-        );
-
-        // returns only positive balances
-        const { balances: nativeBalances }: { balances: Coin[] } =
-          await balancesRes.json();
-
-        // checking providerId to know which specific wallet is connected
-        // this way once Terra v2 is enabled on Keplr again, the users will be able to
-        // fetch their balances even when using Keplr
-        const cw20Balances = await getCW20Balance(chain, address);
-
-        const allBalances = nativeBalances.concat(cw20Balances);
-
-        [result.native_currency, ...result.tokens].forEach((token) => {
-          const balance = allBalances.find((x) => x.denom === token.token_id);
-          token.balance = +utils.formatUnits(
-            balance?.amount ?? 0,
-            token.decimals
+        // fetch balances for juno or terra
+        if (chain.type === "juno-native" || chain.type === "terra-native") {
+          const balancesRes = await fetch(
+            chain.lcd_url + `/cosmos/bank/v1beta1/balances/${address}`
           );
-        });
-      } else {
-        /**fetch balances for ethereum */
-        const jsonProvider = new ethers.providers.JsonRpcProvider(
-          chain.rpc_url,
-          {
-            chainId: +chainId,
-            name: chain.chain_name,
-          }
-        );
-        const queryResults = await jsonProvider.getBalance(address);
 
-        const erc20Holdings = await getERC20Holdings(
-          chain.rpc_url,
-          address,
-          chain.tokens.map((token) => token.token_id)
-        );
+          // returns only positive balances
+          const { balances: nativeBalances }: { balances: Coin[] } =
+            await balancesRes.json();
 
-        result.native_currency.balance = +utils.formatUnits(
-          queryResults,
-          chain.native_currency.decimals
-        );
+          // checking providerId to know which specific wallet is connected
+          // this way once Terra v2 is enabled on Keplr again, the users will be able to
+          // fetch their balances even when using Keplr
+          const cw20Balances = await getCW20Balance(chain, address);
 
-        result.tokens.forEach((token) => {
-          const erc20 = erc20Holdings.find(
-            (x) => x.contractAddress === token.token_id
+          const allBalances = nativeBalances.concat(cw20Balances);
+
+          [result.native_currency, ...result.tokens].forEach((token) => {
+            const balance = allBalances.find((x) => x.denom === token.token_id);
+            token.balance = +utils.formatUnits(
+              balance?.amount ?? 0,
+              token.decimals
+            );
+          });
+        } else {
+          /**fetch balances for ethereum */
+          const jsonProvider = new ethers.providers.JsonRpcProvider(
+            chain.rpc_url,
+            {
+              chainId: +chainId,
+              name: chain.chain_name,
+            }
           );
-          token.balance = +(erc20?.balance ?? 0); // erc20 balance is already in decimal format
-        });
+          const queryResults = await jsonProvider.getBalance(address);
+
+          const erc20Holdings = await getERC20Holdings(
+            chain.rpc_url,
+            address,
+            chain.tokens.map((token) => token.token_id)
+          );
+
+          result.native_currency.balance = +utils.formatUnits(
+            queryResults,
+            chain.native_currency.decimals
+          );
+
+          result.tokens.forEach((token) => {
+            const erc20 = erc20Holdings.find(
+              (x) => x.contractAddress === token.token_id
+            );
+            token.balance = +(erc20?.balance ?? 0); // erc20 balance is already in decimal format
+          });
+        }
+
+        setChainWithBalance(result);
+      } catch (error) {
+        handleError(error);
+      } finally {
+        setLoading(false);
       }
-
-      setChainWithBalance(result);
     })();
-  }, [activeProviderInfo, chain, isLoading]);
+  }, [activeProviderInfo, chain, isChainFetching, handleError]);
 
   return {
     chain: chainWithBalance,
-    isLoading,
+    isLoading: isLoading || isChainFetching,
   };
 }
 
