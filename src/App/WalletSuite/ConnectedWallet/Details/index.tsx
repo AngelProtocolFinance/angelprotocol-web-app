f5b910bba1e9ea5a331f99b2e0b4f1a5ce68f5a4
shonmisic <n.misic@outlook.com>
2022-11-24 14:10:13 +0100

    Remove old ConnectedWallet

--- src/App/WalletSuite/ConnectedWallet/Details/index.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/index.tsx
@@ -1,55 +1,30 @@
 import { Popover } from "@headlessui/react";
 import { Link } from "react-router-dom";
-import { AP_ID, REVIEWER_ID } from "services/juno/custom";
 import { WalletState, useSetWallet } from "contexts/WalletContext";
-import Copier from "components/Copier";
-import { AdminLink } from "components/admin";
-import { maskAddress } from "helpers";
 import { appRoutes } from "constants/routes";
-import Bookmarks from "./Bookmarks";
-import Filter from "./Filter";
-import Holdings from "./Holdings";
-
-const linkStyle =
-  "text-blue hover:text-orange text-sm font-bold font-heading px-3 text-left uppercase";
+import Favourites from "./Favourites";
+import MyEndowment from "./MyEndowment";
+import WalletDetails from "./WalletDetails";
 
 export default function Details(props: WalletState) {
-  const { coins, address, chain } = props;
   const { disconnect } = useSetWallet();
 
   return (
-    <Popover.Panel className="w-60 z-50 grid content-start absolute mt-2 bg-white right-0 rounded-md overflow-hidden shadow-lg">
-      <div className="bg-gray-d2 text-zinc-50 text-xs p-2">
-        <p className="uppercase">network : {chain.chain_name}</p>
+    <Popover.Panel className="absolute top-2 right-0 grid w-80 bg-white rounded-lg border border-gray-l2 shadow-[0_0_16px_rgba(15,46,67,0.25)] text-gray-d2">
+      <MyEndowment />
+      <WalletDetails {...props} />
+      <div className="items-center p-4 border-b border-gray-l2">
+        <Link
+          to={`${appRoutes.donations}/${props.address}`}
+          className="font-heading font-bold text-sm uppercase hover:text-orange"
+        >
+          My Donations
+        </Link>
       </div>
-
-      <div className="flex gap-2 items-center p-3 pb-0">
-        <p className="text-sm text-gray-d2 font-mono">{maskAddress(address)}</p>
-        <Copier
-          text={address}
-          classes="text-gray-d2 text-lg hover:text-orange"
-        />
-      </div>
-
-      <Link
-        to={`${appRoutes.donations}/${props.address}`}
-        className={linkStyle + " mt-2"}
-      >
-        My Donations
-      </Link>
-
-      <AdminLink label="admin" className={linkStyle} id={AP_ID} />
-      <AdminLink label="applications" className={linkStyle} id={REVIEWER_ID} />
-
-      <Bookmarks />
-
-      <Filter coins={coins} classes="mt-1">
-        {(filtered) => <Holdings coins={filtered} />}
-      </Filter>
-
+      <Favourites {...props} />
       <button
         onClick={disconnect}
-        className="mt-4 uppercase text-sm bg-orange hover:bg-orange-l1 p-2 text-white"
+        className="btn h-12 bg-orange-l5 hover:bg-orange-l3 uppercase text-sm font-body font-bold text-base rounded-b"
       >
         disconnect
       </button>
