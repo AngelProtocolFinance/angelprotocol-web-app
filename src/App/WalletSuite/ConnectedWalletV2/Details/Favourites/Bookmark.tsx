b70eaf4eb273bc6a97d58ee46cd1c81dc31d1109
shonmisic <n.misic@outlook.com>
2022-11-24 14:04:54 +0100

    Add hover to Bookmarks

--- src/App/WalletSuite/ConnectedWalletV2/Details/Favourites/Bookmark.tsx
+++ src/App/WalletSuite/ConnectedWalletV2/Details/Favourites/Bookmark.tsx
@@ -4,16 +4,16 @@ import { appRoutes } from "constants/routes";
 
 export default function Bookmark({ name, id, logo }: EndowmentBookmark) {
   return (
-    <li className="flex items-center gap-2 py-1">
-      <img
-        src={logo}
-        alt=""
-        className="w-4 h-4 border border-gray-l2 rounded-full"
-      />
+    <li>
       <Link
         to={appRoutes.profile + "/" + id}
-        className="font-heading font-semibold text-sm"
+        className="flex items-center gap-2 py-1 font-heading font-semibold text-sm hover:bg-orange-l5"
       >
+        <img
+          src={logo}
+          alt=""
+          className="w-4 h-4 border border-gray-l2 rounded-full"
+        />
         {name}
       </Link>
     </li>
