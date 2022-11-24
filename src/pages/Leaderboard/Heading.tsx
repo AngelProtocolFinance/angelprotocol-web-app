8725e8cb8b2dad81f744868cb0ca0ea5955364d3
ap-justin <justin@angelprotocol.io>
2022-11-24 13:39:41 +0800

    re-org files

--- src/pages/Leaderboard/Heading.tsx
+++
@@ -1,9 +0,0 @@
-type Props = { text: string };
-
-export default function Heading(props: Props) {
-  return (
-    <th className="text-left uppercase font-heading text-md p-2 pl-0 text-gray-d2">
-      {props.text}
-    </th>
-  );
-}
