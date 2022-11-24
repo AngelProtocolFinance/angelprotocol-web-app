a21359d0322735a4f682990eea093666de5aeb99
shonmisic <n.misic@outlook.com>
2022-11-24 14:12:02 +0100

    Refactor Details

--- src/App/WalletSuite/ConnectedWallet/Details/index.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/index.tsx
@@ -7,27 +7,39 @@ import MyEndowment from "./MyEndowment";
 import WalletDetails from "./WalletDetails";
 
 export default function Details(props: WalletState) {
-  const { disconnect } = useSetWallet();
-
   return (
     <Popover.Panel className="absolute top-2 right-0 grid w-80 bg-white rounded-lg border border-gray-l2 shadow-[0_0_16px_rgba(15,46,67,0.25)] text-gray-d2">
       <MyEndowment />
       <WalletDetails {...props} />
-      <div className="items-center p-4 border-b border-gray-l2">
-        <Link
-          to={`${appRoutes.donations}/${props.address}`}
-          className="font-heading font-bold text-sm uppercase hover:text-orange"
-        >
-          My Donations
-        </Link>
-      </div>
+      <MyDonations address={props.address} />
       <Favourites {...props} />
-      <button
-        onClick={disconnect}
-        className="btn h-12 bg-orange-l5 hover:bg-orange-l3 uppercase text-sm font-body font-bold text-base rounded-b"
-      >
-        disconnect
-      </button>
+      <DisconnectBtn />
     </Popover.Panel>
+  );
+}
+
+function MyDonations({ address }: { address: string }) {
+  return (
+    <div className="items-center p-4 border-b border-gray-l2">
+      <Link
+        to={`${appRoutes.donations}/${address}`}
+        className="font-heading font-bold text-sm uppercase hover:text-orange"
+      >
+        My Donations
+      </Link>
+    </div>
+  );
+}
+
+function DisconnectBtn() {
+  const { disconnect } = useSetWallet();
+
+  return (
+    <button
+      onClick={disconnect}
+      className="btn h-12 bg-orange-l5 hover:bg-orange-l3 uppercase text-sm font-body font-bold text-base rounded-b"
+    >
+      disconnect
+    </button>
   );
 }
