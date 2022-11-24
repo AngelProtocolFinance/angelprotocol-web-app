0dd85e5274c0c6f6ee295e27202d4cd0dfb16610
shonmisic <n.misic@outlook.com>
2022-11-24 15:10:50 +0100

    Set address width on mobile

--- src/App/WalletSuite/ConnectedWallet/Details/WalletDetails/WalletDetails.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/WalletDetails/WalletDetails.tsx
@@ -6,8 +6,8 @@ export default function WalletDetails(props: WalletState) {
   return (
     <div className="grid gap-3 p-4 border-b border-gray-l2">
       <Balances coins={props.coins} />
-      <div className="flex items-center w-full gap-2 p-4 pl-3 bg-gray-l4 border border-gray-l2 rounded">
-        <span className="w-56 font-body font-normal text-sm truncate hover:cursor-default">
+      <div className="flex items-center justify-between w-full gap-2 p-4 pl-3 bg-gray-l4 border border-gray-l2 rounded">
+        <span className="max-w-[75vw] sm:w-56 font-body font-normal text-sm truncate hover:cursor-default">
           {props.address}
         </span>
         <Copier text={props.address} classes="w-6 h-6 hover:text-orange" />
