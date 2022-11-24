7ab02a1dfe0b868cce04f302ea5eb1b3b6ced33c
shonmisic <n.misic@outlook.com>
2022-11-24 17:17:18 +0100

    Use Logo in Bookmark

--- src/App/WalletSuite/ConnectedWallet/Details/Favourites/Bookmark.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/Favourites/Bookmark.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { EndowmentBookmark } from "types/aws";
 import { appRoutes } from "constants/routes";
+import Logo from "../Logo";
 
 export default function Bookmark({ name, id, logo }: EndowmentBookmark) {
   return (
@@ -9,11 +10,7 @@ export default function Bookmark({ name, id, logo }: EndowmentBookmark) {
         to={appRoutes.profile + "/" + id}
         className="flex items-center gap-2 py-1 font-heading font-semibold text-sm hover:bg-orange-l5"
       >
-        <img
-          src={logo}
-          alt=""
-          className="w-4 h-4 object-contain border border-gray-l2 dark:border-bluegray rounded-full"
-        />
+        <Logo logo="logo" className="w-4 h-4" />
         <span className="truncate max-w-[200px]">{name}</span>
       </Link>
     </li>
