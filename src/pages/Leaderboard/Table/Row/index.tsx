f8a0d246e369aa4a612f08eb1634d5e9f4c6da26
ap-justin <89639563+ap-justin@users.noreply.github.com>
2022-11-24 17:18:57 +0800

    Leaderboard style updates (#1478)
    * leaderboard header color
    
    * re-org files
    
    * update types
    
    * loader style
    
    * add more space
    
    * route update

---
+++ src/pages/Leaderboard/Table/Row/index.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import { LeaderboardEntry } from "types/aws";
+import defaultIcon from "assets/images/angelprotocol-horiz-blu.png";
+import { Cells } from "components/TableSection";
+import { appRoutes } from "constants/routes";
+import Amount from "./Amount";
+import projectFunds from "./projectFunds";
+
+export default function Row({
+  total_liq,
+  total_lock,
+  charity_logo,
+  endowment_id,
+  charity_name,
+}: LeaderboardEntry) {
+  const { locked, liquid } = projectFunds(10, total_lock, total_liq, 20, 5);
+  return (
+    <Cells type="td" cellClass="first:pl-4 last:pr-4 py-3">
+      <img
+        src={charity_logo || defaultIcon}
+        onError={(e) => e.currentTarget.setAttribute("src", defaultIcon)}
+        loading="lazy"
+        alt=""
+        className={`h-16 aspect-video rounded object-contain border border-gray-l2  dark:border-none dark:bg-white p-2`}
+      />
+      <Link
+        to={`../${appRoutes.profile}/${endowment_id}`}
+        className="hover:text-blue active:text-blue-d1"
+      >
+        {charity_name}
+      </Link>
+      <Amount type="total" locked={total_lock} liquid={total_liq} />
+      <Amount type="projected" locked={locked} liquid={liquid} />
+    </Cells>
+  );
+}
---
+++ src/pages/Leaderboard/Table/index.tsx
@@ -0,0 +1,41 @@
+import { LeaderboardEntry } from "types/aws";
+import TableSection, { Cells } from "components/TableSection";
+import Row from "./Row";
+
+type Props = {
+  endowments: LeaderboardEntry[];
+  classes?: string;
+};
+
+export default function Table({ classes = "", endowments }: Props) {
+  return (
+    <div
+      className={`${classes} h-[50rem] overflow-y-scroll scroller border border-gray-l2 dark:border-bluegray rounded`}
+    >
+      <table className="border-collapse table-auto w-full">
+        <TableSection
+          type="thead"
+          rowClass="border-b border-gray-l2 dark:border-bluegray bg-orange-l6 dark:bg-blue-d6"
+        >
+          <Cells
+            type="th"
+            cellClass="text-left py-6 text-lg font-heading font-semibold"
+          >
+            <></>
+            <>Organization</>
+            <>Total</>
+            <>10YR Projection</>
+          </Cells>
+        </TableSection>
+        <TableSection
+          type="tbody"
+          rowClass="border-b border-gray-l3 dark:border-bluegray/70"
+        >
+          {endowments.map((endowment) => (
+            <Row key={endowment.endowment_id} {...endowment} />
+          ))}
+        </TableSection>
+      </table>
+    </div>
+  );
+}
