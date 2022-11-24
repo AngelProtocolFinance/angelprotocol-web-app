2011d58094c731de3a46a5126ae84ac97dbe1eb5
shonmisic <n.misic@outlook.com>
2022-11-24 17:01:31 +0100

    Add dark mode support

--- src/App/WalletSuite/ConnectedWallet/Details/MyEndowment.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/MyEndowment.tsx
@@ -10,8 +10,8 @@ const profile = undefined;
 export default function MyEndowment() {
   return (
     !profile && (
-      <div className="grid p-4 gap-3 border-b border-gray-l2">
-        <h3 className="font-heading font-bold text-sm text-gray-d1">
+      <div className="grid p-4 gap-3 border-b border-gray-l2 dark:border-bluegray">
+        <h3 className="font-heading font-bold text-sm text-gray-d1 dark:text-gray">
           My Endowment
         </h3>
         <div className="flex gap-3">
@@ -22,14 +22,17 @@ export default function MyEndowment() {
               {/* Will be added once possible to fetch endowment profile by wallet address */}
               {"endowment name"}
             </span>
-            <div className="flex items-center uppercase font-heading font-semibold text-xs underline text-orange">
+            <div className="flex items-center uppercase font-heading font-semibold text-xs underline underline-offset-2 decoration-1 text-orange">
               {/* Will be added once possible to fetch endowment profile by wallet address */}
-              <Link to={""} className="pr-2 border-r border-gray-l2">
+              <Link
+                to={""}
+                className="pr-2 border-r border-gray-l2 dark:border-bluegray"
+              >
                 profile
               </Link>
               <AdminLink
                 label="admin"
-                className="px-2 border-r border-gray-l2"
+                className="px-2 border-r border-gray-l2 dark:border-bluegray"
                 id={AP_ID}
               />
               <AdminLink
@@ -53,7 +56,7 @@ function Logo({ logo }: { logo: string }) {
       {isLoading && <ContentLoader className="w-10 h-10 rounded-full" />}
       <img
         src={logo}
-        className={`w-10 h-10 object-contain border border-gray-l2 rounded-full ${
+        className={`w-10 h-10 object-contain border border-gray-l2 dark:border-bluegray rounded-full ${
           isLoading ? "hidden" : ""
         }`}
         alt=""
