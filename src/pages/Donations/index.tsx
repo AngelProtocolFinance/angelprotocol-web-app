d285c4a2057cd0fdcb562cbc3ec64fcf3fd79734
personal <alihayder19@gmail.com>
2022-11-29 03:36:06 +0600

    Add Search Icon

--- src/pages/Donations/index.tsx
+++ src/pages/Donations/index.tsx
@@ -18,22 +18,29 @@ export default function Donations() {
   );
 
   return (
-    <div className="grid grid-rows-[auto_1fr] padded-container pb-8 pt-28 text-white/80 bg-white dark:bg-blue-d4">
+    <div className="grid grid-rows-[auto_1fr] padded-container pb-8 pt-28 bg-white dark:bg-blue-d4">
       <div className="flex sm:justify-between justify-center mt-10 sm:mb-6">
         <h1 className="text-2xl font-bold sm:mb-4 text-gray-d2 dark:text-white">
           My Donations
         </h1>
-        <button className="p-3 px-8 font-semibold bg-orange uppercase rounded-sm hidden sm:block">
+        <button className="p-3 px-8 font-semibold bg-orange uppercase rounded-sm hidden sm:block text-white">
           Export to CSV
         </button>
       </div>
       <div className="grid grid-rows-2 sm:grid-cols-3 my-8">
         <div className="sm:col-span-2 sm:mr-4">
-          <input
-            className="py-3 px-4 border border-gray-l2 dark:border-bluegray w-full dark:bg-blue-d6"
-            type="text"
-            placeholder="Search donations..."
-          />
+          <label htmlFor="search" className="relative">
+            <Icon
+              type="Search"
+              size={24}
+              className="absolute top-0 ml-3 text-gray-d1"
+            ></Icon>
+            <input
+              className="py-3 pl-11 pr-4 border border-gray-l2 dark:border-bluegray w-full dark:bg-blue-d6"
+              type="text"
+              placeholder="Search donations..."
+            />
+          </label>
         </div>
         <div className="hidden sm:block">
           <SearchFilter />
