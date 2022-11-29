b90447d5230dc147391e5636efb8aebe82547be8
personal <alihayder19@gmail.com>
2022-11-29 14:53:16 +0600

    Remove Register2 Route from hsBG file

--- src/helpers/hasBG.ts
+++ src/helpers/hasBG.ts
@@ -2,10 +2,7 @@ import { Location } from "react-router-dom";
 import { appRoutes } from "constants/routes";
 
 export default function hasBg(location: Location) {
-  return [
-    appRoutes.register2,
-    appRoutes.register,
-    appRoutes.leaderboard,
-    appRoutes.donations,
-  ].some((route) => location.pathname.includes(route));
+  return [appRoutes.register, appRoutes.leaderboard, appRoutes.donations].some(
+    (route) => location.pathname.includes(route)
+  );
 }
