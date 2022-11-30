bb9900c2ce84b1cfdc04d12f45c3dedbd186fb8c
ap-justin <justin@angelprotocol.io>
2022-11-30 13:55:31 +0800

    set default app bg and text

--- src/pages/Admin/Charity/index.tsx
+++ src/pages/Admin/Charity/index.tsx
@@ -6,7 +6,7 @@ import Views from "./Views";
 export default function Charity() {
   const { endowmentId } = useAdminResources();
   return (
-    <div className="w-full pt-4 pb-8 bg-gray-l5 dark:bg-blue-d5 text-gray-d2 dark:text-white font-work">
+    <div className="w-full pt-4 pb-8 font-work">
       <div className="padded-container grid grid-rows-[auto_1fr] gap-2 my-4">
         <Nav />
         <div className="block md:hidden">
--- src/pages/Leaderboard/index.tsx
+++ src/pages/Leaderboard/index.tsx
@@ -7,33 +7,31 @@ export default function Leaderboard() {
   const queryState = useLeaderboardsQuery(null);
 
   return (
-    <section className="bg-gray-l5 dark:bg-blue-d5 pb-16 text-gray-d2 dark:text-white font-work">
-      <div className="padded-container grid content-start mt-8">
-        <DonationMetrics />
-        <h3 className="mt-6 uppercase text-3xl font-bold">Leaderboard</h3>
-        <QueryLoader
-          queryState={queryState}
-          messages={{
-            loading: "Fetching data...",
-            error: "Failed to get leaderboard data",
-          }}
-          classes={{ container: "mt-6" }}
-        >
-          {(update) => (
-            <>
-              <p className="justify-self-end flex gap-2 text-sm font-light text-gray-d1 dark:text-gray">
-                last updated:{" "}
-                {new Date(update.last_update).toLocaleString([], {
-                  dateStyle: "short",
-                  timeStyle: "short",
-                  hour12: false,
-                })}
-              </p>
-              <Table endowments={update.endowments} classes="mt-2" />
-            </>
-          )}
-        </QueryLoader>
-      </div>
+    <section className="padded-container grid content-start mt-8 pb-16 font-work">
+      <DonationMetrics />
+      <h3 className="mt-6 uppercase text-3xl font-bold">Leaderboard</h3>
+      <QueryLoader
+        queryState={queryState}
+        messages={{
+          loading: "Fetching data...",
+          error: "Failed to get leaderboard data",
+        }}
+        classes={{ container: "mt-6" }}
+      >
+        {(update) => (
+          <>
+            <p className="justify-self-end flex gap-2 text-sm font-light text-gray-d1 dark:text-gray">
+              last updated:{" "}
+              {new Date(update.last_update).toLocaleString([], {
+                dateStyle: "short",
+                timeStyle: "short",
+                hour12: false,
+              })}
+            </p>
+            <Table endowments={update.endowments} classes="mt-2" />
+          </>
+        )}
+      </QueryLoader>
     </section>
   );
 }
