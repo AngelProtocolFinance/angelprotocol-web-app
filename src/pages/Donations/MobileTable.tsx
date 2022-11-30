76b8ecbe57111f130faafe5636d39066b8b36fcf
personal <alihayder19@gmail.com>
2022-11-30 15:14:17 +0600

    Create and Seperate Mobile Row Component

--- src/pages/Donations/MobileTable.tsx
+++ src/pages/Donations/MobileTable.tsx
@@ -1,8 +1,10 @@
 import { Disclosure } from "@headlessui/react";
+import type { FC } from "react";
 import { Donation } from "types/aws";
 import Icon from "components/Icon";
 import useKYC from "components/KYC/useKYC";
 import { useSort } from "components/donations";
+import MobileRow from "components/donations/MobileRow";
 import { humanize, maskAddress } from "helpers";
 
 export default function MobileTable(props: { donations: Donation[] }) {
@@ -11,7 +13,7 @@ export default function MobileTable(props: { donations: Donation[] }) {
 
   return (
     <div className="sm:hidden">
-      <div className="grid grid-cols-8 bg-orange-l6 dark:bg-blue-d7 text-gray-d2 dark:text-white border border-gray-l2 dark:border-bluegray">
+      <div className="grid grid-cols-8 font-bold bg-orange-l6 dark:bg-blue-d7 text-gray-d2 dark:text-white border border-gray-l2 dark:border-bluegray">
         <div className="col-span-1 border-r-[1px] border-gray-l2 dark:border-bluegray p-4 place-self-center">
           <Icon
             type="ArrowDown"
@@ -68,66 +70,31 @@ export default function MobileTable(props: { donations: Donation[] }) {
               </Disclosure.Button>
               <Disclosure.Panel>
                 <div className="flex flex-col border-x-[1px] border-gray-l2 dark:border-bluegray">
-                  <div className="flex justify-between dark:bg-blue-d7 p-4 border-b-[1px] border-gray-l2 dark:border-bluegray">
-                    <span className="text-gray-d2 dark:text-white font-bold uppercase">
-                      Network
-                    </span>
-                    <span className="text-gray-d2 dark:text-white">
-                      {chainName}
-                    </span>
-                  </div>
-                  <div className="flex justify-between dark:bg-blue-d7 bg-orange-l6 p-4 border-b-[1px] border-gray-l2 dark:border-bluegray">
-                    <span className="text-gray-d2 dark:text-white font-bold uppercase">
-                      Currency
-                    </span>
-                    <span className="text-gray-d2 dark:text-white">
-                      {symbol}
-                    </span>
-                  </div>
-                  <div className="flex justify-between dark:bg-blue-d7 p-4 border-b-[1px] border-gray-l2 dark:border-bluegray">
-                    <span className="text-gray-d2 dark:text-white font-bold uppercase">
-                      Amount
-                    </span>
-                    <span className="text-gray-d2 dark:text-white">
-                      {humanize(amount, 3)}
-                    </span>
-                  </div>
-                  <div className="flex justify-between dark:bg-blue-d7  bg-orange-l6 p-4 border-b-[1px] border-gray-l2 dark:border-bluegray">
-                    <span className="text-gray-d2 dark:text-white font-bold uppercase">
-                      TX Hash
-                    </span>
-                    <span className="text-gray-d2 dark:text-white">
-                      {maskAddress(hash)}
-                    </span>
-                  </div>
-                  <div className="flex justify-between dark:bg-blue-d7 p-4 border-b-[1px] border-gray-l2 dark:border-bluegray">
-                    <span className="text-gray-d2 dark:text-white font-bold uppercase">
-                      Status
-                    </span>
-                    <button className="block bg-green text-white p-1 rounded uppercase text-xs">
-                      Received
-                    </button>
-                  </div>
-                  <div className="flex justify-between dark:bg-blue-d7  bg-orange-l6 p-4">
-                    <span className="text-gray-d2 dark:text-white font-bold uppercase">
-                      Receipt
-                    </span>
-                    <button
-                      className="block"
-                      onClick={() =>
-                        showKYCForm({
-                          type: "post-donation",
-                          txHash: hash,
-                          classes: "grid gap-5",
-                        })
-                      }
-                    >
-                      <Icon
-                        type="FatArrowDownload"
-                        className="text-gray-d2 dark:text-white text-2xl"
-                      />
-                    </button>
-                  </div>
+                  <MobileRow title="Network" data={`${chainName}`} />
+                  <MobileRow title="Currency" data={`${symbol}`} />
+                  <MobileRow title="Amount" data={`${humanize(amount, 3)}`} />
+                  <MobileRow title="TX Hash" data={`${maskAddress(hash)}`} />
+                  <MobileRow title="Status" data="Received" />
+                  <MobileRow
+                    title="Receipt"
+                    data={
+                      <button
+                        className="block"
+                        onClick={() =>
+                          showKYCForm({
+                            type: "post-donation",
+                            txHash: hash,
+                            classes: "grid gap-5",
+                          })
+                        }
+                      >
+                        <Icon
+                          type="FatArrowDownload"
+                          className="text-gray-d2 dark:text-white text-2xl"
+                        />
+                      </button>
+                    }
+                  />
                 </div>
               </Disclosure.Panel>
             </>
