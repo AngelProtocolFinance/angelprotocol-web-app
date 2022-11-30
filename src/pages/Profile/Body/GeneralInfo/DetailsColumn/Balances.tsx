bf630f702c3466032887527ee8a9418b2318c14a
shonmisic <n.misic@outlook.com>
2022-11-30 14:38:25 +0100

    Make balances loading indicator show individually

--- src/pages/Profile/Body/GeneralInfo/DetailsColumn/Balances.tsx
+++ src/pages/Profile/Body/GeneralInfo/DetailsColumn/Balances.tsx
@@ -5,47 +5,64 @@ import { condense, humanize } from "helpers";
 import { axlUSDCDenom } from "constants/tokens";
 import { useProfileContext } from "../../../ProfileContext";
 
+const CONTAINER_STYLE =
+  "flex flex-col justify-center items-center gap-2 h-20 w-full py-4 rounded border border-gray-l2 dark:bg-blue-d6 dark:border-bluegray md:items-start md:h-28 md:px-6 md:py-04";
+
 export default function Balances() {
   const profile = useProfileContext();
   const queryState = useBalanceQuery({ id: profile.id });
 
   return (
-    <QueryLoader
-      queryState={queryState}
-      messages={{
-        loading: "Fetching endowment balances",
-        error: "Failed to get endowment balances ",
-      }}
-      classes={{
-        container: `flex flex-col items-center justify-center w-full`,
-      }}
-    >
-      {({ tokens_on_hand }) => <Content {...tokens_on_hand} />}
-    </QueryLoader>
-  );
-}
-
-function Content({ liquid, locked }: BalanceInfo) {
-  const lockedAmount =
-    locked.native.find((bal) => bal.denom === axlUSDCDenom)?.amount ?? 0;
-  const liquidAmount =
-    liquid.native.find((bal) => bal.denom === axlUSDCDenom)?.amount ?? 0;
-
-  return (
     <div className="flex flex-col items-center gap-4 w-full">
-      <Balance
-        title="Total Value"
-        amount={Number(lockedAmount) + Number(liquidAmount)}
-      />
-      <Balance title="Total Endowment Account" amount={lockedAmount} />
-      <Balance title="Total Liquid Account" amount={liquidAmount} />
+      <QueryLoader
+        queryState={queryState}
+        messages={{
+          loading: "Fetching endowment balances",
+          error: "Failed to get endowment balances ",
+        }}
+        classes={{ container: CONTAINER_STYLE }}
+      >
+        {({ tokens_on_hand }) => (
+          <Balance title="Total Value" amount={getTotal(tokens_on_hand)} />
+        )}
+      </QueryLoader>
+      <QueryLoader
+        queryState={queryState}
+        messages={{
+          loading: "Fetching endowment balances",
+          error: "Failed to get endowment balances ",
+        }}
+        classes={{ container: CONTAINER_STYLE }}
+      >
+        {({ tokens_on_hand }) => (
+          <Balance
+            title="Total Endowment Account"
+            amount={getLocked(tokens_on_hand)}
+          />
+        )}
+      </QueryLoader>
+      <QueryLoader
+        queryState={queryState}
+        messages={{
+          loading: "Fetching endowment balances",
+          error: "Failed to get endowment balances ",
+        }}
+        classes={{ container: CONTAINER_STYLE }}
+      >
+        {({ tokens_on_hand }) => (
+          <Balance
+            title="Total Liquid Account"
+            amount={getLiquid(tokens_on_hand)}
+          />
+        )}
+      </QueryLoader>
     </div>
   );
 }
 
 function Balance(props: { title: string; amount: number | string }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 h-20 w-full py-4 rounded border border-gray-l2 dark:bg-blue-d6 dark:border-bluegray md:items-start md:h-28 md:px-6 md:py-04">
+    <div className={CONTAINER_STYLE}>
       <h6 className="font-heading font-bold text-xs tracking-wider uppercase">
         {props.title}
       </h6>
@@ -53,5 +70,21 @@ function Balance(props: { title: string; amount: number | string }) {
         ${humanize(condense(props.amount))}
       </p>
     </div>
+  );
+}
+
+function getTotal(balance: BalanceInfo): number {
+  return getLocked(balance) + getLiquid(balance);
+}
+
+function getLocked({ locked }: BalanceInfo): number {
+  return Number(
+    locked.native.find((bal) => bal.denom === axlUSDCDenom)?.amount ?? "0"
+  );
+}
+
+function getLiquid({ liquid }: BalanceInfo): number {
+  return Number(
+    liquid.native.find((bal) => bal.denom === axlUSDCDenom)?.amount ?? "0"
   );
 }
