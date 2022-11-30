9fe310f06fb8d467dd2c43c36d87a0038f82aaea
shonmisic <n.misic@outlook.com>
2022-11-30 18:15:10 +0100

    Shuffle things around

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -1,14 +1,54 @@
+import Breadcrumbs from "components/Breadcrumbs";
+import ExtLink from "components/ExtLink";
+import Icon from "components/Icon";
+import { appRoutes } from "constants/routes";
+import { useProfileContext } from "../ProfileContext";
+import DonateButton from "./DonateButton";
 import GeneralInfo from "./GeneralInfo";
-import NameAddressSection from "./NameAddressSection";
-import UrlDonateSection from "./UrlDonateSection";
 
 export default function Body() {
+  const profile = useProfileContext();
+
   return (
     <div className="flex justify-center items-center w-full h-full">
       <div className="padded-container grid gap-8 justify-items-center w-full h-full pt-32 pb-8 xl:grid-rows-[auto_auto_1fr] xl:grid-cols-[auto_auto] xl:justify-items-start xl:items-end xl:gap-16 xl:pt-6 xl:pb-20">
-        <UrlDonateSection className="order-2 xl:order-1 xl:col-span-2" />
+        <Breadcrumbs
+          className="font-body font-normal text-xs sm:text-sm text-gray-d2 dark:text-white xl:ml-52 xl:mr-auto"
+          items={[
+            { title: "Home", to: appRoutes.marketplace },
+            { title: profile.name, to: `${appRoutes.profile}/${profile.id}` },
+          ]}
+        />
+        <DonateButton className="order-4 xl:order-2" />
 
-        <NameAddressSection className="order-1 xl:order-2 xl:pl-2" />
+        <div className="order-2 xl:order-3 xl:pl-2">
+          <p className="font-header font-bold text-2xl max-w-[320px] truncate sm:text-3xl">
+            {profile.name}
+          </p>
+          <p>some text</p>
+        </div>
+
+        <div className="order-3 xl:order-4 flex flex-col gap-4">
+          <span className="flex items-center justify-center gap-2 text-sm sm:text-base font-work uppercase">
+            <Icon type="MapPin" className="h-5 w-5 sm:h-6 sm:w-6 text-orange" />
+            {profile.street_address}
+          </span>
+          {profile.url && (
+            <span className="flex items-center justify-center gap-2 w-full font-sans font-medium text-sm sm:w-auto sm:text-base">
+              <Icon
+                type="Globe"
+                className="h-5 w-5 sm:h-6 sm:w-6 text-orange"
+              />
+              <ExtLink
+                href={profile.url}
+                title="organization url"
+                className="cursor-pointer hover:underline"
+              >
+                {profile.url.replace(/^https?:\/\//i, "")}
+              </ExtLink>
+            </span>
+          )}
+        </div>
 
         <GeneralInfo className="order-4 xl:col-span-2 w-full h-full" />
       </div>
--- src/pages/Profile/Body/DonateButton.tsx
+++ src/pages/Profile/Body/DonateButton.tsx
@@ -2,14 +2,18 @@ import { BtnLink } from "components/BtnLink";
 import { appRoutes } from "constants/routes";
 import { useProfileContext } from "../ProfileContext";
 
-export default function DonateButton() {
+export default function DonateButton({
+  className = "",
+}: {
+  className?: string;
+}) {
   const profile = useProfileContext();
 
   return (
     <BtnLink
       as="link"
       to={appRoutes.donate + `/${profile.id}`}
-      className="btn btn-orange w-full h-12 max-w-xs px-6 rounded text-base"
+      className={`${className} btn btn-orange w-full h-12 max-w-xs px-6 rounded text-base`}
     >
       Donate now
     </BtnLink>
