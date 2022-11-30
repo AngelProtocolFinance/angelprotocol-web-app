bf37c286b5296359b7c277491c39933d05fe1328
personal <alihayder19@gmail.com>
2022-11-30 18:32:16 +0600

    Remove Unused Function

--- src/helpers/hasBG.ts
+++
@@ -1,8 +0,0 @@
-import { Location } from "react-router-dom";
-import { appRoutes } from "constants/routes";
-
-export default function hasBg(location: Location) {
-  return [appRoutes.register, appRoutes.leaderboard, appRoutes.donations].some(
-    (route) => location.pathname.includes(route)
-  );
-}
