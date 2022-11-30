ba10a6c6f790609aabd189819d851f91c10fd95a
shonmisic <n.misic@outlook.com>
2022-11-30 15:02:55 +0100

    Refactor maskAddress to accept custom no. of chars

--- src/helpers/maskAddress.ts
+++ src/helpers/maskAddress.ts
@@ -1,10 +1,9 @@
-export function maskAddress(addr?: string) {
-  const nChars = 6;
+export function maskAddress(addr: string | undefined, numOfChars = 6): string {
   if (!addr) {
     return "";
   } else {
     const len = addr.length;
-    const middle = addr.substring(nChars, len - nChars);
+    const middle = addr.substring(numOfChars, len - numOfChars);
     return addr.replace(middle, "...");
   }
 }
