def0de8cec04f6806c0d29b241a372974de8f465
shonmisic <n.misic@outlook.com>
2022-11-30 15:30:52 +0100

    Extract explorers from chains data

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -12,15 +12,26 @@ export enum chainIDs {
 
 type Info = { txExplorer: string };
 
+const explorers: { [key in chainIDs]: string } = {
+  1: "https://etherscan.io",
+  5: "https://goerli.etherscan.io",
+  97: "https://testnet.bscscan.com",
+  56: "https://bscscan.com",
+  "juno-1": "https://www.mintscan.io/juno",
+  "uni-5": "https://testnet.mintscan.io/juno-testnet",
+  "phoenix-1": "https://finder.terra.money/mainnet",
+  "pisco-1": "https://finder.terra.money/testnet",
+};
+
 const _chains: { [key in chainIDs]: Info } = {
-  1: { txExplorer: "https://etherscan.io/tx" },
-  5: { txExplorer: "https://goerli.etherscan.io/tx" },
-  97: { txExplorer: "https://testnet.bscscan.com/tx" },
-  56: { txExplorer: "https://bscscan.com/tx" },
-  "juno-1": { txExplorer: "https://www.mintscan.io/juno/txs" },
-  "uni-5": { txExplorer: "https://testnet.mintscan.io/juno-testnet/txs" },
-  "phoenix-1": { txExplorer: "https://finder.terra.money/mainnet/tx" },
-  "pisco-1": { txExplorer: "https://finder.terra.money/testnet/tx" },
+  1: { txExplorer: `${explorers[1]}/tx` },
+  5: { txExplorer: `${explorers[5]}/tx` },
+  97: { txExplorer: `${explorers[97]}/tx` },
+  56: { txExplorer: `${explorers[56]}/tx` },
+  "juno-1": { txExplorer: `${explorers["juno-1"]}/txs` },
+  "uni-5": { txExplorer: `${explorers["uni-5"]}/txs` },
+  "phoenix-1": { txExplorer: `${explorers["phoenix-1"]}/tx` },
+  "pisco-1": { txExplorer: `${explorers["pisco-1"]}/tx` },
 };
 
 export const chains: { [index: string]: Info } = new Proxy(_chains, {
