dd8faa853257fe0d7ac1d9aef5cbfae8b632c68c
ap-justin <justin@angelprotocol.io>
2022-12-01 15:35:00 +0800

    gift card contract

--- src/pages/Gift/Purchase/Submit/estimateTx.ts
+++ src/pages/Gift/Purchase/Submit/estimateTx.ts
@@ -1,13 +1,15 @@
+import { MsgExecuteContractEncodeObject } from "@cosmjs/cosmwasm-stargate";
 import { ConnectedWallet } from "@terra-money/wallet-provider";
 import { Estimate } from "./types";
 import { WalletState } from "contexts/WalletContext";
 import { SubmitStep } from "slices/gift";
 import CW20 from "contracts/CW20";
-import { extractFeeAmount, logger } from "helpers";
-import { ap_wallets } from "constants/ap_wallets";
+import GiftCard from "contracts/GiftCard";
+import { extractFeeAmount, logger, scaleToStr } from "helpers";
+import { contracts } from "constants/contracts";
 
 export async function estimateTx({
-  details: { token },
+  details: { token, recipient },
   wallet,
 }: SubmitStep & {
   wallet: WalletState;
@@ -16,12 +18,22 @@ export async function estimateTx({
   const { chain } = wallet;
   const { native_currency } = chain;
   try {
-    const contract = new CW20(wallet, token.token_id);
-    const msg = contract.createTransferMsg(
-      token.amount,
-      ap_wallets.juno_deposit
-    );
-    const fee = await contract.estimateFee([msg]);
+    const gcContract = new GiftCard(wallet);
+    let msg: MsgExecuteContractEncodeObject;
+    if (token.type === "juno-native" || token.type === "ibc") {
+      msg = gcContract.createDepositMsg(recipient, [
+        { amount: scaleToStr(token.amount), denom: token.token_id },
+      ]);
+      /** cw20: this estimate should not run when chain is not juno */
+    } else {
+      const contract = new CW20(wallet, token.token_id);
+      msg = contract.createSendMsg(
+        token.amount,
+        contracts.gift_cards,
+        gcContract.createDepositObject(recipient)
+      );
+    }
+    const fee = await gcContract.estimateFee([msg]);
     const feeAmount = extractFeeAmount(fee, native_currency.token_id);
 
     return {
