20a599c88c4126a6d2abd297fb0e97ea8414663f
shonmisic <n.misic@outlook.com>
2022-12-01 06:38:42 +0100

    Add dark hover to Bookmark

--- src/App/WalletSuite/ConnectedWallet/Details/Favourites/Bookmark.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/Favourites/Bookmark.tsx
@@ -8,7 +8,7 @@ export default function Bookmark({ name, id, logo }: EndowmentBookmark) {
     <li>
       <Link
         to={appRoutes.profile + "/" + id}
-        className="flex items-center gap-2 py-1 font-heading font-semibold text-sm hover:bg-orange-l5"
+        className="flex items-center gap-2 py-1 font-heading font-semibold text-sm hover:bg-orange-l5 dark:hover:bg-blue-d3"
       >
         <Logo src={logo} className="w-4 h-4" />
         <span className="truncate max-w-[200px]">{name}</span>
