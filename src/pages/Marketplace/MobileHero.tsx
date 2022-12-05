27431a7fcd6e71534bbb5f271b7c531c1078ab12
personal <alihayder19@gmail.com>
2022-12-05 19:25:43 +0600

    Update Mobile Hero Image

--- src/pages/Marketplace/MobileHero.tsx
+++ src/pages/Marketplace/MobileHero.tsx
@@ -1,4 +1,4 @@
-import character from "assets/images/character2.png";
+import character from "assets/images/character4.png";
 
 export default function MobileHero({ classes = "" }: { classes?: string }) {
   return (
@@ -9,12 +9,12 @@ export default function MobileHero({ classes = "" }: { classes?: string }) {
         <div className="leading-tight">ANGEL PROTOCOL SUPPORTS</div>
         <div className="leading-tight text-orange">DISPLACED UKRAINIANS.</div>
       </h1>
-      <p className="text-center text-lg max-w-xl leading-relaxed mt-4 -mb-6">
+      <p className="text-center text-lg max-w-xl leading-relaxed mt-4 mb-6">
         Ongoing conflict has led to a humanitarian crisis in Ukraine that could
         displace over 7 million people. Fund relief &amp; refugee efforts by
         donating crypto assets today!
       </p>
-      <img src={character} alt="" className="w-full max-w-md" />
+      <img src={character} alt="" className="w-full max-w-md -scale-x-100" />
     </div>
   );
 }
