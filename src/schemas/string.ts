dc0642cfdb94dc67e5064e9c81c4f39f2ff1bd9f
Nenad <nenad@angelprotocol.io>
2022-12-06 14:54:52 +0100

    Create more precise Yup url validator

--- src/schemas/string.ts
+++ src/schemas/string.ts
@@ -29,7 +29,12 @@ export const requiredWalletAddr = (network: string = chainIds.juno) => {
   );
 };
 
-export const url = Yup.string().url("invalid url").nullable();
+export const url = Yup.string()
+  .matches(
+    /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+    "invalid url"
+  )
+  .nullable();
 
 export const stringByteSchema = (minBytes: number, maxBytes: number) =>
   Yup.string()
