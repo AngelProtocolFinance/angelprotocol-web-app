77e0af3a1b5c7ff88bdaecb66cb2c1e33c5adbe2
ap-justin <89639563+ap-justin@users.noreply.github.com>
2022-12-06 15:09:50 +0800

    add loader (#1532)

--- src/components/registration/index.ts
+++ src/components/registration/index.ts
@@ -3,5 +3,6 @@ export * from "./BtnSec";
 export * from "./constants";
 export * from "./FileDropzone";
 export * from "./TextInput";
+export * from "./LoadText";
 export * from "./Selector";
 export * from "./Separator";
