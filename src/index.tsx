472e75a0ad9ff19b5e7217aa1059cb2d44d6fee7
ap-justin <justin@angelprotocol.io>
2022-12-06 16:24:48 +0800

    fix lint error

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { WalletProvider } from "@terra-money/wallet-provider";
 import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
