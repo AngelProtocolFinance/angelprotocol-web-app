aeee15a36b08a3c1e9a6e400f215343b4090c239
N. M. Ali Hayder <alihayder19@gmail.com>
2022-12-09 05:48:46 +0600

    Review Footer (#1539)
    * WIP
    
    * Update Footer Links
    
    * Adjust Font Size
    
    * Fix Footer Bottom Padding
    
    * Update Fonts Weight

--- src/App/Footer/Links.tsx
+++ src/App/Footer/Links.tsx
@@ -1,13 +1,13 @@
 import { SECTIONS_DATA } from "./constants";
 
-const linkStyles = "font-body font-semibold text-sm";
+const linkStyles = "font-body font-semibold text-xs";
 
 export default function Links() {
   return (
     <div className="grid grid-rows-2 grid-cols-2 gap-4 w-4/5 max-w-[38rem] md:grid-rows-1 md:grid-cols-4 lg:w-full">
       {SECTIONS_DATA.map(({ title, links }) => (
         <div key={title} className="flex flex-col items-start gap-4">
-          <h6 className="font-heading font-black text-base uppercase">
+          <h6 className="font-heading text-sm font-bold uppercase leading-6">
             {title}
           </h6>
           <div className="flex flex-col items-start h-full gap-1">
@@ -25,7 +25,7 @@ function Link(props: { text: string; href?: string }) {
   return props.href ? (
     <a
       href={props.href}
-      className={`${linkStyles} cursor-pointer hover:text-orange-l1 active:text-orange transition ease-in-out duration-300`}
+      className={`${linkStyles} cursor-pointer transition ease-in-out duration-300`}
     >
       {props.text}
     </a>
--- src/App/Footer/Newsletter/Newsletter.tsx
+++ src/App/Footer/Newsletter/Newsletter.tsx
@@ -2,12 +2,12 @@ import SubscriptionForm from "./SubscriptionForm";
 
 export default function Newsletter() {
   return (
-    <div className="flex flex-col items-center gap-6 w-full px-1 md:w-4/5 md:px-0 md:gap-2.5 lg:w-full lg:max-w-xs xl:max-w-md">
-      <div className="flex flex-col items-center w-full md:w-4/5 md:items-start lg:w-full">
-        <h6 className="font-header font-black text-base uppercase text-center md:text-left">
+    <div className="flex flex-col items-center gap-6 w-full px-1 lg:w-full lg:max-w-xs xl:max-w-md">
+      <div className="flex flex-col items-center lg:items-start w-[80vw] lg:w-full">
+        <h6 className="font-header font-bold text-sm uppercase">
           Subscribe to our newsletter
         </h6>
-        <p className="font-body font-normal text-sm text-center md:text-left">
+        <p className="font-body font-normal text-xs text-center lg:text-left">
           By subscribing to this newsletter you confirm that you have read and
           agree with our{" "}
           <a
