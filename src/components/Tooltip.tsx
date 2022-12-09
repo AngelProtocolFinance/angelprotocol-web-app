2a4ece414b8b1129e4acd94ef765dba759571af9
Nenad <nenad@angelprotocol.io>
2022-12-09 08:35:27 +0100

    Add transition

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,3 +1,4 @@
+import { Transition } from "@headlessui/react";
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { logger } from "helpers";
@@ -35,20 +36,23 @@ export default function Tooltip({ anchorId, content }: Props) {
     };
   }, [anchorId]);
 
-  if (!isHovered) {
-    return null;
-  }
-
   return createPortal(
-    <span
-      className={`absolute -translate-x-1/2 -translate-y-full -mt-2 ml-2.5 z-50 flex items-center justify-center px-3 py-2 rounded bg-gray-d2 dark:bg-white font-normal font-body text-sm w-max max-w-[200px] text-white dark:text-gray-d2 after:content-[''] after:absolute after:top-full after:left-1/2 after:-ml-1 after:-translate-y-1/2 after:w-2 after:h-2 after:bg-gray-d2 after:dark:bg-white after:rotate-45`}
+    <Transition
+      show={isHovered}
+      enter="transition-opacity duration-500"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-150"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+      className="absolute -translate-x-1/2 -translate-y-full -mt-2 ml-2.5 z-50 flex items-center justify-center px-3 py-2 rounded bg-gray-d2 dark:bg-white font-normal font-body text-sm w-max max-w-[200px] text-white dark:text-gray-d2 after:content-[''] after:absolute after:top-full after:left-1/2 after:-ml-1 after:-translate-y-1/2 after:w-2 after:h-2 after:bg-gray-d2 after:dark:bg-white after:rotate-45"
       style={{
         top: `${Math.floor(top)}px`,
         left: `${Math.floor(left)}px`,
       }}
     >
       {content}
-    </span>,
+    </Transition>,
     document.body
   );
 }
