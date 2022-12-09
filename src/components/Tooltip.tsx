eda86a749868dc7112a675d33f8b3aacfb11baca
Nenad <nenad@angelprotocol.io>
2022-12-09 08:03:41 +0100

    Update tooltip position

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -37,8 +37,11 @@ function Content({
 }) {
   return (
     <span
-      className={`absolute z-50 flex items-center justify-center px-3 py-2 rounded bg-gray-d2 dark:bg-white font-normal font-body text-sm w-max max-w-[200px] text-white dark:text-gray-d2 after:content-[''] after:absolute after:top-full after:left-1/2 after:-ml-2 after:-translate-y-1/2 after:w-2 after:h-2 after:bg-gray-d2 after:dark:bg-white after:rotate-45`}
-      style={{ top: `${Math.floor(top)}px`, left: `${Math.floor(left)}px` }}
+      className={`absolute -translate-x-1/2 -translate-y-full -mt-2 ml-2.5 z-50 flex items-center justify-center px-3 py-2 rounded bg-gray-d2 dark:bg-white font-normal font-body text-sm w-max max-w-[200px] text-white dark:text-gray-d2 after:content-[''] after:absolute after:top-full after:left-1/2 after:-ml-1 after:-translate-y-1/2 after:w-2 after:h-2 after:bg-gray-d2 after:dark:bg-white after:rotate-45`}
+      style={{
+        top: `${Math.floor(top)}px`,
+        left: `${Math.floor(left)}px`,
+      }}
     >
       {text}
     </span>
--- src/pages/Marketplace/Cards/Card.tsx
+++ src/pages/Marketplace/Cards/Card.tsx
@@ -16,7 +16,7 @@ export default function Card({
 }: Endowment) {
   return (
     <div className="relative overflow-clip dark:bg-blue-d6 rounded-lg border border-gray-l2 dark:border-bluegray hover:border-blue dark:hover:border-blue">
-      <div className="absolute top-3 left-3 right-3 flex justify-between">
+      <div className="absolute top-[14px] left-[14px] right-[14px] flex justify-between">
         <p className="bg-orange-l1 text-white font-semibold text-2xs rounded-sm uppercase px-2 py-0.5 font-heading">
           {endow_type === "Charity" ? "Non-profit" : "For-profit"}
         </p>
