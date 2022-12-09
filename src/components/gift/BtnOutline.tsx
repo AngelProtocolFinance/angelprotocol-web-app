15657d10a6937d29345a364519a4a266bfb9d7dd
ap-justin <justin@angelprotocol.io>
2022-12-09 13:33:05 +0800

    component update

--- src/components/gift/BtnOutline.tsx
+++ src/components/gift/BtnOutline.tsx
@@ -4,7 +4,7 @@ export function BtnOutline({ className = "", ...props }: BtnLinkProps) {
   return (
     <BtnLink
       {...props}
-      className={`${className} uppercase p-3 rounded border border-gray-l2 dark:border-bluegray text-center hover:border-gray-l1 hover:dark:border-blue-d2 font-bold font-body`}
+      className={`${className} text-sm uppercase p-3 rounded border border-gray-l2 dark:border-bluegray text-center hover:border-gray-l1 hover:dark:border-blue-d2 font-bold font-body`}
     />
   );
 }
