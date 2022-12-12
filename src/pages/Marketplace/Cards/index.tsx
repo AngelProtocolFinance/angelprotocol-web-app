e8512f38cb42133d111c9c2b6b7a3d15da1e5cd2
Nenad <nenad@angelprotocol.io>
2022-12-12 16:12:20 +0100

    Remove tiers param if empty

--- src/pages/Marketplace/Cards/index.tsx
+++ src/pages/Marketplace/Cards/index.tsx
@@ -19,7 +19,7 @@ export default function Cards({ classes = "" }: { classes?: string }) {
     query: searchText || "matchall",
     sort: sort ? `${sort.key}+${sort.direction}` : "default",
     endow_types: endow_types.join(",") || undefined,
-    tiers: tiers.join(","),
+    tiers: tiers.join(",") || undefined,
     sdgs: selectedSDGs.length > 0 ? selectedSDGs.join(",") : undefined,
     kyc_only: kycOnly.join(",") || undefined,
   });
