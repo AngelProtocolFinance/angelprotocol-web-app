6605b4656bf565a94d29da8be4d5c2654013746a
Andrey <85138450+SovereignAndrey@users.noreply.github.com>
2022-12-12 06:50:47 +0000

    Dynamic SEO meta tags (#1548)
    * dynamic SEO meta tags
    
    * | >> -

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import { WalletProvider } from "@terra-money/wallet-provider";
 import ModalContext from "contexts/ModalContext";
 import WalletContext from "contexts/WalletContext";
+import Seo from "components/Seo";
 import Footer from "./Footer";
 import Header from "./Header";
 import Views from "./Views";
@@ -12,6 +13,7 @@ export default function App() {
       <WalletContext>
         <ModalContext>
           <div className="grid grid-rows-[auto_1fr_auto] bg-gray-l5 dark:bg-blue-d5 text-gray-d2 dark:text-white">
+            <Seo /> {/* Load all defaults for SEO meta tags */}
             <Header classes="sticky top-0 z-20" />
             <Views />
             <Footer />
