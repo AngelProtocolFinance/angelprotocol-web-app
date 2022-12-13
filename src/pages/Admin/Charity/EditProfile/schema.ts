ac03fb9f3878101c1f1af4da862dba76eb13ec8b
Nenad <nenad@angelprotocol.io>
2022-12-13 10:10:07 +0100

    Make banner a required field

--- src/pages/Admin/Charity/EditProfile/schema.ts
+++ src/pages/Admin/Charity/EditProfile/schema.ts
@@ -14,9 +14,13 @@ export const VALID_MIME_TYPES = [
   "image/svg",
 ];
 
-const imgShape: SchemaShape<ImgLink> = {
-  file: genFileSchema(1e6, VALID_MIME_TYPES).optional(),
-};
+const fileObj = Yup.object().shape<SchemaShape<ImgLink>>({
+  file: genFileSchema(1e6, VALID_MIME_TYPES).when("publicUrl", {
+    is: (value: string) => !value,
+    then: (schema) => schema.required(),
+    otherwise: (schema) => schema.optional(),
+  }),
+});
 
 //construct strict shape to avoid hardcoding shape keys
 const shape: SchemaShape<ProfileFormValues> = {
@@ -26,7 +30,7 @@ const shape: SchemaShape<ProfileFormValues> = {
   //sdgNum: no need to validate, selected from dropdown with default value
   //tier: TODO: this field is not touched here for endowment owner, will be added on distinction of config owner
   //logo: no need to validate, url is auto generated
-  image: Yup.object().shape(imgShape),
+  image: fileObj,
   url: url.required("required"),
   // registration_number: no need to validate
   // country_city_origin: no need to validate
