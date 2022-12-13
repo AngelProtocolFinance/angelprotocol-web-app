b5755161e79344d1ea3df4ad33eb94c3951f464d
Nenad <nenad@angelprotocol.io>
2022-12-13 08:43:01 +0100

    Move marketFilter into separate folder

---
+++ src/slices/components/marketFilter/constants.ts
@@ -0,0 +1,63 @@
+import { SdgGroups, Sort } from "types/aws";
+import { CapitalizedEndowmentType, EndowmentTier } from "types/contracts";
+import { UNSDG_NUMS } from "types/lists";
+
+export const SDG_GROUPS: {
+  key: number;
+  label: string;
+  options: UNSDG_NUMS[];
+}[] = [
+  {
+    key: 1,
+    label: "Reducing overall inequality",
+    options: [1, 2, 10],
+  },
+  {
+    key: 2,
+    label: "Access to safe conditions",
+    options: [3, 6, 7],
+  },
+  {
+    key: 3,
+    label: "Sustainable growth",
+    options: [8, 9, 16],
+  },
+  {
+    key: 4,
+    label: "Equality through education",
+    options: [4, 5],
+  },
+  {
+    key: 5,
+    label: "Sustainable partnerships",
+    options: [11, 12, 17],
+  },
+  {
+    key: 6,
+    label: "Holistic climate action",
+    options: [13, 14, 15],
+  },
+];
+
+type State = {
+  isOpen: boolean;
+  searchText: string;
+  endow_types: CapitalizedEndowmentType[];
+  sort?: Sort;
+  //geography
+  sdgGroups: SdgGroups;
+  kyc_only: boolean[];
+  tiers: Exclude<EndowmentTier, "Level1">[];
+};
+
+export const initialState: State = {
+  sdgGroups: SDG_GROUPS.reduce(
+    (prev, curr) => ({ ...prev, [curr.key]: [...curr.options] }),
+    {} as SdgGroups
+  ),
+  isOpen: false,
+  searchText: "",
+  endow_types: ["Charity", "Normal"],
+  kyc_only: [true, false],
+  tiers: ["Level3"],
+};
---
+++ src/slices/components/marketFilter/index.ts
@@ -0,0 +1,3 @@
+export { default } from "./marketFilter";
+export * from "./marketFilter";
+export { SDG_GROUPS } from "./constants";
