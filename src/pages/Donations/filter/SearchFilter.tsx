514ee304fc6ac80ec8a8f1212c35cfe9f7ea5e43
personal <alihayder19@gmail.com>
2022-12-13 20:16:02 +0600

    Disable Apply Filter Button on Blank Data

--- src/pages/Donations/filter/SearchFilter.tsx
+++ src/pages/Donations/filter/SearchFilter.tsx
@@ -16,6 +16,7 @@ const SearchFilter = ({
   address: string | undefined;
 }) => {
   const [startDate, setStartDate] = useState<string>("");
+  const [isNetworkSelected, setIsNetworkSelected] = useState<Boolean>(false);
   const { handleError } = useErrorContext();
   const {
     handleSubmit,
@@ -30,13 +31,13 @@ const SearchFilter = ({
   const { data: networks } = useChainsQuery("");
   // const { data: currencies } = useChainQuery({ address: address });
 
-  async function submit(data: FormValues) {
-    const filters: Filters = {
-      transactionDate: "",
-      chainName: "",
-      denomination: "",
-    };
+  const filters: Filters = {
+    transactionDate: "",
+    chainName: "",
+    denomination: "",
+  };
 
+  async function submit(data: FormValues) {
     !data.startDate || !data.endDate
       ? delete filters.transactionDate
       : (filters.transactionDate = `${data.startDate.toString()} ${data.endDate.toString()}`);
@@ -57,76 +58,98 @@ const SearchFilter = ({
     <div>
       {/* {Mobile} */}
       <Popover className="sm:hidden">
-        <Popover.Button
-          className={
-            "w-full flex justify-center items-center text-white bg-orange p-3 rounded-md"
-          }
-        >
-          <Icon type="Filter" size={20}></Icon>
-          <div className="uppercase font-semibold">Filter</div>
-        </Popover.Button>
+        <form onSubmit={handleSubmit(submit)} method="get">
+          <Popover.Button
+            ref={buttonRef}
+            className={
+              "w-full flex justify-center items-center text-white bg-orange p-3 rounded-md"
+            }
+          >
+            <Icon type="Filter" size={20}></Icon>
+            <div className="uppercase font-semibold">Filter</div>
+          </Popover.Button>
 
-        <Popover.Panel className="fixed top-0 left-0 bg-white dark:bg-blue-d6 min-w-[100vw] min-h-[100vh] z-50 border border-gray-l2 dark:border-bluegray">
-          <div className="flex flex-col">
-            <div className="flex justify-between border-b-[1px] bg-orange-l6 dark:bg-blue-d7 border-gray-l2 dark:border-bluegray">
-              <h2 className="text-xl text-orange font-bold p-5 uppercase">
-                Filters
-              </h2>
-              <Popover.Button className="p-5">
-                <Icon type="Close" size={24} className="text-gray-d2"></Icon>
-              </Popover.Button>
-            </div>
-            <div className="flex justify-end items-center gap-4 bg-orange-l6 dark:bg-blue-d7 border-b-[1px] border-gray-l2 dark:border-bluegray py-3 px-5">
-              <a href="#" className="text-orange underline">
-                Reset filters
-              </a>
-              <button className="flex justify-center items-center text-white bg-orange p-3 rounded-md">
-                Apply filter
-              </button>
-            </div>
-            <div className="overflow-y-scroll">
-              <div className="flex flex-col w-full p-6 gap-6 bg-white dark:bg-blue-d6">
-                <div className="flex flex-col text-gray-d2 gap-2">
-                  <label className="dark:text-white">Date</label>
-                  <div className="flex gap-4">
-                    <input
-                      {...register("startDate")}
-                      type="date"
-                      className="w-full py-3 pl-3 border border-gray-l2 dark:border-bluegray rounded-sm"
-                      placeholder="From"
-                      min="2018-12-31"
-                      max={new Date().toISOString().split("T")[0]}
-                      onChange={(e) => setStartDate(e.target.value)}
-                    />
-                    <input
-                      {...register("endDate")}
-                      type="date"
-                      className="w-full py-3 pl-3 border border-gray-l2 dark:border-bluegray rounded-sm"
-                      placeholder="To"
-                      min={startDate}
-                      max={new Date().toISOString().split("T")[0]}
-                      disabled={startDate ? false : true}
-                      defaultValue={startDate && startDate}
-                    />
+          <Popover.Panel className="fixed top-0 left-0 bg-white dark:bg-blue-d6 min-w-[100vw] min-h-[100vh] z-50 border border-gray-l2 dark:border-bluegray">
+            <div className="flex flex-col">
+              <div className="flex justify-between border-b-[1px] bg-orange-l6 dark:bg-blue-d7 border-gray-l2 dark:border-bluegray">
+                <h2 className="text-xl text-orange font-bold p-5 uppercase">
+                  Filters
+                </h2>
+                <Popover.Button className="p-5">
+                  <Icon type="Close" size={24} className="text-gray-d2"></Icon>
+                </Popover.Button>
+              </div>
+              <div className="flex justify-end items-center gap-4 bg-orange-l6 dark:bg-blue-d7 border-b-[1px] border-gray-l2 dark:border-bluegray py-3 px-5">
+                <button
+                  type="button"
+                  className="text-orange underline"
+                  onClick={() => {
+                    reset({
+                      startDate: new Date(),
+                      endDate: new Date(),
+                      network: "",
+                      currency: "",
+                    });
+                    updateFilterValues(filters);
+                  }}
+                >
+                  Reset filters
+                </button>
+                <button
+                  type="submit"
+                  className="flex justify-center items-center text-white bg-orange p-3 rounded-md disabled:bg-gray"
+                  disabled={startDate || isNetworkSelected ? false : true}
+                >
+                  Apply filter
+                </button>
+              </div>
+              <div className="overflow-y-scroll">
+                <div className="flex flex-col w-full p-6 gap-6 bg-white dark:bg-blue-d6">
+                  <div className="flex flex-col text-gray-d2 gap-2">
+                    <label className="dark:text-white">Date</label>
+                    <div className="flex gap-4">
+                      <input
+                        {...register("startDate")}
+                        type="date"
+                        className="w-full py-3 pl-3 border border-gray-l2 dark:border-bluegray rounded-sm"
+                        placeholder="From"
+                        min="2018-12-31"
+                        max={new Date().toISOString().split("T")[0]}
+                        onChange={(e) => setStartDate(e.target.value)}
+                      />
+                      <input
+                        {...register("endDate")}
+                        type="date"
+                        className="w-full py-3 pl-3 border border-gray-l2 dark:border-bluegray rounded-sm"
+                        placeholder="To"
+                        min={startDate}
+                        max={new Date().toISOString().split("T")[0]}
+                        disabled={startDate ? false : true}
+                        defaultValue={startDate && startDate}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-col text-gray-d2 gap-2">
-                  <label className="dark:text-white">Network</label>
-                  <select
-                    {...register("network")}
-                    className={
-                      "inline-flex w-full justify-between items-center border border-gray-l2 dark:border-bluegray rounded-sm p-3"
-                    }
-                  >
-                    <option value="">Select a network...</option>
-                    {networks?.map((network) => (
-                      <option key={network.chain_id} value={network.chain_name}>
-                        {network.chain_name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                {/* <div className="flex flex-col text-gray-d2 gap-2">
+                  <div className="flex flex-col text-gray-d2 gap-2">
+                    <label className="dark:text-white">Network</label>
+                    <select
+                      {...register("network")}
+                      className={
+                        "inline-flex w-full justify-between items-center border border-gray-l2 dark:border-bluegray rounded-sm p-3"
+                      }
+                      onChange={() => setIsNetworkSelected(true)}
+                    >
+                      <option value="">Select a network...</option>
+                      {networks?.map((network) => (
+                        <option
+                          key={network.chain_id}
+                          value={network.chain_name}
+                        >
+                          {network.chain_name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  {/* <div className="flex flex-col text-gray-d2 gap-2">
                   <label className="dark:text-white">Currency</label>
                   <Listbox>
                     <Listbox.Button
@@ -149,10 +172,11 @@ const SearchFilter = ({
                     </Listbox.Options>
                   </Listbox>
                 </div> */}
+                </div>
               </div>
             </div>
-          </div>
-        </Popover.Panel>
+          </Popover.Panel>
+        </form>
       </Popover>
 
       {/* {Desktop} */}
@@ -212,6 +236,7 @@ const SearchFilter = ({
                     <label className="dark:text-white">Network</label>
                     <select
                       {...register("network")}
+                      onChange={() => setIsNetworkSelected(true)}
                       className={
                         "inline-flex w-full justify-between items-center border border-gray-l2 dark:border-bluegray rounded-sm p-3"
                       }
@@ -248,12 +273,25 @@ const SearchFilter = ({
                   </div> */}
                 </div>
                 <div className="flex justify-end items-center gap-4 bg-orange-l6 dark:bg-blue-d7 border-b-[1px] border-gray-l2 dark:border-bluegray py-3 px-5">
-                  <button type="button" className="text-orange underline">
+                  <button
+                    type="button"
+                    className="text-orange underline"
+                    onClick={() => {
+                      reset({
+                        startDate: new Date(),
+                        endDate: new Date(),
+                        network: "",
+                        currency: "",
+                      });
+                      updateFilterValues(filters);
+                    }}
+                  >
                     Reset filters
                   </button>
                   <button
                     type="submit"
-                    className="flex justify-center items-center text-white bg-orange p-3 rounded-md"
+                    className="flex justify-center items-center text-white bg-orange p-3 rounded-md disabled:bg-gray"
+                    disabled={startDate || isNetworkSelected ? false : true}
                   >
                     Apply filter
                   </button>
