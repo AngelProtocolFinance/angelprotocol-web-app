21ead0c3a5a75c291764da1b544be37b9b65a501
Nenad <nenad@angelprotocol.io>
2022-12-14 12:50:17 +0100

    Implement MyEndowments links

--- src/App/WalletSuite/ConnectedWallet/Details/MyEndowments/MyEndowments.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/MyEndowments/MyEndowments.tsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 import { EndowmentBookmark } from "types/aws";
+import { appRoutes } from "constants/routes";
 import Logo from "../Logo";
-import Links from "./Links";
 
 type Props = { endowments?: EndowmentBookmark[] };
 
@@ -11,13 +12,23 @@ export default function MyEndowments({ endowments = [] }: Props) {
         My Endowments
       </h3>
       {endowments.map((e) => (
-        <div className="grid grid-cols-[auto_1fr] gap-3">
+        <div key={e.id} className="grid grid-cols-[auto_1fr] gap-3">
           <Logo src={e.logo} className="w-10 h-10" />
           <div className="grid items-center">
             {/* Will be added once possible to fetch endowment profile by wallet address */}
             <span className="font-heading font-semibold text-sm">{e.name}</span>
 
-            <Links />
+            <div className="flex items-center uppercase font-heading font-semibold text-xs underline underline-offset-2 decoration-1 text-orange">
+              <Link to={`${appRoutes.profile}/${e.id}`} className="pr-2">
+                profile
+              </Link>
+              <Link
+                to={`${appRoutes.admin}/${e.id}`}
+                className="px-2 border-l border-gray-l2 dark:border-bluegray"
+              >
+                admin
+              </Link>
+            </div>
           </div>
         </div>
       ))}
