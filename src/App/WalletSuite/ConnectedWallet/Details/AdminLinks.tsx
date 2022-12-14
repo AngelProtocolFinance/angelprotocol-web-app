e42d1b963cefb70158d3755b2ac9099b475dac35
Nenad <nenad@angelprotocol.io>
2022-12-14 13:20:54 +0100

    Fix hover on specific link

--- src/App/WalletSuite/ConnectedWallet/Details/AdminLinks.tsx
+++ src/App/WalletSuite/ConnectedWallet/Details/AdminLinks.tsx
@@ -10,16 +10,19 @@ export default function AdminLinks(props: Props) {
       <h3 className="font-heading font-bold text-sm text-gray-d1 dark:text-gray">
         Platform Administration
       </h3>
-      <div className="flex items-center uppercase font-heading font-semibold text-xs underline underline-offset-2 decoration-1 text-orange hover:text-orange-l2">
+      <div className="flex items-center uppercase font-heading font-semibold text-xs underline underline-offset-2 decoration-1">
         {props.isApMember && (
-          <Link to={`${appRoutes.admin}/${AP_ID}`} className="pr-2">
+          <Link
+            to={`${appRoutes.admin}/${AP_ID}`}
+            className="pr-2 text-orange hover:text-orange-l2"
+          >
             ap admin
           </Link>
         )}
         {props.isReviewMember && (
           <Link
             to={`${appRoutes.admin}/${REVIEWER_ID}`}
-            className="pl-2 border-l border-gray-l2 dark:border-bluegray"
+            className="pl-2 border-l border-gray-l2 dark:border-bluegray text-orange hover:text-orange-l2"
           >
             applications
           </Link>
