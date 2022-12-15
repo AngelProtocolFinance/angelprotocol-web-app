af962cd07ab42a80822d35f1e7ae403ccc65001c
personal <alihayder19@gmail.com>
2022-12-16 02:24:45 +0600

    Fix Height of the Filter Button on Blank Page

--- src/components/donations/FilterForm.tsx
+++ src/components/donations/FilterForm.tsx
@@ -46,7 +46,7 @@ const FilterForm = ({
   }
 
   return (
-    <Popover className="sm:block sm:relative sm:py-3 sm:px-4 mt-6 sm:mt-0 border border-gray-l2 dark:border-bluegray rounded-md sm:rounded-sm dark:bg-blue-d6">
+    <Popover className="sm:block sm:relative sm:py-3 sm:px-4 mt-6 sm:mt-0 sm:max-h-[3.1rem] border border-gray-l2 dark:border-bluegray rounded-md sm:rounded-sm dark:bg-blue-d6">
       <Popover.Button
         ref={buttonRef}
         className={
