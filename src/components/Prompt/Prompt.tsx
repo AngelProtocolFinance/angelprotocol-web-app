815608b590770e030099321c4e21602163908540
ap-justin <justin@angelprotocol.io>
2022-12-15 12:51:40 +0800

    prompt content

--- src/components/Prompt/Prompt.tsx
+++ src/components/Prompt/Prompt.tsx
@@ -5,7 +5,7 @@ import Icon from "../Icon";
 import LoaderRing from "../LoaderRing";
 
 export default function Prompt({ type, headline, title, children }: Props) {
-  const { closeModal } = useModalContext();
+  const { closeModal, isDismissible } = useModalContext();
 
   return (
     <Dialog.Panel className="fixed-center z-10 grid text-gray-d2 dark:text-white bg-white dark:bg-blue-d4 sm:w-full w-[90vw] sm:max-w-lg rounded overflow-hidden">
@@ -14,10 +14,11 @@ export default function Prompt({ type, headline, title, children }: Props) {
           {headline}
         </p>
         <button
+          disabled={!isDismissible}
           onClick={() => closeModal()}
-          className="border border-gray-l2 dark:border-bluegray p-2 rounded-md absolute top-1/2 right-4 transform -translate-y-1/2"
+          className="border border-gray-l2 dark:border-bluegray p-2 rounded-md absolute top-1/2 right-4 transform -translate-y-1/2 disabled:text-gray-l2 dark:disabled:text-bluegray-d1 disabled:dark:border-bluegray-d1"
         >
-          <Icon type="Close" size={24} className="dark:text-white" />
+          <Icon type="Close" size={24} />
         </button>
       </div>
       <PromptIcon type={type} classes="mb-6 sm:mb-8 mt-4 sm:mt-12" />
@@ -29,14 +30,16 @@ export default function Prompt({ type, headline, title, children }: Props) {
       <div className="px-6 pb-4 text-center text-gray-d1 dark:text-gray">
         {children}
       </div>
-      <div className="p-3 sm:px-8 sm:py-4 w-full text-end bg-orange-l6 dark:bg-blue-d7 border-t border-gray-l2 dark:border-bluegray">
-        <button
-          type="button"
-          className="rounded btn-orange font-body px-8 py-2 max-sm:w-full"
-          onClick={() => closeModal()}
-        >
-          {type === "success" ? "Done" : "Ok"}
-        </button>
+      <div className="p-3 sm:px-8 sm:py-4 empty:h-12 w-full text-end bg-orange-l6 dark:bg-blue-d7 border-t border-gray-l2 dark:border-bluegray">
+        {isDismissible && (
+          <button
+            type="button"
+            className="rounded btn-orange font-body px-8 py-2 max-sm:w-full"
+            onClick={() => closeModal()}
+          >
+            {type === "success" ? "Done" : "Ok"}
+          </button>
+        )}
       </div>
     </Dialog.Panel>
   );
@@ -61,6 +64,6 @@ function PromptIcon({
         />
       );
     default:
-      return <LoaderRing thickness={4} classes={common + ""} />;
+      return <LoaderRing thickness={12} classes={common + " h-24"} />;
   }
 }
--- src/components/Prompt/TxPrompt.tsx
+++ src/components/Prompt/TxPrompt.tsx
@@ -7,29 +7,32 @@ import {
   isLoading,
   isSuccess,
 } from "hooks/useCosmosTxSender";
-import { getTxUrl, maskAddress } from "helpers";
+import { getTxUrl } from "helpers";
 import Prompt from "./Prompt";
 
 export function TxPrompt(props: TxState) {
   const { message, ...rest } = toPrompt(props);
   return (
     <Prompt {...rest}>
+      <p>{message}</p>
+      {isSuccess(props) && props.success.link && (
+        <>
+          <Link
+            to={props.success.link.url}
+            className="inline-block justify-self-center bg-blue uppercase text-xs py-1 px-2 text-white rounded mt-4"
+          >
+            {props.success.link.description}
+          </Link>
+        </>
+      )}
       {(isError(props) || isSuccess(props)) && props.tx && (
         <>
           <ExtLink
             href={getTxUrl(props.tx.chainID, props.tx.hash)}
-            className="text-blue dark:text-blue-l2 text-sm"
+            className="text-blue dark:text-blue-l2 text-xs block mt-4 uppercase hover:text-blue-l2 hover:dark:text-orange-l2"
           >
-            {maskAddress(props.tx.hash)}
+            transaction details
           </ExtLink>
-        </>
-      )}
-      <p>{message}</p>]
-      {isSuccess(props) && props.success.link && (
-        <>
-          <Link to={props.success.link.url}>
-            {props.success.link.description}
-          </Link>
         </>
       )}
     </Prompt>
@@ -44,10 +47,10 @@ function toPrompt(props: TxState): PromptProps & { message: string } {
       message: props.loading,
     };
   } else if (isError(props)) {
-    return { type: "loading", headline: "Transaction", message: props.error };
+    return { type: "error", headline: "Transaction", message: props.error };
   } else {
     return {
-      type: "loading",
+      type: "success",
       headline: "Transaction",
       message: props.success.message,
     };
