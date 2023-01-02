1e40297de0d14d21cae407420a3f3e11f8f455c6
personal <alihayder19@gmail.com>
2023-01-03 03:57:24 +0600

    Remove Extra Div

--- src/components/donations/Filter/Form.tsx
+++ src/components/donations/Filter/Form.tsx
@@ -20,17 +20,51 @@ type FormProps = {
 
 const Form: FC<FormProps> = (props) => {
   return (
-    <form onSubmit={props.handleSubmit(props.submit)} method="get">
-      <div className="flex flex-col">
-        <div className="flex sm:hidden justify-between border-b-[1px] bg-orange-l6 dark:bg-blue-d7 border-gray-l2 dark:border-bluegray">
-          <h2 className="text-xl text-orange font-bold p-5 uppercase">
-            Filters
-          </h2>
-          <Popover.Button className="p-5">
-            <Icon type="Close" size={24} className="text-gray-d2" />
-          </Popover.Button>
+    <form
+      className="flex flex-col"
+      onSubmit={props.handleSubmit(props.submit)}
+      method="get"
+    >
+      <div className="flex sm:hidden justify-between border-b-[1px] bg-orange-l6 dark:bg-blue-d7 border-gray-l2 dark:border-bluegray">
+        <h2 className="text-xl text-orange font-bold p-5 uppercase">Filters</h2>
+        <Popover.Button className="p-5">
+          <Icon type="Close" size={24} className="text-gray-d2" />
+        </Popover.Button>
+      </div>
+      <div className="flex sm:hidden justify-end items-center gap-4 bg-orange-l6 dark:bg-blue-d7 border-b-[1px] border-gray-l2 dark:border-bluegray py-3 px-5">
+        <button
+          type="button"
+          className="text-orange underline"
+          onClick={props.formReset}
+        >
+          Reset filters
+        </button>
+        <button
+          type="submit"
+          className="flex justify-center items-center text-white bg-orange p-3 rounded-md disabled:bg-gray"
+          disabled={
+            props.isDirty || props.selectedNetwork || props.selectedCurrency
+              ? false
+              : true
+          }
+        >
+          Apply filter
+        </button>
+      </div>
+      <div>
+        <div className="flex flex-col w-full p-6 gap-6 bg-white dark:bg-blue-d5">
+          <DateRange register={props.register} errors={props.errors} />
+          <NetworkDropdown
+            selectedNetwork={props.selectedNetwork}
+            setSelectedNetwork={props.setSelectedNetwork}
+          />
+
+          <CurrencyDropdown
+            selectedCurrency={props.selectedCurrency}
+            setSelectedCurrency={props.setSelectedCurrency}
+          />
         </div>
-        <div className="flex sm:hidden justify-end items-center gap-4 bg-orange-l6 dark:bg-blue-d7 border-b-[1px] border-gray-l2 dark:border-bluegray py-3 px-5">
+        <div className="hidden sm:flex justify-end items-center gap-4 bg-orange-l6 dark:bg-blue-d7 border-b-[1px] border-gray-l2 dark:border-bluegray py-3 px-5">
           <button
             type="button"
             className="text-orange underline"
@@ -49,40 +83,6 @@ const Form: FC<FormProps> = (props) => {
           >
             Apply filter
           </button>
-        </div>
-        <div>
-          <div className="flex flex-col w-full p-6 gap-6 bg-white dark:bg-blue-d5">
-            <DateRange register={props.register} errors={props.errors} />
-            <NetworkDropdown
-              selectedNetwork={props.selectedNetwork}
-              setSelectedNetwork={props.setSelectedNetwork}
-            />
-
-            <CurrencyDropdown
-              selectedCurrency={props.selectedCurrency}
-              setSelectedCurrency={props.setSelectedCurrency}
-            />
-          </div>
-          <div className="hidden sm:flex justify-end items-center gap-4 bg-orange-l6 dark:bg-blue-d7 border-b-[1px] border-gray-l2 dark:border-bluegray py-3 px-5">
-            <button
-              type="button"
-              className="text-orange underline"
-              onClick={props.formReset}
-            >
-              Reset filters
-            </button>
-            <button
-              type="submit"
-              className="flex justify-center items-center text-white bg-orange p-3 rounded-md disabled:bg-gray"
-              disabled={
-                props.isDirty || props.selectedNetwork || props.selectedCurrency
-                  ? false
-                  : true
-              }
-            >
-              Apply filter
-            </button>
-          </div>
         </div>
       </div>
     </form>
