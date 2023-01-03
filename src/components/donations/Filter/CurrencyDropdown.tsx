cd02b29de5001303c2f75fcae21f6dce45058afa
personal <alihayder19@gmail.com>
2023-01-04 01:06:04 +0600

    Add Loader

--- src/components/donations/Filter/CurrencyDropdown.tsx
+++ src/components/donations/Filter/CurrencyDropdown.tsx
@@ -10,7 +10,7 @@ const CurrencyDropdown: FC<CurrencyDropdownProps> = ({
   selectedCurrency,
   setSelectedCurrency,
 }) => {
-  const { data: currencies } = useCurrenciesQuery();
+  const { data: currencies, isLoading } = useCurrenciesQuery();
 
   return (
     <div className="flex flex-col text-gray-d2 gap-2">
@@ -22,12 +22,18 @@ const CurrencyDropdown: FC<CurrencyDropdownProps> = ({
           "inline-flex w-full justify-between items-center border border-gray-l2 dark:border-bluegray rounded-sm p-3 dark:text-gray dark:bg-blue-d6 dark:placeholder:text-gray"
         }
       >
-        <option value="">Select a currency...</option>
-        {currencies?.map((currency) => (
-          <option key={currency.token_id} value={currency.symbol}>
-            {currency.symbol}
-          </option>
-        ))}
+        {isLoading ? (
+          <option value="">Loading...</option>
+        ) : (
+          <>
+            <option value="">Select a currency...</option>
+            {currencies?.map((currency) => (
+              <option key={currency.token_id} value={currency.symbol}>
+                {currency.symbol}
+              </option>
+            ))}
+          </>
+        )}
       </select>
     </div>
   );
--- src/components/donations/Filter/NetworkDropdown.tsx
+++ src/components/donations/Filter/NetworkDropdown.tsx
@@ -10,7 +10,7 @@ const NetworkDropdown: FC<NetworkDropdownProps> = ({
   selectedNetwork,
   setSelectedNetwork,
 }) => {
-  const { data: networks } = useChainsQuery("");
+  const { data: networks, isLoading } = useChainsQuery("");
   return (
     <div className="flex flex-col text-gray-d2 gap-2">
       <label className="dark:text-white">Network</label>
@@ -21,12 +21,18 @@ const NetworkDropdown: FC<NetworkDropdownProps> = ({
           "inline-flex w-full justify-between items-center border border-gray-l2 dark:border-bluegray rounded-sm p-3 dark:text-gray dark:bg-blue-d6 dark:placeholder:text-gray"
         }
       >
-        <option value="">Select a network...</option>
-        {networks?.map((network) => (
-          <option key={network.chain_id} value={network.chain_name}>
-            {network.chain_name}
-          </option>
-        ))}
+        {isLoading ? (
+          <option value="">Loading...</option>
+        ) : (
+          <>
+            <option value="">Select a network...</option>
+            {networks?.map((network) => (
+              <option key={network.chain_id} value={network.chain_name}>
+                {network.chain_name}
+              </option>
+            ))}
+          </>
+        )}
       </select>
     </div>
   );
