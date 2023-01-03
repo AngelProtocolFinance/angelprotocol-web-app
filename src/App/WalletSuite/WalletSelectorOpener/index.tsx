a52555291f76d37d371276a573fc003d36c56799
Nenad Misic <nenad@angelprotocol.io>
2023-01-03 15:54:26 +0100

    Update connect btn font and gap (#1589)
    * Reduce connect btn font size
    
    * Increase connect btn gap

--- src/App/WalletSuite/WalletSelectorOpener/index.tsx
+++ src/App/WalletSuite/WalletSelectorOpener/index.tsx
@@ -12,7 +12,7 @@ export default function WalletSelectorOpener(props: Props) {
 
   return (
     <button
-      className={`${COMMON_BUTTON_STYLE} text-base`}
+      className={`${COMMON_BUTTON_STYLE} gap-1 text-sm`}
       disabled={props.isLoading}
       onClick={handleClick}
     >
