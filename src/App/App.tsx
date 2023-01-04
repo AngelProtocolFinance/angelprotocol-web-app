a6acafb2875e774055dbbb488c9e65a4642c23de
ap-justin <justin@angelprotocol.io>
2023-01-04 19:08:06 +0800

    fix test

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -41,7 +41,7 @@ export default function App() {
               draggable={false}
               pauseOnHover
             />,
-            document.getElementById("body")!
+            document.getElementsByTagName("body")[0]
           )}
           <div className="grid grid-rows-[auto_1fr_auto] bg-gray-l5 dark:bg-blue-d5 text-gray-d2 dark:text-white">
             <Seo /> {/* Load all defaults for SEO meta tags */}
