ffcdbab504d95479f0de8f81b05f1c9c6c6d7bbd
personal <alihayder19@gmail.com>
2023-01-06 04:28:33 +0600

    Update Schema Message

--- src/components/donations/Filter/schema.ts
+++ src/components/donations/Filter/schema.ts
@@ -4,7 +4,7 @@ export const schema = Yup.object().shape({
   startDate: Yup.date()
     .transform((curr, orig) => (orig === "" ? null : curr))
     .nullable(true)
-    .min("2018-12-31", "Date is too early")
+    .min("2018-12-31", "Start date should start from 31st December 2018")
     .max(new Date(), "Start date must be before today")
     .default(new Date())
     .notRequired(),
