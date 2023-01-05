df118078538ddb22e6e5502b9c165d0837bd5e42
Nenad <nenad@angelprotocol.io>
2023-01-05 11:07:02 +0100

    Add query loader scaffold to Widget

--- src/pages/Donate/Widget.tsx
+++ src/pages/Donate/Widget.tsx
@@ -1,5 +1,44 @@
+// import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { QueryLoader } from "components/admin";
+import { useSetter } from "store/accessors";
+import { setRecipient } from "slices/donation";
 import Steps from "./Steps";
 
 export default function Widget() {
+  // const { apiKey } = useParams<{ apiKey: string }>();
+  // const queryState = useEndowInfoByAPIKeyQuery(apiKey, { skip: !apiKey });
+
+  return (
+    <QueryLoader
+      queryState={{
+        isError: false,
+        isLoading: false,
+        data: { name: "TestEndow", id: 11, kyc_donors_only: false },
+      }}
+      messages={{
+        loading: "Getting endowment info..",
+        error: "Failed to get endowment info",
+      }}
+      classes={{ container: "text-center mt-8" }}
+    >
+      {(endowment) => (
+        <Content
+          id={endowment.id}
+          isKYCRequired={endowment.kyc_donors_only}
+          name={endowment.name}
+        />
+      )}
+    </QueryLoader>
+  );
+}
+
+function Content(props: { id: number; name: string; isKYCRequired: boolean }) {
+  const dispatch = useSetter();
+
+  useEffect(() => {
+    dispatch(setRecipient(props));
+  }, [dispatch, props]);
+
   return <Steps />;
 }
