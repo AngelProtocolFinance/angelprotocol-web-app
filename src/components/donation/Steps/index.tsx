098f2ad768fa01099f749e694d9006c11b40be0f
Nenad <nenad@angelprotocol.io>
2023-01-05 16:33:54 +0100

    Move Kado modal to Steps & extract CurrentStep

--- src/components/donation/Steps/index.tsx
+++ src/components/donation/Steps/index.tsx
@@ -1,88 +1,46 @@
-import { useEffect } from "react";
-import { useErrorContext } from "contexts/ErrorContext";
-import { useGetWallet } from "contexts/WalletContext";
-import KYC from "components/KYC";
-import { Tooltip } from "components/donation";
-import { useGetter, useSetter } from "store/accessors";
-import { DonationState, resetDetails } from "slices/donation";
-import { UnexpectedStateError } from "errors/errors";
-import Donater from "./Donater";
+import { useCallback } from "react";
+import { useModalContext } from "contexts/ModalContext";
+import KadoModal from "components/KadoModal";
+import { useGetter } from "store/accessors";
+import { DonationState } from "slices/donation";
+import CurrentStep from "./CurrentStep";
 import Progress from "./Progress";
-import Result from "./Result";
-import Submit from "./Submit";
 
 export function Steps() {
+  const { showModal } = useModalContext();
   const state = useGetter((state) => state.donation);
+
+  const handleOpenKado = useCallback(
+    () => showModal(KadoModal, {}),
+    [showModal]
+  );
 
   return (
     <div className="justify-self-center grid">
-      {isProgressShown(state) && <Progress classes="my-12" />}
+      {isHeadingShown(state) && (
+        <>
+          <h3 className="text-center text-3xl font-bold leading-snug mb-4">
+            You're about to make a donation to {state.recipient?.name}
+          </h3>
+          <span className="text-center font-normal text-sm">
+            Don't have crypto in your wallet?{" "}
+            <button
+              className="font-bold underline hover:text-orange transition ease-in-out duration-300"
+              onClick={handleOpenKado}
+            >
+              Buy some to make your donation
+            </button>
+          </span>
+          <Progress classes="my-12" />
+        </>
+      )}
 
-      <CurrStep {...state} />
+      <CurrentStep />
     </div>
   );
 }
 
-function CurrStep(props: DonationState) {
-  const dispatch = useSetter();
-  const { wallet, isLoading } = useGetWallet();
-  const { handleError } = useErrorContext();
-
-  /** reset form state when user disconnects, user might change wallet */
-  useEffect(() => {
-    !wallet && dispatch(resetDetails());
-  }, [wallet, dispatch]);
-
-  useEffect(() => {
-    if (props.step < 0 || props.step > 4) {
-      handleError(new UnexpectedStateError(`Donation step is ${props.step}`));
-    }
-  }, [props.step, handleError]);
-
-  if (props.step < 0 || props.step > 4) {
-    return <></>;
-  }
-
-  if (props.step === 4) {
-    return <Result {...props} classes="justify-self-center mt-16" />;
-  }
-
-  if (isLoading) {
-    return <Tooltip type="Loading" message="Loading wallet" />;
-  }
-
-  if (!wallet) {
-    return (
-      <Tooltip
-        type="Info"
-        message="You need to connect your wallet to make a donation"
-      />
-    );
-  }
-
-  switch (props.step) {
-    case 3: {
-      return <Submit {...props} wallet={wallet} />;
-    }
-    case 2: {
-      return (
-        <KYC
-          type="on-donation"
-          state={props}
-          classes="grid gap-5 sm:grid-cols-2"
-        />
-      );
-    }
-    case 1: {
-      return <Donater {...props} wallet={wallet} />;
-    }
-    default: {
-      return <></>; // <Steps /> sets to step 1 onMount
-    }
-  }
-}
-
-function isProgressShown(state: DonationState): boolean {
+function isHeadingShown(state: DonationState): boolean {
   return (
     state.step !== 4 || (state.status !== "error" && !("hash" in state.status))
   );
--- src/pages/Donate/Content.tsx
+++ src/pages/Donate/Content.tsx
@@ -1,29 +1,16 @@
-import { useCallback, useEffect } from "react";
-import { useModalContext } from "contexts/ModalContext";
+import { useEffect } from "react";
 import Breadcrumbs from "components/Breadcrumbs";
-import KadoModal from "components/KadoModal";
 import { Steps } from "components/donation";
-import { useGetter, useSetter } from "store/accessors";
-import {
-  DonationRecipient,
-  DonationState,
-  setRecipient,
-} from "slices/donation";
+import { useSetter } from "store/accessors";
+import { DonationRecipient, setRecipient } from "slices/donation";
 import { appRoutes } from "constants/routes";
 
 export default function Page(props: DonationRecipient) {
-  const { showModal } = useModalContext();
   const dispatch = useSetter();
-  const state = useGetter((state) => state.donation);
 
   useEffect(() => {
     dispatch(setRecipient(props));
   }, [dispatch, props]);
-
-  const handleOpenKado = useCallback(
-    () => showModal(KadoModal, {}),
-    [showModal]
-  );
 
   return (
     <div className="justify-self-center grid padded-container max-w-[35rem] py-8 sm:py-20">
@@ -42,30 +29,7 @@ export default function Page(props: DonationRecipient) {
         ]}
       />
 
-      {isHeadingShown(state) && (
-        <>
-          <h3 className="text-center text-3xl font-bold leading-snug mb-4">
-            You're about to make a donation to {props.name}
-          </h3>
-          <span className="text-center font-normal text-sm">
-            Don't have crypto in your wallet?{" "}
-            <button
-              className="font-bold underline hover:text-orange transition ease-in-out duration-300"
-              onClick={handleOpenKado}
-            >
-              Buy some to make your donation
-            </button>
-          </span>
-        </>
-      )}
-
       <Steps />
     </div>
-  );
-}
-
-function isHeadingShown(state: DonationState): boolean {
-  return (
-    state.step !== 4 || (state.status !== "error" && !("hash" in state.status))
   );
 }
