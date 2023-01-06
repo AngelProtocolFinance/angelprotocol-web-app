4a27eaf90b780743973b405b982b9309a6eed962
Nenad <nenad@angelprotocol.io>
2023-01-06 09:16:25 +0100

    Extract Balance out of Balances into separate file

--- src/components/Balances/Balances.tsx
+++ src/components/Balances/Balances.tsx
@@ -1,11 +1,7 @@
-import { BalanceInfo, EndowmentBalance } from "types/contracts";
+import { BalanceInfo } from "types/contracts";
 import { useBalanceQuery } from "services/juno/account";
-import { QueryLoader } from "components/admin";
-import { condense, humanize } from "helpers";
 import { axlUSDCDenom } from "constants/tokens";
-
-const CONTAINER_STYLE =
-  "flex flex-col justify-center items-center gap-2 h-20 w-full py-4 rounded border border-gray-l2 dark:bg-blue-d6 dark:border-bluegray md:items-start md:h-28 md:px-6 md:py-04";
+import Balance from "./Balance";
 
 type Props = { profileId: number; direction?: "row" | "column" };
 
@@ -34,34 +30,6 @@ export default function Balances({ profileId, direction = "row" }: Props) {
         getAmount={getLiquid}
       />
     </div>
-  );
-}
-
-function Balance(props: {
-  queryState: { data?: EndowmentBalance; isLoading: boolean; isError: boolean };
-  title: string;
-  getAmount: (balance: BalanceInfo) => number;
-}) {
-  return (
-    <QueryLoader
-      queryState={props.queryState}
-      messages={{
-        loading: `Fetching ${props.title.toLowerCase()}`,
-        error: `Failed to get ${props.title.toLowerCase()}`,
-      }}
-      classes={{ container: CONTAINER_STYLE }}
-    >
-      {({ tokens_on_hand }) => (
-        <div className={CONTAINER_STYLE}>
-          <h6 className="font-heading font-bold text-xs tracking-wider uppercase">
-            {props.title}
-          </h6>
-          <p className="font-work font-normal text-lg text-gray-d1 dark:text-gray">
-            ${humanize(condense(props.getAmount(tokens_on_hand)))}
-          </p>
-        </div>
-      )}
-    </QueryLoader>
   );
 }
 
