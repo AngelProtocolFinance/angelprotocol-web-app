6ab024ca44a012a9e27146162cca28deaed66ca7
Nenad <nenad@angelprotocol.io>
2023-01-06 09:01:09 +0100

    Add widget header section

--- src/DonateWidget/Content.tsx
+++ src/DonateWidget/Content.tsx
@@ -1,4 +1,5 @@
 // import { useParams } from "react-router-dom";
+import WalletSuite from "App/WalletSuite";
 import { useEffect } from "react";
 import { Steps } from "components/donation";
 import { useSetter } from "store/accessors";
@@ -15,5 +16,21 @@ export default function Content(props: {
     dispatch(setRecipient(props));
   }, [dispatch, props]);
 
-  return <Steps />;
+  return (
+    <div className="padded-container justify-self-center grid max-w-3xl">
+      <header className="sticky top-0 z-20 flex justify-between items-center w-full h-[90px]">
+        <span>{getPossessiveForm(props.name)} endowment</span>
+        <WalletSuite />
+      </header>
+      <Steps />
+    </div>
+  );
+}
+
+function getPossessiveForm(name: string) {
+  let result = `${name}'`;
+  if (!name.endsWith("s")) {
+    result += "s";
+  }
+  return result;
 }
