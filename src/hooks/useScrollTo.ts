c4c5f2c4449af8f575c0a2c7aa953a769e2ab78f
personal <alihayder19@gmail.com>
2023-01-10 01:41:29 +0600

    Remove Console.log

--- src/hooks/useScrollTo.ts
+++ src/hooks/useScrollTo.ts
@@ -4,6 +4,5 @@ export default function useScrollTo(id: string) {
   useEffect(() => {
     const element = document.getElementById(id);
     element?.scrollIntoView({ behavior: "smooth" });
-    console.log("hola");
   }, [id]);
 }
