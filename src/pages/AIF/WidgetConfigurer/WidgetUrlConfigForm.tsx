49b564b47ba718a47e207aeb360c8d047f4db0af
Nenad <nenad@angelprotocol.io>
2023-01-09 16:44:35 +0100

    Add state to WidgetUrlConfigForm

--- src/pages/AIF/WidgetConfigurer/WidgetUrlConfigForm.tsx
+++ src/pages/AIF/WidgetConfigurer/WidgetUrlConfigForm.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
 export default function WidgetUrlConfigForm() {
+  const [hideText, setHideText] = useState(false);
+  const [hideEndowmentGauges, setHideEndowmentGauges] = useState(false);
+  const [hideAdvancedOptions, setHideAdvancedOptions] = useState(false);
+  const [unfoldAdvancedOptions, setUnfoldAdvancedOptions] = useState(false);
+  const [liquidPercentage, setLiquidPercentage] = useState(0);
+  const [availableCurrencies, setAvailableCurrencies] = useState<string[]>([]);
+
   return <div>WidgetUrlConfigForm</div>;
 }
