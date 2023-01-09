8c17c0bf5d46f1016bf7725713ca75c55bf6bb19
Nenad <nenad@angelprotocol.io>
2023-01-09 09:45:00 +0100

    Move Endowment loading into separate loader component

--- src/DonateWidget/DonateWidget.tsx
+++ src/DonateWidget/DonateWidget.tsx
@@ -1,24 +1,12 @@
-// import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useEndowInfoQuery } from "services/juno/custom";
-import { QueryLoader } from "components/admin";
-import {
-  idParamToNum,
-  isPrevDark,
-  setToDarkMode,
-  setToLightMode,
-} from "helpers";
+import { isPrevDark, setToDarkMode, setToLightMode } from "helpers";
 import ApiKeyChecker from "./ApiKeyChecker";
 import Content from "./Content";
+import EndowmentLoader from "./EndowmentLoader";
 
 const isPrevThemeDark = isPrevDark();
 
 export default function DonateWidget() {
-  const { id } = useParams<{ id: string }>();
-  const endowId = idParamToNum(id);
-  const queryState = useEndowInfoQuery(endowId, { skip: endowId === 0 });
-
   /**
    * need to set the theme to light, but after widget is closed we need to
    * reverse the user selected theme on the main webapp to the previous theme
@@ -43,14 +31,7 @@ export default function DonateWidget() {
 
   return (
     <ApiKeyChecker>
-      <QueryLoader
-        queryState={queryState}
-        messages={{
-          loading: "Getting endowment info..",
-          error: "Failed to get endowment info",
-        }}
-        classes={{ container: "text-center mt-8" }}
-      >
+      <EndowmentLoader>
         {(endowment) => (
           <Content
             id={endowment.id}
@@ -58,7 +39,7 @@ export default function DonateWidget() {
             name={endowment.name}
           />
         )}
-      </QueryLoader>
+      </EndowmentLoader>
     </ApiKeyChecker>
   );
 }
