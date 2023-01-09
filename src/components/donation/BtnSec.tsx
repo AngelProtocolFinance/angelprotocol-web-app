93e1fce60147f818ea17f92d4d488d957d6d15ee
Nenad Misic <nenad@angelprotocol.io>
2023-01-09 09:23:54 +0100

    Donate widget (#1605)
    * Add Donate/Widget page
    
    * Rename Steps to StepsPage
    
    * Refactor isHeadingShown
    
    * Update donate widget route to /donate-widget
    
    * Refactor CurrStep
    
    * Add missing space in BtnSec style
    
    * Add query loader scaffold to Widget
    
    * Update BtnSec to use .btn class
    
    * Move DonateWidget to separate /page folder (includes other refactors)
    
    * Move DonateWidget outside the /App component
    
    * Set light mode when opening donation widget
    
    * Add theme setting logic to widget
    
    * Move DonateWidget Content to separate file
    
    * Move Kado modal to Steps & extract CurrentStep
    
    * Refactor isHeadingShown to isFinalized
    
    * Remove redundant WalletSuite Props
    
    * Add widget header section
    
    * Move profile Balances to shared components
    
    * Update Balances to accept direction
    
    * Add Balances to widget
    
    * Extract Balance out of Balances into separate file
    
    * Remove widget header stickiness
    
    * Add footer
    
    * Move WalletSuite to shared components
    
    * Fix title font size in widget
    
    * Add text styling
    
    * Make padded-container have mx-auto on all screen widths
    
    * Add className to Header Logo
    
    * Move Header/Logo to components/APLogo
    
    * Use APLogo in widget footer
    
    * Create separate Balances for widget
    
    * Update Balances styles on widget
    
    * Add alignment styles
    
    * Reduce Steps text size on mobile
    
    * Reduce Steps width inside widget
    
    * Move Donate steps title to just Donate page
    
    * Set z-index of widget header to z-20
    
    * Hide Intercom Chatbot on widget
    
    * Fix App.test
    
    * Refactor CoinBalances
    
    * Remove leftover useBalances
    
    * Remove Balances

--- src/components/donation/BtnSec.tsx
+++ src/components/donation/BtnSec.tsx
@@ -4,7 +4,7 @@ export function BtnSec({ className = "", ...props }: BtnLinkProps) {
   return (
     <BtnLink
       {...props}
-      className={`${className}bg-orange-l5 dark:bg-blue-d4 hover:bg-orange-l4 dark:hover:bg-blue-d3 text-sm md:text-base p-3 rounded border border-prim text-center hover:border-gray-l1 hover:dark:border-blue-d2 font-bold font-body`}
+      className={`${className} btn bg-orange-l5 dark:bg-blue-d4 hover:bg-orange-l4 dark:hover:bg-blue-d3 text-sm md:text-base p-3 rounded border border-prim text-center hover:border-gray-l1 hover:dark:border-blue-d2 font-bold font-body`}
     />
   );
 }
---
+++ src/pages/Donate/Content.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef } from "react";
+import Breadcrumbs from "components/Breadcrumbs";
+import { Steps } from "components/donation";
+import { useGetter, useSetter } from "store/accessors";
+import {
+  DonationRecipient,
+  DonationState,
+  setRecipient,
+} from "slices/donation";
+import { appRoutes } from "constants/routes";
+
+export default function Page(props: DonationRecipient) {
+  const dispatch = useSetter();
+  const state = useGetter((state) => state.donation);
+
+  useEffect(() => {
+    dispatch(setRecipient(props));
+  }, [dispatch, props]);
+
+  const CONTAINER_ID = "__container_id";
+  const prevStep = useRef(state.step);
+  useEffect(() => {
+    /** at first visit, equal so no scrolling happens
+     * on next or back, would be different
+     */
+    if (state.step === prevStep.current) return;
+    const element = document.getElementById(CONTAINER_ID);
+    element?.scrollIntoView();
+    prevStep.current = state.step;
+  }, [state.step]);
+
+  return (
+    <div
+      className="justify-self-center grid padded-container max-w-[35rem] py-8 sm:py-20 scroll-mt-6"
+      id={CONTAINER_ID}
+    >
+      <Breadcrumbs
+        className="font-body font-normal text-sm justify-self-start sm:justify-self-auto mb-10 sm:mb-12"
+        items={[
+          { title: "Marketplace", to: appRoutes.marketplace },
+          {
+            title: props.name,
+            to: `${appRoutes.profile}/${props.id}`,
+          },
+          {
+            title: "Donate",
+            to: `${appRoutes.donate}/${props.id}`,
+          },
+        ]}
+      />
+
+      {isFinalized(state) && (
+        <h3 className="text-center text-xl sm:text-3xl font-bold leading-snug mb-4">
+          You're about to make a donation to {state.recipient?.name}
+        </h3>
+      )}
+      <Steps />
+    </div>
+  );
+}
+
+function isFinalized(state: DonationState): boolean {
+  return (
+    state.step === 4 && (state.status === "error" || "hash" in state.status)
+  );
+}
