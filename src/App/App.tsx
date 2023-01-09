2dada14b6cf036e37d69682cf11b6e23d92e9edb
Nenad Misic <nenad@angelprotocol.io>
2023-01-09 09:23:54 +0100

    Donate widget (#1605)
    * Add Donate/Widget page
    
    * Rename Steps to StepsPage
    
    * Refactor isHeadingShown
    
    * Update donate widget route to /donate-widget
    
    * Refactor CurrStep
    
    * Add missing space in BtnSec style
    
    * Add query loader scaffold to Widget
    
    * Update BtnSec to use .btn class
    
    * Move DonateWidget to separate /page folder (includes other refactors)
    
    * Move DonateWidget outside the /App component
    
    * Set light mode when opening donation widget
    
    * Add theme setting logic to widget
    
    * Move DonateWidget Content to separate file
    
    * Move Kado modal to Steps & extract CurrentStep
    
    * Refactor isHeadingShown to isFinalized
    
    * Remove redundant WalletSuite Props
    
    * Add widget header section
    
    * Move profile Balances to shared components
    
    * Update Balances to accept direction
    
    * Add Balances to widget
    
    * Extract Balance out of Balances into separate file
    
    * Remove widget header stickiness
    
    * Add footer
    
    * Move WalletSuite to shared components
    
    * Fix title font size in widget
    
    * Add text styling
    
    * Make padded-container have mx-auto on all screen widths
    
    * Add className to Header Logo
    
    * Move Header/Logo to components/APLogo
    
    * Use APLogo in widget footer
    
    * Create separate Balances for widget
    
    * Update Balances styles on widget
    
    * Add alignment styles
    
    * Reduce Steps text size on mobile
    
    * Reduce Steps width inside widget
    
    * Move Donate steps title to just Donate page
    
    * Set z-index of widget header to z-20
    
    * Hide Intercom Chatbot on widget
    
    * Fix App.test
    
    * Refactor CoinBalances
    
    * Remove leftover useBalances
    
    * Remove Balances

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,15 @@
-import { WalletProvider } from "@terra-money/wallet-provider";
-import ModalContext from "contexts/ModalContext";
-import WalletContext from "contexts/WalletContext";
 import Seo from "components/Seo";
 import Footer from "./Footer";
 import Header from "./Header";
 import Views from "./Views";
-import { chainOptions } from "./chainOptions";
 
 export default function App() {
   return (
-    <WalletProvider {...chainOptions}>
-      <WalletContext>
-        <ModalContext>
-          <div className="grid grid-rows-[auto_1fr_auto]">
-            <Seo /> {/* Load all defaults for SEO meta tags */}
-            <Header classes="sticky top-0 z-20" />
-            <Views />
-            <Footer />
-          </div>
-        </ModalContext>
-      </WalletContext>
-    </WalletProvider>
+    <div className="grid grid-rows-[auto_1fr_auto]">
+      <Seo /> {/* Load all defaults for SEO meta tags */}
+      <Header classes="sticky top-0 z-20" />
+      <Views />
+      <Footer />
+    </div>
   );
 }
