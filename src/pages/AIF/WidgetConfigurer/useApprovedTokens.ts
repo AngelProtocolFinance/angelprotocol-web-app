06196cb8afea6727c6ea4013d0a74df3cb2a845d
Nenad <nenad@angelprotocol.io>
2023-01-10 07:20:14 +0100

    Update useApprovedTokens to use useLazyTokensQuery

--- src/pages/AIF/WidgetConfigurer/useApprovedTokens.ts
+++ src/pages/AIF/WidgetConfigurer/useApprovedTokens.ts
@@ -1,44 +1,25 @@
 import { useEffect, useState } from "react";
-import { Token } from "types/aws";
-import { useLazyChainQuery } from "services/apes";
-import { useGetWallet } from "contexts/WalletContext";
-import { chainIds } from "constants/chainIds";
+import { useLazyTokensQuery } from "services/apes";
+import { useErrorContext } from "contexts/ErrorContext";
 
 export default function useApprovedTokens(): string[] {
-  const { wallet } = useGetWallet();
   const [approvedTokens, setApprovedTokens] = useState<string[]>([]);
+  const { handleError } = useErrorContext();
 
-  const [getChain] = useLazyChainQuery();
+  const [getTokens] = useLazyTokensQuery();
 
   useEffect(() => {
-    if (!wallet?.address) {
-      return;
-    }
-
-    (async function () {
-      const getChainPromises = Object.values(chainIds).map((chainId) =>
-        getChain({ address: wallet?.address, chainId }, true).unwrap()
-      );
-
-      const chainDataArray = await Promise.all(getChainPromises);
-
-      function pushToken(token: Token) {
-        if (token.approved) {
-          approvedTokenSymbols.push(token.symbol);
-        }
-      }
-
-      const approvedTokenSymbols: string[] = [];
-      chainDataArray.forEach((chain) => {
-        chain.tokens.forEach((token) => {
-          pushToken(token);
-        });
-        pushToken(chain.native_currency);
-      });
-
-      setApprovedTokens(approvedTokenSymbols);
-    })();
-  }, [wallet, getChain]);
+    getTokens({}, true)
+      .unwrap()
+      .then((tokens) => {
+        const approvedTokenSymbols: string[] = [];
+        tokens.forEach(
+          (token) => token.approved && approvedTokenSymbols.push(token.symbol)
+        );
+        setApprovedTokens(approvedTokenSymbols);
+      })
+      .catch((err) => handleError(err));
+  }, [getTokens, handleError]);
 
   return approvedTokens;
 }
