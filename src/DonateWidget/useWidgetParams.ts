f0eb5df43448c178f376a8b796e7dc891aed058c
Nenad <nenad@angelprotocol.io>
2023-01-11 10:28:11 +0100

    Update useWidgetParams result

--- src/DonateWidget/useWidgetParams.ts
+++ src/DonateWidget/useWidgetParams.ts
@@ -2,26 +2,32 @@ import { useSearchParams } from "react-router-dom";
 import { URL_PARAMS } from "./constants";
 
 type Result = {
-  hideText: any;
-  hideAdvOpts: any;
-  hideEndowGauges: any;
-  unfoldAdvOpts: any;
-  liquidPercentage: number | null;
-  availableCurrencies: string[] | null;
+  hideText: boolean;
+  hideAdvOpts: boolean;
+  hideEndowGauges: boolean;
+  unfoldAdvOpts: boolean;
+  liquidPercentage: number;
+  availableCurrencies?: string[];
 };
 
 export default function useWidgetParams(): Result {
   const [search] = useSearchParams();
   const searchParams = new URLSearchParams(search);
 
+  function get(key: keyof Result): boolean {
+    const param = searchParams.get(key);
+    return param != null && (param === "" || Boolean(param));
+  }
+
   const result: Result = {
-    hideText: searchParams.get(URL_PARAMS.hideText),
-    hideAdvOpts: searchParams.get(URL_PARAMS.hideAdvancedOptions),
-    hideEndowGauges: searchParams.get(URL_PARAMS.hideEndowmentGauges),
-    unfoldAdvOpts: searchParams.get(URL_PARAMS.unfoldAdvancedOptions),
+    hideText: get(URL_PARAMS.hideText),
+    hideAdvOpts: get(URL_PARAMS.hideAdvancedOptions),
+    hideEndowGauges: get(URL_PARAMS.hideEndowmentGauges),
+    unfoldAdvOpts: get(URL_PARAMS.unfoldAdvancedOptions),
     liquidPercentage: Number(searchParams.get(URL_PARAMS.liquidPercentage)),
-    availableCurrencies:
-      searchParams.get(URL_PARAMS.availableCurrencies)?.split(",") || null,
+    availableCurrencies: searchParams
+      .get(URL_PARAMS.availableCurrencies)
+      ?.split(","),
   };
 
   return result;
