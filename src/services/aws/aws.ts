8882436c33a5ad5b517edf8b4cd04dc14650310f
Nenad <nenad@angelprotocol.io>
2023-01-13 12:32:24 +0100

    Remove unused getProfileQuery and imports from aws.ts

--- src/services/aws/aws.ts
+++ src/services/aws/aws.ts
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import {
   Endowment,
-  EndowmentBookmark,
   EndowmentInfo,
   EndowmentsQueryParams,
   PaginatedAWSQueryRes,
