32a096bc9554940296eb4225e0607f5b06f6d13a
ap-justin <justin@angelprotocol.io>
2023-01-18 17:13:21 +0800

    simplified mobile table markup

--- src/pages/Donations/MobileTable.tsx
+++ src/pages/Donations/MobileTable.tsx
@@ -23,33 +23,25 @@ export default function MobileTable({ donations, classes = "" }: TableProps) {
       <div className="grid grid-cols-8 text-sm">
         {sorted.map((row, index) => (
           <Disclosure key={index} as={React.Fragment}>
-            <Disclosure.Button className="col-span-full grid w-fulljy grid-cols-8">
+            <Disclosure.Button className="col-span-full grid grid-cols-8 border-b last:border-0 border-gray-l2 dark:border-bluegray divide-x divide-gray-l2 dark:divide-bluegray even:bg-orange-l6 dark:even:bg-blue-d7">
               {({ open }) => (
-                <div
-                  className={`${
-                    open ? "bg-orange-l5 dark:bg-blue-d4" : ""
-                  } border-b border-gray-l2 dark:border-bluegray divide-x divide-gray-l2 dark:divide-bluegray`}
-                >
-                  <div className="col-span-1 w-full flex justify-center border-r border-gray-l2 dark:border-bluegray p-4">
-                    <DrawerIcon
-                      size={24}
-                      className={`${open ? "text-orange" : ""}`}
-                      isOpen={open}
-                    />
-                  </div>
-                  <div className="col-span-4 border-r border-gray-l2 dark:border-bluegray p-4 text-left">
-                    <p className="text-sm">{row.charityName}</p>
-                  </div>
+                <>
+                  <DrawerIcon
+                    size={25.5}
+                    className={`${open ? "text-orange" : ""} place-self-center`}
+                    isOpen={open}
+                  />
+                  <p className="text-sm col-span-4 p-4 text-left">
+                    {row.charityName}
+                  </p>
                   <div className="col-span-3 p-4 text-left text-sm">
                     {new Date(row.date).toLocaleDateString()}
                   </div>
-                </div>
+                </>
               )}
             </Disclosure.Button>
-            <Disclosure.Panel>
-              <Row classes="font-work" title="Network">
-                {row.chainName}
-              </Row>
+            <Disclosure.Panel className="col-span-full font-work group">
+              <Row title="Network">{row.chainName}</Row>
               <Row title="Currency">{row.symbol}</Row>
               <Row title="Amount">{humanize(row.amount, 3)}</Row>
               <Row title="USD Value">{`$${humanize(row.usdValue, 2)}`}</Row>
@@ -84,7 +76,7 @@ interface RowProps {
 function Row({ title, children, classes }: PropsWithChildren<RowProps>) {
   return (
     <div
-      className={`flex justify-between p-4 border-b border-gray-l2 dark:border-bluegray ${classes}`}
+      className={`flex col-span-full justify-between p-4 border-b border-gray-l2 dark:border-bluegray ${classes} group-even:odd:bg-orange-l6 dark:group-even:odd:bg-blue-d7 group-odd:even:bg-orange-l6 dark:group-odd:even:bg-blue-d7`}
     >
       <span className="font-bold uppercase">{title}</span>
       {children}
