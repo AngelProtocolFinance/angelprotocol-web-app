59b7f670b351cec5bc4428ff9a553550e72505de
Nenad <nenad@angelprotocol.io>
2023-01-20 08:19:08 +0100

    Remove chain_id from ADR36Payload

--- src/types/aws/ap/index.ts
+++ src/types/aws/ap/index.ts
@@ -123,7 +123,7 @@ export interface DonationsMetricList {
 type AminoSignRes = Awaited<ReturnType<Keplr["signAmino"]>>;
 type Signed = AminoSignRes["signed"];
 type Signature = AminoSignRes["signature"];
-export type ADR36Payload = Pick<Signed, "chain_id" | "fee" | "memo"> & {
+export type ADR36Payload = Pick<Signed, "fee" | "memo"> & {
   msg: Signed["msgs"];
   signatures: Signature[];
 };
