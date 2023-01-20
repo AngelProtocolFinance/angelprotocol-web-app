32506d6834168838022bdcc0d11ec0e8249d0b53
Nenad <nenad@angelprotocol.io>
2023-01-20 12:54:16 +0100

    Refactor how widgetSnippet is generated

--- src/pages/AIF/WidgetConfigurer/WidgetConfigurer.tsx
+++ src/pages/AIF/WidgetConfigurer/WidgetConfigurer.tsx
@@ -2,21 +2,25 @@ import { useCallback, useState } from "react";
 import { useParams } from "react-router-dom";
 import widgetSample from "assets/images/widget-example.png";
 import Copier from "components/Copier";
+import { IS_TEST } from "constants/env";
+import { appRoutes } from "constants/routes";
 import WidgetUrlGenerator from "./WidgetUrlGenerator";
 
 const TITLE_STYLE = "text-lg sm:text-2xl font-heading font-bold";
 
+const ROOT_URL = IS_TEST
+  ? `http://localhost:4200${appRoutes.donate_widget}`
+  : `https://app.angelprotocol.io${appRoutes.donate_widget}`;
+
 export default function WidgetConfigurer() {
   const { id } = useParams<{ id: string }>();
-  const [widgetSnippet, setWidgetSnippet] = useState("");
+  const [params, setParams] = useState("");
 
-  const handleOnUrlChange = useCallback(
-    (url: string) =>
-      setWidgetSnippet(
-        `<iframe src="${url}" width="700" height="900" style="border: 0px;"></iframe>`
-      ),
-    []
-  );
+  const handleOnChange = useCallback((url: string) => setParams(url), []);
+
+  const url = `${ROOT_URL}/${id}?apiKey=API_KEY${params ? `&${params}` : ""}`;
+
+  const widgetSnippet = `<iframe src="${url}" width="700" height="900" style="border: 0px;"></iframe>`;
 
   return (
     <div className="padded-container grid grid-rows-[auto_1fr] gap-10 w-full h-full">
@@ -52,7 +56,7 @@ export default function WidgetConfigurer() {
         </section>
         <section className="flex flex-col gap-3 max-sm:items-center">
           <h2 className={TITLE_STYLE}>Configure your widget</h2>
-          <WidgetUrlGenerator endowId={id} onChange={handleOnUrlChange} />
+          <WidgetUrlGenerator endowId={id} onChange={handleOnChange} />
 
           <h2 className={`${TITLE_STYLE} mt-10`}>Copy / paste this URL:</h2>
           <div className="flex items-center justify-center gap-4 h-32 px-10 rounded bg-gray-l3 dark:bg-blue-d4">
