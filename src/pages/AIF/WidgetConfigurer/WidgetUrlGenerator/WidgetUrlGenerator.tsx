af6c2a5e7f2ef79fb2802e5d556f405f99115531
Nenad <nenad@angelprotocol.io>
2023-01-23 09:45:22 +0100

    Revert Checkbox to Sidebar/common

--- src/pages/AIF/WidgetConfigurer/WidgetUrlGenerator/WidgetUrlGenerator.tsx
+++ src/pages/AIF/WidgetConfigurer/WidgetUrlGenerator/WidgetUrlGenerator.tsx
@@ -1,5 +1,5 @@
 import { FormProvider } from "react-hook-form";
-import CheckboxFormField from "components/CheckboxFormField";
+import { CheckField } from "components/form/CheckField";
 import DenomSelector from "./DenomSelector";
 import Split from "./Split";
 import { FormValues } from "./schema";
@@ -15,24 +15,22 @@ export default function WidgetUrlGenerator({ endowId, onChange }: Props) {
   return (
     <FormProvider {...methods}>
       <div className="flex flex-col gap-2 w-4/5 sm:text-lg font-normal font-body">
-        <CheckboxFormField<FormValues> name="hideText">
-          Hide text
-        </CheckboxFormField>
+        <CheckField<FormValues> name="hideText">Hide text</CheckField>
 
         <span>Available currencies:</span>
         <DenomSelector />
 
-        <CheckboxFormField<FormValues> name="hideAdvancedOptions">
+        <CheckField<FormValues> name="hideAdvancedOptions">
           Hide "advanced options"
-        </CheckboxFormField>
+        </CheckField>
 
-        <CheckboxFormField<FormValues>
+        <CheckField<FormValues>
           name="unfoldAdvancedOptions"
           disabled={hideAdvancedOptions}
           classes={{ label: "peer-disabled:text-gray" }}
         >
           Unfold "advanced options" by default
-        </CheckboxFormField>
+        </CheckField>
 
         <span>Define split value by default:</span>
         <Split />
