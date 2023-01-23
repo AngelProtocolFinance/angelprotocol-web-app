8b495d15e5754ce80cf118e3e0e73a65e503ad25
Nenad Misic <nenad@angelprotocol.io>
2023-01-23 13:35:30 +0100

    Make widget example responsive to selected options (#1653)
    * Update WidgetConfig example title
    
    * Move url param reading to DonateWidget
    
    * Enable passing of props to DonateWidget
    
    * Refactor DonateWidget to accept default query params
    
    * Refactor how widgetSnippet is generated
    
    * Add className prop to DonateWidget
    
    * Add DonateWidget to WidgetConfigurer instead of image
    
    * Remove widget example img
    
    * Add missing className prop assignment in DonateWidget
    
    * Fix bug in AdvancedOptions unfold logic
    
    * Make DonateWidget responsive to changing params passed from WidgetConfigurer
    
    * Fix DonateWidget width on WidgetConfigurer
    
    * Fix bug with theme switch on widget
    
    * Make DonateValues.pctLiquidSplit number type
    
    * Fix slider value not updating
    
    * Reduce z-index of Donate widget header

--- src/DonateWidget/InnerComponent.tsx
+++ src/DonateWidget/InnerComponent.tsx
@@ -1,17 +1,21 @@
 import { useEffect } from "react";
+import { UrlParamValues } from "./types";
 import APLogo from "components/APLogo";
 import WalletSuite from "components/WalletSuite";
 import { Steps } from "components/donation";
 import { useSetter } from "store/accessors";
 import { setRecipient } from "slices/donation";
-import useWidgetParams from "./useWidgetParams";
 
-export default function InnerComponent(props: {
+type Props = {
+  className?: string;
   id: number;
   name: string;
   isKYCRequired: boolean;
-}) {
-  const { hideText, ...rest } = useWidgetParams();
+  params: UrlParamValues;
+};
+
+export default function InnerComponent(props: Props) {
+  const { hideText, ...rest } = props.params;
 
   const dispatch = useSetter();
 
@@ -20,9 +24,11 @@ export default function InnerComponent(props: {
   }, [dispatch, props]);
 
   return (
-    <div className="grid grid-rows-[1fr_auto] gap-10">
+    <div
+      className={`${props.className || ""} grid grid-rows-[1fr_auto] gap-10`}
+    >
       <div className="flex flex-col gap-3 max-w-3xl h-full mx-auto px-5">
-        <header className="flex justify-center items-center gap-10 w-full h-24 z-20">
+        <header className="flex justify-center items-center gap-10 w-full h-24 z-10">
           <h1 className="text-xl sm:text-3xl font-heading font-bold">
             {getPossessiveForm(props.name)} endowment
           </h1>
