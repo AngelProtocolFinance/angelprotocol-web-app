d913b3db776f66f21672d2701dfe18d5d768a225
Nenad <nenad@angelprotocol.io>
2023-01-23 12:36:47 +0100

    Reduce z-index of Donate widget header

--- src/DonateWidget/InnerComponent.tsx
+++ src/DonateWidget/InnerComponent.tsx
@@ -28,7 +28,7 @@ export default function InnerComponent(props: Props) {
       className={`${props.className || ""} grid grid-rows-[1fr_auto] gap-10`}
     >
       <div className="flex flex-col gap-3 max-w-3xl h-full mx-auto px-5">
-        <header className="flex justify-center items-center gap-10 w-full h-24 z-20">
+        <header className="flex justify-center items-center gap-10 w-full h-24 z-10">
           <h1 className="text-xl sm:text-3xl font-heading font-bold">
             {getPossessiveForm(props.name)} endowment
           </h1>
