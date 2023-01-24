547a85eb83e1cafd6ff733ebc2346ce7147f7f59
Nenad <nenad@angelprotocol.io>
2023-01-24 08:10:51 +0100

    Refactor widget iframe snippet to update example on clicking update

---
+++ src/pages/AIF/WidgetConfigurer/WidgetUrlGenerator/useSubmit.ts
@@ -0,0 +1,64 @@
+import { UrlParamValues } from "DonateWidget";
+import { URL_PARAMS } from "DonateWidget/constants";
+import { useCallback } from "react";
+import { useErrorContext } from "contexts/ErrorContext";
+import { isEmpty } from "helpers";
+import { UnexpectedStateError } from "errors/errors";
+import { IS_TEST } from "constants/env";
+import { appRoutes } from "constants/routes";
+import { FormValues } from "./schema";
+
+const APP_URL = IS_TEST
+  ? "http://localhost:4200"
+  : "https://app.angelprotocol.io";
+
+export default function useSubmit(
+  endowId: string | undefined,
+  onChange: (url: string) => void
+) {
+  const { handleError } = useErrorContext();
+
+  const submit = useCallback(
+    (formValues: FormValues) => {
+      if (!endowId) {
+        return handleError(
+          new UnexpectedStateError(`Endowment ID is undefined`)
+        );
+      }
+
+      const param1 = append(formValues.hideText, URL_PARAMS.hideText);
+      const param2 = append(
+        formValues.hideAdvancedOptions,
+        URL_PARAMS.hideAdvancedOptions
+      );
+      const param3 = append(
+        !formValues.hideAdvancedOptions && formValues.unfoldAdvancedOptions,
+        URL_PARAMS.unfoldAdvancedOptions
+      );
+      const param4 = append(
+        !!formValues.liquidPercentage,
+        URL_PARAMS.liquidPercentage,
+        formValues.liquidPercentage
+      );
+      const param5 = append(
+        !isEmpty(formValues.availableCurrencies),
+        URL_PARAMS.availableCurrencies,
+        formValues.availableCurrencies.map((x) => x.value).join(",")
+      );
+      onChange(
+        `${APP_URL}${appRoutes.donate_widget}/${endowId}?apiKey=API_KEY${param1}${param2}${param3}${param4}${param5}`
+      );
+    },
+    [endowId, handleError, onChange]
+  );
+
+  return submit;
+}
+
+function append(
+  condition: boolean,
+  name: keyof UrlParamValues,
+  values?: string | number
+): string {
+  return condition ? `&${name}${!values ? "" : `=${values}`}` : "";
+}
