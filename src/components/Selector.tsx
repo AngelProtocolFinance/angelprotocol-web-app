14aaeae058941c8283b103cb6e894eb13674984f
Nenad <nenad@angelprotocol.io>
2023-01-25 17:47:10 +0100

    Reduce z index of Listbox options

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -109,7 +109,7 @@ export function Selector<
             </>
           )}
         </Listbox.Button>
-        <Listbox.Options className="rounded-sm text-sm border border-prim absolute top-full mt-2 z-20 bg-gray-l5 dark:bg-blue-d6 w-full max-h-[10rem] overflow-y-auto scroller">
+        <Listbox.Options className="rounded-sm text-sm border border-prim absolute top-full mt-2 z-10 bg-gray-l5 dark:bg-blue-d6 w-full max-h-[10rem] overflow-y-auto scroller">
           {multiple && (
             <Listbox.Option
               key={SELECT_ALL.value}
