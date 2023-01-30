975016eb6bc604bd5555e3f9b4f8f1a1adad4996
Nenad <nenad@angelprotocol.io>
2023-01-30 09:15:09 +0100

    Implement generic Split in Donate Steps

--- src/components/Split/Slider.tsx
+++ src/components/Split/Slider.tsx
@@ -1,16 +1,23 @@
 type Props = {
   className?: string;
+  disabled?: boolean;
   value: number;
   onChange(value: number): void;
 };
 
-export default function Slider({ className = "", value, onChange }: Props) {
+export default function Slider({
+  className = "",
+  disabled,
+  value,
+  onChange,
+}: Props) {
   return (
     <div className={`${className} select-none`}>
       <input
         className="range"
         value={value}
         onChange={(e) => onChange(Number(e.target.value))}
+        disabled={disabled}
         min={0}
         max={100}
         type="range"
