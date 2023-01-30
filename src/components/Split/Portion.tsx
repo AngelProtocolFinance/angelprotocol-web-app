492b1702a185933730453e0bdfb698b34f34c262
Nenad <nenad@angelprotocol.io>
2023-01-30 09:22:32 +0100

    Fix split radtio on widget and config page

--- src/components/Split/Portion.tsx
+++ src/components/Split/Portion.tsx
@@ -29,7 +29,7 @@ export default function Portion(props: Props) {
     : null;
 
   return (
-    <div className="flex flex-col items-center w-40 xl:w-48 aspect-square p-6 bg-orange-l6 dark:bg-blue-d6 border border-prim rounded">
+    <div className="flex flex-col items-center w-1/2 xl:w-48 aspect-square p-6 bg-orange-l6 dark:bg-blue-d6 border border-prim rounded">
       <p className="uppercase font-bold text-sm sm:text-base">{props.title}</p>
       <p className="text-xs sm:text-sm mb-2 font-bold">{props.percentage}%</p>
       <p className="uppercase text-xs sm:text-sm text-center font-body">
