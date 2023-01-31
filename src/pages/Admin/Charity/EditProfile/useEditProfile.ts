99b26ec460e91f036e7f3d5e929d7b695e2b5615
ap-justin <justin@angelprotocol.io>
2023-01-31 11:18:44 +0800

    improve loading and error prompts

--- src/pages/Admin/Charity/EditProfile/useEditProfile.ts
+++ src/pages/Admin/Charity/EditProfile/useEditProfile.ts
@@ -7,6 +7,7 @@ import { useModalContext } from "contexts/ModalContext";
 import { useGetWallet } from "contexts/WalletContext";
 import { ImgLink } from "components/ImgEditor";
 import { TxPrompt } from "components/Prompt";
+import { isEmpty } from "helpers";
 import { getPayloadDiff } from "helpers/admin";
 import { getFullURL, uploadFiles } from "helpers/uploadFiles";
 import { appRoutes } from "constants/routes";
@@ -18,7 +19,7 @@ export default function useEditProfile() {
   const { endowmentId, endowment } = useAdminResources();
   const {
     handleSubmit,
-    formState: { isSubmitting, isDirty },
+    formState: { isSubmitting },
   } = useFormContext<FV>();
 
   const { showModal } = useModalContext();
@@ -33,54 +34,70 @@ export default function useEditProfile() {
     categories_sdgs,
     ...newData
   }) => {
-    const [bannerUrl, logoUrl] = await uploadImgs([image, logo]);
+    try {
+      const [bannerUrl, logoUrl] = await uploadImgs([image, logo], () => {
+        showModal(TxPrompt, { loading: "Uploading images.." });
+      });
 
-    const changes: FlatFormValues = {
-      image: bannerUrl,
-      logo: logoUrl,
-      hq_country: hq_country.name,
-      categories_sdgs: categories_sdgs.map((opt) => opt.value),
-      ...newData,
-    };
-    const diff = getPayloadDiff(initial, changes);
+      const changes: FlatFormValues = {
+        image: bannerUrl,
+        logo: logoUrl,
+        hq_country: hq_country.name,
+        categories_sdgs: categories_sdgs.map((opt) => opt.value),
+        ...newData,
+      };
+      const diff = getPayloadDiff(initial, changes);
 
-    if (Object.entries(diff).length <= 0) {
-      return showModal(TxPrompt, { error: "No changes detected" });
-    }
+      if (Object.entries(diff).length <= 0) {
+        return showModal(TxPrompt, { error: "No changes detected" });
+      }
 
-    /** already clean - no need to futher clean "": to unset values { field: val }, field must have a value 
+      /** already clean - no need to futher clean "": to unset values { field: val }, field must have a value 
      like ""; unlike contracts where if fields is not present, val is set to null.
     */
-    const updates: Partial<EndowmentProfileUpdate> = {
-      ...diff,
-      id: endowmentId,
-      owner: endowment.owner,
-    };
-    const result = await submit(await createADR36Payload(updates, wallet!)); //wallet is asserted in admin guard
-    if ("error" in result) {
-      return showModal(TxPrompt, { error: "Failed to update profile" });
-    }
+      const updates: Partial<EndowmentProfileUpdate> = {
+        ...diff,
+        id: endowmentId,
+        owner: endowment.owner,
+      };
 
-    return showModal(TxPrompt, {
-      success: {
-        message: "Profile successfully updated",
-        link: {
-          description: "View changes",
-          url: `${appRoutes.profile}/${endowmentId}`,
+      showModal(TxPrompt, { loading: "Signing changes" });
+      const payload = await createADR36Payload(updates, wallet!);
+
+      const result = await submit(payload); //wallet is asserted in admin guard
+      if ("error" in result) {
+        return showModal(TxPrompt, { error: "Failed to update profile" });
+      }
+
+      return showModal(TxPrompt, {
+        success: {
+          message: "Profile successfully updated",
+          link: {
+            description: "View changes",
+            url: `${appRoutes.profile}/${endowmentId}`,
+          },
         },
-      },
-    });
+      });
+    } catch (err) {
+      showModal(TxPrompt, {
+        error: err instanceof Error ? err.message : "Unknown error occured",
+      });
+    }
   };
 
   return {
     editProfile: handleSubmit(editProfile),
-    isSubmitDisabled: isSubmitting || !isDirty,
+    isSubmitDisabled: isSubmitting,
     id: endowmentId,
   };
 }
 
-async function uploadImgs(imgs: ImgLink[]): Promise<string[]> {
+async function uploadImgs(
+  imgs: ImgLink[],
+  onUpload: () => void
+): Promise<string[]> {
   const files = imgs.flatMap((img) => (img.file ? [img.file] : []));
+  if (!isEmpty(files)) onUpload();
   const baseURL = await uploadFiles(files, "endow-profiles");
   return imgs.map((img) =>
     img.file && baseURL ? getFullURL(baseURL, img.file.name) : img.publicUrl
