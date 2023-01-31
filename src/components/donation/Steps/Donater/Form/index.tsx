06492e674e5788e4a8f9059634e9592e99c66b65
Nenad <nenad@angelprotocol.io>
2023-01-31 07:53:13 +0100

    Display donater Cancel btn only outside widget

--- src/components/donation/Steps/Donater/Form/index.tsx
+++ src/components/donation/Steps/Donater/Form/index.tsx
@@ -1,6 +1,7 @@
 import { useFormContext } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { matchRoutes, useLocation } from "react-router-dom";
 import { DonateValues } from "../types";
 import TokenField from "components/TokenField";
 import { useGetter } from "store/accessors";
@@ -19,8 +20,12 @@ export default function Form(props: {
     formState: { isValid, isDirty, isSubmitting },
   } = useFormContext<DonateValues>();
 
+  const isInsideWidget = useIsInsideWidget();
+
   const endowId = useGetter((state) => state.donation.recipient?.id);
+
   const dispatch = useDispatch();
+
   function submit(data: DonateValues) {
     dispatch(setDetails(data));
     reset();
@@ -44,15 +49,21 @@ export default function Form(props: {
         <AdvancedOptions classes="mt-10" unfold={props.unfoldAdvOpts} />
       )}
 
-      <div className="grid grid-cols-2 gap-5 font-body mt-8 md:mt-12">
-        <Link
-          className="btn-outline-filled btn-donate"
-          to={`${appRoutes.profile}/${endowId}`}
-        >
-          Cancel
-        </Link>
+      <div
+        className={`flex ${
+          isInsideWidget ? "justify-center" : "justify-between"
+        } font-body mt-8 md:mt-12`}
+      >
+        {!isInsideWidget && (
+          <Link
+            className="btn-outline-filled btn-donate"
+            to={`${appRoutes.profile}/${endowId}`}
+          >
+            Cancel
+          </Link>
+        )}
         <button
-          className="btn-orange btn-donate"
+          className="btn-orange btn-donate w-44"
           disabled={
             !isValid || (wasCompleted ? false : !isDirty) || isSubmitting
           }
@@ -63,4 +74,15 @@ export default function Form(props: {
       </div>
     </form>
   );
+}
+
+function useIsInsideWidget() {
+  const location = useLocation();
+
+  const isInsideWidget = !!matchRoutes(
+    [{ path: appRoutes.donate_widget + "/:id" }],
+    location
+  );
+
+  return isInsideWidget;
 }
