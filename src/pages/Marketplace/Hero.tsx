1ff81ad3a6065488de95f09588b254736b6a46e3
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-02-01 11:38:18 +0000

    new hero banner, image, and text; (#1689)
    * new hero banner, image, and text;
    
    * fix failing test;
    
    * new hero image w/ 4 logos;

--- src/pages/Marketplace/Hero.tsx
+++ src/pages/Marketplace/Hero.tsx
@@ -1,4 +1,4 @@
-import character from "assets/images/three-logos-white.png";
+import heroImage from "assets/images/bhm-hero.png";
 import ExtLink from "components/ExtLink";
 
 export default function Hero({ classes = "" }: { classes?: string }) {
@@ -8,25 +8,25 @@ export default function Hero({ classes = "" }: { classes?: string }) {
     >
       <h1 className="order-1 font-extrabold w-full text-[1.63rem] md:text-3xl lg:text-[2.75rem] text-center lg:text-left lg:self-end">
         <span className="lg:block leading-tight">
-          Support longer, healthier lives worldwide
+          Reflecting on the past,{" "}
+          <span className="text-orange">building for the future</span>
         </span>
       </h1>
       <img
-        src={character}
+        src={heroImage}
         alt=""
         className="order-3 row-span-2 self-end w-[420px] aspect-square"
       />
       <p className="order-2 lg:order-3 text-[1.13rem] lg:text-2xl w-full leading-relaxed text-center lg:text-left">
-        Every second, someone dies of age-related diseases such as Parkinson's
-        or Alzheimer's. If your support helps shorten the timetable for curing
-        these diseases by just one second, you will have saved someone's life.
+        Racial bias in philanthropic giving has resulted in an underfunding of
+        Black communities by $2 billion. Despite this, Black led nonprofits and
+        businesses outperform when equipped with the resources needed.
         <span className="my-4 block">
-          If you contribute now, your donation will be doubled thanks to $10K in
-          matches from the Angel Alliance.
+          TOGETHER WE CAN CREATE A MORE JUST AND EQUITABLE FUTURE!
         </span>
         <ExtLink
           className="z-[1] inline-block rounded-md btn-orange uppercase px-4 py-2 mb-16 font-bold text-sm mt-5"
-          href="https://longevity.angelprotocol.io"
+          href="https://blackhistorymonth.angelprotocol.io"
         >
           Donate Now
         </ExtLink>
