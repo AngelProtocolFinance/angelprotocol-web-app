fd8687b222df81b7553364745bfd04483b8afe2b
ap-justin <justin@angelprotocol.io>
2023-02-01 13:05:01 +0800

    schema and default values

--- src/pages/Admin/Charity/EditProfile/types.ts
+++ src/pages/Admin/Charity/EditProfile/types.ts
@@ -8,19 +8,18 @@ type K = keyof EndowmentProfileUpdate;
 const _logo: K = "logo";
 const _img: K = "image";
 const _country: K = "hq_country";
+const _activity_countries: K = "active_in_countries";
 const _sdgs: K = "categories_sdgs";
 const _general: K = "categories_general";
 const _id: K = "id";
 const _tier: K = "tier";
 const _owner: K = "owner";
-const _active_countries: K = "active_in_countries";
 
 export type FlatFormValues = Omit<
   EndowmentProfileUpdate,
   /** to flatten */
   /** don't include for now */
   | typeof _general
-  | typeof _active_countries
   /** not editable fields*/
   | typeof _id
   | typeof _tier
@@ -29,11 +28,16 @@ export type FlatFormValues = Omit<
 
 export type FormValues = Omit<
   FlatFormValues,
-  typeof _logo | typeof _img | typeof _country | typeof _sdgs
+  | typeof _logo
+  | typeof _img
+  | typeof _country
+  | typeof _sdgs
+  | typeof _activity_countries
 > & {
   [_logo]: ImgLink;
   [_img]: ImgLink;
   [_country]: CountryOption;
   [_sdgs]: OptionType<UNSDG_NUMS>[];
+  [_activity_countries]: OptionType<string>[];
   initial: FlatFormValues;
 };
