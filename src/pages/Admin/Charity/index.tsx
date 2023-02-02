98c16870ab787d39a8deb3270592e76c0d1f4999
ap-justin <justin@angelprotocol.io>
2023-02-02 23:05:02 +0800

    remove view layer

--- src/pages/Admin/Charity/index.tsx
+++ src/pages/Admin/Charity/index.tsx
@@ -1,17 +1,25 @@
-import { AdminMobileNavPortal } from "App/Header/MobileNav";
-import { useAdminResources } from "../Guard";
+import { Route, Routes } from "react-router-dom";
+import { adminRoutes } from "constants/routes";
+import Proposal from "../Proposal";
+import Proposals from "../Proposals";
+import Dashboard from "./Dashboard";
+import EditProfile from "./EditProfile";
 import Nav from "./Nav";
-import Views from "./Views";
+import Templates from "./Templates";
+import Withdraws from "./Withdraws";
 
 export default function Charity() {
-  const { endowmentId } = useAdminResources();
   return (
     <div className="padded-container grid grid-rows-[auto_1fr] gap-2 pt-4 pb-8 font-work">
       <Nav />
-      <div className="block md:hidden">
-        <AdminMobileNavPortal id={endowmentId} />
-      </div>
-      <Views />
+      <Routes>
+        <Route path={`${adminRoutes.proposal}/:id`} element={<Proposal />} />
+        <Route path={adminRoutes.proposals} element={<Proposals />} />
+        <Route path={`${adminRoutes.templates}/*`} element={<Templates />} />
+        <Route path={adminRoutes.withdraws} element={<Withdraws />} />
+        <Route path={adminRoutes.edit_profile} element={<EditProfile />} />
+        <Route index element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
