16e3d5778106c47a95a4ab586a10edf8ab11581e
Nenad <nenad@angelprotocol.io>
2023-02-02 10:19:06 +0100

    Extract useCreateWidgetUrl

--- src/pages/AIF/WidgetConfigurer/WidgetConfigurer.tsx
+++ src/pages/AIF/WidgetConfigurer/WidgetConfigurer.tsx
@@ -1,21 +1,12 @@
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { URL_PARAMS, UrlParamValues } from "pages/DonateWidget";
-import { useErrorContext } from "contexts/ErrorContext";
 import Copier from "components/Copier";
-import { isEmpty } from "helpers";
-import { UnexpectedStateError } from "errors/errors";
-import { IS_TEST } from "constants/env";
-import { appRoutes } from "constants/routes";
 import WidgetExample from "./WidgetExample";
 import WidgetUrlGenerator from "./WidgetUrlGenerator";
 import { FormValues } from "./WidgetUrlGenerator/schema";
+import useCreateWidgerUrl from "./useCreateWidgerUrl";
 
 const TITLE_STYLE = "text-lg sm:text-2xl font-heading font-bold";
-
-const APP_URL = IS_TEST
-  ? "http://localhost:4200"
-  : "https://app.angelprotocol.io";
 
 export default function WidgetConfigurer() {
   const { id } = useParams<{ id: string }>();
@@ -84,51 +75,4 @@ export default function WidgetConfigurer() {
       </div>
     </div>
   );
-}
-
-const useCreateWidgerUrl = () => {
-  const { handleError } = useErrorContext();
-
-  const createWidgetUrl = useCallback(
-    (endowId: string | undefined, formValues: FormValues) => {
-      const rootUrl = `${APP_URL}${appRoutes.donate_widget}/${endowId}?apiKey=API_KEY`;
-
-      if (!endowId) {
-        handleError(new UnexpectedStateError(`Endowment ID is undefined`));
-        return rootUrl;
-      }
-
-      const param1 = append(formValues.hideText, URL_PARAMS.hideText);
-      const param2 = append(
-        formValues.hideAdvancedOptions,
-        URL_PARAMS.hideAdvancedOptions
-      );
-      const param3 = append(
-        !formValues.hideAdvancedOptions && formValues.unfoldAdvancedOptions,
-        URL_PARAMS.unfoldAdvancedOptions
-      );
-      const param4 = append(
-        !!formValues.liquidPercentage,
-        URL_PARAMS.liquidPercentage,
-        formValues.liquidPercentage
-      );
-      const param5 = append(
-        !isEmpty(formValues.availableCurrencies),
-        URL_PARAMS.availableCurrencies,
-        formValues.availableCurrencies.map((x) => x.value).join(",")
-      );
-      return `${rootUrl}${param1}${param2}${param3}${param4}${param5}`;
-    },
-    [handleError]
-  );
-
-  return createWidgetUrl;
-};
-
-function append(
-  condition: boolean,
-  name: keyof UrlParamValues,
-  values?: string | number
-): string {
-  return condition ? `&${name}${!values ? "" : `=${values}`}` : "";
 }
