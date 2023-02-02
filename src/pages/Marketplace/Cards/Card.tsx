a44914a02fba72554d1907cad9189d78c919e384
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-02-02 11:45:29 +0800

    card height fixed so all cards are same size

--- src/pages/Marketplace/Cards/Card.tsx
+++ src/pages/Marketplace/Cards/Card.tsx
@@ -23,7 +23,7 @@ export default function Card({
   kyc_donors_only,
 }: EndowmentCard) {
   return (
-    <div className="relative overflow-clip dark:bg-blue-d6 rounded-lg border border-prim hover:border-blue dark:hover:border-blue">
+    <div className="relative overflow-clip dark:bg-blue-d6 rounded-lg border border-prim hover:border-blue dark:hover:border-blue h-[430px]">
       <div className="absolute top-[14px] left-[14px] right-[14px] flex justify-between gap-3">
         <p className="bg-orange-l1 text-white font-semibold text-2xs rounded-sm uppercase px-2 py-0.5 font-heading">
           {endow_type === "Charity" ? "Non-profit" : "For-profit"}
