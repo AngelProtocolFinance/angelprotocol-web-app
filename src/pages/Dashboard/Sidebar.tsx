83899af563d694964d00be501005d356a4c3c64e
Nenad <nenad@angelprotocol.io>
2023-02-03 09:44:40 +0100

    Add overflow and scroller to sidebar

--- src/pages/Dashboard/Sidebar.tsx
+++ src/pages/Dashboard/Sidebar.tsx
@@ -2,6 +2,6 @@ type Props = {};
 
 export default function Sidebar(props: Props) {
   return (
-    <div className="grid w-64 max-h-[1383px] bg-white border-r border-prim"></div>
+    <div className="grid w-64 max-h-[1383px] bg-white border-r border-prim overflow-y-auto scroller"></div>
   );
 }
