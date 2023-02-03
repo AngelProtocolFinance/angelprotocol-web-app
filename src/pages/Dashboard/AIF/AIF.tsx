d9152e1f2c3c086ebd9e7de9104d0acc908369f2
Nenad <nenad@angelprotocol.io>
2023-02-03 12:01:05 +0100

    Move ProfileContext to AIF

--- src/pages/Dashboard/AIF/AIF.tsx
+++ src/pages/Dashboard/AIF/AIF.tsx
@@ -1,10 +1,33 @@
+import { useParams } from "react-router-dom";
+import { useProfileQuery } from "services/aws/aws";
+import ProfileContext from "contexts/ProfileContext";
+import QueryLoader from "components/QueryLoader";
+import { idParamToNum } from "helpers";
 import Dashboard from "../Dashboard";
 import { LINK_GROUPS } from "./constants";
 
 export default function AIF() {
+  const { id } = useParams<{ id: string }>();
+  const numId = idParamToNum(id);
+  const queryState = useProfileQuery(numId, {
+    skip: numId === 0,
+  });
+
   return (
-    <Dashboard linkGroups={LINK_GROUPS}>
-      <div>AIF</div>
-    </Dashboard>
+    <QueryLoader
+      queryState={queryState}
+      messages={{
+        error: "Error fetching profile",
+        loading: "Loading profile",
+      }}
+    >
+      {(endowmentProfile) => (
+        <ProfileContext.Provider value={endowmentProfile}>
+          <Dashboard linkGroups={LINK_GROUPS}>
+            <div>AIF</div>
+          </Dashboard>
+        </ProfileContext.Provider>
+      )}
+    </QueryLoader>
   );
 }
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,37 +1,14 @@
 import { PropsWithChildren } from "react";
-import { useParams } from "react-router-dom";
 import { LinkGroup } from "./types";
-import { useProfileQuery } from "services/aws/aws";
-import ProfileContext from "contexts/ProfileContext";
-import QueryLoader from "components/QueryLoader";
-import { idParamToNum } from "helpers";
 import Sidebar from "./Sidebar";
 
 type Props = PropsWithChildren<{ linkGroups: LinkGroup[] }>;
 
 export default function Dashboard({ children, linkGroups }: Props) {
-  const { id } = useParams<{ id: string }>();
-  const numId = idParamToNum(id);
-  const queryState = useProfileQuery(numId, {
-    skip: numId === 0,
-  });
-
   return (
-    <QueryLoader
-      queryState={queryState}
-      messages={{
-        error: "Error fetching profile",
-        loading: "Loading profile",
-      }}
-    >
-      {(endowmentProfile) => (
-        <ProfileContext.Provider value={endowmentProfile}>
-          <div className="grid grid-cols-[auto_1fr] w-full h-full">
-            <Sidebar linkGroups={linkGroups} />
-            <div className="p-10 h-full">{children}</div>
-          </div>
-        </ProfileContext.Provider>
-      )}
-    </QueryLoader>
+    <div className="grid grid-cols-[auto_1fr] w-full h-full">
+      <Sidebar linkGroups={linkGroups} />
+      <div className="p-10 h-full">{children}</div>
+    </div>
   );
 }
