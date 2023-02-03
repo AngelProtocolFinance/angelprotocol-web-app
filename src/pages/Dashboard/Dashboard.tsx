66058b986a3b0cdb781969210c63b708def5eb4b
Nenad <nenad@angelprotocol.io>
2023-02-03 09:44:30 +0100

    Extract Sidebar

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import { PropsWithChildren } from "react";
+import Sidebar from "./Sidebar";
 
 type Props = PropsWithChildren<{}>;
 
 export default function Dashboard(props: Props) {
   return (
     <div className="grid grid-cols-[auto_1fr] w-full h-full">
-      <div className="grid w-64 max-h-[1383px] bg-white border-r border-prim"></div>
+      <Sidebar />
       <div className="p-10 h-full">{props.children}</div>
     </div>
   );
