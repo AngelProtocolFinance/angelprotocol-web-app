54374699fd7abf7ffc156d31b22e563a532caa55
Nenad <nenad@angelprotocol.io>
2023-02-03 11:03:17 +0100

    Add ProfileContext to Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,36 @@
 import { PropsWithChildren } from "react";
+import { useParams } from "react-router-dom";
+import { useProfileQuery } from "services/aws/aws";
+import ProfileContext from "contexts/ProfileContext";
+import QueryLoader from "components/QueryLoader";
+import { idParamToNum } from "helpers";
 import Sidebar from "./Sidebar";
 
 type Props = PropsWithChildren<{}>;
 
 export default function Dashboard(props: Props) {
+  const { id } = useParams<{ id: string }>();
+  const numId = idParamToNum(id);
+  const queryState = useProfileQuery(numId, {
+    skip: numId === 0,
+  });
+
   return (
-    <div className="grid grid-cols-[auto_1fr] w-full h-full">
-      <Sidebar />
-      <div className="p-10 h-full">{props.children}</div>
-    </div>
+    <QueryLoader
+      queryState={queryState}
+      messages={{
+        error: "Error fetching profile",
+        loading: "Loading profile",
+      }}
+    >
+      {(endowmentProfile) => (
+        <ProfileContext.Provider value={endowmentProfile}>
+          <div className="grid grid-cols-[auto_1fr] w-full h-full">
+            <Sidebar />
+            <div className="p-10 h-full">{props.children}</div>
+          </div>
+        </ProfileContext.Provider>
+      )}
+    </QueryLoader>
   );
 }
