f95a9c3e70340ba5539462357026e34487fe9a6f
Nenad <nenad@angelprotocol.io>
2023-02-03 09:42:17 +0100

    Add Dashboard layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,12 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+import { PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{}>;
+
+export default function Dashboard(props: Props) {
+  return (
+    <div className="grid grid-cols-[auto_1fr] w-full h-full">
+      <div className="grid w-64 max-h-[1383px] bg-white border-r border-prim"></div>
+      <div className="p-10 h-full">{props.children}</div>
+    </div>
+  );
 }
