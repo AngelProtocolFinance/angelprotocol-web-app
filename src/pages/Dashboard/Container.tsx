e82eafea226e4c82b1a5e4d4a88f9a6c8f293528
Nenad <nenad@angelprotocol.io>
2023-02-06 13:51:17 +0100

    Create NavigationButton

--- src/pages/Dashboard/Container.tsx
+++ src/pages/Dashboard/Container.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
 import { LinkGroup } from "./types";
+import NavigationButton from "./NavigationButton";
 import Sidebar from "./Sidebar";
 
 type Props = PropsWithChildren<{ linkGroups: LinkGroup[] }>;
@@ -7,12 +8,7 @@ type Props = PropsWithChildren<{ linkGroups: LinkGroup[] }>;
 export default function Container({ children, linkGroups }: Props) {
   return (
     <div className="grid sm:grid-cols-[auto_1fr] w-full h-full">
-      <button
-        type="button"
-        className="sm:hidden flex items-center gap-2 py-5 px-6 dark:bg-blue-d6 border-b border-prim font-bold text-sm"
-      >
-        active
-      </button>
+      <NavigationButton className="sm:hidden" linkGroups={linkGroups} />
       <Sidebar linkGroups={linkGroups} className="max-sm:hidden" />
       <div className="max-sm:py-8 max-sm:px-6 sm:p-10 h-full">{children}</div>
     </div>
---
+++ src/pages/Dashboard/NavigationButton.tsx
@@ -0,0 +1,34 @@
+import { Dialog } from "@headlessui/react";
+import { LinkGroup } from "./types";
+import { useModalContext } from "contexts/ModalContext";
+import Sidebar from "./Sidebar";
+
+type Props = { className?: string; linkGroups: LinkGroup[] };
+
+export default function ActivePageButton({
+  className = "",
+  linkGroups,
+}: Props) {
+  const { showModal } = useModalContext();
+
+  const openSidebar = () => showModal(MobileSidebar, { linkGroups });
+
+  return (
+    <button
+      type="button"
+      onClick={openSidebar}
+      className={`flex items-center gap-2 py-5 px-6 dark:bg-blue-d6 border-b border-prim font-bold text-sm ${className}`}
+    >
+      active
+    </button>
+  );
+}
+
+const MobileSidebar = ({ linkGroups }: { linkGroups: LinkGroup[] }) => (
+  <Dialog.Panel className="fixed top-0 left-0 z-20">
+    <Sidebar
+      linkGroups={linkGroups}
+      className="max-h-screen overflow-y-auto scroller"
+    />
+  </Dialog.Panel>
+);
