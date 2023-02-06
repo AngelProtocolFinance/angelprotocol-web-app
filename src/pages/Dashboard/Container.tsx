618e8dcf6288a05ff141157a3f7ccd3b53d6fb88
Nenad <nenad@angelprotocol.io>
2023-02-06 13:37:08 +0100

    Add navigate button on mobile

--- src/pages/Dashboard/Container.tsx
+++ src/pages/Dashboard/Container.tsx
@@ -7,6 +7,12 @@ type Props = PropsWithChildren<{ linkGroups: LinkGroup[] }>;
 export default function Container({ children, linkGroups }: Props) {
   return (
     <div className="grid sm:grid-cols-[auto_1fr] w-full h-full">
+      <button
+        type="button"
+        className="sm:hidden flex items-center gap-2 py-5 px-6 dark:bg-blue-d6 border-b border-prim font-bold text-sm"
+      >
+        active
+      </button>
       <Sidebar linkGroups={linkGroups} className="max-sm:hidden" />
       <div className="max-sm:py-8 max-sm:px-6 sm:p-10 h-full">{children}</div>
     </div>
