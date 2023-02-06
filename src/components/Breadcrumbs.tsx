f09a34999554dd858f3220ea531a08f314933488
ap-justin <justin@angelprotocol.io>
2023-02-06 14:14:16 +0800

    revert breadcrumb changes

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,35 +1,34 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import { NavLink } from "react-router-dom";
 
 type Props = {
   className?: string;
-  children: string[];
+  items: {
+    title: string;
+    to: string;
+  }[];
 };
 
-export default function Breadcrumbs({ children, className = "" }: Props) {
+export default function Breadcrumbs({ items, className = "" }: Props) {
   return (
     <div className={`flex justify-center items-center gap-1 ${className}`}>
-      {React.Children.map(children, (child, i) => {
-        const [title, to] = child.split("_");
-        return (
-          <Fragment key={i}>
-            <NavLink
-              end={i === 0}
-              to={to}
-              className={({ isActive }) =>
-                `max-w-xs truncate ${
-                  isActive
-                    ? "font-bold cursor-default pointer-events-none"
-                    : "underline hover:text-orange transition ease-in-out duration-300"
-                }`
-              }
-            >
-              {title}
-            </NavLink>
-            {i < children.length - 1 && ">"}
-          </Fragment>
-        );
-      })}
+      {items.map((item, i) => (
+        <Fragment key={i}>
+          <NavLink
+            to={item.to}
+            className={({ isActive }) =>
+              `max-w-xs truncate ${
+                isActive
+                  ? "font-bold cursor-default pointer-events-none"
+                  : "underline hover:text-orange transition ease-in-out duration-300"
+              }`
+            }
+          >
+            {item.title}
+          </NavLink>
+          {i < items.length - 1 && ">"}
+        </Fragment>
+      ))}
     </div>
   );
 }
