b98560822fc2e925141802d9e1e54523f52aadfa
Nenad <nenad@angelprotocol.io>
2023-02-06 13:29:52 +0100

    Hide Sidebar on mobile

--- src/pages/Dashboard/Container.tsx
+++ src/pages/Dashboard/Container.tsx
@@ -7,7 +7,7 @@ type Props = PropsWithChildren<{ linkGroups: LinkGroup[] }>;
 export default function Container({ children, linkGroups }: Props) {
   return (
     <div className="grid grid-cols-[auto_1fr] w-full h-full">
-      <Sidebar linkGroups={linkGroups} />
+      <Sidebar linkGroups={linkGroups} className="max-sm:hidden" />
       <div className="p-10 h-full">{children}</div>
     </div>
   );
