1eda800b28c6a38980f678460c996c2f5e4fb401
Nenad <nenad@angelprotocol.io>
2023-02-06 13:31:12 +0100

    Update body padding on mobile

--- src/pages/Dashboard/Container.tsx
+++ src/pages/Dashboard/Container.tsx
@@ -8,7 +8,7 @@ export default function Container({ children, linkGroups }: Props) {
   return (
     <div className="grid grid-cols-[auto_1fr] w-full h-full">
       <Sidebar linkGroups={linkGroups} className="max-sm:hidden" />
-      <div className="p-10 h-full">{children}</div>
+      <div className="max-sm:py-8 max-sm:px-6 sm:p-10 h-full">{children}</div>
     </div>
   );
 }
