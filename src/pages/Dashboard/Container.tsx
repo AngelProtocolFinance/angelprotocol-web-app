1e6288a0faa8f3d8e3282fb88254627383c47db2
Nenad <nenad@angelprotocol.io>
2023-02-06 13:31:55 +0100

    Update grid cols of Container to show on desktop

--- src/pages/Dashboard/Container.tsx
+++ src/pages/Dashboard/Container.tsx
@@ -6,7 +6,7 @@ type Props = PropsWithChildren<{ linkGroups: LinkGroup[] }>;
 
 export default function Container({ children, linkGroups }: Props) {
   return (
-    <div className="grid grid-cols-[auto_1fr] w-full h-full">
+    <div className="grid sm:grid-cols-[auto_1fr] w-full h-full">
       <Sidebar linkGroups={linkGroups} className="max-sm:hidden" />
       <div className="max-sm:py-8 max-sm:px-6 sm:p-10 h-full">{children}</div>
     </div>
