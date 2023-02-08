ac689798a13a14b8ff66caafdba1c4e4077919dd
Nenad <nenad@angelprotocol.io>
2023-02-08 08:51:47 +0100

    Remove react hook form from widget example

--- src/pages/AIF/WidgetConfigurer/WidgetExample/WidgetExample.tsx
+++ src/pages/AIF/WidgetConfigurer/WidgetExample/WidgetExample.tsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { DonateValues } from "components/donation/Steps/Donater/types";
 import { placeholderChain } from "contexts/WalletContext/constants";
 import APLogo from "components/APLogo";
 import Icon from "components/Icon";
-import Split from "components/Split";
 import Progress from "components/donation/Steps/Progress";
 import { humanize } from "helpers";
-import { FormValues } from "./WidgetUrlGenerator/schema";
+import { FormValues } from "../WidgetUrlGenerator/schema";
 
-type Props = FormValues & { valuesTrigger: boolean };
+type Props = FormValues;
 
 export default function WidgetExample(props: Props) {
   return (
@@ -67,80 +64,68 @@ function Donater({
   hideAdvancedOptions,
   liquidPercentage,
   unfoldAdvancedOptions,
-  valuesTrigger: setValueTrigger,
 }: Props) {
   const token = placeholderChain.tokens[0];
 
-  const methods = useForm<DonateValues>({
-    mode: "onChange",
-    reValidateMode: "onChange",
-    defaultValues: {
-      token: { ...token, amount: "1" },
-      pctLiquidSplit: liquidPercentage,
-      tokens: [{ ...token, amount: "1" }],
-      chainName: placeholderChain.chain_name,
-      chainId: placeholderChain.chain_id,
-    },
-  });
-
-  useEffect(
-    () => methods.setValue("pctLiquidSplit", liquidPercentage),
-    [liquidPercentage, setValueTrigger, methods]
-  );
-
   return (
-    <FormProvider {...methods}>
-      <div className="grid rounded-md w-full">
-        <div className="grid">
-          <div className="flex max-sm:flex-col max-sm:items-start items-center mb-1">
-            <label
-              htmlFor="amount"
-              className="text-lg font-bold mr-auto max-sm:mb-2"
+    <div className="grid rounded-md w-full">
+      <div className="grid">
+        <div className="flex max-sm:flex-col max-sm:items-start items-center mb-1">
+          <label
+            htmlFor="amount"
+            className="text-lg font-bold mr-auto max-sm:mb-2"
+          >
+            Enter the donation amount:
+          </label>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="text-right hover:text-blue text-xs flex"
             >
-              Enter the donation amount:
-            </label>
-            <div className="flex items-center gap-2">
-              <button
-                type="button"
-                className="text-right hover:text-blue text-xs flex"
-              >
-                BAL: {humanize(+token.balance, 3)} {token.symbol}
-              </button>
-            </div>
+              BAL: {humanize(+token.balance, 3)} {token.symbol}
+            </button>
           </div>
-
-          <div className="relative grid grid-cols-[1fr_auto] items-center gap-2 py-3 px-4 dark:bg-blue-d6 border border-prim rounded">
-            <input
-              disabled
-              {...methods.register("token.amount")}
-              type="text"
-              placeholder="0.0000"
-              className="w-full text-sm bg-transparent focus:outline-none dark:text-gray dark:placeholder:text-gray-d1"
-            />
-            <div className="flex items-center gap-1 w-full dark:text-gray">
-              <span className="text-sm">{token.symbol}</span>
-            </div>
-          </div>
-          <p className="text-xs">
-            Minimal amount: {token.symbol} {token.min_donation_amnt}
-          </p>
         </div>
-        {!hideAdvancedOptions && (
-          <AdvancedOptions unfold={unfoldAdvancedOptions} />
-        )}
 
-        <button
-          type="button"
-          className="btn-orange justify-self-center w-44 font-body mt-8 md:mt-12"
-        >
-          Continue
-        </button>
+        <div className="relative grid grid-cols-[1fr_auto] items-center gap-2 py-3 px-4 dark:bg-blue-d6 border border-prim rounded">
+          <input
+            disabled
+            type="text"
+            placeholder="0.0000"
+            className="w-full text-sm bg-transparent focus:outline-none dark:text-gray dark:placeholder:text-gray-d1"
+          />
+          <div className="flex items-center gap-1 w-full dark:text-gray">
+            <span className="text-sm">{token.symbol}</span>
+          </div>
+        </div>
+        <p className="text-xs">
+          Minimal amount: {token.symbol} {token.min_donation_amnt}
+        </p>
       </div>
-    </FormProvider>
+      {!hideAdvancedOptions && (
+        <AdvancedOptions
+          liquidPercentage={liquidPercentage}
+          unfold={unfoldAdvancedOptions}
+        />
+      )}
+
+      <button
+        type="button"
+        className="btn-orange justify-self-center w-44 font-body mt-8 md:mt-12"
+      >
+        Continue
+      </button>
+    </div>
   );
 }
 
-function AdvancedOptions({ unfold }: { unfold: boolean }) {
+function AdvancedOptions({
+  unfold,
+  liquidPercentage,
+}: {
+  liquidPercentage: number;
+  unfold: boolean;
+}) {
   const [isOpen, setIsOpen] = useState(unfold);
 
   function toggle() {
@@ -166,11 +151,36 @@ function AdvancedOptions({ unfold }: { unfold: boolean }) {
       {isOpen && (
         <div className="grid p-6 pt-4 font-heading border-t border-prim">
           <p className="text-xs uppercase font-bold mb-2">Split</p>
-          <Split<DonateValues, "pctLiquidSplit", "token">
-            className="mb-6"
-            liqPctField="pctLiquidSplit"
-            tokenField="token"
-          />
+
+          <div className="grid grid-cols-2 gap-2 mb-6">
+            <div className="flex flex-col items-center p-6 bg-orange-l6 dark:bg-blue-d6 border border-prim rounded">
+              <p className="uppercase font-bold text-sm sm:text-base">
+                Endowment
+              </p>
+              <p className="text-xs sm:text-sm mb-2 font-bold">
+                {100 - liquidPercentage}%
+              </p>
+              <p className="uppercase text-xs sm:text-sm text-center font-body">
+                Compounded forever
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center p-6 bg-orange-l6 dark:bg-blue-d6 border border-prim rounded">
+              <p className="uppercase font-bold text-sm sm:text-base">
+                Current
+              </p>
+              <p className="text-xs sm:text-sm mb-2 font-bold">
+                {liquidPercentage}%
+              </p>
+              <p className="uppercase text-xs sm:text-sm text-center font-body">
+                Instantly available
+              </p>
+              <div className="my-2.5 select-none">
+                <input className="range" type="range" />
+              </div>
+            </div>
+          </div>
+
           <div className="flex items-center gap-4 px-4 py-3 text-center dark:bg-blue-d6 border border-prim rounded">
             <Icon type="Info" size={44} />
             <p className="text-sm leading-normal text-left">
