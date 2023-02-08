f68f9cc97d28cbca693115b8b5ce2fe92f02a65e
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-02-08 15:25:01 +0800

    other referal copy label

--- src/pages/Registration/Steps/ContactDetails/Form/index.tsx
+++ src/pages/Registration/Steps/ContactDetails/Form/index.tsx
@@ -83,7 +83,7 @@ export default function Form({ classes = "" }: { classes?: string }) {
           value === "other" && (
             <Field<FV>
               name="otherReferralMethod"
-              label="Other referral method"
+              label="Please provide additional information"
               required
               classes={{ container: "mt-4" }}
             />
