4cdbb039d96d3c8087cd3392ce1a661a1c0ada21
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-02-10 06:18:58 +0800

    more AP > AG branding changes found

--- src/pages/Admin/Charity/Withdraws/WithdrawTabs/Withdrawer/Submit.tsx
+++ src/pages/Admin/Charity/Withdraws/WithdrawTabs/Withdrawer/Submit.tsx
@@ -71,7 +71,7 @@ function SubmitWithReason({
     return (
       <>
         <Warning classes="mb-4">
-          Withdrawing from endowment funds requires Angel Protocol team
+          Withdrawing from endowment funds requires Angel Giving team
           approval.
         </Warning>
         <Field<WV>
