66fc9b3e9754b2744faeeaf16c93728575215183
Nenad <nenad@angelprotocol.io>
2023-02-10 09:15:46 +0100

    Add formValues state to enable manual snippet update

--- src/pages/WidgetConfigurer/WidgetExample/WidgetExample.tsx
+++ src/pages/WidgetConfigurer/WidgetExample/WidgetExample.tsx
@@ -1,11 +1,10 @@
-import { useFormContext } from "react-hook-form";
 import APLogo from "components/APLogo";
 import { FormValues } from "../WidgetUrlGenerator/schema";
 import Donater from "./Donater";
 
-export default function WidgetExample() {
-  const { watch, getValues } = useFormContext<FormValues>();
+type Props = FormValues;
 
+export default function WidgetExample(props: Props) {
   return (
     <div className="h-full overflow-y-auto scroller w-full xl:w-5/6 max-h-[900px] border border-gray-l2 rounded text-gray-d2 bg-white">
       <div className="grid grid-rows-[1fr_auto] gap-10 h-full">
@@ -19,7 +18,7 @@ export default function WidgetExample() {
             </button>
           </header>
           <section className="flex flex-col items-center gap-5 h-full">
-            {!watch("hideText") && (
+            {!props.hideText && (
               <>
                 <p className="font-body text-xs">
                   Donate today to ENDOWMENT_NAME's endowment. Your donation will
@@ -50,7 +49,7 @@ export default function WidgetExample() {
                 <p className="text-center">Finalize payment</p>
                 <div className="mt-3 h-2 w-full col-span-full bg-gray-l2 rounded-full overflow-hidden" />
               </div>
-              <Donater {...getValues()} />
+              <Donater {...props} />
             </div>
           </section>
         </div>
--- src/pages/WidgetConfigurer/WidgetUrlGenerator/WidgetUrlGenerator.tsx
+++ src/pages/WidgetConfigurer/WidgetUrlGenerator/WidgetUrlGenerator.tsx
@@ -1,62 +1,67 @@
-import { useFormContext } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import Split from "components/Split";
 import { CheckField } from "components/form/CheckField";
 import Combobox from "./Combobox";
 import DenomSelector from "./DenomSelector";
 import { FormValues } from "./schema";
 
-type Props = { onChange(formValues: FormValues): void };
+type Props = {
+  defaultValues: FormValues;
+  onChange(formValues: FormValues): void;
+};
 
-export default function WidgetUrlGenerator({ onChange }: Props) {
-  const { handleSubmit, reset, watch } = useFormContext<FormValues>();
+export default function WidgetUrlGenerator({ defaultValues, onChange }: Props) {
+  const methods = useForm<FormValues>({ defaultValues });
 
-  const hideAdvancedOptions = watch("hideAdvancedOptions");
+  const hideAdvancedOptions = methods.watch("hideAdvancedOptions");
 
   return (
-    <form
-      className="flex flex-col gap-2 xl:w-full xl:max-w-md text-sm font-normal font-body"
-      onSubmit={handleSubmit(onChange)}
-    >
-      <Combobox<FormValues, "endowIdName">
-        fieldName="endowIdName"
-        classes={{ container: "min-h-[3rem]" }}
-      />
-
-      <CheckField<FormValues> name="hideText">Hide text</CheckField>
-
-      <span>Available currencies:</span>
-      <DenomSelector />
-
-      <CheckField<FormValues> name="hideAdvancedOptions">
-        Hide "advanced options"
-      </CheckField>
-
-      <CheckField<FormValues>
-        name="unfoldAdvancedOptions"
-        disabled={hideAdvancedOptions}
-        classes={{ label: "peer-disabled:text-gray" }}
+    <FormProvider {...methods}>
+      <form
+        className="flex flex-col gap-2 xl:w-full xl:max-w-md text-sm font-normal font-body"
+        onSubmit={methods.handleSubmit(onChange)}
       >
-        Unfold "advanced options" by default
-      </CheckField>
+        <Combobox<FormValues, "endowIdName">
+          fieldName="endowIdName"
+          classes={{ container: "min-h-[3rem]" }}
+        />
 
-      <span>Define split value by default:</span>
-      <Split<FormValues, "liquidPercentage", never>
-        className="mb-6 xl:w-96"
-        liqPctField="liquidPercentage"
-      />
+        <CheckField<FormValues> name="hideText">Hide text</CheckField>
 
-      <div className="flex gap-3 w-full max-xl:justify-center mt-8">
-        <button
-          type="reset"
-          className="btn-outline-filled btn-donate max-sm:mx-auto w-40"
-          onClick={() => reset()}
+        <span>Available currencies:</span>
+        <DenomSelector />
+
+        <CheckField<FormValues> name="hideAdvancedOptions">
+          Hide "advanced options"
+        </CheckField>
+
+        <CheckField<FormValues>
+          name="unfoldAdvancedOptions"
+          disabled={hideAdvancedOptions}
+          classes={{ label: "peer-disabled:text-gray" }}
         >
-          Reset Changes
-        </button>
-        <button className="btn-orange btn-donate max-sm:mx-auto w-40">
-          Update Snippet
-        </button>
-      </div>
-    </form>
+          Unfold "advanced options" by default
+        </CheckField>
+
+        <span>Define split value by default:</span>
+        <Split<FormValues, "liquidPercentage", never>
+          className="mb-6 xl:w-96"
+          liqPctField="liquidPercentage"
+        />
+
+        <div className="flex gap-3 w-full max-xl:justify-center mt-8">
+          <button
+            type="reset"
+            className="btn-outline-filled btn-donate max-sm:mx-auto w-40"
+            onClick={() => methods.reset()}
+          >
+            Reset Changes
+          </button>
+          <button className="btn-orange btn-donate max-sm:mx-auto w-40">
+            Update Snippet
+          </button>
+        </div>
+      </form>
+    </FormProvider>
   );
 }
