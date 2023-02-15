870fe66796a205dee4866e50a840d9a2d9e20be0
ap-justin <justin@angelprotocol.io>
2023-02-15 19:24:10 +0800

    generate react app and resolve yarn warnings

---
+++ apps/giving/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./app.module.css";
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="giving" />
+
+      <div />
+    </>
+  );
+}
+
+export default App;
