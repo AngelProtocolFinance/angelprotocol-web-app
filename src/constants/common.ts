aae37ed6af44255d406b8ecd8289becfdd0a5285
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-02-15 14:49:39 +0800

    fix subdomain function

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -2,9 +2,8 @@
 export const APP_NAME = "Angel Giving";
 export const BASE_DOMAIN = "https://angelgiving.io";
 export const DAPP_DOMAIN = "https://app.angelgiving.io";
-export const SUBDOMAIN_BUILDER = (subdomain) =>
-  `https://${subdomain}/angelgiving.io`;
-export const MAIN_LOGO = "";
+export const SUBDOMAIN_BUILDER = (subdomain: string) =>
+  `https://${subdomain}.angelgiving.io`;
 export const SEO_IMAGE =
   "https://charity-profile-images.s3.amazonaws.com/logo/angelprotocol-wings-bl.png";
 export const EMAIL_SUPPORT = "support@angelgiving.io";
