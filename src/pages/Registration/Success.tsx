c912b12b9e2b3f3987679a7bc265c33bdf3f698c
Nenad Misic <nenad@angelprotocol.io>
2023-02-15 02:34:32 +0100

    Update text on Reg. Success (#1776)
    * Update text on Reg. Success
    
    * Remove temp & placeholder data
    
    * Update src/pages/Registration/Success.tsx
    
    Co-authored-by: ap-justin <89639563+ap-justin@users.noreply.github.com>
    
    * Reduce back max width
    
    ---------
    
    Co-authored-by: ap-justin <89639563+ap-justin@users.noreply.github.com>

--- src/pages/Registration/Success.tsx
+++ src/pages/Registration/Success.tsx
@@ -22,18 +22,11 @@ export default function Success({ classes = "" }: { classes?: string }) {
       <h1 className="text-[2rem] font-bold mt-10 text-center">
         {contact.orgName}’s endowment has been created!
       </h1>
-      <p className="mt-6 text-center">
-        Thank you for your interest in Angel Giving!
-      </p>
-      <p className="mb-8 mt-4 text-gray-d1 dark:text-white/75 text-center">
-        Use the button below to start filling out {contact.orgName}’s profile
-        and attract donors!
-      </p>
       <Link
-        className="w-full max-w-[26.25rem] btn-orange btn-reg"
+        className="mt-6 text-orange hover:text-orange-l2 underline decoration-1 hover:decoration-2 text-center text-lg transition ease-in-out duration-300"
         to={`${appRoutes.admin}/${reg.endowId}/${adminRoutes.edit_profile}`}
       >
-        edit profile
+        Start filling out {contact.orgName}’s profile and attract donors!
       </Link>
     </div>
   );
