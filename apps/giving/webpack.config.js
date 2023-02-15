3df078d3c1e4d90a0ca0a053f7e64489d27d64e0
ap-justin <justin@angelprotocol.io>
2023-02-15 19:28:16 +0800

    move config overrides

--- apps/giving/webpack.config.js
+++ apps/giving/webpack.config.js
@@ -5,5 +5,32 @@ const { withReact } = require("@nrwl/react");
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false,
+      },
+    },
+  ];
+  config.resolve.fallback = {
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer"),
+    path: require.resolve("path-browserify"),
+    https: require.resolve("https-browserify"),
+    http: require.resolve("stream-http"),
+    os: require.resolve("os-browserify/browser"),
+    assert: require.resolve("assert/"),
+  };
+  config.plugins.push(
+    new ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    })
+  );
+
   return config;
 });
