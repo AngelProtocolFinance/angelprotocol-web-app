fae4090bfb3bcaf4611569e84cbc6435a6cd965c
ap-justin <justin@angelprotocol.io>
2023-02-16 11:19:10 +0800

    common webpack config

--- apps/giving/jest.config.ts
+++ apps/giving/jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "jest";
 /* eslint-disable */
 const config: Config = {
   displayName: "giving",
-  preset: "../../jest/jest.preset.js",
+  preset: "../../configs/jest/jest.preset.js",
   coverageDirectory: "../../coverage/apps/giving",
 };
 
--- configs/jest/jest.preset.js
+++ configs/jest/jest.preset.js
@@ -12,8 +12,8 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "./jest/__mocks__/file.js",
-    "\\.(css|less)$": "./jest/__mocks__/style.js",
+      "./configs/jest/__mocks__/file.js",
+    "\\.(css|less)$": "./configs/jest/__mocks__/style.js",
   },
-  setupFilesAfterEnv: ["./jest/setupTests.ts"],
+  setupFilesAfterEnv: ["./configs/jest/setupTests.ts"],
 };
