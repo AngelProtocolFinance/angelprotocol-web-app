a8aca4a0ce668f70b799b5f43ff2556c47ce40a3
jp-mariano <juanpaolo.mariano28@gmail.com>
2023-02-17 13:00:43 +0800

    HQ Country attribute refactor

--- src/pages/Profile/Body/Body.tsx
+++ src/pages/Profile/Body/Body.tsx
@@ -41,12 +41,12 @@ export default function Body() {
             </div>
             <p className="w-full font-normal text-lg">{profile.tagline}</p>
           </div>
-          {(profile.hq.country || profile.url) && (
+          {(profile.hq_country || profile.url) && (
             <div className="flex flex-col lg:flex-row gap-4 lg:gap-6 items-center w-full font-semibold text-base">
-              {profile.hq.country && (
+              {profile.hq_country && (
                 <span className="flex items-center gap-2 uppercase">
                   <Icon type="MapPin" className="h-6 w-6 text-orange" />
-                  {profile.hq.country}
+                  {profile.hq_country}
                 </span>
               )}
               {profile.url && (
