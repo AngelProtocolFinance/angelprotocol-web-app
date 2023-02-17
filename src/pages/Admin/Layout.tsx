c8a5a7becbe035c26e9887f134252f897f816adb
Nenad <nenad@angelprotocol.io>
2023-02-17 12:55:03 +0100

    Remove 'divide-x' className from admin layout

--- src/pages/Admin/Layout.tsx
+++ src/pages/Admin/Layout.tsx
@@ -4,7 +4,7 @@ import Sidebar, { SidebarOpener } from "./Sidebar";
 
 export default function Layout({ linkGroups }: { linkGroups: LinkGroup[] }) {
   return (
-    <div className="grid md:grid-cols-[auto_1fr] md:divide-x md:divide-prim">
+    <div className="grid md:grid-cols-[auto_1fr]">
       {/** sidebar */}
       <SidebarOpener className="md:hidden" linkGroups={linkGroups} />
       <Sidebar className="max-md:hidden" linkGroups={linkGroups} />
