7788cfd096873f3d962f77fdfaa5264dc9146d5c
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-02-21 13:31:50 +0800

    hot fix: fix link for main logo

--- src/components/APLogo.tsx
+++ src/components/APLogo.tsx
@@ -1,15 +1,10 @@
 import angelProtocolLogo from "assets/images/angelgiving-H-logo-beta-wht.png";
-import ExtLink from "components/ExtLink";
 import { BASE_DOMAIN } from "constants/common";
 
 export default function APLogo({ className = "" }) {
   return (
-    <ExtLink
-      href={BASE_DOMAIN}
-      title="Go to Marketing page"
-      className={className}
-    >
+    <a href={BASE_DOMAIN} title="Go to Marketing page" className={className}>
       <img src={angelProtocolLogo} alt="" className="w-full h-full" />
-    </ExtLink>
+    </a>
   );
 }
