6dc961b8d05b08266ddb1bc18992ad7c2df38350
Nenad <nenad@angelprotocol.io>
2023-02-22 07:20:22 +0100

    Update Header shown to AP and Review team

--- src/pages/Admin/Sidebar/Header.tsx
+++ src/pages/Admin/Sidebar/Header.tsx
@@ -1,42 +1,60 @@
 import { useProfileQuery } from "services/aws/aws";
 import ContentLoader from "components/ContentLoader";
 import Icon from "components/Icon";
+import APLogo from "components/Logo";
 import Logo from "components/Logo";
 import { useAdminResources } from "../Guard";
 
 export default function Header() {
-  const { id } = useAdminResources();
-  const { data: profile, isLoading, isError } = useProfileQuery(id);
+  const { id, type } = useAdminResources();
+  const isCharity = type === "charity";
+
+  const {
+    data: profile,
+    isLoading,
+    isError,
+  } = useProfileQuery(id, { skip: !isCharity });
 
   return (
     <div className="flex flex-col gap-3 w-full py-6 px-5 border-b border-prim">
       <div className="flex justify-between">
-        <Logo
-          className="w-14 h-14"
-          logo={{ src: profile?.logo, isSrcLoading: isLoading }}
-        />
-
-        {isLoading ? (
-          <ContentLoader className="h-10 w-24" />
+        {isCharity ? (
+          <Logo
+            className="w-14 h-14"
+            logo={{ src: profile?.logo, isSrcLoading: isLoading }}
+          />
         ) : (
-          <button
-            type="button"
-            className="btn-outline gap-2 normal-case h-10 pr-4 pl-3"
-          >
-            <Icon type="Sync" />
-            Switch
-          </button>
+          <APLogo className="w-32" />
         )}
+
+        {isCharity &&
+          (isLoading ? (
+            <ContentLoader className="h-10 w-24" />
+          ) : (
+            <button
+              type="button"
+              className="btn-outline gap-2 normal-case h-10 pr-4 pl-3"
+            >
+              <Icon type="Sync" />
+              Switch
+            </button>
+          ))}
       </div>
 
       <div className="grid gap-1">
         <h5 className="text-sm font-bold truncate">
-          {isLoading ? (
-            <ContentLoader className="h-5 w-full" />
-          ) : isError || !profile ? (
-            "Error loading endowment name"
+          {isCharity ? (
+            isLoading ? (
+              <ContentLoader className="h-5 w-full" />
+            ) : isError || !profile ? (
+              "Error loading endowment name"
+            ) : (
+              profile.name
+            )
+          ) : type === "ap" ? (
+            "Angel Giving Team Admin"
           ) : (
-            profile.name
+            "Charity Applications Review"
           )}
         </h5>
         {/* <span className="text-xs truncate">
