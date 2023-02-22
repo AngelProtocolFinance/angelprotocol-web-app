6a7ac054b8c7be3422f29873b8f20cd577789589
Nenad <nenad@angelprotocol.io>
2023-02-22 07:58:58 +0100

    Update colors of header

--- src/pages/Admin/Sidebar/Header/Header.tsx
+++ src/pages/Admin/Sidebar/Header/Header.tsx
@@ -6,13 +6,21 @@ import CharityHeader from "./CharityHeader";
 export default function Header() {
   const { type } = useAdminResources();
   return (
-    <div className="flex flex-col gap-1 w-full py-6 px-5 border-b border-prim">
+    <div
+      className={`flex flex-col gap-1 w-full py-6 px-5 border-b border-prim ${
+        type === "ap"
+          ? "bg-orange-l2 dark:bg-blue-d4"
+          : type === "review"
+          ? "bg-gray-l2 dark:bg-bluegray"
+          : ""
+      }`}
+    >
       {type === "charity" ? (
         <CharityHeader />
       ) : (
         <>
           <Logo className="w-32" logo={LOGO} />
-          <h5 className="text-sm font-bold truncate mt-2">
+          <h5 className="text-sm text-white font-bold truncate mt-2">
             {type === "ap"
               ? "Angel Giving Team Admin"
               : "Charity Applications Review"}
