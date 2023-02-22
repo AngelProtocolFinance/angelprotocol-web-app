33913a04ed8269aa7da9eef0a1ec93eff19c3d1a
ap-justin <justin@angelprotocol.io>
2023-02-22 22:20:40 +0800

    unknown flag to assets

--- libs/assets/src/icons.ts
+++ libs/assets/src/icons.ts
@@ -1,4 +1,5 @@
 export const alliance = "/assets/icons/angel-alliance.png";
+export const unknownFlag = "/assets/icons/unknown-flag.jpeg";
 export const angelGivingLogo = "/assets/images/angelgiving-H-logo-beta-wht.png";
 export const apHorizBlue = "/assets/images/angelprotocol-horiz-blu.png";
 export const apRounded = "/assets/images/angelprotocol-rounded-logo.png";
