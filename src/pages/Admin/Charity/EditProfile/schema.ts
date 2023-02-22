7198a9c1f09bf077b25c960cd6206ef8c83f5451
Abbas Ali Lokhandwala <meetabbasali1994@gmail.com>
2023-02-22 15:02:53 +0530

    add max limit to sdg field in schema (#1838)

--- src/pages/Admin/Charity/EditProfile/schema.ts
+++ src/pages/Admin/Charity/EditProfile/schema.ts
@@ -5,6 +5,7 @@ import { CountryOption } from "services/types";
 import { ImgLink } from "components/ImgEditor";
 import { genFileSchema } from "schemas/file";
 import { requiredString, url } from "schemas/string";
+import { MAX_SDGS } from "constants/unsdgs";
 
 export const VALID_MIME_TYPES = [
   "image/jpeg",
@@ -22,7 +23,9 @@ const fileObj = Yup.object().shape<SchemaShape<ImgLink>>({
 
 //construct strict shape to avoid hardcoding shape keys
 const shape: SchemaShape<FormValues> = {
-  categories_sdgs: Yup.array().min(1, "required"),
+  categories_sdgs: Yup.array()
+    .min(1, "required")
+    .max(MAX_SDGS, `maximum ${MAX_SDGS} selections allowed`),
   tagline: requiredString.max(140, "max length is 140 chars"),
   image: fileObj,
   logo: fileObj,
