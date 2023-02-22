eb9b904e4ff25a2f66a94027ca26656f96319d96
Nenad <nenad@angelprotocol.io>
2023-02-22 07:36:16 +0100

    Extract CharityHeader to separate file

---
+++ src/pages/Admin/Sidebar/Header/CharityHeader.tsx
@@ -0,0 +1,45 @@
+import { useProfileQuery } from "services/aws/aws";
+import ContentLoader from "components/ContentLoader";
+import Icon from "components/Icon";
+import Logo from "components/Logo";
+import { useAdminResources } from "../../Guard";
+
+export default function CharityHeader() {
+  const { id } = useAdminResources();
+  const { data: profile, isLoading, isError } = useProfileQuery(id);
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <Logo
+          className="w-14 h-14"
+          logo={{ src: profile?.logo, isSrcLoading: isLoading }}
+        />
+
+        {isLoading ? (
+          <ContentLoader className="h-10 w-24" />
+        ) : (
+          <button
+            type="button"
+            className="btn-outline gap-2 normal-case h-10 pr-4 pl-3"
+          >
+            <Icon type="Sync" />
+            Switch
+          </button>
+        )}
+      </div>
+      <h5 className="text-sm font-bold truncate mt-2">
+        {isLoading ? (
+          <ContentLoader className="h-5 w-full" />
+        ) : isError || !profile ? (
+          "Error loading endowment name"
+        ) : (
+          profile.name
+        )}
+      </h5>
+      {/* <span className="text-xs truncate">
+            juno1rhaasmvq6t3a607ua90ufrr8srkr08lxauqnpz
+          </span> */}
+    </>
+  );
+}
--- src/pages/Admin/Sidebar/Header/Header.tsx
+++ src/pages/Admin/Sidebar/Header/Header.tsx
@@ -1,9 +1,7 @@
-import { useProfileQuery } from "services/aws/aws";
-import ContentLoader from "components/ContentLoader";
-import Icon from "components/Icon";
-import APLogo from "components/Logo";
 import Logo from "components/Logo";
+import { LOGO } from "constants/common";
 import { useAdminResources } from "../../Guard";
+import CharityHeader from "./CharityHeader";
 
 export default function Header() {
   const { type } = useAdminResources();
@@ -13,7 +11,7 @@ export default function Header() {
         <CharityHeader />
       ) : (
         <>
-          <APLogo className="w-32" />
+          <Logo className="w-32" logo={LOGO} />
           <h5 className="text-sm font-bold truncate mt-2">
             {type === "ap"
               ? "Angel Giving Team Admin"
@@ -22,45 +20,5 @@ export default function Header() {
         </>
       )}
     </div>
-  );
-}
-
-function CharityHeader() {
-  const { id } = useAdminResources();
-  const { data: profile, isLoading, isError } = useProfileQuery(id);
-
-  return (
-    <>
-      <div className="flex justify-between">
-        <Logo
-          className="w-14 h-14"
-          logo={{ src: profile?.logo, isSrcLoading: isLoading }}
-        />
-
-        {isLoading ? (
-          <ContentLoader className="h-10 w-24" />
-        ) : (
-          <button
-            type="button"
-            className="btn-outline gap-2 normal-case h-10 pr-4 pl-3"
-          >
-            <Icon type="Sync" />
-            Switch
-          </button>
-        )}
-      </div>
-      <h5 className="text-sm font-bold truncate mt-2">
-        {isLoading ? (
-          <ContentLoader className="h-5 w-full" />
-        ) : isError || !profile ? (
-          "Error loading endowment name"
-        ) : (
-          profile.name
-        )}
-      </h5>
-      {/* <span className="text-xs truncate">
-            juno1rhaasmvq6t3a607ua90ufrr8srkr08lxauqnpz
-          </span> */}
-    </>
   );
 }
