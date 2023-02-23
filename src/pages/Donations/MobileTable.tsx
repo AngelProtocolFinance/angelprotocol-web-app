3f7514808f6fb71eecd87cd8bb53e62d2fc16d94
Nenad <nenad@angelprotocol.io>
2023-02-23 11:12:40 +0100

    Remove redundant div around mobiletable items

--- src/pages/Donations/MobileTable.tsx
+++ src/pages/Donations/MobileTable.tsx
@@ -18,71 +18,69 @@ export default function MobileTable({ donations, classes = "" }: TableProps) {
         <div className="p-4 w-28 text-center">Date</div>
       </div>
 
-      <div className="text-sm">
-        {sorted.map((row, index) => (
-          <Disclosure
-            key={index}
-            as="div"
-            className="even:bg-orange-l6 dark:odd:bg-blue-d6 dark:even:bg-blue-d7 w-full border-b border-prim"
-          >
-            {({ open }) => (
-              <>
-                <Disclosure.Button
+      {sorted.map((row, index) => (
+        <Disclosure
+          key={index}
+          as="div"
+          className="text-sm odd:bg-orange-l6 dark:even:bg-blue-d6 dark:odd:bg-blue-d7 w-full border-b border-prim"
+        >
+          {({ open }) => (
+            <>
+              <Disclosure.Button
+                className={`${
+                  open ? "bg-orange-l5 dark:bg-blue-d4" : ""
+                } w-full h-12 grid grid-cols-[auto_1fr_auto] border-b last:border-0 border-prim divide-x divide-prim`}
+              >
+                <DrawerIcon
+                  size={24}
                   className={`${
-                    open ? "bg-orange-l5 dark:bg-blue-d4" : ""
-                  } w-full h-12 grid grid-cols-[auto_1fr_auto] border-b last:border-0 border-prim divide-x divide-prim`}
-                >
-                  <DrawerIcon
-                    size={24}
+                    open ? "text-orange" : ""
+                  } w-12 place-self-center`}
+                  isOpen={open}
+                />
+                <p className="text-sm p-4 text-left h-full truncate">
+                  {row.charityName}
+                </p>
+                <div className="p-4 text-center text-sm w-28">
+                  {new Date(row.date).toLocaleDateString()}
+                </div>
+              </Disclosure.Button>
+              <Disclosure.Panel className="w-full font-work divide-y divide-prim">
+                <Row title="Network">{row.chainName}</Row>
+                <Row title="Currency">{row.symbol}</Row>
+                <Row title="Amount">{humanize(row.amount, 3)}</Row>
+                <Row title="USD Value">{`$${humanize(row.usdValue, 2)}`}</Row>
+                <Row title="TX Hash">{maskAddress(row.hash)}</Row>
+                <Row title="Status">
+                  <div
                     className={`${
-                      open ? "text-orange" : ""
-                    } w-12 place-self-center`}
-                    isOpen={open}
-                  />
-                  <p className="text-sm p-4 text-left h-full truncate">
-                    {row.charityName}
-                  </p>
-                  <div className="p-4 text-center text-sm w-28">
-                    {new Date(row.date).toLocaleDateString()}
+                      row.donationFinalized
+                        ? "bg-green"
+                        : "bg-gray-d1 dark:bg-gray"
+                    } font-body text-white px-2 py-0.5 rounded`}
+                  >
+                    {row.donationFinalized ? "RECEIVED" : "PENDING"}
                   </div>
-                </Disclosure.Button>
-                <Disclosure.Panel className="w-full font-work divide-y divide-prim">
-                  <Row title="Network">{row.chainName}</Row>
-                  <Row title="Currency">{row.symbol}</Row>
-                  <Row title="Amount">{humanize(row.amount, 3)}</Row>
-                  <Row title="USD Value">{`$${humanize(row.usdValue, 2)}`}</Row>
-                  <Row title="TX Hash">{maskAddress(row.hash)}</Row>
-                  <Row title="Status">
-                    <div
-                      className={`${
-                        row.donationFinalized
-                          ? "bg-green"
-                          : "bg-gray-d1 dark:bg-gray"
-                      } font-body text-white px-2 py-0.5 rounded`}
-                    >
-                      {row.donationFinalized ? "RECEIVED" : "PENDING"}
-                    </div>
-                  </Row>
-                  <Row title="Receipt">
-                    <button
-                      className="block"
-                      onClick={() =>
-                        showKYCForm({
-                          type: "post-donation",
-                          txHash: row.hash,
-                          classes: "grid gap-5",
-                        })
-                      }
-                    >
-                      <Icon type="FatArrowDownload" className="text-2xl" />
-                    </button>
-                  </Row>
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
-        ))}
-      </div>
+                </Row>
+                <Row title="Receipt">
+                  <button
+                    className="block"
+                    onClick={() =>
+                      showKYCForm({
+                        type: "post-donation",
+                        txHash: row.hash,
+                        classes: "grid gap-5",
+                      })
+                    }
+                  >
+                    <Icon type="FatArrowDownload" className="text-2xl" />
+                  </button>
+                </Row>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      ))}
     </div>
   );
 }
