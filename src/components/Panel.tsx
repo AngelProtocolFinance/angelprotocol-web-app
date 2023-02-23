4a9bb6c5522487cfebafed93690ba2adbbe962c9
ap-justin <justin@angelprotocol.io>
2023-02-23 09:33:20 +0800

    create panel and apply to wallet modal

---
+++ src/components/Panel.tsx
@@ -0,0 +1,18 @@
+import { Dialog, Transition } from "@headlessui/react";
+import React, { FC } from "react";
+
+const Panel: FC<any> = (props) => (
+  <Transition.Child
+    as={React.Fragment}
+    enter="transform ease-out duration-300"
+    enterFrom="opacity-0"
+    enterTo="opacity-100"
+    leave="transform ease-in duration-300"
+    leaveFrom="opacity-100"
+    leaveTo="opacity-0"
+  >
+    <Dialog.Panel {...props}>{props.children}</Dialog.Panel>
+  </Transition.Child>
+);
+
+export default Panel as typeof Dialog.Panel;
