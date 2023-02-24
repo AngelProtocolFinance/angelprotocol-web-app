108d4d554fc5d0ed822afe4d9f90710717400666
Nenad <nenad@angelprotocol.io>
2023-02-24 07:59:40 +0100

    Add limit prop to donations

--- src/types/aws/apes/donation.ts
+++ src/types/aws/apes/donation.ts
@@ -63,6 +63,7 @@ export type DonationsQueryParams = {
   denomination?: string;
   status?: string;
   start?: number; //to load next page, set start to ItemCutOff + 1
+  limit?: number; // Number of items to be returned per request
 };
 
 type TxDetails = FiatTx | CryptoTx;
