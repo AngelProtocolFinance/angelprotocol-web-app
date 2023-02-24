4ad0bbc24aad9f6569d1de548af89ccc796d64fb
Nenad <nenad@angelprotocol.io>
2023-02-24 07:29:47 +0100

    Refactor activeLink calc

--- src/pages/Admin/Sidebar/SidebarOpener/useSidebarOpener.ts
+++ src/pages/Admin/Sidebar/SidebarOpener/useSidebarOpener.ts
@@ -21,13 +21,17 @@ export default function useSidebarOpener(linkGroups: LinkGroup[]) {
   // includes all the parent paths, but we want to return only the "first parent" link
   // (the one with more path segments), we can find that "first parent" by simply checking the
   // `to` field length -> higher the length, more "recent" the parrent
-  const activeLink =
-    linkGroups
-      .flatMap((g) => g.links)
-      .filter((link) => !!matchPath(`${ADMIN_ROUTE}${link.to}/*`, currPath))
-      .reduce((prev, curr) =>
-        prev.to.length > curr.to.length ? prev : curr
-      ) ?? DEFAULT_LINK;
+  const activeLink = linkGroups
+    .flatMap((g) => g.links)
+    .reduce((prev, curr) => {
+      const match = matchPath(`${ADMIN_ROUTE}${curr.to}/*`, currPath);
+
+      if (!match || prev.to.length > curr.to.length) {
+        return prev;
+      }
+
+      return curr;
+    }, DEFAULT_LINK);
 
   const { showModal, closeModal, isModalOpen } = useModalContext();
 
