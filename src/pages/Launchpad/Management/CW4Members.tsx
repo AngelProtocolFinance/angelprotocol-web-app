2738a6c8c277b7de17e385c2a1b01cc8b0f31170
ap-justin <justin@angelprotocol.io>
2023-02-27 12:23:30 +0800

    cw4 members table

--- src/pages/Launchpad/Management/CW4Members.tsx
+++ src/pages/Launchpad/Management/CW4Members.tsx
@@ -1,12 +1,12 @@
-import { ErrorMessage } from "@hookform/error-message";
 import { useFieldArray, useFormContext } from "react-hook-form";
-import { Label } from "components/form";
+import Icon from "components/Icon";
+import TableSection, { Cells } from "components/TableSection";
 
 const defaultValue = { addr: "", weight: 1 };
 const name = "members";
 
 export default function CW4Members() {
-  const { register, trigger } = useFormContext();
+  const { trigger } = useFormContext();
   const { fields, append, remove } = useFieldArray({
     name,
   });
@@ -15,66 +15,75 @@ export default function CW4Members() {
     append(defaultValue);
     await trigger();
   }
-  function handleRemove(index: number) {
-    return async function () {
-      remove(index);
-      await trigger();
-    };
+  async function handleRemove(index: number) {
+    remove(index);
+    await trigger();
   }
 
   return (
-    <div className="mb-8 grid content-start">
-      {fields.map((field, i) => (
-        <div
-          className="relative mb-7 grid grid-cols-[1fr_4rem] items-center gap-4"
-          key={field.id}
+    <div className="mb-8 grid content-start border border-prim p-8 rounded">
+      <h3 className="text-xl font-bold mb-8">Members</h3>
+      <table>
+        <TableSection
+          type="thead"
+          rowClass="border-b border-prim bg-orange-l6 dark:bg-blue-d7"
         >
-          <div className="grid w-full">
-            <Label className="mb-2" htmlFor={`${name}.${i}.addr`} required>
-              Wallet address
-            </Label>
-            <input
-              {...register(`${name}.${i}.addr`)}
-              key={field.id} // important to include key with field's id
-            />
-          </div>
-          <div className="grid">
-            <Label className="mb-2" htmlFor={`${name}.${i}.weight`} required>
-              Weight
-            </Label>
-            <input
-              {...register(`${name}.${i}.weight`)}
-              key={field.id} // important to include key with field's id
-            />
-          </div>
-          {i !== 0 && (
-            <button
-              type="button"
-              className="absolute -right-8"
-              onClick={handleRemove(i)}
-            >
-              --
-            </button>
-          )}
-          <ErrorMessage
-            name={`${name}.${i}.addr`}
-            as="p"
-            className="absolute -bottom-5 text-xs text-rose-400"
-          />
-          <ErrorMessage
-            name={`${name}.${i}.weight`}
-            as="p"
-            className="absolute -bottom-5 right-0 text-xs text-rose-400"
-          />
-        </div>
-      ))}
+          <Cells type="th" cellClass="text-xs uppercase text-left py-3 px-4">
+            <>Member</>
+            <>Vote weight</>
+            <></>
+          </Cells>
+        </TableSection>
+        <TableSection type="tbody" rowClass="border-b border-prim">
+          {fields.map((field, idx) => (
+            <MemberField key={field.id} idx={idx} onRemove={handleRemove} />
+          ))}
+        </TableSection>
+      </table>
+
       <button
         type="button"
         onClick={handleAppend}
-        className="flex items-center gap-2 justify-self-end text-xs uppercase"
+        className="flex items-center gap-2 justify-self-end text-sm uppercase"
       >
         add another member
       </button>
     </div>
+  );
+}
+
+type Props = {
+  idx: number;
+  onRemove(idx: number): void;
+};
+
+function MemberField({ idx, onRemove }: Props) {
+  const { register } = useFormContext();
+  return (
+    <Cells type="td" cellClass="relative">
+      <input
+        className="w-full focus:outline-none bg-transparent py-3 px-4 text-sm"
+        {...register(`${name}.${idx}.addr`)}
+        key={idx} // important to include key with field's id
+      />
+
+      <input
+        className="w-min focus:outline-none bg-transparent py-3 px-4 text-sm"
+        {...register(`${name}.${idx}.weight`)}
+        key={idx} // important to include key with field's id
+      />
+
+      <button
+        className="text-center absolute-center"
+        type="button"
+        onClick={() => onRemove(idx)}
+      >
+        <Icon
+          type="CloseCircle"
+          size={22}
+          className="text-gray-d1 dark:text-gray"
+        />
+      </button>
+    </Cells>
   );
 }
--- src/pages/Launchpad/Management/index.tsx
+++ src/pages/Launchpad/Management/index.tsx
@@ -2,7 +2,13 @@ import { FormProvider, useForm } from "react-hook-form";
 import Form from "./Form";
 
 export default function Management() {
-  const methods = useForm();
+  const methods = useForm({
+    defaultValues: {
+      members: [
+        { addr: "juno1akkesf6xfuny3upfaq6yfvefzfr8jt2jfhvlw2", weight: 1 },
+      ],
+    },
+  });
 
   return (
     <FormProvider {...methods}>
