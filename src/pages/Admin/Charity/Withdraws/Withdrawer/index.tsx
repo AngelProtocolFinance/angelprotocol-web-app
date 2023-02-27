941a658289498c01ac597bbfef6ced0702eb7ab8
Nenad <nenad@angelprotocol.io>
2023-02-27 15:37:42 +0100

    Fix general Withdrawer layout and tabs style

--- src/pages/Admin/Charity/Withdraws/Withdrawer/index.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/index.tsx
@@ -5,25 +5,26 @@ import { accountTypeDisplayValue } from "../../constants";
 import WithdrawForm from "./WithdrawForm";
 
 const tabs: AccountType[] = ["liquid", "locked"];
+
 export default function Withdrawer({ tokens_on_hand }: EndowmentState) {
   const { state } = useLocation(); //state is set from dashboard withdraw link
   const type = state as AccountType;
   return (
     <Tab.Group
       as="div"
-      className="mt-6 justify-self-center"
+      className="justify-self-center flex flex-col items-center p-8 gap-6 border border-prim rounded"
       defaultIndex={type === "locked" ? 1 : 0}
     >
-      <Tab.List className="grid grid-cols-2 rounded mb-1 overflow-hidden border border-prim p-0.5">
+      <Tab.List className="grid grid-cols-2 place-items-center gap-1 w-full h-10 p-1 border border-prim rounded-3xl">
         {tabs.map((t) => (
           <Tab
             key={t}
             className={({ selected }) =>
               `${
                 selected
-                  ? "bg-blue-l1 dark:bg-blue-d2 text-white"
-                  : "bg-white dark:bg-blue-d6"
-              } rounded px-4 py-2 uppercase text-sm font-bold focus:outline-none`
+                  ? "bg-orange-l5 dark:bg-blue-d4 border border-prim"
+                  : ""
+              } rounded-2xl flex items-center justify-center w-full h-full uppercase text-sm font-bold focus:outline-none`
             }
           >
             {accountTypeDisplayValue[t]}
