c85af474ca2110f03e15da7b1a77702814343f6a
Nenad <nenad@angelprotocol.io>
2023-03-01 18:53:20 +0100

    Refactor Submit

--- src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Submit/SubmitLocked.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Submit/SubmitLocked.tsx
@@ -1,41 +1,16 @@
-import { useFormContext } from "react-hook-form";
-import { WithdrawValues as WV } from "./types";
+import { WithdrawValues as WV } from "../types";
 import { EndowmentDetails } from "types/contracts";
-import { useAdminResources } from "pages/Admin/Guard";
 import { Field } from "components/form";
 import { APP_NAME } from "constants/common";
-import Warning from "./Warning";
+import Warning from "../Warning";
+import WithdrawButton from "./WithdrawButton";
 
-export default function Submit() {
-  const { endow_type, maturity_time } = useAdminResources<"charity">();
-  const {
-    getValues,
-    formState: { isDirty, isValid, isSubmitting },
-  } = useFormContext<WV>();
-  const isSubmitDisabled = !isDirty || !isValid || isSubmitting;
-
-  const type = getValues("type");
-  if (type === "liquid") {
-    return <WithdrawButton disabled={isSubmitDisabled} />;
-  }
-  //check maturity when locked
-  return (
-    <SubmitWithReason
-      endowment={{ endow_type, maturity_time }}
-      isSubmitDisabled={isSubmitDisabled}
-    />
-  );
-}
-
-type SubmitWithReasonProps = {
+type Props = {
   isSubmitDisabled: boolean;
   endowment: Pick<EndowmentDetails, "endow_type" | "maturity_time">;
 };
 
-function SubmitWithReason({
-  isSubmitDisabled,
-  endowment,
-}: SubmitWithReasonProps) {
+export default function SubmitLocked({ isSubmitDisabled, endowment }: Props) {
   if (endowment.endow_type === "charity") {
     return (
       <>
@@ -67,13 +42,5 @@ function SubmitWithReason({
       )}
       <WithdrawButton disabled={!isMatured} />
     </>
-  );
-}
-
-function WithdrawButton({ disabled }: { disabled: boolean }) {
-  return (
-    <button type="submit" disabled={disabled} className="btn-orange py-3">
-      Withdraw
-    </button>
   );
 }
