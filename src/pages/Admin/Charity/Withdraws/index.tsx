1b45777d7469f22d3491c7362f36e59c04831f53
Nenad <nenad@angelprotocol.io>
2023-03-01 19:01:05 +0100

    Move QueryLoader into Withdrawer

--- src/pages/Admin/Charity/Withdraws/index.tsx
+++ src/pages/Admin/Charity/Withdraws/index.tsx
@@ -1,25 +1,11 @@
-import { useAdminResources } from "pages/Admin/Guard";
-import { useStateQuery } from "services/juno/account";
-import QueryLoader from "components/QueryLoader";
 import Transactions from "./Transactions";
 import Withdrawer from "./Withdrawer";
 
 export default function Withdraws() {
-  const { id } = useAdminResources();
-  const queryState = useStateQuery({ id });
-
   return (
     <div className="grid gap-6">
       <h2 className="text-center font-bold text-3xl">Withdraw</h2>
-      <QueryLoader
-        queryState={queryState}
-        messages={{
-          loading: "Loading withdraw form...",
-          error: "Failed to load withdraw form",
-        }}
-      >
-        {(balance) => <Withdrawer {...balance} />}
-      </QueryLoader>
+      <Withdrawer />
 
       <h3 className="uppercase font-extrabold text-2xl mt-6 border-t border-prim pt-2">
         Transactions
