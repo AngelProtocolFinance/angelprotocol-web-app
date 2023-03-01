102c10f8c4b602687bc207d0085bc34c2c790f77
Nenad <nenad@angelprotocol.io>
2023-03-01 19:01:05 +0100

    Move QueryLoader into Withdrawer

--- src/pages/Admin/Charity/Withdraws/Withdrawer/index.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/index.tsx
@@ -1,6 +1,9 @@
 import { Tab } from "@headlessui/react";
 import { useLocation } from "react-router-dom";
-import { AccountType, EndowmentState } from "types/contracts";
+import { AccountType } from "types/contracts";
+import { useAdminResources } from "pages/Admin/Guard";
+import { useStateQuery } from "services/juno/account";
+import QueryLoader from "components/QueryLoader";
 import { accountTypeDisplayValue } from "../../constants";
 import WithdrawForm from "./WithdrawForm";
 
@@ -10,38 +13,57 @@ type TabEntries = [AccTypes, AccTypeDispVal[AccTypes]][];
 
 const tabEntries = Object.entries(accountTypeDisplayValue) as TabEntries;
 
-export default function Withdrawer({ tokens_on_hand }: EndowmentState) {
+export default function Withdrawer() {
   const { state } = useLocation(); //state is set from dashboard withdraw link
   const type = state as AccountType;
+  const { id } = useAdminResources();
+  const queryState = useStateQuery({ id });
+
   return (
-    <Tab.Group
-      as="div"
-      className="justify-self-center flex flex-col items-center p-8 gap-6 border border-prim rounded"
-      defaultIndex={type === "locked" ? 1 : 0}
+    <QueryLoader
+      queryState={queryState}
+      messages={{
+        loading: "Loading withdraw form...",
+        error: "Failed to load withdraw form",
+      }}
     >
-      <Tab.List className="grid grid-cols-2 place-items-center gap-1 w-full h-10 p-1 border border-prim rounded-3xl">
-        {tabEntries.map(([accType, value]) => (
-          <Tab
-            key={`tab-list-${accType}`}
-            className={({ selected }) =>
-              `${
-                selected
-                  ? "bg-orange-l5 dark:bg-blue-d4 border border-prim"
-                  : ""
-              } rounded-2xl flex items-center justify-center w-full h-full uppercase text-sm font-bold focus:outline-none`
-            }
-          >
-            {value}
-          </Tab>
-        ))}
-      </Tab.List>
-      <Tab.Panels>
-        {tabEntries.map(([accType]) => (
-          <Tab.Panel key={`tab-panel-${accType}`} className="w-full max-w-md">
-            <WithdrawForm balance={tokens_on_hand[accType]} type={accType} />
-          </Tab.Panel>
-        ))}
-      </Tab.Panels>
-    </Tab.Group>
+      {({ tokens_on_hand }) => (
+        <Tab.Group
+          as="div"
+          className="justify-self-center flex flex-col items-center p-8 gap-6 border border-prim rounded"
+          defaultIndex={type === "locked" ? 1 : 0}
+        >
+          <Tab.List className="grid grid-cols-2 place-items-center gap-1 w-full h-10 p-1 border border-prim rounded-3xl">
+            {tabEntries.map(([accType, value]) => (
+              <Tab
+                key={`tab-list-${accType}`}
+                className={({ selected }) =>
+                  `${
+                    selected
+                      ? "bg-orange-l5 dark:bg-blue-d4 border border-prim"
+                      : ""
+                  } rounded-2xl flex items-center justify-center w-full h-full uppercase text-sm font-bold focus:outline-none`
+                }
+              >
+                {value}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels>
+            {tabEntries.map(([accType]) => (
+              <Tab.Panel
+                key={`tab-panel-${accType}`}
+                className="w-full max-w-md"
+              >
+                <WithdrawForm
+                  balance={tokens_on_hand[accType]}
+                  type={accType}
+                />
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
+      )}
+    </QueryLoader>
   );
 }
