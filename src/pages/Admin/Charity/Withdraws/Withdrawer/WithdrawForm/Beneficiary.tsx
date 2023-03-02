d4d8657da346637dc87844931bf94a6aa8e69dfb
Nenad <nenad@angelprotocol.io>
2023-03-02 07:56:28 +0100

    Remove Paster from Beneficiary

--- src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Beneficiary.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Beneficiary.tsx
@@ -1,41 +1,32 @@
 import { ErrorMessage } from "@hookform/error-message";
 import { useFormContext } from "react-hook-form";
-import { WithdrawValues } from "../types";
-import Paster from "./Paster";
+import { WithdrawValues } from "./types";
 
 const id = "__beneficiary";
 
 export default function Beneficiary() {
   const {
     register,
-    setValue,
     formState: { errors },
   } = useFormContext<WithdrawValues>();
-
-  const handlePaste = (text: string) => setValue("beneficiary", text);
 
   return (
     <div className="relative grid gap-3 w-full">
       <label htmlFor={id} className="font-bold font-work">
         Destination wallet
       </label>
-      <span
-        className={`flex justify-between items-center p-4 ${
+      <input
+        {...register("beneficiary")}
+        id={id}
+        type="text"
+        autoComplete="off"
+        className={`flex justify-between items-center w-full p-4 ${
           errors?.beneficiary && errors.beneficiary.message
             ? "border-2 border-red dark:border-red-l2"
             : "border border-prim"
-        } rounded`}
-      >
-        <input
-          {...register("beneficiary")}
-          id={id}
-          type="text"
-          autoComplete="off"
-          className="w-full max-w-[12rem] md:max-w-xs truncate focus:outline-none"
-          placeholder="Input wallet address..."
-        />
-        <Paster onPaste={handlePaste} />
-      </span>
+        } rounded truncate focus:outline-none`}
+        placeholder="Input wallet address..."
+      />
       <ErrorMessage
         errors={errors}
         name="beneficiary"
