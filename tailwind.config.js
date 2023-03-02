e1ab3e39b4c989958bc9e8caf257e24d052bf4eb
ap-justin <justin@angelprotocol.io>
2023-03-02 15:11:10 +0800

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/index.html"],
   theme: {
     colors: {
       transparent: colors.transparent,
