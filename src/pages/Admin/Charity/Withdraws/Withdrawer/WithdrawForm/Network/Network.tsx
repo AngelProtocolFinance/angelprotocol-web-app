e7d7734eb2d9c218ceefa5a24037cf4f9d950e21
Nenad <nenad@angelprotocol.io>
2023-03-02 13:27:07 +0100

    Read networks from AWS

--- src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Network/Network.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Network/Network.tsx
@@ -2,11 +2,12 @@ import { Listbox } from "@headlessui/react";
 import { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 import { WithdrawValues } from "../types";
-import junoIcon from "assets/icons/currencies/juno.svg";
+import { useChainsQuery } from "services/apes";
 import Icon, { DrawerIcon } from "components/Icon";
-import Logo from "components/Logo";
+import QueryLoader from "components/QueryLoader";
 
 export default function Network() {
+  const queryState = useChainsQuery({});
   const { register, watch, trigger } = useFormContext<WithdrawValues>();
   const network = watch("network");
 
@@ -21,11 +22,10 @@ export default function Network() {
     <div className="grid gap-3">
       <span className="font-work font-bold">Select network</span>
       <Listbox {...register("network")} as="div" className="relative">
-        <Listbox.Button className="grid grid-cols-[auto_1fr_auto] items-center text-left gap-2 w-full md:w-52 p-4 border border-prim rounded">
-          {({ open }) => (
+        <Listbox.Button className="grid grid-cols-[1fr_auto] items-center text-left gap-2 w-full md:w-52 p-4 border border-prim rounded">
+          {({ open, value }) => (
             <>
-              <Logo logo={{ src: junoIcon }} className="h-5 w-5 rounded-full" />
-              {"JUNO"}
+              <span>{value}</span>
               <DrawerIcon
                 isOpen={open}
                 className="text-2xl text-gray-d1 dark:text-white"
@@ -36,37 +36,46 @@ export default function Network() {
         </Listbox.Button>
 
         <Listbox.Options className="absolute mt-1 z-10 flex flex-col gap-1 w-full p-1 border border-prim rounded bg-white dark:bg-blue-d6 shadow-lg">
-          {[1, 2, 3].map((suppChain) => (
-            <Listbox.Option
-              key={suppChain}
-              value={`juno-1-${suppChain}`}
-              className={({ active, selected }) =>
-                `grid grid-cols-[auto_1fr_auto] items-center gap-2 p-4 pl-2 rounded cursor-pointer ${
-                  selected
-                    ? "bg-orange-l4 dark:bg-blue-d4"
-                    : active
-                    ? "bg-orange-l5 dark:bg-blue-d3"
-                    : ""
-                }`
-              }
-            >
-              {({ selected }) => (
-                <>
-                  <Logo
-                    logo={{ src: junoIcon }}
-                    className="h-5 w-5 rounded-full"
-                  />
-                  {`JUNO ${suppChain}`}
-                  {selected && (
-                    <Icon
-                      type="Check"
-                      className="text-2xl text-gray-d1 dark:text-white"
-                    />
-                  )}
-                </>
-              )}
-            </Listbox.Option>
-          ))}
+          <QueryLoader
+            queryState={queryState}
+            messages={{
+              loading: "Loading available networks...",
+              empty: "No available networks",
+              error: "Error loading networks",
+            }}
+          >
+            {(chains) => (
+              <>
+                {chains.map((chain) => (
+                  <Listbox.Option
+                    key={`listbox-opt-${chain.chain_id}`}
+                    value={chain.chain_id}
+                    className={({ active, selected }) =>
+                      `grid grid-cols-[auto_1fr_auto] items-center gap-2 p-4 pl-2 rounded uppercase cursor-pointer ${
+                        selected
+                          ? "bg-orange-l4 dark:bg-blue-d4"
+                          : active
+                          ? "bg-orange-l5 dark:bg-blue-d3"
+                          : ""
+                      }`
+                    }
+                  >
+                    {({ selected }) => (
+                      <>
+                        {chain.chain_name}
+                        {selected && (
+                          <Icon
+                            type="Check"
+                            className="text-2xl text-gray-d1 dark:text-white"
+                          />
+                        )}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </>
+            )}
+          </QueryLoader>
         </Listbox.Options>
       </Listbox>
     </div>
