f1a504fd469cf0b99cf44cc88f235f3598658e69
ap-justin <justin@angelprotocol.io>
2023-03-02 14:07:46 +0800

    REACT_APP_ to NX_

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
+const nodelib = require("node-stdlib-browser");
+const { ProvidePlugin } = require("webpack");
+const {
+  NodeProtocolUrlPlugin,
+} = require("node-stdlib-browser/helpers/webpack/plugin");
 const { composePlugins, withNx } = require("@nrwl/webpack");
 const { withReact } = require("@nrwl/react");
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), withReact(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
+module.exports = composePlugins(withNx(), withReact(), (config, context) => {
+  const isProd = context.configuration === "production";
+
+  config.stats = "errors-warnings";
+  if (isProd) {
+    config.plugins.push(new SizePlugin({ writeFile: false }));
+  }
+
+  config.resolve.alias = nodelib;
+
+  config.plugins.push(new NodeProtocolUrlPlugin());
+  config.plugins.push(
+    new ProvidePlugin({
+      process: nodelib.process,
+      Buffer: [nodelib.buffer, "Buffer"],
+    })
+  );
+
   return config;
 });
