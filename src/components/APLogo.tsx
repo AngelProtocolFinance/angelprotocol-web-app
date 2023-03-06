59264b83f63bb578c79cfc85fb2ef2d95e6e915d
Nenad <nenad@angelprotocol.io>
2023-03-06 18:30:20 +0100

    Update APLogo to use Image

--- src/components/APLogo.tsx
+++ src/components/APLogo.tsx
@@ -1,10 +1,11 @@
 import angelProtocolLogo from "assets/images/angelgiving-H-logo-beta-wht.svg";
 import { BASE_DOMAIN } from "constants/common";
+import Image from "./Image";
 
 export default function APLogo({ className = "" }) {
   return (
     <a href={BASE_DOMAIN} title="Go to Marketing page" className={className}>
-      <img src={angelProtocolLogo} alt="" className="w-full h-full" />
+      <Image src={angelProtocolLogo} className="w-full h-full" />
     </a>
   );
 }
