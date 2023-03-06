ad06194fd1571c36ba7f15e38a4047a264115d8c
ap-justin <justin@angelprotocol.io>
2023-03-06 13:56:09 +0800

    add box shadow top only

--- src/pages/Launchpad/Layout.tsx
+++ src/pages/Launchpad/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ classes = "" }) {
       className={`w-full h-50 md:w-[90%] max-w-[62.5rem] md:py-8 grid md:grid-cols-[auto_1fr] md:border border-prim rounded-none md:rounded-lg bg-white dark:bg-blue-d6 ${classes}`}
     >
       <Progress classes="mx-6 md:ml-8 md:min-w-[12rem] lg:min-w-[15.5rem]" />
-      <div className="grid z-10 w-full px-6 py-8 md:p-0 md:pr-8">
+      <div className="grid z-10 w-full px-6 py-8 md:p-0 md:pr-8 md:shadow-none shadow-[0px_-5px_5px_0px] shadow-gray-l3/50 dark:shadow-blue-d7">
         <Outlet />
       </div>
     </div>
