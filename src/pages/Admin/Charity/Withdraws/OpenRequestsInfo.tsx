abcfa530170f78e4470aceef1be6d66667151d74
Nenad <nenad@angelprotocol.io>
2023-03-06 16:11:30 +0100

    Fix OpenRequestsInfo link to proposals

--- src/pages/Admin/Charity/Withdraws/OpenRequestsInfo.tsx
+++ src/pages/Admin/Charity/Withdraws/OpenRequestsInfo.tsx
@@ -26,7 +26,7 @@ export default function OpenRequestsInfo() {
           <span className="text-sm">
             There are open requests that need your attention.{" "}
             <Link
-              to={adminRoutes.proposals}
+              to={`../${adminRoutes.proposals}`}
               className="text-orange hover:text-orange-l2 active:text-orange-d1 underline"
             >
               Check them out now
