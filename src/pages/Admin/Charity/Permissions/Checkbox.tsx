483caca32aee3047d5c58f44cff1e7a0214f0be0
Nenad <nenad@angelprotocol.io>
2023-03-10 12:09:44 +0100

    Revert "Extract Checkbox & refactor it"
    This reverts commit f7c206e744b2fbb7856b6579ff4e62f702af040f.

--- src/pages/Admin/Charity/Permissions/Checkbox.tsx
+++
@@ -1,5 +0,0 @@
-import { InputHTMLAttributes } from "react";
-
-export default function Checkbox(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input type="checkbox" className="checkbox-orange" {...props} />;
-}
