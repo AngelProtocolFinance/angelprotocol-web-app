cf94eb6ee260a4a2b1e3bdeeb2dd6539710fe794
ap-justin <justin@angelprotocol.io>
2023-03-14 15:45:43 +0800

    set defautl values

--- src/pages/Admin/Core/Templates/registrar/Config/index.tsx
+++ src/pages/Admin/Core/Templates/registrar/Config/index.tsx
@@ -23,22 +23,37 @@ export default function Config() {
 }
 
 function RegistrarConfigContext(props: RegistrarConfigExtension) {
-  const initialConfigPayload: Partial<RegistrarConfigExtensionPayload> = {
-    accounts_contract: props.accounts_contract,
-    index_fund_contract: props.index_fund, //addr
-    halo_token: props.halo_token,
-    gov_contract: props.gov_contract,
-    charity_shares_contract: props.charity_shares_contract,
-    applications_review: props.applications_review,
-    swaps_router: props.swaps_router,
-    cw3_code: props.cw3_code,
-    cw4_code: props.cw4_code,
+  const initialConfigPayload: RegistrarConfigExtensionPayload = {
+    accounts_contract: props.accounts_contract || "",
+    accounts_settings_controller: props.accounts_settings_controller || "",
+    applications_review: props.applications_review || "",
+    charity_shares_contract: props.charity_shares_contract || "",
+    collector_addr: props.collector_addr || "",
+    donation_match_charites_contract:
+      props.donation_match_charites_contract || "",
+    fundraising_contract: "",
+    gov_contract: props.gov_contract || "",
+    halo_token_lp_contract: props.halo_token_lp_contract || "",
+    halo_token: props.halo_token || "",
+    index_fund_contract: props.index_fund || "",
+    swap_factory: props.swap_factory || "",
+    swaps_router: props.swaps_router || "",
+
+    //wasm codes
+    /** NaN is blank ("") for <input type="number />"
+     *  can't directly use "" since string is not assignable to number
+     */
+    cw3_code: props.cw3_code || NaN,
+    cw4_code: props.cw4_code || NaN,
+    subdao_bonding_token_code: props.subdao_bonding_token_code || NaN,
+    subdao_cw20_token_code: props.subdao_cw20_token_code || NaN,
+    subdao_cw900_code: props.subdao_cw900_code || NaN,
+    subdao_distributor_code: props.subdao_distributor_code || NaN,
+    subdao_gov_code: props.subdao_gov_code || NaN,
   };
 
   const methods = useForm<RegistrarConfigExtensionValues>({
     resolver: yupResolver(schema),
-    mode: "onChange",
-    reValidateMode: "onChange",
     defaultValues: { ...initialConfigPayload, initialConfigPayload },
   });
 
