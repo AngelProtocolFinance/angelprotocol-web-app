5e6c82511f85ab2fe0d2fbe9b18cd672c957a0ce
ap-justin <justin@angelprotocol.io>
2023-03-14 15:46:17 +0800

    update schema

--- src/pages/Admin/Core/Templates/registrar/Config/schema.ts
+++ src/pages/Admin/Core/Templates/registrar/Config/schema.ts
@@ -5,33 +5,31 @@ import { positiveNumber } from "schemas/number";
 import { contractAddr } from "schemas/string";
 import { proposalShape } from "../../../../constants";
 
-const contractAddrSchema = contractAddr.nullable();
 const shape: SchemaShape<RegistrarConfigExtensionValues> = {
   ...proposalShape,
 
-  accounts_contract: contractAddrSchema,
-  accounts_settings_controller: contractAddrSchema,
-  applications_review: contractAddrSchema,
-  charity_shares_contract: contractAddrSchema,
-  collector_addr: contractAddrSchema,
-  donation_match_charites_contract: contractAddrSchema,
-  fundraising_contract: contractAddrSchema,
-  gov_contract: contractAddrSchema,
-  halo_token_lp_contract: contractAddrSchema,
-  halo_token: contractAddrSchema,
-  index_fund_contract: contractAddrSchema,
-  swap_factory: contractAddrSchema,
-  swaps_router: contractAddrSchema,
+  accounts_contract: contractAddr,
+  accounts_settings_controller: contractAddr,
+  applications_review: contractAddr,
+  charity_shares_contract: contractAddr,
+  collector_addr: contractAddr,
+  donation_match_charites_contract: contractAddr,
+  fundraising_contract: contractAddr,
+  gov_contract: contractAddr,
+  halo_token_lp_contract: contractAddr,
+  halo_token: contractAddr,
+  index_fund_contract: contractAddr,
+  swap_factory: contractAddr,
+  swaps_router: contractAddr,
 
   //wasm codes
   cw3_code: positiveNumber,
   cw4_code: positiveNumber,
-  donation_match_code: positiveNumber, // donation matching contract wasm code
-  subdao_bonding_token_code: positiveNumber, // subdao gov token (w/ bonding-curve) wasm code
-  subdao_cw20_token_code: positiveNumber, // subdao gov token (basic CW20) wasm code
-  subdao_cw900_code: positiveNumber, // subdao gov ve-CURVE contract for locked token voting
-  subdao_distributor_code: positiveNumber, // subdao gov fee distributor wasm code
-  subdao_gov_code: positiveNumber, // subdao gov wasm code
+  subdao_bonding_token_code: positiveNumber,
+  subdao_cw20_token_code: positiveNumber,
+  subdao_cw900_code: positiveNumber,
+  subdao_distributor_code: positiveNumber,
+  subdao_gov_code: positiveNumber,
 };
 
 export const schema = Yup.object(shape);
