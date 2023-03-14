c472089550a79b8caab81242dc02d0383723b20c
ap-justin <justin@angelprotocol.io>
2023-03-14 15:29:48 +0800

    make payload required

--- src/types/contracts/registrar.ts
+++ src/types/contracts/registrar.ts
@@ -49,28 +49,29 @@ export type RegistrarConfig = {
 };
 
 export type RegistrarConfigExtensionPayload = {
-  accounts_contract?: string;
-  accounts_settings_controller?: string;
-  applications_review?: string;
-  charity_shares_contract?: string;
-  collector_addr?: string;
-  donation_match_charites_contract?: string;
-  fundraising_contract?: string;
-  gov_contract?: string;
-  halo_token_lp_contract?: string;
-  halo_token?: string;
-  index_fund_contract?: string;
-  swap_factory?: string;
-  swaps_router?: string;
+  //make all required, encourages form default values
+  //submitter responsible for removing unset fields
+  accounts_contract: string;
+  accounts_settings_controller: string;
+  applications_review: string;
+  charity_shares_contract: string;
+  collector_addr: string;
+  donation_match_charites_contract: string;
+  fundraising_contract: string;
+  gov_contract: string;
+  halo_token_lp_contract: string;
+  halo_token: string;
+  index_fund_contract: string;
+  swap_factory: string;
+  swaps_router: string;
   /// WASM CODES
-  cw3_code?: number;
-  cw4_code?: number;
-  donation_match_code?: number; // donation matching contract wasm code
-  subdao_bonding_token_code?: number; // subdao gov token (w/ bonding-curve) wasm code
-  subdao_cw20_token_code?: number; // subdao gov token (basic CW20) wasm code
-  subdao_cw900_code?: number; // subdao gov ve-CURVE contract for locked token voting
-  subdao_distributor_code?: number; // subdao gov fee distributor wasm code
-  subdao_gov_code?: number; // subdao gov wasm code
+  cw3_code: number;
+  cw4_code: number;
+  subdao_bonding_token_code: number;
+  subdao_cw20_token_code: number;
+  subdao_cw900_code: number;
+  subdao_distributor_code: number;
+  subdao_gov_code: number;
 };
 
 export type RegistrarOwnerPayload = {
