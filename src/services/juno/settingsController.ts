2353a5b6f0a6b8a3880cd8791cbf98599efe341e
Nenad <nenad@angelprotocol.io>
2023-03-14 09:34:27 +0100

    Revert "Revert "Remove mock data and console.logs""
    This reverts commit 538a51130be6e4bef99f6988d01c72156a89dc09.

--- src/services/juno/settingsController.ts
+++ src/services/juno/settingsController.ts
@@ -4,93 +4,6 @@ import { contracts } from "constants/contracts";
 import { junoApi } from ".";
 import { genQueryPath } from "./queryContract/genQueryPath";
 
-const data: Res<"endowmentController"> = {
-  data: {
-    endowment_controller: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    name: {
-      gov_controlled: true,
-      modifiable: true,
-      delegate: {
-        address: "juno1fsdjfkdsafdaldfa",
-      },
-      owner_controlled: true,
-    },
-    image: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    logo: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    categories: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: false,
-    },
-    kyc_donors_only: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: false,
-    },
-    split_to_liquid: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    ignore_user_splits: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    donation_match_active: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    beneficiaries_allowlist: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    contributors_allowlist: {
-      gov_controlled: true,
-      modifiable: false,
-      owner_controlled: true,
-    },
-    maturity_allowlist: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    earnings_fee: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    deposit_fee: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    withdraw_fee: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-    aum_fee: {
-      gov_controlled: true,
-      modifiable: true,
-      owner_controlled: true,
-    },
-  },
-};
 const settingsController = contracts.settingsController;
 
 export const settingsController_api = junoApi.injectEndpoints({
@@ -105,16 +18,11 @@ export const settingsController_api = junoApi.injectEndpoints({
           id: settingsControllerTags.endowment_controller,
         },
       ],
-      async queryFn(args) {
-        return {
-          data: data.data,
-        };
+      query: (args) =>
+        genQueryPath("endowmentController", args, settingsController),
+      transformResponse: (res: Res<"endowmentController">) => {
+        return res.data;
       },
-      // query: (args) =>
-      //   genQueryPath("endowmentController", args, settingsController),
-      // transformResponse: (res: Res<"endowmentController">) => {
-      //   return res.data;
-      // },
     }),
   }),
 });
