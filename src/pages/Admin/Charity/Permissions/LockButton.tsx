e5707d88d1695cf73b3c4953d5eb5aadb6d86615
Nenad <nenad@angelprotocol.io>
2023-03-16 08:29:46 +0100

    Update hover/active states for locked forever btn

--- src/pages/Admin/Charity/Permissions/LockButton.tsx
+++ src/pages/Admin/Charity/Permissions/LockButton.tsx
@@ -16,7 +16,7 @@ export default function LockButton(props: Props) {
       type="button"
       /** Color #54595F is hardcoded because this is the only place where it's necessary */
       className={`btn-red ${
-        currModifiable ? "" : "bg-[#54595F]"
+        currModifiable ? "" : "bg-[#54595F] hover:bg-gray-d1 active:bg-gray-d2"
       } py-2 md:py-1 px-2 font-semibold text-xs uppercase tracking-wider`}
       disabled={props.disabled}
       onClick={() => setValue(`${props.name}.modifiable`, !currModifiable)}
