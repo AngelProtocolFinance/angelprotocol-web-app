8bda79a575a2a09783f383690c19cb4909ab6406
abbasali1994 <meetabbasali1994@gmail.com>
2023-03-16 19:14:17 +0530

    Add endowdesignation tag on profile page

---
+++ src/pages/Profile/Body/EndowDesignationTag.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren } from "react";
+import { useProfileContext } from "pages/Profile/ProfileContext";
+import Icon, { IconType } from "components/Icon";
+import { ENDOW_DESIGNATIONS } from "constants/common";
+
+export default function EndowDesignationTag() {
+  const profile = useProfileContext();
+
+  const endowDesignation = ENDOW_DESIGNATIONS.find(
+    (option) => option.value === profile.endow_designation
+  );
+  console.log(profile.endow_designation);
+  return endowDesignation && profile.endow_designation !== "Other" ? (
+    <div className="flex flex-col items-start gap-3">
+      <Tag>
+        <Icon type={endowDesignation.icon as IconType} size={24} />{" "}
+        {endowDesignation.label}
+      </Tag>
+    </div>
+  ) : null;
+}
+
+const Tag = (props: PropsWithChildren<{}>) => (
+  <div className="flex items-center gap-2 px-4 py-2 bg-blue-l4 rounded-full font-body font-semibold text-sm dark:bg-blue-d4">
+    {props.children}
+  </div>
+);
