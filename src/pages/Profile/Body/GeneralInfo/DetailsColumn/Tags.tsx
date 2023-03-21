1cab225195c4ff6e057a1474ca7584b7d10a8139
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-03-21 10:14:36 +0800

    Profile's NPO Type tag moved into Details column (replaces a placeholder tag);

--- src/pages/Profile/Body/GeneralInfo/DetailsColumn/Tags.tsx
+++ src/pages/Profile/Body/GeneralInfo/DetailsColumn/Tags.tsx
@@ -2,18 +2,19 @@ import { PropsWithChildren } from "react";
 import { useProfileContext } from "pages/Profile/ProfileContext";
 import Icon from "components/Icon";
 import { unsdgs } from "constants/unsdgs";
+import EndowDesignationTag from "./EndowDesignationTag";
 
 export default function Tags() {
   const profile = useProfileContext();
 
   return (
     <div className="flex flex-col items-start gap-3">
+      <EndowDesignationTag />
       {profile.kyc_donors_only && (
         <Tag>
           Verification required <Icon type="Info" size={24} />
         </Tag>
       )}
-      <Tag>Registered Non-Profit</Tag>
       {profile.categories.sdgs.map((unsdg_num) => (
         <Tag key={unsdg_num}>
           SDG #{unsdg_num} : {unsdgs[unsdg_num].title}
