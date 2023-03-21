122b2fa0d783e8f258ed7a7f39a3bb9cbfa17eec
ap-justin <justin@angelprotocol.io>
2023-03-21 12:50:04 +0800

    show steps only when sufficient balance

--- src/components/TokenField/Steps.tsx
+++ src/components/TokenField/Steps.tsx
@@ -9,26 +9,27 @@ export default function Steps({
 }: AmountOptionsProps) {
   const base = token.min_donation_amnt || 100;
   const precision = new Decimal(token.min_donation_amnt).decimalPlaces();
+  const steps = scale.map((s) => roundDownToNum(s * base, precision));
+
+  //only show steps if balance is gte all steps
+  if (!steps.every((s) => token.balance >= s)) return null;
 
   return (
     <div className="grid grid-cols-5 gap-2 text-xs">
-      {scale.map((s) => {
-        const step = roundDownToNum(s * base, precision);
-        return (
-          <button
-            type="button"
-            key={s}
-            onClick={() => onSetAmount(step)}
-            className={`${
-              step === Number(token.amount)
-                ? "bg-blue-l3 border-blue-l3 dark:bg-blue-d2 dark:border-blue-d2"
-                : "bg-blue-l4 dark:bg-blue-d4 border-prim"
-            }  rounded-full py-1.5 border`}
-          >
-            {step > token.balance ? "Max" : humanize(step, precision)}
-          </button>
-        );
-      })}
+      {steps.map((step) => (
+        <button
+          type="button"
+          key={step}
+          onClick={() => onSetAmount(step)}
+          className={`${
+            step === Number(token.amount)
+              ? "bg-blue-l3 border-blue-l3 dark:bg-blue-d2 dark:border-blue-d2"
+              : "bg-blue-l4 dark:bg-blue-d4 border-prim"
+          }  rounded-full py-1.5 border`}
+        >
+          {humanize(step, precision)}
+        </button>
+      ))}
     </div>
   );
 }
