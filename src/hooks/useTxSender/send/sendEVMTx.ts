f37fa1fa9ff7e16fcd7706c58322991ae8aeca19
ap-justin <justin@angelprotocol.io>
2023-03-22 13:12:02 +0800

    move evm tx sender

---
+++ src/hooks/useTxSender/send/sendEVMTx.ts
@@ -0,0 +1,26 @@
+import { EVMTx } from "types/evm";
+import { TxResult } from "types/tx";
+import { WalletState } from "contexts/WalletContext";
+import { getProvider } from "helpers/evm";
+import { EIPMethods } from "constants/evm";
+
+export async function sendEVMTx(
+  wallet: WalletState,
+  tx: EVMTx
+): Promise<TxResult> {
+  try {
+    const provider = getProvider(wallet.providerId)!;
+    const hash = await provider.request<string>({
+      method: EIPMethods.eth_sendTransaction,
+      params: [tx],
+    });
+
+    return { hash, chainID: wallet.chain.chain_id };
+
+    //TODO:
+    //1. get transaction_receipt by polling
+    //2. get id from logs
+  } catch (err) {
+    return { error: "Error encountered while sending transaction" };
+  }
+}
