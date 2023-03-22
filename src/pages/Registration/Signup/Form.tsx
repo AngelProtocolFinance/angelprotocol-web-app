e33dd426315e84384a2e223679595cfcecaa2443
abbasali1994 <meetabbasali1994@gmail.com>
2023-03-22 10:24:40 +0530

    fix lint errors

--- src/pages/Registration/Signup/Form.tsx
+++ src/pages/Registration/Signup/Form.tsx
@@ -25,9 +25,7 @@ export default function Form({ classes = "" }: { classes?: string }) {
       onSubmit={submit}
       className={`${classes} padded-container w-full max-w-[37.5rem] grid`}
     >
-      <h3 className="text-3xl text-center">
-        {`Register to ${APP_NAME}`}
-      </h3>
+      <h3 className="text-3xl text-center">{`Register to ${APP_NAME}`}</h3>
 
       <Field<FV>
         name="email"
