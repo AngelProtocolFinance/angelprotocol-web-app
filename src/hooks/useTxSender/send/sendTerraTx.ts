b7a3db323d56671ff13ecfa64a76f3d125c12d92
ap-justin <justin@angelprotocol.io>
2023-03-22 13:12:33 +0800

    terra tx sender helper

---
+++ src/hooks/useTxSender/send/sendTerraTx.ts
@@ -0,0 +1,43 @@
+import { CreateTxOptions } from "@terra-money/terra.js";
+import { ConnectedWallet } from "@terra-money/wallet-provider";
+import { Timeout, TxFailed, UserDenied } from "@terra-money/wallet-provider";
+import { SubmittedTx, TxResult } from "types/tx";
+
+export async function sendTerraTx(
+  wallet: ConnectedWallet,
+  tx: CreateTxOptions
+): Promise<TxResult> {
+  try {
+    const { result, success } = await wallet.post(tx);
+
+    const submitted: SubmittedTx = {
+      hash: result.txhash,
+      chainID: wallet.network.chainID,
+    };
+    if (!success) {
+      return { error: "Transaction submitted but failed", tx: submitted };
+    }
+    return submitted;
+  } catch (err) {
+    if (err instanceof UserDenied) {
+      return { error: "Transaction not signed" };
+    }
+    if (err instanceof Timeout) {
+      return {
+        error: "Timeout: user failed to approve transaction within time limit",
+      };
+    }
+    if (err instanceof TxFailed) {
+      return {
+        error: "Transaction submitted but failed",
+        tx: err.txhash
+          ? {
+              hash: err.txhash,
+              chainID: wallet.network.chainID,
+            }
+          : undefined,
+      };
+    }
+    return { error: "Error encountered while sending transaction" };
+  }
+}
