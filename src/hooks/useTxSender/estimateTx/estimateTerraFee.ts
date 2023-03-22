094436837cd34fa5d3ccdb8e8f68a47166513416
ap-justin <justin@angelprotocol.io>
2023-03-22 13:11:37 +0800

    move terra fee estimate

---
+++ src/hooks/useTxSender/estimateTx/estimateTerraFee.ts
@@ -0,0 +1,31 @@
+import { Coin, LCDClient, Msg } from "@terra-money/terra.js";
+import { ConnectedWallet } from "@terra-money/wallet-provider";
+import { Estimate } from "types/tx";
+import { WalletState } from "contexts/WalletContext";
+import { extractFeeAmount } from "helpers";
+
+export default async function estimateTerraFee(
+  wallet: WalletState,
+  signer: ConnectedWallet,
+  msgs: Msg[]
+): Promise<Estimate> {
+  const client = new LCDClient({
+    chainID: wallet.chain.chain_id,
+    URL: wallet.chain.lcd_url,
+    gasAdjustment: 1.6,
+    //https://station-assets.terra.money/chains.json
+    gasPrices: [new Coin("uluna", 0.015)],
+  });
+
+  const account = await client.auth.accountInfo(wallet.address);
+  const fee = await client.tx.estimateFee(
+    [{ sequenceNumber: account.getSequenceNumber() }],
+    { msgs, feeDenoms: [wallet.chain.native_currency.token_id] }
+  );
+  const amount = extractFeeAmount(fee, wallet.displayCoin.token_id);
+
+  return {
+    fee: { amount, symbol: wallet.displayCoin.symbol },
+    tx: { type: "terra", val: { fee, msgs }, signer },
+  };
+}
