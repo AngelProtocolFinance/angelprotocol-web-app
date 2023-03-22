ffd00096f9251d3d88b8bb2dc729733108f69eb0
Nenad <nenad@angelprotocol.io>
2023-03-22 08:18:17 +0100

    Update value type to EndowDesignation in getEndowDesignationLabelValuePair

--- src/pages/Admin/Charity/EditProfile/getEndowDesignationLabelValuePair.ts
+++ src/pages/Admin/Charity/EditProfile/getEndowDesignationLabelValuePair.ts
@@ -1,8 +1,9 @@
+import { EndowDesignation } from "types/aws";
 import { OptionType } from "components/Selector";
 import { ENDOW_DESIGNATIONS } from "constants/common";
 
 export const getEndowDesignationLabelValuePair = (
-  value: string
+  value: EndowDesignation
 ): OptionType<string> => {
   const label =
     ENDOW_DESIGNATIONS.find((option) => option.value === value)?.label || value;
