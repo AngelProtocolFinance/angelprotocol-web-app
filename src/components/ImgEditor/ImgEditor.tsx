5b3a15a89d46fe42c9a33ac593a4bfea3b853efb
satishkumar <satishsajjan4564@gmail.com>
2023-03-24 12:08:18 +0530

    Removed the duplicate "Crop" button

--- src/components/ImgEditor/ImgEditor.tsx
+++ src/components/ImgEditor/ImgEditor.tsx
@@ -94,11 +94,7 @@ export default function ImgEditor<T extends FieldValues, K extends keyof T>(
                 </IconButton>
               )
             }
-            {!isInitial && (
-              <IconButton onClick={handleOpenCropper} disabled={isSubmitting}>
-                <Icon type="Crop" />
-              </IconButton>
-            )}
+            
           </div>
         )}
         {
