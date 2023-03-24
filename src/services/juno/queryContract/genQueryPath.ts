5e565f8aaa494be39b5778ac1f225659bac966d9
ap-justin <justin@angelprotocol.io>
2023-03-25 00:01:36 +0800

    revert name

--- src/services/juno/queryContract/genQueryPath.ts
+++ src/services/juno/queryContract/genQueryPath.ts
@@ -1,13 +1,13 @@
 import { ContractQueries as Q, ContractQueryTypes as QT } from "./types";
 import { toBase64 } from "helpers";
-import { queryObject } from "./queryObject";
+import { queryObjects } from "./queryObjects";
 
 export function genQuery<T extends QT>(
   type: T,
   args: Q[QT]["args"],
   contract: string
 ): [string, Q[QT]["transform"]] {
-  const [query, transform] = queryObject[type];
+  const [query, transform] = queryObjects[type];
   const msg = typeof query === "function" ? query(args) : query;
   return [
     `cosmwasm/wasm/v1/contract/${contract}/smart/${toBase64(msg)}`,
