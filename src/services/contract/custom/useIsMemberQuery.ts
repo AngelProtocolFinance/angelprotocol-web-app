091a5351fdec014667d10d01500f643059e6ae7e
ap-justin <justin@angelprotocol.io>
2023-03-25 16:30:08 +0800

    is member query to swr

---
+++ src/services/contract/custom/useIsMemberQuery.ts
@@ -0,0 +1,32 @@
+import useSWR from "swr";
+import { queryContract } from "services/juno/queryContract";
+import { idParamToNum } from "helpers";
+import { contracts } from "constants/contractsV2";
+import { apCWs } from "./constants";
+
+export default function useIsMemberQuery(user: string, endowmentId?: number) {
+  //TODO: skip if not juno/polygon and no endowmentId
+  return useSWR([user, endowmentId], async ([user, id]) => {
+    const numId = idParamToNum(id);
+    const AP = apCWs[numId];
+    /** special case for ap admin usage */
+    if (AP) {
+      const { cw3 } = AP;
+      //skip endowment query, query hardcoded cw3 straight
+      const voter = await queryContract("cw3Voter", cw3, {
+        addr: user,
+      });
+      return !!voter.weight;
+    }
+
+    const endowment = await queryContract("accEndowment", contracts.accounts, {
+      id: numId,
+    });
+
+    const voter = await queryContract("cw3Voter", endowment.owner, {
+      addr: user,
+    });
+
+    return !!voter.weight;
+  });
+}
--- src/services/juno/custom/custom.ts
+++ src/services/juno/custom/custom.ts
@@ -8,41 +8,6 @@ import { apCWs, getMeta } from "./helpers/admin-resource";
 
 export const customApi = junoApi.injectEndpoints({
   endpoints: (builder) => ({
-    isMember: builder.query<boolean, { user: string; endowmentId?: string }>({
-      providesTags: [
-        { type: "admin", id: adminTags.voter },
-        { type: "account", id: accountTags.endowment },
-      ],
-      async queryFn(args) {
-        const numId = idParamToNum(args.endowmentId);
-        const AP = apCWs[numId];
-        /** special case for ap admin usage */
-        if (AP) {
-          const { cw3 } = AP;
-          //skip endowment query, query hardcoded cw3 straight
-          const voter = await queryContract("cw3Voter", cw3, {
-            addr: args.user,
-          });
-          return {
-            data: !!voter.weight,
-          };
-        }
-
-        const endowment = await queryContract(
-          "accEndowment",
-          contracts.accounts,
-          { id: numId }
-        );
-
-        const voter = await queryContract("cw3Voter", endowment.owner, {
-          addr: args.user,
-        });
-
-        return {
-          data: !!voter.weight,
-        };
-      },
-    }),
     adminResources: builder.query<
       AdminResources,
       { user?: string; endowmentId?: string }
@@ -127,8 +92,4 @@ export const customApi = junoApi.injectEndpoints({
   }),
 });
 
-export const {
-  useIsMemberQuery,
-  useAdminResourcesQuery,
-  useProposalDetailsQuery,
-} = customApi;
+export const { useAdminResourcesQuery, useProposalDetailsQuery } = customApi;
