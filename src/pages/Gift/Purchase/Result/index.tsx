802745f8f4e8437b9a427fccd540ee933be521ad
Abbas Ali Lokhandwala <meetabbasali1994@gmail.com>
2023-03-27 15:15:14 +0530

    fix giftcard error display (#2001)

--- src/pages/Gift/Purchase/Result/index.tsx
+++ src/pages/Gift/Purchase/Result/index.tsx
@@ -1,4 +1,4 @@
-import { GiftCard, TxResult, TxStep } from "slices/gift";
+import { GiftCard, TError, TxResult, TxStep } from "slices/gift";
 import { errKey, loadingKey } from "../constants";
 import Err from "./Err";
 import Loading from "./Loading";
@@ -7,7 +7,7 @@ import Success from "./Success";
 export default function Result({ ...state }: TxStep) {
   const { status } = state;
   if (errKey in status) {
-    return <Err {...status} />;
+    return <Err {...(status as TError)} />;
   } else if (loadingKey in status) {
     return <Loading {...status} classes="mb-4 sm:mb-40 sm:mt-20" />;
   } else {
