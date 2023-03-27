965789036e2743e18aa0ba1cfb6db84a0d86ff1a
ap-justin <justin@angelprotocol.io>
2023-03-27 18:44:36 +0800

    rename query files

--- src/services/contract/queryContract/getQuery.ts
+++ src/services/contract/queryContract/getQuery.ts
@@ -1,13 +1,13 @@
 import { ContractQueries as Q, ContractQueryTypes as QT } from "./types";
 import { toBase64 } from "helpers";
-import { queryObjects } from "./queryObjects";
+import { queries } from "./queries";
 
-export function genQuery<T extends QT>(
+export function getQuery<T extends QT>(
   type: T,
   args: Q[QT]["args"],
   contract: string
 ): [string, Q[QT]["transform"]] {
-  const [query, transform] = queryObjects[type];
+  const [query, transform] = queries[type];
   const msg = typeof query === "function" ? query(args) : query;
   return [
     `cosmwasm/wasm/v1/contract/${contract}/smart/${toBase64(msg)}`,
