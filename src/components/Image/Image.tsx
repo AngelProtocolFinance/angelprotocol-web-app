f5c104b85895c44d320461475f956ee46eee6963
Nenad <n.misic@outlook.com>
2023-03-28 11:26:14 +0200

    Add isError state to Image

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -13,19 +13,21 @@ export type ImageProps = {
   className?: string;
   isSrcLoading?: boolean;
   src?: string;
+  onError?: React.ReactEventHandler<HTMLImageElement>;
 } & ({ href: string; title: string } | { href?: never; title?: never });
 
 const Image = React.forwardRef<HTMLImageElement, ImageProps>(
   (props, forwardRef) => {
     const ref = useRef<HTMLImageElement>(null);
     const [isLoading, setLoading] = useState(!!props.src || props.isSrcLoading);
+    const [isError, setError] = useState(false);
 
     useImperativeHandle<HTMLImageElement | null, HTMLImageElement | null>(
       forwardRef,
       () => ref.current
     );
 
-    if (!props.src) {
+    if (!props.src || isError) {
       return <ImagePlaceholder className={props.className} />;
     }
 
@@ -46,6 +48,9 @@ const Image = React.forwardRef<HTMLImageElement, ImageProps>(
               className={`${props.className} object-contain w-full h-full`}
               alt={props.alt || ""}
               onLoad={() => setLoading(false)}
+              onError={(e) =>
+                props.onError ? props.onError(e) : setError(true)
+              }
             />
           </WithLink>
         )}
