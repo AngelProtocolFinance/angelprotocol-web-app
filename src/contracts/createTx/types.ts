964982b03f912905f928faa8ea3151a522b2377c
ap-justin <justin@angelprotocol.io>
2023-03-29 22:10:54 +0800

    create Tx signature

--- src/contracts/createTx/types.ts
+++ src/contracts/createTx/types.ts
@@ -1,9 +1,6 @@
 import { NewAIF } from "types/contracts/evm";
 import { Tupleable } from "types/evm";
-
-export const txs = {
-  "accounts.create-endowment": {},
-};
+import { Contract } from "types/lists";
 
 interface NewTransaction extends Tupleable {
   title: string;
@@ -14,7 +11,13 @@ interface NewTransaction extends Tupleable {
 }
 
 type Tx<T extends Tupleable> = {
-  tags: string[]; //tags to invalidate
+  tags: string[]; //tags to invalidate.
+  /**
+   * or create static map
+   * [event_topic]: query_tag[]
+   *
+   * after tx, for each log, if log.topic in map, invalidate query_tag[]
+   */
   payload: T;
 };
 
@@ -22,9 +25,24 @@ type Txs = {
   "accounts.create-endowment": Tx<NewAIF>;
   "accounts/settings.update-controller": Tx<NewAIF>;
   //create and sign
-  "multisig.add-transaction": Tx<NewTransaction>;
+  "cw3.add-transaction": Tx<NewTransaction>;
 };
 
 export type TxTypes = keyof Txs;
+export type TxPayload<T extends TxTypes> = Txs[T]["payload"];
 
-export type TxArgs<T extends TxTypes> = Txs[T]["payload"];
+export const txs: { [T in TxTypes]: (payload: TxPayload<T>) => string } = {
+  "accounts.create-endowment": () => "",
+  "accounts/settings.update-controller": () => "",
+  "cw3.add-transaction": () => "",
+};
+
+type Empty = { [key: string]: never };
+export type TxOptions<T extends TxTypes> = T extends `${infer C}.${string}`
+  ? C extends Contract
+    ? Txs[T]["payload"]
+    : { [key in C]: string } & Txs[T]["payload"]
+  : Empty;
+
+function createTx<T extends TxTypes>(type: T, options: TxOptions<T>) {}
+// createTx("accounts.create-endowment", {})
