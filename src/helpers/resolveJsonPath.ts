021096a8113f7349b3de68c8c7fc107fce255b90
abbasali1994 <meetabbasali1994@gmail.com>
2023-03-29 17:38:25 +0530

    redefine errorclass method

--- src/helpers/resolveJsonPath.ts
+++ src/helpers/resolveJsonPath.ts
@@ -1,2 +1,8 @@
 export const resolvePath = (object: any, path: string) =>
   path.split(".").reduce((o, p) => (o ? o[p] : {}), object);
+
+export const getErrorClasses = (errors: any, name: string) => {
+  return resolvePath(errors, name)?.message
+    ? "!border-1 !border-red dark:!border-red-l2"
+    : "";
+};
