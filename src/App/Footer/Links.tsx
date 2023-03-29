5425290343e307506ec53f8bc2ed8c12942861a0
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-03-29 12:12:54 +0800

    sync footer links with marketing site (#2015)
    * sync footer with marketing site
    
    * add a little more col width

--- src/App/Footer/Links.tsx
+++ src/App/Footer/Links.tsx
@@ -4,7 +4,7 @@ const linkStyles = "font-body font-semibold text-xs";
 
 export default function Links() {
   return (
-    <div className="grid grid-rows-2 grid-cols-2 gap-4 w-4/5 max-w-[38rem] md:grid-rows-1 md:grid-cols-4 lg:w-full">
+    <div className="grid grid-rows-2 grid-cols-2 gap-4 w-4/5 max-w-[45rem] md:grid-rows-1 md:grid-cols-4 lg:w-full">
       {SECTIONS_DATA.map(({ title, links }) => (
         <div key={title} className="flex flex-col items-start gap-4">
           <p className="font-heading text-sm font-bold uppercase leading-6">
