d994bd0e09fd144ed591648b2498630ae30bf9e6
Nenad <n.misic@outlook.com>
2023-03-31 09:37:37 +0200

    Remove useJunoEndowWithdraw

--- src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/useWithdraw/useWithdraw.ts
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/useWithdraw/useWithdraw.ts
@@ -1,22 +1,15 @@
 import { useFormContext } from "react-hook-form";
 import { WithdrawValues } from "../types";
-import { chainIds } from "constants/chainIds";
-import useJunoEndowWithdraw from "./useJunoEndowWithdraw";
 import usePolygonEndowWithdraw from "./usePolygonEndowWithdraw";
-
-const endow_chain = chainIds.juno;
 
 export default function useWithdraw() {
   const { handleSubmit } = useFormContext<WithdrawValues>();
 
   const polygonEndowWithdraw = usePolygonEndowWithdraw();
-  const junoEndowWithdraw = useJunoEndowWithdraw();
 
   //NOTE: submit is disabled on Normal endowments with unmatured accounts
   function withdraw(data: WithdrawValues) {
-    return endow_chain === chainIds.polygon
-      ? polygonEndowWithdraw(data)
-      : junoEndowWithdraw(data);
+    return polygonEndowWithdraw(data);
   }
 
   return handleSubmit(withdraw);
