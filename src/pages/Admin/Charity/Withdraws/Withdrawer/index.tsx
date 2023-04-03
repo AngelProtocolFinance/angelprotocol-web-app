dbff4e32a6f5fc08e9ed7fea124fc982f42aa4f5
Nenad <n.misic@outlook.com>
2023-04-03 11:32:23 +0200

    Hide tab list for endow_type === normal

--- src/pages/Admin/Charity/Withdraws/Withdrawer/index.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/index.tsx
@@ -27,16 +27,18 @@ export default function Withdrawer() {
           as="div"
           className="flex flex-col items-center max-w-lg p-8 gap-6 dark:bg-blue-d6 border border-prim rounded"
         >
-          <Tab.List className="grid grid-cols-2 place-items-center gap-1 w-full h-10 p-1 border border-prim rounded-3xl">
-            {types[endow_type].map((type) => (
-              <Tab
-                key={`tab-${type}`}
-                className="rounded-2xl flex items-center justify-center w-full h-full uppercase text-sm font-bold focus:outline-none aria-selected:bg-orange-l5 aria-selected:dark:bg-blue-d4 aria-selected:border aria-selected:border-prim"
-              >
-                {type}
-              </Tab>
-            ))}
-          </Tab.List>
+          {endow_type === "charity" && (
+            <Tab.List className="grid grid-cols-2 place-items-center gap-1 w-full h-10 p-1 border border-prim rounded-3xl">
+              {types[endow_type].map((type) => (
+                <Tab
+                  key={`tab-${type}`}
+                  className="rounded-2xl flex items-center justify-center w-full h-full uppercase text-sm font-bold focus:outline-none aria-selected:bg-orange-l5 aria-selected:dark:bg-blue-d4 aria-selected:border aria-selected:border-prim"
+                >
+                  {type}
+                </Tab>
+              ))}
+            </Tab.List>
+          )}
           <Tab.Panels>
             {types[endow_type].map((type) => (
               <Tab.Panel key={`tab-panel-${type}`}>
