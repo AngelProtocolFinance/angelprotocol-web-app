a25fb4f95b43a7bc2ccd47697fc7d05a3a545562
Nenad <n.misic@outlook.com>
2023-04-04 10:42:08 +0200

    Add comments to Account

--- src/contracts/evm/Account/index.ts
+++ src/contracts/evm/Account/index.ts
@@ -4,7 +4,15 @@ import { WalletState } from "contexts/WalletContext";
 import Contract from "../Contract";
 import abi from "./abi.json";
 
-// It is crucial to have event arguments in the same order as they appear in the event from the logs
+/**
+ * By creating an event template constant and creating a type from it we enable the following:
+ *   1. The parent's event decoder (decodeEventInternal) knows how to populate the expected event arguments
+ *   2. The parent's event decoder can preserve type-safety
+ *   3. The Account can create a wrapper around the parent's event decoder to ensure only expected events can be decoded
+ *      using its (Account's) `decodeEvent` method
+ *
+ * IMPORTANT: it is crucial to have event arguments in the same order as they appear in the event from the logs.
+ */
 const eventTemplates = {
   EndowmentCreated: {
     endowId: 0,
@@ -13,13 +21,29 @@ const eventTemplates = {
 
 type Events = typeof eventTemplates;
 
+/**
+ * Representation of the `Account` contract that extends and wraps the generic `Contract` functionality.
+ * Ensures that `Contract` methods are called with the Account ABI.
+ */
 export default class Account extends Contract {
+  /**
+   * Passes the Account ABI to the parent `Contract` class, ensuring its methods are called with that ABI.
+   *
+   * @param contractAddress Address of the contract.
+   * @param wallet The connected wallet.
+   */
   constructor(contractAddress: string, wallet: WalletState) {
     super(abi, contractAddress, wallet);
   }
 
-  // we create a wrapper for the decodeEventInternal to ensure only expected events
-  // can be decoded
+  /**
+   * Wrapper around the parent's event decoder that ensures only expected events are decoded for this contract.
+   *
+   * @param event Name of the event to be decoded.
+   * @param logs Transaction logs.
+   * @returns The specified event's arguments if it was found (if it was emitted). Otherwise returns `null`.
+   * @see {@link Events}
+   */
   static decodeEvent = <K extends Extract<keyof Events, string>>(
     event: K,
     logs: TxLog[]
@@ -27,6 +51,11 @@ export default class Account extends Contract {
     return super.decodeEventInternal(abi, eventTemplates, event, logs);
   };
 
+  /**
+   *
+   * @param aif New AIF to create.
+   * @returns An object {@link SimulContractTx} containing transaction data necessary to call an EVM contract, see
+   */
   createCreateEndowmentTx(aif: NewAIF): SimulContractTx {
     return super.createContractTx("createEndowment", aif);
   }
