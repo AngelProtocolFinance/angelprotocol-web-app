ff27dc1134a6001bf5ca33763461fde7076e069c
abbasali1994 <meetabbasali1994@gmail.com>
2023-04-04 16:55:36 +0530

    Add sendFiatDonation thunk

--- src/slices/donation/sendDonation/index.ts
+++ src/slices/donation/sendDonation/index.ts
@@ -1,13 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { TxStatus } from "../types";
-import { DonateArgs } from "../types";
+import { DonateArgs, FiatDonateArgs, TxStatus } from "../types";
 import { KYCData } from "types/aws";
 import { isTxResultError } from "types/tx";
 import { invalidateApesTags } from "services/apes";
 import { logger } from "helpers";
 import { sendTx } from "helpers/tx";
+import { SERVICE_PROVIDER } from "constants/fiatTransactions";
 import donation, { setTxStatus } from "../donation";
 import logDonation from "./logDonation";
+import logFiatDonation from "./logFiatDonation";
 
 export const sendDonation = createAsyncThunk<void, DonateArgs>(
   `${donation.name}/sendDonation`,
@@ -18,7 +19,6 @@ export const sendDonation = createAsyncThunk<void, DonateArgs>(
     const updateTx = (status: TxStatus) => {
       dispatch(setTxStatus(status));
     };
-
     try {
       const { token, pctLiquidSplit } = details;
       updateTx({ loadingMsg: "Payment is being processed..." });
@@ -67,6 +67,70 @@ export const sendDonation = createAsyncThunk<void, DonateArgs>(
       });
 
       updateTx({ hash });
+
+      //invalidate cache entries
+      dispatch(invalidateApesTags(["chain", "donations"]));
+    } catch (err) {
+      logger.error(err);
+      updateTx("error");
+    }
+  }
+);
+
+export const sendFiatDonation = createAsyncThunk<void, FiatDonateArgs>(
+  `${donation.name}/sendFiatDonation`,
+  async (
+    { wallet, externalSessionId, donation: { details, kyc, recipient } },
+    { dispatch }
+  ) => {
+    const updateTx = (status: TxStatus) => {
+      dispatch(setTxStatus(status));
+    };
+
+    try {
+      const { token, pctLiquidSplit } = details;
+      updateTx({ loadingMsg: "Payment is being processed..." });
+
+      const kycData: KYCData | undefined =
+        kyc === "skipped"
+          ? undefined
+          : {
+              fullName: `${kyc.name.first} ${kyc.name.last}`,
+              email: kyc.email,
+              streetAddress: `${kyc.address.street} ${kyc.address.complement}`,
+              city: kyc.city,
+              state: kyc.usState.value || kyc.state,
+              zipCode: kyc.postalCode,
+              country: kyc.country.name,
+              consent_tax: true,
+              consent_marketing: true,
+            };
+
+      updateTx({
+        loadingMsg:
+          kyc !== "skipped"
+            ? "Requesting receipt.."
+            : "Saving donation details",
+      });
+
+      await logFiatDonation({
+        ...kycData /** receipt is sent to user if kyc is provider upfront */,
+        amount: +token.amount,
+        chainId: wallet.chain.chain_id,
+        chainName: wallet.chain.chain_name,
+        charityName: recipient.name,
+        denomination: token.symbol,
+        splitLiq: `${pctLiquidSplit}`,
+        transactionId: "To be Processed",
+        transactionDate: new Date().toISOString(),
+        endowmentId: recipient.id,
+        fiatRamp: SERVICE_PROVIDER,
+        paymentMethod: externalSessionId || "",
+        destinationChainId: "",
+      });
+
+      //need to create an additional state for submitted txn
+      updateTx({ hash: "Transaction Submitted" });
 
       //invalidate cache entries
       dispatch(invalidateApesTags(["chain", "donations"]));
