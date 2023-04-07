9b98859092b79fb28e82e17d8dc8b6349620a627
ap-justin <justin@angelprotocol.io>
2023-04-07 13:17:00 +0800

    tooltip when no member

--- src/pages/Admin/templates/cw4/Members/Form.tsx
+++ src/pages/Admin/templates/cw4/Members/Form.tsx
@@ -1,6 +1,8 @@
 import { useAdminResources } from "pages/Admin/Guard";
 import { useModalContext } from "contexts/ModalContext";
+import { ErrorStatus } from "components/Status";
 import { DivContainer, Submitter } from "components/admin";
+import { isEmpty } from "helpers";
 import AddForm from "./Adder";
 import Member from "./Member";
 
@@ -10,11 +12,15 @@ export default function Form() {
   return (
     <DivContainer>
       <div className="p-3 rounded border border-prim bg-orange-l6 dark:bg-blue-d7">
-        <div className="flex flex-col gap-2 mb-2">
-          {members.map((m) => (
-            <Member key={m} address={m} />
-          ))}
-        </div>
+        {isEmpty(members) ? (
+          <ErrorStatus>No members found</ErrorStatus>
+        ) : (
+          <div className="flex flex-col gap-2 mb-2">
+            {members.map((m) => (
+              <Member key={m} address={m} />
+            ))}
+          </div>
+        )}
       </div>
 
       <button
