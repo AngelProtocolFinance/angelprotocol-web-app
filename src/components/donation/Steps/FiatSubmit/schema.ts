e67e26bb86c3467ca9f59a2110aa0dfc4dd82e48
abbasali1994 <meetabbasali1994@gmail.com>
2023-04-17 15:13:48 +0530

    Add country ISO code list

--- src/components/donation/Steps/FiatSubmit/schema.ts
+++ src/components/donation/Steps/FiatSubmit/schema.ts
@@ -1,7 +1,11 @@
 import * as Yup from "yup";
 import { FiatDonateValues } from "./types";
 import { SchemaShape } from "schemas/types";
+import { Country } from "types/countries";
+import { requiredString } from "schemas/string";
 
 export const schema = Yup.object().shape<SchemaShape<FiatDonateValues>>({
-  //no need to validate split, restricted by slider
+  country: Yup.object().shape<SchemaShape<Country>>({
+    name: requiredString,
+  }),
 });
