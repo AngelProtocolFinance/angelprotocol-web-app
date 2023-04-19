06748f6b17f433f01dba974638272399e5d863a9
ap-justin <justin@angelprotocol.io>
2023-04-19 20:09:15 +0800

    add back config template

--- src/pages/Admin/Review/Templates/index.tsx
+++ src/pages/Admin/Review/Templates/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { templateRoutes as routes } from "../../constants";
+import Config from "../../templates/cw3/Config";
 import FundSender from "../../templates/cw3/FundSender";
 import Members from "../../templates/cw4/Members";
 import Nav from "./Nav";
@@ -11,6 +12,7 @@ export default function Templates() {
       <Routes>
         {/**_cw3 */}
         <Route path={routes.cw3_transfer} element={<FundSender />} />
+        <Route path={routes.cw3_config} element={<Config />} />
         {/**_cw4 */}
         <Route path={routes.cw4_members} element={<Members />} />
       </Routes>
