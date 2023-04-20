65c4961089fa98e08100315cc59704d384936119
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-04-20 11:31:16 +0800

    Contribution vs Donation text to toggle

--- src/components/donation/Steps/Progress.tsx
+++ src/components/donation/Steps/Progress.tsx
@@ -1,4 +1,5 @@
 import { useGetter } from "store/accessors";
+import { IS_AST } from "constants/env";
 
 export default function Progress({ classes = "" }: { classes?: string }) {
   const step = useGetter((state) => state.donation.step);
@@ -7,9 +8,13 @@ export default function Progress({ classes = "" }: { classes?: string }) {
     <div
       className={`${classes} text-sm mb-10 grid grid-cols-3 justify-items-center gap-2`}
     >
-      <p className="text-center">Donation method</p>
-      <p className="text-center">Donor details</p>
-      <p className="text-center">Finalize payment</p>
+      <p className="text-center">
+        {IS_AST ? "Contribution" : "Donation"} method
+      </p>
+      <p className="text-center">{IS_AST ? "Contributor" : "Donor"} details</p>
+      <p className="text-center">
+        Finalize {IS_AST ? "contribution" : "donation"}
+      </p>
       <div className="mt-3 relative h-2 w-full col-span-full bg-gray-l3 dark:bg-bluegray rounded-full overflow-hidden">
         <div
           style={{ width: `${(step / 3) * 100}%` }}
