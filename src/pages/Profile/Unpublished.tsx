21bca4c0f19cb59c350e1baf16d7f89ad7720f4f
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-04-25 07:46:01 +0800

    AST wording toggle;

--- src/pages/Profile/Unpublished.tsx
+++ src/pages/Profile/Unpublished.tsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 import Icon from "components/Icon";
+import { IS_AST } from "constants/env";
 import { appRoutes } from "constants/routes";
 
 export default function Unpublished() {
   return (
     <section className="flex flex-col items-center justify-center w-full h-screen gap-2 bg-blue dark:bg-blue-d5 text-red-l4 dark:text-red-l2">
       <Icon type="ExclamationCircleFill" size={30} />
-      <p className="text-lg text-center">This AST has no public profile</p>
+      <p className="text-lg text-center">
+        This {IS_AST ? "AST" : "Endowment"} has no public profile
+      </p>
       <Link
         to={`${appRoutes.index}`}
         className="text-blue-l5 hover:text-blue text-sm"
       >
-        back to Marketplace
+        Go Back
       </Link>
     </section>
   );
