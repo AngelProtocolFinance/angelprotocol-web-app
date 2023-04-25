573ab7a78a06b12dc0b1082fe9892cd5fd745469
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-04-25 11:04:51 +0800

    use ExtLink component

--- src/App/Header/DesktopNav.tsx
+++ src/App/Header/DesktopNav.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { Link } from "../types";
+import ExtLink from "components/ExtLink";
 import { createNavLinkStyler } from "helpers";
 
 type Props = { classes: string; links: Link[] };
@@ -9,15 +10,13 @@ export default function DesktopNav({ classes, links }: Props) {
     <nav className={`${classes} items-center justify-end font-body text-base`}>
       {links.map((link) =>
         link.external ? (
-          <a
+          <ExtLink
             key={`header-link-${link.title}`}
             className={styles}
             href={link.href}
-            target="_blank"
-            rel="noreferrer"
           >
             {link.title}
-          </a>
+          </ExtLink>
         ) : (
           <NavLink
             key={`header-link-${link.title}`}
