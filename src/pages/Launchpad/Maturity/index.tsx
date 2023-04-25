f6f575ff177bcfe2464ae60ffce9432027ea2594
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-04-25 16:08:21 +0800

    Optional maturity in launchpad (#2104)
    * update schema
    
    * initial hookform string
    
    * maturity time comparison

--- src/pages/Launchpad/Maturity/index.tsx
+++ src/pages/Launchpad/Maturity/index.tsx
@@ -1,9 +1,9 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { FormProvider, useForm } from "react-hook-form";
-import { date, object } from "yup";
+import { lazy, object, string, date as yupDate } from "yup";
 import { FV } from "./types";
 import { SchemaShape } from "schemas/types";
-import { dateToFormFormat } from "components/form";
+import { dateToFormFormat, hookFormInitialDateString } from "components/form";
 import { useLaunchpad } from "slices/launchpad";
 import { withStepGuard } from "../withStepGuard";
 import Form from "./Form";
@@ -13,13 +13,19 @@ export default withStepGuard<4>(function Maturity({ data }) {
   const methods = useForm<FV>({
     resolver: yupResolver(
       object().shape<SchemaShape<FV>>({
-        date: date()
-          .typeError("invalid date")
-          .min(new Date(), "must be in the future"),
+        date: lazy((val) => {
+          if (val === "" || val === hookFormInitialDateString) return string();
+          return yupDate()
+            .typeError("invalid date")
+            .min(new Date(), "must be in the future");
+        }),
       })
     ),
     defaultValues: data
-      ? { ...data, date: dateToFormFormat(new Date(data.date)) }
+      ? {
+          ...data,
+          date: data.date ? "" : dateToFormFormat(new Date(data.date)),
+        }
       : {
           date: "",
           beneficiaries: [],
@@ -31,7 +37,12 @@ export default withStepGuard<4>(function Maturity({ data }) {
     <FormProvider {...methods}>
       <Form
         onSubmit={handleSubmit(({ date, ...data }) =>
-          update({ ...data, date: new Date(date).toISOString() })
+          update({
+            ...data,
+            date: yupDate().isValidSync(date)
+              ? new Date(date).toISOString()
+              : "",
+          })
         )}
       />
     </FormProvider>
