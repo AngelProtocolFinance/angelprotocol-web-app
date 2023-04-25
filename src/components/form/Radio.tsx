230057df9de2d0a6de455ae43889afb4d36d4c39
Nenad <n.misic@outlook.com>
2023-04-25 12:35:51 +0200

    Convert Radio component to plain non-react-hook-form

--- src/components/form/Radio.tsx
+++ src/components/form/Radio.tsx
@@ -1,52 +1,45 @@
 import { PropsWithChildren } from "react";
-import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
 import { Classes } from "./types";
 import { unpack } from "./helpers";
 
-export function Radio<T extends FieldValues, K extends Path<T>>({
+type Props<Value extends string> = PropsWithChildren<{
+  checked: boolean;
+  classes?: Classes;
+  disabled?: boolean;
+  id?: string;
+  required?: boolean;
+  value: Value;
+  onChange: (value: Value) => void;
+}>;
+
+export function Radio<Value extends string>({
+  checked,
   children,
   classes,
   disabled,
-  name,
   required,
   value,
-}: PropsWithChildren<{
-  name: K;
-  classes?: Classes;
-  disabled?: boolean;
-  required?: boolean;
-  value: T[K];
-}>) {
-  const { control } = useFormContext<T>();
-
-  const id = `__${name}`;
-  const { container, input, lbl } = unpack(classes);
-
+  id = `__radio-btn-${value}`,
+  onChange,
+}: Props<Value>) {
+  const { container, input } = unpack(classes);
   return (
-    <Controller
-      control={control}
-      name={name}
-      render={({
-        field: { value: controlValue, onChange },
-        formState: { isSubmitting },
-      }) => (
-        <div className={`flex items-center gap-4 ${container}`}>
-          <input
-            id={id}
-            type="radio"
-            className={input}
-            disabled={isSubmitting || disabled}
-            checked={controlValue === value}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-          />
-          {!!children && (
-            <label data-required={required} className={lbl} htmlFor={id}>
-              {children}
-            </label>
-          )}
-        </div>
-      )}
-    />
+    <label
+      data-required={required}
+      className={`radio ${container}`}
+      htmlFor={id}
+    >
+      <input
+        id={id}
+        type="radio"
+        className={`peer ${input}`}
+        disabled={disabled}
+        checked={checked}
+        value={value}
+        onChange={(e) => onChange(e.target.value as Value)}
+        required={required}
+      />
+      {children}
+    </label>
   );
 }
