30a11bcbf3424ab46d011fd51a7831c3e4c138ff
Abbas Ali Lokhandwala <meetabbasali1994@gmail.com>
2023-04-26 14:50:48 +0530

    Admin ast switch button display ASTs Toggle (#2110)
    * Add AST list component
    
    * Add ASt list to charity header
    
    * hide button if wallet not connected
    
    * Add transition to display
    
    * minor tweaks;
    
    ---------
    
    Co-authored-by: SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>

---
+++ src/pages/Admin/Sidebar/Header/MyEndowments.tsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { EndowmentBookmark } from "types/aws";
+import Image from "components/Image";
+import { IS_AST } from "constants/env";
+import { appRoutes } from "constants/routes";
+
+type Props = { endowments: EndowmentBookmark[]; showEndowments: boolean };
+
+export default function MyEndowments({ endowments, showEndowments }: Props) {
+  return (
+    <div
+      className={`${
+        showEndowments ? "max-h-56" : "max-h-0"
+      } overflow-hidden transition-max-height duration-500 ease-in-out`}
+    >
+      <div className="grid py-2 mt-5 gap-3">
+        <p className="text-sm text-gray-d1 dark:text-gray font-bold">
+          Other {IS_AST ? "Angel Smart Treasuries" : "Endowments"}
+        </p>
+        <div className="overflow-y-auto max-h-40 scroller grid gap-3">
+          {endowments.map((endowment) => (
+            <div
+              key={`my-endow-${endowment.endowId}`}
+              className="grid grid-cols-[auto_1fr] gap-3"
+            >
+              <Image
+                src={endowment.logo}
+                className="w-10 h-10 border border-prim rounded-full"
+              />
+              <Link
+                to={`${appRoutes.admin}/${endowment.endowId}`}
+                className="grid items-center"
+              >
+                <span className="font-heading font-semibold text-sm">
+                  {endowment.name}
+                </span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
