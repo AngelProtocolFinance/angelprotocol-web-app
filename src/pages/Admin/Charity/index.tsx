60a5389ded9e1e99f2b915d896f61dab54807332
Nenad <n.misic@outlook.com>
2023-04-26 08:45:06 +0200

    Add the Other Settings page only to ASTs

--- src/pages/Admin/Charity/index.tsx
+++ src/pages/Admin/Charity/index.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { LinkGroup } from "../Sidebar/types";
+import { EndowmentType } from "types/contracts";
 import { adminRoutes } from "constants/routes";
+import { useAdminResources } from "../Guard";
 import Layout from "../Layout";
 import Proposal from "../Proposal";
 import Proposals from "../Proposals";
@@ -17,7 +19,7 @@ import Templates from "./Templates";
 import WidgetConfigurer from "./WidgetConfigurer";
 import Withdraws from "./Withdraws";
 
-const LINK_GROUPS: LinkGroup[] = [
+const COMMON: LinkGroup[] = [
   {
     links: [LINKS.index, LINKS.withdraws, LINKS.contributions],
   },
@@ -35,16 +37,28 @@ const LINK_GROUPS: LinkGroup[] = [
     title: "Manage",
     links: [LINKS[adminRoutes.proposals]],
   },
-  {
-    title: "Settings",
-    links: [LINKS.contributor_verification, LINKS[adminRoutes.permissions]],
-  },
 ];
 
+const LINK_GROUPS: { [key in EndowmentType]: LinkGroup[] } = {
+  charity: [
+    ...COMMON,
+    { title: "Settings", links: [LINKS[adminRoutes.permissions]] },
+  ],
+  normal: [
+    ...COMMON,
+    {
+      title: "Settings",
+      links: [LINKS.contributor_verification, LINKS[adminRoutes.permissions]],
+    },
+  ],
+};
+
 export default function Charity() {
+  const { endow_type } = useAdminResources<"charity">();
+
   return (
     <Routes>
-      <Route element={<Layout linkGroups={LINK_GROUPS} />}>
+      <Route element={<Layout linkGroups={LINK_GROUPS[endow_type]} />}>
         <Route path={`${adminRoutes.proposal}/:id`} element={<Proposal />} />
         <Route path={adminRoutes.proposals} element={<Proposals />} />
         <Route path={`${adminRoutes.templates}/*`} element={<Templates />} />
