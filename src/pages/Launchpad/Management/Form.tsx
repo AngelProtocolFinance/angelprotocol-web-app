c61438fb0c1f32497aaaad3fa39acb282f7766c7
ap-justin <justin@angelprotocol.io>
2023-04-27 15:44:07 +0800

    duration and auto-execute

--- src/pages/Launchpad/Management/Form.tsx
+++ src/pages/Launchpad/Management/Form.tsx
@@ -1,11 +1,10 @@
-import { useEffect } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 import { FV } from "./types";
 import { useModalContext } from "contexts/ModalContext";
-import { Field } from "components/form";
 import Form, { Desc, FormProps, Title } from "../common/Form";
 import NavButtons from "../common/NavButtons";
-import Toggle from "../common/Toggle";
+import DurationForm from "./DurationForm";
 import Members from "./Members";
 import ThresholdForm from "./ThresholdForm";
 
@@ -14,14 +13,14 @@ export default function ManageForm(props: FormProps) {
   const { showModal } = useModalContext();
 
   const members = watch("members");
-  const threshold = watch("proposal.threshold");
+  const { duration, threshold, isAutoExecute } = watch("proposal");
 
   /** watch for members and adjust threshold accordingly */
   useEffect(() => {
     const n = members.length || 1;
     const threshold = getValues("proposal.threshold");
     if (n < +threshold) setValue("proposal.threshold", n);
-  }, [members]);
+  }, [members, getValues, setValue]);
 
   return (
     <Form {...props}>
@@ -36,45 +35,72 @@ export default function ManageForm(props: FormProps) {
       <Members classes="mb-8" />
 
       <div className="content-start border border-prim p-4 md:p-8 rounded">
-        <h2 className="font-bold text-center sm:text-left text-xl mb-2">
+        <h2 className="font-bold text-center sm:text-left text-xl mb-8">
           Proposal settings
         </h2>
-        <div className="border border-prim rounded px-4">
-          <div className="flex items-center py-3">
+        <div className="border border-prim rounded divide-y divide-prim">
+          <Container
+            onBtnClick={() =>
+              showModal(ThresholdForm, {
+                added: getValues("members").map((m) => m),
+                initial: threshold,
+                onChange: (v) => setValue("proposal.threshold", v),
+              })
+            }
+          >
             <p>
-              Proposals can be executed when {threshold} out{" "}
-              {members.length || 1} members cast their vote.
+              Proposals can be executed when <Bold>{threshold}</Bold> out{" "}
+              <Bold>{members.length || 1}</Bold> members cast their vote.
             </p>
-            <button
-              type="button"
-              className="btn-outline-filled px-8 py-2 text-sm"
-              onClick={() =>
-                showModal(ThresholdForm, {
-                  added: getValues("members").map((m) => m),
-                  initial: threshold,
-                  onChange: (v) => setValue("proposal.threshold", v),
-                })
-              }
-            >
-              change
-            </button>
-          </div>
+          </Container>
+          <Container
+            onBtnClick={() =>
+              showModal(DurationForm, {
+                initial: duration,
+                onChange: (v) => setValue("proposal.duration", v),
+              })
+            }
+          >
+            <p>
+              Proposals duration is <Bold>{duration}</Bold> hour
+              {+duration > 1 ? "s" : ""}.
+            </p>
+          </Container>
+          <Container
+            onBtnClick={() => {
+              const prev = getValues("proposal.isAutoExecute");
+              setValue("proposal.isAutoExecute", !prev);
+            }}
+          >
+            <p>
+              Proposals auto-execution is turned{" "}
+              <Bold>{isAutoExecute ? "ON" : "OFF"}</Bold>.
+            </p>
+          </Container>
         </div>
-        <Field<FV>
-          name="proposal.duration"
-          label="Duration ( hours )"
-          classes={{ container: "mt-8 mb-6" }}
-          required
-        />
-        <Toggle<FV>
-          name="proposal.isAutoExecute"
-          classes={{ container: "mb-4 text-sm" }}
-        >
-          Auto execute after passing vote
-        </Toggle>
       </div>
 
       <NavButtons classes="mt-8" curr={2} />
     </Form>
   );
 }
+
+const Container = ({
+  children,
+  onBtnClick,
+}: PropsWithChildren<{ onBtnClick(): void }>) => (
+  <div className="flex items-center py-3 px-4 justify-between">
+    {children}
+    <button
+      type="button"
+      className="btn-outline-filled px-8 py-2 text-sm"
+      onClick={onBtnClick}
+    >
+      change
+    </button>
+  </div>
+);
+
+const Bold = ({ children }: PropsWithChildren) => (
+  <span className="font-bold">{children}</span>
+);
