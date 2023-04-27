2474a964288de75e1ba8ef62c64d256c968eb531
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-04-27 12:02:56 +0800

    tweak - fix spacing

--- src/pages/Admin/Charity/ContributorVerification/Message.tsx
+++ src/pages/Admin/Charity/ContributorVerification/Message.tsx
@@ -4,17 +4,12 @@ import { PAYMENT_WORDS, titleCase } from "constants/env";
 type Props = { verificationRequired: VerificationRequired };
 
 export default function Message({ verificationRequired }: Props) {
-  return verificationRequired === "yes" ? (
+  const not = verificationRequired === "yes" ? "" : "not";
+  return (
     <span>
-      {titleCase(PAYMENT_WORDS.payer)}s <b>aren't</b> able to donate
-      anonymously. {titleCase(PAYMENT_WORDS.payer)}
-      verification <b>is</b> enforced.
-    </span>
-  ) : (
-    <span>
-      {titleCase(PAYMENT_WORDS.payer)}s <b>are</b> able to donate anonymously.{" "}
-      {titleCase(PAYMENT_WORDS.payer)}
-      verification <b>is not</b> enforced.
+      {titleCase(PAYMENT_WORDS.payer)}s <b>are {not}</b> able to donate
+      anonymously. {titleCase(PAYMENT_WORDS.payer)} verification <b>is {not}</b>{" "}
+      enforced.
     </span>
   );
 }
