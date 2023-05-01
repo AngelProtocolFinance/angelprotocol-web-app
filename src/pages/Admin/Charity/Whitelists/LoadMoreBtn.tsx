4b4e9115331ab9cddfe14d60f0ea7fe76ef45a2e
abbasali1994 <meetabbasali1994@gmail.com>
2023-05-01 19:43:52 +0530

    Added Load more button

---
+++ src/pages/Admin/Charity/Whitelists/LoadMoreBtn.tsx
@@ -0,0 +1,28 @@
+import LoaderRing from "components/LoaderRing";
+
+export default function LoadMoreBtn({
+  onClick,
+  isLoading,
+  title,
+}: {
+  onClick(): void;
+  disabled: boolean;
+  isLoading: boolean;
+  title: string;
+}) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="flex items-center justify-center gap-3 uppercase text-sm font-bold rounded-b w-full h-12 hover:bg-orange-l5 dark:hover:bg-blue-d3 active:bg-orange-l4 dark:active:bg-blue-d2 disabled:bg-gray-l3 disabled:text-gray aria-disabled:bg-gray-l3 aria-disabled:dark:bg-bluegray disabled:dark:bg-bluegray"
+    >
+      {isLoading ? (
+        <>
+          <LoaderRing thickness={10} classes="w-6" /> Loading...
+        </>
+      ) : (
+        `Load More ${title}`
+      )}
+    </button>
+  );
+}
