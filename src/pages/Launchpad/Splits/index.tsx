d1c023821649e5d3ed5536033c33a2075e4fe8b6
ap-justin <justin@angelprotocol.io>
2023-05-04 13:28:15 +0800

    persisted defaultMin

--- src/pages/Launchpad/Splits/index.tsx
+++ src/pages/Launchpad/Splits/index.tsx
@@ -6,16 +6,19 @@ import Form from "./Form";
 
 export default withStepGuard<5>(function Splits({ data }) {
   const { update } = useLaunchpad(5);
-  const methods = useForm<FV>({
-    defaultValues: data || {
-      isCustom: false,
-      //set max diff so user see how UI is used
-      min: "0",
-      max: "100",
 
-      default: "50",
-      defaultMin: "0",
-    },
+  const methods = useForm<FV>({
+    defaultValues: data
+      ? { ...data, defaultMin: "0" }
+      : {
+          isCustom: false,
+          //set max diff so user see how UI is used
+          min: "0",
+          max: "100",
+
+          default: "50",
+          defaultMin: "0",
+        },
   });
 
   const { handleSubmit } = methods;
