850422788aa98d4c63e19643260725dd9ff9437e
abbasali1994 <meetabbasali1994@gmail.com>
2023-05-15 09:39:14 +0530

    Code Fix

--- src/helpers/isPolygonChain.ts
+++ src/helpers/isPolygonChain.ts
@@ -1,10 +1,3 @@
 import { chainIds } from "constants/chainIds";
 
-export function isPolygonChain(chain: string) {
-  switch (chain) {
-    case chainIds.polygon:
-      return true;
-    default:
-      return false;
-  }
-}
+export const isPolygonChain = (chain: string) => chain === chainIds.polygon;
