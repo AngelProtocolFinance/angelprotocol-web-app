2fe51fda8f7904d40a18707462cf2c6c5445cbb5
abbasali1994 <meetabbasali1994@gmail.com>
2023-05-16 14:52:17 +0530

    Update permission handling for deposits

--- src/pages/Admin/PermissionHandler.tsx
+++ src/pages/Admin/PermissionHandler.tsx
@@ -1,6 +1,11 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { matchPath, useLocation } from "react-router-dom";
 import { AdminResources } from "services/types";
 import { useGetWallet } from "contexts/WalletContext";
+import { adminRoutes, appRoutes } from "constants/routes";
+import { useAdminResources } from "./Guard";
+
+const ADMIN_ROUTE = `${appRoutes.admin}/:id/`;
 
 export default function PermissionHandler({
   data,
@@ -10,10 +15,19 @@ export default function PermissionHandler({
   children: ReactNode;
 }) {
   const { wallet } = useGetWallet();
+  const currPath = useLocation().pathname;
+  const [disableInputs, setDisableInputs] = useState(true);
+  const { whitelistedBeneficiaries, members } = useAdminResources<"charity">();
+  useEffect(() => {
+    if (wallet) {
+      const isMember = members.includes(wallet.address);
+      const isWhitelisted = whitelistedBeneficiaries.includes(wallet.address);
+      // allow deposits to any connected admin wallet
+      if (matchPath(`${ADMIN_ROUTE}${adminRoutes.deposits}`, currPath))
+        setDisableInputs(false);
+      else setDisableInputs(!isMember && !isWhitelisted);
+    } else setDisableInputs(true);
+  }, [currPath, members, wallet, whitelistedBeneficiaries]);
 
-  return (
-    <fieldset disabled={!wallet || !data?.members.includes(wallet.address)}>
-      {children}
-    </fieldset>
-  );
+  return <fieldset disabled={disableInputs}>{children}</fieldset>;
 }
