2cf051b40eaccd5782ee1b7f3bb180025a61b681
ap-justin <justin@angelprotocol.io>
2023-05-18 16:15:26 +0800

    update contract address

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -17,13 +17,13 @@ const mainnet:Contracts = {
 
 //prettier-ignore
 const testnet:Contracts = {
-  registrar:             "0xf9ae9FEb01B382C87d391F04a021fae312264CA7",
-  "index-fund":          "0xba4A7987e34BFDCb1D10e1318934dBce1024bDC8",
-  "multisig/ap":         "0xC26Ac43b14ebCbff5029792052aF3e4DF3233902",
-  "multisig/review":     "0x1edC050B5d84cbB0cA0b56356f3F7307efcd50Fb",
-  accounts:              "0xf725Ff6235D53dA06Acb4a70AA33206a1447D550",
-  "locked-withdraw":     "0x2d618cDa8cbd2FFc742DD9E8BBEa774B30d06665",
-  "charity-application": "0x14A7bb3A01D9011D39FC0ECb5d09C84f7F596e4B",
+  registrar:             "0x5602bAF3002df39FbAA568C0657bBBBB8e84527a",
+  "index-fund":          "0x486b1C056AC365dD969056C717ab8E7AEd59a178",
+  "multisig/ap":         "0xD3d0B4fFF90c36e1a31d478e35d699f432428Ae5",
+  "multisig/review":     "0xB2BD05A652973f567aD6472b9FEa29FE293f6cD9",
+  accounts:              "0x6d8778e0C742e0f0c31d85d271089C0668a95738",
+  "locked-withdraw":     "0x84efD60753f006DE6878C7139675b110Da654459",
+  "charity-application": "0xbF6b148a440acAb936796ad40947C0C33C78639A",
   "gift-card":           "0x47e49a7700c9D79412bb47385eD349106d4941F9",
 }
 
