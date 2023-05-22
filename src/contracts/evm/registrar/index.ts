287a63c7e95971d759d37346d069e98f1c8d3c36
ap-justin <justin@angelprotocol.io>
2023-05-22 14:58:00 +0800

    registrar owner abi

--- src/contracts/evm/registrar/index.ts
+++ src/contracts/evm/registrar/index.ts
@@ -1,15 +1,6 @@
 import { Interface } from "@ethersproject/abi";
-import { Contract, ContractInterface } from "ethers";
-import { Registrar as TRegistrar } from "types/typechain-types/contracts/core/registrar/registrar.sol/Registrar";
-import { contracts } from "constants/contracts";
 import config from "./abi/config.json";
+import queries from "./abi/queries.json";
 import txs from "./abi/txs.json";
 
-export const Registrar = new Contract(contracts["registrar"], [
-  ...config,
-  ...txs,
-]) as TRegistrar;
-
-export const iregistrar: ContractInterface = Registrar.interface;
-
-export const registrar = new Interface([...config, ...txs]);
+export const registrar = new Interface([...queries, ...config, ...txs]);
