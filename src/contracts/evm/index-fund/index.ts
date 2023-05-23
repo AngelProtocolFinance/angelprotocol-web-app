aff9415594b7b7361d44795f65f9bc2bb0ff5e11
ap-justin <justin@angelprotocol.io>
2023-05-23 18:05:05 +0800

    update indexfund abis

--- src/contracts/evm/index-fund/index.ts
+++ src/contracts/evm/index-fund/index.ts
@@ -1,11 +1,9 @@
 import { Interface } from "@ethersproject/abi";
 import queryConfig from "./queries/config.json";
 import fundDetails from "./queries/fund.json";
-import queries from "./queries/queries.json";
 import txs from "./txs.json";
 
 export const indexFund = new Interface([
-  ...queries,
   ...queryConfig,
   ...fundDetails,
   ...txs,
