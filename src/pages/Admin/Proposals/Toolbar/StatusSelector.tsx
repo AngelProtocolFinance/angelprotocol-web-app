5a16a0f8e0cbe046e95e3aa1cf72732e2fc36070
ap-justin <justin@angelprotocol.io>
2023-06-01 17:45:07 +0800

    change status selector style

--- src/pages/Admin/Proposals/Toolbar/StatusSelector.tsx
+++ src/pages/Admin/Proposals/Toolbar/StatusSelector.tsx
@@ -1,3 +1,4 @@
+import { RadioGroup } from "@headlessui/react";
 import { ProposalStatusOptions } from "slices/admin/types";
 import { useGetter, useSetter } from "store/accessors";
 import { changeSelectedStatus } from "slices/admin/proposals";
@@ -6,38 +7,29 @@ export default function StatusSelector() {
   const dispatch = useSetter();
   const { activeStatus } = useGetter((state) => state.admin.proposals);
 
-  function handleStatusChange(ev: React.ChangeEvent<HTMLSelectElement>) {
-    dispatch(changeSelectedStatus(ev.target.value as ProposalStatusOptions));
-  }
-
   return (
-    <div className="flex gap-2 items-center border p-2 border-prim rounded dark:bg-blue-d4">
-      <label htmlFor="status_selector" className="uppercase text-sm font-bold">
-        status :
-      </label>
-      <select
-        value={activeStatus}
-        onChange={handleStatusChange}
-        id="status_selector"
-        className="bg-transparent text-sm focus:outline-none uppercase "
-      >
-        {Object.entries(pollStatusOptions).map(
-          ([optionValue, optionDescription]) => (
-            <option
-              key={optionValue}
-              value={optionValue}
-              className="text-sm uppercase p-1"
-            >
-              {optionDescription}
-            </option>
-          )
-        )}
-      </select>
-    </div>
+    <RadioGroup
+      value={activeStatus}
+      onChange={(s) => dispatch(changeSelectedStatus(s))}
+      className="flex"
+    >
+      <RadioGroup.Label>Status:</RadioGroup.Label>
+      {Object.entries(pollStatusOptions).map(
+        ([optionValue, optionDescription]) => (
+          <RadioGroup.Option
+            key={optionValue}
+            value={optionValue}
+            className="px-4 py-[0.0625rem] border border-prim rounded capitalize"
+          >
+            {optionDescription}
+          </RadioGroup.Option>
+        )
+      )}
+    </RadioGroup>
   );
 }
 
 const pollStatusOptions: { [key in ProposalStatusOptions]: string } = {
-  executed: "executed",
-  pending: "pending",
+  approved: "approved",
+  open: "open",
 };
