ef7f0e9af5f8fa5f7b575a91072cee78ef175667
ap-justin <justin@angelprotocol.io>
2023-06-01 17:47:51 +0800

    remove block nubmer

--- src/pages/Admin/Proposals/Toolbar/index.tsx
+++ src/pages/Admin/Proposals/Toolbar/index.tsx
@@ -1,25 +1,14 @@
 import { Link } from "react-router-dom";
 import { templates } from "pages/Admin/constants";
-import { useLatestBlockQuery } from "services/juno";
-import Icon from "components/Icon";
-import { humanize } from "helpers";
 import { adminRoutes } from "constants/routes";
 import StatusSelector from "./StatusSelector";
 
 export default function Toolbar({ classes = "" }: { classes?: string }) {
-  const { data: block_height = "0" } = useLatestBlockQuery(10_000);
   return (
     <div
       className={`flex items-center gap-3 ${classes} border-b-2 pb-3 border-prim`}
     >
       <StatusSelector />
-      <p className="ml-auto text-sm flex items-center mr-2 text-gray-d1 dark:text-gray">
-        <span className="font-heading uppercase text-3xs mr-2">
-          current block{" "}
-        </span>
-        <Icon type="Blockchain" className="mr-1" />
-        <span>{humanize(+block_height, 0)}</span>
-      </p>
 
       <Link
         to={`../${adminRoutes.templates}/${templates["multisig.owners"]}`}
