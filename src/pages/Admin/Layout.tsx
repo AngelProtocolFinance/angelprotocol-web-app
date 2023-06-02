6354e9a89e3e54a5bef3e72730caf94d231e8000
ap-justin <justin@angelprotocol.io>
2023-06-02 11:56:11 +0800

    fix active nav padding

--- src/pages/Admin/Layout.tsx
+++ src/pages/Admin/Layout.tsx
@@ -4,7 +4,7 @@ import Sidebar, { SidebarOpener } from "./Sidebar";
 
 export default function Layout({ linkGroups }: { linkGroups: LinkGroup[] }) {
   return (
-    <div className="grid md:grid-cols-[auto_1fr]">
+    <div className="grid content-start md:grid-cols-[auto_1fr]">
       {/** sidebar */}
       <SidebarOpener className="md:hidden" linkGroups={linkGroups} />
       <Sidebar className="max-md:hidden" linkGroups={linkGroups} />
