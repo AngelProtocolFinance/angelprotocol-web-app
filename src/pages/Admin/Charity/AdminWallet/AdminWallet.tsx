e08f7829a5deffde1739ebe43a18fdfdbc6ef883
ap-justin <justin@angelprotocol.io>
2023-06-08 15:21:15 +0800

    gap mobile

--- src/pages/Admin/Charity/AdminWallet/AdminWallet.tsx
+++ src/pages/Admin/Charity/AdminWallet/AdminWallet.tsx
@@ -3,7 +3,7 @@ import Settings from "./Settings";
 
 export default function AdminWallet() {
   return (
-    <div className="grid content-start gap-y-8 @container">
+    <div className="grid content-start gap-y-6 @lg:gap-y-8 @container">
       <h3 className="text-[2rem]">Admin Wallet</h3>
       <Members />
       <Settings />
