97c3a6bb92628702661afafc8067ce1f2f8c70fb
ap-justin <justin@angelprotocol.io>
2023-06-15 17:02:03 +0800

    watch only for usdc

--- src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Breakdown.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Breakdown.tsx
@@ -9,8 +9,7 @@ export default function Breakdown() {
   const fees = getValues("fees");
   const network = watch("network");
   const amounts = watch("amounts");
-  const amount =
-    amounts.find((a) => a.tokenId === denoms.axlusdc)?.value ?? "0";
+  const amount = amounts.find((a) => a.tokenId === denoms.dusd)?.value ?? "0";
   const usdc = +amount;
 
   const prettyFee = fee(network, fees);
@@ -24,8 +23,8 @@ export default function Breakdown() {
   if (network === chainIds.juno || usdc <= 0 || toReceive < 0) return null;
 
   return (
-    <div className="divide-y divide-gray-l2 dark:divide-bluegray-d1">
-      <p className="uppercase font-bold font-heading mb-2 text-sm">Summary</p>
+    <div className="divide-y divide-prim">
+      <p className="font-bold font-work mb-2">Summary</p>
       <Item title="amount" value={usdc.toFixed(2) + " USDC"} />
       <Item title="bridge fee" value={prettyFee.toFixed(2) + " USDC"} />
       <Item title="to receive" value={toReceive.toFixed(2) + " USDC"} />
@@ -36,7 +35,7 @@ export default function Breakdown() {
 type Props = { title: string; value: string };
 function Item({ title, value }: Props) {
   return (
-    <div className="flex justify-between items-center w-full text-sm py-1 text-gray-d1 dark:text-gray">
+    <div className="flex justify-between items-center w-full text-sm py-2 text-gray-d1 dark:text-gray">
       <span className="uppercase font-semibold text-xs">{title}:</span>
       <span>{value}</span>
     </div>
