ea6d2a0bdadfab0ccb680692677a8c91bf2702c4
ap-justin <justin@angelprotocol.io>
2023-06-19 22:46:39 +0800

    auto execute prompt

---
+++ src/pages/Admin/Charity/AdminWallet/Settings/AutoExecutePrompt.tsx
@@ -0,0 +1,58 @@
+import { useAdminResources } from "pages/Admin/Guard";
+import Modal from "components/Modal";
+import { createTx, encodeTx } from "contracts/createTx/createTx";
+import useTxSender from "hooks/useTxSender";
+import { getTagPayloads } from "helpers/admin";
+
+type Props = {
+  newValue: boolean;
+};
+
+export default function AutoExecutePrompt({ newValue }: Props) {
+  const { checkSubmit, multisig } = useAdminResources<"charity">();
+  const { sendTx, isSending } = useTxSender(true);
+
+  async function toggle() {
+    const checkResult = checkSubmit();
+    if (typeof checkResult === "function") return checkResult();
+
+    const [data, dest, meta] = encodeTx("multisig.change-auto-execute", {
+      multisig,
+      autoExecute: newValue,
+    });
+
+    const { wallet, txMeta } = checkResult;
+    const tx = createTx(wallet.address, "multisig.submit-transaction", {
+      multisig: dest,
+      title: `Turn auto-execute ${newValue ? "ON" : "OFF"}`,
+      description: `proposer: ${wallet.address}`,
+      destination: dest,
+      value: "0",
+      data,
+      meta: meta.encoded,
+    });
+
+    await sendTx({
+      content: { type: "evm", val: tx },
+      ...txMeta,
+      tagPayloads: getTagPayloads(txMeta.willExecute && meta.id),
+    });
+  }
+
+  return (
+    <Modal className="p-6 fixed-center z-10 grid gap-4 text-gray-d2 dark:text-white bg-white dark:bg-blue-d4 sm:w-full w-[90vw] sm:max-w-lg rounded overflow-hidden">
+      <p>
+        Turn auto-execute{" "}
+        <span className="font-bold">{newValue ? "ON" : "OFF"}</span>
+      </p>
+      <button
+        type="button"
+        onClick={toggle}
+        className="btn btn-orange mt-6"
+        disabled={isSending}
+      >
+        {isSending ? "Submitting.." : "Submit"}
+      </button>
+    </Modal>
+  );
+}
