d58015934ba6c18d70fb02980b0c51eed53459d4
ap-justin <justin@angelprotocol.io>
2023-06-21 21:25:24 +0800

    remoev console log

--- src/pages/Admin/Charity/OtherSettings/common/Form.tsx
+++ src/pages/Admin/Charity/OtherSettings/common/Form.tsx
@@ -2,19 +2,16 @@ import { FC, PropsWithChildren } from "react";
 
 export const Form: FC<
   PropsWithChildren<React.FormHTMLAttributes<HTMLFormElement>>
-> = ({ children, className, ...props }) => {
-  console.log(props);
-  return (
-    <form
-      className={`${className} grid content-start gap-6 @lg:gap-8 p-4 @lg:p-8 border border-prim rounded dark:bg-blue-d6`}
-      {...props}
+> = ({ children, className, ...props }) => (
+  <form
+    className={`${className} grid content-start gap-6 @lg:gap-8 p-4 @lg:p-8 border border-prim rounded dark:bg-blue-d6`}
+    {...props}
+  >
+    <fieldset
+      disabled={!!props["aria-disabled"]}
+      className="contents" //to not affect layout
     >
-      <fieldset
-        disabled={!!props["aria-disabled"]}
-        className="contents" //to not affect layout
-      >
-        {children}
-      </fieldset>
-    </form>
-  );
-};
+      {children}
+    </fieldset>
+  </form>
+);
