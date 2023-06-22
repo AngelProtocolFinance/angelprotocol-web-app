ba749753e57e56e3bd5a4f13ecdf2c3eabe2796a
ap-justin <justin@angelprotocol.io>
2023-06-22 12:14:18 +0800

    proposal read-only

--- src/pages/Admin/Proposal/index.tsx
+++ src/pages/Admin/Proposal/index.tsx
@@ -3,17 +3,17 @@ import { ProposalParams } from "../types";
 import { useProposalDetailsQuery } from "services/juno/custom";
 import QueryLoader from "components/QueryLoader";
 import Seo from "components/Seo";
-import { DetailLabel, Status } from "components/admin";
+import { DetailLabel, Status, Tooltip } from "components/admin";
 import { APP_NAME, DAPP_URL } from "constants/env";
 import { adminRoutes } from "constants/routes";
-import { useAdminContext } from "../Context";
+import { isTooltip, useAdminContext } from "../Context";
 import Content from "./Content";
 import PollAction from "./PollAction";
 import Stats from "./Stats";
 import Votes from "./Votes";
 
 export default function Proposal() {
-  const { multisig } = useAdminContext();
+  const { multisig, txResource } = useAdminContext();
   const params = useParams<ProposalParams>();
   const queryState = useProposalDetailsQuery({
     id: params.id,
@@ -37,13 +37,19 @@ export default function Proposal() {
               name={proposal.title}
               url={`${DAPP_URL}/${adminRoutes.proposal}/${proposal.id}`}
             />
+            {isTooltip(txResource) && proposal.status !== "approved" ? (
+              <Tooltip tooltip={txResource} classes="mb-4" />
+            ) : null}
             <div className="flex justify-between flex-wrap">
               <p>ID : {proposal.id}</p>
               <Status status={proposal.status} />
             </div>
             <div className="mt-8 mb-6 flex justify-between items-center border-b-2 border-prim pb-2">
               <h4 className="text-lg">{proposal.title}</h4>
-              <PollAction {...proposal} />
+              {isTooltip(txResource) &&
+              proposal.status !== "approved" ? null : (
+                <PollAction {...proposal} />
+              )}
             </div>
             <DetailLabel>description</DetailLabel>
             <p className="mb-6 text-gray-d1 dark:text-gray">
