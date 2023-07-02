7c0e086052e6106b40c0370cda8915442f825797
ap-justin <justin@angelprotocol.io>
2023-07-02 20:18:39 +0800

    update polyfills

--- config-overrides.js
+++ config-overrides.js
@@ -1,29 +1,25 @@
 const { ProvidePlugin } = require("webpack");
+const nodelib = require("node-stdlib-browser");
+const {
+  NodeProtocolUrlPlugin,
+} = require("node-stdlib-browser/helpers/webpack/plugin");
 
 module.exports = function override(config, env) {
   config.module.rules = [
     ...config.module.rules,
     {
-      test: /\.m?js$/,
+      test: /\.m?js/,
       resolve: {
         fullySpecified: false,
       },
     },
   ];
-  config.resolve.fallback = {
-    crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"),
-    buffer: require.resolve("buffer"),
-    path: require.resolve("path-browserify"),
-    https: require.resolve("https-browserify"),
-    http: require.resolve("stream-http"),
-    os: require.resolve("os-browserify/browser"),
-    assert: require.resolve("assert/"),
-  };
+  config.resolve.fallback = nodelib;
+  config.plugins.push(new NodeProtocolUrlPlugin());
   config.plugins.push(
     new ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-      process: "process/browser",
+      process: nodelib.process,
+      Buffer: [nodelib.buffer, "Buffer"],
     })
   );
 
