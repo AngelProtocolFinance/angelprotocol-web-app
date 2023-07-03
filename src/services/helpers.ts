f7583a21e166529e3f8a3c0fe32f17cab750f9a0
ap-justin <justin@angelprotocol.io>
2023-07-03 14:59:35 +0800

    version staging

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -5,5 +5,5 @@ import { IS_TEST } from "constants/env";
  * @param v version number
  * @returns v{v} if not staging
  */
-export const version = (v: number) => (IS_TEST ? `v${v}` : "staging");
+export const version = (v: number) => (IS_TEST ? "staging" : `v${v}`);
 //NOTE: staging usually runs on testnet
