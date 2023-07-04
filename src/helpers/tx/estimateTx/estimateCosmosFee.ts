27b0cf80277302cc4811961ab6519344fc8d02f9
ap-justin <justin@angelprotocol.io>
2023-07-04 16:05:11 +0800

    udpate cosmos gas estimate

--- src/helpers/tx/estimateTx/estimateCosmosFee.ts
+++ src/helpers/tx/estimateTx/estimateCosmosFee.ts
@@ -1,19 +1,114 @@
-import { EncodeObject } from "@cosmjs/proto-signing";
+import Long from "long";
+import { PubKey } from "@keplr-wallet/proto-types/cosmos/crypto/secp256k1/keys";
+import { SignMode } from "@keplr-wallet/proto-types/cosmos/tx/signing/v1beta1/signing";
+import type { SignerInfo } from "@keplr-wallet/proto-types/cosmos/tx/v1beta1/tx";
+import {
+  AuthInfo,
+  TxBody,
+  TxRaw,
+} from "@keplr-wallet/proto-types/cosmos/tx/v1beta1/tx";
+import type { Any } from "@keplr-wallet/proto-types/google/protobuf/any";
+import { JSONAccount, SimulateRes } from "types/cosmos";
 import { Estimate } from "types/tx";
 import { WalletState } from "contexts/WalletContext";
-import Contract from "contracts/Contract";
-import { extractFeeAmount } from "../../extractFeeData";
+import { condenseToNum } from "helpers/decimal";
+import { base64FromU8a } from "helpers/encoding";
 
-export default async function estimateCosmosFee(
+// TODO: uni-6 and juno-1 have diff gas prices for fee display only,
+// actual rate during submission is set by wallet - can be overridden with custom but keplr is buggy when customizing
+// NOTE: use "High" fee setting on JUNO testnet, otherwise transactions will fail
+const GAS_PRICE = "0.075";
+
+// This is the multiplier used when auto-calculating the fees
+// https://github.com/cosmos/cosmjs/blob/5bd6c3922633070dbb0d68dd653dc037efdf3280/packages/stargate/src/signingstargateclient.ts#L290
+const GAS_ADJUSTMENT = 1.3;
+
+export async function estimateCosmosFee(
   wallet: WalletState,
-  msgs: EncodeObject[],
+  msgs: Any[],
   attribute?: string
 ): Promise<Estimate> {
-  const contract = new Contract(wallet);
-  const fee = await contract.estimateFee(msgs);
-  const feeAmount = extractFeeAmount(fee, wallet.displayCoin.token_id);
+  const { chain_id, lcd_url, native_currency } = wallet.chain;
+  const { account } = await fetch(
+    lcd_url + `/cosmos/auth/v1beta1/accounts/${wallet.address}`
+  ).then<{ account: JSONAccount }>((res) => res.json());
+
+  const pub = PubKey.fromJSON({ key: account.pub_key.key });
+  const signer: SignerInfo = {
+    publicKey: {
+      typeUrl: account.pub_key["@type"],
+      value: PubKey.encode(pub).finish(),
+    },
+    modeInfo: {
+      single: { mode: SignMode.SIGN_MODE_DIRECT },
+      multi: undefined,
+    },
+    sequence: account.sequence,
+  };
+
+  const authInfo: AuthInfo = {
+    signerInfos: [signer],
+    fee: {
+      amount: [],
+      gasLimit: "0",
+      granter: "",
+      payer: wallet.address,
+    },
+  };
+
+  const txBody: TxBody = {
+    messages: msgs,
+    memo: "",
+    extensionOptions: [],
+    nonCriticalExtensionOptions: [],
+    timeoutHeight: "0",
+  };
+
+  const bodyBytes = TxBody.encode(txBody).finish();
+  const simTx: TxRaw = {
+    bodyBytes,
+    authInfoBytes: AuthInfo.encode(authInfo).finish(),
+    //num of signature must match num of signers
+    signatures: [new Uint8Array(0)],
+  };
+
+  const res = await fetch(lcd_url + "/cosmos/tx/v1beta1/simulate", {
+    method: "POST",
+    body: JSON.stringify({
+      tx_bytes: base64FromU8a(TxRaw.encode(simTx).finish()),
+    }),
+  }).then<SimulateRes>((res) => {
+    //TODO: create fetch wrapper than handle response error by default
+    if (!res.ok) throw new Error();
+    return res.json();
+  });
+
+  const gas = res.gas_info.gas_used;
+  const adjusted = Math.ceil(+gas * GAS_ADJUSTMENT);
+  const feeAmount = condenseToNum(adjusted * +GAS_PRICE);
+
+  const authInfoWithFee: AuthInfo = {
+    ...authInfo,
+    fee: {
+      amount: [{ amount: GAS_PRICE, denom: native_currency.token_id }],
+      gasLimit: `${adjusted}`,
+      granter: "",
+      payer: wallet.address,
+    },
+  };
+
+  //add fee to estimated Tx
   return {
-    fee: { amount: feeAmount, symbol: wallet.displayCoin.symbol },
-    tx: { type: "cosmos", val: { fee, msgs }, attribute },
+    fee: { amount: feeAmount, symbol: native_currency.symbol },
+    tx: {
+      type: "cosmos",
+      val: {
+        authInfoBytes: AuthInfo.encode(authInfoWithFee).finish(),
+        bodyBytes,
+        accountNumber: Long.fromString(account.account_number),
+        chainId: chain_id,
+      },
+      attribute,
+    },
   };
 }
