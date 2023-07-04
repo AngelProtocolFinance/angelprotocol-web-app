1c47fd3fb17b9d4facfd665a14fd9415acac4de0
ap-justin <justin@angelprotocol.io>
2023-07-04 16:05:22 +0800

    encoding helpers

--- src/helpers/encoding.ts
+++ src/helpers/encoding.ts
@@ -1,9 +1,17 @@
+import { toBase64 as base64FromU8a, toUtf8 as toU8a } from "@cosmjs/encoding";
+
+export {
+  fromBase64 as u8aFromBase64,
+  fromHex as u8aFromHex,
+} from "@cosmjs/encoding";
+export { base64FromU8a, toU8a };
+
 /**
  *
  * @param value object that may contain strings with some [charaters occupying more than one byte](https://developer.mozilla.org/en-US/docs/Web/API/btoa#unicode_strings)
  * @returns base64 encoded string
  */
-export function toBase64<T extends object>(value: T): string {
+export function objToBase64<T extends object>(value: T): string {
   const encoder = new TextEncoder();
   /**
    * convert the string such that each character occupies only one byte
