f97c82c18cbcc5b377b975df00c356a43395b62e
ap-justin <justin@angelprotocol.io>
2023-07-09 20:22:42 +0800

    remove submit variants

--- src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Submit/Submit.tsx
+++ src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Submit/Submit.tsx
@@ -1,26 +1,19 @@
 import { useFormContext } from "react-hook-form";
 import { FV } from "../types";
-import { useAdminContext } from "../../../../../Context";
-import SubmitLocked from "./SubmitLocked";
-import WithdrawButton from "./WithdrawButton";
 
 export default function Submit() {
-  const { endowType } = useAdminContext<"charity">();
   const {
-    getValues,
     formState: { isDirty, isValid, isSubmitting },
   } = useFormContext<FV>();
   const isSubmitDisabled = !isDirty || !isValid || isSubmitting;
 
-  const type = getValues("type");
-  if (type === "liquid") {
-    return <WithdrawButton disabled={isSubmitDisabled} />;
-  }
-  //locked withdraw form not visible to normal endowments when not matured
   return (
-    <SubmitLocked
-      endowment={{ endowType }}
-      isSubmitDisabled={isSubmitDisabled}
-    />
+    <button
+      type="submit"
+      disabled={isSubmitDisabled}
+      className="btn-orange py-3"
+    >
+      Withdraw
+    </button>
   );
 }
--- src/pages/Admin/Charity/Withdraws/Withdrawer/WithdrawForm/Submit/WithdrawButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function WithdrawButton({ disabled }: { disabled: boolean }) {
-  return (
-    <button type="submit" disabled={disabled} className="btn-orange py-3">
-      Withdraw
-    </button>
-  );
-}
