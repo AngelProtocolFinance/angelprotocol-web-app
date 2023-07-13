26b2843e9f81f6fc4eb0133d81b990db6d681964
ap-justin <justin@angelprotocol.io>
2023-07-13 11:25:45 +0800

    programs header

--- src/pages/Admin/Charity/Programs/Programs.tsx
+++ src/pages/Admin/Charity/Programs/Programs.tsx
@@ -1,3 +1,10 @@
 export default function Programs() {
-  return <div>Programs</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h2 className="text-[2rem] font-bold">Programs</h2>
+        <button className="btn-orange px-8 py-2">Create program</button>
+      </div>
+    </div>
+  );
 }
