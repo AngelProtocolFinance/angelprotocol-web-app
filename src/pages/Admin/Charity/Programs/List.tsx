1fbec35782749b038bfca63e367ba7d72efca5b4
ap-justin <justin@angelprotocol.io>
2023-07-18 17:06:07 +0800

    delete wiring

--- src/pages/Admin/Charity/Programs/List.tsx
+++ src/pages/Admin/Charity/Programs/List.tsx
@@ -1,12 +1,9 @@
-import { Link } from "react-router-dom";
-import { Program as TProgram } from "types/aws";
 import { useAdminContext } from "pages/Admin/Context";
 import { useProfileQuery } from "services/aws/aws";
 import ContentLoader from "components/ContentLoader";
-import Image from "components/Image";
 import QueryLoader from "components/QueryLoader";
 import { ErrorStatus, Info } from "components/Status";
-import { adminRoutes } from "constants/routes";
+import { Program } from "./Program";
 
 export default function List() {
   const { id } = useAdminContext();
@@ -29,38 +26,6 @@ export default function List() {
         </div>
       )}
     </QueryLoader>
-  );
-}
-
-function Program(props: TProgram) {
-  return (
-    <div className="p-6 border border-prim rounded bg-gray-l6 dark:bg-blue-d5 grid @lg:flex items-center gap-x-4 gap-y-8">
-      <div className="flex items-center gap-x-4 @lg:contents">
-        <Image
-          src={props.program_banner}
-          alt="program banner"
-          className="w-10 aspect-square object-cover"
-        />
-        <p className="font-bold">{props.program_title}</p>
-      </div>
-
-      <div className="flex items-center gap-x-4 @lg:contents">
-        <button
-          className="btn-outline-filled w-24 py-2 text-sm @lg:ml-auto"
-          type="button"
-          disabled={true}
-        >
-          delete
-        </button>
-        <Link
-          to={"../" + adminRoutes.create_program}
-          className="btn-outline-filled w-24 py-2 text-sm"
-          state={props}
-        >
-          edit
-        </Link>
-      </div>
-    </div>
   );
 }
 
