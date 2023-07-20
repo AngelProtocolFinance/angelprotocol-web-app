bca487f418ca3c3e9ac1b9e767fcf2f0a07bab9a
ap-justin <justin@angelprotocol.io>
2023-07-20 19:01:04 +0800

    convert to disclosure

--- src/pages/Admin/Charity/CreateProgram/Milestones/Milestone.tsx
+++ src/pages/Admin/Charity/CreateProgram/Milestones/Milestone.tsx
@@ -1,9 +1,17 @@
+import { Disclosure } from "@headlessui/react";
 import { FormMilestone } from "../types";
+import { DrawerIcon } from "components/Icon";
 
 export default function Milestone({ milestone_title }: FormMilestone) {
   return (
-    <div className="py-3 px-4 rounded border border-prim text-center dark:bg-blue-d7">
+    <Disclosure
+      as="div"
+      className="relative py-3 px-4 rounded border border-prim text-center dark:bg-blue-d7"
+    >
       <span className="text-xl font-bold font-heading">{milestone_title}</span>
-    </div>
+      <Disclosure.Button className="absolute right-4 top-1/2 -translate-y-1/2">
+        {({ open }) => <DrawerIcon isOpen={open} size={24} />}
+      </Disclosure.Button>
+    </Disclosure>
   );
 }
