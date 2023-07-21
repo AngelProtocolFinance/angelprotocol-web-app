a64beaf80d2540d3880e0724ac9f2af1d5a50926
ap-justin <justin@angelprotocol.io>
2023-07-21 12:08:36 +0800

    milestone panel

--- src/pages/Admin/Charity/CreateProgram/Milestones/Milestone.tsx
+++ src/pages/Admin/Charity/CreateProgram/Milestones/Milestone.tsx
@@ -6,12 +6,25 @@ export default function Milestone({ milestone_title }: FormMilestone) {
   return (
     <Disclosure
       as="div"
-      className="relative py-3 px-4 rounded border border-prim text-center dark:bg-blue-d7"
+      className="border border-prim rounded dark:bg-blue-d7 overflow-hidden"
     >
-      <span className="text-xl font-bold font-heading">{milestone_title}</span>
-      <Disclosure.Button className="absolute right-4 top-1/2 -translate-y-1/2">
-        {({ open }) => <DrawerIcon isOpen={open} size={24} />}
-      </Disclosure.Button>
+      <div className="relative py-3 px-4 text-center">
+        <span className="text-xl font-bold font-heading">
+          {milestone_title}
+        </span>
+        <Disclosure.Button className="absolute right-4 top-1/2 -translate-y-1/2">
+          {({ open }) => <DrawerIcon isOpen={open} size={24} />}
+        </Disclosure.Button>
+      </div>
+
+      <Disclosure.Panel
+        as="div"
+        className={({ open }) =>
+          `${open ? "border-t border-prim" : ""} dark:bg-blue-d5`
+        }
+      >
+        hello world
+      </Disclosure.Panel>
     </Disclosure>
   );
 }
