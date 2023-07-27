6076085d41f8a31b6c55351b5f6b226be54507c7
ap-justin <justin@angelprotocol.io>
2023-07-27 15:51:47 +0800

    milestone tickmark

--- src/pages/Profile/Body/Program/Milestones.tsx
+++ src/pages/Profile/Body/Program/Milestones.tsx
@@ -36,20 +36,25 @@ function Milestone({
   milestone_description,
 }: TMilestone) {
   return (
-    <div className="pb-4 pt-4 first:pt-0 last:pb-0 pl-8 border-l border-orange">
-      <Image
-        src={milestone_media}
-        className="h-60 w-full rounded"
-        height={240}
-      />
-      <p className="mt-4 mb-3 text-gray-d1 dark:text-gray text-xs">
+    <div className="pb-4 pt-4 first:pt-0 last:pb-0 border-l border-orange">
+      <div className="pl-8">
+        <Image src={milestone_media} className="h-60 w-full rounded" />
+      </div>
+
+      <p className="mt-4 pl-8 mb-3 text-gray-d1 dark:text-gray text-xs">
         {new Date(milestone_date).toLocaleDateString()}
       </p>
-      <h6 className="font-bold font-work mb-3">{milestone_title}</h6>
+      <h6 className="pl-8 font-bold font-work mb-3 relative">
+        {milestone_title}
+        <span className="bg-white w-4 h-6 rounded-full absolute left-[-0.5px] top-1/2 -translate-y-1/2 -translate-x-1/2" />
+        <span className="bg-orange w-4 h-4 rounded-full absolute left-[-0.5px] top-1/2 -translate-y-1/2 -translate-x-1/2" />
+      </h6>
       <RichText
         content={milestone_description}
         readOnly
-        classes={{ container: "text-gray-d1 dark:text-gray text-sm" }}
+        classes={{
+          container: "text-gray-d1 dark:text-gray text-sm ml-8 w-full",
+        }}
       />
     </div>
   );
