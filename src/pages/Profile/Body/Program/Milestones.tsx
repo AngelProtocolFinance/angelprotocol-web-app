d62f6f62a595c3b5589d2a31cb60d3742a28a566
ap-justin <justin@angelprotocol.io>
2023-07-27 15:27:05 +0800

    milestone card

--- src/pages/Profile/Body/Program/Milestones.tsx
+++ src/pages/Profile/Body/Program/Milestones.tsx
@@ -1,24 +1,56 @@
-import { MileStone } from "types/aws";
+import { MileStone as TMilestone } from "types/aws";
+import Image from "components/Image";
+import RichText from "components/RichText";
 import { Info } from "components/Status";
 import Container from "../common/Container";
 
 type Props = {
   classes?: string;
-  milestones: MileStone[];
+  milestones: TMilestone[];
 };
 
 export default function Milestones({ classes = "", milestones }: Props) {
   return (
     <Container
       title="Milestones"
-      classes={classes + " w-full lg:w-96"}
+      classes={classes + " w-full lg:w-[29.75rem]"}
       expanded
     >
       {milestones.length > 0 ? (
-        <></>
+        <div className="m-8">
+          {milestones.map((m, idx) => (
+            <Milestone {...m} key={idx} />
+          ))}
+        </div>
       ) : (
         <Info classes="m-6">No milestones found</Info>
       )}
     </Container>
+  );
+}
+
+function Milestone({
+  milestone_media,
+  milestone_date,
+  milestone_title,
+  milestone_description,
+}: TMilestone) {
+  return (
+    <div className="pb-8">
+      <Image
+        src={milestone_media}
+        className="h-60 w-full rounded"
+        height={240}
+      />
+      <p className="mt-4 mb-3 text-gray-d1 dark:text-gray text-xs">
+        {new Date(milestone_date).toLocaleDateString()}
+      </p>
+      <h6 className="font-bold font-work mb-3">{milestone_title}</h6>
+      <RichText
+        content={milestone_description}
+        readOnly
+        classes={{ container: "text-gray-d1 dark:text-gray text-sm" }}
+      />
+    </div>
   );
 }
--- src/pages/Profile/Body/Program/Program.tsx
+++ src/pages/Profile/Body/Program/Program.tsx
@@ -45,7 +45,7 @@ function Skeleton({ className = "" }) {
       className={`${className} grid grid-rows-[auto_auto] gap-8 w-full h-full lg:grid-rows-1 lg:grid-cols-[1fr_auto]`}
     >
       <ContentLoader className="h-80" />
-      <div className="self-start lg:sticky lg:top-28 flex flex-col gap-8 w-full lg:w-96 p-8 border border-prim rounded">
+      <div className="self-start lg:sticky lg:top-28 flex flex-col gap-8 w-full lg:w-[29.75rem] p-8 border border-prim rounded">
         <ContentLoader className="h-40" />
         <ContentLoader className="h-40" />
         <ContentLoader className="h-40" />
