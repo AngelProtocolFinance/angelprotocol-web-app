e201c4396c30e3f5c74ec6820608badac1909dc6
ap-justin <justin@angelprotocol.io>
2023-08-02 10:58:55 +0800

    strategy related abis

--- src/contracts/evm/registrar/abi/queries/index.ts
+++ src/contracts/evm/registrar/abi/queries/index.ts
@@ -1,4 +1,5 @@
 import feeSetting from "./feeSetting.json";
 import queriesAbi from "./queries.json";
+import strategyParams from "./strategy-params.json";
 
-export const queries = [...queriesAbi, ...feeSetting];
+export const queries = [...queriesAbi, ...feeSetting, ...strategyParams];
