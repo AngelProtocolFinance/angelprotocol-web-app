f14b1450fb8d235c88bb0fea2ead081bc63de218
ap-justin <justin@angelprotocol.io>
2023-08-21 20:35:28 +0800

    sign result query params

--- src/pages/Registration/SigningResult.tsx
+++ src/pages/Registration/SigningResult.tsx
@@ -1,3 +1,27 @@
+//www.useanvil.com/docs/api/e-signatures/#redirecturl
+
+type BaseQueryParams = {
+  signerStatus: string;
+  signerEid: string;
+  documentGroupStatus: "completed" | "partial" | "sent";
+  documentGroupEid: string;
+  etchPacketEid: string;
+};
+
+type SignerCompleteQueryParams = BaseQueryParams & { action: "signerComplete" };
+
+type ErrorQueryParams = BaseQueryParams & {
+  action: "signerError";
+  errorType: "tokenExpired" | "tokenInvalid" | "notFound" | "unknown";
+  error: string;
+  message: string;
+};
+
+type QueryParams = SignerCompleteQueryParams | ErrorQueryParams;
+
+const isSuccess = (params: QueryParams): params is SignerCompleteQueryParams =>
+  params.action === "signerComplete";
+
 export default function SignResult({ classes = "" }) {
   //continue registration
   return (
