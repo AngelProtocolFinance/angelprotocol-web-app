f9002612d805397fdfe02a09fc98b5597c8f781d
Andrey <SovereignAndrey>
2023-08-21 13:27:17 +0800

    integrate sentry setup w/ Router-wrapper; add logging w/in existing ErrorHandler to start;

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
+import * as Sentry from "@sentry/react";
+import React from "react";
 import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
 import Loader from "components/Loader";
 import { store } from "store/store";
 import { initTheme } from "helpers";
@@ -20,6 +28,26 @@ const LoaderComponent = () => (
 
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
+
+Sentry.init({
+  dsn: "https://4da0ddd9b67e4801af699469048265f8@o1061537.ingest.sentry.io/6051923",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // See docs for support of different versions of variation of react router
+      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes
+      ),
+    }),
+    new Sentry.Replay(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 root.render(
   <StrictMode>
