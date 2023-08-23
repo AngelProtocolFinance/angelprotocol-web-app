dfce51b53f89e485433e8e08d85e9113561a9823
ap-justin <justin@angelprotocol.io>
2023-08-23 14:20:57 +0800

    update contract addresses

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -15,11 +15,11 @@ const mainnet:Contracts = {
 
 //prettier-ignore
 const testnet:Contracts = {
-  registrar:             "0xe1215B3e150dd298d787D41a4879959D30Dc3939",
-  "index-fund":          "0x2b75bc94116b4037677A270035850b280A3874E9",
-  "multisig/ap":         "0x58F5663E1029F85Ee77b1AD08241d9e626fa4AA9",
-  "multisig/review":     "0x34742B1297AFc4E0F08b6511133091B53C3DF427",
-  accounts:              "0x232A7A490290978D2610f077CC5bcc182Ae47034",
+  registrar:             "0xAF5aC6c9de770303b2a1E8280dB36527dD82199d",
+  "index-fund":          "0xABeD6ced3B2DAe00B6Eb0d1F93eCb479A5c853D4",
+  "multisig/ap":         "0xbC8543cF59ABB82265f421Fe470698E588510443",
+  "multisig/review":     "0xe1E4CD8a1c0273508A114A469e82eF28efB93c44",
+  accounts:              "0xAF6BD38A5cF88bd6D32C22b819507E09dED37e59",
   "gift-card":           "0x47e49a7700c9D79412bb47385eD349106d4941F9",
 }
 
