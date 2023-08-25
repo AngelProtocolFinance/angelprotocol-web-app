bea4592e127f5c28428945375aa3519aa43ada4b
ap-justin <justin@angelprotocol.io>
2023-08-25 17:01:42 +0800

    update consumers

--- src/components/ActivityCountries.tsx
+++ src/components/ActivityCountries.tsx
@@ -1,9 +1,9 @@
-import { FieldValues, Path } from "react-hook-form";
+import { FieldValues, Path, PathValue } from "react-hook-form";
 import countries from "assets/countries/all.json";
-import { OptionType, Selector } from "components/Selector";
+import { MultiSelector, OptionType } from "components/Selector";
 
 type Props<T extends FieldValues, K extends Path<T>> = {
-  name: T[K] extends OptionType<string>[] ? K : never;
+  name: PathValue<T, K> extends OptionType<string>[] ? K : never;
   classes?: {
     container?: string;
     button?: string;
@@ -15,9 +15,8 @@ export default function ActivityCountries<
   K extends Path<T>,
 >({ name, classes }: Props<T, K>) {
   return (
-    <Selector<T, K, string, true>
+    <MultiSelector<T, K, string>
       name={name}
-      multiple
       classes={{ button: classes?.button, container: classes?.container }}
       options={countries.map((c) => ({
         label: c.name,
