9ababe2405d786c58493fdc45e19740c38ac8c96
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-09-06 16:58:31 +0800

    RC-v1.8: fixes (#2342)
    * cherry: store new reg ref on signup
    
    * cherry: update subgraph url

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -4,4 +4,4 @@ import { IS_AST, IS_TEST } from "constants/env";
 export const network: NetworkType = IS_TEST ? "testnet" : "mainnet";
 export const client: DonateClient = IS_AST ? "normal" : "apes";
 export const GRAPHQL_ENDPOINT =
-  "https://api.studio.thegraph.com/query/49156/angel-giving/v0.0.39";
+  "https://api.studio.thegraph.com/query/49156/angel-giving/v0.0.43";
