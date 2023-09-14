7ce7312a6e6473948142af426101f93b7a3cc2a7
Andrey <SovereignAndrey>
2023-09-14 12:34:09 +0800

    admin word/title tweaks

--- src/pages/Admin/Charity/Contributions.tsx
+++ src/pages/Admin/Charity/Contributions.tsx
@@ -1,3 +1,4 @@
+import { PAYMENT_WORDS, titleCase } from "constants/common";
 import { adminRoutes } from "constants/routes";
 import DonationsTable from "./DonationsTable";
 import Seo from "./Seo";
@@ -7,7 +8,9 @@ export default function Contributions() {
     <div>
       <Seo title="Contributions" url={adminRoutes.contributions} />
 
-      <h2 className="text-[2rem] font-bold mb-10">Contributions</h2>
+      <h2 className="text-[2rem] font-bold mb-10">
+        {titleCase(PAYMENT_WORDS.noun.plural)}
+      </h2>
       <DonationsTable />
     </div>
   );
