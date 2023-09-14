d51c16d648213c828a22eafe4104801bf80415dc
ap-justin <justin@angelprotocol.io>
2023-09-14 18:35:31 +0800

    simplify endow selector markup

--- src/pages/Admin/Charity/WidgetConfigurer/WidgetUrlGenerator/EndowmentCombobox/Combobox.tsx
+++ src/pages/Admin/Charity/WidgetConfigurer/WidgetUrlGenerator/EndowmentCombobox/Combobox.tsx
@@ -1,9 +1,9 @@
 import { Combobox as HuiCombobox } from "@headlessui/react";
 import { ErrorMessage } from "@hookform/error-message";
 import React, { useState } from "react";
-import { Controller, useFormContext } from "react-hook-form";
-import { EndowmentIdName } from "types/aws";
-import { useEndowmentIdNamesQuery } from "services/aws/aws";
+import { useController, useFormContext } from "react-hook-form";
+import { EndowmentOption } from "types/aws";
+import { useEndowmentOptionsQuery } from "services/aws/aws";
 import { DrawerIcon } from "components/Icon";
 import QueryLoader from "components/QueryLoader";
 import useDebouncer from "hooks/useDebouncer";
@@ -15,15 +15,19 @@ const containerStyle =
 
 export default function Combobox() {
   const {
-    control,
     formState: { errors, isSubmitting, defaultValues },
   } = useFormContext<FormValues>();
 
-  const [query, setQuery] = useState(defaultValues!.endowIdName!.name);
+  const {
+    field: { value: endowment, onChange: onEndowmentChange, ref },
+  } = useController<Pick<FormValues, "endowment">>({
+    name: "endowment",
+  });
 
+  const [query, setQuery] = useState(defaultValues?.endowment?.name ?? "");
   const [debouncedQuery] = useDebouncer(query, 500);
 
-  const { data, isLoading, isError } = useEndowmentIdNamesQuery({
+  const queryState = useEndowmentOptionsQuery({
     query: debouncedQuery || "matchall",
     sort: "default",
     endow_types: "charity",
@@ -35,78 +39,66 @@ export default function Combobox() {
   });
 
   return (
-    <Controller
-      control={control}
-      name="endowIdName"
-      render={({ field: { value, onChange, ref } }) => (
-        <HuiCombobox
-          aria-disabled={isSubmitting}
-          value={value}
-          onChange={onChange}
-          as="div"
-          className="relative items-center grid grid-cols-[1fr_auto] w-full field-container min-h-[3rem]"
-        >
-          <HuiCombobox.Input
-            ref={ref}
-            placeholder="Select an endowment..."
-            onChange={(event) => setQuery(event.target.value)}
-            displayValue={(value: EndowmentIdName) => value.name}
-            className="pl-4"
-          />
+    <HuiCombobox
+      aria-disabled={isSubmitting}
+      value={endowment}
+      onChange={onEndowmentChange}
+      as="div"
+      className="relative items-center grid grid-cols-[1fr_auto] w-full field-container min-h-[3rem]"
+    >
+      <HuiCombobox.Input
+        ref={ref}
+        placeholder="Select an endowment..."
+        onChange={(event) => setQuery(event.target.value)}
+        displayValue={(value: EndowmentOption) => value.name}
+        className="pl-4"
+      />
 
-          <HuiCombobox.Button>
-            {({ open }) => (
-              <DrawerIcon isOpen={open} size={25} className="mx-1" />
-            )}
-          </HuiCombobox.Button>
+      <HuiCombobox.Button>
+        {({ open }) => <DrawerIcon isOpen={open} size={25} className="mx-1" />}
+      </HuiCombobox.Button>
 
-          <QueryLoader
-            queryState={{
-              data: data?.Items,
-              isLoading,
-              isError,
-            }}
-            messages={{
-              loading: "loading options..",
-              error: "failed to get endowments",
-              empty: debouncedQuery
-                ? `${debouncedQuery} not found`
-                : "no options found",
-            }}
-            classes={{ container: containerStyle + " p-2" }}
+      <QueryLoader
+        queryState={queryState}
+        messages={{
+          loading: "loading options..",
+          error: "failed to get endowments",
+          empty: debouncedQuery
+            ? `${debouncedQuery} not found`
+            : "no options found",
+        }}
+        classes={{ container: containerStyle + " p-2" }}
+      >
+        {(endowments) => (
+          <HuiCombobox.Options
+            className={containerStyle}
+            style={{ height: endowments.length <= 10 ? "auto" : "10rem" }}
           >
-            {(endowIdNames) => (
-              <HuiCombobox.Options
-                className={containerStyle}
-                style={{ height: endowIdNames.length <= 10 ? "auto" : "10rem" }}
-              >
-                {(endowIdNames.length > 0 &&
-                  endowIdNames.map((endowIdName) => (
-                    <HuiCombobox.Option
-                      as={React.Fragment}
-                      key={endowIdName.name}
-                      value={endowIdName}
+            {(endowments.length > 0 &&
+              endowments.map((endowment) => (
+                <HuiCombobox.Option
+                  as={React.Fragment}
+                  key={endowment.name}
+                  value={endowment}
+                >
+                  {({ active }) => (
+                    <li
+                      className={`${
+                        active ? "bg-blue-l2 dark:bg-blue-d1" : ""
+                      } cursor-pointer flex gap-2 p-2 text-sm`}
                     >
-                      {({ active }) => (
-                        <li
-                          className={`${
-                            active ? "bg-blue-l2 dark:bg-blue-d1" : ""
-                          } cursor-pointer flex gap-2 p-2 text-sm`}
-                        >
-                          <span>{endowIdName.name}</span>
-                        </li>
-                      )}
-                    </HuiCombobox.Option>
-                  ))) || (
-                  <div className="p-2 text-sm">{debouncedQuery} not found</div>
-                )}
-              </HuiCombobox.Options>
+                      <span>{endowment.name}</span>
+                    </li>
+                  )}
+                </HuiCombobox.Option>
+              ))) || (
+              <div className="p-2 text-sm">{debouncedQuery} not found</div>
             )}
-          </QueryLoader>
+          </HuiCombobox.Options>
+        )}
+      </QueryLoader>
 
-          <ErrorMessage errors={errors} name="endowIdName.name" as="span" />
-        </HuiCombobox>
-      )}
-    />
+      <ErrorMessage errors={errors} name="endowment.name" as="span" />
+    </HuiCombobox>
   );
 }
