ab211b644f4a2ab3416bb159095e6912d539cd69
ap-justin <justin@angelprotocol.io>
2023-09-14 19:19:21 +0800

    create widget slice

---
+++ src/slices/widget.ts
@@ -0,0 +1,25 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { WidgetConfig } from "types/widget";
+
+type State = WidgetConfig;
+
+const initialState: State = {
+  endowment: { id: 0, name: "Select endowment" },
+  isDescriptionTextShown: true,
+  advancedOptions: {
+    display: "collapsed",
+    liquidSplitPCT: 50,
+  },
+  tokens: [],
+};
+
+const widget = createSlice({
+  name: "widget",
+  initialState,
+  reducers: {
+    update: (state, { payload }: PayloadAction<State>) => payload,
+  },
+});
+
+export default widget.reducer;
+export const { update: updateWidgetConfig } = widget.actions;
