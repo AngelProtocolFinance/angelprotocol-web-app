0eb31e8ad34044b783bea53a7c44c8af938083e9
ap-justin <justin@angelprotocol.io>
2023-09-15 22:50:08 +0800

    widget url height

--- src/pages/Admin/Charity/Widget/Snippet/Snippet.tsx
+++ src/pages/Admin/Charity/Widget/Snippet/Snippet.tsx
@@ -20,10 +20,10 @@ export default function Snippet({ classes = "" }) {
       <h2 className="text-lg @4xl/widget:text-2xl text-center @4xl/widget:text-left mb-3">
         Copy / paste this code snippet:
       </h2>
-      <div className="flex items-center justify-center gap-4 h-32 w-full max-w-xl px-10 rounded bg-gray-l3 dark:bg-blue-d4">
-        <span className="w-full text-sm sm:text-base font-mono break-all">
+      <div className="flex items-center justify-center gap-x-4 max-w-xl px-10 rounded bg-gray-l3 dark:bg-blue-d4">
+        <div className="w-full text-sm sm:text-base font-mono break-all py-4">
           {iframeURL}
-        </span>
+        </div>
         <Copier classes="w-10 h-10 hover:text-orange" text={iframeURL} />
       </div>
     </div>
