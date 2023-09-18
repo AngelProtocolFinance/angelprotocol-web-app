487f196c266e95e8d88c0ee8f398d041e882fb2c
ap-justin <justin@angelprotocol.io>
2023-09-19 00:02:44 +0800

    remove postcssconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
+import postcssImport from "postcss-import";
+import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -9,6 +12,11 @@ const esbuildShim = require.resolve("node-stdlib-browser/helpers/esbuild/shim");
 export default defineConfig(async () => {
   const { default: stdLibBrowser } = await import("node-stdlib-browser");
   return {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer(), postcssImport()],
+      },
+    },
     server: {
       port: 4200,
     },
