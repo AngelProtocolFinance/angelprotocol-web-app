575a32c2582541a7e0ec894576c319e2a58a5488
ap-justin <justin@angelprotocol.io>
2023-09-18 19:56:21 +0800

    add polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,30 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const inject = require("@rollup/plugin-inject");
+const esbuildShim = require.resolve("node-stdlib-browser/helpers/esbuild/shim");
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(async () => {
+  const { default: stdLibBrowser } = await import("node-stdlib-browser");
+  return {
+    resolve: {
+      alias: stdLibBrowser,
+    },
+    optimizeDeps: {
+      include: ["buffer", "process"],
+    },
+    plugins: [
+      {
+        ...inject({
+          global: [esbuildShim, "global"],
+          process: [esbuildShim, "process"],
+          Buffer: [esbuildShim, "Buffer"],
+        }),
+        enforce: "post",
+      },
+      react(),
+      tsconfigPaths(),
+    ],
+  };
 });
