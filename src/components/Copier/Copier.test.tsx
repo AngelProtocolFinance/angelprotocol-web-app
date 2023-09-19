7fd6eb797218c08cac8caea9c8087ec4507c627a
ap-justin <justin@angelprotocol.io>
2023-09-19 15:45:59 +0800

    copier test

--- src/components/Copier/Copier.test.tsx
+++ src/components/Copier/Copier.test.tsx
@@ -1,5 +1,7 @@
+// @vitest-environment jsdom
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { describe, expect, test, vi } from "vitest";
 import Copier from "./Copier";
 
 // Mock copy button
@@ -17,7 +19,7 @@ Object.assign(navigator, {
 
 describe("Copier component test:", () => {
   test("copies correct text and able to change appearance when clicked", async () => {
-    jest.spyOn(navigator.clipboard, "writeText");
+    vi.spyOn(navigator.clipboard, "writeText");
 
     render(<CopyButton />);
 
@@ -35,9 +37,9 @@ describe("Copier component test:", () => {
     });
 
     // Expects the button to change appearance once clicked
-    expect(await screen.findByTitle("Copied!")).toBeInTheDocument();
+    expect(await screen.findByTitle("Copied!")).toBeDefined();
 
     // Expects the button to revert back to it's original appearance
-    expect(await screen.findByTitle("Copy Address")).toBeInTheDocument();
+    expect(await screen.findByTitle("Copy Address")).toBeDefined();
   });
 });
