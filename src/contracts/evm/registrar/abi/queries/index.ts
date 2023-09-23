7b40be4bb9874a1965d720600d1ea5bc146cf534
ap-justin <justin@angelprotocol.io>
2023-09-23 20:58:28 +0800

    add network connection

--- src/contracts/evm/registrar/abi/queries/index.ts
+++ src/contracts/evm/registrar/abi/queries/index.ts
@@ -1,5 +1,11 @@
 import feeSetting from "./feeSetting.json";
+import networkConnection from "./network-connection.json";
 import queriesAbi from "./queries.json";
 import strategyParams from "./strategy-params.json";
 
-export const queries = [...queriesAbi, ...feeSetting, ...strategyParams];
+export const queries = [
+  ...queriesAbi,
+  ...feeSetting,
+  ...strategyParams,
+  ...networkConnection,
+];
