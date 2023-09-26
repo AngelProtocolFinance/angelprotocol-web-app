3eb28ffc806bbac32b132fa86fa2cb15ee204263
Andrey <SovereignAndrey>
2023-09-26 16:12:34 +0800

    add words for 501c3 status question

--- src/pages/Registration/Steps/Documentation/Form/index.tsx
+++ src/pages/Registration/Steps/Documentation/Form/index.tsx
@@ -108,7 +108,11 @@ export default function Form() {
       <Label className="mt-6">
         Is your organization registered in the United States and recognized by
         the Internal Revenue Service as a nonprofit organization exempt under
-        IRC 501(c)(3)?
+        IRC 501(c)(3)?{" "}
+        <strong>
+          If you are NOT registered in the United States as a 501(c)(3) please
+          select No. You will still be able to proceed with registration.
+        </strong>
       </Label>
       <div className="flex gap-4 mt-4 accent-orange text-sm">
         <Radio<FV, "isAuthorizedToReceiveTaxDeductibleDonations">
