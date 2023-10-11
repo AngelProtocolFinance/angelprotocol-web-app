ec4cdda31303c04dce4311edcbb964f42bbc07de
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-10-11 11:08:16 +0000

    Amplify staging added (#2428)
    * Amplify Staging env added
    
    * remove and re-add auth for complete config and setup;
    
    * try #2 fresh dev env backend setup
    
    ---------
    
    Co-authored-by: Andrey <SovereignAndrey>

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "angelgivingwebapp755446e9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
