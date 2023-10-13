ca943b668517c8fdf76ffe582f8b82492c415969
ap-justin <justin@angelprotocol.io>
2023-10-13 13:24:37 +0800

    remove wallet provider in App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,8 @@
 import * as Sentry from "@sentry/react";
-import { WalletProvider } from "@terra-money/wallet-provider";
 import { lazy } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import ModalContext from "contexts/ModalContext";
-import WalletContext from "contexts/WalletContext";
 import useScrollTop from "hooks/useScrollTop";
-import { chainOptions } from "constants/chainOptions";
 import { appRoutes } from "constants/routes";
 import Layout from "./Layout";
 
@@ -27,50 +24,43 @@ export default function App() {
   const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
 
   return (
-    <WalletProvider {...chainOptions}>
-      <WalletContext>
-        <ModalContext>
-          <SentryRoutes>
-            <Route
-              path={`${appRoutes.donate_widget}/:id`}
-              element={<DonateWidget />}
-            />
-            <Route element={<Layout />}>
-              <Route
-                path={`${appRoutes.profile}/:id/*`}
-                element={<Profile legacy />}
-              />
-              <Route path={`${appRoutes.admin}/:id/*`} element={<Admin />} />
+    <ModalContext>
+      <SentryRoutes>
+        <Route
+          path={`${appRoutes.donate_widget}/:id`}
+          element={<DonateWidget />}
+        />
+        <Route element={<Layout />}>
+          <Route
+            path={`${appRoutes.profile}/:id/*`}
+            element={<Profile legacy />}
+          />
+          <Route path={`${appRoutes.admin}/:id/*`} element={<Admin />} />
 
-              <Route
-                path={`${appRoutes.donations}/:address`}
-                element={<Donations />}
-              />
-              <Route path={`${appRoutes.donate}/:id`} element={<Donate />} />
-              <Route
-                path={appRoutes.donate_fiat_thanks}
-                element={<DonateFiatThanks />}
-              />
-              <Route path={appRoutes.leaderboard} element={<Leaderboard />} />
-              <Route
-                path={`${appRoutes.register}/*`}
-                element={<Registration />}
-              />
-              <Route path={`${appRoutes.gift}/*`} element={<Gift />} />
-              <Route path={appRoutes.marketplace} element={<Marketplace />} />
+          <Route
+            path={`${appRoutes.donations}/:address`}
+            element={<Donations />}
+          />
+          <Route path={`${appRoutes.donate}/:id`} element={<Donate />} />
+          <Route
+            path={appRoutes.donate_fiat_thanks}
+            element={<DonateFiatThanks />}
+          />
+          <Route path={appRoutes.leaderboard} element={<Leaderboard />} />
+          <Route path={`${appRoutes.register}/*`} element={<Registration />} />
+          <Route path={`${appRoutes.gift}/*`} element={<Gift />} />
+          <Route path={appRoutes.marketplace} element={<Marketplace />} />
 
-              <Route path={appRoutes.marketplace}>
-                <Route path=":id/*" element={<Profile />} />
-                <Route index element={<Marketplace />} />
-              </Route>
-            </Route>
-            <Route
-              path="*"
-              element={<Navigate replace to={appRoutes.marketplace} />}
-            />
-          </SentryRoutes>
-        </ModalContext>
-      </WalletContext>
-    </WalletProvider>
+          <Route path={appRoutes.marketplace}>
+            <Route path=":id/*" element={<Profile />} />
+            <Route index element={<Marketplace />} />
+          </Route>
+        </Route>
+        <Route
+          path="*"
+          element={<Navigate replace to={appRoutes.marketplace} />}
+        />
+      </SentryRoutes>
+    </ModalContext>
   );
 }
