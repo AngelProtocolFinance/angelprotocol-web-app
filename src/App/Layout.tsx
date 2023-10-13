d9408ed04ba197c38b3aeb39b501aa99da69c8ff
Andrey <SovereignAndrey>
2023-10-13 12:14:16 +0800

    revert temp sunset banner

--- src/App/Layout.tsx
+++ src/App/Layout.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import ExtLink from "components/ExtLink";
 import Loader from "components/Loader";
 import Seo from "components/Seo";
 import { LOGO_DARK } from "constants/common";
@@ -15,23 +14,10 @@ const { HEADER_LINKS, GROUPS_DATA, SOCIAL_MEDIA_LINKS } = IS_AST
 
 export default function Layout() {
   return (
-    <div className="grid grid-rows-[auto_auto_1fr_auto]">
+    <div className="grid grid-rows-[auto_1fr_auto]">
       <Seo /> {/* Load all defaults for SEO meta tags */}
-      <div className="text-center dark:bg-blue bg-blue-d3">
-        <span className="md:text-sm md:uppercase text-white">
-          Looking for the old site? It has moved to{" "}
-          <ExtLink
-            key="banner-link-legacy"
-            className="transition ease-in-out duration-300 hover:text-orange-l1"
-            href="https://legacy.angelgiving.io"
-          >
-            https://legacy.angelgiving.io
-          </ExtLink>{" "}
-          and will be sunset on Oct 13, 2023. Migrate to Polygon today!
-        </span>
-      </div>
       <Header
-        classes="top-0 sticky z-20"
+        classes="sticky top-0 z-20"
         links={HEADER_LINKS}
         logo={LOGO_DARK}
       />
