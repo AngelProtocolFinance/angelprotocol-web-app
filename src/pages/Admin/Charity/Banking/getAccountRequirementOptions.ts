4f30f2e6c4caca65a51b36a11bb5e86aac00a3a1
Nenad <n.misic@outlook.com>
2023-10-16 12:06:22 +0200

    Enable 'real flow' for getAccountRequirementOptions

--- src/pages/Admin/Charity/Banking/getAccountRequirementOptions.ts
+++ src/pages/Admin/Charity/Banking/getAccountRequirementOptions.ts
@@ -5,31 +5,33 @@ export default async function getAccountRequirementOptions(
   targetCurrency: string,
   sourceAmount = 10000
 ): Promise<AccountRequirements[]> {
-  // return fetch(`${URLS.rootUrl}${URLS.createQuote.url()}`, {
-  //   method: URLS.createQuote.method,
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     sourceCurrency: "USD",
-  //     targetCurrency: targetCurrency,
-  //     sourceAmount: sourceAmount,
-  //   }),
-  // })
-  //   .then<{ id: string }>((res) => res.json())
-  //   .then((quote) =>
-  //     fetch(`${URLS.rootUrl}${URLS.getAccountRequirements.url(quote.id)}`)
-  //   )
-  return fetch(
-    `${URLS.rootUrl}${URLS.getAccountRequirementsForRoute.url(
-      targetCurrency,
-      sourceAmount
-    )}`,
-    {
-      method: URLS.getAccountRequirementsForRoute.method,
+  return (
+    fetch(`${URLS.rootUrl}${URLS.createQuote.url()}`, {
+      method: URLS.createQuote.method,
       headers: {
         "Content-Type": "application/json",
       },
-    }
-  ).then<AccountRequirements[]>((res) => res.json());
+      body: JSON.stringify({
+        sourceCurrency: "USD",
+        targetCurrency: targetCurrency,
+        sourceAmount: sourceAmount,
+      }),
+    })
+      .then<{ id: string }>((res) => res.json())
+      .then((quote) =>
+        fetch(`${URLS.rootUrl}${URLS.getAccountRequirements.url(quote.id)}`)
+      )
+      // return fetch(
+      //   `${URLS.rootUrl}${URLS.getAccountRequirementsForRoute.url(
+      //     targetCurrency,
+      //     sourceAmount
+      //   )}`,
+      //   {
+      //     method: URLS.getAccountRequirementsForRoute.method,
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   }
+      .then<AccountRequirements[]>((res) => res.json())
+  );
 }
