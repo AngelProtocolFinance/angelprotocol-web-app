74e01fa60d4076bfedaf8fd9f1e2a8138aa0df28
ap-justin <justin@angelprotocol.io>
2023-10-18 13:40:20 +0800

    pull latest auth changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:55921389-c12e-453e-9dac-0262a490def1",
+    "aws_cognito_identity_pool_id": "us-east-1:be8b5874-c985-49b6-8968-eedb10f7ade2",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_OCKipTloJ",
-    "aws_user_pools_web_client_id": "1fvhhkrvv7lr5q6jmuiomnjeuc",
+    "aws_user_pools_id": "us-east-1_p1kV3Cb0C",
+    "aws_user_pools_web_client_id": "2ho9v068hhr4uae9qmbopom76u",
     "oauth": {
         "domain": "2h40h8r8d938-dev.auth.us-east-1.amazoncognito.com",
         "scope": [
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:4200/",
-        "redirectSignOut": "http://localhost:4200/",
+        "redirectSignIn": "https://auth-ui.dpspevs7tj1ov.amplifyapp.com/,http://localhost:4200/,https://staging.angelgiving.io/",
+        "redirectSignOut": "https://auth-ui.dpspevs7tj1ov.amplifyapp.com/,http://localhost:4200/,https://staging.angelgiving.io/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
@@ -28,11 +28,12 @@ const awsmobile = {
         "GOOGLE"
     ],
     "aws_cognito_signup_attributes": [
-        "EMAIL"
+        "EMAIL",
+        "NAME"
     ],
-    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_configuration": "OPTIONAL",
     "aws_cognito_mfa_types": [
-        "SMS"
+        "TOTP"
     ],
     "aws_cognito_password_protection_settings": {
         "passwordPolicyMinLength": 8,
