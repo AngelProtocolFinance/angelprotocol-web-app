4b6e00fa1d46d2d4a3b24dddbae4d26fbb70b849
Nenad <n.misic@outlook.com>
2023-10-19 06:31:26 +0200

    Extract fields into Form.tsx

--- src/pages/Admin/Charity/Banking/RecipientDetailsForm/Form.tsx
+++ src/pages/Admin/Charity/Banking/RecipientDetailsForm/Form.tsx
@@ -1,3 +1,52 @@
-export default function Form() {
-  return <div>Form</div>;
+import { Path } from "react-hook-form";
+import { AccountRequirements } from "../types";
+import { FormValues } from "./types";
+import { Selector } from "components/Selector";
+import { Field, Label } from "components/form";
+import { isEmpty } from "helpers";
+
+type Props = { accountRequirements: AccountRequirements };
+
+export default function Form({ accountRequirements }: Props) {
+  return (
+    <>
+      {accountRequirements.fields.map((field) => {
+        const requirements = field.group[0];
+
+        // react-hook-form turns dot-fields into nested objects, https://github.com/react-hook-form/react-hook-form/issues/3755#issuecomment-943408807
+        const requirementsKey = requirements.key.replace(".", "__");
+
+        const name: Path<FormValues> = `requirements.${accountRequirements.type}.${requirementsKey}`;
+
+        if (
+          requirements.type === "text" ||
+          !requirements.valuesAllowed ||
+          isEmpty(requirements.valuesAllowed)
+        ) {
+          return (
+            <Field<FormValues>
+              key={name}
+              name={name}
+              label={requirements.name}
+              placeholder={requirements.example}
+              classes="field-admin"
+            />
+          );
+        }
+
+        return (
+          <div key={name} className="flex flex-col">
+            <Label required={requirements.required}>{requirements.name}</Label>
+            <Selector<FormValues, string>
+              name={name}
+              options={requirements.valuesAllowed.map((valuesAllowed) => ({
+                label: valuesAllowed.name,
+                value: valuesAllowed.key,
+              }))}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
 }
--- src/pages/Admin/Charity/Banking/RecipientDetailsForm/RecipientDetailsForm.tsx
+++ src/pages/Admin/Charity/Banking/RecipientDetailsForm/RecipientDetailsForm.tsx
@@ -1,11 +1,8 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { FormProvider } from "react-hook-form";
 import { AccountRequirements } from "../types";
-import { FormValues } from "./types";
-import { Selector } from "components/Selector";
-import { Field, Label } from "components/form";
-import { isEmpty } from "helpers";
 import AccountRequirementsSelector from "../AccountRequirementsSelector";
+import Form from "./Form";
 import useRecipientForm from "./useRecipientForm";
 
 type Props = {
@@ -24,61 +21,21 @@ export default function RecipientDetailsForm({
 
   const methods = useRecipientForm(targetCurrency, accountRequirements);
 
-  const fields = useMemo(() => {
-    if (selectedAccountRequirementsIndex == null) {
-      return null;
-    }
-
-    const selectedRequirements =
-      accountRequirements[selectedAccountRequirementsIndex];
-
-    return selectedRequirements.fields.map((field) => {
-      const requirements = field.group[0];
-
-      // react-hook-form turns dot-fields into nested objects, https://github.com/react-hook-form/react-hook-form/issues/3755#issuecomment-943408807
-      const requirementsKey = requirements.key.replace(".", "__");
-
-      if (
-        requirements.type === "text" ||
-        !requirements.valuesAllowed ||
-        isEmpty(requirements.valuesAllowed)
-      ) {
-        return (
-          <Field<FormValues>
-            key={`requirements.${selectedRequirements.type}.${requirementsKey}`}
-            classes="field-admin"
-            name={`requirements.${selectedRequirements.type}.${requirementsKey}`}
-            label={requirements.name}
-            placeholder={requirements.example}
-          />
-        );
-      }
-
-      return (
-        <div
-          key={`requirements.${selectedRequirements.type}.${requirementsKey}`}
-          className="flex flex-col"
-        >
-          <Label required={requirements.required}>{requirements.name}</Label>
-          <Selector<FormValues, string>
-            name={`requirements.${selectedRequirements.type}.${requirementsKey}`}
-            options={requirements.valuesAllowed.map((valuesAllowed) => ({
-              label: valuesAllowed.name,
-              value: valuesAllowed.key,
-            }))}
-          />
-        </div>
-      );
-    });
-  }, [accountRequirements, selectedAccountRequirementsIndex]);
-
   return (
     <>
       <AccountRequirementsSelector
         accountRequirements={accountRequirements}
         onChange={(index: number) => setSelectedAccountRequirementsIndex(index)}
       />
-      <FormProvider {...methods}>{fields}</FormProvider>
+      <FormProvider {...methods}>
+        {selectedAccountRequirementsIndex != null && (
+          <Form
+            accountRequirements={
+              accountRequirements[selectedAccountRequirementsIndex]
+            }
+          />
+        )}
+      </FormProvider>
     </>
   );
 }
