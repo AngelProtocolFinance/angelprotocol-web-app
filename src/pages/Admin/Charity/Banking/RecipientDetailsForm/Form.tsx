0f8da986d20a6c6d213f881593a514ff57eba5f7
Nenad <n.misic@outlook.com>
2023-10-19 09:45:18 +0200

    Format form and console log form values on submit

--- src/pages/Admin/Charity/Banking/RecipientDetailsForm/Form.tsx
+++ src/pages/Admin/Charity/Banking/RecipientDetailsForm/Form.tsx
@@ -1,4 +1,4 @@
-import { Path } from "react-hook-form";
+import { Path, useFormContext } from "react-hook-form";
 import { AccountRequirements } from "../types";
 import { FormValues } from "./types";
 import { Selector } from "components/Selector";
@@ -9,63 +9,87 @@ import { undot } from "./dot";
 type Props = { accountRequirements: AccountRequirements };
 
 export default function Form({ accountRequirements }: Props) {
+  const {
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useFormContext<FormValues>();
+
+  console.log("errors");
+  console.log(errors);
+  const onSubmit = handleSubmit(async (formValues) => {
+    console.log("formValues");
+    console.log(formValues);
+  });
+
   return (
-    <>
-      {accountRequirements.fields.map((field) => {
-        const requirements = field.group[0];
+    <form onSubmit={onSubmit} className="grid gap-4">
+      <div className="grid grid-cols-2 gap-2">
+        {accountRequirements.fields.map((field) => {
+          const requirements = field.group[0];
 
-        const requirementsKey = undot(requirements.key);
+          const requirementsKey = undot(requirements.key);
 
-        const name: Path<FormValues> = `requirements.${accountRequirements.type}.${requirementsKey}`;
+          const name: Path<FormValues> = `requirements.${accountRequirements.type}.${requirementsKey}`;
 
-        if (requirements.type === "date") {
+          if (requirements.type === "date") {
+            return (
+              <Field<FormValues, "date">
+                key={name}
+                name={name}
+                type="date"
+                label={requirements.name}
+                required={requirements.required}
+                classes={{
+                  input: "date-input uppercase",
+                  container: "field-admin",
+                }}
+              />
+            );
+          }
+
+          if (
+            requirements.type === "text" ||
+            // if by any chance there are fields that are of type `"text"`, but DO have `valuesAllowed` defined,
+            // they should be treated as selectors
+            !requirements.valuesAllowed ||
+            isEmpty(requirements.valuesAllowed)
+          ) {
+            return (
+              <Field<FormValues>
+                key={name}
+                name={name}
+                label={requirements.name}
+                placeholder={requirements.example}
+                required={requirements.required}
+                classes="field-admin"
+              />
+            );
+          }
+
           return (
-            <Field<FormValues, "date">
-              key={name}
-              name={name}
-              type="date"
-              label={requirements.name}
-              required={requirements.required}
-              classes={{
-                input: "date-input uppercase",
-                container: "field-admin",
-              }}
-            />
+            <div key={name} className="flex flex-col">
+              <Label required={requirements.required}>
+                {requirements.name}
+              </Label>
+              <Selector<FormValues, string>
+                name={name}
+                options={requirements.valuesAllowed.map((valuesAllowed) => ({
+                  label: valuesAllowed.name,
+                  value: valuesAllowed.key,
+                }))}
+              />
+            </div>
           );
-        }
+        })}
+      </div>
 
-        if (
-          requirements.type === "text" ||
-          // if by any chance there are fields that are of type `"text"`, but DO have `valuesAllowed` defined,
-          // they should be treated as selectors
-          !requirements.valuesAllowed ||
-          isEmpty(requirements.valuesAllowed)
-        ) {
-          return (
-            <Field<FormValues>
-              key={name}
-              name={name}
-              label={requirements.name}
-              placeholder={requirements.example}
-              required={requirements.required}
-              classes="field-admin"
-            />
-          );
-        }
-
-        return (
-          <div key={name} className="flex flex-col">
-            <Label required={requirements.required}>{requirements.name}</Label>
-            <Selector<FormValues, string>
-              name={name}
-              options={requirements.valuesAllowed.map((valuesAllowed) => ({
-                label: valuesAllowed.name,
-                value: valuesAllowed.key,
-              }))}
-            />
-          </div>
-        );
-      })}
-    </>
+      <button
+        disabled={isSubmitting}
+        type="submit"
+        className="px-6 btn-orange text-sm w-80"
+      >
+        Save
+      </button>
+    </form>
   );
 }
