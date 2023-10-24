17170edea896dc6aa4624a186dd9fa6f16844e41
Nenad <n.misic@outlook.com>
2023-10-24 09:11:28 +0200

    Remove comment from Banking.tsx

--- src/pages/Admin/Charity/Banking/Banking.tsx
+++ src/pages/Admin/Charity/Banking/Banking.tsx
@@ -3,14 +3,6 @@ import { debounce } from "helpers";
 import CurrencySelector from "./CurrencySelector";
 import RecipientDetails from "./RecipientDetails";
 
-// TODO: Once recipient is created by filling fields returned using `GET /v1/account-requirements?source=EUR&target=USD&sourceAmount=1000`
-// we need to use its recipientID to create a quote `https://docs.wise.com/api-docs/api-reference/quote#create-authenticated`
-// and use this quote to get maybe even some other fields, fetched with:
-// `GET /v1/quotes/{{quoteId}}/account-requirements`
-// `POST /v1/quotes/{{quoteId}}/account-requirements`
-// Reason: not all required fields get returned by `GET /v1/account-requirements`, see 4th paragraph in docs:
-// https://docs.wise.com/api-docs/api-reference/recipient#account-requirements
-
 export default function Banking() {
   const [targetCurrency, setTargetCurrency] = useState<string>();
   const [sourceAmount, setSourceAmount] = useState<number>();
