fa02dbea68c4f0462d3ef6a7d6ceb27850264e1d
Nenad <n.misic@outlook.com>
2023-10-27 05:10:56 +0200

    Update 'dot.ts' comment

--- src/pages/Admin/Charity/Banking/RecipientDetails/dot.ts
+++ src/pages/Admin/Charity/Banking/RecipientDetails/dot.ts
@@ -2,7 +2,8 @@ const DOT = ".";
 const DOLLAR_SIGN = "$";
 
 /**
- * `react-hook-form` turns dot-field names into nested object fields, causing weird behavior when accessing/assigning said fields if they're deeply nested.
+ * `react-hook-form` turns dot-field names into nested object fields, causing weird behavior when
+ *  accessing/assigning said fields if they're deeply nested.
  *
  * E.g. with an object like:
  * ```
@@ -26,12 +27,18 @@ const DOLLAR_SIGN = "$";
  *      }
  *  }
  * ```
- * But when accessing/assigning this `address.country` field, `react-hook-form` gets confused and has no idea how to access the field:
+ * This is the default behavior, see:
+ * - https://github.com/react-hook-form/react-hook-form/issues/3351#issuecomment-721996499
+ * - https://react-hook-form.com/docs/useform/register
+ *
+ * The problem is that it is then difficult to access this `address.country` field, as `react-hook-form`
+ * gets confused and has no idea how to access the field:
  * ```
  *  console.log(methods.getValues("requirements.type.address.country")) // returns undefined
  * ```
  *
- * To solve, turn dots into some other character https://github.com/react-hook-form/react-hook-form/issues/3755#issuecomment-943408807
+ * To solve, turn dots into some other character when creating the form, see:
+ * https://github.com/react-hook-form/react-hook-form/issues/3755#issuecomment-943408807
  *
  * @param key string
  * @returns same string with dot characters (.) replaced with dollar signs ($)
