08093fd9f19a6c8568dafa494852ec1928b22455
ap-justin <justin@angelprotocol.io>
2023-11-01 01:00:33 +0800

    manual keeping of redirect state

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,14 +1,24 @@
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import { useEffect } from "react";
 import { Location, useLocation, useNavigate } from "react-router-dom";
+import LoaderRing from "components/LoaderRing";
+import { OAUTH_PATH_STORAGE_KEY } from "constants/o-auth";
 
 export default function Signin() {
   const { state } = useLocation();
   const navigate = useNavigate();
   const _state = state as { from?: Location } | undefined;
-  const { route } = useAuthenticator((context) => [context.route]);
+  const { route, authStatus } = useAuthenticator((context) => [
+    context.route,
+    context.authStatus,
+  ]);
 
   const from = _state?.from?.pathname || "/";
+
+  useEffect(() => {
+    localStorage.setItem(OAUTH_PATH_STORAGE_KEY, from);
+    //eslint-disable-next-line
+  }, []);
 
   useEffect(() => {
     if (route === "authenticated") {
@@ -17,8 +27,13 @@ export default function Signin() {
   }, [route, navigate, from]);
 
   return (
-    <div className="grid content-start justify-items-center pt-8 pb-16">
-      <Authenticator formFields={{ signUp: { name: { order: 1 } } }} />
+    <div className="grid place-items-center py-14">
+      {authStatus === "configuring" || authStatus === "authenticated" ? (
+        //while user is still authenticated, use loader in place of authenticator while navigating
+        <LoaderRing thickness={12} classes="w-32" />
+      ) : (
+        <Authenticator formFields={{ signUp: { name: { order: 1 } } }} />
+      )}
     </div>
   );
 }
