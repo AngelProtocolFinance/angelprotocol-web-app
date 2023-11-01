b2abf3a925a9e8027e91ecb8c92910be03fed485
Nenad <n.misic@outlook.com>
2023-11-01 07:24:33 +0100

    Update dark theme for VerificationStatus

--- src/pages/Admin/Charity/Banking/VerificationStatus.tsx
+++ src/pages/Admin/Charity/Banking/VerificationStatus.tsx
@@ -42,35 +42,34 @@ function VerificationStatus() {
   switch (profile.bank_verified) {
     case "Not Submitted":
       return (
-        <span className="flex gap-1 items-center justify-center w-36 text-xs bg-gray-l4 sm:text-sm font-semibold p-2 rounded">
+        <span className="flex gap-1 items-center justify-center w-36 p-2 rounded bg-gray-l4 dark:bg-gray text-xs sm:text-sm font-semibold">
           Not submitted{" "}
           <Icon type="Exclamation" aria-hidden className="text-sm" />
         </span>
       );
     case "Under Review":
       return (
-        <span className="flex gap-1 items-center justify-center w-36 bg-orange-l4 text-xs sm:text-sm text-orange-d1 font-semibold px-2 rounded">
+        <span className="flex gap-1 items-center justify-center w-36 p-2 rounded bg-orange-l4 dark:bg-orange-l1 text-xs sm:text-sm text-orange-d1 dark:text-white font-semibold">
           Under review{" "}
           <Icon type="HourglassSplit" aria-hidden className="text-sm" />
         </span>
       );
     case "Approved":
       return (
-        <span className="flex gap-1 items-center justify-center w-36 bg-green-l3 text-xs sm:text-sm text-green-d2 font-semibold px-2 rounded">
-          Approved{" "}
-          <Icon type="Check" aria-hidden className="text-xl sm:text-2xl" />
+        <span className="flex gap-1 items-center justify-center w-36 p-2 rounded bg-green-l3 dark:bg-green-d1 text-xs sm:text-sm text-green-d2 dark:text-white font-semibold">
+          Approved <Icon type="Check" aria-hidden className="text-sm" />
         </span>
       );
     case "Rejected":
       return (
-        <span className="flex gap-1 items-center justify-center w-36 bg-red-l4 text-xs sm:text-sm text-red-d1 font-semibold px-2 rounded">
+        <span className="flex gap-1 items-center justify-center w-36 p-2 rounded bg-red-l4 dark:bg-red-l1 text-xs sm:text-sm text-red-d1 dark:text-white font-semibold">
           Rejected{" "}
           <Icon type="ExclamationCircleFill" aria-hidden className="text-sm" />
         </span>
       );
     default:
       return (
-        <span className="flex items-center text-red text-sm">
+        <span className="flex items-center text-red dark:text-red-l2 text-sm">
           error, no bank status present
         </span>
       );
