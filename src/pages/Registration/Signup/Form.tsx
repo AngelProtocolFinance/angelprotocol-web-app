c17d9d175c99896d05db64ed85cb80c020d92c38
ap-justin <justin@angelprotocol.io>
2023-11-04 16:32:20 +0800

    remove redundant privacy policy

--- src/pages/Registration/Signup/Form.tsx
+++ src/pages/Registration/Signup/Form.tsx
@@ -1,17 +1,13 @@
 import { Link } from "react-router-dom";
-import { FormValues as FV } from "./types";
-import ExtLink from "components/ExtLink";
-import { CheckField } from "components/form";
 import { Separator } from "components/registration";
 import { APP_NAME } from "constants/env";
-import { PRIVACY_POLICY } from "constants/urls";
 import routes from "../routes";
 import useSubmit from "./useSubmit";
 
 const NEED_HELP_ARTICLE_ID = 6628120;
 
 export default function Form({ classes = "" }: { classes?: string }) {
-  const { submit, isSubmitting } = useSubmit();
+  const { handleSubmit, isSubmitting } = useSubmit();
 
   const openIntercomHelp = () => {
     const w = window as any;
@@ -22,24 +18,10 @@ export default function Form({ classes = "" }: { classes?: string }) {
 
   return (
     <form
-      onSubmit={submit}
+      onSubmit={handleSubmit}
       className={`${classes} justify-center gap-8 padded-container w-full max-w-[37.5rem] grid`}
     >
       <h3 className="text-3xl text-center">{`Register to ${APP_NAME}`}</h3>
-
-      <CheckField<FV>
-        required
-        name="hasAgreedToPrivacyPolicy"
-        classes={{
-          container: "check-field-reg justify-self-center -mt-4 text-xs",
-          error: "mt-2",
-        }}
-      >
-        I declare that I have read and agreed to{" "}
-        <ExtLink className="underline text-orange-l1" href={PRIVACY_POLICY}>
-          Privacy Policy
-        </ExtLink>
-      </CheckField>
 
       <button
         type="submit"
