4a00ffb95cdb0b93155bd4459cba09f0e1b126ed
Andrey <SovereignAndrey>
2023-11-13 10:17:31 +0800

    more filters css cleanup;

--- src/pages/Marketplace/Sidebar/index.tsx
+++ src/pages/Marketplace/Sidebar/index.tsx
@@ -17,13 +17,13 @@ export default function Sidebar({ classes = "" }: { classes?: string }) {
     <div
       className={`border border-prim dark:bg-blue-d4 dark:text-white overflow-y-auto md:overflow-hidden md:rounded-md content-start md:h-fit md:w-80 ${classes}`}
     >
-      <div className="flex justify-between p-3 items-center md:hidden bg-red-l6 dark:bg-blue-d7 border-b border-prim">
+      <div className="flex justify-between p-3 items-center md:hidden border-b border-prim">
         <h3 className="text-red text-xl font-black uppercase">Filters</h3>
         <button onClick={toggleFilter} className="active:text-red">
           <Icon type="Close" size={25} />
         </button>
       </div>
-      <div className="bg-gray-l4 dark:bg-blue-d4 flex items-center justify-between p-4 border-b border-prim">
+      <div className="bg-gray-l4 dark:bg-gray-d1 flex items-center justify-between p-4 border-b border-prim">
         <h3 className="uppercase">Filter by</h3>
         <button
           type="button"
