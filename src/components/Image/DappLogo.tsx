a94e6f48839ad800aa962c2c99fc7c5f186cb2cc
Andrey <SovereignAndrey>
2023-11-13 10:32:53 +0800

    temp logo BG added for now;

--- src/components/Image/DappLogo.tsx
+++ src/components/Image/DappLogo.tsx
@@ -1,11 +1,11 @@
-import dappLogo from "assets/images/angelgiving-H-logo-wht.svg";
+import dappLogo from "assets/images/bettergiving-logo.svg";
 import { BASE_URL } from "constants/env";
 import Image from "./Image";
 
 export default function DappLogo({ classes = "" }) {
   return (
     <Image
-      className={classes}
+      className={classes + " h-[90px]"}
       src={dappLogo}
       title="Go to Marketing page"
       href={BASE_URL}
