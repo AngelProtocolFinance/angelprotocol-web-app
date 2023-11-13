d67f6a836747d8128008038526f950cbec95dd37
ap-justin <justin@angelprotocol.io>
2023-11-13 16:58:49 +0800

    fsa documentaiton done

--- src/pages/Registration/Steps/Documentation/FSA/Form/index.tsx
+++ src/pages/Registration/Steps/Documentation/FSA/Form/index.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { FormValues as FV, Props } from "../types";
 import { useRegState } from "pages/Registration/Steps/StepGuard";
 import ExtLink from "components/ExtLink";
+import Icon from "components/Icon";
 import { Field, Label } from "components/form";
 import { FileDropzone, LoadText } from "components/registration";
 import { steps } from "../../../../routes";
@@ -14,13 +15,7 @@ export default function Form(props: Props) {
 
   return (
     <form className="w-full" onSubmit={submit}>
-      <h2 className="text-center sm:text-left text-xl mb-2">Documentation</h2>
-
-      <Separator classes="my-8" />
-
-      <h4 className="text-center sm:text-left text-lg mt-8">
-        Government issued ID
-      </h4>
+      <h4 className="text-center sm:text-left">Government issued ID</h4>
       <Label required className="mb-2 mt-1">
         Please provide passport, driver's license, or ID card.
       </Label>
@@ -29,19 +24,15 @@ export default function Form(props: Props) {
         tooltip={fileTooltip}
       />
 
-      <h2 className="text-center sm:text-left text-lg mt-8">
-        Organizational Details
-      </h2>
-
       <Field<FV>
         name="RegistrationNumber"
         label="Registration number"
         required
-        classes={{ container: "mb-6 mt-1" }}
+        classes={{ container: "mb-6 mt-10", label: "font-semibold" }}
         placeholder="e.g. xx-xxxxxxxxxx"
       />
 
-      <Label className="mb-2" required>
+      <Label className="mb-2 mt-10 font-semibold" required>
         Proof of registration as a 501(C)(3) nonprofit or equivalent
       </Label>
       <FileDropzone<FV, "ProofOfRegistration">
@@ -54,7 +45,7 @@ export default function Form(props: Props) {
         label="What type of legal entity is your organization registered as? This can
         usually be found in your registration/organizing document"
         required
-        classes={{ container: "mb-2 mt-6" }}
+        classes={{ container: "mb-2 mt-10" }}
         placeholder="e.g. Nonprofit Organization"
       />
 
@@ -63,38 +54,24 @@ export default function Form(props: Props) {
         name="ProjectDescription"
         label="Please provide a description of your organization's charitable activities as well as your charitable mission."
         required
-        classes={{ container: "mb-6 mt-4" }}
+        classes={{ container: "mb-6 mt-10" }}
         placeholder=""
       />
 
-      <Separator classes="my-8" />
-
-      {!props.doc?.SignedFiscalSponsorshipAgreement ? (
-        <>
-          <p className="text-center mt-6">
-            Angel Giving serves as a granting organization, accepting donations
-            on behalf of our partner nonprofits and granting them out after
-            processing and converting. This requires our partner nonprofits to
-            be able to accept US tax-deductible donations.
-          </p>
-
-          <p className="mt-6 text-center">
-            Angel Giving provides fiscal sponsorship services at market-leading
-            cost for our partner organizations to enable this ability (2.9%). If
-            you would like to proceed, please review and sign the fiscal
-            sponsorship agreement:
-          </p>
-        </>
-      ) : (
-        <ExtLink href={props.doc.SignedFiscalSponsorshipAgreement}>
-          ^^ Signed Fiscal sponsorship agreement
+      {props.doc?.SignedFiscalSponsorshipAgreement ? (
+        <ExtLink
+          href={props.doc.SignedFiscalSponsorshipAgreement}
+          className="text-sm text-blue hover:text-blue-l2 flex items-center gap-2"
+        >
+          <Icon type="ExternalLink" size={20} />
+          <span>Signed Fiscal sponsorship agreement</span>
         </ExtLink>
-      )}
+      ) : null}
 
       <div className="grid grid-cols-2 sm:flex gap-2 mt-8">
         <Link
           aria-disabled={isSubmitting}
-          to={`../${steps.orgDetails}`}
+          to={`../${steps.fsaInquiry}`}
           state={data.init}
           className="py-3 min-w-[8rem] btn-outline-filled btn-reg"
         >
@@ -116,9 +93,5 @@ export default function Form(props: Props) {
     </form>
   );
 }
-
-const Separator = ({ classes = "" }: { classes?: string }) => (
-  <div className={`${classes} h-px w-full bg-gray-l3 dark:bg-bluegray`} />
-);
 
 const fileTooltip = `Valid types are: PDF, JPG, PNG and WEBP. File should be less than ${MB_LIMIT}MB.`;
