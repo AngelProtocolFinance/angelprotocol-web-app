7db3d4199910e3a00f702773170e5c5a9c0d4d50
Nenad <n.misic@outlook.com>
2023-11-14 11:03:17 +0100

    Add additional data to Dashboard UI (pending/total payout, total contrib.)

--- src/pages/Admin/Charity/Dashboard/Balance.tsx
+++ src/pages/Admin/Charity/Dashboard/Balance.tsx
@@ -11,14 +11,14 @@ type Props = {
 export default function Balance({ type, current, pending, payedOut }: Props) {
   return (
     <div className="@container rounded border border-prim bg-orange-l6 dark:bg-blue-d6 p-4">
-      <h4 className="uppercase text-sm sm:text-xl font-bold mb-5">
+      <h4 className="uppercase text-sm @xs:text-xl font-bold mb-5">
         {type} account
       </h4>
-      <p className="text-xs sm:text-sm text-gray-d1 dark:text-gray mb-8">
+      <p className="text-xs @xs:text-sm text-gray-d1 dark:text-gray mb-8">
         Note: balances are estimations.
       </p>
-      <div className="grid grid-cols-[auto_1fr] gap-y-5 justify-self-start gap-x-2 sm:gap-x-8 @lg:flex @lg:gap-x-8">
-        <Amount title="Current balance" classes="col-span-full @lg:mr-auto">
+      <div className="grid @xs:grid-cols-[auto_1fr] gap-y-5 justify-self-start gap-x-2 @xs:gap-x-8">
+        <Amount title="Current balance" classes="col-span-full @xs:mr-auto">
           {humanize(current, 2)}
         </Amount>
         <Amount title="Pending payout">{humanize(pending, 2)}</Amount>
@@ -34,12 +34,12 @@ function Amount({
 }: PropsWithChildren<{ classes?: string; title: string }>) {
   return (
     <div className={classes}>
-      <p className="text-2xs sm:text-xs text-gray-d1 dark:text-gray sm:mb-1 uppercase">
+      <p className="text-2xs @xs:text-xs text-gray-d1 dark:text-gray @xs:mb-1 uppercase">
         {props.title}
       </p>
-      <span className="font-bold text-sm sm:text-xl font-heading">
+      <span className="font-bold text-sm @xs:text-xl font-heading">
         {props.children}{" "}
-        <span className="text-2xs sm:text-xs font-normal">USD</span>
+        <span className="text-2xs @xs:text-xs font-normal">USD</span>
       </span>
     </div>
   );
--- src/pages/Admin/Charity/Dashboard/Dashboard.tsx
+++ src/pages/Admin/Charity/Dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { PropsWithChildren } from "react";
 import { useAdminContext } from "pages/Admin/Context";
 import { useProfileQuery } from "services/aws/aws";
 import ContentLoader from "components/ContentLoader";
@@ -10,7 +11,7 @@ export default function Dashboard() {
   const queryState = useProfileQuery({ endowId: id }, { skip: !id });
 
   return (
-    <div className="w-full max-w-4xl grid content-start mt-6">
+    <div className="@container w-full max-w-4xl grid content-start mt-6">
       <Seo title="Endowment Dashboard" />
       <h3 className="uppercase font-extrabold text-2xl mb-4">Dashboard</h3>
       <QueryLoader
@@ -27,11 +28,35 @@ export default function Dashboard() {
           payoutsMade,
           sustainabilityFundBal,
           totalEarnings,
+          totalContributions,
         }) => {
           const earningsPayoutsPending = payoutsPending - donationsBal;
           const earningsPayoutsMade = totalEarnings - earningsPayoutsPending;
           return (
-            <div className="grid gap-4 @4xl:grid-cols-2">
+            <div className="grid gap-4 @lg:grid-cols-2">
+              {/** Header */}
+              <div className="col-span-2 grid @2xl:grid-cols-3 gap-4">
+                <HeaderPart>
+                  Pending payout:
+                  <span className="flex items-center gap-1">
+                    {payoutsPending}{" "}
+                    <span className="text-2xs sm:text-xs font-normal">USD</span>
+                  </span>
+                </HeaderPart>
+                <HeaderPart>
+                  Total Contributions:
+                  <span className="flex items-center gap-1">
+                    {totalContributions}{" "}
+                    <span className="text-2xs sm:text-xs font-normal">USD</span>
+                  </span>
+                </HeaderPart>
+                <HeaderPart>
+                  Contribution Count:
+                  <span>{contributionsCount}</span>
+                </HeaderPart>
+              </div>
+
+              {/** Balances */}
               <Balance
                 type="Donations"
                 current={donationsBal}
@@ -44,10 +69,6 @@ export default function Dashboard() {
                 pending={earningsPayoutsPending}
                 payedOut={earningsPayoutsMade}
               />
-              <div className="@container col-span-2 flex justify-end gap-5 items-center p-4 rounded border border-prim bg-orange-l6 dark:bg-blue-d6 font-bold text-sm sm:text-lg font-heading">
-                Total Number of Contributions to Your Fund:
-                <span>{contributionsCount}</span>
-              </div>
             </div>
           );
         }}
@@ -58,10 +79,22 @@ export default function Dashboard() {
 
 function LoaderSkeleton() {
   return (
-    <div className="grid gap-4 @4xl:grid-cols-2">
+    <div className="grid gap-4 @lg:grid-cols-2">
+      <div className="col-span-2 grid @2xl:grid-cols-3 gap-4">
+        <ContentLoader className="h-14" />
+        <ContentLoader className="h-14" />
+        <ContentLoader className="h-14" />
+      </div>
       <ContentLoader className="h-60 w-full" />
       <ContentLoader className="h-60 w-full" />
-      <ContentLoader className="h-16 w-full @4xl:col-span-2" />
+    </div>
+  );
+}
+
+function HeaderPart({ children }: PropsWithChildren<{}>) {
+  return (
+    <div className="flex justify-center items-center gap-5 p-4 rounded border border-prim bg-orange-l6 dark:bg-blue-d6 font-bold text-sm @sm:text-base font-heading">
+      {children}
     </div>
   );
 }
