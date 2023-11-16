c5ff41f0c86cb02baf68020239904560c5f5239e
ap-justin <justin@angelprotocol.io>
2023-11-16 13:58:39 +0800

    add environment var

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -12,10 +12,9 @@ export const APP_NAME = "Better Giving";
 export const BASE_URL = "https://better.giving";
 export const DAPP_URL = "https://app.better.giving";
 
-// NETWORK TYPE
-const NETWORK: "TESTNET" | "LOCAL" | undefined /** mainnet if not specified */ =
-  process.env.REACT_APP_NETWORK as any;
-export const IS_TEST = NETWORK === "TESTNET" || NETWORK === "LOCAL";
+const ENV = process.env.REACT_APP_ENVIRONMENT;
+
+export const IS_TEST = ENV === "STAGING";
 export const EXPECTED_NETWORK_TYPE: NetworkType = IS_TEST
   ? "testnet"
   : "mainnet";
