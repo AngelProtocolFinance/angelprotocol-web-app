6fe48ab3944708a5027212284df3fc83ee786b06
ap-justin <justin@angelprotocol.io>
2023-11-16 10:03:21 +0800

    remove mobile

--- src/pages/Applications/index.tsx
+++ src/pages/Applications/index.tsx
@@ -1,12 +1,8 @@
 import { useParams } from "react-router-dom";
-import { DonationMadeByDonor } from "types/aws";
 import { usePaginatedDonationRecords } from "services/apes";
-import CsvExporter from "components/CsvExporter";
 import Icon from "components/Icon";
 import QueryLoader from "components/QueryLoader";
-import { isEmpty } from "helpers";
 import Filter from "./Filter";
-import MobileTable from "./MobileTable";
 import NoDonations from "./NoDonations";
 import Table from "./Table";
 
@@ -29,17 +25,8 @@ export default function Applications() {
   return (
     <div className="grid grid-cols-[1fr_auto] content-start gap-y-4 lg:gap-y-8 lg:gap-x-3 relative padded-container pt-8 lg:pt-20 pb-8">
       <h1 className="text-3xl max-lg:font-work max-lg:text-center max-lg:col-span-full max-lg:mb-4">
-        My donations
+        Applications Review - Dashboard
       </h1>
-      <CsvExporter
-        aria-disabled={isLoadingOrError || !data?.Items || isEmpty(data.Items)}
-        classes="max-lg:row-start-5 max-lg:col-span-full lg:justify-self-end btn-orange px-8 py-3"
-        headers={csvHeaders}
-        data={data?.Items || []}
-        filename="donations.csv"
-      >
-        Export to CSV
-      </CsvExporter>
       <div className="relative flex gap-x-3 items-center border border-prim w-full bg-white dark:bg-blue-d6 rounded">
         <Icon
           type="Search"
@@ -83,25 +70,9 @@ export default function Applications() {
               disabled={isLoadingOrError}
               isLoading={isLoadingNextPage}
             />
-            <MobileTable
-              donations={donations}
-              classes="lg:hidden max-lg:my-4"
-              hasMore={hasMore}
-              onLoadMore={loadNextPage}
-              disabled={isLoadingOrError}
-              isLoading={isLoadingNextPage}
-            />
           </div>
         )}
       </QueryLoader>
     </div>
   );
 }
-
-const csvHeaders: { key: keyof DonationMadeByDonor; label: string }[] = [
-  { key: "amount", label: "Amount" },
-  { key: "usdValue", label: "USD Value" },
-  { key: "symbol", label: "Currency" },
-  { key: "date", label: "Date" },
-  { key: "hash", label: "Transaction Hash" },
-];
