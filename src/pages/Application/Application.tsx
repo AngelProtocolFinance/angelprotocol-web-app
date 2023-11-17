32629674238cc5351457f786d9f46cd454536fe1
ap-justin <justin@angelprotocol.io>
2023-11-17 19:55:35 +0800

    collapsible container

--- src/pages/Application/Application.tsx
+++ src/pages/Application/Application.tsx
@@ -4,14 +4,18 @@ import Container from "./Container";
 const app = applications[0];
 export default function Application() {
   return (
-    <div className="grid grid-cols-[1fr_auto] content-start gap-y-4 lg:gap-y-8 lg:gap-x-3 relative padded-container pt-8 lg:pt-20 pb-8">
+    <div className="grid content-start gap-y-4 lg:gap-y-8 lg:gap-x-3 relative padded-container pt-8 lg:pt-20 pb-8">
       <h1 className="text-center text-3xl max-lg:font-work col-span-full max-lg:mb-4">
         Applications Review - Details
       </h1>
       <h3 className="text-lg">{app.OrganizationName}</h3>
       <p>Application ID: {app.PK}</p>
-      <Container title="Endowment application">content</Container>
-      <Container title="Banking details">content</Container>
+      <Container title="Endowment application">
+        <div>content</div>
+      </Container>
+      <Container title="Banking details">
+        <div>content</div>
+      </Container>
     </div>
   );
 }
--- src/pages/Application/Container.tsx
+++ src/pages/Application/Container.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useState } from "react";
 import Icon from "components/Icon";
 
 type Props = PropsWithChildren<{
@@ -11,36 +11,19 @@ export default function Container({ title, children, classes = "" }: Props) {
 
   return (
     <div
-      className={`flex flex-col gap-px w-full border border-prim rounded dark:bg-blue-d6 ${classes}`}
+      className={`w-full border border-prim rounded dark:bg-blue-d6 divide-y divide-prim ${classes}`}
     >
-      <Header
-        isOpen={isOpen}
-        title={title}
-        onClick={() => setOpen((prev) => !prev)}
-      />
+      <div className="flex items-center gap-x-3 p-3">
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="flex items-center justify-center p-px w-6 h-6 border border-prim rounded"
+          aria-label="toggle section content's visibility"
+        >
+          <Icon type={isOpen ? "Dash" : "Plus"} />
+        </button>
+        <p className="uppercase text-sm font-bold">{title}</p>
+      </div>
       {isOpen && children}
-    </div>
-  );
-}
-
-type HeaderProps = {
-  isOpen: boolean;
-  onClick(): void;
-  classes?: string;
-  title: string;
-  children?: React.ReactNode;
-};
-
-function Header(props: HeaderProps) {
-  return (
-    <div className={props.isOpen ? "border-b" : ""} title={props.title}>
-      <button
-        onClick={props.onClick}
-        className="flex items-center justify-center p-px w-10 h-10 border border-prim rounded"
-        aria-label="toggle section content's visibility"
-      >
-        <Icon type={props.isOpen ? "Dash" : "Plus"} />
-      </button>
     </div>
   );
 }
