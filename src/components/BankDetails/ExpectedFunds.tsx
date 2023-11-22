d80fbecb1873be5513e3b6e46ed7e9df7bc98033
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2023-11-22 11:04:35 +0800

    AP-901 Copy tweaks: Banking Details + Reg step (#2505)
    * clarify banking details monthly est popovercopy;
    
    * update reg non-501c3 & FS org copy to be mor positive;
    
    ---------
    
    Co-authored-by: Andrey <SovereignAndrey>

--- src/components/BankDetails/ExpectedFunds.tsx
+++ src/components/BankDetails/ExpectedFunds.tsx
@@ -2,6 +2,7 @@ import { Popover, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import Icon from "components/Icon";
 import { Label } from "components/form";
+import { APP_NAME } from "constants/env";
 
 type Props = {
   disabled: boolean;
@@ -33,11 +34,9 @@ export default function ExpectedFunds({ disabled, onChange }: Props) {
             >
               {/** using `-translate-x-2/4` instead of `-translate-x-1/2` here as the latter occasionally has no effect for some reason */}
               <Popover.Panel className="absolute -left-6 -translate-x-3/4 lg:-translate-x-2/4 lg:-top-40 mt-1 sm:mt-0 z-10 w-screen max-w-xs sm:max-w-sm p-4 bg-white dark:bg-blue-d3 overflow-hidden text-xs sm:text-sm rounded-lg shadow-lg ring-1 ring-black/5">
-                As the total amount of funds you keep on our platform increases,
-                so will the size of your payouts. Depending on the payout amount
-                and your bank's requirements, we may need to ask you to provide
-                additional information to be able to transfer funds to your bank
-                account.
+                Depending on how much you expect to receive each month via{" "}
+                {APP_NAME}, different details are required. At this point, we
+                recommend using a conservative figure - Maybe $1000 per month.
               </Popover.Panel>
             </Transition>
           </>
