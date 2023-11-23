1389a662be4e17ad55af3369ea87ee3a2485bf36
ap-justin <justin@angelprotocol.io>
2023-11-23 12:39:46 +0800

    use redux instead

--- src/App/Protected.tsx
+++ src/App/Protected.tsx
@@ -1,13 +1,23 @@
 // RequireAuth.js
-import { useAuthenticator } from "@aws-amplify/ui-react";
 import { PropsWithChildren } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import LoaderRing from "components/LoaderRing";
+import { useGetter } from "store/accessors";
 import { appRoutes } from "constants/routes";
 
 export default function Protected({ children }: PropsWithChildren) {
   const location = useLocation();
-  const { route } = useAuthenticator((context) => [context.route]);
-  if (route !== "authenticated") {
+  const user = useGetter((state) => state.auth.user);
+
+  if (user === "loading" || user?.isSigningOut) {
+    return (
+      <div className="grid content-start place-items-center py-8">
+        <LoaderRing thickness={12} classes="w-32 mt-8" />
+      </div>
+    );
+  }
+
+  if (!user) {
     return (
       <Navigate to={appRoutes.signin} state={{ from: location }} replace />
     );
