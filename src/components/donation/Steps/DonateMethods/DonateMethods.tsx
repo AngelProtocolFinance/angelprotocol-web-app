9431b386621d0e3f396124ca9993f93fb134c6fa
Nenad <n.misic@outlook.com>
2023-11-23 10:56:39 +0100

    Remove justify-stretch from tab.list

--- src/components/donation/Steps/DonateMethods/DonateMethods.tsx
+++ src/components/donation/Steps/DonateMethods/DonateMethods.tsx
@@ -25,10 +25,7 @@ export default function DonateMethods({ donaterConfig, state }: Props) {
       className="grid content-start mt-2"
       defaultIndex={state.details?.method === "crypto" ? 1 : 0}
     >
-      <Tab.List
-        as="div"
-        className="grid grid-flow-col sm:grid-cols-4 justify-stretch mb-6"
-      >
+      <Tab.List as="div" className="grid grid-flow-col sm:grid-cols-4 mb-6">
         <Tab className={({ selected }) => tabClasses(selected)}>
           Credit/Debit
         </Tab>
