e448125fa851ecf331f6d8895798376cf6223921
ap-justin <justin@angelprotocol.io>
2023-11-24 23:12:22 +0800

    add header text

--- src/App/Header/UserMenu/Menu.tsx
+++ src/App/Header/UserMenu/Menu.tsx
@@ -15,11 +15,11 @@ export default function Menu({ classes = "", user, signOut }: Props) {
     <Popover.Panel
       className={`${classes} shadow-xl bg-white dark:bg-blue-d6 w-max rounded overflow-hidden`}
     >
-      <p className="text-sm p-3 text-gray-d1 dark:text-gray border-b border-prim">
+      <p className="text-sm p-3 border-b border-prim">
         Welcome, {user.firstName || user.email}!
       </p>
 
-      <div className="w-60 min-h-[5rem] px-3 pb-3 text-gray-d2 dark:text-gray">
+      <div className="w-64 min-h-[5rem] px-3 pb-3">
         <Link
           to={appRoutes.donations}
           className="mt-3 hover:text-orange text-sm flex items-center gap-2"
@@ -27,6 +27,10 @@ export default function Menu({ classes = "", user, signOut }: Props) {
           <Icon type="Money" className="text-lg" />
           <span>My Donations</span>
         </Link>
+        <div className="mt-2 text-sm flex items-center gap-2 text-gray">
+          <Icon type="User" className="text-lg" />
+          <span>My profile (coming soon!)</span>
+        </div>
 
         {/** TODO: temp hidden until user endowments are in jwt claim  */}
         <div className="hidden p-4 border-t border-prim">
@@ -38,15 +42,18 @@ export default function Menu({ classes = "", user, signOut }: Props) {
           </Link>
         </div>
 
-        <div className="empty:hidden p-4 border-t border-prim before-content:[*]">
-          {user.groups.includes(groups["ap-admin"]) && (
-            <Link
-              to={appRoutes.applications}
-              className="text-orange hover:text-orange-l2 text-sm uppercase"
-            >
-              Applications Review
-            </Link>
-          )}
+        <div className="hidden [&:has(div)]:block mt-6">
+          <h5 className="uppercase text-sm text-gray-d1">BG Admin</h5>
+          <div className="empty:hidden p-4">
+            {user.groups.includes(groups["ap-admin"]) && (
+              <Link
+                to={appRoutes.applications}
+                className="text-orange hover:text-orange-l2 text-sm uppercase"
+              >
+                Applications Review
+              </Link>
+            )}
+          </div>
         </div>
       </div>
       <button
