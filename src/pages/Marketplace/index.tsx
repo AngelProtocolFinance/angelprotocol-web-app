2688cd1006574089fc297ebe5e849535ea2f99d9
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-11-28 13:14:50 +0800

    marketplace layout

--- src/pages/Marketplace/index.tsx
+++ src/pages/Marketplace/index.tsx
@@ -1,34 +1,17 @@
-import { useGetter } from "store/accessors";
 import Cards from "./Cards";
-import Filter from "./Filter";
 import Hero from "./Hero";
 import Toolbar from "./Toolbar";
 
 export default function Marketplace() {
-  const isFilterOpen = useGetter(
-    (state) => state.component.marketFilter.isOpen
-  );
-
   return (
     <div className="w-full grid content-start pb-16">
       <div className="relative overlay bg-cover bg-left-top">
         <Hero classes="grid isolate mt-28 mb-16" />
       </div>
 
-      <div className="grid grid-cols-[auto_1fr] gap-x-8 grid-rows-[auto_1fr] padded-container min-h-screen">
-        <Toolbar classes="my-10 col-span-2" />
-        <Filter
-          classes={`${
-            isFilterOpen
-              ? "fixed z-20 inset-0 md:grid md:relative md:z-0"
-              : "hidden"
-          }`}
-        />
-        <Cards
-          classes={`${
-            isFilterOpen ? "col-span-2 md:col-span-1" : "col-span-2"
-          }`}
-        />
+      <div className="grid gap-x-8 grid-rows-[auto_1fr] padded-container min-h-screen">
+        <Toolbar classes="my-10" />
+        <Cards />
       </div>
     </div>
   );
