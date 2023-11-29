8107b94599c4ed8009f41b4cd14e6f1f010a2508
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-11-29 20:10:58 +0800

    specify bare mime type

--- src/pages/Registration/Steps/Documentation/FSA/schema.ts
+++ src/pages/Registration/Steps/Documentation/FSA/schema.ts
@@ -1,13 +1,19 @@
 import { ObjectSchema, object } from "yup";
 import { FormValues } from "./types";
 import { SchemaShape } from "schemas/types";
-import { MIMEType, fileDropzoneAssetShape } from "schemas/file";
+import { MIMEType } from "types/lists";
+import { fileDropzoneAssetShape } from "schemas/file";
 import { requiredString } from "schemas/string";
 import { BYTES_IN_MB } from "constants/common";
 
 export const MB_LIMIT = 6;
 
-export const VALID_MIME_TYPES: MIMEType[] = ["JPEG", "PNG", "PDF", "WEBP"];
+export const VALID_MIME_TYPES: MIMEType[] = [
+  "image/jpeg",
+  "image/png",
+  "application/pdf",
+  "image/webp",
+];
 
 const assetShape = fileDropzoneAssetShape(
   MB_LIMIT * BYTES_IN_MB,
