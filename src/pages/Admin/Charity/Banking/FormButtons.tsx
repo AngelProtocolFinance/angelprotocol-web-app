f7f4660208f22b77b0afdaf2d42d74b82d205b48
Nenad <n.misic@outlook.com>
2023-11-29 11:36:34 +0100

    Use FormButtonsProps in admin panel bank details and handle alreadySubmitted case

--- src/pages/Admin/Charity/Banking/FormButtons.tsx
+++ src/pages/Admin/Charity/Banking/FormButtons.tsx
@@ -1,36 +1,44 @@
+import { FormButtonsProps } from "components/BankDetails/types";
 import { LoadText } from "components/registration";
 
-type Props = {
-  disabled: boolean;
-  isSubmitting: boolean;
-  newRequirementsAdded: boolean;
-  refreshRequired: boolean;
-};
-
 export default function FormButtons({
-  disabled,
-  isSubmitting,
-  newRequirementsAdded,
-  refreshRequired,
-}: Props) {
-  return refreshRequired ? (
-    <div className="grid gap-4 mt-8">
-      <i className="text-xs sm:text-sm">
-        After you fill the form, we may need additional information to be able
-        to submit your bank details for validation. Please fill the form and
-        click the "Check requirements" button below.
+  alreadySubmitted = false,
+  disabled = false,
+  isSubmitting = false,
+  newRequirementsAdded = false,
+  refreshRequired = false,
+}: FormButtonsProps) {
+  if (alreadySubmitted) {
+    return (
+      <i className="mt-8 text-xs sm:text-sm">
+        You have already successfully submitted your bank details. Click "Update
+        Bank Details" button above to submit new bank details for review.
       </i>
-      <button
-        disabled={disabled || isSubmitting}
-        type="submit"
-        className="px-6 btn-orange gap-1 text-sm w-80"
-      >
-        <LoadText isLoading={isSubmitting} text="Checking...">
-          Check Requirements
-        </LoadText>
-      </button>
-    </div>
-  ) : (
+    );
+  }
+
+  if (refreshRequired) {
+    return (
+      <div className="grid gap-4 mt-8">
+        <i className="text-xs sm:text-sm">
+          After you fill the form, we may need additional information to be able
+          to submit your bank details for validation. Please fill the form and
+          click the "Check requirements" button below.
+        </i>
+        <button
+          disabled={disabled || isSubmitting}
+          type="submit"
+          className="px-6 btn-orange gap-1 text-sm w-80"
+        >
+          <LoadText isLoading={isSubmitting} text="Checking...">
+            Check Requirements
+          </LoadText>
+        </button>
+      </div>
+    );
+  }
+
+  return (
     <div className="grid gap-4">
       <i className="text-xs sm:text-sm">
         {newRequirementsAdded
