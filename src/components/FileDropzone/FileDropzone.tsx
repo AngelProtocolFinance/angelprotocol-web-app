5f43700dcc7eab4b1e638a7d6bdb709d6338b339
Nenad <n.misic@outlook.com>
2023-11-30 10:16:08 +0100

    Add red border to FileDropzone on error

--- src/components/FileDropzone/FileDropzone.tsx
+++ src/components/FileDropzone/FileDropzone.tsx
@@ -19,7 +19,7 @@ export default function FileDropzone<
 }) {
   const {
     field: { value, onChange: onFilesChange, ref },
-    formState: { errors, isSubmitting },
+    formState: { errors, isSubmitting, isValid },
   } = useController<Record<string, FileDropzoneAsset>, K>({
     name: props.name,
   });
@@ -42,7 +42,9 @@ export default function FileDropzone<
           className: `relative grid place-items-center rounded border border-dashed w-full h-[11.375rem] focus:outline-none ${
             isDragActive
               ? "border-gray-d1 dark:border-gray"
-              : "border-prim focus:border-orange-l2 focus:dark:border-blue-d1"
+              : `${
+                  isValid ? "border-prim" : "border-red"
+                } focus:border-orange-l2 focus:dark:border-blue-d1`
           } ${
             disabled
               ? "cursor-default bg-gray-l5 dark:bg-bluegray-d1"
