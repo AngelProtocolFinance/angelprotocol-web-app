618a86f46f8707da3c7936e7f42770f297850a29
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-01 15:03:58 +0800

    update styles

--- src/pages/Admin/Charity/Donations/Donations.tsx
+++ src/pages/Admin/Charity/Donations/Donations.tsx
@@ -6,7 +6,7 @@ export default function Donations() {
   return (
     <div>
       <Seo title="Donations" url={adminRoutes.donations} />
-      <h2 className="text-[2rem] font-bold mb-10">Donations</h2>
+      <h2 className="text-[2rem] font-bold mb-4">Donations</h2>
       <DonationsTable />
     </div>
   );
