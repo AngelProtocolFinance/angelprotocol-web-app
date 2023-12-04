96df4b2e3e209d677634631e0c35c89a056e1eef
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-04 08:55:28 +0800

    AP-939: Admin donations changes (#2553)
    * update table style
    
    * update styles
    
    * re-arrange columns
    
    * real splitted value
    
    * icon row height
    
    * complementary split amount

--- src/pages/Admin/Charity/Donations/DonationsTable.tsx
+++ src/pages/Admin/Charity/Donations/DonationsTable.tsx
@@ -1,4 +1,7 @@
+import { DonationReceivedByEndow, KYCData } from "types/aws";
 import { usePaginatedDonationRecords } from "services/apes";
+import CsvExporter from "components/CsvExporter";
+import Icon from "components/Icon";
 import QueryLoader from "components/QueryLoader";
 import { useAdminContext } from "../../Context";
 import Table from "./Table";
@@ -27,15 +30,64 @@ export default function DonationsTable({ classes = "" }) {
         }}
       >
         {(donations) => (
-          <Table
-            donations={donations}
-            hasMore={hasMore}
-            onLoadMore={loadNextPage}
-            disabled={isLoadingOrError}
-            isLoading={isLoadingNextPage}
-          />
+          <>
+            <div className="grid w-full sm:flex items-center sm:justify-end mb-2 gap-2">
+              <CsvExporter
+                classes="border border-blue text-blue-d1 hover:border-blue-l2 hover:text-blue rounded px-4 py-2 text-sm"
+                headers={csvHeadersDonations}
+                data={donations}
+                filename="received_donations.csv"
+              >
+                <Icon type="FileCSV" size={17} className="text-2xl" />
+                Donation Records
+              </CsvExporter>
+              <CsvExporter
+                classes="border border-blue text-blue-d1 hover:border-blue-l2 hover:text-blue rounded px-4 py-2 text-sm"
+                headers={csvHeadersReceipts}
+                data={donations
+                  .filter((x) => !!x.kycData)
+                  .map((x) => x.kycData!)}
+                filename="receipts.csv"
+              >
+                <Icon type="FileCSV" size={17} className="text-2xl" />
+                Donor Information
+              </CsvExporter>
+            </div>
+
+            <div className="overflow-x-auto">
+              <Table
+                donations={donations}
+                hasMore={hasMore}
+                onLoadMore={loadNextPage}
+                disabled={isLoadingOrError}
+                isLoading={isLoadingNextPage}
+              />
+            </div>
+          </>
         )}
       </QueryLoader>
     </div>
   );
 }
+
+const csvHeadersDonations: {
+  key: keyof DonationReceivedByEndow;
+  label: string;
+}[] = [
+  { key: "amount", label: "Amount" },
+  { key: "symbol", label: "Currency" },
+  { key: "date", label: "Date" },
+  { key: "hash", label: "Transaction Hash" },
+];
+
+const csvHeadersReceipts: { key: keyof KYCData; label: string }[] = [
+  { key: "fullName", label: "Full Name" },
+  { key: "email", label: "Email" },
+  { key: "consent_marketing", label: "Consented to Marketing" },
+  { key: "consent_tax", label: "Consented to tax" },
+  { key: "streetAddress", label: "Street Address" },
+  { key: "city", label: "City" },
+  { key: "zipCode", label: "Zip Code" },
+  { key: "state", label: "State" },
+  { key: "country", label: "Country" },
+];
