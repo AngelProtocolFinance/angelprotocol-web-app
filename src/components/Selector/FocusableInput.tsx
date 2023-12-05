50a295ad3be80e3a58f79bb335a6e892eb9dbdc7
Nenad <n.misic@outlook.com>
2023-12-05 11:07:19 +0100

    Remove focus styling from FocusableInput

--- src/components/Selector/FocusableInput.tsx
+++ src/components/Selector/FocusableInput.tsx
@@ -3,12 +3,7 @@ import { forwardRef } from "react";
 const FocusableInput = forwardRef<HTMLInputElement>(
   function Input(_props, ref) {
     return (
-      <input
-        ref={ref}
-        aria-hidden
-        className="h-0 w-0 focus:outline-none absolute"
-        tabIndex={-1}
-      />
+      <input ref={ref} aria-hidden className="h-0 w-0 absolute" tabIndex={-1} />
     );
   }
 );
