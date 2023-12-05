1107663ac4c1d3c127b57b615a9ade4a4cbacb18
Nenad <n.misic@outlook.com>
2023-12-05 09:48:07 +0100

    Add underline char to props in FocusableInput

--- src/components/Selector/FocusableInput.tsx
+++ src/components/Selector/FocusableInput.tsx
@@ -1,14 +1,16 @@
 import { forwardRef } from "react";
 
-const FocusableInput = forwardRef<HTMLInputElement>(function Input(props, ref) {
-  return (
-    <input
-      ref={ref}
-      aria-hidden
-      className="h-0 w-0 focus:outline-none absolute"
-      tabIndex={-1}
-    />
-  );
-});
+const FocusableInput = forwardRef<HTMLInputElement>(
+  function Input(_props, ref) {
+    return (
+      <input
+        ref={ref}
+        aria-hidden
+        className="h-0 w-0 focus:outline-none absolute"
+        tabIndex={-1}
+      />
+    );
+  }
+);
 
 export default FocusableInput;
