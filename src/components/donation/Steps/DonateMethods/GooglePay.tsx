80acbe8859070ee56129750ca73ffcba4adfdb0e
Nenad <n.misic@outlook.com>
2023-12-06 10:47:08 +0100

    Add donation amount field

--- src/components/donation/Steps/DonateMethods/GooglePay.tsx
+++ src/components/donation/Steps/DonateMethods/GooglePay.tsx
@@ -1,24 +1,71 @@
 import GooglePayButton from "@google-pay/button-react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRef, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { ObjectSchema, number, object } from "yup";
+import { SchemaShape } from "schemas/types";
+import { Field } from "components/form";
 import { FormStep } from "slices/donation";
 import { IS_TEST } from "constants/env";
 
 type Props = { state: FormStep };
 
 export default function GooglePay({ state }: Props) {
-  const paymentRequest = createPaymentRequest(state);
+  const [paymentRequest, setPaymentRequest] = useState(
+    createPaymentRequest(state)
+  );
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const methods = useForm<FormValues>({
+    resolver: yupResolver(schema),
+  });
 
   return (
-    <div className="grid place-content-center h-full">
-      <GooglePayButton
-        environment={IS_TEST ? "TEST" : "PRODUCTION"}
-        buttonType="donate"
-        buttonSizeMode="fill"
-        paymentRequest={paymentRequest}
-        style={{ width: 240, height: 40 }}
-      />
-    </div>
+    <FormProvider {...methods}>
+      <form
+        ref={formRef}
+        className="flex flex-col gap-5 items-center justify-center min-h-[16rem]"
+        onSubmit={methods.handleSubmit(({ amount }) =>
+          setPaymentRequest((prev) => {
+            prev.transactionInfo.totalPrice = amount.toString();
+            return prev;
+          })
+        )}
+      >
+        <Field<FormValues>
+          name="amount"
+          label="Donation amount (USD)"
+          classes={{ label: "font-bold", container: "w-96" }}
+        />
+        <GooglePayButton
+          environment={IS_TEST ? "TEST" : "PRODUCTION"}
+          buttonType="donate"
+          buttonSizeMode="fill"
+          paymentRequest={paymentRequest}
+          style={{ width: 240, height: 40 }}
+          onClick={(e) => {
+            if (!methods.formState.isValid) {
+              e.preventDefault();
+            }
+            formRef.current?.requestSubmit();
+          }}
+        />
+      </form>
+    </FormProvider>
   );
 }
+
+type FormValues = { amount: number };
+
+const schema = object<any, SchemaShape<FormValues>>({
+  // for making a number field optional using `nullable + transform`,
+  // see https://github.com/jquense/yup/issues/500#issuecomment-818582829
+  amount: number()
+    .required("required")
+    .positive("must be greater than zero")
+    .typeError("must be a number")
+    .transform((cur, orig) => (orig === "" ? undefined : cur)),
+}) as ObjectSchema<FormValues>;
 
 const createPaymentRequest = (
   state: FormStep
@@ -36,7 +83,6 @@ const createPaymentRequest = (
         type: "PAYMENT_GATEWAY",
         parameters: {
           gateway: "example",
-          gatewayMerchantId: "exampleGatewayMerchantId",
         },
       },
     },
