eb018721d862c195f24eeeda01a3869385850fa8
Nenad <n.misic@outlook.com>
2023-12-11 11:30:32 +0100

    Configure PayPal

--- src/components/donation/Steps/DonateMethods/PayPal.tsx
+++ src/components/donation/Steps/DonateMethods/PayPal.tsx
@@ -1,3 +1,23 @@
+import {
+  PayPalButtons,
+  PayPalScriptProvider,
+  ReactPayPalScriptOptions,
+} from "@paypal/react-paypal-js";
+
+const initialOptions: ReactPayPalScriptOptions = {
+  clientId: "sb",
+  commit: true,
+  currency: "USD",
+  enableFunding: "card,paylater,venmo",
+  dataSdkIntegrationSource: "integrationbuilder_sc",
+};
+
 export default function PayPal() {
-  return <div>PayPal</div>;
+  return (
+    <div className="grid place-items-center min-h-[16rem]">
+      <PayPalScriptProvider options={initialOptions}>
+        <PayPalButtons />
+      </PayPalScriptProvider>
+    </div>
+  );
 }
