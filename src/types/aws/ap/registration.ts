138812ee1c98ec52e234e529df7c662fe30b6aa2
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-13 17:50:56 +0800

    submitted state

--- src/types/aws/ap/registration.ts
+++ src/types/aws/ap/registration.ts
@@ -137,7 +137,7 @@ export type DoneFSAInquiry = Append<DoneOrgDetails, FSAInquiry, {}>;
 export type DoneDocs = Append<DoneFSAInquiry, TDocumentation, {}>;
 export type DoneBanking = Append<DoneDocs, BankingDetails, {}>;
 
-export type SubmissionDetails = { Email: string; EndowmentId: number };
+export type SubmissionDetails = { Email: string; EndowmentId?: number };
 
 export type InReview = Append<DoneBanking, SubmissionDetails, {}>;
 
@@ -243,5 +243,5 @@ export function isDoneBanking(data: SavedRegistration): data is DoneBanking {
 }
 
 export function isSubmitted(data: SavedRegistration): data is InReview {
-  return !!(data.Registration as SubmissionDetails).EndowmentId;
+  return !!(data.Registration as SubmissionDetails).Email;
 }
