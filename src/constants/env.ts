7d23d0b10c82a2ee033ff2f194556b0b1bf97fbc
Andrey <SovereignAndrey>
2023-12-18 13:50:36 +0800

    update favicon image

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -5,7 +5,7 @@ export const IS_MOBILE = isMobile();
 // THE CONSTANTS BELOW ARE ALL CONFIGURED BY ENVIRONMENT VARIABLES
 // AND DISPLAY THE DESIRED TEXT/IMAGES/URLS/ETC THROUGHOUT THE APP
 export const SEO_IMAGE =
-  "https://charity-profile-images.s3.amazonaws.com/logo/angelprotocol-wings-bl.png";
+  "https://charity-profile-images.s3.amazonaws.com/bettergiving-favicon-min.png";
 export const EMAIL_SUPPORT = "support@better.giving";
 export const APP_NAME = "Better Giving";
 export const BASE_URL = "https://better.giving";
