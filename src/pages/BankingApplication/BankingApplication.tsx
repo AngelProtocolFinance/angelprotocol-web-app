869bbf016189d55f33d35c11eb908c7f54ab7262
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-18 13:19:55 +0800

    banking application

--- src/pages/BankingApplication/BankingApplication.tsx
+++ src/pages/BankingApplication/BankingApplication.tsx
@@ -1,31 +1,17 @@
 import { useParams } from "react-router-dom";
 import { useApplicationQuery } from "services/aws/aws";
 import withAuth from "contexts/Auth";
-import Icon from "components/Icon";
 import LoaderRing from "components/LoaderRing";
 import QueryLoader from "components/QueryLoader";
 import { ErrorStatus } from "components/Status";
-import { groups } from "constants/auth";
 import Loaded from "./Loaded";
 
-export default withAuth(function BankingApplication({ user }) {
+function BankingApplication() {
   const { id = "" } = useParams();
 
-  const isAuthorized =
-    user.groups.includes(groups["ap-admin"]) || user.endowments.includes(+id);
-
   const queryState = useApplicationQuery(id, {
-    skip: !id || !isAuthorized,
+    skip: !id,
   });
-
-  if (!isAuthorized) {
-    return (
-      <div className="grid content-start place-items-center py-20">
-        <Icon type="ExclamationCircleFill" size={80} className="text-red" />
-        <p className="text-xl mt-8 font-work ">Unauthorized</p>
-      </div>
-    );
-  }
 
   return (
     <div className="grid content-start gap-y-4 lg:gap-y-8 lg:gap-x-3 relative padded-container pt-8 lg:pt-20 pb-8">
@@ -46,4 +32,6 @@ export default withAuth(function BankingApplication({ user }) {
       </QueryLoader>
     </div>
   );
-});
+}
+
+export default withAuth(BankingApplication);
