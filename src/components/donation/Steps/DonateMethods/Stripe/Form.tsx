353548a4f0d126af14651cea1f9fdd11612033b3
Nenad <n.misic@outlook.com>
2023-12-21 08:50:15 +0100

    Upper case currency code

--- src/components/donation/Steps/DonateMethods/Stripe/Form.tsx
+++ src/components/donation/Steps/DonateMethods/Stripe/Form.tsx
@@ -1,16 +1,22 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { ObjectSchema, number, object } from "yup";
 import { FormValues, Props } from "./types";
 import { SchemaShape } from "schemas/types";
+import { useStripeCurrenciesQuery } from "services/apes";
+import { useErrorContext } from "contexts/ErrorContext";
 import { Currency } from "components/CurrencySelector";
 import ExtLink from "components/ExtLink";
 import LoadText from "components/LoadText";
+import LoaderRing from "components/LoaderRing";
+import QueryLoader from "components/QueryLoader";
 import Split from "components/Split";
+import { ErrorStatus } from "components/Status";
 import { CheckField, Field } from "components/form";
 import { requiredString } from "schemas/string";
+import { GENERIC_ERROR_MESSAGE } from "constants/common";
 import { appRoutes } from "constants/routes";
 import { TERMS_OF_USE_DONOR } from "constants/urls";
 import AdvancedOptions from "../../../AdvancedOptions";
@@ -21,19 +27,60 @@ type FormProps = Props & {
   onSubmit: (formValues: FormValues) => void | Promise<void>;
 };
 
-export default function Form({
+export default function Form(props: FormProps) {
+  const [countryCode, setCountryCode] = useState<string>();
+
+  const queryState = useStripeCurrenciesQuery(
+    { country_code: countryCode || "" },
+    { skip: !countryCode }
+  );
+  const { handleError } = useErrorContext();
+
+  useEffect(() => {
+    fetch("https://ipapi.co/json/")
+      .then<{ country_code: string }>((response) => response.json())
+      .then((data) => setCountryCode(data.country_code))
+      .catch((err) => handleError(err, GENERIC_ERROR_MESSAGE));
+  });
+
+  return (
+    <QueryLoader
+      queryState={queryState}
+      classes={{ container: "grid h-full w-full" }}
+      messages={{
+        loading: (
+          <LoaderRing thickness={10} classes="w-32 justify-self-center" />
+        ),
+        error: (
+          <ErrorStatus classes="justify-self-center">
+            Failed to load banking application
+          </ErrorStatus>
+        ),
+      }}
+    >
+      {(data) => (
+        <Content {...props} currencies={data.supported_payment_currencies} />
+      )}
+    </QueryLoader>
+  );
+}
+
+function Content({
   advanceOptDisplay,
+  currencies,
   defaultValues,
   state: { recipient },
   widgetConfig,
   onSubmit,
-}: FormProps) {
-  const [currencies, setCurrencies] = useState<Currency[]>([]);
+}: FormProps & { currencies: string[] }) {
   const methods = useForm<FormValues>({
     resolver: yupResolver(schema),
     defaultValues: {
       amount: defaultValues.amount,
-      currency: { code: "", name: "" },
+      currency: {
+        code: currencies[0].toUpperCase(),
+        name: currencies[0].toUpperCase(),
+      },
       pctLiquidSplit: defaultValues.pctLiquidSplit ?? 50,
       userOptForKYC: recipient.isKYCRequired || defaultValues.userOptForKYC, // if KYC required, user opts in by default
     },
