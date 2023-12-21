769b898b4949ba46a021ec594c71b8d2d6b9a609
Nenad <n.misic@outlook.com>
2023-12-21 08:51:49 +0100

    Display only currency code if no name is present

--- src/components/CurrencySelector.tsx
+++ src/components/CurrencySelector.tsx
@@ -60,7 +60,9 @@ function CurrencySelector(props: Props) {
           ref={inputRef}
           className="w-full border-r border-gray-l3 dark:border-bluegray px-4 py-3.5 text-sm leading-5 text-gray-900 focus:ring-0"
           displayValue={(currency: WiseCurrency) =>
-            `${currency.code} - ${currency.name}`
+            !!currency.name
+              ? `${currency.code} - ${currency.name}`
+              : currency.code
           }
           onChange={(event) => setQuery(event.target.value)}
           spellCheck={false}
