f79873c70b0a97543013875747355ea52325a246
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-21 23:28:39 +0800

    favicon

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,5 +3,10 @@ import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
+  server: { port: 4200 },
+  html: {
+    appIcon: "./public/favicon.png",
+    favicon: "./public/favicon.png",
+  },
   plugins: [pluginReact(), pluginNodePolyfill()],
 });
