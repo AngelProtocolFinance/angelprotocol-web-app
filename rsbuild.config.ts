7aec1f1c3e780aa940f76563f50dab1f71962a14
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-21 23:34:07 +0800

    favicon title

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 import { pluginReact } from "@rsbuild/plugin-react";
+import tailwind from "tailwindcss";
 
 export default defineConfig({
   server: { port: 4200 },
   html: {
     appIcon: "./public/favicon.png",
     favicon: "./public/favicon.png",
+    title: "Support an impact organization",
   },
   plugins: [pluginReact(), pluginNodePolyfill()],
+  tools: {
+    postcss: {
+      postcssOptions: {
+        plugins: [tailwind()],
+      },
+    },
+  },
 });
