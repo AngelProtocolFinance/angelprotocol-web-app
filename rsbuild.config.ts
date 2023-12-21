e6287c499593818d0f090c33cc66d252f010f55a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-21 23:20:09 +0800

    add polyfill

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@rsbuild/core";
+import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginNodePolyfill()],
 });
