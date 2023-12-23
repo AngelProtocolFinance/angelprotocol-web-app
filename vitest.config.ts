ffb61a7b23d3d2b86e4e78cfc544e1f891b3a9cd
ap-justin <89639563+ap-justin@users.noreply.github.com>
2023-12-23 17:56:27 +0800

    from vitest only

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 /// <reference types="vitest" />
 export default defineConfig({
