577fd7a178e7edc9c899dec80ffe1f269af2697d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-02 23:21:49 +0800

    types

--- src/pages/Admin/Charity/Banking/V2Form/Requirements/Requirements.tsx
+++ src/pages/Admin/Charity/Banking/V2Form/Requirements/Requirements.tsx
@@ -1,3 +1,4 @@
+import { Tab } from "@headlessui/react";
 import { useRequirementsQuery } from "services/aws/wise";
 import useDebouncer from "hooks/useDebouncer";
 
@@ -18,7 +19,25 @@ export default function Requirements({ currency, amount }: Props) {
     { skip: !amnt || isDebouncing }
   );
 
-  console.log({ debouncedAmount, amnt, requirements, requestId, isDebouncing });
+  if (requirements.length <= 0)
+    return <div>payout to ${currency} is not available</div>;
 
-  return <div>requirements</div>;
+  if (requirements.length === 1) {
+    return <div>show single requirement</div>;
+  }
+
+  return (
+    <Tab.Group>
+      <Tab.List className="flex gap-2">
+        {requirements.map((r) => (
+          <Tab key={r.type}>{r.title}</Tab>
+        ))}
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>Content 1</Tab.Panel>
+        <Tab.Panel>Content 2</Tab.Panel>
+        <Tab.Panel>Content 3</Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
+  );
 }
