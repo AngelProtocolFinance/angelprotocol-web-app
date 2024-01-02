be9d820b12d63b8d82a650c4fa36afe758ad5a37
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-02 18:06:38 +0800

    wise currencies

--- src/pages/Admin/Charity/Banking/V2Form/V2Form.tsx
+++ src/pages/Admin/Charity/Banking/V2Form/V2Form.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import { WiseCurrency } from "types/aws";
 import Requirements from "./Requirements";
+import WiseCurrencies from "./Requirements/WiseCurrencies";
 
 export default function V2Form() {
+  const [currency, setCurrency] = useState<Pick<WiseCurrency, "code" | "name">>(
+    { code: "USD", name: "United States Dollar" }
+  );
+
   return (
     <div className="grid w-full gap-6 p-6 border border-prim rounded bg-white dark:bg-blue-d6 max-w-4xl">
       <h3 className="text-2xl font-body">Bank account details</h3>
@@ -8,6 +15,12 @@ export default function V2Form() {
         The following information will be used to register your bank account
         that will be used to withdraw your funds.
       </p>
+      <WiseCurrencies
+        onChange={(c) => setCurrency(c)}
+        value={currency}
+        disabled={false}
+        classes={{ combobox: "w-full md:w-80" }}
+      />
       <Requirements />
     </div>
   );
--- src/services/aws/wise.ts
+++ src/services/aws/wise.ts
@@ -2,6 +2,7 @@ import {
   CreateRecipientRequest,
   V1RecipientAccount,
   V2RecipientAccount,
+  WiseCurrency,
 } from "types/aws";
 import { aws } from "../aws/aws";
 import { version as v } from "../helpers";
@@ -24,7 +25,14 @@ export const wise = aws.injectEndpoints({
     recipient: builder.query<V2RecipientAccount, string>({
       query: (id: string) => `/${v(1)}/wise-proxy/v2/accounts/${id}`,
     }),
+    currencis: builder.query<WiseCurrency[], unknown>({
+      query: () => `/${v(1)}/wise-proxy/v1/currencies`,
+    }),
   }),
 });
 
-export const { useCreateRecipientMutation, useRecipientQuery } = wise;
+export const {
+  useCreateRecipientMutation,
+  useRecipientQuery,
+  useCurrencisQuery,
+} = wise;
