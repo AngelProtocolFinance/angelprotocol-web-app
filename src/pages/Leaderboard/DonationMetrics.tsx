e3c4ffd92e08c8f39a19455e43de9903b431b0b3
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-04 11:08:18 +0800

    BG-1030: Leaderboard crash (#2624)
    * update endpoint
    
    * fix default image bug
    
    * use humanize

--- src/pages/Leaderboard/DonationMetrics.tsx
+++ src/pages/Leaderboard/DonationMetrics.tsx
@@ -2,21 +2,15 @@ import {
   dummyDonationsMetricList as metricsPlaceholder,
   useMetricsListQuery,
 } from "services/aws/business_metrics";
+import { humanize } from "helpers";
 
 export default function DonationMetrics() {
-  // The parameter passed to the function is a list of attrs that we want to display
-  // It is a single string with the attrs being comma-separated because it will go to the request's url
-  const { data: metrics = metricsPlaceholder } = useMetricsListQuery(
-    "donations_total_amount_v2,donations_daily_count,donations_daily_amount"
-  );
+  const { data: metrics = metricsPlaceholder } = useMetricsListQuery({});
 
   return (
     <div className="text-center">
       <h2 className="my-3 text-3xl font-extrabold">
-        Total Donations: ${" "}
-        {`${Number(
-          metrics.donations_total_amount.toFixed(2)
-        ).toLocaleString()}`}
+        Total Donations: $ {humanize(metrics.donations_total_amount_v2, 2)}
       </h2>
       {/*<h3 className="text-xl font-semibold">
         Daily Donations Total: ${" "}
