e6f8a08e2f9078eb20273e0291ed4aecf75f6ad9
Nenad Misic <nenad@angelprotocol.io>
2024-01-05 04:39:30 +0100

    BG-1005: Fix /auth-redirector URL appearing after redirecting (#2629)
    * Reinstall yarn pgks
    
    * Add setTimeout before redirecting in OAUTHRedirector
    
    * Revert yarn.lock

--- src/pages/OAuthRedirector.tsx
+++ src/pages/OAuthRedirector.tsx
@@ -1,6 +1,25 @@
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { OAUTH_PATH_STORAGE_KEY } from "constants/auth";
 
+// For some reason aws-amplify redirects to this page twice; the slowness of the 2nd
+// redirect causes the final URL to be of this page even though the rendered page is
+// completely different (Marketplace, Register etc.).
+// To account for this slowness, we set a timeout and navigate to the desired page afterwards.
+const DELAY = 300;
+
 export default function OAUTHRedirector() {
-  return <Navigate to={localStorage.getItem(OAUTH_PATH_STORAGE_KEY) ?? "/"} />;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(
+      () => navigate(localStorage.getItem(OAUTH_PATH_STORAGE_KEY) ?? "/"),
+      DELAY
+    );
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [navigate]);
+
+  return <h3 className="text-3xl place-self-center p-5">Redirecting...</h3>;
 }
