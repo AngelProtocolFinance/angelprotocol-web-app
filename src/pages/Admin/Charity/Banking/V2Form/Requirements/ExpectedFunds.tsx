7224aeb34ef03933ff4cf091dfd6a5e9a6d1922a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-07 17:37:50 +0800

    remove popover

--- src/pages/Admin/Charity/Banking/V2Form/Requirements/ExpectedFunds.tsx
+++ src/pages/Admin/Charity/Banking/V2Form/Requirements/ExpectedFunds.tsx
@@ -14,36 +14,10 @@ type Props = {
 export default function ExpectedFunds(props: Props) {
   return (
     <div className="field">
-      <div className="flex sm:gap-2 items-center mb-1">
-        <Label htmlFor="wise__amount">
-          What is the amount of donations (in USD) you expect to receive monthly
-          on our platform?
-        </Label>
-        <Popover className="relative">
-          <>
-            <Popover.Button className="group flex items-center rounded-full text-base font-medium hover:text-orange focus:outline-none">
-              <Icon type="Info" className="text-2xl" />
-            </Popover.Button>
-            {/** Transition is configured so that the popover appears from the top on smaller screens and from the bottom on larger screens*/}
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-0 lg:translate-y-1"
-              enterTo="opacity-100 translate-y-1 lg:translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-1 lg:translate-y-0"
-              leaveTo="opacity-0 translate-y-0 lg:translate-y-1"
-            >
-              {/** using `-translate-x-2/4` instead of `-translate-x-1/2` here as the latter occasionally has no effect for some reason */}
-              <Popover.Panel className="absolute -left-6 -translate-x-3/4 lg:-translate-x-2/4 lg:-top-40 mt-1 sm:mt-0 z-10 w-screen max-w-xs sm:max-w-sm p-4 bg-white dark:bg-blue-d3 overflow-hidden text-xs sm:text-sm rounded-lg shadow-lg ring-1 ring-black/5">
-                Depending on how much you expect to receive each month via{" "}
-                {APP_NAME}, different details are required. At this point, we
-                recommend using a conservative figure - Maybe $1000 per month.
-              </Popover.Panel>
-            </Transition>
-          </>
-        </Popover>
-      </div>
+      <Label htmlFor="wise__amount">
+        What is the amount of donations (in USD) you expect to receive monthly
+        on our platform?
+      </Label>
       <input
         id="wise__amount"
         type="text"
@@ -58,6 +32,11 @@ export default function ExpectedFunds(props: Props) {
         disabled={props.disabled}
         inputMode="numeric"
       />
+      <p className="text-gray-d1 text-sm my-2 italic">
+        Depending on how much you expect to receive each month via {APP_NAME},
+        different details are required. At this point, we recommend using a
+        conservative figure - Maybe $1000 per month.
+      </p>
     </div>
   );
 }
