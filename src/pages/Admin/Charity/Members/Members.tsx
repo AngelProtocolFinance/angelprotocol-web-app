976e90050f3f00e9203488c47f3c24d87fb3d323
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-10 11:26:43 +0800

    title

--- src/pages/Admin/Charity/Members/Members.tsx
+++ src/pages/Admin/Charity/Members/Members.tsx
@@ -1,3 +1,9 @@
 export default function Members() {
-  return <div>Members</div>;
+  return (
+    <div className="grid content-start gap-y-6 @lg:gap-y-8 @container">
+      <h3 className="text-[2rem]">Manage Members</h3>
+      {/* <Members /> */}
+      {/* <Settings /> */}
+    </div>
+  );
 }
