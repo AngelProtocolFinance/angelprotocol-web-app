37d29895818b1c12e485779579a06bf0bf7f262e
jp-mariano <juanpaolo.mariano28@gmail.com>
2024-01-16 13:02:18 +0800

    Fix inverted placement of chain IDs

--- src/constants/chainIds.ts
+++ src/constants/chainIds.ts
@@ -3,7 +3,7 @@ import { IS_TEST } from "./env";
 
 export const chainIds: { [key in Chains]: string } = IS_TEST
   ? {
-      arbitrum: "42161",
+      arbitrum: "421614",
       binance: "97",
       ethereum: "5",
       juno: "uni-6",
@@ -11,7 +11,7 @@ export const chainIds: { [key in Chains]: string } = IS_TEST
       terra: "pisco-1",
     }
   : {
-      arbitrum: "421614",
+      arbitrum: "42161",
       binance: "56",
       ethereum: "1",
       juno: "juno-1",
