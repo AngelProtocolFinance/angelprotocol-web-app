a033809ec89c44a8d4290ccda3bcb2a8b8af0686
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-19 23:03:26 +0800

    form with fieldset

---
+++ src/components/BankDetails/RecipientDetails/RecipientDetailsForm/Form.tsx
@@ -0,0 +1,15 @@
+import { FormHTMLAttributes } from "react";
+
+export default function Form({
+  isSubmitting,
+  children,
+  ...props
+}: FormHTMLAttributes<HTMLFormElement> & { isSubmitting: boolean }) {
+  return (
+    <form {...props}>
+      <fieldset className="contents" disabled={isSubmitting}>
+        {children}
+      </fieldset>
+    </form>
+  );
+}
