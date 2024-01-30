62ce59b8e2ccc6ed6e22c79ae8ffe16df8106df4
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-01-30 12:07:18 +0800

    mobile layout

--- src/pages/Donate/Content.tsx
+++ src/pages/Donate/Content.tsx
@@ -1,8 +1,9 @@
 import ExtLink from "components/ExtLink";
 import { Steps } from "components/donation";
+import { APP_NAME } from "constants/env";
 import { appRoutes } from "constants/routes";
 import { TERMS_OF_USE_DONOR } from "constants/urls";
-import { memo, useEffect, useRef } from "react";
+import { PropsWithChildren, memo, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { DonationRecipient, setRecipient } from "slices/donation";
 import { useGetter, useSetter } from "store/accessors";
@@ -50,22 +51,42 @@ function Content(props: Props) {
         name={props.name}
         tagline={props.tagline}
         logo={props.logo}
-        classes="md:col-start-1 md:row-start-2 w-64 xl:w-80 md:h-full lg:h-auto"
+        classes="md:col-start-1 md:row-start-2 md:w-64 xl:w-80 md:h-full lg:h-auto"
       />
       <Steps
-        className="md:row-span-2 md:row-start-2 md:col-start-2"
+        className="col-start-1 md:col-start-2 md:row-start-2 md:row-span-2 lg:row-span-1"
         donaterConfig={null}
       />
-      <FAQ classes="md:row-start-3 md:col-start-1 w-64 xl:w-80 lg:row-start-2 lg:col-start-3" />
+      <FAQ classes="col-start-1 md:row-start-3 md:col-start-1 md:w-64 xl:w-80 lg:row-start-2 lg:col-start-3 row-span-4" />
 
-      <p className="text-sm text-gray-d1 dark:text-gray mt-2 text-center col-start-2 row-start:3 lg:col-span-full">
-        By making a donation, you agree to our{" "}
+      <p className="text-sm text-left text-gray-d1 dark:text-gray col-start-1 md:col-start-2 md:row-start-4 lg:row-start-3">
+        By making a donation to {APP_NAME}, you agree to our{" "}
+        <B>Terms of Service</B>, <B>Privacy Policy</B>, and{" "}
+        <B>Nonprofit Support Fee</B>. 100% of your donation is tax-deductible to
+        the extent allowed by US law. Your donation is made to {APP_NAME}, a
+        tax-exempt US 501(c)(3) charity that grants unrestricted funds to{" "}
+        {props.name} on your behalf. As a legal matter, {APP_NAME} must provide
+        any donations to {props.name} on an unrestricted basis, regardless of
+        any designations or restrictions made by you.{" "}
         <ExtLink
-          className="hover:underline text-gray-d2 "
+          className="hover:underline font-medium"
           href={TERMS_OF_USE_DONOR}
         >
-          Terms & Conditions
+          See Terms.
         </ExtLink>
+      </p>
+      <p className="text-sm text-left text-gray-d1 dark:text-gray col-start-1  md:col-start-2 md:row-start-5  lg:row-start-4">
+        <span className="block mb-1">
+          Need help? See FAQs or contact us at our <B>Help Center</B>.
+        </span>
+        <span className="block mb-1">
+          Have ideas for how we can build a better donation experience?{" "}
+          <B>Send us feedback</B>.
+        </span>
+        <span className="block">
+          We respect your privacy. To learn more, check out our{" "}
+          <B>Privacy Policy</B>.
+        </span>
       </p>
     </div>
   );
@@ -73,3 +94,7 @@ function Content(props: Props) {
 
 //memoize to prevent useEffect ( based on props ) from running when parent re-renders with the same props
 export default memo(Content);
+
+function B(props: PropsWithChildren) {
+  return <span className="font-medium">{props.children}</span>;
+}
