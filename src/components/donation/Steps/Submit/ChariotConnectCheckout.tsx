d14922e1f4ac7dedf2e56acaae61e390761bf6c3
Nenad <n.misic@outlook.com>
2024-01-31 11:37:28 +0100

    Fix workflowSessionId fetching

--- src/components/donation/Steps/Submit/ChariotConnectCheckout.tsx
+++ src/components/donation/Steps/Submit/ChariotConnectCheckout.tsx
@@ -1,12 +1,11 @@
-import { GENERIC_ERROR_MESSAGE } from "constants/common";
 import { CHARIOT_CONNECT_ID } from "constants/env";
-import { useErrorContext } from "contexts/ErrorContext";
 import ChariotConnect from "react-chariot-connect";
 import { useChariotGrantIntentMutation } from "services/apes";
 import { ChariotCheckoutStep, setStep } from "slices/donation";
 import { useSetter } from "store/accessors";
 import BackBtn from "../BackBtn";
 import Err from "./Err";
+import Loader from "./Loader";
 
 // Followed Stripe's custom flow docs
 // https://stripe.com/docs/payments/quickstart
@@ -15,7 +14,6 @@ export default function ChariotConnectCheckout(props: ChariotCheckoutStep) {
   const [createGrant, { isLoading, isError }] = useChariotGrantIntentMutation();
 
   const dispatch = useSetter();
-  const { handleError } = useErrorContext();
 
   /** @see {@link https://givechariot.readme.io/reference/integrating-connect#prepopulate-data-into-your-connect-session} */
   const onDonationRequest = () => {
@@ -39,7 +37,9 @@ export default function ChariotConnectCheckout(props: ChariotCheckoutStep) {
         }}
       />
 
-      {isError ? (
+      {isLoading ? (
+        <Loader msg="Processing donation..." />
+      ) : isError ? (
         <Err />
       ) : (
         <ChariotConnect
@@ -48,30 +48,26 @@ export default function ChariotConnectCheckout(props: ChariotCheckoutStep) {
           onDonationRequest={onDonationRequest}
           // This hook should be used to update our internal donation DB
           // see https://givechariot.readme.io/reference/integrating-connect#capture-your-grant-intent
-          onSuccess={async (r: { workflowSessionId: string }) => {
-            try {
-              await createGrant({
-                amount: +details.amount,
-                currency: details.currency.code,
-                endowmentId: recipient.id,
-                email: details.email,
-                splitLiq: details.pctLiquidSplit.toString(),
-                kycData: kyc
-                  ? {
-                      city: kyc.city,
-                      country: kyc.country.name,
-                      fullName: `${kyc.name.first} ${kyc.name.last}`,
-                      kycEmail: kyc.kycEmail,
-                      streetAddress: `${kyc.address.street} ${kyc.address.complement}`,
-                      state: kyc.usState.value || kyc.state,
-                      zipCode: kyc.postalCode,
-                    }
-                  : undefined,
-                transactionId: r.workflowSessionId,
-              });
-            } catch (error) {
-              handleError(error, GENERIC_ERROR_MESSAGE);
-            }
+          onSuccess={async (r: { detail: { workflowSessionId: string } }) => {
+            await createGrant({
+              amount: +details.amount,
+              currency: details.currency.code,
+              endowmentId: recipient.id,
+              email: details.email,
+              splitLiq: details.pctLiquidSplit.toString(),
+              kycData: kyc
+                ? {
+                    city: kyc.city,
+                    country: kyc.country.name,
+                    fullName: `${kyc.name.first} ${kyc.name.last}`,
+                    kycEmail: kyc.kycEmail,
+                    streetAddress: `${kyc.address.street} ${kyc.address.complement}`,
+                    state: kyc.usState.value || kyc.state,
+                    zipCode: kyc.postalCode,
+                  }
+                : undefined,
+              transactionId: r.detail.workflowSessionId,
+            });
           }}
         />
       )}
