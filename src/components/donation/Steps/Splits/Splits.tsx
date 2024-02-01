e290fe2293f0285e51733b1f20f0086ba88b5994
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-02-01 14:57:03 +0800

    splits back button

--- src/components/donation/Steps/Splits/Splits.tsx
+++ src/components/donation/Steps/Splits/Splits.tsx
@@ -1,16 +1,25 @@
-import { SplitsStep, setSplit } from "slices/donation";
+import { SplitsStep, setSplit, setStep } from "slices/donation";
 import { useSetter } from "store/accessors";
+import BackBtn from "../BackBtn";
 
 export default function Split(props: SplitsStep) {
   const dispatch = useSetter();
   return (
-    <button
-      type="button"
-      onClick={() => {
-        dispatch(setSplit(props.liquidSplitPct || 50));
-      }}
-    >
-      split
-    </button>
+    <div className="grid content-start p-4 @md:p-8">
+      <BackBtn
+        type="button"
+        onClick={() =>
+          dispatch(setStep(props.kyc ? "kyc-form" : "donate-form"))
+        }
+      />
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(setSplit(props.liquidSplitPct || 50));
+        }}
+      >
+        split
+      </button>
+    </div>
   );
 }
