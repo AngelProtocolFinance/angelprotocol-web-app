5f58aa5b9ac235464b257d0ae84a12dd4057e15e
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-02-01 15:04:35 +0800

    splits descp

--- src/components/donation/Steps/Splits/Splits.tsx
+++ src/components/donation/Steps/Splits/Splits.tsx
@@ -12,6 +12,10 @@ export default function Split(props: SplitsStep) {
           dispatch(setStep(props.kyc ? "kyc-form" : "donate-form"))
         }
       />
+      <h4 className="mt-4">Split donation</h4>
+      <p className="text-gray-d1">
+        Create a sustainable impact by dividing funds
+      </p>
       <button
         type="button"
         onClick={() => {
@@ -20,6 +24,12 @@ export default function Split(props: SplitsStep) {
       >
         split
       </button>
+      <p className="text-sm text-gray-d1 mt-6">
+        With splitting your donation into sustainable funds, you align
+        investments with personal values, improving long-term financial
+        performance, reducing risk exposure, and contributing to global
+        sustainability goals.
+      </p>
     </div>
   );
 }
