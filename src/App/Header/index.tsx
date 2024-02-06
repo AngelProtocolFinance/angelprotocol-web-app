4aea4b3f54c8d58ee28d1aa4411a7a19b5329b48
Nenad <nenad@better.giving>
2024-02-06 17:49:14 +0100

    Limit width to 6xl

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -34,7 +34,7 @@ export default function Header({ classes, links }: Props) {
         isScrolled ? "shadow-lg bg-white dark:bg-blue-d3" : ""
       } transition-shadow ease-in-out duration-300 w-full h-20 mb-0`}
     >
-      <div className="grid items-center gap-4 px-5 grid-cols-[auto_1fr_auto] h-full bg-white">
+      <div className="grid items-center gap-4 px-5 grid-cols-[auto_1fr_auto] w-full max-w-6xl mx-auto h-full bg-white">
         <DappLogo />
         <DesktopNav
           classes="hidden lg:flex font-heading font-bold uppercase"
