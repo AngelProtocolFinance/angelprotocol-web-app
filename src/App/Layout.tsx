45d26786c11aff3f8bad28a923e20f6536445da8
Nenad <nenad@better.giving>
2024-02-07 10:06:27 +0100

    Remove gray background from Layout.tsx

--- src/App/Layout.tsx
+++ src/App/Layout.tsx
@@ -12,7 +12,7 @@ const { GROUPS_DATA, SOCIAL_MEDIA_LINKS } = CHARITY_LINKS;
 export default function Layout() {
   const headerLinks = useHeaderLinks();
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] bg-gray">
+    <div className="grid grid-rows-[auto_1fr_auto]">
       <Seo /> {/* Load all defaults for SEO meta tags */}
       <Header classes="top-0 sticky z-20" links={headerLinks} />
       <Suspense fallback={<LoaderComponent />}>
