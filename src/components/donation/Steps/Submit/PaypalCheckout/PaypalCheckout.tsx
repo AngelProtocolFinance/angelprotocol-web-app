5d79e085317ef9ccf0dfc27f635d46c69039d0e1
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-02-07 19:25:37 +0800

    paypal checkout

--- src/components/donation/Steps/Submit/PaypalCheckout/PaypalCheckout.tsx
+++ src/components/donation/Steps/Submit/PaypalCheckout/PaypalCheckout.tsx
@@ -1,11 +1,14 @@
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import ContentLoader from "components/ContentLoader";
 import { PAYPAL_CLIENT_ID } from "constants/env";
 import { usePaypalOrderQuery } from "services/apes";
 import { PaypalCheckoutStep, setStep } from "slices/donation";
 import { useSetter } from "store/accessors";
 import BackBtn from "../../BackBtn";
 import Err from "../Err";
-import Loader from "../Loader";
+import Currency from "../common/Currrency";
+import Heading from "../common/Heading";
+import SplitSummary from "../common/SplitSummary";
 import Checkout from "./Checkout";
 
 // Followed Stripe's custom flow docs
@@ -39,12 +42,26 @@ export default function PaypalCheckout(props: PaypalCheckoutStep) {
 
   const dispatch = useSetter();
 
+  const currency = details.currency;
+  const total = +details.amount;
+  const liq = total * (liquidSplitPct / 100);
+  const locked = total - liq;
+
   return (
-    <div className="grid grid-rows-[auto_1fr] min-h-[16rem] isolate p-4 @md:p-8">
+    <div className="flex flex-col min-h-[16rem] isolate p-4 @md:p-8">
       <BackBtn onClick={() => dispatch(setStep("splits"))} type="button" />
 
+      <Heading classes="my-4" />
+
+      <SplitSummary
+        classes="mb-auto"
+        total={<Currency {...currency} amount={total} classes="text-gray-d2" />}
+        liquid={<Currency {...currency} amount={liq} classes="text-sm" />}
+        locked={<Currency {...currency} amount={locked} classes="text-sm" />}
+      />
+
       {isLoading ? (
-        <Loader msg="Loading payment form..." />
+        <ContentLoader className="rounded h-14 w-full mt-auto" />
       ) : isError || !orderId ? (
         <Err error={error} />
       ) : (
