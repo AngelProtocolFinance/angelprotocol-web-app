2632d3dff2f9cdffdb8ebc934492677c3c40bea9
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-02-09 14:54:09 +0800

    done date

--- src/components/form/DateInput.tsx
+++ src/components/form/DateInput.tsx
@@ -1,5 +1,10 @@
-import { ErrorMessage } from "@hookform/error-message";
-import { FieldValues, Path, useFormContext } from "react-hook-form";
+import {
+  FieldValues,
+  Path,
+  UseFormRegisterReturn,
+  get,
+  useFormContext,
+} from "react-hook-form";
 
 export default function DateInput<T extends FieldValues>({
   name,
@@ -10,19 +15,26 @@ export default function DateInput<T extends FieldValues>({
     register,
     formState: { errors },
   } = useFormContext<T>();
+  return <NativeDateInput {...register(name)} error={get(errors, name)} />;
+}
+
+export function NativeDateInput({
+  error,
+  ...props
+}: UseFormRegisterReturn & { error?: string }) {
   return (
     <div>
       <input
-        {...register(name)}
+        {...props}
         type="date"
         className="date-input uppercase text-sm relative w-full px-4 py-3 border border-prim rounded-md border-collapse dark:bg-blue-d6"
       />
-      <ErrorMessage
-        errors={errors}
-        as="span"
-        name={name as any}
-        className="w-full text-xs text-red-l4 dark:text-red-l2"
-      />
+
+      {error && (
+        <span className="w-full text-xs text-red-l4 dark:text-red-l2">
+          {error}
+        </span>
+      )}
     </div>
   );
 }
