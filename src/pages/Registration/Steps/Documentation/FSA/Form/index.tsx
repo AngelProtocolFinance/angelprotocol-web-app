6afd933f410e12d897f784fc1ffb840f09030c4b
Nenad <nenad@better.giving>
2024-02-12 12:22:49 +0100

    FSA form's back btn should be disabled while redirecting

--- src/pages/Registration/Steps/Documentation/FSA/Form/index.tsx
+++ src/pages/Registration/Steps/Documentation/FSA/Form/index.tsx
@@ -71,7 +71,7 @@ export default function Form(props: Props) {
 
       <div className="grid grid-cols-2 sm:flex gap-2 mt-8">
         <Link
-          aria-disabled={isSubmitting}
+          aria-disabled={isSubmitting || isRedirecting}
           to={`../${steps.fsaInquiry}`}
           state={data.init}
           className="py-3 min-w-[8rem] btn-outline-filled btn-reg"
