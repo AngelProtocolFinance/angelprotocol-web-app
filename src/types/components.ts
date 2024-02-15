e95a61565103d745b41b5bb9b5d508a49740e79c
Nenad <nenad@better.giving>
2024-02-15 10:56:30 +0100

    Use ValKey instead of Key in OptionType generic def

--- src/types/components.ts
+++ src/types/components.ts
@@ -1,9 +1,8 @@
-import { Key } from "react";
 import { FileObject } from "./aws";
 
 //selector
 export type ValKey = string | number;
-export type OptionType<V extends Key | symbol> = { label: string; value: V };
+export type OptionType<V extends ValKey | symbol> = { label: string; value: V };
 
 //dropzone
 export type FileDropzoneAsset = {
