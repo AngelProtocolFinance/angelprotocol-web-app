b23f22d4ebe6484271c42fdaac878e9906f2cf38
Nenad <nenad@better.giving>
2024-02-16 09:03:56 +0100

    Update email format

--- src/components/donation/Steps/Submit/DAFCheckout/ManualDonation.tsx
+++ src/components/donation/Steps/Submit/DAFCheckout/ManualDonation.tsx
@@ -6,20 +6,25 @@ export default function ManualDonation(props: ChariotCheckoutStep) {
   const profileUrl = `${window.location.origin}${appRoutes.donate}/${props.recipient.id}`;
 
   return (
-    <div className="grid content-start p-4 @md:p-8">
+    <div className="grid content-start">
       <p className="text-center text-gray-d1 uppercase">Manual DAF donation</p>
       <p className="mt-4 text-center">
-        To complete this donation, please email or provide your broker with the
-        following information:
+        To complete this donation, please email your provider with the following
+        information:
       </p>
       <div className="grid rounded bg-gray-l5 dark:bg-bluegray-d1 p-3 text-sm leading-relaxed mt-6">
         <p>Please make a one-time grant of ${props.details.amount} to:</p>
-        <p>Deliver to: Fidelity Investments</p>
-        <p>DTC number: 0226</p>
-        <p>Account number: Z40390069</p>
-        <p>Account name: Altruistic Partners Empowering Society, Inc</p>
+        <br />
+        <p>Altruistic Partners Empowering Society Inc (EIN: 87-3758939)</p>
+        <p>16192 Costal Highway Lewes, DE 19958 USA</p>
         <p>
-          Reference: [Internal Ref#, if needed] {props.recipient.name} (
+          Memo: For Altruistic Partners Empowering Society Inc (DBA: Better
+          Giving)
+        </p>
+        <p>Contact Info: Chauncey St. John, Board Member</p>
+        <p>Email: hi@better.giving</p>
+        <p>
+          Reference: [INTERNAL REF#, if needed] {props.recipient.name} (
           {profileUrl})
         </p>
       </div>
@@ -55,7 +60,7 @@ export default function ManualDonation(props: ChariotCheckoutStep) {
 const NEW_LINE = "%0D%0A";
 
 const emailLink = (charityName: string, profileUrl: string, amount: string) => `
-mailto:${"[ Your broker's email ]"}
+mailto:${"[ Your provider's email ]"}
   ?cc=${EMAIL_SUPPORT}
   &subject=Grant donation to Better.Giving supporting ${charityName}
   &body=
@@ -66,10 +71,12 @@ I have CCed better.giving (EIN 87-3758939) to ensure this tax-deductible donatio
 please ask them if you have any technical questions.${NEW_LINE}
 ${NEW_LINE}
 Please transfer $${amount || "[AMOUNT]"} to:${NEW_LINE}
-Deliver to: Fidelity Investments${NEW_LINE}
-DTC number: 0226${NEW_LINE}
-Account number: Z40390069${NEW_LINE}
-Account name: Altruistic Partners Empowering Society, Inc${NEW_LINE}
+${NEW_LINE}
+Altruistic Partners Empowering Society Inc (EIN: 87-3758939)${NEW_LINE}
+16192 Costal Highway Lewes, DE 19958 USA${NEW_LINE}
+Memo: For Altruistic Partners Empowering Society Inc (DBA: Better Giving)${NEW_LINE}
+Contact Info: Chauncey St. John, Board Member${NEW_LINE}
+Email: hi@better.giving${NEW_LINE}
 Reference: [INTERNAL REF#, if needed] ${charityName} (${profileUrl})${NEW_LINE}
 ${NEW_LINE}
 Thank you.`;
