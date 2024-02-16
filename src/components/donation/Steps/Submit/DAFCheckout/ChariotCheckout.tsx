f4a194492c8a508ef49d6cd32fc083f0fc071077
Nenad <nenad@better.giving>
2024-02-16 08:43:51 +0100

    Unwrap createGrant call

--- src/components/donation/Steps/Submit/DAFCheckout/ChariotCheckout.tsx
+++ src/components/donation/Steps/Submit/DAFCheckout/ChariotCheckout.tsx
@@ -86,7 +86,7 @@ export default function ChariotCheckout(
                     }
                   : undefined,
                 transactionId: r.detail.workflowSessionId,
-              });
+              }).unwrap();
 
               navigate(
                 details.source === "bg-widget"
