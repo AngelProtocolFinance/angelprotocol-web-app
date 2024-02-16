8cdbf8bb965f9bab4345edfcf79bd8cc83ecc5da
Nenad <nenad@better.giving>
2024-02-16 11:46:24 +0100

    Revert change to walletAddr

--- src/schemas/string.ts
+++ src/schemas/string.ts
@@ -9,7 +9,7 @@ export const requiredString = Yup.string().required("required");
 
 export const walletAddr = (chainId: ChainID) =>
   Yup.lazy((val) =>
-    !val || (typeof val === "string" && val.trim() === "")
+    val === ""
       ? Yup.string()
       : Yup.string()
           .trim()
