6ab938082b18323d43d5dca1fbdfb2dda1a4d519
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-02-28 18:42:00 +0800

    validation

--- src/components/Signup/SignupForm.tsx
+++ src/components/Signup/SignupForm.tsx
@@ -7,12 +7,31 @@ import { object } from "yup";
 
 export default function SignupForm() {
   const [isPasswordShown, setIsPasswordShown] = useState(false);
-  const { register } = useForm({
-    resolver: yupResolver(object({ password: requiredString })),
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(
+      object({
+        password: requiredString
+          .min(8, ({ min }) => `must have at least ${min} characters`)
+          .matches(/[a-z]/, "must have lowercase letters")
+          .matches(/[A-Z]/, "must have uppercase letters")
+          .matches(/\d/, "must have numbers")
+          .matches(
+            /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/,
+            "must have special characters"
+          ),
+      })
+    ),
   });
 
   return (
-    <form className="grid w-96 gap-4 mt-4">
+    <form
+      onSubmit={handleSubmit((fv) => console.log({ fv }))}
+      className="grid w-96 mt-4"
+    >
       <div className="grid grid-cols-[auto_1fr_auto] border border-prim rounded items-center px-3 has-[:focus]:ring-2 ring-blue-d1">
         <Icon type="Padlock" className="mr-3 text-gray" />
         <input
@@ -21,6 +40,7 @@ export default function SignupForm() {
           className="h-full focus:outline-none"
           placeholder="Create password"
         />
+
         <button
           type="button"
           className="py-3"
@@ -32,7 +52,14 @@ export default function SignupForm() {
           />
         </button>
       </div>
-      <button className="btn-blue rounded-full text-lg normal-case px-4 w-full">
+      <p className="text-xs text-red text-right empty:mb-4 mt-1">
+        {errors.password?.message}
+      </p>
+
+      <button
+        type="submit"
+        className="btn-blue rounded-full text-lg normal-case px-4 w-full mt-4"
+      >
         Create account
       </button>
     </form>
