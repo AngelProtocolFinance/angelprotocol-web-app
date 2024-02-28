0ff76d7025541229d6308f5d34fa8fb042819051
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-02-28 18:14:03 +0800

    field and submit

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -7,17 +7,28 @@ export default function SignupForm() {
   const methods = useForm();
   return (
     <FormProvider {...methods}>
-      <form className="">
-        <div className="grid grid-cols-[auto_1fr_auto] border border-prim rounded">
-          <Icon type="Padlock" />
-          <input type={isPasswordShown ? "text" : "password"} />
+      <form className="grid w-96 gap-4 mt-4">
+        <div className="grid grid-cols-[auto_1fr_auto] border border-prim rounded items-center px-3 has-[:focus]:ring-2 ring-blue-d1">
+          <Icon type="Padlock" className="mr-3 text-gray" />
+          <input
+            type={isPasswordShown ? "text" : "password"}
+            className="h-full focus:outline-none"
+            placeholder="Create password"
+          />
           <button
             type="button"
+            className="py-3"
             onClick={() => setIsPasswordShown((prev) => !prev)}
           >
-            <Icon type={isPasswordShown ? "EyeSlashed" : "Eye"} />
+            <Icon
+              type={isPasswordShown ? "EyeSlashed" : "Eye"}
+              className="text-gray"
+            />
           </button>
         </div>
+        <button className="btn-blue rounded-full text-lg normal-case px-4 w-full">
+          Create account
+        </button>
       </form>
     </FormProvider>
   );
