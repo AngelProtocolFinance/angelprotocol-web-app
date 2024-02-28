115d78d334ee16b17cc766332e9bac33232df434
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-02-28 18:51:03 +0800

    invalid style

--- src/components/Signup/SignupForm.tsx
+++ src/components/Signup/SignupForm.tsx
@@ -32,24 +32,22 @@ export default function SignupForm() {
       onSubmit={handleSubmit((fv) => console.log({ fv }))}
       className="grid w-96 mt-4"
     >
-      <div className="grid grid-cols-[auto_1fr_auto] border border-prim rounded items-center px-3 has-[:focus]:ring-2 ring-blue-d1">
+      <div className='grid grid-cols-[auto_1fr_auto] border border-prim rounded items-center px-3 has-[:focus]:ring-2 ring-blue ring-offset-1  has-[input[aria-invalid="true"]]:border-red'>
         <Icon type="Padlock" className="mr-3 text-gray" />
         <input
           {...register("password")}
           type={isPasswordShown ? "text" : "password"}
           className="h-full focus:outline-none"
           placeholder="Create password"
+          aria-invalid={!!errors.password?.message}
         />
 
         <button
           type="button"
-          className="py-3"
+          className="py-3 focus:outline-none focus:text-black text-gray"
           onClick={() => setIsPasswordShown((prev) => !prev)}
         >
-          <Icon
-            type={isPasswordShown ? "EyeSlashed" : "Eye"}
-            className="text-gray"
-          />
+          <Icon type={isPasswordShown ? "EyeSlashed" : "Eye"} />
         </button>
       </div>
       <p className="text-xs text-red text-right empty:mb-4 mt-1">
