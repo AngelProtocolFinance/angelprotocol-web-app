297528048119af5c2174b55331b9d53bd712af68
Nenad <nenad@better.giving>
2024-03-01 08:59:20 +0100

    Revert "use aws-aligned terms for donation status"
    This reverts commit 958bc5f60d5f840ef81a430e4db3019d56d2b171.

--- src/pages/Donations/Filter/constants.ts
+++ src/pages/Donations/Filter/constants.ts
@@ -1,8 +1,6 @@
-import { DonationsQueryParams } from "types/aws";
 import { OptionType } from "types/components";
 
-export const statuses: OptionType<Required<DonationsQueryParams>["status"]>[] =
-  [
-    { label: "RECEIVED", value: "FINALIZED" },
-    { label: "PENDING", value: "ON-HOLD" },
-  ];
+export const statuses: OptionType<string>[] = [
+  { label: "RECEIVED", value: "RECEIVED" },
+  { label: "PENDING", value: "PENDING" },
+];
--- src/types/aws/apes/donation.ts
+++ src/types/aws/apes/donation.ts
@@ -57,7 +57,7 @@ export type DonationsQueryParams = {
   beforeDate?: string;
   chainName?: string;
   denomination?: string;
-  status?: "ON-HOLD" | "FINALIZED";
+  status?: "PENDING" | "RECEIVED";
   start?: number; //to load next page, set start to ItemCutOff + 1
   limit?: number; // Number of items to be returned per request
 };
