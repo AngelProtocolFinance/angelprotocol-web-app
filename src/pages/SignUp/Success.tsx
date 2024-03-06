e4e7ea0bb169a5a2b1110a7c2f61ba7b59c87937
Nenad <nenad@better.giving>
2024-03-06 10:26:15 +0100

    Make Success mobile-friendly

--- src/pages/SignUp/Success.tsx
+++ src/pages/SignUp/Success.tsx
@@ -17,20 +17,23 @@ export default function Success({ userType }: Props) {
   };
 
   return (
-    <div className="grid justify-items-center w-full max-w-md px-7 py-8 bg-white border border-gray-l4 rounded-2xl">
-      <Icon type="CheckCircle" className="text-blue-d1 h-20 w-20" />
+    <div className="grid justify-items-center w-full max-w-xs sm:max-w-md px-6 sm:px-7 py-7 sm:py-8 bg-white border border-gray-l4 rounded-2xl">
+      <Icon
+        type="CheckCircle"
+        className="text-blue-d1 h-16 sm:h-20 w-16 sm:w-20"
+      />
 
-      <h3 className="text-center text-2xl font-bold text-navy-d4 mt-6">
+      <h3 className="text-center text-xl sm:text-2xl font-bold text-navy-d4 mt-6">
         Account created successfully
       </h3>
-      <p className="text-center font-normal mt-2">
+      <p className="text-center font-normal max-sm:text-sm mt-2">
         You can now proceed to sign in to your account
       </p>
 
       <Link
         to={appRoutes.signin}
         state={signInRouteState}
-        className="flex-center mt-9 w-full bg-blue-d1 disabled:bg-gray text-white enabled:hover:bg-blue enabled:active:bg-blue-d2 h-[52px] rounded-full normal-case text-lg font-bold"
+        className="flex-center mt-9 w-full bg-blue-d1 disabled:bg-gray text-white enabled:hover:bg-blue enabled:active:bg-blue-d2 h-12 sm:h-[52px] rounded-full normal-case sm:text-lg font-bold"
       >
         Continue to Sign in
       </Link>
