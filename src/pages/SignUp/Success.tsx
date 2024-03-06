3d0f782194a1cf6e74a68528ce38fba405686d6c
Nenad <nenad@better.giving>
2024-03-06 10:22:22 +0100

    center align check circle on success

--- src/pages/SignUp/Success.tsx
+++ src/pages/SignUp/Success.tsx
@@ -17,7 +17,7 @@ export default function Success({ userType }: Props) {
   };
 
   return (
-    <div className="w-full max-w-md px-7 py-8 bg-white border border-gray-l4 rounded-2xl">
+    <div className="grid justify-items-center w-full max-w-md px-7 py-8 bg-white border border-gray-l4 rounded-2xl">
       <Icon type="CheckCircle" className="text-blue-d1 h-20 w-20" />
 
       <h3 className="text-center text-2xl font-bold text-navy-d4 mt-6">
