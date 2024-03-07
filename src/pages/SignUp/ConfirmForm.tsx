d9e551995bc9b1a17c56fb73b21a6c9b119a6d3e
Nenad <nenad@better.giving>
2024-03-07 13:13:24 +0100

    remove redundant className in ConfirmForm.time remaining span

--- src/pages/SignUp/ConfirmForm.tsx
+++ src/pages/SignUp/ConfirmForm.tsx
@@ -95,9 +95,7 @@ export default function ConfirmForm(props: Props) {
       />
 
       <span className="flex items-center justify-between text-sm font-medium">
-        <span className="justify-self-end">
-          Time remaining: 00:{String(counter).padStart(2, "0")}
-        </span>
+        <span>Time remaining: 00:{String(counter).padStart(2, "0")}</span>
         <button
           type="button"
           className="text-blue-d1 hover:text-blue active:text-blue-d2 disabled:text-gray-l2 font-bold underline"
