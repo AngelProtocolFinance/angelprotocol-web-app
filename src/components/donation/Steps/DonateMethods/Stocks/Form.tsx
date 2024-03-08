8f5ac8a6c2079f3762ff6abe1945199655eea44e
ap-justin <justin@angelprotocol.io>
2024-03-08 22:35:55 +0800

    apply to stocks

--- src/components/donation/Steps/DonateMethods/Stocks/Form.tsx
+++ src/components/donation/Steps/DonateMethods/Stocks/Form.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Field } from "components/form";
+import { Field, Form as FormContainer } from "components/form";
 import { FormProvider, UseFormReturn, useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { requiredString } from "schemas/string";
@@ -12,13 +12,13 @@ export default function Form(props: StockFormStep) {
   const methods = useForm({
     defaultValues: props.details
       ? {
-          symbol: props.details.symbol,
-          numShares: props.details.numShares.toString(),
-        }
+        symbol: props.details.symbol,
+        numShares: props.details.numShares.toString(),
+      }
       : {
-          symbol: "",
-          numShares: "",
-        },
+        symbol: "",
+        numShares: "",
+      },
     resolver: yupResolver(
       object({
         symbol: requiredString.trim(),
@@ -30,68 +30,67 @@ export default function Form(props: StockFormStep) {
   type FV = typeof methods extends UseFormReturn<infer U> ? U : never;
 
   return (
-    <FormProvider {...methods}>
-      <form
-        className="grid"
-        onSubmit={methods.handleSubmit((fv) => {
-          dispatch(
-            setDetails({
-              method: "stocks",
-              symbol: fv.symbol,
-              numShares: +fv.numShares,
-            })
-          );
-        })}
+    <FormContainer methods={methods}
+      className="grid"
+      onSubmit={methods.handleSubmit((fv) => {
+        dispatch(
+          setDetails({
+            method: "stocks",
+            symbol: fv.symbol,
+            numShares: +fv.numShares,
+          })
+        );
+      })}
+    >
+      <Field<FV>
+        required
+        name="symbol"
+        label="Symbol name of share"
+        placeholder="Ex. AAPL"
+        classes={{ label: "font-semibold", container: "field-donate" }}
+      />
+
+      <Field<FV>
+        required
+        name="numShares"
+        label="Number of shares you are donating"
+        placeholder="Enter quantity"
+        classes={{
+          label: "font-semibold",
+          container: "mt-6 field-donate",
+          error: "left-0",
+        }}
+      />
+
+      <h4 className="mt-6 mb-2">Benefits of donating appreciated stock</h4>
+      <p className="text-sm">
+        You can enjoy significant tax advantages and maximize the size of your
+        contributions when you transfer securities through Better.Giving:
+      </p>
+      <div className="grid rounded bg-gray-l5 dark:bg-navy-d3 p-2 my-4">
+        <span className="text-sm text-navy-l2">
+          NOTE: This is not financial advice! Please speak to your tax advisor
+          or broker about your specific situation and country's tax laws.
+        </span>
+      </div>
+      <p className="text-sm">
+        If you held the stock for at least one year, you receive a tax
+        deduction for the full value of the stock at the time of donation (not
+        just the amount you paid for the stock).
+      </p>
+      <p className="text-sm mt-4">
+        You avoid paying both capital gains tax and stock sales commissions.
+        When you give appreciated stocks directly to a nonprofit, your gift
+        can be up to 20% larger because you avoid the taxes you'd incur from
+        selling and donating the cash.
+      </p>
+      <button
+        disabled={methods.formState.isSubmitting}
+        className="btn-orange btn-donate mt-6"
+        type="submit"
       >
-        <Field<FV>
-          required
-          name="symbol"
-          label="Stock symbol (Ticker)"
-          placeholder="AAPL"
-          classes={{ label: "font-semibold" }}
-        />
-
-        <Field<FV>
-          required
-          name="numShares"
-          label="Number of Shares to donate"
-          classes={{
-            label: "font-semibold",
-            container: "mt-4",
-            error: "left-0",
-          }}
-        />
-
-        <h4 className="mt-6 mb-2">Benefits of donating appreciated stock</h4>
-        <p className="text-sm">
-          You can enjoy significant tax advantages and maximize the size of your
-          contributions when you transfer securities through Better.Giving:
-        </p>
-        <div className="grid rounded bg-gray-l5 dark:bg-navy-d3 p-2 my-4">
-          <span className="text-sm text-navy-l2">
-            NOTE: This is not financial advice! Please speak to your tax advisor
-            or broker about your specific situation and country's tax laws.
-          </span>
-        </div>
-        <p className="text-sm">
-          If you held the stock for at least one year, you receive a tax
-          deduction for the full value of the stock at the time of donation (not
-          just the amount you paid for the stock).
-        </p>
-        <p className="text-sm mt-4">
-          You avoid paying both capital gains tax and stock sales commissions.
-          When you give appreciated stocks directly to a nonprofit, your gift
-          can be up to 20% larger because you avoid the taxes you'd incur from
-          selling and donating the cash.
-        </p>
-        <button
-          disabled={methods.formState.isSubmitting}
-          className="btn-orange btn-donate mt-6"
-          type="submit"
-        >
-          Continue
-        </button>
-      </form>
-    </FormProvider>
+        Continue
+      </button>
+    </FormContainer>
   );
 }
