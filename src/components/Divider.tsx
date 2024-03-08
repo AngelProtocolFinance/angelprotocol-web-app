9d211e793defe78f7fd5b6a40dd58844cbdcf209
Nenad <nenad@better.giving>
2024-03-08 10:31:06 +0100

    use Separator in BankDetails + remove Divider

--- src/components/Divider.tsx
+++
@@ -1,9 +0,0 @@
-export default function Divider({ vertical = false }) {
-  return (
-    <div
-      className={`bg-gray-l3 dark:bg-navy ${
-        vertical ? "w-px h-full" : "h-px w-full"
-      }`}
-    />
-  );
-}
