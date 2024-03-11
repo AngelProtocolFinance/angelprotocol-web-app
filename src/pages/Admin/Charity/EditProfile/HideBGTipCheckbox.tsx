00d4ce1f172a052f1b939a6cece9335899e3ba7b
Nenad <nenad@better.giving>
2024-03-11 11:54:31 +0100

    update checkbox ui

--- src/pages/Admin/Charity/EditProfile/HideBGTipCheckbox.tsx
+++ src/pages/Admin/Charity/EditProfile/HideBGTipCheckbox.tsx
@@ -3,8 +3,11 @@ import { FV } from "./types";
 
 export default function HideBGTipCheckbox() {
   return (
-    <div className="grid gap-2 text-sm">
-      <span>
+    <div className="grid gap-2">
+      <CheckField<FV> name="hide_bg_tip" classes="text-sm">
+        Opt out of tip-based donations
+      </CheckField>
+      <span className="text-xs sm:text-sm italic text-navy-l1">
         During the donation flow, there is a step in which users can choose to
         tip us any amount they desire alongside their donation to you - the
         amount they tip will not affect the donation amount you receive. You may
@@ -12,9 +15,6 @@ export default function HideBGTipCheckbox() {
         checkbox below and we will instead apply a fixed 1.5% fee to any
         donation amount you receive.
       </span>
-      <CheckField<FV> name="hide_bg_tip">
-        Opt out of tip-based donations
-      </CheckField>
     </div>
   );
 }
