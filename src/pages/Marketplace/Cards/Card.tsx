6276e31b179cadc421dd613ec872978eb9472b28
ap-justin <justin@angelprotocol.io>
2024-03-11 15:56:54 +0800

    endow designation chip

--- src/pages/Marketplace/Cards/Card.tsx
+++ src/pages/Marketplace/Cards/Card.tsx
@@ -26,7 +26,7 @@ export default function Card({
   return (
     <div className="relative overflow-clip dark:bg-blue-d6 rounded-lg border border-gray-l4 hover:border-blue dark:hover:border-blue">
       <div className="absolute top-[14px] left-[14px] right-[14px] flex justify-between gap-3">
-        <p className="bg-orange-l1 text-white font-semibold text-2xs rounded-sm uppercase px-2 py-0.5 font-heading">
+        <p className="bg-blue-d1 text-white font-semibold text-2xs rounded-sm uppercase px-2 py-0.5 font-heading">
           {endow_designation}
         </p>
         {kyc_donors_only && <KYCIcon className="ml-auto" />}
