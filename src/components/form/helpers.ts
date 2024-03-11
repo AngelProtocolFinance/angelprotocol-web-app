3e2391b0b118d02fa5bc023a94484028c81b559d
ap-justin <justin@angelprotocol.io>
2024-03-11 10:48:11 +0800

    container only

--- src/components/form/helpers.ts
+++ src/components/form/helpers.ts
@@ -2,12 +2,14 @@ type Base = { container?: string };
 export function unpack<T extends string | Base>(
   classes?: T
 ): Readonly<Required<Exclude<T, string>>> {
-  if (typeof classes === "string") return { container: classes } as any;
-  return new Proxy(classes || {}, {
-    get(target: any, key) {
-      return target[key] ?? "";
-    },
-  });
+  return new Proxy(
+    typeof classes === "string" ? { container: classes } : classes || {},
+    {
+      get(target: any, key) {
+        return target[key] ?? "";
+      },
+    }
+  );
 }
 
 export function dateToFormFormat(date: Date) {
