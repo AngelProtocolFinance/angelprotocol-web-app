2222aa9a07f509172094fed56cadb337184b9618
ap-justin <justin@angelprotocol.io>
2024-03-11 19:32:41 +0800

    revert radius

--- src/App/Footer/Newsletter/SubscriptionForm.tsx
+++ src/App/Footer/Newsletter/SubscriptionForm.tsx
@@ -52,7 +52,7 @@ export default function SubscriptionForm() {
         <input
           {...register("email")}
           id="email"
-          className="text-xs field-input rounded-lg enabled:focus:ring-blue-d2 border-none w-full h-10 px-3 bg-white"
+          className="text-xs field-input enabled:focus:ring-blue-d2 border-none w-full h-10 px-3 bg-white"
           placeholder="Enter your email address here"
           disabled={isSubmitting}
         />
@@ -66,7 +66,7 @@ export default function SubscriptionForm() {
       </div>
       <button
         type="submit"
-        className="btn-blue enabled:hover:bg-blue-l4  enabled:hover:text-navy-d4 rounded-full px-4 text-xs h-10 w-[80vw] lg:w-full xl:w-min"
+        className="btn-blue enabled:hover:bg-blue-l4  enabled:hover:text-navy-d4 px-4 text-xs h-10 w-[80vw] lg:w-full xl:w-min"
         disabled={isSubmitting}
       >
         Subscribe
