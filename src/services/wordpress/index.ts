4f24da646f47bf24ed9bdc06c87c0c27d06e6d6c
ap-justin <justin@angelprotocol.io>
2024-03-12 20:46:14 +0800

    wordpress endpoints

--- src/services/wordpress/index.ts
+++ src/services/wordpress/index.ts
@@ -2,59 +2,58 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { APIs } from "constants/urls";
 import { Page, Post, WpQueryParams } from "types/wp";
 
-export const wpApi = createApi({
-  // Set the baseUrl for every endpoint below
-  baseQuery: fetchBaseQuery({
-    baseUrl: APIs.wp,
-    endpoints: (builder) => ({
-      listPosts: builder.query<Post[], WpQueryParams>({
-        query: (_params) => {
-          return {
-            url: `/posts`,
-            params: {},
-          };
-        },
-      }),
-      getPost: builder.query<Post>({
-        query: (slug) => {
-          return {
-            url: `/posts?slug=${slug}`,
-            params,
-          };
-        },
-      }),
-      listPages: builder.query<Page[], WpQueryParams>({
-        query: (_params) => {
-          return {
-            url: `/pages`,
-            params: {},
-          };
-        },
-      }),
-      getPage: builder.query<Page>({
-        query: (slug) => {
-          return {
-            url: `/pages?slug=${slug}`,
-            params: {},
-          };
-        },
-      }),
-      // listCategories: builder.query<Category[], WpQueryParams>({
-      // 	query: (params) => {
-      // 		return {
-      // 			url: `/categories`,
-      // 			params: {},
-      // 		};
-      // 	},
-      // }),
-      // getCategory: builder.query<Category>({
-      // 	query: (id) => {
-      // 		return {
-      // 			url: `/category/${id}`,
-      // 			params: {},
-      // 		};
-      // 	},
-      // }),
+export const wordpress = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: APIs.wordpress }),
+  reducerPath: "wordpress",
+  endpoints: (builder) => ({
+    posts: builder.query<Post[], WpQueryParams>({
+      query: (_params) => {
+        return {
+          url: "post",
+          params: {},
+        };
+      },
     }),
+    post: builder.query<Post, string>({
+      query: (slug) => {
+        return {
+          url: `posts?slug=${slug}`,
+        };
+      },
+    }),
+    pages: builder.query<Page[], WpQueryParams>({
+      query: (_params) => {
+        return {
+          url: "pages",
+          params: {},
+        };
+      },
+    }),
+    page: builder.query<Page, string>({
+      query: (slug) => {
+        return {
+          url: `/pages?slug=${slug}`,
+          params: {},
+        };
+      },
+    }),
+    // categories: builder.query<Category[], WpQueryParams>({
+    //   query: () => {
+    //     return {
+    //       url: `/categories`,
+    //       params: {},
+    //     };
+    //   },
+    // }),
+    // category: builder.query<Category>({
+    //   query: (id) => {
+    //     return {
+    //       url: `/category/${id}`,
+    //       params: {},
+    //     };
+    //   },
+    // }),
   }),
 });
+
+export const { usePagesQuery } = wordpress;
