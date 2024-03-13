49a226a5c586ed67c25cc34f7a0c8784d3d0935a
ap-justin <justin@angelprotocol.io>
2024-03-13 11:25:07 +0800

    add skeleton

--- src/pages/Blog/Posts.tsx
+++ src/pages/Blog/Posts.tsx
@@ -1,11 +1,12 @@
+import ContentLoader from "components/ContentLoader";
 import Image from "components/Image";
+import QueryLoader from "components/QueryLoader";
 import { Link } from "react-router-dom";
 import { useMediaQuery, usePostsQuery } from "services/wordpress";
+import { Wordpress } from "types/wordpress";
 
 export default function Posts() {
-  const { data: _posts, isLoading, isError } = usePostsQuery({});
-  if (isLoading) return <p>Loading...TODO: create skeleton</p>;
-  if (!_posts || isError) return <p>Error loading posts..</p>;
+  const query = usePostsQuery({});
 
   return (
     <div className="grid gap-6 m:grid-cols-2 lg:grid-cols-3 content-start padded-container min-h-screen pb-6">
@@ -13,31 +14,24 @@ export default function Posts() {
         Posts
       </h1>
 
-      {_posts.map((post, _index) => (
-        <Link
-          key={post.slug}
-          to={post.slug}
-          className="grid grid-rows-[auto_1fr] h-full rounded-lg overflow-clip bg-blue-l5 shadow-sm border-blue-l4 border hover:border-blue-l2 hover:shadow-blue-l2"
-        >
-          <Media id={post.featured_media} />
-          <div className="flex flex-col p-4 gap-3">
-            <h2
-              className="text-pretty"
-              // biome-ignore lint: trusted html
-              dangerouslySetInnerHTML={{
-                __html: post.title.rendered,
-              }}
-            />
-            <p
-              className="text-navy-l1"
-              // biome-ignore lint: trusted html
-              dangerouslySetInnerHTML={{
-                __html: post.excerpt.rendered,
-              }}
-            />
-          </div>
-        </Link>
-      ))}
+      <QueryLoader
+        queryState={query}
+        messages={{
+          empty: "No posts found.",
+          error: "Failed to load posts",
+          loading: (
+            <>
+              {Array(9)
+                .fill(null)
+                .map((_, idx) => (
+                  <CardSkeleton key={idx} />
+                ))}
+            </>
+          ),
+        }}
+      >
+        {(posts) => <Cards posts={posts} />}
+      </QueryLoader>
     </div>
   );
 }
@@ -46,4 +40,40 @@ function Media({ id }: { id: number }) {
   //TODO: detailed image type and use src set
   const { data: url } = useMediaQuery(id);
   return <Image src={url} className="h-32 sm:h-60 w-full object-cover" />;
+}
+
+const Cards = (props: { posts: Wordpress.Post[] }) =>
+  props.posts.map((post, _index) => (
+    <Link
+      key={post.slug}
+      to={post.slug}
+      className="grid grid-rows-[auto_1fr] h-full rounded-lg overflow-clip bg-blue-l5 hover:bg-blue-l4"
+    >
+      <Media id={post.featured_media} />
+      <div className="flex flex-col p-4 gap-3">
+        <h2
+          className="text-pretty"
+          // biome-ignore lint: trusted html
+          dangerouslySetInnerHTML={{
+            __html: post.title.rendered,
+          }}
+        />
+        <p
+          className="text-navy-l1"
+          // biome-ignore lint: trusted html
+          dangerouslySetInnerHTML={{
+            __html: post.excerpt.rendered,
+          }}
+        />
+      </div>
+    </Link>
+  ));
+
+function CardSkeleton() {
+  return (
+    <div className="rounded-lg border border-gray-l4">
+      <ContentLoader className="h-32 sm:h-60 w-full" />
+      <div className="h-40" />
+    </div>
+  );
 }
