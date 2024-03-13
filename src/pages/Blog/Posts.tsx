59a740cf539cb745450863c2484b94beb32ef2c9
ap-justin <justin@angelprotocol.io>
2024-03-13 11:09:22 +0800

    reduce nesting and add affordance

--- src/pages/Blog/Posts.tsx
+++ src/pages/Blog/Posts.tsx
@@ -8,42 +8,42 @@ export default function Posts() {
   if (!_posts || isError) return <p>Error loading posts..</p>;
 
   return (
-    <div className="grid gap-y-4 content-start padded-container min-h-screen">
-      <h1 className="text-orange font-header text-[1.63rem] md:text-3xl lg:text-[2.75rem] break-words my-5">
+    <div className="grid gap-6 m:grid-cols-2 lg:grid-cols-3 content-start padded-container min-h-screen pb-6">
+      <h1 className="col-span-full text-xl md:text-2xl lg:text-3xl break-words mt-6">
         Posts
       </h1>
-      <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 gap-4 content-start">
-        {_posts.map((post, _index) => (
-          <Link
-            key={post.slug}
-            to={post.slug}
-            className="grid grid-rows-[auto_1fr] h-full bg-white rounded-lg overflow-clip shadow-sm"
-          >
-            <Media id={post.featured_media} />
-            <div className="flex flex-col p-3 pb-4 gap-3">
-              <h2
-                className="text-pretty"
-                // biome-ignore lint: trusted html
-                dangerouslySetInnerHTML={{
-                  __html: post.title.rendered,
-                }}
-              />
-              <p
-                className="text-navy-l1"
-                // biome-ignore lint: trusted html
-                dangerouslySetInnerHTML={{
-                  __html: post.excerpt.rendered,
-                }}
-              />
-            </div>
-          </Link>
-        ))}
-      </div>
+
+      {_posts.map((post, _index) => (
+        <Link
+          key={post.slug}
+          to={post.slug}
+          className="grid grid-rows-[auto_1fr] h-full rounded-lg overflow-clip bg-blue-l5 shadow-sm border-blue-l4 border hover:border-blue-l2 hover:shadow-blue-l2"
+        >
+          <Media id={post.featured_media} />
+          <div className="flex flex-col p-4 gap-3">
+            <h2
+              className="text-pretty"
+              // biome-ignore lint: trusted html
+              dangerouslySetInnerHTML={{
+                __html: post.title.rendered,
+              }}
+            />
+            <p
+              className="text-navy-l1"
+              // biome-ignore lint: trusted html
+              dangerouslySetInnerHTML={{
+                __html: post.excerpt.rendered,
+              }}
+            />
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
 
 function Media({ id }: { id: number }) {
+  //TODO: detailed image type and use src set
   const { data: url } = useMediaQuery(id);
-  return <Image src={url} />;
+  return <Image src={url} className="h-32 sm:h-60 w-full object-cover" />;
 }
