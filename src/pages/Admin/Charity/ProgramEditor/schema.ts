736d005a030fc0752221eeea6eaa456f40b016f2
ap-justin <justin@angelprotocol.io>
2024-03-19 15:10:56 +0800

    program editor

--- src/pages/Admin/Charity/ProgramEditor/schema.ts
+++ src/pages/Admin/Charity/ProgramEditor/schema.ts
@@ -14,7 +14,7 @@ export const VALID_MIME_TYPES: ImageMIMEType[] = [
   "image/svg+xml",
 ];
 
-const MAX_SIZE_IN_BYTES = 1e6;
+export const MAX_SIZE_IN_BYTES = 1e6;
 export const MAX_CHARS = 500;
 
 // we only need to validate the pre-crop image and if we confirm it is valid
