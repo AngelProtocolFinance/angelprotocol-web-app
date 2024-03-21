0f880214bc8d4ce0508e92a3d0c23d74da792f7a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-03-21 19:26:11 +0800

    new settings page

---
+++ src/pages/Admin/Charity/Settings/Form.tsx
@@ -0,0 +1,62 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Field, Form as FormContainer } from "components/Form";
+import { useErrorContext } from "contexts/ErrorContext";
+import { useForm } from "react-hook-form";
+import { schema } from "schemas/shape";
+import { useEditEndowmentMutation } from "services/aws/aws";
+import { string } from "yup";
+import { FV } from "./types";
+
+type Props = {
+  id: number;
+  receiptMsg?: string;
+};
+
+export default function Form(props: Props) {
+  const [submit] = useEditEndowmentMutation();
+  const { handleError } = useErrorContext();
+  const methods = useForm({
+    resolver: yupResolver(
+      schema<FV>({
+        receiptMsg: string().max(100),
+      })
+    ),
+    defaultValues: { receiptMsg: props.receiptMsg ?? "" },
+  });
+
+  const { reset, handleSubmit } = methods;
+
+  return (
+    <FormContainer
+      methods={methods}
+      onReset={(e) => {
+        e.preventDefault();
+        reset();
+      }}
+      onSubmit={handleSubmit(async (fv) => {
+        try {
+          await submit({ id: props.id, receiptMsg: fv.receiptMsg });
+        } catch (err) {
+          handleError(err);
+        }
+      })}
+      className="w-full max-w-4xl justify-self-center grid content-start gap-6 mt-6"
+    >
+      <Field<FV, "textarea">
+        type="textarea"
+        classes="field-admin"
+        name="receiptMsg"
+        label="Receipt message"
+        placeholder="your message to your donor"
+      />
+      <div className="flex gap-3">
+        <button type="reset" className="px-6 btn-outline-filled text-sm">
+          Reset changes
+        </button>
+        <button type="submit" className="px-6 btn-blue text-sm">
+          Submit changes
+        </button>
+      </div>
+    </FormContainer>
+  );
+}
