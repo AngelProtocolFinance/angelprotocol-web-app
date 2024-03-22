06e0bf581854d783940344fc32f7da5a59da5b95
Nenad <nenad@better.giving>
2024-03-22 09:31:51 +0100

    fix widget preview overflow being cut off

--- src/pages/Widget/Preview.tsx
+++ src/pages/Widget/Preview.tsx
@@ -18,21 +18,22 @@ export default function Preview({ classes = "" }) {
       <h2 className="text-lg @4xl/widget:text-2xl text-center @4xl/widget:text-left mb-3">
         That's what our widget looks like:
       </h2>
-      <div className="pt-6 flex flex-col items-center gap-5 @xl/preview:pt-10 h-full overflow-y-auto scroller w-full max-h-[800px] border border-gray-l2 rounded text-navy-d4 bg-white">
-        <h1 className="flex justify-center items-center gap-10 w-full h-24 z-20 text-lg sm:text-3xl">
-          Donate to {endowName}
-        </h1>
-        <Steps
-          className="mt-5 w-full h-full md:w-3/4 border border-gray-l4"
-          donaterConfig={{
-            isPreview: true,
-            liquidSplitPct: widgetConfig.liquidSplitPct,
-          }}
-        />
-
-        <footer className="grid place-items-center h-20 w-full bg-blue mt-auto">
-          <DappLogo classes="w-20" />
-        </footer>
+      <div className="h-full overflow-y-auto scroller w-full max-h-[800px] border border-gray-l2 rounded text-navy-d4 bg-white">
+        <div className="grid grid-rows-[auto_1fr_auto] justify-items-center gap-10 h-full pt-6 @xl/preview:pt-10">
+          <h1 className="flex justify-center items-center gap-10 w-full h-24 z-20 text-lg @sm/preview:text-3xl">
+            Donate to {endowName}
+          </h1>
+          <Steps
+            className="w-full @md/preview:w-3/4 border border-gray-l4"
+            donaterConfig={{
+              isPreview: true,
+              liquidSplitPct: widgetConfig.liquidSplitPct,
+            }}
+          />
+          <footer className="grid place-items-center h-20 w-full bg-blue">
+            <DappLogo classes="w-20" />
+          </footer>
+        </div>
       </div>
     </section>
   );
