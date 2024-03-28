40be3aa37a154fde616ef684968abfc719d6ac08
Nenad <nenad@better.giving>
2024-03-28 09:06:39 +0100

    Add explanatory comments to useDetermineLiquidSplitPct

--- src/components/donation/Steps/useDetermineLiquidSplitPct.ts
+++ src/components/donation/Steps/useDetermineLiquidSplitPct.ts
@@ -7,12 +7,24 @@ export default function useDetermineLiquidSplitPct(
   config: Config | null,
   state: DonationState
 ) {
+  /**
+   * Initial value is set using config.liquidSplitPct if it exists.
+   * This is mainly to "work around" React's "strict" mode, which performs
+   * each render 2 times, causing a bug where either state's or config's
+   * liquid % value unintentionally overwrites the previous value.
+   */
   const [liquidSplitPct, setLiquidSplitPct] = useState<number>();
+  /**
+   * Need the ref to avoid state.liquidSplitPct overwriting config's value on first render
+   */
   const initRef = useRef(true);
 
   // update liquid % value when a new state value is set
   useEffect(() => {
+    // this check is performed only on initial render
     if (config?.liquidSplitPct && initRef.current) {
+      // this check is necessary mainly for React's "strict" mode, to avoid
+      // overwriting liquidSplitPct on the 2nd render
       if (liquidSplitPct) {
         initRef.current = false;
       }
@@ -21,6 +33,8 @@ export default function useDetermineLiquidSplitPct(
     if ("liquidSplitPct" in state && state.liquidSplitPct) {
       setLiquidSplitPct(state.liquidSplitPct);
     }
+    // no need to add either config or liquidSplitPct to the deps. array
+    // as those are needed only on the initial render anyway
   }, [state, (state as SplitsStep).liquidSplitPct]);
 
   // update liquid % value when a new config value is passed
