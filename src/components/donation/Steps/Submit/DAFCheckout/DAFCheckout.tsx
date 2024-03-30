36e92555853b1b082b14ec8aba643ae6258afb66
Nenad Misic <nenad@better.giving>
2024-03-30 07:36:12 +0100

    remove chariot from the webapp (#2821)

--- src/components/donation/Steps/Submit/DAFCheckout/DAFCheckout.tsx
+++ src/components/donation/Steps/Submit/DAFCheckout/DAFCheckout.tsx
@@ -3,10 +3,6 @@ import { useSetter } from "store/accessors";
 import BackBtn from "../../common/BackBtn";
 import ManualDonation from "./ManualDonation";
 
-// NOTE: We are removing the Chariot option for now while their internal team
-// decided whether or not to move forward with production access.
-// type Method = "chariot" | "manual" | undefined;
-
 export default function DAFCheckout(props: DafCheckoutStep) {
   const dispatch = useSetter();
 
