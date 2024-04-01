7addcbc9e9267189e2b43bcf0994b7ff16999e83
ap-justin <justin@angelprotocol.io>
2024-04-01 15:18:35 +0800

    endow claim type

--- src/types/aws/ap/registration.ts
+++ src/types/aws/ap/registration.ts
@@ -39,6 +39,12 @@ export type ContactRoles =
   | "treasurer"
   | "vice-president";
 
+export type EndowClaim = {
+  id: number;
+  name: string;
+  ein: string;
+};
+
 type InitReg = {
   PK: string;
   SK: "Registration";
@@ -47,6 +53,7 @@ type InitReg = {
   RejectionReason: string;
   UN_SDG: UNSDG_NUMS[];
   bank_verification_status: BankVerificationStatus;
+  InitClaim?: EndowClaim;
 };
 
 export type InitContact = {
