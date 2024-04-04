653d2cbb8b0a2cf734d7f505cf2cab0bcde3c40c
Andrey <SovereignAndrey>
2024-04-04 10:51:27 +0800

    update logo image used

--- src/components/Image/DappLogo.tsx
+++ src/components/Image/DappLogo.tsx
@@ -1,4 +1,4 @@
-import dappLogo from "assets/images/bettergiving-logo-with-name-all-blue.svg";
+import dappLogo from "assets/images/bettergiving-logo.png";
 import { BASE_URL } from "constants/env";
 import Image from "./Image";
 
