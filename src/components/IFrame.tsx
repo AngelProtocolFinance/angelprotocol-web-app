b4c38f83344907703ef7ad7665b266cc1cf418b8
Nenad <nenad@better.giving>
2024-04-08 13:41:40 +0200

    Return IFrame

---
+++ src/components/IFrame.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import LoaderRing from "./LoaderRing";
+
+type Props = React.IframeHTMLAttributes<HTMLIFrameElement>;
+
+export default function IFrame({
+  src,
+  title,
+  className = "",
+  onLoad,
+  ...rest
+}: Props) {
+  const [isLoading, setLoading] = useState(true);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: self-explanatory
+  useEffect(() => setLoading(true), [src]);
+
+  return (
+    <>
+      {isLoading && (
+        <div
+          className={`${className} flex justify-center items-center gap-2`}
+          style={{
+            height: rest.height ? `${rest.height}px` : undefined,
+            width: rest.width ? `${rest.width}px` : undefined,
+          }}
+        >
+          <LoaderRing thickness={10} classes="w-8" />
+          Loading...
+        </div>
+      )}
+      <iframe
+        {...rest}
+        src={src}
+        title={title}
+        className={`${isLoading ? "hidden" : ""} ${className}`}
+        onLoad={(e) => {
+          onLoad && onLoad(e);
+          setLoading(false);
+        }}
+      ></iframe>
+    </>
+  );
+}
