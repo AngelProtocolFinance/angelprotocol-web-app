f275a28f605cc21c2afb18e975d46084f3314f49
ap-justin <justin@angelprotocol.io>
2024-04-08 15:44:31 +0800

    add color prop

--- src/components/Image/DappLogo.tsx
+++ src/components/Image/DappLogo.tsx
@@ -1,12 +1,18 @@
+import dappLogoWhite from "assets/images/bettergiving-logo-white.png";
 import dappLogo from "assets/images/bettergiving-logo.png";
 import { BASE_URL } from "constants/env";
 import Image from "./Image";
 
-export default function DappLogo({ classes = "" }) {
+type Props = {
+  classes?: string;
+  color?: "blue" | "white";
+};
+
+export default function DappLogo({ classes = "", color = "blue" }: Props) {
   return (
     <Image
       className={classes}
-      src={dappLogo}
+      src={color === "blue" ? dappLogo : dappLogoWhite}
       title="Go to Marketing page"
       href={BASE_URL}
     />
