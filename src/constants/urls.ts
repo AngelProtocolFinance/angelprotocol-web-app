3baead61705bf67d3d265a25ebe69b221a44282c
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2024-04-09 10:04:16 +0800

    switch WP API based on environment (test v. prod) (#2863)
    Co-authored-by: Kevin <SovereignAndrey>

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,9 +1,11 @@
-import { BASE_URL, ENVIRONMENT } from "./env";
+import { BASE_URL, ENVIRONMENT, IS_TEST } from "./env";
 
 export const APIs = {
   aws: "https://kpnxz5rzo2.execute-api.us-east-1.amazonaws.com",
   apes: `https://fctqkloitc.execute-api.us-east-1.amazonaws.com/${ENVIRONMENT}`,
-  wordpress: "https://angelgiving-dev.10web.site/wp-json/wp/v2",
+  wordpress: IS_TEST
+    ? "https://angelgiving-dev.10web.site/wp-json/wp/v2"
+    : "34.29.12.245",
 };
 
 export const LITEPAPER = `${BASE_URL}/docs/litepaper-introduction/`;
