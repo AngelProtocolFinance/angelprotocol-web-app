49175e7df7ee49305246dd27dc464be8871935de
Nenad <nenad@better.giving>
2024-04-10 08:29:51 +0200

    Make more mobile-friendly

--- src/pages/Donations/StatusTabs.tsx
+++ src/pages/Donations/StatusTabs.tsx
@@ -10,7 +10,7 @@ export default function StatusTabs(props: Props) {
     <div className="flex">
       <button
         onClick={() => props.changeStatus("final")}
-        className={`relative group w-full sm:w-52 rounded-t-lg py-2.5 text-sm font-bold leading-5
+        className={`relative group w-full sm:w-52 rounded-t-lg p-2.5 text-xs sm:text-sm font-bold leading-5
         focus:outline-none border-t border-x border-gray-l4 ${
           props.status === "final"
             ? "bg-blue-l4 z-20"
@@ -26,7 +26,7 @@ export default function StatusTabs(props: Props) {
       </button>
       <button
         onClick={() => props.changeStatus("pending")}
-        className={`relative group w-full sm:w-52 rounded-t-lg py-2.5 text-sm font-bold leading-5
+        className={`relative group w-full sm:w-52 rounded-t-lg p-2.5 text-xs sm:text-sm font-bold leading-5
         focus:outline-none border-t border-x border-gray-l4 ${
           props.status === "pending"
             ? "bg-blue-l4 z-20"
@@ -44,7 +44,7 @@ export default function StatusTabs(props: Props) {
       </button>
       <button
         onClick={() => props.changeStatus("intent")}
-        className={`relative group w-full sm:w-52 rounded-t-lg py-2.5 text-sm font-bold leading-5
+        className={`relative group w-full sm:w-52 rounded-t-lg p-2.5 text-xs sm:text-sm font-bold leading-5
         focus:outline-none border-t border-x border-gray-l4 ${
           props.status === "intent"
             ? "bg-blue-l4 z-20"
