705047cc488b5c8348d92d62f63867865e303b6b
Andrey <SovereignAndrey>
2024-04-11 10:00:38 +0800

    BG-1278 Fix WP URL

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,9 +3,9 @@ import { BASE_URL, ENVIRONMENT, IS_TEST } from "./env";
 export const APIs = {
   aws: "https://kpnxz5rzo2.execute-api.us-east-1.amazonaws.com",
   apes: `https://fctqkloitc.execute-api.us-east-1.amazonaws.com/${ENVIRONMENT}`,
-  wordpress: IS_TEST
-    ? "https://angelgiving-dev.10web.site/wp-json/wp/v2"
-    : "34.29.12.245",
+  wordpress: `https://${
+    IS_TEST ? "angelgiving-dev.10web.site" : "34.29.12.245"
+  }/wp-json/wp/v2`,
 };
 
 export const LITEPAPER = `${BASE_URL}/docs/litepaper-introduction/`;
