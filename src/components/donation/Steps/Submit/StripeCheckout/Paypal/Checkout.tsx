be50382714420f1ea3feece6b63ebb1aa7782c84
Nenad <nenad@better.giving>
2024-04-11 11:11:53 +0200

    Create PayPal order (donation intent) only on button click

--- src/components/donation/Steps/Submit/StripeCheckout/Paypal/Checkout.tsx
+++ src/components/donation/Steps/Submit/StripeCheckout/Paypal/Checkout.tsx
@@ -7,19 +7,17 @@ import { isEmpty } from "helpers";
 import { DonateFiatThanksState } from "pages/DonateFiatThanks";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useCapturePayPalOrderMutation } from "services/apes";
-import { DonationRecipient } from "slices/donation";
-import { DonationSource } from "types/lists";
-
-type Props = {
-  orderId: string;
-  recipient: DonationRecipient;
-  source: DonationSource;
-};
+import {
+  useCapturePayPalOrderMutation,
+  usePaypalOrderMutation,
+} from "services/apes";
+import { StripeCheckoutStep } from "slices/donation";
 
 // Code inspired by React Stripe.js docs, see:
 // https://stripe.com/docs/stripe-js/react#useelements-hook
-export default function Checkout({ orderId, recipient, source }: Props) {
+export default function Checkout(props: StripeCheckoutStep) {
+  const { details, recipient, liquidSplitPct, tip = 0, donor } = props;
+
   const navigate = useNavigate();
 
   const [isSubmitting, setSubmitting] = useState(false);
@@ -27,6 +25,8 @@ export default function Checkout({ orderId, recipient, source }: Props) {
   const [{ isPending }] = usePayPalScriptReducer();
   const [captureOrder] = useCapturePayPalOrderMutation();
   const { handleError } = useErrorContext();
+
+  const [createOrder, { isLoading }] = usePaypalOrderMutation();
 
   return (
     <>
@@ -41,54 +41,58 @@ export default function Checkout({ orderId, recipient, source }: Props) {
             height: 40,
           }}
           className="w-40 flex gap-2"
-          disabled={isSubmitting}
+          disabled={isSubmitting || isLoading}
           onCancel={() => setSubmitting(false)}
           onError={(error) => {
             setSubmitting(false);
-            handleError(error);
+            handleError(error, GENERIC_ERROR_MESSAGE);
           }}
           onApprove={async (data, actions) => {
-            try {
-              const order = await captureOrder({
-                orderId: data.orderID,
-              }).unwrap();
+            const order = await captureOrder({
+              orderId: data.orderID,
+            }).unwrap();
 
-              if ("details" in order) {
-                if (order.details[0]?.issue === "INSTRUMENT_DECLINED") {
-                  // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-                  // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-                  return actions.restart();
-                } else if ("debug_id" in order) {
-                  // (2) Other non-recoverable errors -> Show a failure message
-                  throw new Error(
-                    `${order.details[0]?.description} (${order.debug_id})`
-                  );
-                }
-              } else if (isEmpty(order.purchase_units ?? [])) {
-                throw new Error(JSON.stringify(order));
-              } else {
-                //accessed by redirect page
-                const state: DonateFiatThanksState = {
-                  guestDonor: order.guestDonor,
-                  recipientName: recipient.name,
-                };
-                if (source === "bg-widget") {
-                  navigate(
-                    `${appRoutes.donate_widget}/${donateWidgetRoutes.donate_fiat_thanks}`,
-                    { state }
-                  );
-                } else {
-                  navigate(appRoutes.donate_fiat_thanks, { state });
-                }
+            if ("details" in order) {
+              if (order.details[0]?.issue === "INSTRUMENT_DECLINED") {
+                // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+                return actions.restart();
+              } else if ("debug_id" in order) {
+                // (2) Other non-recoverable errors -> Show a failure message
+                throw new Error(
+                  `${order.details[0]?.description} (${order.debug_id})`
+                );
               }
-            } catch (error) {
-              handleError(error, GENERIC_ERROR_MESSAGE);
-            } finally {
-              setSubmitting(false);
+            } else if (isEmpty(order.purchase_units ?? [])) {
+              throw new Error(JSON.stringify(order));
+            } else {
+              //accessed by redirect page
+              const state: DonateFiatThanksState = {
+                guestDonor: order.guestDonor,
+                recipientName: recipient.name,
+              };
+              if (details.source === "bg-widget") {
+                navigate(
+                  `${appRoutes.donate_widget}/${donateWidgetRoutes.donate_fiat_thanks}`,
+                  { state }
+                );
+              } else {
+                navigate(appRoutes.donate_fiat_thanks, { state });
+              }
             }
           }}
           createOrder={async () => {
             setSubmitting(true);
+            const orderId = await createOrder({
+              amount: +details.amount,
+              tipAmount: tip,
+              usdRate: details.currency.rate,
+              currency: details.currency.code,
+              endowmentId: recipient.id,
+              splitLiq: liquidSplitPct,
+              donor,
+              source: details.source,
+            }).unwrap();
             return orderId;
           }}
         />
