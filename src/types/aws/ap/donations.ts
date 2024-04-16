e46b26f5d2121416b44dbee00dd38761c03a2210
Nenad Misic <nenad@better.giving>
2024-04-16 07:21:18 +0200

    Add third tab in the Donor's "My Donations" page that would allow them to see all payment intents explicitly (#2868)
    * Include 'intent' in DonationsQueryParams['status']
    
    * Add new 'Awaiting Payment' tab
    
    * fix overlay for tabs
    
    * Increase tab width to w-52
    
    * Make more mobile-friendly

--- src/types/aws/ap/donations.ts
+++ src/types/aws/ap/donations.ts
@@ -65,5 +65,5 @@ export type DonationsQueryParams = {
   viaId?: string;
   symbol?: string;
   asker: number | string;
-  status?: "final" | "pending";
+  status?: "final" | "pending" | "intent";
 };
