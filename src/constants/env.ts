afe4e986632fd138e8d8b65328445c1b5346b87a
Andrey <SovereignAndrey>
2024-04-22 12:03:52 +0800

    fix DAPP link to drop app if on prod

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -11,13 +11,9 @@ export const SEO_IMAGE =
 export const EMAIL_SUPPORT = "support@better.giving";
 export const APP_NAME = "Better Giving";
 export const BASE_URL = "https://better.giving";
-export const DAPP_URL = "https://app.better.giving";
+export const DAPP_URL = `https://${IS_TEST ? "staging." : ""}better.giving`;
 export const INTERCOM_HELP = "https://intercom.help/better-giving/en";
 export const AWS_S3_PUBLIC_BUCKET = "https://endow-profiles.s3.amazonaws.com";
-
-export const ENVIRONMENT = process.env.PUBLIC_ENVIRONMENT;
-
-export const IS_TEST = ENVIRONMENT === "dev";
 
 export const PUBLIC_STRIPE_KEY = process.env.PUBLIC_STRIPE_KEY || "";
 
