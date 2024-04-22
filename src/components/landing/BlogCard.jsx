407405f5cccc56587fb7419a1042921e10475c15
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-04-22 15:43:56 +0800

    blog card classes

--- src/components/landing/BlogCard.jsx
+++ src/components/landing/BlogCard.jsx
@@ -12,21 +12,21 @@ const BlogCard = ({ blog, imgUrl }) => {
   };
 
   return (
-    <div className="w-full md:w-full h-[460px] borderLine flex flex-col gap-3 pb-[20px] rounded-[18px] relative blog_card overflow-hidden bg-white font-body">
+    <div className="w-full md:w-full h-[460px] borderLine flex flex-col gap-3 pb-5 rounded-[18px] relative blog_card overflow-hidden bg-white font-body">
       <img
         src={imgUrl}
         alt="thumbnail"
-        className="rounded-t-[18px] object-fill object-center  h-[179px]"
+        className="rounded-t-[18px] object-fill object-center h-[179px]"
       />
 
-      <div className="flex gap-2 px-[24px]"></div>
+      <div className="flex gap-2 px-6"></div>
       <h4
         className="text-[#0D283A] font-semibold w-full text-xl line-clamp-2  px-[24px]"
         //biome-ignore lint: trusted html
         dangerouslySetInnerHTML={{ __html: blog.title.rendered }}
       />
       <p
-        className="text-[#0D283A] text-[14px] lg:text-[16px] md:text-[16px] line-clamp-4 px-[24px]  tracking-[.8px]  text w-full  "
+        className="text-[#0D283A] text-sm md:text-base line-clamp-4 px-6 tracking-tighter text w-full  "
         id="desc"
         //biome-ignore lint: trusted html
         dangerouslySetInnerHTML={{ __html: sanitize(blog?.excerpt?.rendered) }}
@@ -34,7 +34,7 @@ const BlogCard = ({ blog, imgUrl }) => {
 
       <a
         href={`${DAPP_URL}/blog/${blog.slug}`}
-        className="self-end mt-auto  text-[#2D89C8] px-4  py-2 rounded-full  font-semibold uppercase"
+        className="self-end mt-auto  text-blue-d1 px-4  py-2 rounded-full  font-semibold uppercase"
       >
         Read More
       </a>
