678a6e4c703e2be585752be1cd9eca332f34672c
ap-justin <justin@angelprotocol.io>
2024-04-23 18:04:02 +0800

    watch sticky instead of scroll

--- src/App/Layout.tsx
+++ src/App/Layout.tsx
@@ -1,5 +1,6 @@
 import Loader from "components/Loader";
 import Seo from "components/Seo";
+import { appRoutes } from "constants/routes";
 import ErrorBoundary from "errors/ErrorBoundary";
 import { Suspense } from "react";
 import { Outlet, useLocation } from "react-router-dom";
@@ -12,11 +13,19 @@ const { SOCIAL_MEDIA_LINKS } = CHARITY_LINKS;
 
 export default function Layout() {
   const headerLinks = useHeaderLinks();
-  const { key } = useLocation();
+  const { key, pathname } = useLocation();
+  const isHome = pathname === appRoutes.home;
   return (
-    <div className="grid grid-rows-[4rem_minmax(calc(100dvh-4rem),1fr)_auto]">
+    <div
+      className={`grid ${
+        isHome ? "" : "grid-rows-[4rem_minmax(calc(100dvh-4rem),1fr)_auto]"
+      }`}
+    >
       <Seo /> {/* Load all defaults for SEO meta tags */}
-      <Header links={headerLinks} classes="sticky top-0 z-20" />
+      <Header
+        links={headerLinks}
+        classes={`${isHome ? "mt-8" : ""} sticky z-[999] top-[-1px]`}
+      />
       <Suspense fallback={<LoaderComponent />}>
         <ErrorBoundary key={key} /** allows for recovery when changing page */>
           <Outlet />
