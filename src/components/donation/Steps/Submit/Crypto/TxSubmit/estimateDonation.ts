99a3a9b08afced1d75ee421c69d4a40c56540d76
Nenad <nenad@better.giving>
2024-04-24 11:37:05 +0200

    Rename totalAmount->grossAmount

--- src/components/donation/Steps/Submit/Crypto/TxSubmit/estimateDonation.ts
+++ src/components/donation/Steps/Submit/Crypto/TxSubmit/estimateDonation.ts
@@ -18,10 +18,10 @@ export async function estimateDonation(
   tipAmount = 0
 ): Promise<Exclude<EstimateStatus, "loading">> {
   try {
-    const totalAmount = +token.amount + tipAmount;
+    const grossAmount = +token.amount + tipAmount;
 
     const balance = await tokenBalance(token, chainID, sender);
-    if (balance < totalAmount) {
+    if (balance < grossAmount) {
       return { error: "Not enough balance" };
     }
 
@@ -31,7 +31,7 @@ export async function estimateDonation(
     switch (chainID) {
       case "juno-1":
       case "uni-6": {
-        const scaledAmount = scaleToStr(totalAmount, token.decimals);
+        const scaledAmount = scaleToStr(grossAmount, token.decimals);
         const to = apWallets.junoDeposit;
         const msg =
           token.type === "juno-native" || token.type === "ibc"
@@ -52,7 +52,7 @@ export async function estimateDonation(
 
       case "pisco-1":
       case "phoenix-1": {
-        const scaledAmount = scaleToStr(totalAmount, token.decimals);
+        const scaledAmount = scaleToStr(grossAmount, token.decimals);
         const msg =
           token.type === "terra-native" || token.type === "ibc"
             ? new MsgSend(sender, apWallets.terra, [
@@ -70,7 +70,7 @@ export async function estimateDonation(
       //evm chains
       default: {
         const tx: SimulSendNativeTx | SimulContractTx = (() => {
-          const scaledAmount = scale(totalAmount, token.decimals).toHex();
+          const scaledAmount = scale(grossAmount, token.decimals).toHex();
 
           switch (token.type) {
             case "evm-native":
