6d3a9889dae7f10346ecbe30281633501ea6f489
Andrey <SovereignAndrey>
2024-04-25 11:33:28 +0800

    placeholder static pages

---
+++ src/pages/Informational/AboutUs.tsx
@@ -0,0 +1,13 @@
+// import ExtLink from "components/ExtLink";
+// import { appRoutes } from "constants/routes";
+// import { Link } from "react-router-dom";
+
+export default function AboutUs() {
+  return (
+    <div className="padded-container min-h-screen pb-6">
+      <h1 className="font-bold uppercase col-span-full text-2xl lg:text-3xl break-words mt-6">
+        About Us
+      </h1>
+    </div>
+  );
+}
