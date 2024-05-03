4b4f886b7787f9061a47aa3964d9b487cc1f09da
ap-justin <justin@angelprotocol.io>
2024-05-03 21:39:46 +0800

    brands content

--- src/pages/Home/Brands.tsx
+++ src/pages/Home/Brands.tsx
@@ -1,10 +1,4 @@
-import brand1 from "assets/landing/brand_1.svg";
-import brand2 from "assets/landing/brand_2.svg";
-import brand3 from "assets/landing/brand_3.svg";
-import brand4 from "assets/landing/brand_4.svg";
-import brand5 from "assets/landing/brand_5.svg";
-import brand6 from "assets/landing/brand_6.svg";
-import brand7 from "assets/landing/brand_7.svg";
+import { brands } from "content/brands";
 import Marquee from "react-fast-marquee";
 
 const Brands = () => {
@@ -13,25 +7,17 @@ const Brands = () => {
       <h2 className="text-[32px] md:text-[42px] text-navy-d4 px-8 text-center max-w-lg mx-auto text-balance">
         Trusted by impactful nonprofits worldwide
       </h2>
-      <Marquee pauseOnHover className="h-max">
-        {[
-          brand1,
-          brand2,
-          brand3,
-          brand4,
-          brand5,
-          brand6,
-          brand7,
-          brand1,
-          brand2,
-          brand3,
-          brand4,
-          brand5,
-          brand6,
-          brand7,
-        ].map((b, idx) => (
-          <img key={idx} src={b} className="" alt="brand img" />
-        ))}
+      <Marquee
+        pauseOnHover
+        className="[&_.rfm-initial-child-container]:gap-x-12"
+      >
+        {brands
+          .concat(brands)
+          .concat(brands)
+          .concat(brands)
+          .map((b, idx) => (
+            <img key={idx} src={b} height={80} alt="brand img" />
+          ))}
       </Marquee>
     </section>
   );
