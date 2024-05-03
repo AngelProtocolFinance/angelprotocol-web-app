5a750f70fae280494c7f319590f747a060ba86ed
ap-justin <justin@angelprotocol.io>
2024-05-03 22:00:22 +0800

    extract benefits content

--- src/pages/Home/Benefits/Path.tsx
+++ src/pages/Home/Benefits/Path.tsx
@@ -1,82 +1,13 @@
+import { benefits } from "content/benefits";
 import { useState } from "react";
 // import Button from "../common/Button";
 import BenefitsCarousel from "./BenefitsCarousel";
 import Carousel from "./Carousel";
 import s from "./benefits.module.css";
-import icon1 from "./icons/Icon1_wb.webp";
-import icon2 from "./icons/Icon2_wb.webp";
-import icon3 from "./icons/Icon3_wb.webp";
-import icon4 from "./icons/Icon4_wb.webp";
-import icon5 from "./icons/Icon5_wb.webp";
-import icon6 from "./icons/Icon6_wb.webp";
-import icon7 from "./icons/Icon7_wb.webp";
-import icon8 from "./icons/Icon8_wb.webp";
-import type { Slide } from "./types";
 
-const colors = ["bg-[#EDFCE2]", "bg-lilac", "bg-[#FCF6E2]", "bg-[#EDF2FE]"];
-type TPath = "nonprofits" | "donors";
+type TPath = keyof typeof benefits;
 const Path = () => {
   const [path, setPath] = useState<TPath>("donors");
-  const nonProfits: Slide[] = [
-    {
-      title: "Sustainable Impact",
-      description:
-        "Your donation grows through our nonprofit Sustainability Fund. Give the gift that keeps on giving.",
-      img_src: icon1,
-      bgColorClass: colors[0],
-    },
-    {
-      title: "The World At Your Fingertips",
-      description:
-        "Donate to any nonprofit, anywhere in the world. Support local grassroots organizations who understand the problems on the ground",
-      img_src: icon2,
-      bgColorClass: colors[1],
-    },
-    {
-      title: "Receive and Track Tax Benefits",
-      description:
-        "Get automated receipts to claim deductions and eliminate capital gains tax when donating property such as stocks and crypto",
-      img_src: icon3,
-      bgColorClass: colors[2],
-    },
-    {
-      title: "Your Donation, Your Way",
-      description:
-        "Use the giving method that works for you - from check, card and bank transfers to stock, DAF and crypto",
-      img_src: icon4,
-      bgColorClass: colors[3],
-    },
-  ];
-  const donors: Slide[] = [
-    {
-      title: "Expanded Funding Channels",
-      description:
-        "Tap into new sources of funding by accepting cash, crypto, stocks, donor funds, and more.",
-      img_src: icon5,
-      bgColorClass: colors[0],
-    },
-    {
-      title: "Recurring Revenue Streams",
-      description:
-        "Get perpetual funding as your donor gifts grow inside our Sustainability Fund's balanced investment portfolio",
-      img_src: icon6,
-      bgColorClass: colors[1],
-    },
-    {
-      title: "Simplified Donation Management",
-      description:
-        "Forget the admin hassle. We handle the complexity of processing diverse donations, issuing automated tax receipts, and consolidating donor data",
-      img_src: icon7,
-      bgColorClass: colors[2],
-    },
-    {
-      title: "Global Reach and Fiscal Sponsorship",
-      description:
-        "Unlock new worldwide donor pipelines and accept cross-border gifts through fiscal sponsorship. Expand your global community.",
-      img_src: icon8,
-      bgColorClass: colors[3],
-    },
-  ];
 
   return (
     <section
@@ -116,14 +47,8 @@ const Path = () => {
           : "Join our global community and amplify your charitable giving."}
       </p>
 
-      <BenefitsCarousel
-        slides={path === "nonprofits" ? nonProfits : donors}
-        classes="max-lg:hidden"
-      />
-      <Carousel
-        slides={path === "nonprofits" ? nonProfits : donors}
-        classes="lg:hidden"
-      />
+      <BenefitsCarousel slides={benefits[path]} classes="max-lg:hidden" />
+      <Carousel slides={benefits[path]} classes="lg:hidden" />
       {/* TODO: enable once we have the static pages ready */}
       {/*<Button text="Learn More" />*/}
     </section>
--- src/pages/informational/NonprofitInfo/NonprofitInfo.tsx
+++ src/pages/informational/NonprofitInfo/NonprofitInfo.tsx
@@ -1,11 +1,13 @@
 import DonationFormInfo from "./DonationFormInfo";
+import FellowNonProfits from "./FellowNonProfits";
 import Hero from "./Hero";
 import WhyBG from "./WhyBG";
 
 export default function Nonprofits() {
   return (
-    <div className="w-full grid content-start pb-16 padded-container @container">
-      <Hero />
+    <div className="w-full grid content-start pb-16 @container">
+      <Hero className="padded-container" />
+      <FellowNonProfits className="my-20" />
       <DonationFormInfo className="mt-28" />
       <WhyBG className="mt-40" />
     </div>
