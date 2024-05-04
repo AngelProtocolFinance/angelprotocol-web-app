739a045de178324113fd13f6e5506ec36814205e
ap-justin <justin@angelprotocol.io>
2024-05-04 16:22:30 +0800

    move testimonial content

--- src/pages/Home/Testimonials/TestimonialCard.tsx
+++ src/pages/Home/Testimonials/TestimonialCard.tsx
@@ -1,19 +1,19 @@
-import type { Testimonial } from "./types";
+import type { Testimonial } from "content/testimonials";
 
 const TestimonialCard = (props: Testimonial) => {
   return (
     <div className="@container">
       <img
-        src={props.reviewer_logo}
+        src={props.reviewer_org_logo}
         alt="logo"
         className="object-contain size-28 rounded-lg ml-8 bg-white shadow-xl"
       />
       <div className="relative -mt-14 pt-24 px-8 pb-8 rounded-3xl bg-white -z-10">
-        <p className="text-lg @md:h-64 mt-8">{props.review}</p>
+        <p className="text-lg @md:h-64 mt-8">{props.content}</p>
         <p className="mt-4 @md:mt-0 text-lg opacity-90 font-bold">
           {props.reviewer}
         </p>
-        <p className="md:text-lg font-medium">{props.reviewer_profession}</p>
+        <p className="md:text-lg font-medium">{props.reviewer_org_role}</p>
       </div>
     </div>
   );
