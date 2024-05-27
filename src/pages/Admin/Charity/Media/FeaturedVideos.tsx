7fb76c04f1c4f033a1c15b784e531bfd3224d69b
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-05-27 23:16:20 +0800

    edit pros

--- src/pages/Admin/Charity/Media/FeaturedVideos.tsx
+++ src/pages/Admin/Charity/Media/FeaturedVideos.tsx
@@ -1,11 +1,17 @@
 import QueryLoader from "components/QueryLoader";
 import { usePaginatedMedia } from "services/aws/usePaginatedMedia";
 
-export default function FeaturedVideos({ endowId }: { endowId: number }) {
+type Props = {
+  classes?: string;
+  endowId: number;
+};
+
+export default function FeaturedVideos({ endowId, classes = "" }: Props) {
   const query = usePaginatedMedia(endowId);
   return (
     <QueryLoader
       queryState={query}
+      classes={{ container: classes }}
       messages={{ loading: "loading...", error: "failed to get videos" }}
     >
       {(data) => <>{data.items.length}</>}
