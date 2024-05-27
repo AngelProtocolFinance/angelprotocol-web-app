aa2df9ccb9494db3213971d508a613cdb4c96e22
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-05-27 10:59:53 +0800

    BG-1369, BG-1371: website update (#3022)
    * add links to header
    
    * add explore all link
    
    * click first button
    
    * text center

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -1,11 +1,12 @@
+import Icon from "components/Icon";
 import { DappLogo } from "components/Image";
 import { appRoutes } from "constants/routes";
-import { useLocation } from "react-router-dom";
-import type { Link } from "../types";
+import { Link, useLocation } from "react-router-dom";
+import type { Link as TLink } from "../types";
 import NavDropdown from "./NavDropdown";
 import UserMenu from "./UserMenu";
 
-type Props = { links: Link[]; classes?: string };
+type Props = { links: TLink[]; classes?: string };
 
 export default function Header({ links, classes }: Props) {
   const location = useLocation();
@@ -27,8 +28,17 @@ export default function Header({ links, classes }: Props) {
         observer.observe(node);
       }}
     >
-      <div className="grid items-center grid-cols-2 gap-4 padded-container bg-white rounded-full py-2">
+      <div className="grid relative items-center grid-cols-2 gap-4 padded-container bg-white rounded-full py-2">
         <DappLogo classes="w-48 h-12" />
+        {location.pathname !== appRoutes.marketplace && (
+          <Link
+            to={appRoutes.marketplace}
+            className="max-md:hidden absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 btn-blue capitalize items-center px-4 py-1 text-sm gap-1 rounded-full font-heading"
+          >
+            <Icon type="Search" className="mr-1 text-lg" />
+            <span>Explore all causes</span>
+          </Link>
+        )}
         <div className="flex gap-2 md:gap-4 justify-self-end items-center">
           {!(
             location.pathname === appRoutes.signin ||
