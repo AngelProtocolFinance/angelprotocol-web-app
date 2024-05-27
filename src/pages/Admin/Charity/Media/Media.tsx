d0f97ff5e68e4410cf61f5a650454129e21e2827
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-05-27 17:34:22 +0800

    show modal

--- src/pages/Admin/Charity/Media/Media.tsx
+++ src/pages/Admin/Charity/Media/Media.tsx
@@ -1,13 +1,20 @@
 import Icon from "components/Icon";
+import { useModalContext } from "contexts/ModalContext";
+import VideoAdder from "./VideoAdder";
 
 export default function Media() {
+  const { showModal } = useModalContext();
   return (
     <div className="grid content-start gap-y-6 @lg:gap-y-8 @container">
       <h3 className="text-[2rem]">Media</h3>
       <div className="border border-gray-l4 rounded p-8">
         <div className="flex justify-between items-center">
           <h4 className="text-2xl">Videos</h4>
-          <button className="btn-outline-filled text-sm px-8 py-2 gap-2">
+          <button
+            onClick={() => showModal(VideoAdder, {})}
+            type="button"
+            className="btn-outline-filled text-sm px-8 py-2 gap-1"
+          >
             <Icon type="Plus" />
             <span>add video</span>
           </button>
--- src/pages/Admin/Charity/Media/VideoAdder.tsx
+++ src/pages/Admin/Charity/Media/VideoAdder.tsx
@@ -4,24 +4,29 @@ import Modal from "components/Modal";
 import { Field, Form } from "components/form";
 import { useModalContext } from "contexts/ModalContext";
 import { type UseFormReturn, useForm } from "react-hook-form";
-import { object, string } from "yup";
+import { url } from "schemas/string";
+import { object } from "yup";
 
 export default function VideoAdder() {
   const { closeModal, isDismissible } = useModalContext();
 
   const methods = useForm({
-    resolver: yupResolver(
-      object({ url: string().required().url("invalid url") })
-    ),
+    resolver: yupResolver(object({ url: url.required("required") })),
     defaultValues: {
       url: "",
     },
   });
 
   type FV = typeof methods extends UseFormReturn<infer U> ? U : never;
+  const { handleSubmit } = methods;
 
   return (
-    <Modal className="fixed-center z-10 grid text-navy-d4 dark:text-white bg-white dark:bg-blue-d4 sm:w-full w-[90vw] sm:max-w-lg rounded overflow-hidden">
+    <Modal
+      onSubmit={handleSubmit((fv) => console.log(fv.url))}
+      as={Form}
+      methods={methods}
+      className="fixed-center z-10 grid text-navy-d4 dark:text-white bg-white dark:bg-blue-d4 sm:w-full w-[90vw] sm:max-w-lg rounded overflow-hidden"
+    >
       <div className="relative">
         <p className="text-xl font-bold text-center border-b bg-blue-l5 dark:bg-blue-d7 border-gray-l4 p-5">
           Add video
@@ -35,21 +40,27 @@ export default function VideoAdder() {
           </button>
         )}
       </div>
-      <Form methods={methods} className="">
-        <Field<FV> name="url" label="Web Address (URL)" required />
-        <div className="p-3 sm:px-8 sm:py-4 flex items-center justify-end gap-4 w-full text-center sm:text-right bg-blue-l4 dark:bg-blue-d7 border-t border-gray-l4">
-          <button
-            type="button"
-            className="btn-outline-filled text-sm px-8 py-2"
-            onClick={closeModal}
-          >
-            Cancel
-          </button>
-          <button type="submit" className="btn-blue px-8 py-2 text-sm">
-            Continue
-          </button>
-        </div>
-      </Form>
+      <div className="p-4">
+        <Field<FV>
+          placeholder="e.g. https://youtu.be/XOUjJqQ68Ec?si=-WX60lgPXUWAXPCY"
+          name="url"
+          label="Web Address (URL)"
+          required
+        />
+      </div>
+
+      <div className="mt-4 p-3 sm:px-8 sm:py-4 flex items-center justify-end gap-4 w-full text-center sm:text-right bg-blue-l4 dark:bg-blue-d7 border-t border-gray-l4">
+        <button
+          type="button"
+          className="btn-outline-filled text-sm px-8 py-2"
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+        <button type="submit" className="btn-blue px-8 py-2 text-sm">
+          Continue
+        </button>
+      </div>
     </Modal>
   );
 }
