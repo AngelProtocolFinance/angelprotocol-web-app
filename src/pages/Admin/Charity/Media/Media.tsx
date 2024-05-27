782aae428c8db5f261400890109e33c42c0a5850
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-05-27 16:19:25 +0800

    container and header

--- src/pages/Admin/Charity/Media/Media.tsx
+++ src/pages/Admin/Charity/Media/Media.tsx
@@ -1,3 +1,7 @@
 export default function Media() {
-  return <div>media page</div>;
+  return (
+    <div className="grid content-start gap-y-6 @lg:gap-y-8 @container">
+      <h3 className="text-[2rem]">Media</h3>
+    </div>
+  );
 }
