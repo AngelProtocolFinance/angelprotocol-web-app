2da73d313d61b9ff3660fe587dbab3355187be47
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-05-28 15:43:00 +0800

    responsive video

--- src/pages/Admin/Charity/Media/VideoPreview.tsx
+++ src/pages/Admin/Charity/Media/VideoPreview.tsx
@@ -49,14 +49,22 @@ export default function VideoPreview(props: Media) {
         </CRUDBtn>
       </div>
       {/** render only thumbnails on lists */}
-      <ReactPlayer
-        style={{ pointerEvents: "none" }}
-        url={props.url}
-        width={320}
-        height={180}
-        light
-        playIcon={<></>}
-      />
+      {/** @see https://github.com/CookPete/react-player/issues/145 */}
+      <div className="relative pt-[56.25%] aspect-[16/9] rounded-lg overflow-clip">
+        <ReactPlayer
+          style={{
+            pointerEvents: "none",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+          url={props.url}
+          width="100%"
+          height="100%"
+          light
+          playIcon={<></>}
+        />
+      </div>
     </div>
   );
 }
