4ddcb045dd1d3a675c101ff6fa2b80f4f18d3f3b
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2024-06-06 08:55:18 +0800

    BG-1383 snippet copy change

--- src/pages/Widget/Configurer/Configurer.tsx
+++ src/pages/Widget/Configurer/Configurer.tsx
@@ -165,7 +165,7 @@ export default function Configurer({ classes = "", config, setConfig }: Props) {
             type="submit"
             className="btn-blue @max-sm/configurer:mx-auto w-40"
           >
-            Update Snippet
+            Update Form
           </button>
         </div>
       </div>
