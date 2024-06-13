813a5227539e04550fad5dc12ea92e0fdc992e3c
SovereignAndrey <85138450+SovereignAndrey@users.noreply.github.com>
2024-06-13 11:02:02 +0800

    WP API updated

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,7 +3,7 @@ import { BASE_URL, ENVIRONMENT } from "./env";
 export const APIs = {
   aws: "https://ap-api.better.giving",
   apes: `https://apes-api.better.giving/${ENVIRONMENT}`,
-  wordpress: `https://wp-blog.better.giving/wp-json/wp/v2`,
+  wordpress: "https://wp-api.better.giving",
 };
 
 export const LITEPAPER = `${BASE_URL}/docs/litepaper-introduction/`;
