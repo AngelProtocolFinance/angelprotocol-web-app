e39680b5ac19b603bb7c2541747d452295235581
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-06-25 08:57:28 +0800

    Fix token selector search and updates (#3091)
    * combobox inside popover
    
    * search icon on right
    
    * rename interface

--- src/components/TokenField/TokenOptions.tsx
+++ src/components/TokenField/TokenOptions.tsx
@@ -1,9 +1,17 @@
-import { ComboboxOption, ComboboxOptions } from "@headlessui/react";
+import {
+  CloseButton,
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  PopoverPanel,
+} from "@headlessui/react";
 import { chains, juno, kujira, stargaze, terraMainnet } from "constants/chains";
 import { useModalContext } from "contexts/ModalContext";
 import { isEmpty } from "helpers";
 import { useState } from "react";
 import { useTokensQuery } from "services/apes";
+import type { Token } from "types/aws";
 import type { ChainID } from "types/chain";
 import type { TokenWithAmount } from "types/tx";
 import Icon from "../Icon";
@@ -14,16 +22,17 @@ import TokenSearch from "./TokenSearch";
 type Props = {
   selectedChainId: ChainID;
   onChange: (token: TokenWithAmount) => void;
+  token: TokenWithAmount;
   classes?: string;
 };
 
 const container =
-  "w-max border border-gray-l4 p-1 [--anchor-max-height:13rem] w-max overflow-y-auto rounded-md bg-gray-l5 dark:bg-blue-d7 shadow-lg focus:outline-none";
-export default function TokenOptions({ selectedChainId, onChange }: Props) {
-  const [searchText, setSearchText] = useState("");
-
-  const { showModal } = useModalContext();
-
+  "w-56 border border-gray-l4 p-1 [--anchor-max-height:13rem] overflow-y-auto rounded-md bg-gray-l5 dark:bg-blue-d7 shadow-lg focus:outline-none";
+export default function TokenOptions({
+  selectedChainId,
+  onChange,
+  token,
+}: Props) {
   const {
     data: tokens = [],
     isLoading,
@@ -31,6 +40,51 @@ export default function TokenOptions({ selectedChainId, onChange }: Props) {
     isError,
   } = useTokensQuery(selectedChainId);
 
+  if (isLoading || isFetching) {
+    return (
+      <PopoverPanel anchor="bottom end" className={container}>
+        <LoadingStatus classes="text-sm text-navy-d4 dark:text-navy-l2 p-2">
+          Loading..
+        </LoadingStatus>
+      </PopoverPanel>
+    );
+  }
+
+  if (isError) {
+    return (
+      <PopoverPanel anchor="bottom end" className={container}>
+        <ErrorStatus classes="text-sm p-2">Failed to load tokens</ErrorStatus>
+      </PopoverPanel>
+    );
+  }
+
+  return (
+    <PopoverPanel
+      anchor={{ to: "bottom end", gap: 8, offset: 20 }}
+      className={container}
+    >
+      <TokenCombobox
+        token={token}
+        tokens={tokens}
+        coingeckoPlatformId={chains[selectedChainId].coingeckoPlatformId}
+        onChange={onChange}
+      />
+    </PopoverPanel>
+  );
+}
+
+interface ITokenCombobox extends Pick<Props, "onChange" | "token"> {
+  tokens: Token[];
+  coingeckoPlatformId: string | null;
+}
+function TokenCombobox({
+  token,
+  tokens,
+  coingeckoPlatformId,
+  onChange,
+}: ITokenCombobox) {
+  const { showModal } = useModalContext();
+  const [searchText, setSearchText] = useState("");
   const searchResult =
     searchText === ""
       ? tokens
@@ -38,57 +92,38 @@ export default function TokenOptions({ selectedChainId, onChange }: Props) {
           return t.symbol.toLowerCase().includes(searchText.toLowerCase());
         });
 
-  if (isLoading || isFetching) {
-    return (
-      <ComboboxOptions anchor="bottom" className={container}>
-        <LoadingStatus classes="text-sm text-navy-d4 dark:text-navy-l2 p-2">
-          Loading..
-        </LoadingStatus>
-      </ComboboxOptions>
-    );
-  }
-
-  if (isError) {
-    return (
-      <ComboboxOptions anchor="bottom" className={container}>
-        <ErrorStatus classes="text-sm p-2">Failed to load tokens</ErrorStatus>
-      </ComboboxOptions>
-    );
-  }
-
-  const coingeckoPlatformId = chains[selectedChainId].coingeckoPlatformId;
-
   return (
-    <ComboboxOptions
-      anchor={{ to: "bottom", gap: 10 }}
-      className={`${container} scroller`}
-    >
-      <div className="flex p-2 gap-2 border border-gray-l4 rounded mb-1">
-        <Icon type="Search" size={20} />
-        <input
+    <Combobox value={token} onChange={onChange}>
+      <div className="grid grid-cols-[1fr_auto] p-2 gap-2 rounded mb-1 border border-gray-l4">
+        <ComboboxInput
+          value={searchText}
           placeholder="Search..."
           aria-disabled={tokens.length < 1}
           className="text-left text-sm focus:outline-none bg-transparent w-20"
           onChange={(event) => setSearchText(event.target.value)}
         />
+        <Icon type="Search" size={20} />
       </div>
       {isEmpty(searchResult) && searchText !== "" ? (
         <div className="relative cursor-default select-none py-2 px-4 text-sm">
           {searchText} not found
         </div>
       ) : (
-        searchResult.map((token) => (
-          <ComboboxOption
-            key={token.token_id + token.type}
-            className={
-              "flex items-center gap-2 p-3 hover:bg-[--accent-secondary] cursor-pointer"
-            }
-            value={{ ...token, amount: "0" }}
-          >
-            <Image src={token.logo} className="w-6 h-6" />
-            <span className="text-sm">{token.symbol}</span>
-          </ComboboxOption>
-        ))
+        <ComboboxOptions static>
+          {searchResult.map((token) => (
+            <CloseButton
+              as={ComboboxOption}
+              key={token.token_id + token.type}
+              className={
+                "flex items-center gap-2 p-3 hover:bg-[--accent-secondary] cursor-pointer"
+              }
+              value={{ ...token, amount: "0" }}
+            >
+              <Image src={token.logo} className="w-6 h-6" />
+              <span className="text-sm">{token.symbol}</span>
+            </CloseButton>
+          ))}
+        </ComboboxOptions>
       )}
       {coingeckoPlatformId && (
         <button
@@ -123,6 +158,6 @@ export default function TokenOptions({ selectedChainId, onChange }: Props) {
           Not listed?
         </button>
       )}
-    </ComboboxOptions>
+    </Combobox>
   );
 }
