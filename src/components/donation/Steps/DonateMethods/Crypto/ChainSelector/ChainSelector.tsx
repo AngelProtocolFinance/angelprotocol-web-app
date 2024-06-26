477cd28ca0761c15c2d31b8c8846f28d96c83506
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-06-26 13:23:45 +0800

    Network dropdown bug fixes (#3106)
    * add immediate to open on focus
    
    * remove hidden input

--- src/components/donation/Steps/DonateMethods/Crypto/ChainSelector/ChainSelector.tsx
+++ src/components/donation/Steps/DonateMethods/Crypto/ChainSelector/ChainSelector.tsx
@@ -1,6 +1,5 @@
 import { Combobox, ComboboxButton, ComboboxInput } from "@headlessui/react";
 import { DrawerIcon } from "components/Icon";
-import { FocusableInput } from "components/Selector";
 import { forwardRef, useState } from "react";
 import { Options } from "./Options";
 import type { Chain } from "./types";
@@ -19,14 +18,15 @@ export const ChainSelector = forwardRef<HTMLInputElement, Props>(
     return (
       <Combobox
         disabled={props.disabled}
+        immediate
         value={props.value}
         onChange={(x) => x && props.onChange(x)}
         as="div"
         by="name"
         className="relative items-center flex w-full field-container field-container-donate min-h-[3rem] bg-white dark:bg-blue-d6"
       >
-        <FocusableInput ref={ref} />
         <ComboboxInput
+          ref={ref}
           placeholder="Search network"
           onChange={(event) => setSearchText(event.target.value)}
           displayValue={(value: Chain) => value.name}
