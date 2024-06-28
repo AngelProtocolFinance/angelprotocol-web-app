a7603f97879869ab0d4a6b6916d7b915a006215b
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-06-28 18:09:46 +0800

    processing fees

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -3,3 +3,13 @@ import { EMAIL_SUPPORT } from "./env";
 export const GENERIC_ERROR_MESSAGE = `An unexpected error occurred and has been reported. Please get in touch with ${EMAIL_SUPPORT} if the problem persists.`;
 
 export const BYTES_IN_MB = 1e6;
+
+export const PROCESSING_FEES = {
+  stripe: 0.024,
+  crypto: {
+    ethereum: 0.02,
+    evm: 0.01,
+    cosmos: 0.01,
+    others: 0.015,
+  },
+};
