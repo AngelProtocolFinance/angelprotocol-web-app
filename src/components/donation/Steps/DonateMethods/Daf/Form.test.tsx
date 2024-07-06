9b48633ab217adf1b8e45c689d8a253787422f1c
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-06 12:42:22 +0800

    DAF form test

---
+++ src/components/donation/Steps/DonateMethods/Daf/Form.test.tsx
@@ -0,0 +1,104 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { store } from "store/store";
+import { describe, expect, test, vi } from "vitest";
+import { DEFAULT_PROGRAM, USD_CODE } from "../../common/constants";
+import type { DafDonationDetails, Init } from "../../types";
+import Form from "./Form";
+
+const mockedSetState = vi.hoisted(() => vi.fn());
+vi.mock("../../Context", () => ({
+  useDonationState: vi
+    .fn()
+    .mockReturnValue({ state: {}, setState: mockedSetState }),
+}));
+
+const _Form: typeof Form = (props) => (
+  <Provider store={store}>{<Form {...props} />}</Provider>
+);
+
+describe("DAF form test", () => {
+  test("initial state: blank", () => {
+    const init: Init = {
+      source: "bg-marketplace",
+      config: { liquidSplitPct: 50, splitDisabled: false },
+      recipient: { id: 0, name: "" },
+      mode: "live",
+    };
+
+    render(<_Form init={init} step="donate-form" />);
+    const currencySelector = screen.getByRole("combobox");
+    expect(currencySelector).toHaveDisplayValue(/usd/i);
+
+    const amountInput = screen.getByPlaceholderText(/enter amount/i);
+    expect(amountInput).toHaveDisplayValue("");
+  });
+
+  test("initial blank state: program donations now allowed", () => {
+    const init: Init = {
+      source: "bg-marketplace",
+      config: { liquidSplitPct: 50, splitDisabled: false },
+      recipient: { id: 0, name: "", progDonationsAllowed: false },
+      mode: "live",
+    };
+    render(<_Form init={init} step="donate-form" />);
+    const programSelector = screen.queryByRole("button", {
+      name: /general donation/i,
+    });
+    expect(programSelector).toBeNull();
+  });
+
+  test("initial state: persisted and submittable", async () => {
+    const init: Init = {
+      source: "bg-marketplace",
+      config: { liquidSplitPct: 50, splitDisabled: false },
+      recipient: { id: 0, name: "" },
+      mode: "live",
+    };
+    const details: DafDonationDetails = {
+      method: "daf",
+      amount: "100",
+      currency: { code: USD_CODE, rate: 1, min: 1 },
+      program: DEFAULT_PROGRAM,
+    };
+    render(<_Form init={init} step="donate-form" details={details} />);
+    const amountInput = screen.getByPlaceholderText(/enter amount/i);
+    expect(amountInput).toHaveDisplayValue("100");
+
+    const continueBtn = screen.getByRole("button", { name: /continue/i });
+    await userEvent.click(continueBtn);
+    expect(mockedSetState).toHaveBeenCalledOnce();
+    mockedSetState.mockReset();
+  });
+  test("user encounters validation errors and corrects them", async () => {
+    const init: Init = {
+      source: "bg-marketplace",
+      config: { liquidSplitPct: 50, splitDisabled: false },
+      recipient: { id: 0, name: "", progDonationsAllowed: false },
+      mode: "live",
+    };
+    render(<_Form init={init} step="donate-form" />);
+    const continueBtn = screen.getByRole("button", { name: /continue/i });
+    await userEvent.click(continueBtn);
+
+    expect(screen.getByText(/please enter an amount/i)).toBeInTheDocument();
+
+    const amountInput = screen.getByPlaceholderText(/enter amount/i);
+    expect(amountInput).toHaveFocus();
+
+    //user inputs amount
+    await userEvent.type(amountInput, "abc");
+    expect(screen.getByText(/must be a number/i)).toBeInTheDocument();
+
+    await userEvent.clear(amountInput);
+    await userEvent.type(amountInput, "-5");
+    expect(screen.getByText(/must be greater than 0/i)).toBeInTheDocument();
+
+    await userEvent.clear(amountInput);
+    await userEvent.type(amountInput, "10");
+
+    await userEvent.click(continueBtn);
+    expect(mockedSetState).toHaveBeenCalledOnce();
+  });
+});
