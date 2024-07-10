f22f5b1411f940724b13d8eb49f8b770b4415fdc
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-10 18:17:23 +0800

    update words

--- src/components/DonationTerms.tsx
+++ src/components/DonationTerms.tsx
@@ -10,15 +10,14 @@ interface Props {
 export function DonationTerms({ classes = "", endowName, Link: A }: Props) {
   return (
     <p className={classes}>
-      By making a donation to {APP_NAME}, you agree to our{" "}
+      100% of your donation is tax-deductible to the extent allowed by US law.
+      Your donation is made to {APP_NAME}, a tax-exempt US 501(c)(3) charity
+      that grants unrestricted funds to {endowName} on your behalf. As a legal
+      matter, {APP_NAME} must provide any donations to {endowName} on an
+      unrestricted basis, regardless of any designations or restrictions made by
+      you. By making a donation to {APP_NAME}, you agree to our{" "}
       <A href={TERMS_OF_USE_DONOR}>Terms of Service</A>,{" "}
-      <A href={PRIVACY_POLICY}>Privacy Policy</A>. 100% of your donation is
-      tax-deductible to the extent allowed by US law. Your donation is made to{" "}
-      {APP_NAME}, a tax-exempt US 501(c)(3) charity that grants unrestricted
-      funds to {endowName} on your behalf. As a legal matter, {APP_NAME} must
-      provide any donations to {endowName} on an unrestricted basis, regardless
-      of any designations or restrictions made by you.{" "}
-      <A href={TERMS_OF_USE_DONOR}>See Terms.</A>
+      <A href={PRIVACY_POLICY}>Privacy Policy</A>.
     </p>
   );
 }
