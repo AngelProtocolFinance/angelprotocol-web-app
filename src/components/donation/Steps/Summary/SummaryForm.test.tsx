e562176028f54f592c2a37a4a2df741136ed10a0
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-12 12:38:23 +0800

    done tribute fields

--- src/components/donation/Steps/Summary/SummaryForm.test.tsx
+++ src/components/donation/Steps/Summary/SummaryForm.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, expectTypeOf, test, vi } from "vitest";
 import type { FormDonor, Honorary } from "../types";
 import SummaryForm, { type Props } from "./SummaryForm";
 
@@ -70,13 +70,10 @@ describe("summary form: expandable fields", async () => {
       <SummaryForm
         {...props}
         donor={{
+          ...donor,
           firstName: "first",
           lastName: "last",
           email: "donor@mail.com",
-          ukTaxResident: false,
-          title: { label: "Select", value: "" },
-          zipCode: "",
-          streetAddress: "",
         }}
       />
     );
@@ -102,10 +99,91 @@ describe("summary form: expandable fields", async () => {
     await userEvent.click(checkoutBtn);
     expect(mockOnSubmit).not.toHaveBeenCalled();
     expect(houseNumberInput).toHaveAccessibleErrorMessage(/required/i);
+    expect(houseNumberInput).toHaveFocus();
     expect(postalCodeInput).toHaveAccessibleErrorMessage(/required/i);
 
     await userEvent.type(houseNumberInput, "221B Baker Street");
     await userEvent.type(postalCodeInput, "2000");
+    await userEvent.click(checkoutBtn);
+    expect(mockOnSubmit).toHaveBeenCalledOnce();
+    mockOnSubmit.mockClear();
+  });
+
+  test("dedication fields", async () => {
+    render(
+      <SummaryForm
+        {...props}
+        donor={{
+          ...donor,
+          firstName: "first",
+          lastName: "last",
+          email: "donor@mail.com",
+        }}
+      />
+    );
+
+    //form is good for submission nonetheless
+    const checkoutBtn = screen.getByRole("button", { name: /checkout/i });
+    await userEvent.click(checkoutBtn);
+    expect(mockOnSubmit).toHaveBeenCalledOnce();
+    mockOnSubmit.mockClear();
+
+    //user intends to add honorary
+    const withHonoraryCheckField =
+      screen.getByLabelText(/dedicate my donation/i);
+    await userEvent.click(withHonoraryCheckField);
+    const honoreeInput = screen.getByLabelText(/honoree's name/i);
+    const tributeNotifCheckbox = screen.getByLabelText(
+      /notify someone about this tribute/i
+    );
+    expect(honoreeInput).toBeInTheDocument();
+    expect(tributeNotifCheckbox).toBeInTheDocument();
+
+    //user needs to fill out honoree's name
+    await userEvent.click(checkoutBtn);
+    expect(mockOnSubmit).not.toHaveBeenCalled();
+    expect(honoreeInput).toHaveAccessibleErrorMessage(/required/i);
+    expect(honoreeInput).toHaveFocus();
+
+    //user inputs honoree's name
+    await userEvent.type(honoreeInput, "first last");
+
+    //form is good for submission
+    await userEvent.click(checkoutBtn);
+    expect(mockOnSubmit).toHaveBeenCalledOnce();
+    mockOnSubmit.mockClear();
+
+    //user intends to notify someone of the honorary
+    await userEvent.click(tributeNotifCheckbox);
+    const recipientNameInput = screen.getByLabelText(/recipient name/i);
+    const emailAddrInput = screen.getByLabelText(/email address/i);
+    const msgInput = screen.getByLabelText(/custom message/i);
+
+    expect(recipientNameInput).toBeInTheDocument();
+    expect(emailAddrInput).toBeInTheDocument();
+    expect(msgInput).toBeInTheDocument();
+
+    //user needs to fill-out additional information
+    await userEvent.click(checkoutBtn);
+    expect(mockOnSubmit).not.toHaveBeenCalled();
+    expect(recipientNameInput).toHaveAccessibleErrorMessage(/required/i);
+    expect(recipientNameInput).toHaveFocus();
+    expect(emailAddrInput).toHaveAccessibleErrorMessage(/required/i);
+
+    //user populates additional fields
+    await userEvent.type(recipientNameInput, "first last");
+    expect(recipientNameInput).not.toHaveAccessibleErrorMessage();
+
+    //wrong email
+    await userEvent.type(emailAddrInput, "email");
+    expect(emailAddrInput).toHaveAccessibleErrorMessage(/invalid email/i);
+
+    //user corrects email
+    await userEvent.clear(emailAddrInput);
+    await userEvent.type(emailAddrInput, "mail@mail.com");
+    expect(emailAddrInput).not.toHaveAccessibleErrorMessage();
+
+    //form is good for submission
     await userEvent.click(checkoutBtn);
     expect(mockOnSubmit).toHaveBeenCalledOnce();
     mockOnSubmit.mockClear();
