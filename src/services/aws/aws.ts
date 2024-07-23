be8f7a62a13079401e575e78d86132801fb4e2e5
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-23 15:37:44 +0800

    No edit name (#3177)
    * disable and add tooltip
    
    * bump version

--- src/services/aws/aws.ts
+++ src/services/aws/aws.ts
@@ -190,7 +190,7 @@ export const aws = createApi({
       invalidatesTags: (_, error) => (error ? [] : ["endowments", "endowment"]),
       query: ({ id, ...payload }) => {
         return {
-          url: `/${v(7)}/endowments/${id}`,
+          url: `/${v(8)}/endowments/${id}`,
           method: "PATCH",
           headers: { authorization: TEMP_JWT },
           body: payload,
