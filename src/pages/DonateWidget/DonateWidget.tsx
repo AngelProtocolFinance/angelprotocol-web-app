6b645ea6d9306c965270c4efa79ecac8cc8d6c04
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-25 12:18:32 +0800

    Email bugs (#3184)
    * switch to no cors: verified to be still running
    
    * move override further down

--- src/pages/DonateWidget/DonateWidget.tsx
+++ src/pages/DonateWidget/DonateWidget.tsx
@@ -1,6 +1,5 @@
 import LoaderRing from "components/LoaderRing";
 import QueryLoader from "components/QueryLoader";
-import Seo from "components/Seo";
 import { ErrorStatus } from "components/Status";
 import { idParamToNum, setToLightMode } from "helpers";
 import { useEffect } from "react";
@@ -48,7 +47,6 @@ export default function DonateWidget() {
       }}
       className="grid grid-rows-[1fr_auto] justify-items-center gap-10"
     >
-      <Seo scripts={[]} />
       <QueryLoader
         queryState={queryState}
         messages={{
