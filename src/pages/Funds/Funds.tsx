531f2b7505d2658ce0914feb0d5e963ee90aab40
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-31 12:40:31 +0800

    List funds (#3192)
    * funds routes
    
    * setup fields and schema
    
    * logo
    
    * wire validation and add submit
    
    * expiratiom field
    
    * featured checkbox
    
    * group headers
    
    * create fund
    
    * add split slider
    
    * tip checkbox
    
    * endowments multiselector
    
    * add debounce
    
    * selected and hovered
    
    * add card_imgs
    
    * logo size
    
    * remove sort param
    
    * remove italic
    
    * move fund selector
    
    * single endow setting hook
    
    * disable when single setting is applied
    
    * disable when single setting is applied
    
    * set effect dep
    
    * rename
    
    * generic name
    
    * include endow name in config
    
    * update schema
    
    * submit button
    
    * revert to custom
    
    * remove unused
    
    * endow req valid window
    
    * wrap with auth
    
    * save to server
    
    * show loader when debouncing
    
    * add aws type
    
    * construct new payload
    
    * error validation
    
    * change bucket name
    
    * wip fund page
    
    * fundraiser list
    
    * funds credential
    
    * success msg
    
    * add expiration
    
    * correct propt
    
    * route fund page
    
    * setup funds page
    
    * fix type error

--- src/pages/Funds/Funds.tsx
+++ src/pages/Funds/Funds.tsx
@@ -1,6 +1,23 @@
-import { appRoutes } from "constants/routes";
-import { Link } from "react-router-dom";
+import Icon from "components/Icon";
+import { useState } from "react";
+import Cards from "./Cards";
 
 export default function Funds() {
-  return <Link to={appRoutes.funds + "/new"}>Create fund</Link>;
+  const [search, setSearch] = useState("");
+  return (
+    <div className="padded-container mt-8">
+      <div
+        className={`flex gap-2 items-center rounded-lg overflow-clip field-container`}
+      >
+        <Icon type="Search" size={20} className="ml-2" />
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-full py-2 pr-3 placeholder:text-navy-l3 text-navy-d4 font-medium font-heading"
+          placeholder="Search funds..."
+        />
+      </div>
+      <Cards search={search} classes="mt-4" />
+    </div>
+  );
 }
--- src/types/aws/ap/index.ts
+++ src/types/aws/ap/index.ts
@@ -128,7 +128,8 @@ export interface UserFund {
   name?: string;
   logo: string;
   email: string;
-  fundId: string;
+  /** uuidv4 */
+  id: string;
 }
 
 export interface EndowAdmin {
@@ -265,5 +266,31 @@ export namespace Fund {
     > {
     /** endowment ids */
     members: number[];
+  }
+
+  export interface Card
+    extends Pick<
+      Fund,
+      | "id"
+      | "name"
+      | "description"
+      | "env"
+      | "logo"
+      | "featured"
+      | "active"
+      | "verified"
+      | "donation_total_usd"
+      | "members"
+    > {}
+
+  export interface CardsPage {
+    items: Card[];
+    page: number;
+    numPages: number;
+  }
+
+  export interface CardsQueryParams {
+    query?: string;
+    page?: number;
   }
 }
