b5caf9e156fb051017c140b24d66a7d6ff418edb
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-07-31 12:01:02 +0800

    Create fund UI Iteration 1 (#3185)
    * funds routes
    
    * setup fields and schema
    
    * logo
    
    * wire validation and add submit
    
    * expiratiom field
    
    * featured checkbox
    
    * group headers
    
    * create fund
    
    * add split slider
    
    * tip checkbox
    
    * endowments multiselector
    
    * add debounce
    
    * selected and hovered
    
    * add card_imgs
    
    * logo size
    
    * remove sort param
    
    * remove italic
    
    * move fund selector
    
    * single endow setting hook
    
    * disable when single setting is applied
    
    * disable when single setting is applied
    
    * set effect dep
    
    * rename
    
    * generic name
    
    * include endow name in config
    
    * update schema
    
    * submit button
    
    * revert to custom
    
    * remove unused
    
    * endow req valid window
    
    * wrap with auth
    
    * save to server
    
    * show loader when debouncing
    
    * add aws type
    
    * construct new payload
    
    * error validation
    
    * change bucket name
    
    * wip fund page
    
    * fundraiser list
    
    * funds credential
    
    * success msg
    
    * add expiration

--- src/services/aws/users.ts
+++ src/services/aws/users.ts
@@ -1,5 +1,10 @@
 import { TEMP_JWT } from "constants/auth";
-import type { AletPrefUpdate, UserEndow, UserUpdate } from "types/aws";
+import type {
+  AletPrefUpdate,
+  UserEndow,
+  UserFund,
+  UserUpdate,
+} from "types/aws";
 import { version as v } from "../helpers";
 import { aws } from "./aws";
 
@@ -23,6 +28,13 @@ const endowAdmins = aws.injectEndpoints({
         headers: { authorization: TEMP_JWT },
       }),
     }),
+    userFunds: builder.query<UserFund[], string>({
+      providesTags: ["user-endows"],
+      query: (userId) => ({
+        url: `/${v(3)}/users/${userId}/funds`,
+        headers: { authorization: TEMP_JWT },
+      }),
+    }),
     updateUserEndows: builder.mutation<
       unknown,
       { userId: string; alertPrefs: AletPrefUpdate[] }
@@ -43,5 +55,6 @@ const endowAdmins = aws.injectEndpoints({
 export const {
   useEditUserMutation,
   useUserEndowsQuery,
+  useUserFundsQuery,
   useUpdateUserEndowsMutation,
 } = endowAdmins;
