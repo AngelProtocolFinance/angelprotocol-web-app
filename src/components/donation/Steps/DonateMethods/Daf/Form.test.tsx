90a9a330651b013d8e349db198a7bef191ddb65f
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-08-03 09:11:44 +0800

    Chariot prod key requirements (#3198)
    * 50 usd min amount
    
    * summary skips
    
    * create dedicated chariot checkout form
    
    * use props namespace
    
    * modal dismissible
    
    * floor instead of ceil
    
    * set  min
    
    * update test

--- src/components/donation/Steps/DonateMethods/Daf/Form.test.tsx
+++ src/components/donation/Steps/DonateMethods/Daf/Form.test.tsx
@@ -93,7 +93,7 @@ describe("DAF form test", () => {
 
     await userEvent.clear(amountInput);
     await userEvent.type(amountInput, "-5");
-    expect(screen.getByText(/minimum donation is \$50/i)).toBeInTheDocument();
+    expect(screen.getByText(/must be greater than 0/i)).toBeInTheDocument();
 
     await userEvent.clear(amountInput);
     await userEvent.type(amountInput, "50");
