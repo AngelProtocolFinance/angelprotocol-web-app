c435833a46a181a6bc6cf17af4b4e742f2c225f6
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-08-03 09:11:44 +0800

    Chariot prod key requirements (#3198)
    * 50 usd min amount
    
    * summary skips
    
    * create dedicated chariot checkout form
    
    * use props namespace
    
    * modal dismissible
    
    * floor instead of ceil
    
    * set  min
    
    * update test

--- src/pages/OAuthRedirector.tsx
+++ src/pages/OAuthRedirector.tsx
@@ -1,5 +1,4 @@
 import LoaderRing from "components/LoaderRing";
-<<<<<<< HEAD
 
 export default function OAuthRedirector() {
   /**
@@ -15,31 +14,4 @@ export default function OAuthRedirector() {
       classes={{ container: "w-32 place-self-center" }}
     />
   );
-=======
-import { Navigate, useLocation } from "react-router-dom";
-import type { OAuthState } from "types/auth";
-
-export default function OAuthRedirector() {
-  const location = useLocation();
-
-  const state: OAuthState | null = location.state
-    ? JSON.parse(location.state)
-    : null;
-
-  /** when provider (`A.`) redirects back to our app thru `/oauth-redirector` it doesn't pass `state`
-   * `A.` - Amplify is configured in src/index.tsx
-   *  App.tsx listens to `auth.customOAuthState` event and navigates to `/oauth-redirector`, this time with `state`
-   */
-  if (!state) {
-    return (
-      <LoaderRing
-        thickness={10}
-        classes={{ container: "w-32 place-self-center" }}
-      />
-    );
-  }
-
-  const { pathname = "/", data } = state || {};
-  return <Navigate to={pathname} state={data} />;
->>>>>>> ab7d97e0d (Auth redirect (#3165))
 }
