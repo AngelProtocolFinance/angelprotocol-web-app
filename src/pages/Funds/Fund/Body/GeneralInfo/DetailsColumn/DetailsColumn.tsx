e0bd7f79f6cd2daa3cd724943f400429025478e9
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-08-12 14:05:40 +0800

    lint fix

--- src/pages/Funds/Fund/Body/GeneralInfo/DetailsColumn/DetailsColumn.tsx
+++ src/pages/Funds/Fund/Body/GeneralInfo/DetailsColumn/DetailsColumn.tsx
@@ -1,8 +1,6 @@
-import { useFundContext } from "../../../FundContext";
 import Balances from "./Balances";
 
 export default function DetailsColumn({ className = "" }) {
-  const p = useFundContext();
   return (
     <div className="flex flex-col gap-6 w-full">
       <Balances />
