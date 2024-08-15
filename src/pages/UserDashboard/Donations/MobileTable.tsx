133707b9e5166577ec6baac9dfc94b87e242576c
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-08-15 14:58:04 +0800

    fix lint

--- src/pages/UserDashboard/Donations/MobileTable.tsx
+++ src/pages/UserDashboard/Donations/MobileTable.tsx
@@ -25,7 +25,6 @@ export default function MobileTable({
   onLoadMore,
 }: TableProps) {
   const { sorted } = useSort(donations, "date");
-  const showKYCForm = useShowKYCForm();
 
   return (
     <div
