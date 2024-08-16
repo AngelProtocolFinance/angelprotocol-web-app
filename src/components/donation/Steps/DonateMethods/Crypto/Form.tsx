088cba08434fd4f033c770674abe618dc30eff9a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-08-16 21:13:21 +0800

    update chain selector removed

--- src/components/donation/Steps/DonateMethods/Crypto/Form.tsx
+++ src/components/donation/Steps/DonateMethods/Crypto/Form.tsx
@@ -1,26 +1,20 @@
-import { Label } from "components/form";
 import TokenField from "../../../../TokenField";
 import { useDonationState } from "../../Context";
 import ContinueBtn from "../../common/ContinueBtn";
 import { ProgramSelector } from "../../common/ProgramSelector";
-import { initTokenOption } from "../../common/constants";
 import type { CryptoFormStep } from "../../types";
 import { nextFormState } from "../helpers";
-import { ChainSelector } from "./ChainSelector";
 import type { DonateValues } from "./types";
 import { useRhf } from "./useRhf";
 
 export default function Form(props: CryptoFormStep) {
   const { setState } = useDonationState();
 
-  const { handleSubmit, reset, setValue, program, token, chainId, errors } =
-    useRhf(props);
+  const { handleSubmit, reset, program, token, errors } = useRhf(props);
 
   function submit({ chainId, ...data }: DonateValues) {
     if (!chainId) throw "dev: chainId should be validated";
-    setState((prev) =>
-      nextFormState(prev, { ...data, method: "crypto", chainId })
-    );
+    setState((prev) => nextFormState(prev, { ...data, method: "crypto" }));
     reset();
   }
 
@@ -30,29 +24,10 @@ export default function Form(props: CryptoFormStep) {
       className="flex flex-col gap-4 rounded-md min-h-full"
       autoComplete="off"
     >
-      <Label
-        htmlFor="chainId"
-        className="-mb-2 font-semibold font-heading text-base"
-        required
-      >
-        Network
-      </Label>
-      <ChainSelector
-        ref={chainId.ref}
-        value={chainId.value}
-        error={errors.chainId}
-        onChange={(id) => {
-          chainId.onChange(id);
-          //reset selected token
-          setValue("token", initTokenOption);
-        }}
-      />
-
       <TokenField
         ref={token.ref}
         token={token.value}
         onChange={token.onChange}
-        chainId={chainId.value}
         error={errors.token}
         withBalance
         label="Donation amount"
