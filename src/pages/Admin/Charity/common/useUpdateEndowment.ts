db16cf22cff2b2963ec2163857b184c3eb477135
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-09-02 15:23:27 +0800

    edit profile remove tx wording (#3270)

--- src/pages/Admin/Charity/common/useUpdateEndowment.ts
+++ src/pages/Admin/Charity/common/useUpdateEndowment.ts
@@ -1,8 +1,6 @@
-import { TxPrompt } from "components/Prompt";
-import { appRoutes } from "constants/routes";
+import Prompt from "components/Prompt";
 import { useErrorContext } from "contexts/ErrorContext";
 import { useModalContext } from "contexts/ModalContext";
-import { cleanObject } from "helpers/cleanObject";
 import { useEditEndowmentMutation } from "services/aws/aws";
 import type { EndowmentUpdate } from "services/types";
 
@@ -13,24 +11,18 @@ export function useUpdateEndowment() {
 
   const updateEndowment = async (update: EndowmentUpdate) => {
     try {
-      const cleanUpdates = cleanObject(update);
-
       showModal(
-        TxPrompt,
-        { loading: "Submitting changes.." },
+        Prompt,
+
+        { type: "loading", children: "Submitting changes.." },
         { isDismissible: false }
       );
 
       await submit(update).unwrap();
 
-      return showModal(TxPrompt, {
-        success: {
-          message: "Successfully updated",
-          link: {
-            description: "View changes",
-            url: `${appRoutes.marketplace}/${cleanUpdates.id}`,
-          },
-        },
+      return showModal(Prompt, {
+        type: "success",
+        children: "Successfully updated",
       });
     } catch (err) {
       handleError(err, { context: "applying changes" });
