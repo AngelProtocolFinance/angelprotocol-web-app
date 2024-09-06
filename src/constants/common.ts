0570959d0fb5d78c533dde2106d86d249b87dc61
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-09-06 15:15:08 +0800

    up processing fee 1%

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -7,7 +7,7 @@ export const BYTES_IN_MB = 1e6;
 export const PROCESSING_RATES = {
   chariot: 0.029,
   stripe: 0.024,
-  crypto: 0.005,
+  crypto: 0.01,
 };
 
 export const DONATION_INCREMENTS = [40, 100, 200];
