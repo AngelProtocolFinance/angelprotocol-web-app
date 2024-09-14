c2e476d66704cd04fa611191ecfdbda901b29528
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-09-14 13:59:02 +0800

    remoev unused schemas

--- src/schemas/string.ts
+++ src/schemas/string.ts
@@ -1,24 +1,8 @@
-import type { Chain } from "types/chain";
 import * as Yup from "yup";
 
-export const junoAddrPattern = /^juno1[a-z0-9]{38,58}$/i;
-export const stargazeAddrPattern = /^stars1[a-z0-9]{38,58}$/i;
-export const kujiraAddrPattern = /^kujira1[a-z0-9]{38,58}$/i;
-export const osmosisAddrPattern = /^osmo1[a-z0-9]{38,58}$/i;
-export const terraAddrPattern = /^terra1[a-z0-9]{38}$/i;
 export const alphanumeric = /^[0-9a-zA-Z]+$/;
-const evmAddrPattern = /^0x[a-fA-F0-9]{40}$/;
 
 export const requiredString = Yup.string().required("required");
-
-export const walletAddr = (chainId: Chain.Id.Tier1) =>
-  Yup.lazy((val) =>
-    val === ""
-      ? Yup.string()
-      : Yup.string()
-          .trim()
-          .matches(walletAddrPattern(chainId), "wallet address not valid")
-  );
 
 export const url = Yup.string()
   .trim()
@@ -50,42 +34,6 @@ export const segment = Yup.string()
     "invalid segment",
     (v) => !v?.startsWith(".") && !v?.endsWith(".")
   );
-
-export function walletAddrPattern(chainId: Chain.Id.Tier1) {
-  switch (chainId) {
-    case "1":
-    case "11155111":
-    case "56":
-    case "97":
-    case "137":
-    case "42161":
-    case "80002":
-    case "421614":
-    case "10":
-    case "11155420":
-    case "8453":
-    case "84532":
-      return evmAddrPattern;
-    case "pisco-1":
-    case "phoenix-1":
-      return terraAddrPattern;
-    case "juno-1":
-    case "uni-6":
-      return junoAddrPattern;
-    case "kaiyo-1":
-    case "harpoon-4":
-      return kujiraAddrPattern;
-    case "stargaze-1":
-    case "elgafar-1":
-      return stargazeAddrPattern;
-    case "osmosis-1":
-    case "osmo-test-5":
-      return osmosisAddrPattern;
-    default:
-      const x: never = chainId;
-      throw new Error(`unhandled ${x}`);
-  }
-}
 
 export const password = requiredString
   .min(8, ({ min }) => `must have at least ${min} characters`)
