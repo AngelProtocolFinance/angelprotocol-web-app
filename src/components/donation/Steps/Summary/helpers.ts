363f83cd1a0e55c55d11279028a8e65bf335f141
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-09-18 15:23:17 +0800

    use formulate instead of iteration

--- src/components/donation/Steps/Summary/helpers.ts
+++ src/components/donation/Steps/Summary/helpers.ts
@@ -2,52 +2,28 @@ import { PROCESSING_RATES } from "constants/common";
 import type { DonationDetails } from "../types";
 
 export const processingFee = (details: DonationDetails): number => {
+  if (details.method === "stocks") return 0;
+
   if (details.method === "crypto") {
-    return feeFn({
-      amount: +details.token.amount,
-      rate: PROCESSING_RATES.crypto,
-      flat: 0,
-    });
+    return fee(+details.token.amount, PROCESSING_RATES.crypto);
   }
 
   if (details.method === "daf") {
-    return feeFn({
-      amount: +details.amount,
-      rate: PROCESSING_RATES.chariot,
-      flat: 0,
-    });
+    return fee(+details.amount, PROCESSING_RATES.chariot);
   }
 
-  if (details.method === "stocks") return 0;
-
   details.method satisfies "stripe";
-
   /** @see https://stripe.com/pricing */
-  return feeFn({
-    amount: +details.amount,
-    rate: PROCESSING_RATES.stripe,
-    flat: 0.3 /** 30cents */,
-  });
+  return fee(+details.amount, PROCESSING_RATES.stripe, 0.3);
 };
 
-interface Input {
-  /** total amount */
-  amount: number;
-  /** rate: 0 - 1*/
-  rate: number;
-  /** flat:denomination same as amount */
-  flat: number;
-}
-
-function feeFn(input: Input, prev = 0, iterations = 0): number {
-  const { amount, rate, flat } = input;
-  const fee = (amount + prev ?? 0) * rate + flat;
-
-  const diff = fee - prev;
-
-  //base case
-  if (diff < 0.01) return fee;
-  if (iterations > 10) return fee;
-
-  return feeFn(input, fee, iterations + 1);
+function fee(amount: number, rate: number, flat = 0): number {
+  /**
+   * fee(1) = amount * rate + flat
+   * fee(2) = (amount + fee(1)) * rate + flat
+   * fee(3) = (amount + fee(2)) * rate + flat
+   * i.e. F₍ₙ₎ = a · ∑ᵏ⁼¹ⁿ (rᵏ) + f · ∑ᵏ⁼⁰ⁿ⁻¹ (rᵏ)
+   * which converges to this formula: n approaches infinity
+   */
+  return (amount * rate + flat) / (1 - rate);
 }
