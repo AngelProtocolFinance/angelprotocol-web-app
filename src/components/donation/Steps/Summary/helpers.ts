1407426cd01fdfa829102d8efbdc9a00f8f5bf37
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-09-18 15:42:31 +0800

    Processing fee calculation (#3309)
    * iterative
    
    * use formulate instead of iteration

--- src/components/donation/Steps/Summary/helpers.ts
+++ src/components/donation/Steps/Summary/helpers.ts
@@ -2,13 +2,28 @@ import { PROCESSING_RATES } from "constants/common";
 import type { DonationDetails } from "../types";
 
 export const processingFee = (details: DonationDetails): number => {
+  if (details.method === "stocks") return 0;
+
   if (details.method === "crypto") {
-    return +details.token.amount * PROCESSING_RATES.crypto;
+    return fee(+details.token.amount, PROCESSING_RATES.crypto);
   }
 
-  if (details.method !== "stripe") return 0;
+  if (details.method === "daf") {
+    return fee(+details.amount, PROCESSING_RATES.chariot);
+  }
 
+  details.method satisfies "stripe";
   /** @see https://stripe.com/pricing */
-  const fixedFeeUnits = 0.3 /** 30cents */ * details.currency.rate;
-  return PROCESSING_RATES.stripe * +details.amount + fixedFeeUnits;
+  return fee(+details.amount, PROCESSING_RATES.stripe, 0.3);
 };
+
+function fee(amount: number, rate: number, flat = 0): number {
+  /**
+   * fee(1) = amount * rate + flat
+   * fee(2) = (amount + fee(1)) * rate + flat
+   * fee(3) = (amount + fee(2)) * rate + flat
+   * i.e. F₍ₙ₎ = a · ∑ᵏ⁼¹ⁿ (rᵏ) + f · ∑ᵏ⁼⁰ⁿ⁻¹ (rᵏ)
+   * which converges to this formula: n approaches infinity
+   */
+  return (amount * rate + flat) / (1 - rate);
+}
