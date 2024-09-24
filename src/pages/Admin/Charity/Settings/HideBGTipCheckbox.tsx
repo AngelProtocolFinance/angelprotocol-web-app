c94828df58ec9a1448cf61a9e9bd77ee5abe5a72
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-09-24 17:54:55 +0800

    Bg 1597 (#3330)
    * custom donation
    
    * edit verviage

--- src/pages/Admin/Charity/Settings/HideBGTipCheckbox.tsx
+++ src/pages/Admin/Charity/Settings/HideBGTipCheckbox.tsx
@@ -5,15 +5,15 @@ export default function HideBGTipCheckbox() {
   return (
     <div className="grid gap-2">
       <CheckField<FV> name="hide_bg_tip" classes="font-medium">
-        Opt out of tip-based donations
+        Opt out of Support Contribution Model
       </CheckField>
       <span className="text-xs sm:text-sm italic text-navy-l1">
         During the donation flow, there is a step in which users can choose to
-        tip Better Giving any amount they desire alongside their donation to you
-        - the amount they tip will not affect the donation amount you receive.
-        You may choose to turn this step off in the donation flow by ticking the
-        checkbox above and we will instead apply a fixed 1.5% fee to any
-        donation amount you receive.
+        support Better Giving by contributing any amount they desire alongside
+        their donation to you - the amount they contribute will not affect the
+        donation amount you receive. You may choose to turn this step off in the
+        donation flow by ticking the checkbox above and we will instead apply a
+        fixed 1.5% fee to any donation amount you receive.
       </span>
     </div>
   );
