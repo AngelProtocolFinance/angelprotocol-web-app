f8f7b7d8d373c6427b34aa07b2098786b5444b97
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-06 22:02:15 +0800

    approval prompts

--- src/services/aws/endow-funds.ts
+++ src/services/aws/endow-funds.ts
@@ -7,36 +7,39 @@ interface PathParams {
   endowId: number;
   fundId: string;
 }
-export const { useFundsEndowMemberOfQuery, useOptOutMutation } =
-  aws.injectEndpoints({
-    endpoints: (builder) => ({
-      fundsEndowMemberOf: builder.query<FundItem[], { endowId: number }>({
-        providesTags: ["endow-funds"],
-        query: ({ endowId }) => {
-          return {
-            url: `${v(8)}/endowments/${endowId}/funds`,
-          };
-        },
-      }),
-      optOut: builder.mutation<unknown, PathParams>({
-        invalidatesTags: ["endow-funds"],
-        query: ({ fundId, endowId }) => {
-          return {
-            url: `${v(8)}/endowments/${endowId}/funds/${fundId}/opt-out`,
-            method: "POST",
-            headers: { authorization: TEMP_JWT },
-          };
-        },
-      }),
-      approve: builder.mutation<unknown, PathParams>({
-        invalidatesTags: ["endow-funds"],
-        query: ({ fundId, endowId }) => {
-          return {
-            url: `${v(8)}/endowments/${endowId}/funds/${fundId}/approve`,
-            method: "POST",
-            headers: { authorization: TEMP_JWT },
-          };
-        },
-      }),
+export const {
+  useFundsEndowMemberOfQuery,
+  useOptOutMutation,
+  useApproveMutation,
+} = aws.injectEndpoints({
+  endpoints: (builder) => ({
+    fundsEndowMemberOf: builder.query<FundItem[], { endowId: number }>({
+      providesTags: ["endow-funds"],
+      query: ({ endowId }) => {
+        return {
+          url: `${v(8)}/endowments/${endowId}/funds`,
+        };
+      },
     }),
-  });
+    optOut: builder.mutation<unknown, PathParams>({
+      invalidatesTags: ["endow-funds"],
+      query: ({ fundId, endowId }) => {
+        return {
+          url: `${v(8)}/endowments/${endowId}/funds/${fundId}/opt-out`,
+          method: "POST",
+          headers: { authorization: TEMP_JWT },
+        };
+      },
+    }),
+    approve: builder.mutation<unknown, PathParams>({
+      invalidatesTags: ["endow-funds"],
+      query: ({ fundId, endowId }) => {
+        return {
+          url: `${v(8)}/endowments/${endowId}/funds/${fundId}/approve`,
+          method: "POST",
+          headers: { authorization: TEMP_JWT },
+        };
+      },
+    }),
+  }),
+});
