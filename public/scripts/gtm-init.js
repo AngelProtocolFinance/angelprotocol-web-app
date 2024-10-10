fe57d94ea4ecc18e890865a1eefefd8703a2b860
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-10 17:46:13 +0800

    change gtm id (#3354)

--- public/scripts/gtm-init.js
+++ public/scripts/gtm-init.js
@@ -7,4 +7,4 @@
   j.async = true;
   j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
   f.parentNode.insertBefore(j, f);
-})(window, document, "script", "dataLayer", "GTM-MBRJ2RM");
+})(window, document, "script", "dataLayer", "GTM-TTW8BGSZ");
