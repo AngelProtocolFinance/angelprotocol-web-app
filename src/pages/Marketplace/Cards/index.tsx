1c2a4465945b9b5fd8f2661b8b23b38c0f3ded7a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-12 19:18:18 +0800

    pagination

--- src/pages/Marketplace/Cards/index.tsx
+++ src/pages/Marketplace/Cards/index.tsx
@@ -1,53 +1,46 @@
-import QueryLoader from "components/QueryLoader";
+import { Info } from "components/Status";
+import { useNavigation, useSearchParams } from "react-router-dom";
+import type { EndowListPaginatedAWSQueryRes, EndowmentCard } from "types/aws";
 import Card from "./Card";
-import useCards from "./useCards";
 
-export default function Cards({ classes = "" }: { classes?: string }) {
-  const {
-    hasMore,
-    isLoading,
-    isFetching,
-    isLoadingNextPage,
-    loadNextPage,
-    data,
-    isError,
-  } = useCards();
+interface Props {
+  classes?: string;
+  page: EndowListPaginatedAWSQueryRes<EndowmentCard[]>;
+}
+
+export default function Cards({ classes = "", page }: Props) {
+  const navigation = useNavigation();
+  const [params, setParams] = useSearchParams();
+  const { Items: endowments, NumOfPages, Page } = page;
+
+  const hasMore = Page < NumOfPages;
+
+  if (endowments.length === 0) {
+    return <Info>No organisations found</Info>;
+  }
+
   return (
-    <QueryLoader
-      queryState={{
-        data: data?.Items || [],
-        isLoading,
-        isFetching,
-        isError,
-      }}
-      messages={{
-        error: "Failed to get organisations",
-        loading: "Getting organisations..",
-        empty: "No organisations found",
-      }}
-      classes={{
-        container: `${classes} dark:text-white`,
-      }}
+    <div
+      className={`${classes} w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 content-start`}
     >
-      {(endowments) => (
-        <div
-          className={`${classes} w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 content-start`}
-        >
-          {endowments.map((endow) => (
-            <Card {...endow} key={endow.id} />
-          ))}
+      {endowments.map((endow) => (
+        <Card {...endow} key={endow.id} />
+      ))}
 
-          {hasMore && (
-            <button
-              className="col-span-full btn-blue rounded-md p-2 text-sm w-full mt-6"
-              onClick={loadNextPage}
-              disabled={isLoading || isLoadingNextPage}
-            >
-              Load more organizations
-            </button>
-          )}
-        </div>
+      {hasMore && (
+        <button
+          type="button"
+          disabled={navigation.state === "loading"}
+          className="col-span-full btn-blue rounded-md p-2 text-sm w-full mt-6"
+          onClick={() => {
+            const n = new URLSearchParams(params);
+            n.set("page", String(Page + 1));
+            setParams(n, { replace: true, preventScrollReset: true });
+          }}
+        >
+          Load more organizations
+        </button>
       )}
-    </QueryLoader>
+    </div>
   );
 }
