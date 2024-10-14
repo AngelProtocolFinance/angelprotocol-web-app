dc1734fd0cdd9b0de9434a33aacfa2797a11f802
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-14 23:28:38 +0800

    dashboard loader not deferred

--- src/pages/Admin/Charity/Dashboard/Dashboard.tsx
+++ src/pages/Admin/Charity/Dashboard/Dashboard.tsx
@@ -1,13 +1,10 @@
-import ContentLoader from "components/ContentLoader";
-import { ErrorStatus } from "components/Status";
 import { CircleAlert } from "lucide-react";
-import { Suspense } from "react";
-import { Await, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { Loaded } from "./Loaded";
 import { monthPeriod } from "./monthPeriod";
 
 export default function Dashboard() {
-  const data: any = useLoaderData();
+  const [alloc, balance]: any = useLoaderData();
   const period = monthPeriod();
 
   return (
@@ -20,32 +17,7 @@ export default function Dashboard() {
         </div>
       )}
 
-      <Suspense fallback={<LoaderSkeleton />}>
-        <Await
-          resolve={data.data}
-          errorElement={
-            <ErrorStatus>Failed to get nonprofit organisation info</ErrorStatus>
-          }
-        >
-          {([alloc, balance]) => (
-            <Loaded balances={balance} allocation={alloc} />
-          )}
-        </Await>
-      </Suspense>
-    </div>
-  );
-}
-
-function LoaderSkeleton() {
-  return (
-    <div className="grid gap-4 @lg:grid-cols-2">
-      <ContentLoader className="h-40" />
-      <ContentLoader className="h-40" />
-      <ContentLoader className="h-40" />
-      <ContentLoader className="h-2 col-span-full" />
-
-      <ContentLoader className="h-60 col-span-full" />
-      <ContentLoader className="h-60 col-span-full" />
+      <Loaded balances={balance} allocation={alloc} />
     </div>
   );
 }
