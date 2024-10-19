7478d8ce65c0e316d35900c94054979390649e2b
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-19 21:56:09 +0800

    program create delete

--- src/pages/Admin/Charity/Programs/List.tsx
+++ src/pages/Admin/Charity/Programs/List.tsx
@@ -1,3 +1,4 @@
+import { Info } from "components/Status";
 import { useLoaderData } from "react-router-dom";
 import type { Program as TProgram } from "types/aws";
 import { Program } from "./Program";
@@ -7,9 +8,11 @@ export default function List() {
 
   return (
     <div className="@container grid gap-3 p-4 @lg:p-8 border border-gray-l4 rounded bg-white dark:bg-blue-d6">
-      {programs.map((p) => (
-        <Program {...p} key={p.id} />
-      ))}
+      {programs.length === 0 ? (
+        <Info>No programs</Info>
+      ) : (
+        programs.map((p) => <Program {...p} key={p.id} />)
+      )}
     </div>
   );
 }
