574c115616443414b6f71a0cb7692a2fc9f03fc9
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-19 22:25:36 +0800

    programs loader and actions

--- src/pages/Admin/Charity/ProgramEditor/index.ts
+++ src/pages/Admin/Charity/ProgramEditor/index.ts
@@ -1,7 +1,32 @@
 import { getProgram } from "api/get/program";
-import type { LoaderFunction } from "react-router-dom";
+import { loadAuth, redirectToAuth } from "auth";
+import { APIs } from "constants/urls";
+import type { ActionFunction, LoaderFunction } from "react-router-dom";
+import { version as v } from "services/helpers";
+export { ErrorElement as ErrorBoundary } from "errors/ErrorElement";
 
 export { default as Component } from "./ProgramEditor";
 
 export const loader: LoaderFunction = async ({ params }) =>
   getProgram(params.id, params.programId);
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const auth = await loadAuth();
+  if (!auth) return redirectToAuth(request);
+
+  const url = new URL(APIs.aws);
+  url.pathname = `${v(1)}/endowments/${params.id}/programs/${params.programId}`;
+
+  const req = new Request(url, {
+    method: "PATCH",
+    body: await request.json().then((res) => JSON.stringify(res)),
+    headers: { authorization: auth.idToken },
+  });
+
+  await caches.open("bg").then((cache) => cache.delete(url));
+
+  const res = await fetch(req);
+  if (!res.ok) throw res;
+
+  return res;
+};
