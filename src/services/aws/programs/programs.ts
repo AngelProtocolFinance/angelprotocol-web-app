532655f321b03da08010f8e081701fa541e78b63
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-19 21:56:43 +0800

    remove unused

--- src/services/aws/programs/programs.ts
+++ src/services/aws/programs/programs.ts
@@ -1,25 +1,10 @@
 import { TEMP_JWT } from "constants/auth";
-import type { NewProgram, Program, ProgramUpdate } from "types/aws";
+import type { Program, ProgramUpdate } from "types/aws";
 import { version as v } from "../../helpers";
-import type { ProgramDeleteMsg } from "../../types";
 import { aws } from "../aws";
 
 const programs = aws.injectEndpoints({
   endpoints: (builder) => ({
-    newProgram: builder.mutation<
-      { id: string },
-      NewProgram & { endowId: number }
-    >({
-      invalidatesTags: (_, error) => (error ? [] : ["programs"]),
-      query: ({ endowId, ...payload }) => {
-        return {
-          url: `/${v(1)}/endowments/${endowId}/programs`,
-          method: "POST",
-          headers: { authorization: TEMP_JWT },
-          body: payload,
-        };
-      },
-    }),
     editProgram: builder.mutation<Program, ProgramUpdate & { endowId: number }>(
       {
         invalidatesTags: (_, error) => (error ? [] : ["program", "programs"]),
@@ -33,21 +18,7 @@ const programs = aws.injectEndpoints({
         },
       }
     ),
-    deleteProgram: builder.mutation<unknown, ProgramDeleteMsg>({
-      invalidatesTags: (_, error) => (error ? [] : ["programs"]),
-      query: ({ id, program_id }) => {
-        return {
-          url: `/${v(2)}/endowments/${id}/programs/${program_id}`,
-          method: "DELETE",
-          headers: { authorization: TEMP_JWT },
-        };
-      },
-    }),
   }),
 });
 
-export const {
-  useNewProgramMutation,
-  useEditProgramMutation,
-  useDeleteProgramMutation,
-} = programs;
+export const { useEditProgramMutation } = programs;
