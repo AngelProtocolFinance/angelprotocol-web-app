090841ff4e54d8640fd1fe04ec22b2bc62fff52b
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-21 18:26:26 +0800

    milestone: conform

--- src/App/root-action.ts
+++ src/App/root-action.ts
@@ -1,18 +1,39 @@
 import { loadAuth, redirectToAuth } from "auth";
 import { cognito } from "auth/cognito";
+import { parseWithValibot } from "conform-to-valibot";
 import { appRoutes } from "constants/routes";
 import { APIs } from "constants/urls";
 import { type ActionFunction, redirect } from "react-router-dom";
 import { version as v } from "services/helpers";
+import { emailSubs } from "types/hubspot-subscription";
 
 export const rootAction: ActionFunction = async ({ request }) => {
   const r = request.clone();
+  const form = await r.formData();
+  const intent = form.get("intent");
+
+  if (intent === "subscribe") {
+    const payload = await request
+      .formData()
+      .then((f) => parseWithValibot(f, { schema: emailSubs }));
+
+    if (payload.status !== "success") return payload.reply();
+
+    const url = new URL(APIs.aws);
+    url.pathname = `${v(1)}/hubspot/email-subs`;
+    const res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ email: payload.value.email }),
+    });
+
+    if (!res.ok) return "error";
+    return "success";
+  }
 
   const auth = await loadAuth();
   if (!auth) return redirectToAuth(request);
 
-  const form = await r.formData();
-  const intent = form.get("intent");
+  //authenticated requests
 
   if (intent === "logout") {
     await cognito.signOut(auth.accessToken);
--- src/components/Footer/Newsletter/SubscriptionForm.tsx
+++ src/components/Footer/Newsletter/SubscriptionForm.tsx
@@ -1,70 +1,60 @@
-import { ErrorMessage } from "@hookform/error-message";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useErrorContext } from "contexts/ErrorContext";
+import {
+  type SubmissionResult,
+  getFormProps,
+  getInputProps,
+  useForm,
+} from "@conform-to/react";
+import { parseWithValibot } from "conform-to-valibot";
 import { Check } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { useNewsletterSubscribeMutation } from "services/aws/hubspot";
-import { object, string } from "yup";
-
-type FV = { email: string };
+import { useFetcher } from "react-router-dom";
+import { emailSubs } from "types/hubspot-subscription";
 
 export default function SubscriptionForm() {
-  const { handleError } = useErrorContext();
-  const [subscribe, { isSuccess }] = useNewsletterSubscribeMutation();
+  const fetcher = useFetcher<SubmissionResult | "success" | "error">();
 
-  const {
-    handleSubmit,
-    register,
-    reset,
-    formState: { isSubmitting, errors },
-  } = useForm<FV>({
-    resolver: yupResolver(
-      object({
-        email: string()
-          .trim()
-          .email("Invalid email format")
-          .required("Please enter your email."),
-      })
-    ),
-    reValidateMode: "onSubmit",
+  const [form, fields] = useForm({
+    shouldRevalidate: "onInput",
+    // Optional: Required only if you're validating on the server
+    lastResult: typeof fetcher.data === "string" ? undefined : fetcher.data,
+    onValidate({ formData }) {
+      return parseWithValibot(formData, { schema: emailSubs });
+    },
   });
 
-  async function submit(data: FV) {
-    try {
-      await subscribe(data.email).unwrap();
-      reset();
-    } catch (error) {
-      //user might already be subscribed and server returns a helpful message
-      handleError(error, "parsed");
-    }
-  }
-
   return (
-    <form className="grid content-start" onSubmit={handleSubmit(submit)}>
+    <fetcher.Form
+      {...getFormProps(form)}
+      action="/"
+      method="post"
+      className="grid content-start"
+      onSubmit={form.onSubmit}
+    >
       <div className="grid mb-3">
         <input
-          {...register("email")}
-          id="email"
-          className="cursor-default text-[#000000] opacity-[.9] p-3 rounded-md font-normal border text-[15px] md:text-[13px] border-solid  border-[#0000001a] w-full"
+          {...getInputProps(fields.email, { type: "email" })}
+          className="text-[#000000] opacity-[.9] p-3 rounded-md font-normal text-base md:text-sm border border-gray-l4 w-full outline-blue-d1"
           placeholder="Enter your email"
-          disabled={isSubmitting}
+          disabled={fetcher.state === "submitting"}
         />
-        <ErrorMessage
-          errors={errors}
-          as="p"
-          name="email"
-          className="w-full text-xs text-red-d2 text-left mt-0.5"
-        />
-        {isSuccess && !errors.email && <SuccessMessage />}
+        <p className="empty:hidden w-full text-red-d2 text-left mt-0.5 text-xs">
+          {fields.email.errors?.[0]}
+        </p>
+        {fetcher.data === "success" ? (
+          <SuccessMessage />
+        ) : fetcher.data === "error" ? (
+          <p className="text-xs text-red mt-0.5">Failed to subscribe</p>
+        ) : null}
       </div>
       <button
         type="submit"
-        className="sm:justify-self-start bg-blue-d1 rounded-full px-5 py-2 text-white text-sm font-medium"
-        disabled={isSubmitting}
+        name="intent"
+        value="subscribe"
+        className="sm:justify-self-start bg-blue-d1 disabled:bg-gray rounded-full px-5 py-2 text-white text-sm font-medium"
+        disabled={fetcher.state === "submitting"}
       >
         Subscribe
       </button>
-    </form>
+    </fetcher.Form>
   );
 }
 
