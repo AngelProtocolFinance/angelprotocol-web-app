ac618a989c65c5085a555f0835e5474f3f9449a8
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-29 16:14:42 +0800

    hero reduce size

--- src/pages/informational/NonprofitInfo/Hero.tsx
+++ src/pages/informational/NonprofitInfo/Hero.tsx
@@ -29,13 +29,13 @@ export default function Hero({ className = "" }) {
         <div className="flex flex-col @sm:flex-row justify-center @6xl:justify-start items-center gap-6">
           <Link
             to={appRoutes.register}
-            className="btn-blue rounded-full px-8 py-3 @6xl:px-12 @6xl:py-6 @6xl:text-xl shadow-2xl shadow-blue/30 hover:shadow-blue/50"
+            className="px-6 py-2 @6xl:px-10 @6xl:py-4 @6xl:text-lg shadow-blue/30 hover:shadow-blue/50 bg-blue-d1 active:translate-x-1 text-white font-heading uppercase font-bold shadow-2xl rounded-full"
           >
             Start today
           </Link>
           <Link
             to={BOOK_A_DEMO}
-            className="btn-outline-blue border-2 rounded-full px-8 py-3 @6xl:px-12 @6xl:py-6 @6xl:text-xl hover:shadow-2xl hover:shadow-blue/50"
+            className="uppercase border-blue-d1 text-blue-d1 active:translate-x-1 font-bold font-heading border-2 rounded-full px-6 py-2 @6xl:px-8 @6xl:py-4 @6xl:text-xl hover:shadow-2xl hover:shadow-blue/50"
           >
             Book a Demo
           </Link>
