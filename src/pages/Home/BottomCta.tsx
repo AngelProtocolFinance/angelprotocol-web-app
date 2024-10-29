489f282062f21f3f487b78f7e96701a6b2c6dddb
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-29 15:44:22 +0800

    bottom cta distinguish buttons

--- src/pages/Home/BottomCta.tsx
+++ src/pages/Home/BottomCta.tsx
@@ -7,26 +7,26 @@ import { Link } from "react-router-dom";
 export function BottomCta({ className = "" }) {
   return (
     <div
-      className={`${className} grid sm:grid-cols-[3fr_1fr] bg-blue-d1 rounded-3xl sm:rounded-4xl ring-8 [28rem]:ring-[1rem] ring-blue-l4 px-10 py-12 sm:px-16 sm:py-[4.5rem]`}
+      className={`${className} grid md:grid-cols-[3fr_1fr] bg-gradient-to-br from-blue-d1 to-white rounded-3xl md:rounded-4xl px-10 py-12 md:px-16 md:py-[4.5rem]`}
     >
-      <div className="order-2 sm:order-1">
-        <h4 className="text-center sm:text-left uppercase [28rem]:text-lg text-white leading-normal mb-6">
+      <div className="order-2 md:order-1">
+        <h4 className="text-center md:text-left uppercase [28rem]:text-lg text-white leading-normal mb-6">
           Simple. Sustainable. Free.
         </h4>
-        <h3 className="text-center sm:text-left sm:leading-snug font-heading text-2xl @sm:text-4xl text-white mb-9">
+        <h3 className="text-center md:text-left md:leading-snug font-heading text-2xl @md:text-4xl text-white mb-9">
           The all-in-one fundraising solution you deserve is only a few clicks
           away
         </h3>
-        <div className="flex flex-col sm:flex-row justify-center sm:justify-start items-center gap-6">
+        <div className="flex flex-col md:flex-row justify-center md:justify-start items-center gap-6">
           <Link
             to={appRoutes.register}
-            className="btn-outline-blue bg-white rounded-full px-8 py-3 sm:px-12 sm:py-6"
+            className="btn-blue rounded-full px-8 py-3 md:px-12 md:py-6"
           >
             Start today
           </Link>
           <Link
             to={BOOK_A_DEMO}
-            className="btn-outline-blue bg-white border-2 rounded-full px-8 py-3 sm:px-12 sm:py-6"
+            className="bg-white text-blue-d1 font-heading uppercase font-bold rounded-full px-8 py-3 md:px-12 md:py-6"
           >
             Book a Demo
           </Link>
@@ -35,7 +35,7 @@ export function BottomCta({ className = "" }) {
       <Image
         width={140}
         src={laira.waiving}
-        className="place-self-center mb-8 order-1 sm:order-2"
+        className="place-self-center mb-8 order-1 md:order-2"
       />
     </div>
   );
