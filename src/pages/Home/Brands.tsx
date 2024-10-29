5fc314a397756f8b37fe437a2ac092b72f0476ea
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-29 15:13:45 +0800

    fix marquee

--- src/pages/Home/Brands.tsx
+++ src/pages/Home/Brands.tsx
@@ -7,24 +7,16 @@ const Brands = () => {
       <h2 className="text-[32px] md:text-[42px] text-navy-d4 px-8 text-center max-w-lg mx-auto text-balance">
         Over $6M raised for nonprofits worldwide
       </h2>
-      <Marquee
-        pauseOnHover
-        className="[&_.rfm-initial-child-container]:gap-x-12"
-      >
-        {brands
-          .concat(brands)
-          .concat(brands)
-          .concat(brands)
-          .map((b, idx) => (
-            <img
-              key={idx}
-              src={b}
-              height={80}
-              width={120}
-              className="object-contain"
-              alt="brand img"
-            />
-          ))}
+      <Marquee pauseOnHover autoFill>
+        {brands.map((b, idx) => (
+          <img
+            key={idx}
+            src={b}
+            width={140}
+            className="object-contain mx-6"
+            alt="brand img"
+          />
+        ))}
       </Marquee>
     </section>
   );
--- src/pages/informational/NonprofitInfo/FellowNonProfits.tsx
+++ src/pages/informational/NonprofitInfo/FellowNonProfits.tsx
@@ -7,24 +7,16 @@ export default function FellowNonProfits({ className = "" }) {
       <h2 className="px-2 text-center text-4xl text-navy-d4 text-pretty mb-12">
         Over $6 million donations processed for nonprofits worldwide
       </h2>
-      <Marquee
-        pauseOnHover
-        className="[&_.rfm-initial-child-container]:gap-x-12"
-      >
-        {brands
-          .concat(brands)
-          .concat(brands)
-          .concat(brands)
-          .map((b, idx) => (
-            <img
-              key={idx}
-              src={b}
-              alt="brand img"
-              height={80}
-              width={120}
-              className="object-contain"
-            />
-          ))}
+      <Marquee pauseOnHover autoFill>
+        {brands.map((b, idx) => (
+          <img
+            key={idx}
+            src={b}
+            alt="brand img"
+            width={120}
+            className="object-contain mx-6"
+          />
+        ))}
       </Marquee>
     </section>
   );
