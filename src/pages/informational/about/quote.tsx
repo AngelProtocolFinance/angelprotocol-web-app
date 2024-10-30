96b85a65524a764403b5c89189a8aa85c4b14f65
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-10-30 11:10:13 +0800

    done quoate

--- src/pages/informational/about/quote.tsx
+++ src/pages/informational/about/quote.tsx
@@ -1,13 +1,14 @@
+import quotation from "assets/icons/quotation.svg";
 export function Quote({ className = "" }) {
   return (
     <section
-      className={`${className} grid justify-items-center gap-y-10 @6xl:justify-items-start @6xl:grid-cols-[3fr_2fr] py-24`}
+      className={`${className} grid justify-items-center gap-10 @6xl:justify-items-start @6xl:grid-cols-2 py-24`}
     >
-      <div className="max-w-2xl order-2 @6xl:order-1">
-        <h4 className="text-center @6xl:text-left @6xl:text-lg uppercase text-navy-d4 mb-5">
+      <div className="max-w-2xl">
+        <h4 className="text-center @6xl:text-left @6xl:text-lg uppercase text-blue-d1 mb-5">
           Philanthropy Reimagined
         </h4>
-        <h1 className="text-center @6xl:text-left text-4xl @6xl:text-5xl @6xl:leading-tight text-pretty mb-4 text-navy-d4">
+        <h1 className="text-center @6xl:text-left text-3xl @6xl:text-4xl @6xl:leading-tight text-pretty mb-4 text-navy-d4">
           Why We Do What We Do
         </h1>
         <p className="mb-10 text-lg text-center @6xl:text-left">
@@ -27,14 +28,25 @@ export function Quote({ className = "" }) {
         </p>
       </div>
 
-      <div className="order-1 @6xl:order-2 self-center">
-        <blockquote>
-          "Our mission is to give every nonprofit, everywhere, free access to
-          the financial tools they need to grow and thrive. We believe a new
-          model of philanthropy is possible, not based on scarcity and
-          dependence but rather abundance and financial self-sufficiency"
+      <div className="self-center p-8 pb-2 mt-4 max-w-3xl relative">
+        <div className="w-2 shadow-inner h-full absolute inset-y-0 left-0 bg-lilac/50 rounded-full" />
+        <img
+          src={quotation}
+          alt="quotation mark"
+          className="justify-self-start w-24 lg:w-36 mb-4 -mt-24"
+        />
+        <blockquote className="text-xl sm:text-2xl font-medium text-navy-l3 italic mb-2 font-heading">
+          Our mission is to give every nonprofit, everywhere, free access to the
+          financial tools they need to grow and thrive. <br />
+          <br />
+          We believe a new model of philanthropy is possible, not based on
+          scarcity and dependence but rather abundance and financial
+          self-sufficiency
         </blockquote>
-        <p>– Chauncey St. John, Founder​</p>
+        <p className="text-right text-lg text-navy mt-8">
+          – Chauncey St. John
+          <span className="block text-sm font-medium">Founder</span>​
+        </p>
       </div>
     </section>
   );
