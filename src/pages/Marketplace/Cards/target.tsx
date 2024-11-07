b6c311ab20f4be46c6cee1762edbd72568653298
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-11-07 15:14:02 +0800

    target cards

--- src/pages/Marketplace/Cards/target.tsx
+++ src/pages/Marketplace/Cards/target.tsx
@@ -1,35 +1,19 @@
-import diversity from "assets/icons/diversity.svg";
 import { humanize } from "helpers";
-import { useEndowBalanceQuery } from "services/apes";
 
 interface Props {
-  endowId: number;
+  progress: number;
   target?: number | "smart";
   classes?: string;
 }
-export function Target({ target = 0, classes = "", endowId }: Props) {
-  const { data } = useEndowBalanceQuery(endowId);
+export function Target({ target = 0, progress, classes = "" }: Props) {
+  if (!target) return null;
 
-  if (!target || !data) return null;
+  const to = target === "smart" ? nextMilestone(progress) : target;
 
-  const to =
-    target === "smart" ? nextMilestone(data.totalContributions) : target;
-
-  const pct = Math.min(data.totalContributions, to) / to;
+  const pct = Math.min(progress, to) / to;
 
   return (
     <div className={classes}>
-      <p className="mb-2">
-        <img
-          src={diversity}
-          width={20}
-          height={20}
-          className="inline-block relative mr-2 bottom-1"
-        />
-        <span className="text-sm font-heading font-medium text-navy-l1">
-          Help them reach the goal!
-        </span>
-      </p>
       <div className="h-1.5 w-full rounded-full bg-blue-l4 shadow-inner">
         <div
           style={{ width: `${pct * 100}%` }}
@@ -38,9 +22,7 @@ export function Target({ target = 0, classes = "", endowId }: Props) {
       </div>
       <div className="flex items-center justify-between mt-1">
         <p className="flex items-center gap-x-1 text-sm text-navy-l1">
-          <span className="font-medium">
-            ${humanize(data.totalContributions, 2, true)}
-          </span>
+          <span className="font-medium">${humanize(progress, 2, true)}</span>
           <span className="text-xs">Raised</span>
         </p>
         <p className="flex items-center gap-x-1 text-sm text-navy-l1">
