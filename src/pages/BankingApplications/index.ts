58dca6ecee1999088557881a67851964c1d2537d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-11-10 17:56:46 +0800

    banking applications pagination

--- src/pages/BankingApplications/index.ts
+++ src/pages/BankingApplications/index.ts
@@ -1,2 +1,33 @@
-export { Component } from "./BankingApplications";
-export { authLocLoader as loader } from "helpers/state-params";
+import { loadAuth, redirectToAuth } from "auth";
+import { APIs } from "constants/urls";
+import { cacheGet } from "helpers/cache-get";
+import type { LoaderFunction } from "react-router-dom";
+import { version as v } from "services/helpers";
+import type { UserV2 } from "types/auth";
+
+export { BankingApplications as Component } from "./BankingApplications";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  console.log({ request });
+  const auth = await loadAuth();
+  if (auth) return getApplications(new URL(request.url), auth);
+  return redirectToAuth(request);
+};
+
+async function getApplications(source: URL, user: UserV2) {
+  const url = new URL(APIs.aws);
+  url.pathname = `${v(1)}/banking-applications`;
+  url.searchParams.set("requestor", "bg-admin");
+
+  const status = source.searchParams.get("status");
+  const nextPageKey = source.searchParams.get("nextPageKey");
+  const endowId = source.searchParams.get("endowmentID");
+
+  if (status) url.searchParams.set("status", status);
+  if (nextPageKey) url.searchParams.set("nextPageKey", nextPageKey);
+  if (endowId) url.searchParams.set("endowmentID", endowId);
+
+  const req = new Request(url);
+  req.headers.set("authorization", user.idToken);
+  return cacheGet(req).then((res) => res.json());
+}
