45e33aa5bbea5390b5e3865459082bff3a95421b
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-11-14 06:56:35 +0800

    pretty usd

--- src/helpers/decimal/decimal.test.ts
+++ src/helpers/decimal/decimal.test.ts
@@ -1,75 +1,12 @@
-import Dec, { Decimal } from "decimal.js";
 import { describe, expect, test } from "vitest";
 import {
-  condense,
-  condenseToNum,
-  condenseToStr,
   humanize,
   roundDown,
   roundDownToNum,
-  scale,
-  scaleToStr,
   toPreciseLocaleString,
-} from ".";
+} from "./utils";
 
 describe("common decimal helpers", () => {
-  test("condense different input types", () => {
-    expect(condense("1000000")).toMatchObject(new Decimal(1));
-    expect(condense("1234567")).toMatchObject(new Decimal(1.234567));
-    expect(condense(1000000)).toMatchObject(new Decimal(1));
-    expect(condense(1234567)).toMatchObject(new Decimal(1.234567));
-    expect(condense(new Dec(1_000_000))).toMatchObject(new Decimal(1));
-    expect(condense(new Dec(1234567))).toMatchObject(new Decimal(1.234567));
-  });
-
-  test("condense different input value", () => {
-    expect(condense(1_000_000)).toMatchObject(new Decimal(1));
-    expect(condense(12_345_678)).toMatchObject(new Decimal(12.345678));
-    expect(condense(500_000)).toMatchObject(new Decimal(0.5));
-    expect(condense(51_234)).toMatchObject(new Decimal(0.051234));
-    expect(condense(51_234, 7)).toMatchObject(new Decimal(0.0051234));
-    expect(condense(51_234, 8)).toMatchObject(new Decimal(0.00051234));
-
-    expect(condenseToNum(1_000_000)).toBe(1);
-    expect(condenseToNum(12_345_678)).toBe(12.345678);
-    expect(condenseToNum(500_000)).toBe(0.5);
-    expect(condenseToNum(51_234)).toBe(0.051234);
-    expect(condenseToNum(51_234, 7)).toBe(0.0051234);
-    expect(condenseToNum(51_234, 8)).toBe(0.00051234);
-
-    expect(condenseToStr(1_000_000)).toBe("1");
-    expect(condenseToStr(12_345_678)).toBe("12.345678");
-    expect(condenseToStr(500_000)).toBe("0.5");
-    expect(condenseToStr(51_234)).toBe("0.051234");
-    expect(condenseToStr(51_234, 7)).toBe("0.0051234");
-    expect(condenseToStr(51_234, 8)).toBe("0.00051234");
-  });
-
-  test("scale different input types", () => {
-    expect(scale("1")).toMatchObject(new Decimal("1000000"));
-    expect(scale("1.234567")).toMatchObject(new Decimal("1234567"));
-    expect(scale(1)).toMatchObject(new Decimal("1000000"));
-    expect(scale(1.234567)).toMatchObject(new Decimal("1234567"));
-    expect(scale(new Dec(1))).toMatchObject(new Decimal("1000000"));
-    expect(scale(new Dec(1.234567))).toMatchObject(new Decimal("1234567"));
-
-    expect(scaleToStr("1")).toBe("1000000");
-    expect(scaleToStr("1.234567")).toBe("1234567");
-    expect(scaleToStr(1)).toBe("1000000");
-    expect(scaleToStr(1.234567)).toBe("1234567");
-    expect(scaleToStr(new Dec(1))).toBe("1000000");
-    expect(scaleToStr(new Dec(1.234567))).toBe("1234567");
-  });
-
-  test("scale different input value", () => {
-    expect(scale(1)).toMatchObject(new Decimal("1000000"));
-    expect(scale(12.345678)).toMatchObject(new Decimal("12345678"));
-    expect(scale(0.5)).toMatchObject(new Decimal("500000"));
-    expect(scale(0.051234)).toMatchObject(new Decimal("51234"));
-    expect(scale(0.051234, 7)).toMatchObject(new Decimal("512340"));
-    expect(scale(0.051234, 8)).toMatchObject(new Decimal("5123400"));
-  });
-
   test("round down", () => {
     expect(roundDown(1.23456789, 0)).toBe("1");
     expect(roundDown(1.23456789, 1)).toBe("1.2");
