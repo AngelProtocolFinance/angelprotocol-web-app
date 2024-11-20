03c65d977c86f706b8cee12dcc473c7e82352bf4
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-11-21 01:14:46 +0800

    Cheque FAQ update (#3443)
    * cheque faq
    
    * cheque faq

--- src/pages/Donate/FAQ.tsx
+++ src/pages/Donate/FAQ.tsx
@@ -11,10 +11,9 @@ import { Link } from "react-router-dom";
 
 interface Props {
   classes?: string;
-  endowId: number;
 }
 
-export default function FAQ({ classes = "", endowId }: Props) {
+export default function FAQ({ classes = "" }: Props) {
   return (
     <div
       className={
@@ -23,7 +22,7 @@ export default function FAQ({ classes = "", endowId }: Props) {
       }
     >
       <h2 id="faqs">Frequently asked questions</h2>
-      {faqs(endowId).map((faq) => (
+      {faqs.map((faq) => (
         <Disclosure as="div" key={faq.id}>
           {({ open }) => (
             <>
@@ -54,7 +53,7 @@ export default function FAQ({ classes = "", endowId }: Props) {
   );
 }
 
-const faqs = (endowId: number) => [
+const faqs = [
   {
     id: 1,
     question: "How does my donation work to benefit nonprofits?",
@@ -132,20 +131,24 @@ const faqs = (endowId: number) => [
         <Em classes="text-navy-d4">
           Altruistic Partners Empowering Society Inc
         </Em>
-        ,<br /> write{" "}
-        <Em classes="text-navy-d4 text-xs font-mono bg-blue-l4 p-0.5 mr-2">
-          endowment:{endowId}
-        </Em>
-        <Em classes="text-navy-d4 text-xs font-mono bg-blue-l4 p-0.5">
-          donation&nbsp;split:__%
-        </Em>{" "}
-        in the memo section of the check, and send it to:{" "}
+        and send it to:{" "}
         <Em intensity={1} classes="text-navy-d4 block mt-2">
           Miscellaneous Account Services
           <br /> PNC Bank
           <br /> P.O. Box 8108
           <br /> Philadelphia, PA 19101-8108
         </Em>
+      </p>,
+      <p>
+        If you would like to add a contribution for Better Giving to help keep
+        our services free, you may add that in the memo with{" "}
+        <Em
+          intensity={1}
+          classes="text-navy-d4 text-xs font-mono bg-blue-l4 p-0.5"
+        >
+          BG:&nbsp;Amount
+        </Em>
+        .
       </p>,
     ],
   },
