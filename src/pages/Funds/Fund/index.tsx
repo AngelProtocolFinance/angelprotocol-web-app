16edc788f9673d9a69a58e97061c8a71f9f31c4f
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-11-22 16:51:04 +0800

    layout

--- src/pages/Funds/Fund/index.tsx
+++ src/pages/Funds/Fund/index.tsx
@@ -5,9 +5,13 @@ import Seo from "components/Seo";
 import { APP_NAME, BASE_URL } from "constants/env";
 
 import { skipToken } from "@reduxjs/toolkit/query";
-import { useParams } from "react-router-dom";
+import { RichText } from "components/RichText";
+import VerifiedIcon from "components/VerifiedIcon";
+import { appRoutes } from "constants/routes";
+import { Link, useParams } from "react-router-dom";
 import { useFundQuery } from "services/aws/funds";
-import { Body } from "./Body";
+import Balances from "./Body/GeneralInfo/Balances";
+import { Target } from "./Body/common/target";
 import { FundContext } from "./FundContext";
 import PageError from "./PageError";
 import Skeleton from "./Skeleton";
@@ -28,20 +32,86 @@ export function Component() {
         image={data.logo || flying_character}
         url={`${BASE_URL}/profile/${data.id}`}
       />
-      <section className="grid grid-rows-[auto_auto_1fr] items-center isolate w-full h-full">
+      <section className="grid pb-10">
         <div
-          className="relative w-full h-52 sm:h-72 bg-cover bg-center"
+          className="relative w-full h-52 sm:h-72 bg-cover bg-center overlay"
           style={{
             backgroundImage: `url('${data.banner || fallback_banner}')`,
           }}
         />
-        <div className="padded-container flex justify-center items-center w-full overflow-visible h-0 isolate lg:justify-start">
-          <Image
-            src={data.logo || flying_character}
-            className="h-48 w-48 border border-gray-l4 rounded-full object-cover bg-white"
-          />
+        <div className="padded-container grid lg:grid-cols-[3fr_2fr] -mt-48 gap-4">
+          <div className="lg:-mt-24 bg-white z-10 rounded-lg shadow-2xl shadow-black/10 p-4">
+            <div className="grid max-lg:gap-y-4 items-center max-lg:justify-items-center lg:grid-cols-[auto_1fr]">
+              <Image
+                src={data.logo || flying_character}
+                width={60}
+                className="mr-4 lg:row-span-2 border-2 border-blue-d1 rounded-full object-cover bg-white"
+              />
+              <div className="lg:col-start-2 max-lg:text-center">
+                {data.verified && (
+                  <VerifiedIcon classes="relative inline bottom-1" size={20} />
+                )}
+                <span className="font-heading font-bold text-2xl w-full break-words text-center">
+                  {data.name}
+                </span>
+                {!data.active && (
+                  <span className="ml-2 px-3 py-1 text-2xs bg-red-l4 text-red relative inline bottom-1 uppercase rounded-full">
+                    closed
+                  </span>
+                )}
+              </div>
+              <p>
+                <span className="text-sm font-medium text-navy-l1 mr-1">
+                  created by:
+                </span>
+                <span className="font-medium text-navy">
+                  {data.creator || "dev@placeholder.com"}
+                </span>
+              </p>
+            </div>
+            <RichText
+              content={{ value: data.description ?? "" }}
+              classes={{
+                field: "",
+                container: "mt-4 pt-4 border-t border-gray-l4",
+              }}
+              readOnly
+            />
+          </div>
+          <div className="lg:-mt-24 bg-white z-10 rounded-lg shadow-2xl shadow-black/10 p-4">
+            {data.target ? (
+              <Target progress={data.donation_total_usd} target={data.target} />
+            ) : (
+              <Balances amount={data.donation_total_usd} />
+            )}
+            <Link
+              to={appRoutes.donate_fund + `/${data.id}`}
+              className="w-full btn-blue px-6 py-3 mt-4 text-sm"
+            >
+              Donate now
+            </Link>
+            <p className="mt-8 mb-2 text-sm font-bold font-heading text-navy-l1">
+              Proceeds goes to
+            </p>
+            <div className="grid gap-y-4">
+              {data.members.map((m) => (
+                <div key={m.id} className="flex items-center gap-x-2">
+                  <Image
+                    src={m.logo}
+                    className="aspect-square rounded-full border-2 border-blue-d1"
+                    width={40}
+                  />
+                  <Link
+                    to={`${appRoutes.marketplace}/${m.id}`}
+                    className="font-bold font-heading text-navy-l1 hover:text-blue-d1"
+                  >
+                    {m.name}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <Body />
       </section>
     </FundContext.Provider>
   );
