1c8fc6562e5a99a554c8804446caff67959d30c5
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-11-25 22:02:21 +0800

    remove create npo

--- src/pages/Admin/Charity/Funds/Funds.tsx
+++ src/pages/Admin/Charity/Funds/Funds.tsx
@@ -1,7 +1,5 @@
 import ContentLoader from "components/ContentLoader";
 import QueryLoader from "components/QueryLoader";
-import { appRoutes } from "constants/routes";
-import { Link } from "react-router-dom";
 import { useFundsEndowMemberOfQuery } from "services/aws/endow-funds";
 import { useAdminContext } from "../../Context";
 import { FundItem } from "./FundItem";
@@ -11,15 +9,7 @@ export function Funds() {
   const query = useFundsEndowMemberOfQuery({ endowId: id });
   return (
     <div className="grid gap-y-4 grid-cols-[auto_1fr_auto_auto_auto_auto] justify-items-start">
-      <div className="flex items-center justify-between col-span-full mb-2 w-full border-b border-gray-l4 pb-4">
-        <h3 className="text-3xl">My Fundraisers</h3>
-        <Link
-          to={appRoutes.funds + "/new"}
-          className="btn-blue text-sm px-6 py-2 rounded-full"
-        >
-          Create
-        </Link>
-      </div>
+      <h3 className="text-3xl col-span-full mb-2">My Fundraisers</h3>
 
       <QueryLoader
         messages={{
