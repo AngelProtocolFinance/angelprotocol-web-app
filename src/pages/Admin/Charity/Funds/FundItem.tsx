8ae4cc707bb42af10550580b53897fee07d35701
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-12-01 06:32:43 +0800

    card views per owner

--- src/pages/Admin/Charity/Funds/FundItem.tsx
+++ src/pages/Admin/Charity/Funds/FundItem.tsx
@@ -9,10 +9,13 @@ import { ArrowRight, LoaderCircle, Split } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useOptOutMutation } from "services/aws/endow-funds";
 
-export const FundItem = (props: TFundItem & { endowId: number }) => {
+export const FundItem = (
+  props: TFundItem & { endowId: number; isSelf: boolean }
+) => {
   const user = useAuthenticatedUser();
   const isActive = new Date().toISOString() <= props.expiration && props.active;
-  const isEditor = user.funds.includes(props.id);
+  const isEditor =
+    user.funds.includes(props.id) || user.endowments.includes(props.endowId);
   const [optOut, { isLoading: isOptingOut }] = useOptOutMutation();
   const { showModal } = useModalContext();
   const { handleError } = useErrorContext();
@@ -40,9 +43,16 @@ export const FundItem = (props: TFundItem & { endowId: number }) => {
       >
         {props.name}
       </Link>
-      <FundCreator name={props.creator_name} id={props.creator_id} />
+      <div>
+        <span className="text-xs mr-1">by</span>
+        <FundCreator
+          name={props.creator_name}
+          id={props.creator_id}
+          classes="text-sm font-semibold"
+        />
+      </div>
 
-      <div className="grid border-t border-gray-l4 gap-x-6 p-3 items-center">
+      <div className="grid border-t border-gray-l4 gap-x-6 p-3 items-center mt-4">
         <Link
           aria-disabled={!isActive || !isEditor}
           className={`flex items-center gap-x-1 text-sm hover:text-blue-d1 text-blue aria-disabled:pointer-events-none aria-disabled:text-gray ${
@@ -54,35 +64,37 @@ export const FundItem = (props: TFundItem & { endowId: number }) => {
           <span>Edit</span>
         </Link>
         {/** fund item won't show once NPO opted out of it: so no need to hide this button */}
-        <button
-          className="font-heading bg-amber enabled:hover:bg-amber-d1 text-white rounded-full px-4 py-2 text-xs flex items-center gap-1 disabled:bg-gray-l1"
-          disabled={isOptingOut}
-          type="button"
-          onClick={async () => {
-            console.log(props.id);
-            try {
-              await optOut({
-                fundId: props.id,
-                endowId: props.endowId,
-              }).unwrap();
-              showModal(Prompt, {
-                type: "success",
-                children:
-                  "You have successfully opted out of this fund. Changes will take effect shortly.",
-              });
-            } catch (err) {
-              handleError(err, { context: "opting out of fund" });
-            }
-          }}
-        >
-          {isOptingOut ? (
-            <LoaderCircle size={12} className="animate-spin" />
-          ) : (
-            <Split size={12} className="rotate-90" />
-          )}
+        {!props.isSelf && (
+          <button
+            className="font-heading bg-amber enabled:hover:bg-amber-d1 text-white rounded-full px-4 py-2 text-xs flex items-center gap-1 disabled:bg-gray-l1"
+            disabled={isOptingOut}
+            type="button"
+            onClick={async () => {
+              console.log(props.id);
+              try {
+                await optOut({
+                  fundId: props.id,
+                  endowId: props.endowId,
+                }).unwrap();
+                showModal(Prompt, {
+                  type: "success",
+                  children:
+                    "You have successfully opted out of this fund. Changes will take effect shortly.",
+                });
+              } catch (err) {
+                handleError(err, { context: "opting out of fund" });
+              }
+            }}
+          >
+            {isOptingOut ? (
+              <LoaderCircle size={12} className="animate-spin" />
+            ) : (
+              <Split size={12} className="rotate-90" />
+            )}
 
-          <span>{isOptingOut ? "Opting out.." : "Opt out"}</span>
-        </button>
+            <span>{isOptingOut ? "Opting out.." : "Opt out"}</span>
+          </button>
+        )}
       </div>
     </div>
   );
