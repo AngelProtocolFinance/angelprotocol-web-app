d084242af9ba3f3ee60abf9f720d8b97bcc4b27e
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-12-01 21:02:06 +0800

    expires

--- src/pages/Admin/Charity/Funds/FundItem.tsx
+++ src/pages/Admin/Charity/Funds/FundItem.tsx
@@ -14,13 +14,13 @@ import { useOptOutMutation } from "services/aws/endow-funds";
 const MAX_DATE = "9999-12-31T23:59:59Z";
 
 const expires = (input: string): [boolean, string | undefined] => {
-  console.log({ input });
   if (input === MAX_DATE) return [false, undefined];
-  const now = new Date(input);
+  const now = new Date();
   if (now.toISOString() > input) return [true, undefined];
 
   return [false, `ends in ${formatDistance(new Date(input), now)}`];
 };
+
 export const FundItem = (
   props: TFundItem & { endowId: number; isSelf: boolean }
 ) => {
