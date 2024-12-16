e6201dcbdaea4f787a1e35e22150a771cf80b901
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-12-16 19:34:46 +0800

    milestones loader an action

--- src/pages/Admin/Charity/ProgramEditor/index.ts
+++ src/pages/Admin/Charity/ProgramEditor/index.ts
@@ -13,13 +13,43 @@ export const action: ActionFunction = async ({ request, params }) => {
   const auth = await loadAuth();
   if (!auth) return redirectToAuth(request);
 
-  await ap.patch(
-    `${ver(1)}/endowments/${params.id}/programs/${params.programId}`,
-    {
-      headers: { authorization: auth.idToken },
-      json: await request.json(),
-    }
-  );
+  const basePath = `${ver(1)}/endowments/${params.id}/programs/${params.programId}`;
 
-  return { ok: true };
+  const { intent, ...p } = await request.json();
+
+  if (intent === "add-milestone") {
+    const res = await ap.post(`${basePath}/milestones`, {
+      headers: { authorization: auth.idToken },
+      json: {
+        title: `Milestone ${p["next-milestone-num"]}`,
+        description: "milestone description",
+        date: new Date().toISOString(),
+      },
+    });
+    return { ok: res.ok };
+  }
+
+  if (intent === "delete-milestone") {
+    const res = await ap.delete(`${basePath}/milestones/${p["milestone-id"]}`, {
+      headers: { authorization: auth.idToken },
+    });
+    return { ok: res.ok };
+  }
+
+  if (intent === "edit-milestone") {
+    const { "milestone-id": id, ...rest } = p;
+    const res = await ap.patch(`${basePath}/milestones/${id}`, {
+      json: rest,
+      headers: { authorization: auth.idToken },
+    });
+    return { ok: res.ok };
+  }
+
+  //edit program
+  const res = await ap.patch(basePath, {
+    headers: { authorization: auth.idToken },
+    json: p,
+  });
+
+  return { ok: res.ok };
 };
