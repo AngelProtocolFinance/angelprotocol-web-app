48767fc3fce726407a0c5b0728aceef33900c232
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-12-17 15:53:48 +0800

    claim state

--- src/pages/informational/us-nonprofits/claim-cta/claim-cta.tsx
+++ src/pages/informational/us-nonprofits/claim-cta/claim-cta.tsx
@@ -1,4 +1,7 @@
+import type { EndowClaim } from "@better-giving/registration/models";
+import { appRoutes } from "constants/routes";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import type { EndowmentOption } from "types/aws";
 import { EndowSelector } from "./endow-selector";
 
@@ -11,13 +14,23 @@ export function ClaimCta() {
         value={endow ?? { id: 0, name: "", registration_number: "" }}
         onChange={setEndow}
       />
-      <button
-        disabled={!endow?.id}
+      <Link
+        state={
+          endow
+            ? ({
+                id: endow?.id,
+                name: endow?.name,
+                ein: endow?.registration_number,
+              } satisfies EndowClaim)
+            : undefined
+        }
+        to={`${appRoutes.register}/welcome`}
+        aria-disabled={!endow?.id}
         type="button"
-        className="text-sm uppercase font-bold font-heading bg-blue-d1 text-white shadow-xl enabled:active:translate-x-1 hover:bg-blue-d2 rounded-full px-4 py-2 self-start justify-self-start mt-2 disabled:bg-gray"
+        className="block text-sm uppercase font-bold font-heading bg-blue-d1 text-white shadow-xl enabled:active:translate-x-1 hover:bg-blue-d2 rounded-full px-4 py-2 self-start justify-self-start mt-2 disabled:bg-gray"
       >
         continue
-      </button>
+      </Link>
     </div>
   );
 }
