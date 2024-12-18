fbc4986a6ae05e95fbc0898dce4899abf268f677
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-12-18 16:37:00 +0800

    target selector component

--- src/pages/Admin/Charity/Settings/types.ts
+++ src/pages/Admin/Charity/Settings/types.ts
@@ -3,28 +3,9 @@ import {
   increment,
   str,
 } from "@better-giving/endowment/schema";
+import { target } from "components/goal-selector";
 import { donateMethod } from "types/components";
 import * as v from "valibot";
-
-export const target = v.variant("type", [
-  v.object({
-    type: v.literal("fixed"),
-    value: v.pipe(
-      v.string("required"),
-      v.nonEmpty("required"),
-      v.transform((x) => +x),
-      v.number("invalid number"),
-      v.minValue(0, "must be greater than 0"),
-      /** so that the inferred type is string */
-      v.transform((x) => x.toString())
-    ),
-  }),
-  v.object({ type: v.literal("smart"), value: v.optional(v.string()) }),
-  v.object({ type: v.literal("none"), value: v.optional(v.string()) }),
-]);
-
-export type Target = v.InferOutput<typeof target>;
-export type TargetType = Target["type"];
 
 export const schema = v.object({
   receiptMsg: v.pipe(
