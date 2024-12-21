3ba1ea126f8156592a90aa83d4f692f69af9968f
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-12-21 12:09:02 +0800

    capitalize claim nonprofit

--- src/pages/test/us-nonprofits/bottom-cta.tsx
+++ src/pages/test/us-nonprofits/bottom-cta.tsx
@@ -22,7 +22,7 @@ export function BottomCta({ className = "" }) {
         <h3 className="text-center text-white @3xl:leading-snug font-heading text-xl @sm:text-2xl mb-4 col-span-full">
           US 501(c)(3) Nonprofit?
         </h3>
-        <p className="text-white text-lg font-bold mb-2 text-center">
+        <p className="text-white text-lg font-bold mb-2 text-center capitalize">
           Claim your nonprofit
         </p>
         <Image width={200} src={laira.negotiating} className="mt-4 mb-8" />
