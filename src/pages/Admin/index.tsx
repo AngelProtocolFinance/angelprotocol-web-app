9bc3a23d13e8b6792b1d46033ed719385e6e47eb
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-01-01 15:43:25 +0800

    admin layout route module

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,70 +1,10 @@
-import { getEndow } from "api/get/endow";
-import { plusInt } from "api/schema/endow-id";
-import { redirectToAuth } from "auth";
-import { loadAuth } from "auth/load-auth";
-import Footer from "components/Footer";
 import { appRoutes } from "constants/routes";
-import ModalContext from "contexts/ModalContext";
-import Layout from "layout/DashboardLayout";
-import { CircleAlert } from "lucide-react";
-import {
-  type LoaderFunction,
-  type RouteObject,
-  useLoaderData,
-} from "react-router";
-import { parse } from "valibot";
+import { convert } from "helpers/route";
+import type { RouteObject } from "react-router";
 import { charityRoutes } from "./Charity";
-import type { AdminContext } from "./Context";
-import Header from "./Header";
-import SidebarHeader from "./SidebarHeader";
-import { linkGroups } from "./constants";
-function AdminLayout() {
-  const context = useLoaderData() as AdminContext;
-
-  if (
-    !context.user.endowments.includes(context.id) &&
-    !context.user.groups.includes("ap-admin")
-  ) {
-    return (
-      <div className="grid content-start place-items-center pt-40 pb-20">
-        <CircleAlert size={80} className="text-red" />
-        <p className="text-xl mt-8">Unauthorized</p>
-      </div>
-    );
-  }
-
-  return (
-    <div className="grid">
-      <Header classes="sticky z-40 top-[-1px]" />
-      <ModalContext>
-        <Layout
-          rootRoute={`${appRoutes.admin}/:id/`}
-          linkGroups={linkGroups}
-          sidebarHeader={<SidebarHeader endow={context.endow} />}
-          context={context}
-        />
-      </ModalContext>
-      <Footer />
-    </div>
-  );
-}
-
-const loader: LoaderFunction = async ({ request, params }) => {
-  const auth = await loadAuth();
-  if (!auth) return redirectToAuth(request);
-  if (auth) {
-    const id = parse(plusInt, params.id);
-    return {
-      user: auth,
-      id,
-      endow: getEndow(id, ["name", "logo"]),
-    } satisfies AdminContext;
-  }
-};
 
 export const adminRoute: RouteObject = {
   path: appRoutes.admin + "/:id",
-  element: <AdminLayout />,
-  loader,
+  lazy: () => import("./layout").then(convert),
   children: charityRoutes,
 };
