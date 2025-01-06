de937062703c84e57e6359f464eab6a0f8fe19f3
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-01-06 20:32:35 +0800

    charset and viewport meta

--- src/root-layout.tsx
+++ src/root-layout.tsx
@@ -17,6 +17,8 @@ export function Layout({ children }: PropsWithChildren<{ classes?: string }>) {
   return (
     <html lang="en">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.png" />
         <Meta />
         <Links />
