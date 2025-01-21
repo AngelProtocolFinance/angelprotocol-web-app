f6b89f9997cd1c5c0888e315b8755a46ddbaae7d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-01-21 17:30:09 +0800

    localstorage as cache adapter

--- src/api/cache.ts
+++ src/api/cache.ts
@@ -1,6 +1,7 @@
 import type { ClientLoaderFunction } from "@remix-run/react";
-import { cacheClientLoader } from "remix-client-cache";
+import { cacheClientLoader, createCacheAdapter } from "remix-client-cache";
 export { useCachedLoaderData } from "remix-client-cache";
+const { adapter } = createCacheAdapter(() => localStorage);
 export const clientLoader: ClientLoaderFunction = (args) =>
-  cacheClientLoader(args);
+  cacheClientLoader(args, { adapter });
 clientLoader.hydrate = true;
