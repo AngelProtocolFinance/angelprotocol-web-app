c69e0f3a3a7ebe45def54dc883463664d068172d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-01-21 15:58:16 +0800

    revert edge runtime

--- src/routes/cookie-consent[.]js.ts
+++ src/routes/cookie-consent[.]js.ts
@@ -1,5 +1,4 @@
 import cc from "vanilla-cookieconsent/dist/cookieconsent.umd.js?raw"; // Make sure to specify the full path
-export const config = { runtime: "edge" };
 export const loader = async () => {
   return new Response(cc, {
     headers: {
