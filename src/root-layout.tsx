50b2d1929112430447c8669f30e0c3619041732e
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-01-21 14:55:26 +0800

    external scripts

--- src/root-layout.tsx
+++ src/root-layout.tsx
@@ -1,12 +1,12 @@
 import { Links, Meta, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { PropsWithChildren } from "react";
+import { ClientOnly } from "remix-utils/client-only";
+import { ExternalScripts } from "remix-utils/external-scripts";
 import { Toaster } from "sonner";
-import { useCookieConsent } from "use-cookie-consent";
 import { useNProgress } from "use-nprogress";
 
 export function Layout({ children }: PropsWithChildren<{ classes?: string }>) {
   useNProgress();
-  useCookieConsent();
   return (
     <html lang="en">
       <head>
@@ -18,9 +18,10 @@ export function Layout({ children }: PropsWithChildren<{ classes?: string }>) {
         <style />
       </head>
       <body className="grid min-h-screen grid-rows-[1fr_0]">
-        <ScrollRestoration />
         {children}
+        <ScrollRestoration />
         <Scripts />
+        <ClientOnly>{() => <ExternalScripts />}</ClientOnly>
         <Toaster richColors position="top-right" closeButton />
       </body>
     </html>
--- src/root.tsx
+++ src/root.tsx
@@ -2,23 +2,60 @@ import { Outlet } from "@remix-run/react";
 import type { LinksFunction, MetaFunction } from "@vercel/remix";
 import { metas } from "helpers/seo";
 import nProgressStyles from "nprogress/nprogress.css?url";
-import ccbase from "vanilla-cookieconsent/dist/cookieconsent.css?url";
 import laira from "./assets/images/flying-character.webp";
 import cc from "./cookie-consent.css?url";
 import tailwind from "./index.css?url";
 import "@fontsource-variable/dm-sans";
 import "@fontsource-variable/quicksand";
 import "@fontsource/gochi-hand";
+import type { ExternalScriptsHandle } from "remix-utils/external-scripts";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: tailwind },
   { rel: "stylesheet", href: nProgressStyles },
-  { rel: "stylesheet", href: ccbase },
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.css",
+  },
   { rel: "stylesheet", href: cc },
 ];
 
 export const meta: MetaFunction = () => metas({});
 
+export const handle: ExternalScriptsHandle = {
+  scripts({ location }) {
+    if (import.meta.env.VITE_ENVIRONMENT !== "production") return [];
+    // we don't want bg consent banner showing on embeds
+    if (location.pathname.startsWith("/donate-widget")) return [];
+    return [
+      {
+        src: "https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js",
+      },
+      { src: "/scripts/cookie-consent.js" },
+      //functional cookies
+      { src: "/scripts/intercom.js", "data-category": "functional" },
+      //analytics
+      { src: "/scripts/gtm-init.js", "data-category": "analytics" },
+
+      //tracking
+      {
+        src: "/scripts/twitter-conversion-tracking.js",
+        "data-category": "tracking",
+      },
+      {
+        src: "/scripts/meta-pixel.js",
+        "data-category": "tracking",
+      },
+      { src: "/scripts/linkedin-tracking.js", "data-category": "tracking" },
+      { src: "/scripts/hotjar-tracking.js", "data-category": "tracking" },
+      {
+        id: "hs-script-loader",
+        src: "//js-eu1.hs-scripts.com/24900163.js",
+        "data-category": "tracking",
+      },
+    ];
+  },
+};
 export { loader } from "./root-loader";
 export { action } from "./root-action";
 export { Layout } from "./root-layout";
