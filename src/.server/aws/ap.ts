eb8f5bf1466251f34168846dbf32a9ebff141fb0
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-01-23 00:04:53 +0800

    remove reference

--- src/.server/aws/ap.ts
+++ src/.server/aws/ap.ts
@@ -1,5 +1,4 @@
 import awsLite from "@aws-lite/client";
-/// <reference types="@aws-lite/dynamodb-types" />
 import { aws_access_key_id, aws_region, aws_secret_access_key } from "../env";
 export const ap = await awsLite({
   /** @ts-ignore */
