418f08906b35a0e64260b8e42ff6fcbc380e037e
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-01-24 21:58:15 +0800

    endow balance fallback

--- src/api/get/endow-balance.ts
+++ src/api/get/endow-balance.ts
@@ -5,5 +5,20 @@ import { apes, ver } from "../api";
 
 export async function getEndowBalance(idParam: string | undefined) {
   const id = parse(plusInt, idParam);
-  return apes.get<EndowmentBalances>(`${ver(1)}/balances/${id}`).json();
+  const res = await apes.get<EndowmentBalances>(`${ver(1)}/balances/${id}`, {
+    throwHttpErrors: false,
+  });
+  if (!res.ok) {
+    return {
+      contributionsCount: 0,
+      donationsBal: 0,
+      payoutsMade: 0,
+      payoutsPending: 0,
+      sustainabilityFundBal: 0,
+      totalContributions: 0,
+      totalEarnings: 0,
+    };
+  }
+
+  return res.json();
 }
