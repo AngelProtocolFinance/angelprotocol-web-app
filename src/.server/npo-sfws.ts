7c1ce148d07c4842e1d8e7cb040e23f1e428b76d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-02-02 23:09:22 +0800

    sfws

---
+++ src/.server/npo-sfws.ts
@@ -0,0 +1,84 @@
+import type { BalanceTx } from "@better-giving/balance-tx";
+import { tables } from "@better-giving/types/list";
+import { addWeeks, format, subWeeks } from "date-fns";
+import { QueryCommand, apes } from "./aws/db";
+
+const env = "production";
+
+const key = (date: Date) => `${format(date, "yy")}${format(date, "II")}`;
+
+export const npoSfws = async (id: number, limit = 52 / 4) => {
+  const cmd = new QueryCommand({
+    TableName: tables["balance-txs"],
+    KeyConditionExpression: "PK = :pk AND begins_with(SK, :sk)",
+    ExpressionAttributeValues: {
+      ":pk": `SFW#${id}` satisfies BalanceTx.SFW.Key["PK"],
+      ":sk": env,
+    },
+    ScanIndexForward: false,
+    Limit: limit,
+  });
+  const items = await apes
+    .send(cmd)
+    .then<BalanceTx.SFW.DBRecord[]>((res) => (res.Items || []) as any);
+
+  if (items.length === 0) return [];
+
+  const map = items.reduce(
+    (acc, item) => {
+      acc[item.week] = item;
+      return acc;
+    },
+    {} as Record<string, BalanceTx.SFW.DBRecord>
+  );
+
+  const latest = new Date(items[0].date);
+  const oldest = subWeeks(latest, limit);
+
+  type Item = Omit<BalanceTx.SFW.DBRecord, "PK" | "SK">;
+
+  const filled: Item[] = [];
+  let currDate = oldest;
+  while (currDate <= latest) {
+    const currKey = key(currDate);
+    const prev = map[key(subWeeks(currDate, 1))];
+    const curr = map[currKey];
+
+    if (!curr) {
+      const start = prev ? prev.end : 0;
+      const end = 0;
+      const change = end - start;
+      const pct_change = start === 0 ? 0 : change / start;
+      const item: Item = {
+        date: "",
+        week: currKey,
+        endow_id: id,
+        environment: env,
+        flow: 0,
+        start,
+        end,
+        change,
+        pct_change,
+      };
+      filled.push(item);
+      currDate = addWeeks(currDate, 1);
+      continue;
+    }
+    filled.push((({ PK, SK, ...x }) => x)(curr));
+    currDate = addWeeks(currDate, 1);
+  }
+
+  const metered: (Item & { twr: number })[] = [];
+  for (let i = 1; i < filled.length; i++) {
+    const prev = metered.at(i - 1);
+    const curr = filled[i];
+
+    const sub =
+      (curr.end - (curr.start + curr.flow)) / (curr.start + curr.flow);
+    const twr = ((prev?.twr ?? 0) + 1) * (1 + sub) - 1;
+
+    metered.push({ ...curr, twr });
+  }
+
+  return metered;
+};
--- src/.server/npo-txs.ts
+++ src/.server/npo-txs.ts
@@ -1,32 +1,14 @@
+import type { BalanceTx } from "@better-giving/balance-tx";
 import { tables } from "@better-giving/types/list";
 import { QueryCommand, apes } from "./aws/db";
 import { env } from "./env";
-export const getNpoSf = async (id: number, numWeeks: number) => {
-  const cmd = new QueryCommand({
-    TableName: tables["balance-txs"],
-    KeyConditionExpression: "PK = :pk AND begins_with(SK, :sk)",
-    ExpressionAttributeValues: {
-      ":pk": `SFW#${id}`,
-      ":sk": env,
-    },
-    ScanIndexForward: false,
-    Limit: numWeeks,
-  });
-
-  return apes.send(cmd).then((res) => res.Items);
-};
 
 export const npoTxs = async (id: number, nextKey: string | null) => {
-  const startKey = nextKey
-    ? JSON.parse(Buffer.from(nextKey, "base64").toString())
-    : undefined;
-  console.log({ id, nextKey, startKey });
-
   const command = new QueryCommand({
     TableName: tables["balance-txs"],
     KeyConditionExpression: "PK = :PK AND begins_with(SK, :SK)",
     ExpressionAttributeValues: {
-      ":PK": `Tx#${+id}`,
+      ":PK": `Tx#${+id}` satisfies BalanceTx.DBRecord["PK"],
       ":SK": env,
     },
     ScanIndexForward: false,
@@ -37,7 +19,7 @@ export const npoTxs = async (id: number, nextKey: string | null) => {
   });
 
   const result = await apes.send(command);
-  const items = result.Items || [];
+  const items = (result.Items || []) as BalanceTx.DBRecord[];
 
   const toBase64 = (key: Record<string, any> | undefined) =>
     key ? Buffer.from(JSON.stringify(key)).toString("base64") : undefined;
