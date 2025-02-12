49388163cb91aa3549d70bd9eb49e749281b9d9f
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-02-12 14:38:56 +0800

    cta button text and link to

--- src/pages/home/hero/hero.tsx
+++ src/pages/home/hero/hero.tsx
@@ -26,10 +26,10 @@ const Hero = ({ classes = "" }) => {
 
       <div className="mt-8 isolate z-10 justify-self-center relative">
         <Link
-          to={appRoutes.register}
+          to={`${appRoutes.register}/welcome`}
           className="btn-blue ml-1 group active:translate-x-1 font-bold shadow-2xl inline-flex items-center px-10 py-3 gap-1 rounded-full text-lg font-heading"
         >
-          Start Fundraising Today
+          Join Us Today!
           <ArrowRight size={18} className="group-hover:translate-x-1" />
         </Link>
 
