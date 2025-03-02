fd3ed8eb85bc454d7c09b1a8440706e23672503c
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-03 03:40:03 +0800

    add pagination

--- src/pages/nonprofit-outreach/api.ts
+++ src/pages/nonprofit-outreach/api.ts
@@ -1,14 +1,31 @@
 import type { LoaderFunction } from "@vercel/remix";
+import type { NonprofitItem } from "types/mongodb/nonprofits";
 import { nonprofits } from ".server/mongodb/db";
 
+export interface LoaderData {
+  items: NonprofitItem[];
+  page: number;
+  pages: number;
+}
+
+const limit = 2;
 export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const page = Number(url.searchParams.get("page")) || 1;
+  const skip = (page - 1) * limit;
+
+  const all = await nonprofits.countDocuments({});
+  const pages = Math.ceil(all / limit);
   const items = await nonprofits
     .find({})
     .sort({ assets: -1 })
-    // .skip(skip)
-    .limit(5)
-    .toArray()
-    .then((x) => x.map((x) => ({ ...x, _id: x._id.toString() })));
+    .skip(skip)
+    .limit(limit)
+    .toArray();
 
-  return items;
+  return {
+    items,
+    page,
+    pages,
+  } satisfies LoaderData;
 };
