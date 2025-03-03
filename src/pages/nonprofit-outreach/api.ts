94f6f05415fa223bdc650d47ffc1222d595b0fed
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-03 17:52:05 +0800

    temp range filter

--- src/pages/nonprofit-outreach/api.ts
+++ src/pages/nonprofit-outreach/api.ts
@@ -3,11 +3,18 @@ import type { NonprofitItem } from "types/mongodb/nonprofits";
 import * as v from "valibot";
 import { nonprofits } from ".server/mongodb/db";
 
+interface Stats {
+  assets: { min: number | null; max: number | null };
+  income: { min: number | null; max: number | null };
+  revenue: { min: number | null; max: number | null };
+}
+
 export interface LoaderData {
   items: NonprofitItem[];
   page: number;
   size: number;
   num_items: number;
+  stats?: Stats;
 }
 
 const plusInt = v.pipe(
@@ -34,10 +41,34 @@ export const loader: LoaderFunction = async ({ request }) => {
     .limit(limit)
     .toArray();
 
+  const [agg] = await nonprofits
+    .aggregate([
+      {
+        $group: {
+          _id: null,
+          mina: { $min: "$assets" },
+          maxa: { $max: "$assets" },
+          mini: { $min: "$income" },
+          maxi: { $max: "$income" },
+          minr: { $min: "$revenue" },
+          maxr: { $max: "$revenue" },
+        },
+      },
+    ])
+    .toArray();
+
+  const stats = agg
+    ? {
+        assets: { min: agg.mina, max: agg.maxa },
+        income: { min: agg.mini, max: agg.maxi },
+        revenue: { min: agg.minr, max: agg.maxr },
+      }
+    : undefined;
   return {
     items,
     page,
     size: limit,
     num_items: all,
+    stats,
   } satisfies LoaderData;
 };
