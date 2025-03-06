32aef353f3695a9166a97205af38cb67dbaf0fa1
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-07 00:43:05 +0800

    types update

--- src/.server/mongodb/db.ts
+++ src/.server/mongodb/db.ts
@@ -2,7 +2,9 @@ import { MongoClient } from "mongodb";
 import type { NonprofitItem } from "types/mongodb/nonprofits";
 import { mongo_url } from ".server/env";
 
-const db = new MongoClient(mongo_url).connect().then((c) => c.db("database"));
+const db = new MongoClient(mongo_url)
+  .connect()
+  .then((c) => c.db("better-giving"));
 export const nonprofits = await db.then((d) =>
   d.collection<NonprofitItem>("nonprofits")
 );
