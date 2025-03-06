550d843e0be25c7614d9920f42feaa0291465c3d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-07 00:43:05 +0800

    types update

--- src/pages/nonprofit-outreach/index.tsx
+++ src/pages/nonprofit-outreach/index.tsx
@@ -14,47 +14,82 @@ export default function Page() {
   const [, setParams] = useSearchParams();
 
   return (
-    <div className="xl:mx-auto xl:container pt-16 font-heading text-sm">
-      <table className="self-start border-collapse">
-        <thead>
-          <tr>
-            <th className="text-left p-2 border border-gray-l3">Ein</th>
-            <th className="text-left p-2 border border-gray-l3">Name</th>
-            <th className="text-left p-2 border border-gray-l3">Assets</th>
-            <th className="text-left p-2 border border-gray-l3">Income</th>
-            <th className="text-left p-2 border border-gray-l3">Revenue</th>
-            <th className="text-left p-2 border border-gray-l3">City</th>
-            <th className="text-left p-2 border border-gray-l3">State</th>
-            <th className="text-left p-2 border border-gray-l3">State</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.items.map((d, i) => (
-            <tr key={d._id + i}>
-              <td className="border border-gray-l3 p-2">{d.ein}</td>
-              <td className="border border-gray-l3 p-2">{d.name}</td>
-              <td className="border border-gray-l3 p-2 text-sm">
-                {d.assets && !Number.isNaN(d.assets)
-                  ? _usd.format(d.assets)
-                  : "-"}
-              </td>
-              <td className="border border-gray-l3 p-2 text-sm">
-                {d.income && !Number.isNaN(d.income)
-                  ? _usd.format(d.income)
-                  : "-"}
-              </td>
-              <td className="border border-gray-l3 p-2 text-sm">
-                {d.revenue && !Number.isNaN(d.revenue)
-                  ? _usd.format(d.revenue)
-                  : "-"}
-              </td>
-              <td className="border border-gray-l3 p-2">{d.city}</td>
-              <td className="border border-gray-l3 p-2">{d.state}</td>
-              <td className="border border-gray-l3 p-2">{d.country}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="xl:mx-auto xl:container py-16 font-heading text-sm">
+      <div className="w-full">
+        <div className="overflow-x-auto relative">
+          <table className="self-start border-collapse overflow-x-auto [&_th]:text-left [&_td]:align-top [&_td,&_th]:p-2 [&_td,&_th]:border [&_td,&_th]:border-gray-l3">
+            <thead>
+              <tr>
+                <th>EIN</th>
+                <th>Name</th>
+                <th>Asset code</th>
+                <th>Assets</th>
+                <th>Income code</th>
+                <th>Income</th>
+                <th>Revenue</th>
+                <th>City</th>
+                <th>State</th>
+                <th>Country</th>
+                <th>NTEE code</th>
+                <th>In care of</th>
+                <th>Group exemption number</th>
+                <th>Subsection code</th>
+                <th>Affiliation code</th>
+                <th>Classification code</th>
+                <th>Deductability code</th>
+                <th>Foundation code</th>
+                <th>Activity code</th> <th>Organization code</th>
+                <th>Exempt organinzation status code</th>
+                <th>Filing requirement code</th>
+                <th>Sort Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.items.map((d, i) => (
+                <tr key={d._id + i}>
+                  <td>{d.ein}</td>
+                  <td>{d.name}</td>
+                  <td>{d.asset_code}</td>
+                  <td>
+                    {d.asset_amount && !Number.isNaN(d.asset_amount)
+                      ? _usd.format(d.asset_amount)
+                      : "-"}
+                  </td>
+                  <td>{d.income_code}</td>
+                  <td>
+                    {d.income_amount && !Number.isNaN(d.income_amount)
+                      ? _usd.format(d.income_amount)
+                      : "-"}
+                  </td>
+                  <td>
+                    {d.revenue_amount && !Number.isNaN(d.revenue_amount)
+                      ? _usd.format(d.revenue_amount)
+                      : "-"}
+                  </td>
+                  <td>{d.city}</td>
+                  <td>{d.state}</td>
+                  <td>{d.mailing_address?.country}</td>
+
+                  <td>{d.ntee_code}</td>
+                  <td>{d.in_care_of_name?.replace("%", "")}</td>
+                  <td>{d.group_exemption_number}</td>
+                  <td>{d.subsection_code}</td>
+                  <td>{d.affilation_code}</td>
+                  <td>{d.classification_code}</td>
+                  <td>{d.deductibility_code}</td>
+                  <td>{d.foundation_code}</td>
+                  <td>{d.activity_code}</td>
+                  <td>{d.organization_code}</td>
+                  <td>{d.exempt_organization_status_code}</td>
+                  <td>{d.filing_requirement_code}</td>
+
+                  <td>{d.sort_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
       <Paginator
         className="mt-4"
         onPageChange={(page: number) =>
