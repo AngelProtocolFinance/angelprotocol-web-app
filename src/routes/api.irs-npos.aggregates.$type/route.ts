6ff45b8edc29a91a0bb83f5cd54a0a793c3e3449
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-07 08:13:46 +0800

    aggregates endpoint

---
+++ src/routes/api.irs-npos.aggregates.$type/route.ts
@@ -0,0 +1,18 @@
+import type { LoaderFunction } from "@vercel/remix";
+import { distinctFilters } from "types/mongodb/nonprofits";
+import { nonprofits } from ".server/mongodb/db";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const type = params.type;
+  if (!type)
+    return new Response("aggregate type not specified", { status: 400 });
+  //return unique state
+  if (type in distinctFilters) {
+    const states = await nonprofits
+      .distinct(distinctFilters[type as keyof typeof distinctFilters])
+      .then((s) => s.filter((x) => x != null));
+    return new Response(JSON.stringify(states));
+  }
+
+  return new Response(null, { status: 404 });
+};
