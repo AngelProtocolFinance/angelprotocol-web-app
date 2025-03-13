01fa6c8786baadb04ba772691eee19fa50d880ea
jp-mariano <juanpaolo.mariano28@gmail.com>
2025-03-13 15:42:52 +0800

    Add 'toLocaleDateString()'

--- src/helpers/date-format.ts
+++ src/helpers/date-format.ts
@@ -2,5 +2,6 @@ import { format } from "date-fns";
 
 /** @see {@link https://date-fns.org/v4.1.0/docs/format} */
 export function dateFormat(date: string | Date) {
-  return format(date, "PP");
+  const localeDate = new Date(date).toLocaleDateString();
+  return format(localeDate, "PP");
 }
