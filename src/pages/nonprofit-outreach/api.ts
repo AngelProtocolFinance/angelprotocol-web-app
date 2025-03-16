d63cec0c6f8d836e1bb73fb4513ba8c2d77b388a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-16 18:26:42 +0800

    classification code filter

--- src/pages/nonprofit-outreach/api.ts
+++ src/pages/nonprofit-outreach/api.ts
@@ -19,24 +19,45 @@ export interface LoaderData {
 class Filter {
   filter: any = {};
 
-  set_opts(key: string, csv: string) {
-    // const conds: any = [];
-    const opts = csv.split(",");
-    const blank_idx = opts.findIndex((x) => x === "blank");
-    const non_blank_idx = opts.findIndex((x) => x === "exist");
+  private extract_blank_exists(key: string, csv: string): string[] {
+    const vals = csv.split(",");
+    const blank_idx = vals.findIndex((x) => x === "blank");
+    const exists_idx = vals.findIndex((x) => x === "exists");
+
+    if (blank_idx !== -1 && exists_idx !== -1) {
+      vals.splice(blank_idx, 1);
+      vals.splice(exists_idx, 1);
+      return vals;
+    }
+
     if (blank_idx !== -1) {
       this.filter.$and ||= [];
       this.filter.$and.push({ [key]: { $exists: false } });
-      opts.splice(blank_idx, 1);
+      vals.splice(blank_idx, 1);
     }
-    if (non_blank_idx !== -1) {
+    if (exists_idx !== -1) {
       this.filter.$and ||= [];
       this.filter.$and.push({ [key]: { $exists: true } });
-      opts.splice(non_blank_idx, 1);
+      vals.splice(exists_idx, 1);
     }
+    return vals;
+  }
+
+  set_opts(key: string, csv: string) {
+    const opts = this.extract_blank_exists(key, csv);
     if (opts.length > 0) {
       this.filter.$and ||= [];
       this.filter.$and.push({ [key]: { $in: opts } });
+    }
+  }
+  set_texts(key: string, csv: string) {
+    const texts = this.extract_blank_exists(key, csv);
+    const distinct = Array.from(new Set(texts).values());
+    if (distinct.length > 0) {
+      this.filter.$and ||= [];
+      this.filter.$and.push({
+        $and: distinct.map((t) => ({ [key]: { $regex: t, $options: "i" } })),
+      });
     }
   }
 
@@ -60,6 +81,7 @@ export const loader: LoaderFunction = async ({ request }) => {
     deductibility_code = "",
     deductibility_code_pub78 = "",
     ntee_code = "",
+    classification_code = "",
   } = Object.fromEntries(url.searchParams.entries());
 
   const filter = new Filter();
@@ -75,6 +97,8 @@ export const loader: LoaderFunction = async ({ request }) => {
   if (deductibility_code_pub78)
     filter.set_opts("deductibility_code_pub78", deductibility_code_pub78);
   if (ntee_code) filter.set_opts("ntee_code", ntee_code);
+  if (classification_code)
+    filter.set_texts("classification_code", classification_code);
 
   const skip = (+page - 1) * +limit;
 
