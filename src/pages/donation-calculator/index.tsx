812221dcd0a2775195fe33ae2134fcc1bfabe06a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-22 17:24:13 +0800

    heading

--- src/pages/donation-calculator/index.tsx
+++ src/pages/donation-calculator/index.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <div>Donation calculator</div>;
+  return (
+    <div className="xl:container px-5 mx-auto mt-4 pt-8">
+      <h1 className="text-3xl text-blue-d1 text-center">
+        Donation Processing & Investment Calculator
+      </h1>
+    </div>
+  );
 }
--- vite.config.js
+++ vite.config.js
@@ -199,6 +199,7 @@ const rmx = remix({
           r(":verdict", "./pages/application/review-route.tsx");
           r("success", "./pages/application/success-prompt.tsx");
         });
+        r("donation-calculator", "./pages/donation-calculator/index.tsx");
       });
       r("donate-widget", "./pages/donate-widget/widget-context.tsx", () => {
         r(":id", "./pages/donate-widget/index.ts");
@@ -206,7 +207,6 @@ const rmx = remix({
           id: "widget-donate-thanks",
         });
       });
-      r("donation-calculator", "./pages/donation-calculator/index.tsx");
     });
   },
 });
