5c79cd216663e4ac8640af4cda3f3dceda7bd2ad
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-22 14:51:38 +0800

    rename npo

--- src/pages/admin/endow-update-action.ts
+++ src/pages/admin/endow-update-action.ts
@@ -1,7 +1,13 @@
+import type { EndowUpdate } from "@better-giving/endowment";
+import {
+  endowIdParam,
+  endowUpdate as endowUpdateSchema,
+} from "@better-giving/endowment/schema";
 import { type ActionFunction, redirect } from "@vercel/remix";
-import { ap, ver } from "api/api";
 import type { ActionData } from "types/action";
+import { parse } from "valibot";
 import { cognito, toAuth } from ".server/auth";
+import { editNpo, getNpo } from ".server/npo";
 
 type Next = { success: string } | { redirect: string };
 
@@ -10,11 +16,22 @@ export const endowUpdate =
   async ({ params, request }) => {
     const { user, headers } = await cognito.retrieve(request);
     if (!user) return toAuth(request, headers);
+    const id = parse(endowIdParam, params.id);
 
-    await ap.patch(`${ver(9)}/endowments/${params.id}`, {
-      headers: { authorization: user.idToken },
-      json: await request.json(),
-    });
+    const update: EndowUpdate = await request.json();
+    const parsed = parse(endowUpdateSchema, update);
+
+    // check if new slug is already taken
+    if (parsed.slug) {
+      const res = await getNpo(parsed.slug);
+      if (res) {
+        return {
+          __err: `Slug ${parsed.slug} is already taken`,
+        };
+      }
+    }
+
+    await editNpo(id, parsed);
 
     if ("success" in next) {
       return { __ok: next.success } satisfies ActionData;
