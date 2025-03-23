5676d2b1370a55705c3d474b6ca094b7a12630ab
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-23 23:18:52 +0800

    summary mobile

--- src/pages/donation-calculator/view/summary.tsx
+++ src/pages/donation-calculator/view/summary.tsx
@@ -10,47 +10,53 @@ interface Props extends View {
 export function Summary({ classes = "", ...v }: Props) {
   const y1 = v.projection[0];
   return (
-    <div className={`${classes} p-6 shadow-sm rounded-lg bg-white`}>
-      <h2 className="text-xl font-bold mb-4">Annual Impact Summary</h2>
+    <div className={`${classes} p-6 shadow-sm rounded-lg bg-white @container`}>
+      <h2 className="text-lg sm:text-xl font-bold mb-4">
+        Annual Impact Summary
+      </h2>
 
       <div className="mb-6">
         <p className="text-gray">Current Online Donations</p>
-        <p className="text-xl font-bold">{toUsd(v.amount)}</p>
+        <p className="text-lg sm:text-xl font-bold">{toUsd(v.amount)}</p>
       </div>
 
       <div className="border-t border-gray-l3 my-6"></div>
 
-      <h2 className="text-xl font-bold mb-4">Donation Processing Impact</h2>
+      <h2 className="text-lg sm:text-xl font-bold mb-4">
+        Donation Processing Impact
+      </h2>
 
-      <div className="grid grid-cols-2 bg-gray-l5 rounded-lg mb-6">
-        <div className="p-5 border-r border-gray-l3">
-          <p className="text-gray mb-2">Current Amount Received</p>
-          <p className="text-xl font-bold text-red">
+      <div className="grid @md:grid-cols-2 @md:bg-gray-l5 rounded-lg mb-6 divide-gray-l3 divide-y @md:divide-x @md:divide-y-0">
+        <div className="@md:p-5 pb-2">
+          <p className="max-sm:text-sm text-gray mb-2">
+            Current Amount Received
+          </p>
+          <p className="text-lg sm:text-xl font-bold text-red">
             {toUsd(v.ogNet)}{" "}
-            <span className="text-xl">(-{toUsd(v.ogFees)})</span>
+            <span className="text-lg sm:text-xl">(-{toUsd(v.ogFees)})</span>
           </p>
         </div>
-        <div className="p-5">
-          <p className="text-gray mb-2">With Better Giving</p>
-          <p className="text-xl font-bold text-green">
+        <div className="@md:p-5 pt-2">
+          <p className="max-sm:text-sm text-gray mb-2">With Better Giving</p>
+          <p className="text-lg sm:text-xl font-bold text-green">
             {toUsd(v.bgNet)}{" "}
-            <span className="text-xl">(+{toUsd(v.diff)}) </span>
+            <span className="text-lg sm:text-xl">(+{toUsd(v.diff)}) </span>
           </p>
         </div>
       </div>
 
-      <h2 className="text-xl font-bold mb-4">
+      <h2 className="text-lg sm:text-xl font-bold mb-4 text-balance">
         Donation Processing Impact Details
       </h2>
 
       <div className="space-y-3 mb-6">
-        <div className="flex justify-between items-center">
-          <p className="text-gray-d1">Fee Savings:</p>
+        <div className="flex gap-x-4 @max-md:flex-col justify-between @md:items-center">
+          <p className="max-sm:text-sm text-gray-d1">Fee Savings:</p>
           <p className="font-semibold">{toUsd(v.diff - v.ogMissed)}</p>
         </div>
 
-        <div className="flex justify-between items-center">
-          <p className="text-gray-d1">
+        <div className="flex gap-x-4 @max-md:flex-col justify-between @md:items-center">
+          <p className="max-sm:text-sm text-gray-d1 text-balance">
             Additional donations from expanded payment types:
             <Tooltip
               tip={
@@ -73,39 +79,47 @@ export function Summary({ classes = "", ...v }: Props) {
           <p className="font-semibold">{toUsd(v.ogMissed)}</p>
         </div>
 
-        <div className="flex justify-between items-center text-green font-medium">
-          <p className="">Subtotal - Processing Impact:</p>
+        <div className="flex gap-x-4 @max-md:flex-col justify-between @md:items-center text-green font-medium">
+          <p className="max-sm:text-sm">Subtotal - Processing Impact:</p>
           <p className=" font-semibold">{toUsd(v.diff)}</p>
         </div>
       </div>
 
       <div className="border-t border-gray-l3 my-6"></div>
 
-      <h2 className="text-xl font-bold mb-4">First-Year Investment Impact</h2>
+      <h2 className="text-lg sm:text-xl font-bold mb-4">
+        First-Year Investment Impact
+      </h2>
 
       <div className="space-y-3 mb-6">
-        <div className="flex justify-between items-center">
-          <p className=" text-gray-d1">Savings Account Growth (4%):</p>
+        <div className="flex gap-x-4 @max-md:flex-col justify-between @md:items-center">
+          <p className="max-sm:text-sm text-gray">
+            Savings Account Growth (4%):
+          </p>
           <p className=" font-semibold">{toUsd(y1.liq)}</p>
         </div>
 
-        <div className="flex justify-between items-center">
-          <p className=" text-gray-d1">Sustainability Fund Growth (20%):</p>
+        <div className="flex gap-x-4 @max-md:flex-col justify-between @md:items-center">
+          <p className="max-sm:text-sm text-gray">
+            Sustainability Fund Growth (20%):
+          </p>
           <p className=" font-semibold">{toUsd(y1.lock)}</p>
         </div>
 
-        <div className="flex justify-between items-center text-[#A020F0] font-medium">
-          <p className="">Subtotal - Investment Impact:</p>
+        <div className="flex gap-x-4 @max-md:flex-col justify-between @md:items-center text-[#A020F0] font-medium">
+          <p className="max-sm:text-sm text-gray">
+            Subtotal - Investment Impact:
+          </p>
           <p className=" font-semibold">{toUsd(y1.total)}</p>
         </div>
       </div>
 
-      <div className="bg-green-l5 p-6 rounded-lg flex items-center gap-4">
+      <div className="bg-green-l5 p-4 @md:p-6 rounded-lg @md:flex items-center gap-4">
         <div className="text-green">
-          <TrendingUp size={40} />
+          <TrendingUp size={40} className="size-8 sm:size-10" />
         </div>
         <div>
-          <p className="text-xl font-bold uppercase">Total Annual Impact</p>
+          <p className="sm:text-xl font-bold uppercase">Total Annual Impact</p>
           <p className="text-lg font-bold text-green-d1">
             {toUsd(y1.total + v.diff)}
           </p>
