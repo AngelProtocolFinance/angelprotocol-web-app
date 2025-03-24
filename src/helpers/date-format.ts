72c54654139efdfafc011f5403e40490143db4af
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-24 18:30:48 +0800

    pass date to formatter

--- src/helpers/date-format.ts
+++ src/helpers/date-format.ts
@@ -2,6 +2,5 @@ import { format } from "date-fns";
 
 /** @see {@link https://date-fns.org/v4.1.0/docs/format} */
 export function dateFormat(date: string | Date) {
-  const localeDate = new Date(date).toLocaleDateString();
-  return format(localeDate, "PP");
+  return format(new Date(date), "PP");
 }
