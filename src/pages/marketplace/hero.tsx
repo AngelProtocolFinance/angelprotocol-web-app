f34c8b3a50ae61b701c4fc04b7fcffa051342997
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-25 22:48:22 +0800

    marketplace hero typography

--- src/pages/marketplace/hero.tsx
+++ src/pages/marketplace/hero.tsx
@@ -7,14 +7,14 @@ export default function Hero({ classes = "" }: { classes?: string }) {
       className={`${classes} grid-cols-1 lg:grid-cols-[3fr_2fr] gap-4 justify-items-center lg:content-start`}
     >
       <div className="self-center">
-        <p className="max-lg:text-center uppercase mb-2 text-blue-d1">
+        <p className="max-lg:text-center uppercase mb-2 text-blue-d1 font-bold font-heading">
           Fund nonprofits that matter
         </p>
-        <h1 className="mb-4 font-semibold capitalize text-center text-2xl lg:text-4xl text-balance lg:text-left">
+        <h1 className="mb-4 capitalize text-center text-2xl lg:text-4xl text-balance lg:text-left">
           Giving made smarter. Impact made stronger.
         </h1>
 
-        <p className="text-xl lg:text-2xl max-lg:text-center text-balance text-gray-d2">
+        <p className="text-xl lg:text-2xl max-lg:text-center text-balance text-gray-d1">
           Find and support charities, nonprofits, universities, and faith-based
           organizations—all in one place.
         </p>
