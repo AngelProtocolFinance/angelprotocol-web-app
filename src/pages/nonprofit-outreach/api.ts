eec0763ddba83a87c6e02287dff757f8b3e3ce9d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-03-26 19:42:51 +0800

    export endpoint

--- src/pages/nonprofit-outreach/api.ts
+++ src/pages/nonprofit-outreach/api.ts
@@ -1,4 +1,5 @@
 import type { LoaderFunction } from "@vercel/remix";
+import { nposParams } from "helpers/npos-params";
 import type { NonprofitItem } from "types/mongodb/nonprofits";
 import { nonprofits } from ".server/mongodb/db";
 
@@ -9,154 +10,24 @@ export interface LoaderData {
   num_items: number;
 }
 
-class Filter {
-  filter: any = {};
-
-  private extract_blank_exists(key: string, csv: string): string[] {
-    const vals = csv.split(",");
-    const blank_idx = vals.findIndex((x) => x === "blank");
-    const exists_idx = vals.findIndex((x) => x === "exists");
-
-    if (blank_idx !== -1 && exists_idx !== -1) {
-      vals.splice(blank_idx, 1);
-      vals.splice(exists_idx, 1);
-      return vals;
-    }
-
-    if (blank_idx !== -1) {
-      this.filter.$and ||= [];
-      this.filter.$and.push({ [key]: { $exists: false } });
-      vals.splice(blank_idx, 1);
-    }
-    if (exists_idx !== -1) {
-      this.filter.$and ||= [];
-      this.filter.$and.push({ [key]: { $exists: true, $not: { $size: 0 } } });
-      vals.splice(exists_idx, 1);
-    }
-    return vals;
-  }
-
-  opts<T extends string>(kv: { [key in T]: T }) {
-    const [[k, v]] = Object.entries(kv);
-    if (!v) return;
-    const opts = this.extract_blank_exists(k, v as string);
-    if (opts.length > 0) {
-      this.filter.$and ||= [];
-      this.filter.$and.push({ [k]: { $in: opts } });
-    }
-  }
-  texts<T extends string>(kv: { [key in T]: T }) {
-    const [[k, v]] = Object.entries(kv);
-    if (!v) return;
-    const texts = this.extract_blank_exists(k, v as string);
-    const distinct = Array.from(new Set(texts).values());
-    if (distinct.length > 0) {
-      this.filter.$and ||= [];
-      this.filter.$and.push({
-        $and: distinct.map((t) => ({ [k]: { $regex: t, $options: "i" } })),
-      });
-    }
-  }
-  starts_with<T extends string>(kv: { [key in T]: T }) {
-    const [[k, v]] = Object.entries(kv);
-    if (!v) return;
-    const starts = this.extract_blank_exists(k, v as string);
-    if (starts.length > 0) {
-      this.filter.$and ||= [];
-      this.filter.$and.push({
-        $and: starts.map((t) => ({ [k]: { $regex: `^${t}`, $options: "i" } })),
-      });
-    }
-  }
-  range<T extends string>(kv: { [key in T]: T }) {
-    const [[k, v]] = Object.entries(kv);
-    if (!v) return;
-    const range = this.extract_blank_exists(k, v as string);
-    if (range.length > 1) {
-      this.filter.$and ||= [];
-      this.filter.$and.push({
-        [k]: { $gte: Number(range[0]), $lte: Number(range[1]) },
-      });
-    }
-  }
-
-  get all() {
-    return this.filter;
-  }
-}
-
 export const loader: LoaderFunction = async ({ request }) => {
-  const url = new URL(request.url);
-  const {
-    page = "1",
-    limit = "10",
-    sort = "",
-    asset_code,
-    income_code,
-    website,
-    contacts,
-    social_media,
-    donation_platform,
-    state,
-    country,
-    subsection_code,
-    affilation_code,
-    deductibility_code,
-    deductibility_code_pub78,
-    ntee_code,
-    classification_code,
-    foundation_code,
-    activity_code,
-    exempt_organization_status_code,
-    organization_code,
-    filing_requirement_code,
-    income_amount,
-    asset_amount,
-    revenue_amount,
-    sort_name,
-  } = Object.fromEntries(url.searchParams.entries());
-
-  const filter = new Filter();
-  filter.opts({ asset_code });
-  filter.opts({ income_code });
-  filter.opts({ website });
-  filter.opts({ contacts });
-  filter.opts({ social_media });
-  filter.opts({ donation_platform });
-  filter.opts({ state });
-  filter.opts({ country });
-  filter.opts({ subsection_code });
-  filter.opts({ affilation_code });
-  filter.opts({ deductibility_code });
-  filter.opts({ deductibility_code_pub78 });
-  filter.texts({ classification_code });
-  filter.texts({ activity_code });
-  filter.opts({ foundation_code });
-  filter.opts({ organization_code });
-  filter.opts({ exempt_organization_status_code });
-  filter.opts({ filing_requirement_code });
-  filter.opts({ sort_name });
-  filter.starts_with({ ntee_code });
-  filter.range({ income_amount });
-  filter.range({ asset_amount });
-  filter.range({ revenue_amount });
-
-  const skip = (+page - 1) * +limit;
+  const { filter, page, limit, sort } = nposParams(request);
+  const skip = (page - 1) * limit;
   const [sort_key, sort_dir] = sort.split("+");
 
   const collection = await nonprofits;
   const items = await collection
-    .find(filter.all)
+    .find(filter)
     .sort(sort ? { [sort_key]: sort_dir === "asc" ? 1 : -1 } : {})
     .skip(skip)
     .limit(+limit)
     .toArray();
-  const count = await collection.countDocuments(filter.all);
+  const count = await collection.countDocuments(filter);
 
   return {
     items,
-    page: +page,
-    size: +limit,
+    page,
+    size: limit,
     num_items: count,
   } satisfies LoaderData;
 };
