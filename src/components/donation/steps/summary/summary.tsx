ed6efcf189667f4aafd409d036bbf0e87e623dca
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-02 15:17:22 +0800

    donor public msg

--- src/components/donation/steps/summary/summary.tsx
+++ src/components/donation/steps/summary/summary.tsx
@@ -70,7 +70,7 @@ export default function Summary(props: SummaryStep) {
             title: initDonorTitleOption,
             zipCode: "",
             streetAddress: "",
-            isPublic: false,
+            isPublic: true,
             publicMsg: "",
           }
         }
