93c36bb36f52deb33632315b586a1cd2ad111098
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-08 16:39:38 +0800

    pages

---
+++ src/pages/donation-calculator/pdf-export/index.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="w-[49.6125rem] bg-gray-l5 border-x mx-auto">
+      <div className="h-[70.16625rem] bg-red">page1</div>
+      <div className="h-[70.16625rem] bg-blue">page2</div>
+      <div className="h-[70.16625rem] bg-green">page3</div>
+      <div className="h-[70.16625rem] bg-amber">page4</div>
+    </div>
+  );
+}
