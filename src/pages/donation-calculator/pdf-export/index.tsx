65391f86cc8482f85d9647086b015355aa3eea8e
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-08 21:49:35 +0800

    page 2 heading

--- src/pages/donation-calculator/pdf-export/index.tsx
+++ src/pages/donation-calculator/pdf-export/index.tsx
@@ -1,10 +1,11 @@
 import { Page1 } from "./page1";
+import { Page2 } from "./page2";
 
 export default function Page() {
   return (
     <div className="w-full bg-gray-l5 border-x mx-auto">
       <Page1 />
-      <div className="h-[70.16625rem] bg-blue">page2</div>
+      <Page2 />
       <div className="h-[70.16625rem] bg-green">page3</div>
       <div className="h-[70.16625rem] bg-amber">page4</div>
     </div>
---
+++ src/pages/donation-calculator/pdf-export/page2.tsx
@@ -0,0 +1,18 @@
+export function Page2() {
+  return (
+    <div className="w-full aspect-[297/210] pt-8">
+      <div className="grid grid-cols-[auto_1fr] gap-x-4 items-center px-6">
+        <h2 className="text-blue text-3xl font-semibold uppercase">
+          Total 5 - 10 - 15 - 20 years financial advantage
+        </h2>
+        <div className="h-0.5 bg-blue" />
+        <p className="text-blue text-3xl font-semibold uppercase">
+          (Estimated Predictions)
+        </p>
+      </div>
+      <p className="text-2xl px-6 font-bold text-gray-d1 mt-4">
+        Compound Growth = Exponential Impact
+      </p>
+    </div>
+  );
+}
