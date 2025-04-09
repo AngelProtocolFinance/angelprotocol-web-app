738e03980b671da0b3da10f5cac5f1733434bd62
jp-mariano <juanpaolo.mariano28@gmail.com>
2025-04-09 14:52:01 +0800

    'getDonationMessage'

---
+++ src/.server/donation-message.ts
@@ -0,0 +1,16 @@
+import type { DonationMessage } from "@better-giving/donation/donation-message";
+import { tables } from "@better-giving/types/list";
+import { GetCommand, apes } from "./aws/db";
+
+export const getDonationMessage = async (
+  id: DonationMessage.PrimaryKey["PK"]
+): Promise<DonationMessage.DBRecord | [number, string]> => {
+  const cmd = new GetCommand({
+    TableName: tables.donation_messages,
+    Key: { PK: id, SK: id } satisfies DonationMessage.PrimaryKey,
+  });
+  const dm = await apes.send(cmd);
+  return dm.Item
+    ? (dm.Item as DonationMessage.DBRecord)
+    : [404, "Donation message not found"];
+};
