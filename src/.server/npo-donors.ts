a73d3abe18619770f003f6bf58cef2f837e5dbd6
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-09 19:21:53 +0800

    endpoint

--- src/.server/npo-donors.ts
+++ src/.server/npo-donors.ts
@@ -1,23 +1,30 @@
 import { QueryCommand, apes } from "./aws/db";
+import { env } from "./env";
 
-export const getDonors = async (
-  recipient_id: string /** uuid or number id */
+export const npoDonors = async (
+  recipient_id: string /** uuid or number id */,
+  nextKey: string | null
 ) => {
   const cmd = new QueryCommand({
-    TableName: "donor_messages",
+    TableName: "donation_messages",
     IndexName: "recipient-env-gsi",
     KeyConditionExpression: "#pk = :pk",
     ExpressionAttributeNames: {
       "#pk": "gsi1PK",
     },
     ExpressionAttributeValues: {
-      ":pk": `Recipient#${recipient_id}`,
+      ":pk": `Recipient#${recipient_id}#${env}`,
     },
+    ExclusiveStartKey: nextKey
+      ? JSON.parse(Buffer.from(nextKey, "base64").toString())
+      : undefined,
   });
 
   const res = await apes.send(cmd);
 
-  const items = (res.Items || []).map(({ PK, SK, gsi1PK, gsiSK, ...x }) => x);
+  const items = (res.Items || []).map(
+    ({ PK, SK, gsi1PK, gsi1SK, donor_id, env, ...x }) => x
+  );
   const toBase64 = (key: Record<string, any> | undefined) =>
     key ? Buffer.from(JSON.stringify(key)).toString("base64") : undefined;
 
