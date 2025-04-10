37387c6c73b239a58e7e3f2d077c3c847052e084
jp-mariano <juanpaolo.mariano28@gmail.com>
2025-04-10 12:47:29 +0800

    Exclude DB keys for 'getDonationMessage'

--- src/.server/donation-message.ts
+++ src/.server/donation-message.ts
@@ -1,18 +1,26 @@
-import type { DonationMessage } from "@better-giving/donation/donation-message";
+import type {
+  DonationMessage,
+  GSI1 as Index,
+} from "@better-giving/donation/donation-message";
 import { tables } from "@better-giving/types/list";
 import { DeleteCommand, GetCommand, apes } from "./aws/db";
 
+interface DM
+  extends DonationMessage.NonKeyAttributes,
+    Pick<Index.Key, "gsi1SK"> {}
+
 export const getDonationMessage = async (
   id: DonationMessage.PrimaryKey["PK"]
-): Promise<DonationMessage.DBRecord | [number, string]> => {
+): Promise<DM | [number, string]> => {
   const cmd = new GetCommand({
     TableName: tables.donation_messages,
     Key: { PK: id, SK: id } satisfies DonationMessage.PrimaryKey,
   });
-  const dm = await apes.send(cmd);
-  return dm.Item
-    ? (dm.Item as DonationMessage.DBRecord)
-    : [404, "Donation message not found"];
+  const res = await apes.send(cmd);
+  if (!res.Item) return [404, "Donation message not found"];
+
+  const { PK, SK, gsi1PK, ...dm } = res.Item as DonationMessage.DBRecord;
+  return dm;
 };
 
 export const delDonationMessage = async (
