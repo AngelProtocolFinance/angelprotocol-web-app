1242bfe84d489df765d2a2ca697a54a02f979be2
jp-mariano <juanpaolo.mariano28@gmail.com>
2025-04-10 15:25:48 +0800

    Fix correct recording of 'id' and 'date'

--- src/routes/helpers/db.ts
+++ src/routes/helpers/db.ts
@@ -1,5 +1,8 @@
 import crypto from "node:crypto";
-import type { DonationMessage } from "@better-giving/donation/donation-message";
+import type {
+  DMKey,
+  DonationMessage,
+} from "@better-giving/donation/donation-message";
 import type { DonationMessageParams } from "routes/types";
 import { env } from ".server/env";
 
@@ -12,19 +15,21 @@ export const buildDonationMsg = ({
   transaction_id,
   usd_value,
 }: DonationMessageParams): DonationMessage.DBRecord => {
-  const message_id: DonationMessage.PrimaryKey["PK"] = `DM#${crypto.randomUUID()}`;
+  const uuid = crypto.randomUUID();
+  const message_id: DMKey = `DM#${uuid}`;
   return {
     PK: message_id,
     SK: message_id,
     gsi1PK: `Recipient#${recipient_id}#${env}`,
     gsi1SK: date,
     amount: usd_value,
+    date,
     donation_id: transaction_id,
     donor_id,
     donor_message,
     donor_name,
     env,
-    id: message_id,
+    id: uuid,
     recipient_id,
   };
 };
