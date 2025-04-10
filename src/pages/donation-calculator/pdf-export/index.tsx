6f9471ac09df965e3b1eed5179bae758c8922617
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-10 17:36:15 +0800

    page3 content

--- src/pages/donation-calculator/pdf-export/index.tsx
+++ src/pages/donation-calculator/pdf-export/index.tsx
@@ -1,12 +1,13 @@
 import { Page1 } from "./page1";
 import { Page2 } from "./page2";
+import { Page3 } from "./page3";
 
 export default function Page() {
   return (
-    <div className="w-full bg-gray-l5 border-x mx-auto">
+    <div className="w-full border-x mx-auto">
       <Page1 />
       <Page2 />
-      <div className="h-[70.16625rem] bg-green">page3</div>
+      <Page3 />
       <div className="h-[70.16625rem] bg-amber">page4</div>
     </div>
   );
