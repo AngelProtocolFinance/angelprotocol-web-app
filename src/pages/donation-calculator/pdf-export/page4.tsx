c4224813ae80d4ad70cac0df979f1ead575269f2
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-10 19:51:02 +0800

    page4

---
+++ src/pages/donation-calculator/pdf-export/page4.tsx
@@ -0,0 +1,85 @@
+import bg from "assets/images/bettergiving-logo-white.webp";
+import Image from "components/image";
+export function Page4() {
+  return (
+    <div className="w-full aspect-[297/210]">
+      <div className="bg-blue p-6">
+        <div className="grid grid-cols-[1fr_auto] items-center">
+          <h2 className="uppercase text-white text-4xl font-bold leading-tight">
+            Appendix
+          </h2>
+
+          <Image
+            src={bg}
+            alt="Better Giving Logo"
+            width={300}
+            height={60}
+            className="object-contain self-start"
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-[auto_1fr] gap-x-4 items-center px-6">
+        <h2 className="text-3xl font-semibold uppercase">
+          Calculation Details
+        </h2>
+        <div className="h-0.5 bg-blue" />
+      </div>
+      <div className="mt-8 space-y-8">
+        <section>
+          <h4 className="font-semibold mb-4">Better Giving Platform</h4>
+          <ul className="space-y-4 list-disc pl-6">
+            <li className="text-gray">
+              Better Giving doesn't charge processing fees, but third-party
+              services charge an average of 2% (no platform fees)
+            </li>
+            <li className="text-gray">
+              80% of donors opt to cover processing fees (based on platform
+              data)
+            </li>
+            <li className="text-gray">
+              Better Giving accepts all donation types (credit cards, ACH,
+              digital wallets, crypto, stocks, DAF)
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <h4 className="font-semibold mb-4">Donation Type Calculation</h4>
+          <p className="text-gray mb-4">
+            These are approximate percentages for U.S.-based nonprofits,
+            annualized and projected for 2025 based on trends. Our calculations
+            assume 50% of donors will not proceed to make a donation if their
+            preferred donation method is unavailable.
+          </p>
+          <ul className="space-y-4 list-disc pl-6">
+            <li className="text-gray">
+              Credit card donations: 63% of total volume
+            </li>
+            <li className="text-gray">
+              ACH/Bank Transfer donations: 10% of total volume
+            </li>
+            <li className="text-gray">
+              Digital Wallet donations: 7% of total volume
+            </li>
+            <li className="text-gray">
+              Cryptocurrency donations: 2% of total volume
+            </li>
+            <li className="text-gray">Stock donations: 6% of total volume</li>
+            <li className="text-gray">DAF donations: 12% of total volume</li>
+          </ul>
+        </section>
+
+        <section>
+          <h4 className="font-semibold mb-4">Investment Returns</h4>
+          <ul className="space-y-4 list-disc pl-6">
+            <li className="text-gray">Savings Account: 4% annual yield</li>
+            <li className="text-gray">
+              Sustainability Fund: 20% average annual return
+            </li>
+            <li className="text-gray">Returns compound daily</li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
+}
