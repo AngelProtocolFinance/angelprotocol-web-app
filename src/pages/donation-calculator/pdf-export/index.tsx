3ad43bc29bc1ea684409757dda9a9088c823e922
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-10 19:52:51 +0800

    font sizes

--- src/pages/donation-calculator/pdf-export/index.tsx
+++ src/pages/donation-calculator/pdf-export/index.tsx
@@ -1,6 +1,7 @@
 import { Page1 } from "./page1";
 import { Page2 } from "./page2";
 import { Page3 } from "./page3";
+import { Page4 } from "./page4";
 
 export default function Page() {
   return (
@@ -8,7 +9,7 @@ export default function Page() {
       <Page1 />
       <Page2 />
       <Page3 />
-      <div className="h-[70.16625rem] bg-amber">page4</div>
+      <Page4 />
     </div>
   );
 }
