d3dc6986f7dc0159fa76ae04a5062fe635bfed4a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-11 19:07:16 +0800

    socials links dry

--- src/components/footer/socials.tsx
+++ src/components/footer/socials.tsx
@@ -3,40 +3,37 @@ import instagram from "assets/icons/social/instagram.webp";
 import linkedin from "assets/icons/social/linkedin.webp";
 import x from "assets/icons/social/x.webp";
 import youtube from "assets/icons/social/youtube.webp";
+import { socials } from "constants/urls";
 import ExtLink from "../ext-link";
-
 export default function Socials() {
   return (
     <div className="flex items-center gap-3 md:gap-6">
       <ExtLink
-        href="https://www.linkedin.com/company/better-giving/"
+        href={socials.linkedin}
         className="hover:scale-110 active:scale-110"
       >
         <img src={linkedin} alt="linkedin" width={20} />
       </ExtLink>
       <ExtLink
-        href="https://www.facebook.com/BetterGivingFB/"
+        href={socials.facebook}
         className="hover:scale-110 active:scale-110"
       >
         <img src={facebook} alt="facebook" width={18} />
       </ExtLink>
-      <ExtLink
-        href="https://x.com/BetterDotGiving"
-        className="hover:scale-110 active:scale-110"
-      >
+      <ExtLink href={socials.x} className="hover:scale-110 active:scale-110">
         <img src={x} alt="x" width={15} />
       </ExtLink>
       <ExtLink
-        href="https://www.youtube.com/@BetterDotGiving"
+        href={socials.youtube}
         className="hover:scale-110 active:scale-110"
       >
         <img src={youtube} alt="youtube" width={21} />
       </ExtLink>
       <ExtLink
-        href="https://www.instagram.com/better.giving"
+        href={socials.instagram}
         className="hover:scale-110 active:scale-110"
       >
-        <img src={instagram} alt="discord" width={18} />
+        <img src={instagram} alt="instagram" width={18} />
       </ExtLink>
     </div>
   );
