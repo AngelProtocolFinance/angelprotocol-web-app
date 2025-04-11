d66289e45c72e34a4d58c6b9718d7deaac675c65
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-11 19:40:42 +0800

    use div instead of input

--- src/pages/donation-calculator/pdf-export/page1/donation-methods.tsx
+++ src/pages/donation-calculator/pdf-export/page1/donation-methods.tsx
@@ -7,11 +7,8 @@ export function DonationMethods({
     <div className="flex gap-x-6 text-sm font-semibold">
       {Object.entries(methods).map(([id, name]) => (
         <div key={id} className="flex items-center gap-x-1">
-          <input
-            className="size-3 accent-blue"
-            type="checkbox"
-            checked={activeMethods.includes(id)}
-            readOnly
+          <div
+            className={`size-3 rounded-full ${activeMethods.includes(id) ? "bg-blue" : "border border-gray-l1"}`}
           />
           <span className="text-nowrap">{name}</span>
         </div>
