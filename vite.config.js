ad1c4c8ab355c6aea61757b5e57391ccaa047c4c
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-16 11:37:07 +0800

    customizable subtitle

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ const rmx = remix({
       r("donate-fund/:fundId", "./pages/donate-fund/redirect.ts");
       r("fundraisers/:fundId/donate", "./pages/donate-fund/index.tsx");
       r("donate-thanks", "./pages/donate-thanks.tsx");
-      r("landing-c", "./pages/landing-c/index.tsx");
+      r("access-donation-calculator", "./pages/landing-c/index.tsx");
       r("simplify-fundraising-maximize-impact", "./pages/landing-a/index.tsx", {
         id: "page-a",
       });
