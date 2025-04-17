9daddf100890f7955adc939c175aa6aa75c44911
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-17 23:04:34 +0800

    increase space

--- src/pages/donation-calculator/pdf-export/page1/index.tsx
+++ src/pages/donation-calculator/pdf-export/page1/index.tsx
@@ -304,7 +304,7 @@ export function Page1({ v }: Props) {
         style={{
           paddingHorizontal: w["10"],
           fontSize: fs.base,
-          marginTop: -w["10"] + 2,
+          marginTop: -w["6"],
           fontWeight: fw.sb,
         }}
       >
