1200c402f9dd7d26e7ff7ac072bfc35a51e53bb9
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-20 22:58:41 +0800

    submit straight

--- src/pages/registration/steps/dashboard/submit-action.ts
+++ src/pages/registration/steps/dashboard/submit-action.ts
@@ -1,15 +1,33 @@
+import { regId } from "@better-giving/registration/models";
+import { isDone } from "@better-giving/registration/step";
 import type { ActionFunction } from "@vercel/remix";
-import { ap, ver } from "api/api";
 import type { ActionData } from "types/action";
+import { parse } from "valibot";
 import { cognito, toAuth } from ".server/auth";
+import { getReg } from ".server/registration/get-reg";
+import { submit } from ".server/registration/submit";
 
 export const submitAction: ActionFunction = async ({ request, params }) => {
   const { user, headers } = await cognito.retrieve(request);
   if (!user) return toAuth(request, headers);
 
-  await ap.post(`${ver(1)}/registrations/${params.regId}/submit`, {
-    headers: { authorization: user.idToken },
-  });
+  const id = parse(regId, params.regId);
+  const reg = await getReg(id);
+
+  if (!reg) throw new Response("Registration not found", { status: 404 });
+
+  if (!isDone.banking(reg)) {
+    throw `Registration not ready for submission`;
+  }
+
+  if (
+    user.email !== reg.registrant_id.toLowerCase() &&
+    !user.groups.includes("ap-admin")
+  ) {
+    throw new Response("", { status: 403 });
+  }
+
+  await submit(reg);
 
   return {
     __ok: "Your application has been submitted. We will get back to you soon!",
