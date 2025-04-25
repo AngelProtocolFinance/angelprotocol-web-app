9441a2e00c9323d5a4ac4b60005cdb2d4350239a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-04-25 20:14:50 +0800

    save referrer to cookie

--- src/pages/registration/api.ts
+++ src/pages/registration/api.ts
@@ -3,6 +3,7 @@ import { type NewReg, newReg } from "@better-giving/registration/update";
 import {
   type ActionFunction,
   type LoaderFunction,
+  data,
   redirect,
 } from "@vercel/remix";
 import { getEndowWithEin } from "api/get/endow-with-ein";
@@ -16,9 +17,37 @@ import { createRegistration } from ".server/registration/create-reg";
 import { isClaimed } from ".server/registration/helpers";
 
 export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+
+  const referrer = url.searchParams.get("referrer");
+  const cookie = await regCookie
+    .parse(request.headers.get("Cookie"))
+    .then((x) => x || {});
+
+  const prev_referrer = cookie.referrer;
+  const prev_referrer_expiry = cookie.referrer_expiry;
+
+  // user clicked a referral link again, different from the previous one
+  if (referrer && prev_referrer && referrer !== prev_referrer) {
+    const expiry = new Date(prev_referrer_expiry);
+    cookie.referrer = expiry > new Date() ? prev_referrer : referrer;
+  }
+
+  // user clicked a referral link for the first time
+  if (referrer && !prev_referrer) {
+    cookie.referrer = referrer;
+    const duration = 15 * 24 * 60 * 60; // 15 days
+    cookie.referrer_expiry = new Date(
+      Date.now() + duration * 1000
+    ).toISOString();
+  }
+
   const { user, headers } = await cognito.retrieve(request);
   if (!user) return toAuth(request, headers);
-  return user;
+
+  return data(user, {
+    headers: { "Set-Cookie": await regCookie.serialize(cookie) },
+  });
 };
 
 export const newApplicationAction: ActionFunction = async ({ request }) => {
@@ -26,6 +55,9 @@ export const newApplicationAction: ActionFunction = async ({ request }) => {
   if (!user) return toAuth(request, headers);
 
   const url = new URL(request.url);
+  const cookie = await regCookie
+    .parse(request.headers.get("Cookie"))
+    .then((x) => x || {});
 
   const ein = url.searchParams.get("claim");
   const referrer = url.searchParams.get("referrer");
@@ -43,7 +75,16 @@ export const newApplicationAction: ActionFunction = async ({ request }) => {
   };
 
   if (claim) payload.claim = claim;
+
+  // user is registering via fresh referral link
   if (referrer) payload.referrer = referrer;
+
+  /* user is registering on his own,
+   * but he may have discovered the platform via previous referral
+   */
+  if (!referrer && cookie.referrer) {
+    payload.referrer = cookie.referrer;
+  }
 
   const init = parse(newReg, payload);
 
@@ -58,12 +99,10 @@ export const newApplicationAction: ActionFunction = async ({ request }) => {
   }
 
   const id = await createRegistration(init, env);
-  const rc = await regCookie
-    .parse(request.headers.get("Cookie"))
-    .then((x) => x || {});
-  rc.reference = id;
+
+  cookie.reference = id;
 
   return redirect(`${appRoutes.register}/${id}/${steps.contact}`, {
-    headers: { "Set-Cookie": await regCookie.serialize(rc) },
+    headers: { "Set-Cookie": await regCookie.serialize(cookie) },
   });
 };
