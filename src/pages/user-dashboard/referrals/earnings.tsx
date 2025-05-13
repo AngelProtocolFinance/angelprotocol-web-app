3051b54637a0c1604c3515cc095931910897d9ea
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-05-13 23:04:28 +0800

    paid out aggregate

--- src/pages/user-dashboard/referrals/earnings.tsx
+++ src/pages/user-dashboard/referrals/earnings.tsx
@@ -2,7 +2,7 @@ import type { V2RecipientAccount } from "@better-giving/wise";
 import { Link, Outlet } from "@remix-run/react";
 import { endOfMonth, format, formatDistance } from "date-fns";
 import { humanize } from "helpers/decimal";
-import { PencilIcon } from "lucide-react";
+import { ArrowRightIcon, HistoryIcon, PencilIcon } from "lucide-react";
 import { useState } from "react";
 import type { PendingEarnings } from "types/referrals";
 import {
@@ -55,16 +55,52 @@ export function Earnings({
         key={currentEarning.id}
         className="bg-gray-l5 rounded-xl border border-gray-l4 overflow-hidden"
       >
-        <div className="p-6">
-          <div className="flex items-center gap-x-2">
-            <div className="text-2xl font-bold text-gray-d4">
-              ${humanize(pendings.total, 3)}
+        <div className="p-6 @container">
+          <div className="flex flex-wrap items-center justify-between gap-x-2">
+            <div className="flex items-center gap-x-2 flex-wrap">
+              <div className="text-2xl font-bold text-gray-d4">
+                ${humanize(pendings.total, 3)}
+              </div>
+              <p className="text-sm text-gray mt-1">
+                pays out {format(end, "PP")}- in {formatDistance(end, now)}.
+              </p>
             </div>
-            <p className="text-sm text-gray mt-1">
-              pays out {format(end, "PP")}- in {formatDistance(end, now)}.
-            </p>
+            <Link
+              to="payouts"
+              className="group flex items-center @max-lg:mt-2 gap-x-1 text-blue hover:text-blue-d1"
+            >
+              <HistoryIcon
+                size={20}
+                className="group-hover:hidden @max-lg:hidden"
+              />
+              <ArrowRightIcon
+                size={20}
+                className=" @max-lg:hidden hidden @lg:group-hover:block group-active:translate-x-0.5"
+              />
+              <div className="text-xl font-bold text-gray-d">
+                ${humanize(0, 3)}
+              </div>
+              <span className="text-sm mt-1">paid out</span>
+            </Link>
           </div>
-
+          {payout && (
+            <div className="mt-4">
+              <p className="text-sm text-gray">Payout threshold</p>
+              <div className="flex gap-x-1 items-center">
+                <p className="font-semibold text-amber-d1">
+                  ${humanize(payout_min, 3)}
+                </p>
+                <Link
+                  to={{ pathname: "payout-min", search: `?min=${payout_min}` }}
+                  replace
+                  preventScrollReset
+                  className="text-xs"
+                >
+                  <PencilIcon size={12} />
+                </Link>
+              </div>
+            </div>
+          )}
           {payout ? (
             <div className="mt-4">
               <p className="text-sm text-gray">Payout method</p>
@@ -88,24 +124,7 @@ export function Earnings({
               setup payout method
             </Link>
           )}
-          {payout && (
-            <div className="mt-4">
-              <p className="text-sm text-gray">Payout threshold</p>
-              <div className="flex gap-x-1 items-center">
-                <p className="text-sm text-gray-d4">
-                  ${humanize(payout_min, 3)}
-                </p>
-                <Link
-                  to={{ pathname: "payout-min", search: `?min=${payout_min}` }}
-                  replace
-                  preventScrollReset
-                  className="text-xs"
-                >
-                  <PencilIcon size={12} />
-                </Link>
-              </div>
-            </div>
-          )}
+
           <EarningsHistory {...earnings} classes="mt-6" />
         </div>
       </div>
