187949ef37b49af511961cf6fff0421f3c4cd43c
jp-mariano <juanpaolo.mariano28@gmail.com>
2025-05-14 14:04:20 +0800

    Move 'newsletterSubscribe' to server function

---
+++ src/.server/hubspot/newsletter-subscribe.ts
@@ -0,0 +1,16 @@
+import { hsNewsletterSubs } from "api/api";
+import {
+  HUBSPOT_NEWSLETTER_FORM_ID as formId,
+  HUBSPOT_NEWSLETTER_PORTAL_ID as portalId,
+  HUBSPOT_ACCESS_TOKEN as token,
+} from "constants/env";
+
+export async function newsletterSubscribe(email: string) {
+  return hsNewsletterSubs.post(`${portalId}/${formId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    json: { fields: [{ name: "email", value: email }] },
+  });
+}
