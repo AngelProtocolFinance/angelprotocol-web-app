c36b00d50fd23af19286442539a2eb9338d30c51
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-05-15 16:15:55 +0800

    add types

--- src/.server/referrals.ts
+++ src/.server/referrals.ts
@@ -1,12 +1,16 @@
 import type { Gsi1 } from "@better-giving/endowment/db";
+import * as db from "@better-giving/referrals/db";
 import { tables } from "@better-giving/types/list";
 import type { PendingEarnings, Referred } from "types/referrals";
 import { GetCommand, QueryCommand, ap, apes } from "./aws/db";
 
 export const referredBy = async (id: string): Promise<Referred[]> => {
   const ltdCmd = new GetCommand({
-    TableName: "commissions",
-    Key: { PK: `Ltd#${id}`, SK: `Ltd#${id}` },
+    TableName: db.name,
+    Key: { PK: `Ltd#${id}`, SK: `Ltd#${id}` } satisfies Pick<
+      db.Ltd,
+      "PK" | "SK"
+    >,
   });
 
   const { Item: ltds = {} } = await apes.send(ltdCmd);
@@ -35,24 +39,31 @@ export const referredBy = async (id: string): Promise<Referred[]> => {
 
 export const pendingEarnings = async (id: string): Promise<PendingEarnings> => {
   const cmd = new QueryCommand({
-    TableName: "commissions",
+    TableName: db.name,
     KeyConditionExpression: "PK = :pk",
-    ExpressionAttributeValues: { ":pk": `Cm#${id}`, ":status": "pending" },
+    ExpressionAttributeValues: {
+      ":pk": `Cm#${id}` satisfies db.Commission["PK"],
+      ":status": "pending" satisfies db.Commission["status"],
+    },
     FilterExpression: "#status = :status",
     ExpressionAttributeNames: { "#status": "status" },
   });
 
   const res = await apes.send(cmd);
-  const pendings = (res.Items || []).map(({ amount, status, date }) => ({
+  const items = (res.Items || []) as db.Commission[];
+  const pendings = items.map(({ amount, status, date }) => ({
     amount,
     status,
     date,
   }));
   const total = pendings.reduce((acc, { amount }) => acc + amount, 0);
-  const dates = pendings.reduce((acc, { date, status }) => {
-    acc[date] = status;
-    return acc;
-  }, {} as any);
+  const dates = pendings.reduce(
+    (acc, { date, status }) => {
+      acc[date] = status;
+      return acc;
+    },
+    {} as { [date: string]: any }
+  );
 
   return { total, dates };
 };
