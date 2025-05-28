0e76c48525a63b9c85033bee99d792df0cadd3f6
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-05-28 12:57:45 +0800

    add anvil sdk

--- src/.server/sdks.ts
+++ src/.server/sdks.ts
@@ -1,8 +1,16 @@
+import Anvil from "@anvilco/anvil";
 import { Discord } from "@better-giving/helpers/discord";
 import { Nowpayments } from "@better-giving/nowpayments";
 import { Wise } from "@better-giving/wise";
 import Stripe from "stripe";
-import { discordEnvs, env, npEnvs, stripeEnvs, wiseApiToken } from "./env";
+import {
+  anvil_envs,
+  discordEnvs,
+  env,
+  npEnvs,
+  stripeEnvs,
+  wiseApiToken,
+} from "./env";
 
 export const discordAwsMonitor = new Discord(discordEnvs.awsMonitorUrl);
 export const discordFiatMonitor = new Discord(discordEnvs.fiatMonitorUrl);
@@ -12,4 +20,8 @@ export const stripe = new Stripe(stripeEnvs.secretKey);
 export const wise = new Wise({
   apiToken: wiseApiToken,
   sandbox: env === "staging",
+});
+
+export const anvil = new Anvil({
+  apiKey: anvil_envs.api_key,
 });
