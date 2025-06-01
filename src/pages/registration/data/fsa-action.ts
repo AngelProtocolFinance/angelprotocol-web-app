00e4f8f43d214d809b3f8cc1ba54881a2e03c50a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-06-01 12:18:26 +0800

    gen fsa sign url controller

--- src/pages/registration/data/fsa-action.ts
+++ src/pages/registration/data/fsa-action.ts
@@ -1,33 +1,29 @@
-import type { FsaPayload } from "@better-giving/registration/fsa";
+import { type FsaPayload, signer } from "@better-giving/registration/fsa";
 import { type ActionFunction, redirect } from "@vercel/remix";
-import { ap, ver } from "api/api";
 import { regRoutes } from "constants/routes";
+import { parse } from "valibot";
 import { cognito, toAuth } from ".server/auth";
+import { gen_fsa_signing_url } from ".server/registration/gen-fsa-signing-url";
 
 export const action: ActionFunction = async ({ request, params }) => {
   const { user, headers } = await cognito.retrieve(request);
   if (!user) return toAuth(request, headers);
 
-  const contentType = request.headers.get("content-type");
-  const signer =
-    contentType === "application/json"
+  const content_type = request.headers.get("content-type");
+  const signer_payload =
+    content_type === "application/json"
       ? await request.json()
       : await request.formData().then((fv) => fv.get("signer_eid")?.toString());
+
+  const x = parse(signer, signer_payload);
 
   const from = new URL(request.url);
   from.pathname = `register/${params.regId}/${regRoutes.sign_result}`;
   from.search = "";
   const payload: FsaPayload = {
-    signer,
+    signer: x,
     redirectUrl: from.toString(),
   };
-
-  const { url: to } = await ap
-    .post<{ url: string }>(`${ver(1)}/registration-fsa`, {
-      json: payload,
-      headers: { authorization: user.idToken },
-    })
-    .json();
-
+  const to = await gen_fsa_signing_url(payload);
   return redirect(to);
 };
