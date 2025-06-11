b17672a9731de83a5d289d9023ab397467cec6dc
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-06-11 12:43:51 +0800

    lowercase

--- src/routes/api.donation-intents.$type/stripe/create-payment-intent.ts
+++ src/routes/api.donation-intents.$type/stripe/create-payment-intent.ts
@@ -27,7 +27,9 @@ export async function create_payment_intent(
         verification_method: "automatic",
       },
     },
-    payment_method_types: payment_methods[order.denomination] ?? ["card"],
+    payment_method_types: payment_methods[order.denomination.toLowerCase()] ?? [
+      "card",
+    ],
   });
 
   if (!paymentIntent.client_secret)
