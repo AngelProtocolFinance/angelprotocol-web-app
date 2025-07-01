c152f100ff880791f5288797c3d5cc7a52e94ab7
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-07-01 10:45:01 +0800

    fix testimonial

--- src/pages/landing/stop-losing/testimonials/card.tsx
+++ src/pages/landing/stop-losing/testimonials/card.tsx
@@ -3,18 +3,17 @@ import type { Testimonial } from "content/testimonials";
 
 export const Card = (props: Testimonial) => {
   return (
-    <div className="@container">
+    <div className="shrink-0 max-w-80">
       <Image
-        height={240}
+        width={120}
+        height={120}
         src={props.reviewer_org_logo}
         alt="Reviewer's organization logo"
         className="object-contain size-28 rounded-lg ml-8 bg-white shadow-xl"
       />
       <div className="relative -mt-14 pt-24 px-8 pb-8 rounded-3xl bg-white -z-10 border border-gray-l3">
-        <p className="text-lg @md:h-64 mt-8">{props.content}</p>
-        <p className="mt-4 @md:mt-0 text-lg opacity-90 font-bold">
-          {props.reviewer}
-        </p>
+        <p className="text-lg mt-8">{props.content}</p>
+        <p className="mt-4 text-lg opacity-90 font-bold">{props.reviewer}</p>
         <p className="md:text-lg font-medium">{props.reviewer_org_role}</p>
       </div>
     </div>
