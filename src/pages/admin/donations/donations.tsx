4d22eda03dc253564c803b805d58fdf23b72f18d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-07-10 14:45:21 +0800

    layout

--- src/pages/admin/donations/donations.tsx
+++ src/pages/admin/donations/donations.tsx
@@ -1,6 +1,7 @@
 import { useCachedLoaderData } from "api/cache";
 import type { DonationsPage } from "types/donations";
 import DonationsTable from "./donations-table";
+import { Schedule } from "./schedule";
 export { ErrorBoundary } from "components/error";
 export { loader } from "./api";
 export { clientLoader } from "api/cache";
@@ -9,6 +10,15 @@ export default function Donations() {
   return (
     <div>
       <h2 className="text-[2rem] font-bold mb-4">Donations</h2>
+      <Schedule
+        classes="mb-4"
+        amount={100}
+        allocation={{
+          cash: 50,
+          liq: 30,
+          lock: 20,
+        }}
+      />
       <DonationsTable page1={page1} />
     </div>
   );
