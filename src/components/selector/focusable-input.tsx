a28be764f9526a3160a3ce0b7b3325623449d15e
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-07-23 14:38:46 +0800

    track lock units intead

--- src/components/selector/focusable-input.tsx
+++ src/components/selector/focusable-input.tsx
@@ -1,11 +1,9 @@
 import { forwardRef } from "react";
 
-const FocusableInput = forwardRef<HTMLInputElement>(
+export const FocusableInput = forwardRef<HTMLInputElement>(
   function Input(_props, ref) {
     return (
       <input ref={ref} aria-hidden className="h-0 w-0 absolute" tabIndex={-1} />
     );
   }
 );
-
-export default FocusableInput;
--- src/pages/admin/dashboard/api.ts
+++ src/pages/admin/dashboard/api.ts
@@ -1,9 +1,9 @@
 import { type IItem, priority_nums } from "@better-giving/banking-applications";
+import { NavHistoryDB } from "@better-giving/nav-history/db";
 import { type INpoPayoutsPage, PayoutsDB } from "@better-giving/payouts";
 import type { LoaderFunction } from "@vercel/remix";
 import { plusInt } from "api/schema/endow-id";
 import { CronExpressionParser } from "cron-parser";
-import type { NpoBalances } from "types/npo-balance";
 import * as v from "valibot";
 import { endowUpdate } from "../endow-update-action";
 import { cognito, toAuth } from ".server/auth";
@@ -14,7 +14,9 @@ import { npoBalances } from ".server/npo-balances";
 
 export interface DashboardData {
   id: number;
-  bal: NpoBalances;
+  bal_liq: number;
+  bal_lock: number;
+  bal_cash: number;
   /** compute in server save client bundle */
   recent_payouts: INpoPayoutsPage;
   next_payout: string;
@@ -27,24 +29,29 @@ export const loader: LoaderFunction = async ({ params, request }) => {
   if (!user) return toAuth(request, headers);
 
   const payouts_db = new PayoutsDB(apes, env);
+  const navdb = new NavHistoryDB(apes, env);
   const id = v.parse(plusInt, params.id);
   const interval = CronExpressionParser.parse("0 0 */3 * *"); //every 3 days
   // console.log("next payout", interval.next().toString());
 
-  const [bal, recent_payouts, pm] = await Promise.all([
-    npoBalances(id),
-    payouts_db.npo_payouts(id.toString(), { status: "pending", limit: 3 }),
-    npo_banks(id, 1).then(([x]) => {
-      if (!x) return;
-      // no default payout method
-      if (x.thisPriorityNum < priority_nums.approved) return;
-      return x;
-    }),
-  ]);
+  const [ltd, { liq = 0, lock_units = 0, cash = 0 }, recent_payouts, pm] =
+    await Promise.all([
+      navdb.ltd(),
+      npoBalances(id),
+      payouts_db.npo_payouts(id.toString(), { status: "pending", limit: 3 }),
+      npo_banks(id, 1).then(([x]) => {
+        if (!x) return;
+        // no default payout method
+        if (x.thisPriorityNum < priority_nums.approved) return;
+        return x;
+      }),
+    ]);
 
   return {
     id,
-    bal,
+    bal_liq: liq,
+    bal_lock: lock_units * ltd.price,
+    bal_cash: cash,
     recent_payouts,
     next_payout: interval.next().toString(),
     pm,
