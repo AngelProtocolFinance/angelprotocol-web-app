f4ca9e494361ca69880ebd6f3d718d1e836c8ba3
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-07-25 09:30:31 +0800

    nav loader

--- src/pages/fund-management/nav/index.tsx
+++ src/pages/fund-management/nav/index.tsx
@@ -1,4 +1,12 @@
+import { useCachedLoaderData } from "remix-client-cache";
+import type { LoaderData } from "./api";
+
+export { loader } from "./api";
+export { clientLoader } from "api/cache";
+
 export default function Page() {
+  const data = useCachedLoaderData() as LoaderData;
+  console.log(data);
   return (
     <div className="@container w-full max-w-4xl grid content-start">
       <h3 className="font-bold text-2xl mb-4">Nav</h3>
