ebc79fb9049863fc2aeb80c3cf11c127832ffdf1
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-08-04 20:22:37 +0800

    move

--- src/pages/fund-management/tx-requests/index.tsx
+++ src/pages/fund-management/tx-requests/index.tsx
@@ -1,22 +1,11 @@
-import { NavHistoryDB } from "@better-giving/nav-history";
 import { Outlet } from "@remix-run/react";
-// import { RebalanceForm } from "./rebalance";
 import { Requests } from "./table";
-import { apes } from ".server/aws/db";
-import { env } from ".server/env";
-
-export const loader = () => {
-  const db = new NavHistoryDB(apes, env);
-  return db.ltd();
-};
 
 export default function Page() {
-  // const ltd = useLoaderData() as any;
   return (
     <div className="@container w-full max-w-4xl grid content-start">
       <h3 className="font-bold text-2xl mb-4">Tx Requests</h3>
       <Requests />
-      {/* <RebalanceForm {...ltd} /> */}
       <Outlet />
     </div>
   );
