f1bd4f0fc476cc714fd2210fbed7a27c5202250d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-08-27 17:12:39 +0800

    no mas apes ky

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,3 @@
-import { env } from "constants/env";
 import ky from "ky";
 import { version as v } from "./helpers";
 export { version as ver, toSearch } from "./helpers";
@@ -7,12 +6,6 @@ export const apUrl = "https://ap-api.better.giving";
 export const ap = ky.create({ prefixUrl: apUrl, timeout: false });
 export const wise = ap.extend({
   prefixUrl: `${apUrl}/${v(1)}/wise-proxy`,
-  timeout: false,
-});
-
-export const apesUrl = `https://apes-api.better.giving/${env}`;
-export const apes = ky.create({
-  prefixUrl: apesUrl,
   timeout: false,
 });
 
