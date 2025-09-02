5689bc84608ed1052b927511def2cde0ccd7a8d5
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-09-02 16:38:57 +0800

    remove old

--- src/routes/api.anvil-webhook/etch-complete.ts
+++ src/routes/api.anvil-webhook/etch-complete.ts
@@ -1,12 +1,7 @@
-import type { Key } from "@better-giving/registration/db";
-import type { FsaDocs } from "@better-giving/registration/models";
-import type { Step6 } from "@better-giving/registration/step";
-import type { Update } from "@better-giving/registration/update";
-import { tables } from "@better-giving/types/list";
-import type { AttrNames } from "@better-giving/types/utils";
+import type { IRegUpdate } from "@better-giving/reg";
 import { signer_fn } from "./helpers";
 import type { EtchPacket } from "./types";
-import { UpdateCommand, ap } from ".server/aws/db";
+import { regdb } from ".server/aws/db";
 
 export const etch_complete = async (
   data: EtchPacket.Data,
@@ -14,32 +9,16 @@ export const etch_complete = async (
 ) => {
   const { signers, documentGroup } = data;
   const signer = await signer_fn(signers[0].eid);
+  const id = signer.eid;
 
-  const names: Partial<AttrNames<Step6 & FsaDocs>> = {
-    "#docs": "docs",
-    "#fsa_signed_doc_url": "fsa_signed_doc_url",
-    "#irs501c3": "irs501c3",
-    "#update_type": "update_type",
-    "#updated_at": "updated_at",
+  const prev = await regdb.reg(id);
+  if (!prev) throw `reg not found for ${id}`;
+
+  const upd8: IRegUpdate = {
+    $o_type: "other",
+    $o_fsa_signed_doc_url: `${base_url}/api/anvil-doc/${documentGroup.eid}`,
+    update_type: "docs",
   };
 
-  const command = new UpdateCommand({
-    Key: {
-      PK: `Reg#${signer.eid}`,
-      SK: `Reg#${signer.eid}`,
-    } satisfies Key,
-    TableName: tables.registration,
-    UpdateExpression:
-      "SET #docs.#fsa_signed_doc_url = :url, #updated_at = :updated_at, #update_type = :update_type",
-    ConditionExpression: `#irs501c3 = :false AND attribute_exists(#docs) AND attribute_type(#docs,:type)`,
-    ExpressionAttributeNames: names,
-    ExpressionAttributeValues: {
-      ":url": `${base_url}/api/anvil-doc/${documentGroup.eid}`,
-      ":type": "M",
-      ":false": false,
-      ":updated_at": new Date().toISOString(),
-      ":update_type": "docs" satisfies Update["type"],
-    },
-  });
-  return ap.send(command);
+  return regdb.reg_update(id, prev, upd8);
 };
