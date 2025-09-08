bcaf1c4061696e63b8aa4feb4924f21b4e8df480
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-09-08 16:52:49 +0800

    fix contact bug

--- src/pages/registration/steps/contact-details/use-rhf.ts
+++ src/pages/registration/steps/contact-details/use-rhf.ts
@@ -1,9 +1,28 @@
 import type { IReg } from "@better-giving/reg";
 import { valibotResolver } from "@hookform/resolvers/valibot";
-import { useController, useForm } from "react-hook-form";
+import { type DefaultValues, useController, useForm } from "react-hook-form";
 import { type FV, schema } from "./schema";
 
 export function use_rhf(reg: IReg) {
+  const def: DefaultValues<FV> = {
+    r_first_name: reg.r_first_name,
+    r_last_name: reg.r_last_name,
+    r_contact_number: reg.r_contact_number,
+    r_org_role: reg.r_org_role,
+    rm: reg.rm,
+    o_name: reg.o_name,
+  };
+
+  if (reg.r_org_role === "other") {
+    def.r_org_role_other = reg.r_org_role_other;
+  }
+  if (reg.rm === "other") {
+    def.rm_other = reg.rm_other;
+  }
+  if (reg.rm === "referral") {
+    def.rm_referral_code = reg.rm_referral_code;
+  }
+
   const {
     register,
     control,
@@ -11,17 +30,7 @@ export function use_rhf(reg: IReg) {
     formState: { errors, isDirty, isSubmitting, dirtyFields },
   } = useForm<FV>({
     resolver: valibotResolver(schema),
-    defaultValues: {
-      r_first_name: reg.r_first_name,
-      r_last_name: reg.r_last_name,
-      r_contact_number: reg.r_contact_number,
-      r_org_role: reg.r_org_role,
-      r_org_role_other: reg.r_org_role === "other" ? reg.r_org_role : "",
-      rm: reg.rm,
-      rm_other: reg.rm === "other" ? reg.rm : "",
-      rm_referral_code: reg.rm === "referral" ? reg.rm_referral_code : "",
-      o_name: reg.o_name,
-    },
+    defaultValues: def,
   });
 
   const { field: rm } = useController({
