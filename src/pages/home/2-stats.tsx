a508dec7edf55f170de2a6019495f43d14c3f2ce
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-10-03 11:11:47 +0800

    card max-w

--- src/pages/home/2-stats.tsx
+++ src/pages/home/2-stats.tsx
@@ -5,21 +5,21 @@ export function Stats({ classes = "" }) {
     <div className={`${classes} grid md:grid-cols-3 gap-8`}>
       <div className="grid justify-items-center">
         <p className="text-3xl text-center font-bold text-blue">6 Million+</p>
-        <p className="max-w-lg text-center mt-2 text-lg">
+        <p className="max-w-md text-center mt-2 text-lg">
           Raised across crypto, stock, DAF, and card donations for nonprofit
           members.
         </p>
       </div>
       <div className="grid justify-items-center">
         <p className="text-3xl text-center font-bold text-blue">~24%</p>
-        <p className="max-w-lg text-center mt-2 text-lg">
+        <p className="max-w-md text-center mt-2 text-lg">
           Raised across crypto, stock, DAF, and card donations for nonprofit
           members.
         </p>
       </div>
       <div className="grid justify-items-center">
         <p className="text-3xl text-center font-bold text-blue">18,000+</p>
-        <p className="max-w-lg text-center mt-2 text-lg">
+        <p className="max-w-md text-center mt-2 text-lg">
           Causes discoverable across the {APP_NAME} directory.
         </p>
       </div>
