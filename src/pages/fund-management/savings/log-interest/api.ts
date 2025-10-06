91555766593f3db5a846ded41a25b3c5b6b57fc6
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-10-06 21:05:58 +0800

    txs by batch

--- src/pages/fund-management/savings/log-interest/api.ts
+++ src/pages/fund-management/savings/log-interest/api.ts
@@ -1,13 +1,13 @@
-import type { IBalanceTx } from "@better-giving/balance-txs";
-import { Txs } from "@better-giving/db";
+import type { TAllocStatus } from "@better-giving/liquid";
 import { interest_log } from "@better-giving/liquid/schemas";
 import { nanoid } from "nanoid";
 import { MIN_INTR_TO_CREDIT } from "pages/fund-management/constants";
 import { redirect } from "react-router";
 import { parse } from "valibot";
+import { credit_txs } from "../credit-txs";
 import type { Route } from "./+types";
 import { cognito, to_auth } from ".server/auth";
-import { TransactWriteCommand, baldb, btxdb, liqdb } from ".server/aws/db";
+import { TransactWriteCommand, baldb, liqdb } from ".server/aws/db";
 import { npo_interest_shares } from ".server/npos-interest-share";
 
 export const action = async ({ request }: Route.ActionArgs) => {
@@ -23,16 +23,17 @@ export const action = async ({ request }: Route.ActionArgs) => {
     end: fv.date_end,
   });
 
-  const txs = new Txs();
   const intr_id = nanoid();
   const intr_date = new Date().toISOString();
 
   const credits: Record<string, number> = {};
+  const alloc_status: Record<string, TAllocStatus> = {};
   for (const npo in shares) {
     const share = shares[npo];
     const to_credit = share * +fv.total;
     if (to_credit < MIN_INTR_TO_CREDIT) continue; // skip less than 1 cent
     credits[npo] = to_credit;
+    alloc_status[npo] = "pending";
   }
   const npos = Object.keys(credits);
   const bals = await baldb.balances_get(npos.map(Number), ["liq", "id"]);
@@ -44,49 +45,33 @@ export const action = async ({ request }: Route.ActionArgs) => {
     {} as Record<string, number>
   );
 
-  for (const npo of npos) {
-    const to_credit = credits[npo];
-    const bal_upd8 = baldb.balance_update_txi(+npo, {
-      liq: ["inc", to_credit],
-    });
-    txs.update(bal_upd8);
-    const bal = bals_map[npo] || 0;
-
-    const tx: IBalanceTx = {
-      id: nanoid(),
-      date_created: intr_date,
-      date_updated: intr_date,
-      owner: npo,
-      account: "liq",
-      amount: to_credit,
-      amount_units: to_credit,
-      bal_begin: bal,
-      bal_end: bal + to_credit,
-      status: "final",
-
-      account_other: "interest",
-      account_other_id: intr_id,
-      account_other_bal_begin: to_credit,
-      account_other_bal_end: 0,
-    };
-    txs.put(btxdb.tx_put_txi(tx));
-  }
-
-  const intr_log = liqdb.intr_log_put_txi({
+  // log intr, with pending alloc status
+  await liqdb.intr_log_put({
     ...fv,
     date_created: intr_date,
     alloc: shares,
+    alloc_status,
     id: intr_id,
   });
 
-  txs.put(intr_log);
+  for (const npo of npos) {
+    const txs = credit_txs({
+      npo,
+      npo_bal: bals_map[npo] || 0,
+      intr_id,
+      intr_date,
+      to_credit: credits[npo],
+    });
 
-  const cmd = new TransactWriteCommand({
-    TransactItems: txs.all,
-  });
-
-  const res = await liqdb.client.send(cmd);
-  console.info("credited and logged interest", res);
+    const cmd = new TransactWriteCommand({
+      TransactItems: txs.all,
+    });
+    await liqdb.client.send(cmd).catch((err) => {
+      console.error(err);
+      console.error("failed to credit", npo, credits[npo]);
+    });
+    console.info("credited", npo, credits[npo]);
+  }
 
   return redirect("..");
 };
