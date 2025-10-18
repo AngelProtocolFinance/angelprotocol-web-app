713dc3429538359ede122ea8aca650d826dc4cce
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-10-19 03:17:14 +0800

    donations cookie

--- src/.server/cookie.ts
+++ src/.server/cookie.ts
@@ -4,3 +4,12 @@ export const reg_cookie = createCookie("bg-registration", { path: "/" });
 export const bg_session = createCookie("bg-session", {
   secrets: [session_secret],
 });
+
+// give temporary donation access
+export const donations_cookie = createCookie("donations", {
+  path: "/",
+  secrets: [session_secret],
+});
+
+/** donation-id: access expiry */
+export interface IDonationsCookie extends Record<string, number> {}
