8e408644811da99505fb4a96017a2bcc5bae3858
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-10-30 11:09:39 +0800

    fixes bg-1852

--- src/pages/home/header/header.tsx
+++ src/pages/home/header/header.tsx
@@ -2,25 +2,25 @@ import {
   AuthBtns,
   AuthLinks,
   NavDropdown,
-  UserAvatar,
+  // UserAvatar,
 } from "components/header";
 import { DappLogo } from "components/image";
 import { auth_routes } from "constants/routes";
 import { use_root_data } from "hooks/use-root-data";
-import { useState } from "react";
+// import { useState } from "react";
 import { useLocation } from "react-router";
 import type { EndowCardsPage } from "types/npo";
-import SearchDropdown from "./search-dropdown";
-import SearchField from "./search-field";
+// import SearchDropdown from "./search-dropdown";
+// import SearchField from "./search-field";
 
 interface Props {
   classes?: string;
   page1: EndowCardsPage;
 }
 
-export function Header({ classes, page1 }: Props) {
+export function Header({ classes }: Props) {
   const user = use_root_data();
-  const [query, setQuery] = useState("");
+  // const [query, setQuery] = useState("");
   const { pathname: p, search: s } = useLocation();
   const to = auth_routes.includes(p) ? undefined : p + s;
 
@@ -41,27 +41,27 @@ export function Header({ classes, page1 }: Props) {
         observer.observe(node);
       }}
     >
-      <div className="grid relative items-center grid-cols-2 gap-4 xl:container xl:mx-auto px-5 bg-white rounded-full py-2">
-        <DappLogo classes="h-12" />
-        <SearchField
+      <div className="flex items-center gap-4 xl:container xl:mx-auto px-5 bg-white rounded-full py-2">
+        <div className="flex-1">
+          <DappLogo classes="h-12" />
+        </div>
+        {/* <SearchField
           onChange={(txt) => setQuery(txt)}
           classes="max-md:hidden absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+        /> */}
+        {to && !user && <AuthBtns to={to} classes="max-sm:hidden flex-none" />}
+        <NavDropdown
+          user={user}
+          auth_links={
+            to && !user && <AuthLinks to={to} classes="sm:hidden flex-none" />
+          }
         />
-        <div className="flex gap-2 md:gap-4 justify-self-end items-center">
-          {to && !user && <AuthBtns to={to} classes="max-sm:hidden" />}
-          <NavDropdown
-            user={user}
-            auth_links={
-              to && !user && <AuthLinks to={to} classes="sm:hidden" />
-            }
-          />
-        </div>
       </div>
-      <SearchDropdown
+      {/* <SearchDropdown
         page1={page1}
         query={query}
         classes="mt-4 hidden group-has-[input:focus]:block hover:block absolute left-1/2 -translate-x-1/2"
-      />
+      /> */}
     </header>
   );
 }
