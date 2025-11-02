26928d1c27f02847a351d3bdfd40b4e3d833bf21
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-11-02 10:39:19 +0800

    await connection

--- src/.server/mongodb/db.ts
+++ src/.server/mongodb/db.ts
@@ -2,9 +2,7 @@ import { MongoClient } from "mongodb";
 import type { NonprofitItem } from "types/mongodb/nonprofits";
 import { mongodb_url } from ".server/env";
 
-const db = new MongoClient(mongodb_url)
-  .connect()
-  .then((c) => c.db("better-giving"));
-export const nonprofits = db.then((d) =>
-  d.collection<NonprofitItem>("nonprofits")
+const db = await MongoClient.connect(mongodb_url).then((x) =>
+  x.db("better-giving")
 );
+export const nonprofits = db.collection<NonprofitItem>("nonprofits");
