5dbf7f8bf580cb8c6934f5b568262b87d19274eb
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-11-04 18:27:36 +0800

    page title

--- src/pages/admin/forms/index.tsx
+++ src/pages/admin/forms/index.tsx
@@ -1,7 +1,12 @@
+import { metas } from "helpers/seo";
 import type { Route } from "./+types";
 import { FormsList } from "./forms-list";
 
 export { loader } from "./api";
+
+export const meta: Route.MetaFunction = () =>
+  metas({ title: "Donation forms" });
+
 export default function Page({ loaderData: d }: Route.ComponentProps) {
   return (
     <div className="px-6 py-4 md:px-10 md:py-8">
