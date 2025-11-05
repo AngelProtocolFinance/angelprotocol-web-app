9897d43f3e1c2864bc213637ac85ebabe13fc91a
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-11-05 12:12:14 +0800

    allow cross origin

--- src/routes/form-embed.ts
+++ src/routes/form-embed.ts
@@ -1,12 +1,14 @@
+import type { LoaderFunction } from "react-router";
 import script from "./form-embed.js?raw";
 
-export async function loader() {
+export const loader: LoaderFunction = () => {
   return new Response(script, {
     headers: {
       "Content-Type": "text/javascript",
       // Cache for 1 hour, but allow stale content for up to 24 hours while revalidating
       // This means users get instant loads while still checking for updates
       "Cache-Control": "public, max-age=3600, stale-while-revalidate=86400",
+      "Access-Control-Allow-Origin": "*",
     },
   });
-}
+};
