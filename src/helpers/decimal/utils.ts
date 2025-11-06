69e0d15984388b3cbbcc430ce0d1ffb15956fbf1
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-11-06 23:30:34 +0800

    add jsdoc

--- src/helpers/decimal/utils.ts
+++ src/helpers/decimal/utils.ts
@@ -8,17 +8,26 @@ export function toPreciseLocaleString(num: number, precision: number) {
   });
 }
 
-/** round down */
+/** round down
+ * @param precision - default: `2`
+ */
 export function rd(num: Dec.Value, precision = 2): string {
   return new Dec(num).toFixed(precision, Dec.ROUND_DOWN);
 }
 
-/** round down to num*/
+/** round down to num
+ *  @param precision - default: `2`
+ *
+ */
 export function rd2num(num: Dec.Value, precision = 2): number {
   return +rd(num, precision);
 }
 
-/** convert numbers to user's number format with precision defined */
+/** convert numbers to user's number format with precision defined
+ * @param precision - default: `2`
+ * @param truncate - default: `false`, whether to shorten large numbers (e.g. 1,200 -> 1.2K)
+ *
+ */
 export function humanize(num: Dec.Value, precision = 2, truncate = false) {
   const _num = new Dec(num);
   const [truncated, suffix] = truncate ? shorten(_num) : [_num, ""];
@@ -31,6 +40,7 @@ export function humanize(num: Dec.Value, precision = 2, truncate = false) {
 /** appropriate number of decimals depending on usd value
  *  e.g. (1usd -> 1usd), 100 cents per usd -> 2 decimals
  *  e.g. (100,000usd -> 1btc), 10,000,000 cents per btc -> 6 decimals
+ *  @param max_decimals - default: `2`
  */
 export function vdec(usd_per_unit: number | number, max_decimals = 2) {
   //get `x` such that (10^x)cents = rate
@@ -43,7 +53,10 @@ export const usdpu = (amount: number, usd_value: number) => {
   return amount > 0 ? usd_value / amount : 0;
 };
 
-/** round up to approriate number of decimals depending on value */
+/** round up to approriate number of decimals depending on value
+ *  @param max_decimals - default: `2`
+ *
+ */
 export function ru_vdec(
   amount: number | string,
   usd_per_unit: number,
