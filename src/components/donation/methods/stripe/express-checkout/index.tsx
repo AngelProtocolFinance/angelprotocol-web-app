e1aacfc242f5bd35b9188c7da197bde99dc0e851
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-11-11 09:18:32 +0800

    overflow never

--- src/components/donation/methods/stripe/express-checkout/index.tsx
+++ src/components/donation/methods/stripe/express-checkout/index.tsx
@@ -29,6 +29,7 @@ export function ExpressCheckout({
         onConfirm={(x) => {
           console.log(x);
         }}
+        options={{ layout: { overflow: "never" } }}
       />
     </Elements>
   );
