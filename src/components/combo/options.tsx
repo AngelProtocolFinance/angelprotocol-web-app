b2e393472e761d58b979da7fd0d7b67ddaaf6efb
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-11-12 11:37:23 +0800

    use accent secondary

--- src/components/combo/options.tsx
+++ src/components/combo/options.tsx
@@ -15,32 +15,20 @@ export function Options({ query, options, option_disp }: Props) {
   return (
     <ComboboxOptions
       anchor={{ to: "bottom", gap: 4 }}
-      className="bg-white w-(--input-width) rounded shadow-2xl/20"
+      className="bg-white w-(--input-width) z-10 rounded shadow-2xl/20"
     >
-      {query && filtered.length === 0 ? (
+      {query && filtered.length === 0 && (
         <div className="p-2 text-sm text-gray-d1">{query} not found</div>
-      ) : null}
+      )}
       {filtered.slice(0, 5).map((v) => (
         <ComboboxOption
-          className="data-selected:bg-blue-l2 hover:bg-blue-l2 flex items-center gap-2 p-2 text-sm "
+          className="data-selected:bg-(--accent-secondary) hover:bg-(--accent-secondary) flex items-center gap-2 p-2 text-sm "
           key={v}
           value={v}
         >
           {option_disp(v)}
         </ComboboxOption>
       ))}
-      {/* {(options.length > 0 &&
-        filtered.slice(0, 5).map((country) => (
-          <ComboboxOption
-            className="data-selected:bg-blue-l2 hover:bg-blue-l2 flex items-center gap-2 p-2 text-sm "
-            key={country}
-            value={country}
-          >
-            {option_disp(country)}
-          </ComboboxOption>
-        ))) || (
-        <div className="p-2 text-sm text-gray-d1">{query} not found</div>
-      )} */}
     </ComboboxOptions>
   );
 }
