9a6635a67f272a99d72999d42dfca8acc7bc642b
ap-justin <89639563+ap-justin@users.noreply.github.com>
2025-11-25 15:23:48 +0800

    remove generate

--- src/pages/admin/integrations/index.tsx
+++ src/pages/admin/integrations/index.tsx
@@ -11,7 +11,7 @@ export default function Page({ loaderData: { apiKey } }: Route.ComponentProps) {
       <div className="mb-4 border border-gray-l3 rounded p-4">
         <h4 className="text-xl font-semibold mb-2 text-[#FF4F00]">Zapier</h4>
         <p className="text-sm text-gray-d1 mb-4">
-          Generate an API key to use with Better Giving Zapier Integration
+          API key to use with Better Giving Zapier Integration
         </p>
         {apiKey ? <Display apiKey={apiKey} /> : <Form />}
       </div>
